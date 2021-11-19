ab9787f63d41d68e67eec03dceb4d89112faf576
AdamProbert <adamprobert@live.co.uk>
2021-11-19 14:01:12 +0000

    Added aws_glue_dev_endpoint to terraform

--- main.tf
+++ main.tf
@@ -4,9 +4,34 @@ provider "aws" {
   region     = var.region
 }
 
+# --- S3 --- #
+
 resource "aws_s3_bucket" "data_engineering_bucket" {
   bucket = var.bucket_name
   acl    = var.acl_value
+}
+
+# --- Glue --- #
+
+resource "aws_iam_role" "glue_service_iam_role" {
+  name               = "AWSGlueServiceRole-data-engineerng"
+  assume_role_policy = data.aws_iam_policy_document.glue_service_iam_policy.json
+}
+
+data "aws_iam_policy_document" "glue_service_iam_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["glue.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "example-AWSGlueServiceRole" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+  role       = aws_iam_role.glue_service_iam_role.name
 }
 
 resource "aws_glue_catalog_database" "aws_glue_catalog_database" {
@@ -22,3 +47,11 @@ resource "aws_glue_crawler" "aws_glue_crawler" {
     path = "s3://${var.bucket_name}"
   }
 }
+
+resource "aws_glue_dev_endpoint" "glue_dev_endpoint" {
+  name     = "data-engineering-dev-endpoint"
+  role_arn = aws_iam_role.glue_service_iam_role.arn
+}
+
+
+
