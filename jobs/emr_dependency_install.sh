3f7612410024127b637922c92d899a150d91892a
AdamProbert <adamprobert@live.co.uk>
2022-01-18 09:06:13 +0000

    Configure nlp jar for use with emr cluster

--- jobs/emr_dependency_install.sh
+++ jobs/emr_dependency_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo aws s3 cp s3://skillsforcare/jars /usr/lib/spark/jars/
+sudo aws s3 cp s3://skillsforcare/jars/dataplatform-external-libs-1.0-SNAPSHOT-jar-with-dependencies.jar /usr/lib/spark/jars/
