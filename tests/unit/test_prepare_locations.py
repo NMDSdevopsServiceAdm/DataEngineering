8be0e7f873cc5b042f64c0e44acae3d885221c07
AdamProbert <adamprobert@live.co.uk>
2022-01-28 10:56:11 +0000

    Uncomment tests

--- tests/unit/test_prepare_locations.py
+++ tests/unit/test_prepare_locations.py
@@ -13,42 +13,41 @@ class PrepareLocationsTests(unittest.TestCase):
             .appName("test_prepare_locations") \
             .getOrCreate()
 
-    # def test_filter_nulls(self):
+    def test_filter_nulls(self):
+        columns = ["locationid", "wkrrecs", "totalstaff"]
+        rows = [
+            ("1-000000001", None, 20),
+            ("1-000000002", 500, 500),
+            ("1-000000003", 100, None),
+            ("1-000000004", None, None),
+            ("1-000000005", 25, 75),
+            (None, 1, 0),
+        ]
+        df = self.spark.createDataFrame(rows, columns)
 
-    #     columns = ["locationid", "wkrrecs", "totalstaff"]
-    #     rows = [
-    #         ("1-000000001", None, 20),
-    #         ("1-000000002", 500, 500),
-    #         ("1-000000003", 100, None),
-    #         ("1-000000004", None, None),
-    #         ("1-000000005", 25, 75),
-    #         (None, 1, 0),
-    #     ]
-    #     df = self.spark.createDataFrame(rows, columns)
+        filtered_df = prepare_locations.filter_nulls(df)
+        self.assertEqual(filtered_df.count(), 4)
+        self.assertEqual(filtered_df.select("locationid").rdd.flatMap(lambda x: x).collect(), [
+                         "1-000000001", "1-000000002", "1-000000003", "1-000000005"])
 
-    #     filtered_df = prepare_locations.filter_nulls(df)
-    #     self.assertEqual(filtered_df.count(), 4)
-    #     self.assertEqual(filtered_df.select("locationid").rdd.flatMap(lambda x: x).collect(), [
-    #                      "1-000000001", "1-000000002", "1-000000003", "1-000000005"])
+    def test_clean(self):
 
-    # def test_clean(self):
+        columns = ["locationid", "wkrrecs", "totalstaff"]
+        rows = [
+            ("1-000000001", None, "0"),
+            ("1-000000002", "500", "500"),
+            ("1-000000003", "100", "-1"),
+            ("1-000000004", None, "0"),
+            ("1-000000005", "25", "75"),
+            (None, "1", "0"),
+        ]
+        df = self.spark.createDataFrame(rows, columns)
 
-    #     columns = ["locationid", "wkrrecs", "totalstaff"]
-    #     rows = [
-    #         ("1-000000001", None, "0"),
-    #         ("1-000000002", "500", "500"),
-    #         ("1-000000003", "100", "-1"),
-    #         ("1-000000004", None, "0"),
-    #         ("1-000000005", "25", "75"),
-    #         (None, "1", "0"),
-    #     ]
-    #     df = self.spark.createDataFrame(rows, columns)
-
-    #     cleaned_df = prepare_locations.clean(df)
-    #     cleaned_df_list = cleaned_df.collect()
-    #     self.assertEqual(cleaned_df.count(), 6)
-    #     self.assertEqual(cleaned_df_list[0]["totalstaff"], None)
-    #     self.assertEqual(cleaned_df_list[1]["totalstaff"], 500)
+        cleaned_df = prepare_locations.clean(df)
+        cleaned_df_list = cleaned_df.collect()
+        self.assertEqual(cleaned_df.count(), 6)
+        self.assertEqual(cleaned_df_list[0]["totalstaff"], None)
+        self.assertEqual(cleaned_df_list[1]["totalstaff"], 500)
 
     def test_calculate_jobcount(self):
         columns = ["locationid", "wkrrecs", "totalstaff", "numberofbeds"]
