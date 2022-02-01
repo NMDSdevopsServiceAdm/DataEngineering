5c90b9657cf5a50769150d919815528c2747bc34
Adam Probert <adamprobert@live.co.uk>
2022-02-01 15:07:18 +0000

    Prepare locations dataset (#10)
    * Added tests for clean and filter functionality
    
    * Added clean, filter_nulls and calculate_job_count functions
    
    * Added extra tests for prepare_locations.clean
    
    * Added regression coefficients and testing examples
    
    * Added remaining regression bouds and testing
    
    * Uncomment tests
    
    * Remove show command
    
    * Adding main function hooking everything together for prepare_locations
    
    * Handle job arguments
    
    * Added prepare_locations script to terraform as glue job
    
    * Added remove duplicates function and corresponding test
    
    * Add missing commas
    
    * Remove partition selection for cqc data
    
    * Remove redundant show command
    
    * Remove f strings
    
    * Add establishmentid and providername
    
    * Update glue job params
    
    * Fix misnamed variable
    
    * Export as parquet instead of csv
    
    * Split out calculate_jobcount into seperate functions with individual tests
    
    * Swap numbers for constants
    
    * Formatting fixes

---
+++ jobs/prepare_locations.py
@@ -0,0 +1,294 @@
+import argparse
+from datetime import date
+
+from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.functions import abs, coalesce, greatest, lit, max, when, col
+from pyspark.sql.types import IntegerType
+from utils import utils
+
+required_workplace_fields = [
+    "locationid",
+    "establishmentid",
+    "providerid",
+    "totalstaff",
+    "wkrrecs",
+    "year",
+    "month",
+    "day",
+    "import_date",
+    "version"
+]
+
+required_cqc_fields = [
+    "locationid",
+    "organisationtype",
+    "type",
+    "name",
+    "registrationstatus",
+    "registrationdate",
+    "deregistrationdate",
+    "dormancy",
+    "numberofbeds",
+    "region",
+    "postalcode",
+    "carehome",
+    "constituency",
+    "localauthority"
+]
+
+MIN_ABSOLUTE_DIFFERENCE = 5
+MIN_PERCENTAGE_DIFFERENCE = 0.1
+
+
+def main(workplace_source, cqc_location_source, cqc_provider_source, destination):
+    spark = utils.get_spark()
+    print(f"Reading workplaces parquet from {workplace_source}")
+    workplaces_df = spark.read.parquet(
+        workplace_source).select(required_workplace_fields)
+
+    workplaces_df = remove_duplicates(workplaces_df)
+    workplaces_df = clean(workplaces_df)
+    workplaces_df = filter_nulls(workplaces_df)
+    workplaces_df = calculate_jobcount(workplaces_df)
+
+    print(f"Reading CQC locations parquet from {cqc_location_source}")
+    cqc_df = spark.read.parquet(
+        cqc_location_source).select(required_cqc_fields)
+
+    print(f"Reading CQC providers parquet from {cqc_provider_source}")
+    cqc_provider_df = spark.read.parquet(
+        cqc_provider_source).select("providerid", col("name").alias("provider_name"))
+
+    output_df = cqc_df.join(workplaces_df, "locationid", "left")
+    output_df = output_df.join(cqc_provider_df, "providerid", "left")
+
+    output_df = calculate_jobcount(output_df)
+
+    print(f"Exporting as parquet to {destination}")
+    utils.write_to_parquet(output_df, destination)
+
+
+def remove_duplicates(input_df):
+    print("Removing duplicates...")
+    return input_df.drop_duplicates(subset=["locationid", "import_date"])
+
+
+def clean(input_df):
+    print("Cleaning...")
+
+    # Standardise negative and 0 values as None.
+    input_df = input_df.replace('0', None).replace('-1', None)
+
+    # Cast integers to string
+    input_df = input_df.withColumn(
+        "totalstaff", input_df["totalstaff"].cast(IntegerType()))
+
+    input_df = input_df.withColumn(
+        "wkrrecs", input_df["wkrrecs"].cast(IntegerType()))
+
+    return input_df
+
+
+def filter_nulls(input_df):
+    print("Filtering nulls...")
+    # Remove rows with null for wkrrecs and totalstaff
+    input_df = input_df.filter("wkrrecs is not null or totalstaff is not null")
+
+    # Remove rows with null locationId
+    input_df = input_df.na.drop(subset=["locationid"])
+
+    return input_df
+
+
+def calculate_jobcount_totalstaff_equal_wkrrecs(input_df):
+    # totalstaff = wkrrrecs: Take totalstaff
+    return input_df.withColumn("jobcount", when(
+        (
+            col("jobcount").isNull() &
+            (col("wkrrecs") == col("totalstaff")) &
+            col("totalstaff").isNotNull() &
+            col("wkrrecs").isNotNull()
+        ), col("totalstaff")
+    ).otherwise(col("jobcount")))
+
+
+def calculate_jobcount_coalesce_totalstaff_wkrrecs(input_df):
+    # Either wkrrecs or totalstaff is null: return first not null
+    return input_df.withColumn("jobcount", when(
+        (
+            col("jobcount").isNull() &
+            (
+                (
+                    col("totalstaff").isNull() &
+                    col("wkrrecs").isNotNull()
+                ) |
+                (
+                    col("totalstaff").isNotNull() &
+                    col("wkrrecs").isNull()
+                )
+            )
+        ), coalesce(input_df.totalstaff, input_df.wkrrecs)
+    ).otherwise(coalesce(col("jobcount"))))
+
+
+def calculate_jobcount_abs_difference_within_range(input_df):
+    # Abs difference between totalstaff & wkrrecs < 5 or < 10% take average:
+    input_df = input_df.withColumn('abs_difference', abs(
+        input_df.totalstaff - input_df.wkrrecs))
+
+    input_df = input_df.withColumn("jobcount", when(
+        (
+            col("jobcount").isNull() &
+            (
+                (col("abs_difference") < MIN_ABSOLUTE_DIFFERENCE) | (
+                    col("abs_difference") / col("totalstaff") < MIN_PERCENTAGE_DIFFERENCE)
+            )
+        ), (col("totalstaff") + col("wkrrecs")) / 2
+    ).otherwise(col("jobcount")))
+
+    input_df = input_df.drop("abs_difference")
+
+    return input_df
+
+
+def calculate_jobcount_handle_tiny_values(input_df):
+    # totalstaff or wkrrecs < 3: return max
+    return input_df.withColumn("jobcount", when(
+        (
+            col("jobcount").isNull() &
+            (
+                (col("totalstaff") < 3) | (col("wkrrecs") < 3)
+            )
+        ), greatest(col("totalstaff"), col("wkrrecs"))
+    ).otherwise(col("jobcount")))
+
+
+def calculate_jobcount_estimate_from_beds(input_df):
+    beds_to_jobcount_intercept = 8.40975704621392
+    beds_to_jobcount_coefficient = 1.0010753137758377001
+    input_df = input_df.withColumn("bed_estimate_jobcount", when(
+        (
+            col("jobcount").isNull() &
+            (col("numberofbeds") > 0)
+        ), (beds_to_jobcount_intercept + (col("numberofbeds") * beds_to_jobcount_coefficient))
+    ).otherwise(None))
+
+    # Determine differences
+    input_df = input_df.withColumn("totalstaff_diff", abs(
+        input_df.totalstaff - input_df.bed_estimate_jobcount))
+    input_df = input_df.withColumn("wkrrecs_diff", abs(
+        input_df.wkrrecs - input_df.bed_estimate_jobcount))
+    input_df = input_df.withColumn("totalstaff_percentage_diff", abs(
+        input_df.totalstaff_diff/input_df.bed_estimate_jobcount))
+    input_df = input_df.withColumn("wkrrecs_percentage_diff", abs(
+        input_df.wkrrecs/input_df.bed_estimate_jobcount))
+
+    # Bounding predictions to certain locations with differences in range
+    # if totalstaff and wkrrecs within 10% or < 5: return avg(totalstaff + wkrrds)
+    input_df = input_df.withColumn("jobcount", when(
+        (
+            col("jobcount").isNull() &
+            col("bed_estimate_jobcount").isNotNull() &
+            (
+                (
+                    (col("totalstaff_diff") < MIN_ABSOLUTE_DIFFERENCE) | (
+                        col("totalstaff_percentage_diff") < MIN_PERCENTAGE_DIFFERENCE)
+                ) &
+                (
+                    (col("wkrrecs_diff") < MIN_ABSOLUTE_DIFFERENCE) | (
+                        col("wkrrecs_percentage_diff") < MIN_PERCENTAGE_DIFFERENCE)
+                )
+            )
+        ), (col("totalstaff") + col("wkrrecs")) / 2
+    ).otherwise(col("jobcount")))
+
+    # if totalstaff within 10% or < 5: return totalstaff
+    input_df = input_df.withColumn("jobcount", when(
+        (
+            col("jobcount").isNull() &
+            col("bed_estimate_jobcount").isNotNull() &
+            (
+
+                (col("totalstaff_diff") < MIN_ABSOLUTE_DIFFERENCE) | (
+                    col("totalstaff_percentage_diff") < MIN_PERCENTAGE_DIFFERENCE)
+
+            )
+        ), col("totalstaff")
+    ).otherwise(col("jobcount")))
+
+    # if wkrrecs within 10% or < 5: return wkrrecs
+    input_df = input_df.withColumn("jobcount", when(
+        (
+            col("jobcount").isNull() &
+            col("bed_estimate_jobcount").isNotNull() &
+            (
+
+                (col("wkrrecs_diff") < MIN_ABSOLUTE_DIFFERENCE) | (
+                    col("wkrrecs_percentage_diff") < MIN_PERCENTAGE_DIFFERENCE)
+
+            )
+        ), col("wkrrecs")
+    ).otherwise(col("jobcount")))
+
+    # Drop temporary columns
+    columns_to_drop = [
+        "bed_estimate_jobcount",
+        "totalstaff_diff",
+        "wkrrecs_diff",
+        "totalstaff_percentage_diff",
+        "wkrrecs_percentage_diff"
+    ]
+
+    input_df = input_df.drop(*columns_to_drop)
+
+    return input_df
+
+
+def calculate_jobcount(input_df):
+    print("Calculating jobcount...")
+
+    # Add null/empty jobcount column
+    input_df = input_df.withColumn("jobcount", lit(None).cast(IntegerType()))
+
+    input_df = calculate_jobcount_totalstaff_equal_wkrrecs(input_df)
+    input_df = calculate_jobcount_coalesce_totalstaff_wkrrecs(input_df)
+    input_df = calculate_jobcount_abs_difference_within_range(input_df)
+    input_df = calculate_jobcount_handle_tiny_values(input_df)
+    input_df = calculate_jobcount_estimate_from_beds(input_df)
+
+    return input_df
+
+
+def collect_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--workplace_source",
+        help="Source s3 directory for ASCWDS workplace dataset",
+        required=True
+    )
+    parser.add_argument(
+        "--cqc_location_source",
+        help="Source s3 directory for CQC locations api dataset",
+        required=True
+    )
+    parser.add_argument(
+        "--cqc_provider_source",
+        help="Source s3 directory for CQC providers api dataset",
+        required=True
+    )
+    parser.add_argument(
+        "--destination",
+        help="A destination directory for outputting cqc locations, if not provided shall default to S3 todays date.",
+        required=True
+    )
+
+    args, unknown = parser.parse_known_args()
+
+    return args.workplace_source, args.cqc_location_source, args.cqc_provider_source, args.destination
+
+
+if __name__ == "__main__":
+    workplace_source, cqc_location_source, cqc_provider_source, destination, = collect_arguments()
+    main(workplace_source, cqc_location_source,
+         cqc_provider_source, destination)
---
+++ tests/unit/test_prepare_locations.py
@@ -0,0 +1,189 @@
+import shutil
+from pathlib import Path
+import unittest
+from jobs import format_fields, prepare_locations
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType, IntegerType, StructType, StructField, DoubleType
+
+
+class PrepareLocationsTests(unittest.TestCase):
+
+    calculate_jobs_schema = StructType(
+        [
+            StructField('locationid', StringType(), False),
+            StructField(
+                'totalstaff', IntegerType(), True),
+            StructField(
+                'wkrrecs', IntegerType(), True),
+            StructField(
+                'numberofbeds', IntegerType(), True),
+            StructField(
+                'jobcount', DoubleType(), True)
+        ]
+    )
+
+    def setUp(self):
+        self.spark = SparkSession.builder \
+            .appName("test_prepare_locations") \
+            .getOrCreate()
+
+    def test_remove_duplicates(self):
+        columns = ["locationid", "import_date"]
+        rows = [
+            ("1-000000001", "01-12-2022"),
+            ("1-000000001", "01-12-2022"),
+            ("1-000000001", "01-12-2021"),
+            ("1-000000001", "01-12-2021"),
+            ("1-000000002", "01-12-2022"),
+            ("1-000000002", "01-12-2021")
+        ]
+        df = self.spark.createDataFrame(rows, columns)
+
+        filtered_df = prepare_locations.remove_duplicates(df)
+        self.assertEqual(filtered_df.count(), 4)
+        self.assertEqual(filtered_df.select("locationid").rdd.flatMap(lambda x: x).collect(), [
+                         "1-000000001", "1-000000001", "1-000000002", "1-000000002"])
+
+    def test_filter_nulls(self):
+        columns = ["locationid", "wkrrecs", "totalstaff"]
+        rows = [
+            ("1-000000001", None, 20),
+            ("1-000000002", 500, 500),
+            ("1-000000003", 100, None),
+            ("1-000000004", None, None),
+            ("1-000000005", 25, 75),
+            (None, 1, 0),
+        ]
+        df = self.spark.createDataFrame(rows, columns)
+
+        filtered_df = prepare_locations.filter_nulls(df)
+        self.assertEqual(filtered_df.count(), 4)
+        self.assertEqual(filtered_df.select("locationid").rdd.flatMap(lambda x: x).collect(), [
+                         "1-000000001", "1-000000002", "1-000000003", "1-000000005"])
+
+    def test_clean(self):
+
+        columns = ["locationid", "wkrrecs", "totalstaff"]
+        rows = [
+            ("1-000000001", None, "0"),
+            ("1-000000002", "500", "500"),
+            ("1-000000003", "100", "-1"),
+            ("1-000000004", None, "0"),
+            ("1-000000005", "25", "75"),
+            (None, "1", "0"),
+        ]
+        df = self.spark.createDataFrame(rows, columns)
+
+        cleaned_df = prepare_locations.clean(df)
+        cleaned_df_list = cleaned_df.collect()
+        self.assertEqual(cleaned_df.count(), 6)
+        self.assertEqual(cleaned_df_list[0]["totalstaff"], None)
+        self.assertEqual(cleaned_df_list[1]["totalstaff"], 500)
+
+    def test_calculate_jobcount_totalstaff_equal_wkrrecs(self):
+        columns = ["locationid", "wkrrecs",
+                   "totalstaff", "numberofbeds", "jobcount"]
+        rows = [
+            ("1-000000001", 20, 20, 25, None),
+        ]
+        df = self.spark.createDataFrame(
+            data=rows, schema=self.calculate_jobs_schema)
+
+        df = prepare_locations.calculate_jobcount_totalstaff_equal_wkrrecs(df)
+        self.assertEqual(df.count(), 1)
+
+        df = df.collect()
+        self.assertEqual(df[0]["jobcount"], 20)
+
+    def test_calculate_jobcount_coalesce_totalstaff_wkrrecs(self):
+        rows = [
+            ("1-000000001", None, 50, 15, None),
+        ]
+        df = self.spark.createDataFrame(
+            data=rows, schema=self.calculate_jobs_schema)
+
+        df = prepare_locations.calculate_jobcount_coalesce_totalstaff_wkrrecs(
+            df)
+        self.assertEqual(df.count(), 1)
+
+        df = df.collect()
+        self.assertEqual(df[0]["jobcount"], 50)
+
+    def test_calculate_jobcount_abs_difference_within_range(self):
+        rows = [
+            ("1-000000008", 10, 12, 15, None),
+            ("1-000000001", 100, 109, 80, None),
+        ]
+        df = self.spark.createDataFrame(
+            data=rows, schema=self.calculate_jobs_schema)
+
+        df = prepare_locations.calculate_jobcount_abs_difference_within_range(
+            df)
+        self.assertEqual(df.count(), 2)
+
+        df = df.collect()
+        self.assertEqual(df[0]["jobcount"], 11)
+        self.assertEqual(df[1]["jobcount"], 104.5)
+
+    def test_calculate_jobcount_handle_tiny_values(self):
+        rows = [
+            ("1-000000008", 2, 53, 26, None),
+        ]
+        df = self.spark.createDataFrame(
+            data=rows, schema=self.calculate_jobs_schema)
+
+        df = prepare_locations.calculate_jobcount_handle_tiny_values(
+            df)
+        self.assertEqual(df.count(), 1)
+
+        df = df.collect()
+        self.assertEqual(df[0]["jobcount"], 53)
+
+    def test_calculate_jobcount(self):
+        columns = ["locationid", "wkrrecs", "totalstaff", "numberofbeds"]
+        rows = [
+            ("1-000000001", None, 0, 0),  # Both 0: Return 0
+            # Both 500: Return 500
+            ("1-000000002", 500, 500, 490),
+            # Only know wkrrecs: Return wkrrecs (100)
+            ("1-000000003", 100, None, 10),
+            # Only know totalstaff: Return totalstaf (10)
+            ("1-000000004", None, 10, 12),
+            # None of the rules apply: Return None
+            ("1-000000005", 25, 75, 40),
+            # None of the rules apply: Return None
+            ("1-000000006", 30, 60, 40),
+            # None of the rules apply: Return None
+            ("1-000000007", 600, 900, 150),
+            # Absolute difference is within 10%: Return Average
+            ("1-000000008", 10, 12, None),
+            # Either totalstaff or wkrrecs < 3: return max
+            ("1-000000009", 1, 23, None),
+            # Utilise bedcount estimate - Average
+            ("1-000000010", 90, 102, 85),
+            # Utilise bedcount estimate - Wkrrecs
+            ("1-000000011", 90, 102, 95),
+            # Utilise bedcount estimate - Totalstaff
+            ("1-000000012", 90, 102, 80)
+        ]
+        df = self.spark.createDataFrame(rows, columns)
+
+        jobcount_df = prepare_locations.calculate_jobcount(df)
+        jobcount_df_list = jobcount_df.collect()
+
+        self.assertEqual(jobcount_df_list[0]["jobcount"], 0.0)
+        self.assertEqual(jobcount_df_list[1]["jobcount"], 500.0)
+        self.assertEqual(jobcount_df_list[2]["jobcount"], 100.0)
+        self.assertEqual(jobcount_df_list[3]["jobcount"], 10.0)
+        self.assertEqual(jobcount_df_list[4]["jobcount"], None)
+        self.assertEqual(jobcount_df_list[5]["jobcount"], None)
+        self.assertEqual(jobcount_df_list[6]["jobcount"], None)
+        self.assertEqual(jobcount_df_list[7]["jobcount"], 11.0)
+        self.assertEqual(jobcount_df_list[8]["jobcount"], 23.0)
+        self.assertEqual(jobcount_df_list[9]["jobcount"], 96.0)
+        self.assertEqual(jobcount_df_list[10]["jobcount"], 102.0)
+        self.assertEqual(jobcount_df_list[11]["jobcount"], 90.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
