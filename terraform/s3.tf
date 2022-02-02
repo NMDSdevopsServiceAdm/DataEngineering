f8dea4b5f464c39297df949d37c29c0f7db29733
AdamProbert <adamprobert@live.co.uk>
2022-02-02 17:00:53 +0000

    Encrypt s3 bucket with default amazon s3 master-key

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,4 +4,11 @@ resource "aws_s3_bucket" "data_engineering_bucket" {
   versioning {
     enabled = true
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
