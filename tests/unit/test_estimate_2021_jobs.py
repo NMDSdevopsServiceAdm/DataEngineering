7fce0a7838496622db527b6fc169c34677f50ef2
Adam Probert <adamprobert@live.co.uk>
2022-02-15 14:29:38 +0000

    Added test for populating known 2021 jobs

--- tests/unit/test_estimate_2021_jobs.py
+++ tests/unit/test_estimate_2021_jobs.py
@@ -38,8 +38,48 @@ class Estimate2021JobsTests(unittest.TestCase):
         self.assertEqual(df[2]["primary_service_type"], "non-residential")
         self.assertEqual(df[3]["primary_service_type"], "non-residential")
 
-    def test_collect_ascwds_historical_job_figures(self):
-        pass
+    def test_model_populate_known_2021_jobs(self):
+        columns = ["locationid", "jobcount_2021", "estimate_jobcount_2021"]
+        rows = [
+            ("1-000000001", 1, None),
+            ("1-000000002", None, None),
+            ("1-000000003", 5, 4),
+            ("1-000000003", 10, None),
+        ]
+        df = self.spark.createDataFrame(rows, columns)
+
+        df = estimate_2021_jobs.model_populate_known_2021_jobs(df)
+        self.assertEqual(df.count(), 4)
+
+        df = df.collect()
+        self.assertEqual(df[0]["estimate_jobcount_2021"], 1)
+        self.assertEqual(df[1]["estimate_jobcount_2021"], None)
+        self.assertEqual(df[2]["estimate_jobcount_2021"], 4)
+        self.assertEqual(df[3]["estimate_jobcount_2021"], 10)
+
+    def test_model_non_res_historical(self):
+        columns = [
+            "locationid",
+            "primary_service_type",
+            "last_known_job_count",
+            "estimate_jobcount_2021",
+        ]
+        rows = [
+            ("1-000000001", "non-residential", 10, None),
+            ("1-000000002", "Care hom", 10, None),
+            ("1-000000003", "non-residential", 10, None),
+            ("1-000000004", "non-residential", 10, None),
+        ]
+        df = self.spark.createDataFrame(rows, columns)
+
+        df = estimate_2021_jobs.model_populate_known_2021_jobs(df)
+        self.assertEqual(df.count(), 4)
+
+        df = df.collect()
+        self.assertEqual(df[0]["estimate_jobcount_2021"], 1)
+        self.assertEqual(df[1]["estimate_jobcount_2021"], None)
+        self.assertEqual(df[2]["estimate_jobcount_2021"], 4)
+        self.assertEqual(df[3]["estimate_jobcount_2021"], 10)
 
 
 if __name__ == "__main__":
