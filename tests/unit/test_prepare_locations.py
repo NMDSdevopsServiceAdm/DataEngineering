6ade110d3446bcb2bd21611302752218c8f7ffe7
Adam Probert <adamprobert@live.co.uk>
2022-03-02 11:48:31 +0000

    Removed format_fields test

--- tests/unit/test_prepare_locations.py
+++ tests/unit/test_prepare_locations.py
@@ -30,19 +30,6 @@ class PrepareLocationsTests(unittest.TestCase):
     def setUp(self):
         self.spark = SparkSession.builder.appName("test_prepare_locations").getOrCreate()
 
-    def test_format_date(self):
-        columns = ["locationid", "import_date"]
-        rows = [("1-000000001", 20221201), ("1-000000002", 20220328)]
-        df = self.spark.createDataFrame(rows, columns)
-
-        formatted_df = prepare_locations.format_date(df, "import_date", "new_formatted_date")
-
-        self.assertEqual(formatted_df.count(), 2)
-        self.assertEqual(
-            formatted_df.select("new_formatted_date").rdd.flatMap(lambda x: x).collect(),
-            [datetime.date(2022, 12, 1), datetime.date(2022, 3, 28)],
-        )
-
     def test_remove_duplicates(self):
         columns = ["locationid", "ascwds_workplace_import_date"]
         rows = [
