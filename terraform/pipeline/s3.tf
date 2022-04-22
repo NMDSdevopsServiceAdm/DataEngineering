fc625eeb757c6cb64b8fab73d7cf616f1b8eace0
Adam Probert <adamprobert@live.co.uk>
2022-04-22 14:19:07 +0100

    Added darasets bucket

--- terraform/pipeline/s3.tf
+++ terraform/pipeline/s3.tf
@@ -2,3 +2,8 @@ module "pipeline_resources" {
   source = "../modules/s3-bucket"
   bucket_name = "pipeline-resources"
 }
+
+module "datasets_bucket" {
+  source = "../modules/s3-bucket"
+  bucket_name = "datasets"
+}
