73cb9d6785d7c8a1b5fb44d76cde8d872c21d9f9
Adam Probert <adamprobert@live.co.uk>
2022-05-05 17:03:24 +0100

    Re-add generate worker file

--- tests/test_file_generator.py
+++ tests/test_file_generator.py
@@ -37,6 +37,36 @@ def generate_ethnicity_parquet(output_destination):
     return df
 
 
+def generate_worker_parquet(output_destination):
+    spark = utils.get_spark()
+    columns = ["locationid", "workerid", "mainjrid"]
+
+    rows = [
+        ("1-000000001", "100", 1),
+        ("1-000000001", "101", 1),
+        ("1-000000001", "102", 1),
+        ("1-000000001", "103", 1),
+        ("1-000000001", "104", 2),
+        ("1-000000001", "105", 3),
+        ("1-000000002", "106", 1),
+        ("1-000000002", "107", 3),
+        ("1-000000002", "108", 2),
+        ("1-000000003", "109", 1),
+        ("1-000000003", "110", 2),
+        ("1-000000003", "111", 3),
+        ("1-000000004", "112", 1),
+        ("1-000000004", "113", 2),
+        ("1-000000004", "114", 3),
+    ]
+
+    df = spark.createDataFrame(rows, columns)
+
+    if output_destination:
+        df.coalesce(1).write.mode("overwrite").parquet(output_destination)
+
+    return df
+
+
 def generate_all_job_roles_parquet(output_destination):
     spark = utils.get_spark()
     columns = ["master_locationid", "primary_service_type", "main_job_role", "estimate_job_role_count_2021"]
