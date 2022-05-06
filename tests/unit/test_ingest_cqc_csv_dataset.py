3b3660f3852ba48c7fff0594e5120b758fd607c5
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2022-05-06 14:34:02 +0100

    test for unique providerids with array of locationids

--- tests/unit/test_ingest_cqc_csv_dataset.py
+++ tests/unit/test_ingest_cqc_csv_dataset.py
@@ -4,35 +4,42 @@ import unittest
 from pyspark.sql import SparkSession
 
 from jobs import ingest_cqc_csv_dataset
-from tests.test_file_generator import generate_prov_loc_csv
 
 
 class CQC_Care_Directory_Tests(unittest.TestCase):
-
-    TEST_CQC_PROV_LOC_FILE = "tests/test_data/tmp/cqc_care_directory.csv"
-
     def setUp(self):
-        self.spark = SparkSession.builder.appName("test_cqc_care_directory_to_parquet").getOrCreate()
-        generate_prov_loc_csv(self.TEST_CQC_PROV_LOC_FILE)
-
-    def tearDown(self):
-        try:
-            shutil.rmtree(self.TEST_CQC_PROV_LOC_FILE)
-        except OSError():
-            pass  # Ignore dir does not exist
+        self.spark = SparkSession.builder.appName("test_ingest_cqc_care_directory").getOrCreate()
 
     def test_get_all_job_roles_per_location_df(self):
-        pass
+        columns = ["providerid", "locationid", "other_cols"]
 
-        # all_job_roles_df = ingest_cqc_csv_dataset.unique_providers_with_locations(self.TEST_CQC_CARE_DIRECTORY_FILE)
+        rows = [
+            ("1-000000001", "1-000000001", "other_data"),
+            ("1-000000002", "1-000000002", "other_data"),
+            ("1-000000002", "1-000000003", "other_data"),
+            ("1-000000003", "1-000000004", "other_data"),
+            ("1-000000003", "1-000000005", "other_data"),
+            ("1-000000003", "1-000000006", "other_data"),
+            ("1-000000004", "1-000000007", "other_data"),
+            ("1-000000004", "1-000000008", "other_data"),
+            ("1-000000004", "1-000000009", "other_data"),
+            ("1-000000004", "1-000000010", "other_data"),
+        ]
 
-        # self.assertEqual(all_job_roles_df.count(), 15)
-        # self.assertEqual(
-        #     all_job_roles_df.columns, ["master_locationid", "primary_service_type", "main_job_role", "estimated_jobs"]
-        # )
+        df = self.spark.createDataFrame(rows, columns)
 
-        # census_df = census_df.collect()
-        # self.assertEqual(census_df[0]["lsoa"], "E01000001")
+        locations_at_prov_df = ingest_cqc_csv_dataset.unique_providers_with_locations(df)
+
+        self.assertEqual(locations_at_prov_df.count(), 4)
+        self.assertEqual(locations_at_prov_df.columns, ["providerid", "locationids"])
+
+        locations_at_prov_df = locations_at_prov_df.collect()
+        self.assertEqual(sorted(locations_at_prov_df[0]["locationids"]), ["1-000000001"])
+        self.assertEqual(sorted(locations_at_prov_df[1]["locationids"]), ["1-000000002", "1-000000003"])
+        self.assertEqual(sorted(locations_at_prov_df[2]["locationids"]), ["1-000000004", "1-000000005", "1-000000006"])
+        self.assertEqual(
+            sorted(locations_at_prov_df[3]["locationids"]), ["1-000000007", "1-000000008", "1-000000009", "1-000000010"]
+        )
 
     def test_main(self):
         pass
