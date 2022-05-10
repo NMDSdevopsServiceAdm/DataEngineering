0adfe10162b74626b5d78b2ed52267f2c0572e43
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2022-05-10 16:19:07 +0100

    tidied up locations per provider test

--- tests/unit/test_ingest_cqc_care_directory.py
+++ tests/unit/test_ingest_cqc_care_directory.py
@@ -1,7 +1,7 @@
 import shutil
 import unittest
 
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Row
 
 from jobs import ingest_cqc_care_directory
 
@@ -33,29 +33,20 @@ class CQC_Care_Directory_Tests(unittest.TestCase):
             ("1-000000001", "1-000000001", "other_data"),
             ("1-000000002", "1-000000002", "other_data"),
             ("1-000000002", "1-000000003", "other_data"),
-            ("1-000000003", "1-000000004", "other_data"),
-            ("1-000000003", "1-000000005", "other_data"),
-            ("1-000000003", "1-000000006", "other_data"),
-            ("1-000000004", "1-000000007", "other_data"),
-            ("1-000000004", "1-000000008", "other_data"),
-            ("1-000000004", "1-000000009", "other_data"),
-            ("1-000000004", "1-000000010", "other_data"),
         ]
 
         df = self.spark.createDataFrame(rows, columns)
 
         locations_at_prov_df = ingest_cqc_care_directory.unique_providers_with_locations(df)
 
-        self.assertEqual(locations_at_prov_df.count(), 4)
+        self.assertEqual(locations_at_prov_df.count(), 2)
         self.assertEqual(locations_at_prov_df.columns, ["providerid", "locationids"])
 
+        provider1check_df = locations_at_prov_df.filter("providerid=='1-000000001'").select("locationids")
+        self.assertEqual(provider1check_df.collect(), [Row(locationids=["1-000000001"])])
+
         locations_at_prov_df = locations_at_prov_df.collect()
-        self.assertEqual(sorted(locations_at_prov_df[0]["locationids"]), ["1-000000001"])
         self.assertEqual(sorted(locations_at_prov_df[1]["locationids"]), ["1-000000002", "1-000000003"])
-        self.assertEqual(sorted(locations_at_prov_df[2]["locationids"]), ["1-000000004", "1-000000005", "1-000000006"])
-        self.assertEqual(
-            sorted(locations_at_prov_df[3]["locationids"]), ["1-000000007", "1-000000008", "1-000000009", "1-000000010"]
-        )
 
     def test_get_distinct_provider_info(self):
         columns = [
