3fbd9da80710603ba889a4777cd8dd6d9b822888
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2022-05-10 15:37:50 +0100

    generic function to add both servicetypes and specialsims

--- jobs/ingest_cqc_care_directory.py
+++ jobs/ingest_cqc_care_directory.py
@@ -21,6 +21,41 @@ SPECIALISMS_DICT = {
     "Caring for adults under 65 yrs": "Service_user_band_Younger_Adults",
 }
 
+GACSERVICETYPES_DICT = {
+    '["Hospital", "Acute services with overnight beds"]': "Service_type_Acute_services_with_overnight_beds",
+    '["Clinic", "Acute services without overnight beds / listed acute services with or without overnight beds"]': "Service_type_Acute_services_without_overnight_beds__listed_acute_services_with_or_without_overnight_beds",
+    '["Ambulances", "Ambulance service"]': "Service_type_Ambulance_service",
+    '["Blood and transplant service", "Blood and Transplant service"]': "Service_type_Blood_and_Transplant_service",
+    '["Nursing homes", "Care home service with nursing"]': "Service_type_Care_home_service_with_nursing",
+    '["Residential homes", "Care home service without nursing"]': "Service_type_Care_home_service_without_nursing",
+    '["Community services - Substance abuse", "Community based services for people who misuse substances"]': "Service_type_Community_based_services_for_people_who_misuse_substances",
+    '["Community services - Learning disabilities", "Community based services for people with a learning disability"]': "Service_type_Community_based_services_for_people_with_a_learning_disability",
+    '["Community services - Mental Health", "Community based services for people with mental health needs"]': "Service_type_Community_based_services_for_people_with_mental_health_needs",
+    '["Community services - Nursing", "Community health care services - Nurses Agency only"]': "Service_type_Community_health_care_services_Nurses_Agency_only",
+    '["Community services - Healthcare", "Community healthcare service"]': "Service_type_Community_healthcare_service",
+    '["Dentist", "Dental service"]': "Service_type_Dental_service",
+    '["Diagnosis/screening", "Diagnostic and/or screening service"]': "Service_type_Diagnostic_andor_screening_service",
+    '["Diagnosis/screening", "Diagnostic and/or screening service - single handed sessional providers"]': "Service_type_Diagnostic_andor_screening_service_single_handed_sessional_providers",
+    '["Doctors/Gps", "Doctors consultation service"]': "Service_type_Doctors_consultation_service",
+    '["Doctors/Gps", "Doctors treatment service"]': "Service_type_Doctors_treatment_service",
+    '["Homecare agencies", "Domiciliary care service"]': "Service_type_Domiciliary_care_service",
+    '["Supported housing", "Extra Care housing services"]': "Service_type_Extra_Care_housing_services",
+    '["Hospice", "Hospice services"]': "Service_type_Hospice_services",
+    '["Home hospice care", "Hospice services at home"]': "Service_type_Hospice_services_at_home",
+    '["Hospitals - Mental health/capacity", "Hospital services for people with mental health needs", "learning disabilities and problems with substance misuse"]': "Service_type_Hospital_services_for_people_with_mental_health_needs_learning_disabilities_and_problems_with_substance_misuse",
+    '["Hyperbaric chamber services", "Hyperbaric Chamber"]': "Service_type_Hyperbaric_Chamber",
+    '["Long-term conditions", "Long term conditions services"]': "Service_type_Long_term_conditions_services",
+    '["Mobile doctors", "Mobile doctors service"]': "Service_type_Mobile_doctors_service",
+    '["Prison healthcare", "Prison Healthcare Services"]': "Service_type_Prison_Healthcare_Services",
+    '["Rehabilitation (illness/injury)", "Rehabilitation services"]': "Service_type_Rehabilitation_services",
+    '["Phone/online advice", "Remote clinical advice service"]': "Service_type_Remote_clinical_advice_service",
+    '["Rehabilitation (substance abuse)", "Residential substance misuse treatment and/or rehabilitation service"]': "Service_type_Residential_substance_misuse_treatment_andor_rehabilitation_service",
+    '["Shared lives", "Shared Lives"]': "Service_type_Shared_Lives",
+    '["Specialist college service", "Specialist college service"]': "Service_type_Specialist_college_service",
+    '["Supported living", "Supported living service"]': "Service_type_Supported_living_service",
+    '["Urgent care centres", "Urgent care services"]': "Service_type_Urgent_care_services",
+}
+
 
 def main(source, provider_destination=None, location_destination=None):
     return_datasets = []
@@ -47,11 +82,11 @@ def main(source, provider_destination=None, location_destination=None):
     print("Create CQC location parquet file")
     location_df = get_general_location_info(df)
 
-    regulatedactivities_df = get_regulatedactivities(df)
-    gacservicetypes_df = get_gacservicetypes(df)
-    specialisms_df = get_specialisms(df)
+    # regulatedactivities_df = get_regulatedactivities(df)
+    gacservicetypes_df = reformat_cols(df, GACSERVICETYPES_DICT, "gacservicetypes")
+    specialisms_df = reformat_cols(df, SPECIALISMS_DICT, "specialisms")
 
-    location_df = location_df.join(regulatedactivities_df, "locationid")
+    # location_df = location_df.join(regulatedactivities_df, "locationid")
     location_df = location_df.join(gacservicetypes_df, "locationid")
     location_df = location_df.join(specialisms_df, "locationid")
 
@@ -122,148 +157,46 @@ def get_general_location_info(df):
     return loc_info_df
 
 
-def get_regulatedactivities(df):
-    regulatedactivities_df = df.select(
-        "locationid",
-        "registered_manager_name",
-        "Regulated_activity_Accommodation_and_nursing_or_personal_care_in_the_further_education_sector",
-        "Regulated_activity_Accommodation_for_persons_who_require_nursing_or_personal_care",
-        "Regulated_activity_Accommodation_for_persons_who_require_treatment_for_substance_misuse",
-        "Regulated_activity_Assessment_or_medical_treatment_for_persons_detained_under_the_Mental_Health_Act_1983",
-        "Regulated_activity_Diagnostic_and_screening_procedures",
-        "Regulated_activity_Family_planning",
-        "Regulated_activity_Management_of_supply_of_blood_and_blood_derived_products",
-        "Regulated_activity_Maternity_and_midwifery_services",
-        "Regulated_activity_Nursing_care",
-        "Regulated_activity_Personal_care",
-        "Regulated_activity_Services_in_slimming_clinics",
-        "Regulated_activity_Surgical_procedures",
-        "Regulated_activity_Termination_of_pregnancies",
-        "Regulated_activity_Transport_services_triage_and_medical_advice_provided_remotely",
-        "Regulated_activity_Treatment_of_disease_disorder_or_injury",
-    )
+# def get_regulatedactivities(df):
+#     regulatedactivities_df = df.select(
+#         "locationid",
+#         "registered_manager_name",
+#         "Regulated_activity_Accommodation_and_nursing_or_personal_care_in_the_further_education_sector",
+#         "Regulated_activity_Accommodation_for_persons_who_require_nursing_or_personal_care",
+#         "Regulated_activity_Accommodation_for_persons_who_require_treatment_for_substance_misuse",
+#         "Regulated_activity_Assessment_or_medical_treatment_for_persons_detained_under_the_Mental_Health_Act_1983",
+#         "Regulated_activity_Diagnostic_and_screening_procedures",
+#         "Regulated_activity_Family_planning",
+#         "Regulated_activity_Management_of_supply_of_blood_and_blood_derived_products",
+#         "Regulated_activity_Maternity_and_midwifery_services",
+#         "Regulated_activity_Nursing_care",
+#         "Regulated_activity_Personal_care",
+#         "Regulated_activity_Services_in_slimming_clinics",
+#         "Regulated_activity_Surgical_procedures",
+#         "Regulated_activity_Termination_of_pregnancies",
+#         "Regulated_activity_Transport_services_triage_and_medical_advice_provided_remotely",
+#         "Regulated_activity_Treatment_of_disease_disorder_or_injury",
+#     )
 
-    return regulatedactivities_df
-
-
-def get_gacservicetypes(df):
-    gacservicetypes_df = df.select(
-        "locationid",
-        "Service_type_Acute_services_with_overnight_beds",
-        "Service_type_Acute_services_without_overnight_beds__listed_acute_services_with_or_without_overnight_beds",
-        "Service_type_Ambulance_service",
-        "Service_type_Blood_and_Transplant_service",
-        "Service_type_Care_home_service_with_nursing",
-        "Service_type_Care_home_service_without_nursing",
-        "Service_type_Community_based_services_for_people_who_misuse_substances",
-        "Service_type_Community_based_services_for_people_with_a_learning_disability",
-        "Service_type_Community_based_services_for_people_with_mental_health_needs",
-        "Service_type_Community_health_care_services_Nurses_Agency_only",
-        "Service_type_Community_healthcare_service",
-        "Service_type_Dental_service",
-        "Service_type_Diagnostic_andor_screening_service",
-        "Service_type_Diagnostic_andor_screening_service_single_handed_sessional_providers",
-        "Service_type_Doctors_consultation_service",
-        "Service_type_Doctors_treatment_service",
-        "Service_type_Domiciliary_care_service",
-        "Service_type_Extra_Care_housing_services",
-        "Service_type_Hospice_services",
-        "Service_type_Hospice_services_at_home",
-        "Service_type_Hospital_services_for_people_with_mental_health_needs_learning_disabilities_and_problems_with_substance_misuse",
-        "Service_type_Hyperbaric_Chamber",
-        "Service_type_Long_term_conditions_services",
-        "Service_type_Mobile_doctors_service",
-        "Service_type_Prison_Healthcare_Services",
-        "Service_type_Rehabilitation_services",
-        "Service_type_Remote_clinical_advice_service",
-        "Service_type_Residential_substance_misuse_treatment_andor_rehabilitation_service",
-        "Service_type_Shared_Lives",
-        "Service_type_Specialist_college_service",
-        "Service_type_Supported_living_service",
-        "Service_type_Urgent_care_services",
-    )
-
-    # BELOW SHOWS HOW THEY APPEAR IN THE API
-
-    # [Hospital, Acute services with overnight beds]
-    # [Clinic, Acute services without overnight beds / listed acute services with or without overnight beds]
-    # [Ambulances, Ambulance service]
-    # [Blood and transplant service, Blood and Transplant service]
-    # [Nursing homes, Care home service with nursing]
-    # [Residential homes, Care home service without nursing]
-    # [Community services - Substance abuse, Community based services for people who misuse substances]
-    # [Community services - Learning disabilities, Community based services for people with a learning disability]
-    # [Community services - Mental Health, Community based services for people with mental health needs]
-    # [Community services - Nursing, Community health care services - Nurses Agency only]
-    # [Community services - Healthcare, Community healthcare service]
-    # [Dentist, Dental service]
-    # [Diagnosis/screening, Diagnostic and/or screening service]
-    # [Diagnosis/screening, Diagnostic and/or screening service - single handed sessional providers]
-    # [Doctors/Gps, Doctors consultation service]
-    # [Doctors/Gps, Doctors treatment service]
-    # [Homecare agencies, Domiciliary care service]
-    # [Supported housing, Extra Care housing services]
-    # [Hospice, Hospice services]
-    # [Home hospice care, Hospice services at home]
-    # [Hospitals - Mental health/capacity, Hospital services for people with mental health needs, learning disabilities and problems with substance misuse]
-    # [Hyperbaric chamber services, Hyperbaric Chamber]
-    # [Long-term conditions, Long term conditions services]
-    # [Mobile doctors, Mobile doctors service]
-    # [Prison healthcare, Prison Healthcare Services]
-    # [Rehabilitation (illness/injury), Rehabilitation services]
-    # [Phone/online advice, Remote clinical advice service]
-    # [Rehabilitation (substance abuse), Residential substance misuse treatment and/or rehabilitation service]
-    # [Shared lives, Shared Lives]
-    # [Specialist college service, Specialist college service]
-    # [Supported living, Supported living service]
-    # [Urgent care centres, Urgent care services]
-
-    return gacservicetypes_df
+#     return regulatedactivities_df
 
 
 def replace_value(df, key, value):
     return df.withColumn(value, regexp_replace(value, "Y", key))
 
 
-def get_specialisms(df):
-    df = df.selectExpr(
-        "locationid",
-        "`Service_user_band_Children_0-18_years`",
-        "Service_user_band_Dementia",
-        "Service_user_band_Learning_disabilities_or_autistic_spectrum_disorder",
-        "Service_user_band_Mental_Health",
-        "Service_user_band_Older_People",
-        "Service_user_band_People_detained_under_the_Mental_Health_Act",
-        "Service_user_band_People_who_misuse_drugs_and_alcohol",
-        "Service_user_band_People_with_an_eating_disorder",
-        "Service_user_band_Physical_Disability",
-        "Service_user_band_Sensory_Impairment",
-        "Service_user_band_Whole_Population",
-        "Service_user_band_Younger_Adults",
-    )
+def reformat_cols(df, dict, alias):
+    column_names = ["locationid"]
+    column_names.extend(list(dict.values()))
 
-    for new_name, column_name in SPECIALISMS_DICT.items():
+    df = df.select(*column_names)
+
+    for new_name, column_name in dict.items():
         df = replace_value(df, new_name, column_name)
 
-    df = df.select(
-        col("locationid"),
-        array(
-            col("Service_user_band_Children_0-18_years"),
-            col("Service_user_band_Dementia"),
-            col("Service_user_band_Learning_disabilities_or_autistic_spectrum_disorder"),
-            col("Service_user_band_Mental_Health"),
-            col("Service_user_band_Older_People"),
-            col("Service_user_band_People_detained_under_the_Mental_Health_Act"),
-            col("Service_user_band_People_who_misuse_drugs_and_alcohol"),
-            col("Service_user_band_People_with_an_eating_disorder"),
-            col("Service_user_band_Physical_Disability"),
-            col("Service_user_band_Sensory_Impairment"),
-            col("Service_user_band_Whole_Population"),
-            col("Service_user_band_Younger_Adults"),
-        ).alias("specialisms"),
-    )
+    df = df.select(col("locationid"), array(df.columns[1:]).alias(alias))
 
-    df = df.withColumn("specialisms", expr("filter(specialisms, elem -> elem != '')"))
+    df = df.withColumn(alias, expr("filter(" + alias + ", elem -> elem != '')"))
 
     return df
 
