3cdfcea9f7582b404aef97930264c23bd59705f4
LauraZ <zaharialaura9@gmail.com>
2022-05-11 16:56:28 +0100

    Add logs and trim bucket name

--- jobs/ingest_ascwds_dataset.py
+++ jobs/ingest_ascwds_dataset.py
@@ -7,18 +7,23 @@ import sys
 import pyspark
 import argparse
 
-DEFAULT_DELIMITER = ","
 
 def main(source, destination):
     if utils.is_csv(source):
+        print("Single file provided to job. Handling single file.")
         bucket_source, key = utils.split_s3_uri(source)
         sample = utils.read_partial_csv_content(bucket_source, key)
         delimiter = utils.identify_csv_delimiter(sample)
         ingest_dataset(source, destination, delimiter)
     else:
+        print("Multiple files provided to job. Handling each file...")
         bucket_source, prefix = utils.split_s3_uri(source)
         objects_list = utils.get_s3_objects_list(bucket_source, prefix)
         bucket_destination = utils.split_s3_uri(destination)[0]
+
+        print("Objects list:")
+        print(objects_list)
+
         for key in objects_list:
             if utils.is_csv(key):
                 new_source = utils.construct_s3_uri(bucket_source, key)
@@ -27,16 +32,15 @@ def main(source, destination):
                     bucket_destination, dir_path)
                 sample = utils.read_partial_csv_content(bucket_source, key)
                 delimiter = utils.identify_csv_delimiter(sample)
-                print(f"delimiter {delimiter}")
+
                 ingest_dataset(new_source, new_destination, delimiter)
 
 
 def ingest_dataset(source, destination, delimiter):
-    print(f"Reading CSV from {source}")
+    print(
+        f"Reading CSV from {source} and writing to {destination} with delimiter: {delimiter}")
     df = utils.read_csv(source, delimiter)
-    print("Removing ASCWDS test accounts")
     df = filter_test_accounts(df)
-    print("Formatting date fields")
     df = utils.format_date_fields(df)
 
     print(f"Exporting as parquet to {destination}")
@@ -53,34 +57,26 @@ def filter_test_accounts(df):
     return df
 
 
-
 def collect_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--source", help="A CSV file used as source input", required=True)
+    parser.add_argument(
+        "--source", help="A CSV file or directory of files used as job input", required=True)
     parser.add_argument(
         "--destination",
         help="A destination directory for outputting parquet files",
         required=True,
     )
-    parser.add_argument(
-        "--delimiter",
-        help="Specify a custom field delimiter",
-        required=False,
-        default=DEFAULT_DELIMITER,
-    )
 
     args, unknown = parser.parse_known_args()
 
-    if args.delimiter:
-        print(f"Utilising custom delimiter '{args.delimiter}'")
+    return args.source, args.destination
 
-    return args.source, args.destination, args.delimiter
 
 if __name__ == "__main__":
     print("Spark job 'ingest_ascwds_dataset' starting...")
     print(f"Job parameters: {sys.argv}")
 
-    source, destination, delimiter = collect_arguments()
+    source, destination = collect_arguments()
     main(source, destination)
 
     print("Spark job 'ingest_ascwds_dataset' complete")
--- utils/utils.py
+++ utils/utils.py
@@ -103,7 +103,8 @@ def split_s3_uri(uri):
 
 def construct_s3_uri(bucket_name, key):
     s3 = "s3://"
-    s3_uri = os.path.join(s3, bucket_name, key)
+    trimmed_bucket_name = bucket_name.trim()
+    s3_uri = os.path.join(s3, trimmed_bucket_name, key)
     return s3_uri
 
 
