98cefdebd83b63693ce221c16c02efd24d571a3e
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2022-05-17 16:38:39 +0100

    import both workplace files and filter to only those updating within 6 months

--- jobs/stayer_vs_leaver_dataset.py
+++ jobs/stayer_vs_leaver_dataset.py
@@ -1,19 +1,21 @@
 # STEPS
 #
-# import 2 provider files 12 months apart
-# filter both to those updating within 6 months
-# inner join (maybe?) to identify estids in both files
-# import majority of worker data from 'early' file
-# import selected data from later file
+# DONE - import 2 provider files 12 months apart
+# DONE - filter both to those updating within 6 months
+# TODO - inner join (maybe?) to identify estids in both files
+# TODO - import majority of worker data from 'early' file
+# TODO - import selected data from later file
 
 import argparse
+from pyspark.sql.functions import col, add_months
+from jobs import prepare_locations
 
 from utils import utils
 
 START_PERIOD_WORKPLACE_FILE = "s3://sfc-data-engineering/domain=ASCWDS/dataset=workplace/version=0.0.1/year=2021/month=03/day=31/import_date=20210331/"
 START_PERIOD_WORKER_FILE = "s3://sfc-data-engineering/domain=ASCWDS/dataset=worker/version=0.0.1/year=2021/month=03/day=31/import_date=20210331/"
 
-END_PERIOD_WORKPLACE__FILE = "s3://sfc-data-engineering/domain=ASCWDS/dataset=workplace/version=1.0.0/year=2022/month=04/day=01/import_date=20220401/"
+END_PERIOD_WORKPLACE_FILE = "s3://sfc-data-engineering/domain=ASCWDS/dataset=workplace/version=1.0.0/year=2022/month=04/day=01/import_date=20220401/"
 END_PERIOD_WORKER_FILE = "s3://sfc-data-engineering/domain=ASCWDS/dataset=worker/version=1.0.0/year=2022/month=04/day=01/import_date=20220401/"
 
 
@@ -21,11 +23,27 @@ def main(destination=None):
     spark = utils.get_spark()
 
     print("Creating stayer vs leaver parquet file")
+    start_workplace_df = spark.read.parquet(START_PERIOD_WORKPLACE_FILE)
+    start_workplace_df = updated_within_time_period(start_workplace_df)
+
+    end_workplace_df = spark.read.parquet(END_PERIOD_WORKPLACE_FILE)
+    end_workplace_df = updated_within_time_period(end_workplace_df)
 
     starters_vs_leavers_df = None
 
     print(f"Exporting as parquet to {destination}")
     utils.write_to_parquet(starters_vs_leavers_df, destination)
+
+
+def updated_within_time_period(df):
+
+    df = df.select("establishmentid", "mupddate", "import_date")
+    df = prepare_locations.format_import_date(df)
+    df = df.withColumn("mupddate_cutoff", add_months(df.import_date, -6))
+    df = df.filter(df.mupddate > df.mupddate_cutoff)
+    df = df.select("establishmentid")
+
+    return df
 
 
 def collect_arguments():
--- tests/unit/test_stayer_vs_leaver_dataset.py
+++ tests/unit/test_stayer_vs_leaver_dataset.py
@@ -1,1 +1,43 @@
-# ADD TESTS HERE
+import unittest
+
+from pyspark.sql import SparkSession
+from datetime import date
+
+from jobs import stayer_vs_leaver_dataset
+
+
+class CQC_Care_Directory_Tests(unittest.TestCase):
+    def setUp(self):
+        self.spark = SparkSession.builder.appName("test_ingest_cqc_care_directory").getOrCreate()
+
+    def test_updated_within_time_period(self):
+
+        columns = [
+            "establishmentid",
+            "mupddate",
+            "import_date",
+            "other_col",
+        ]
+
+        rows = [
+            ("1", date(2020, 1, 14), "20200715", "abc"),
+            ("2", date(2020, 1, 15), "20200715", "abc"),
+            ("3", date(2020, 1, 16), "20200715", "abc"),
+        ]
+
+        df = self.spark.createDataFrame(rows, columns)
+
+        filtered_df = stayer_vs_leaver_dataset.updated_within_time_period(df)
+
+        self.assertEqual(filtered_df.count(), 1)
+        self.assertEqual(filtered_df.columns, ["establishmentid"])
+
+        collected_df = filtered_df.collect()
+        self.assertEqual(collected_df[0]["establishmentid"], "3")
+
+    def test_main(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
