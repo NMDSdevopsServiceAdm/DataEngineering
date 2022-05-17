cbd68ef896b65a24c5cc7a118545a1d1cb22e6ad
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2022-05-17 10:14:04 +0100

    basic plan of action

---
+++ jobs/stayer_vs_leaver_dataset.py
@@ -0,0 +1,7 @@
+# STEPS
+#
+# import 2 provider files 12 months apart
+# filter both to those updating within 6 months
+# inner join (maybe?) to identify estids in both files
+# import majority of worker data from 'early' file
+# import selected data from later file
