fa494f97f57cf2a046d12737e6822a192fbb2b18
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2022-05-18 09:28:43 +0100

    filter to workplaces updated within 6 months of import_date and create df containing establishmentids in both periods

--- jobs/stayer_vs_leaver_dataset.py
+++ jobs/stayer_vs_leaver_dataset.py
@@ -19,24 +19,30 @@ END_PERIOD_WORKPLACE_FILE = "s3://sfc-data-engineering/domain=ASCWDS/dataset=wor
 END_PERIOD_WORKER_FILE = "s3://sfc-data-engineering/domain=ASCWDS/dataset=worker/version=1.0.0/year=2022/month=04/day=01/import_date=20220401/"
 
 
-def main(destination=None):
-    spark = utils.get_spark()
+def main(
+    source_start_workplace_file=START_PERIOD_WORKPLACE_FILE,
+    source_end_workplace_file=END_PERIOD_WORKPLACE_FILE,
+    destination=None,
+):
 
     print("Creating stayer vs leaver parquet file")
-    start_workplace_df = spark.read.parquet(START_PERIOD_WORKPLACE_FILE)
-    start_workplace_df = updated_within_time_period(start_workplace_df)
+    start_workplace_df = updated_within_time_period(source_start_workplace_file)
 
-    end_workplace_df = spark.read.parquet(END_PERIOD_WORKPLACE_FILE)
-    end_workplace_df = updated_within_time_period(end_workplace_df)
+    end_workplace_df = updated_within_time_period(source_end_workplace_file)
 
-    starters_vs_leavers_df = None
+    starters_vs_leavers_df = workplaces_in_both_dfs(start_workplace_df, end_workplace_df)
 
-    print(f"Exporting as parquet to {destination}")
-    utils.write_to_parquet(starters_vs_leavers_df, destination)
+    if destination:
+        print(f"Exporting as parquet to {destination}")
+        utils.write_to_parquet(starters_vs_leavers_df, destination)
+    else:
+        return starters_vs_leavers_df
 
 
 def updated_within_time_period(df):
+    spark = utils.get_spark()
 
+    df = spark.read.parquet(df)
     df = df.select("establishmentid", "mupddate", "import_date")
     df = prepare_locations.format_import_date(df)
     df = df.withColumn("mupddate_cutoff", add_months(df.import_date, -6))
@@ -46,9 +52,26 @@ def updated_within_time_period(df):
     return df
 
 
+def workplaces_in_both_dfs(start_workplace_df, end_workplace_df):
+
+    df = start_workplace_df.join(end_workplace_df, ["establishmentid"], "inner")
+
+    return df
+
+
 def collect_arguments():
     parser = argparse.ArgumentParser()
 
+    parser.add_argument(
+        "--source_start_workplace_file",
+        help="Source s3 directory for ASCWDS workplace dataset - start of 12 month period.",
+        required=True,
+    )
+    parser.add_argument(
+        "--source_end_workplace_file",
+        help="Source s3 directory for ASCWDS workplace dataset - end of 12 month period.",
+        required=True,
+    )
     parser.add_argument(
         "--destination",
         help="A destination directory for outputting cqc locations, if not provided shall default to S3 todays date.",
@@ -57,10 +80,10 @@ def collect_arguments():
 
     args, unknown = parser.parse_known_args()
 
-    return args.destination
+    return args.source_start_workplace_file, args.source_end_workplace_file, args.destination
 
 
 if __name__ == "__main__":
-    (destination) = collect_arguments()
+    (source_start_workplace_file, source_end_workplace_file, destination) = collect_arguments()
 
-    main(destination)
+    main(source_start_workplace_file, source_end_workplace_file, destination)
