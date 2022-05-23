78daf483826b359b7aeffe380f41d586e7e7a1fe
Adam Probert <adamprobert@live.co.uk>
2022-05-23 11:21:03 +0100

    Add defailt glue service policy

--- terraform/pipeline/iam.tf
+++ terraform/pipeline/iam.tf
@@ -14,3 +14,7 @@ data "aws_iam_policy_document" "glue_service_assume_role_policy" {
   }
 }
 
+resource "aws_iam_role_policy_attachment" "AWSGlueServiceRole_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+  role       = aws_iam_role.sfc_glue_service_iam_role.name
+}
