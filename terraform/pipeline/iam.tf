111ec3c8147ed8f95cf62a9fd6c8303ccb708d58
Adam Probert <adamprobert@live.co.uk>
2022-05-23 14:45:06 +0100

    Add missing wildcard to iam resources

--- terraform/pipeline/iam.tf
+++ terraform/pipeline/iam.tf
@@ -44,8 +44,37 @@ resource "aws_iam_policy" "glue_crawler_logging_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "glue_job_s3_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "glue_crawler_logging_policy_attachment" {
   policy_arn = aws_iam_policy.glue_crawler_logging_policy.arn
   role       = aws_iam_role.sfc_glue_service_iam_role.name
 }
+
+resource "aws_iam_policy" "glue_job_s3_data_engineering_policy" {
+  name        = "${terraform.workspace}-glue_job_bucket_access_policy"
+  path        = "/"
+  description = "Iam policy for the all glue jobs on workspace: ${terraform.workspace}"
+
+  policy = jsonencode({
+
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:*"
+        ],
+        "Resource" : [
+          "arn:aws:s3:::${module.pipeline_resources.bucket_name}/*",
+          "arn:aws:s3:::${module.datasets_bucket.bucket_name}/*",
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "glue_job_s3_policy_attachment" {
+  policy_arn = aws_iam_policy.glue_job_s3_data_engineering_policy.arn
+  role       = aws_iam_role.sfc_glue_service_iam_role.name
+}
+
 
