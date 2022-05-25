d6e131e569fd6b1e4e34067f6355e9b3be14b687
Adam Probert <adamprobert@live.co.uk>
2022-05-25 12:07:04 +0100

    Added new test bucket

--- terraform/pipeline/s3.tf
+++ terraform/pipeline/s3.tf
@@ -7,3 +7,8 @@ module "datasets_bucket" {
   source      = "../modules/s3-bucket"
   bucket_name = "datasets"
 }
+
+module "adams_test_bucket" {
+  source      = "../modules/s3-bucket"
+  bucket_name = "adams_test_bucket"
+}
