04cec224c6c3451b1b2cb98a29225e677933ff83
Emma <emma@madetech.com>
2022-07-07 14:04:22 +0100

    Partition prepare locations by snapshot date

--- jobs/prepare_locations.py
+++ jobs/prepare_locations.py
@@ -1,9 +1,22 @@
 import argparse
 import builtins
 
-import pyspark.sql.functions as F
-from pyspark.sql.types import IntegerType
-from pyspark.sql.utils import AnalysisException
+from pyspark.sql.functions import (
+    abs,
+    add_months,
+    coalesce,
+    col,
+    greatest,
+    lit,
+    lower,
+    max,
+    when,
+    year,
+    month,
+    dayofmonth,
+    lpad,
+)
+from pyspark.sql.types import DateType, IntegerType, StructField, StructType
 
 from utils import utils
 
@@ -81,85 +94,61 @@ def main(
         output_df = output_df.withColumn(
             "snapshot_date", F.lit(snapshot_date_row["snapshot_date"])
         )
+        output_df = output_df.withColumn("snapshot_year", year(col("snapshot_date")))
         output_df = output_df.withColumn(
-            "snapshot_year", F.col("snapshot_date").substr(1, 4)
-        )
-        output_df = output_df.withColumn(
-            "snapshot_month", F.col("snapshot_date").substr(5, 2)
+            "snapshot_month", lpad(month(col("snapshot_date")), 2, "0")
         )
         output_df = output_df.withColumn(
-            "snapshot_day", F.col("snapshot_date").substr(7, 2)
-        )
-        output_df = utils.format_import_date(output_df, fieldname="snapshot_date")
-
-        output_df = output_df.select(
-            "snapshot_date",
-            "snapshot_year",
-            "snapshot_month",
-            "snapshot_day",
-            "ascwds_workplace_import_date",
-            "cqc_locations_import_date",
-            "cqc_providers_import_date",
-            "cqc_pir_import_date",
-            "locationid",
-            "location_type",
-            "location_name",
-            "organisation_type",
-            "providerid",
-            "provider_name",
-            "orgid",
-            "establishmentid",
-            "registration_status",
-            "registration_date",
-            "deregistration_date",
-            "carehome",
-            "dormancy",
-            "number_of_beds",
-            "services_offered",
-            "pir_service_users",
-            "job_count",
-            "region",
-            "postal_code",
-            "constituency",
-            "local_authority",
-            "cqc_sector",
+            "snapshot_day", lpad(dayofmonth(col("snapshot_date")), 2, "0")
         )
 
-        if destination:
-            print(
-                "Exporting snapshot {} as parquet to {}".format(
-                    snapshot_date_row["snapshot_date"], destination
-                )
-            )
-            utils.write_to_parquet(
-                output_df,
-                destination,
-                append=True,
-                partitionKeys=["snapshot_year", "snapshot_month", "snapshot_day"],
-            )
-            if master_df is None:
-                master_df = output_df
-            else:
-                master_df = master_df.union(output_df)
-    return master_df
+        if master_df is None:
+            master_df = output_df
+        else:
+            master_df = master_df.union(output_df)
 
+    master_df = master_df.select(
+        "snapshot_date",
+        "snapshot_year",
+        "snapshot_month",
+        "snapshot_day",
+        "ascwds_workplace_import_date",
+        "cqc_locations_import_date",
+        "cqc_providers_import_date",
+        "cqc_pir_import_date",
+        "locationid",
+        "location_type",
+        "location_name",
+        "organisation_type",
+        "providerid",
+        "provider_name",
+        "orgid",
+        "establishmentid",
+        "registration_status",
+        "registration_date",
+        "deregistration_date",
+        "carehome",
+        "dormancy",
+        "number_of_beds",
+        "services_offered",
+        "pir_service_users",
+        "job_count",
+        "region",
+        "postal_code",
+        "constituency",
+        "local_authority",
+        "cqc_sector",
+    )
 
-def get_max_snapshot_of_locations_prepared(destination):
-    spark = utils.get_spark()
-    try:
-        previous_snpashots = spark.read.option("basePath", destination).parquet(
-            destination
+    if destination:
+        print(f"Exporting as parquet to {destination}")
+        utils.write_to_parquet(
+            master_df,
+            destination,
+            partitionKeys=["snapshot_year", "snapshot_month", "snapshot_day"],
         )
-    except AnalysisException:
-        return None
-
-    max_year = previous_snpashots.select(F.max("snapshot_year")).first()[0]
-    previous_snpashots = previous_snpashots.where(F.col("snapshot_year") == max_year)
-    max_month = previous_snpashots.select(F.max("snapshot_month")).first()[0]
-    previous_snpashots = previous_snpashots.where(F.col("snapshot_month") == max_month)
-    max_day = previous_snpashots.select(F.max("snapshot_day")).first()[0]
-
-    return f"{max_year}{max_month:0>2}{max_day:0>2}"
+    else:
+        return master_df
 
 
 def get_ascwds_workplace_df(workplace_source, since_date=None):
