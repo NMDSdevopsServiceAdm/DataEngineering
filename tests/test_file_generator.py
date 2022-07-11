ad508e7386471aa8798221d5d0a3571967a3ba26
Emma <emma@madetech.com>
2022-07-11 17:19:53 +0100

    Job role breakdown job - Remove 2021 from variable names - Work out job role breakdown per snapshot - Work with the latest partition of the job count estimates - partition output data by run date

--- tests/test_file_generator.py
+++ tests/test_file_generator.py
@@ -48,24 +48,24 @@ def generate_ethnicity_parquet(output_destination):
 
 def generate_worker_parquet(output_destination):
     spark = utils.get_spark()
-    columns = ["locationid", "workerid", "mainjrid"]
+    columns = ["locationid", "workerid", "mainjrid", "import_date"]
 
     rows = [
-        ("1-000000001", "100", 1),
-        ("1-000000001", "101", 1),
-        ("1-000000001", "102", 1),
-        ("1-000000001", "103", 1),
-        ("1-000000001", "104", 2),
-        ("1-000000001", "105", 3),
-        ("1-000000002", "106", 1),
-        ("1-000000002", "107", 3),
-        ("1-000000002", "108", 2),
-        ("1-000000003", "109", 1),
-        ("1-000000003", "110", 2),
-        ("1-000000003", "111", 3),
-        ("1-000000004", "112", 1),
-        ("1-000000004", "113", 2),
-        ("1-000000004", "114", 3),
+        ("1-000000001", "100", 1, "20220101"),
+        ("1-000000001", "101", 1, "20220101"),
+        ("1-000000001", "102", 1, "20220101"),
+        ("1-000000001", "103", 1, "20220101"),
+        ("1-000000001", "104", 2, "20220101"),
+        ("1-000000001", "105", 3, "20220101"),
+        ("1-000000002", "106", 1, "20220101"),
+        ("1-000000002", "107", 3, "20220101"),
+        ("1-000000002", "108", 2, "20220101"),
+        ("1-000000003", "109", 1, "20220101"),
+        ("1-000000003", "110", 2, "20220101"),
+        ("1-000000003", "111", 3, "20220101"),
+        ("1-000000004", "112", 1, "20220101"),
+        ("1-000000004", "113", 2, "20220101"),
+        ("1-000000004", "114", 3, "20220101"),
     ]
 
     df = spark.createDataFrame(rows, columns)
@@ -212,17 +212,19 @@ def generate_ethnicity_census_lsoa_csv(output_destination):
     return df
 
 
-def generate_estimate_jobs_2021_parquet(output_destination):
+def generate_estimate_jobs_parquet(output_destination):
     spark = utils.get_spark()
-    columns = ["locationid", "primary_service_type", "estimate_job_count_2021"]
+    # fmt: off
+    columns = ["locationid", "primary_service_type", "estimate_job_count", "snapshot_date", "run_year", "run_month", "run_day"]
 
     rows = [
-        ("1-000000001", "Care home without nursing", 15.5),
-        ("1-000000002", "Care home with nursing", 90.0),
-        ("1-000000003", "Care home with nursing", 2.1),
-        ("1-000000004", "non-residential", 25.3),
-        ("1-000000005", "non-residential", 94.0),
+        ("1-000000001", "Care home without nursing", 15.5, "20220101", "2022", "01", "02"),
+        ("1-000000002", "Care home with nursing", 90.0, "20220101", "2022", "01", "02"),
+        ("1-000000003", "Care home with nursing", 2.1, "20220101", "2022", "01", "02"),
+        ("1-000000004", "non-residential", 25.3, "20220101", "2022", "01", "02"),
+        ("1-000000005", "non-residential", 94.0, "20220101", "2022", "01", "02"),
     ]
+    # fmt: on
 
     df = spark.createDataFrame(rows, columns)
 
