fb4a1c12b459067d126998677bb5b1138a0bcd12
Emma <emma@madetech.com>
2022-07-18 11:41:38 +0100

    Add default glue argument for ons source

--- terraform/pipeline/glue.tf
+++ terraform/pipeline/glue.tf
@@ -75,6 +75,7 @@ module "prepare_locations_job" {
     "--cqc_location_source" = "${module.datasets_bucket.bucket_uri}/domain=CQC/dataset=locations-api/"
     "--cqc_provider_source" = "${module.datasets_bucket.bucket_uri}/domain=CQC/dataset=providers-api/"
     "--pir_source"          = "${module.datasets_bucket.bucket_uri}/domain=CQC/dataset=pir/"
+    "--ons_source"          = "${module.datasets_bucket.bucket_uri}/domain=ONS/dataset=postcode-directory-denormalised/"
     "--destination"         = "${module.datasets_bucket.bucket_uri}/domain=data_engineering/dataset=locations_prepared/version=1.0.0/"
   }
 }
