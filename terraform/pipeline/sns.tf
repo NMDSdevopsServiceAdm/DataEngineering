b7a9f54531a33d968b481a4ce958fbd5c2483701
Emma <emma@madetech.com>
2022-07-26 16:48:10 +0100

    Remove topic subscription

--- terraform/pipeline/sns.tf
+++ terraform/pipeline/sns.tf
@@ -1,9 +1,3 @@
 resource "aws_sns_topic" "pipeline_failures" {
   name = "${local.workspace_prefix}-pipeline-failures"
 }
-
-resource "aws_sns_topic_subscription" "pipeline_failures_test_email" {
-  endpoint  = "test@madetech.com"
-  protocol  = "email"
-  topic_arn = aws_sns_topic.pipeline_failures.arn
-}
