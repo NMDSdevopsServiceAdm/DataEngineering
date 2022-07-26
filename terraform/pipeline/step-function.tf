648a95a2f40d98cfa0185a89214ed535f123a911
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2022-07-26 09:52:30 +0100

    added job to terraform pipeline

--- terraform/pipeline/step-function.tf
+++ terraform/pipeline/step-function.tf
@@ -5,6 +5,7 @@ resource "aws_sfn_state_machine" "data-engineering-state-machine" {
   definition = templatefile("step-functions/DataEngineeringPipeline-StepFunction.json", {
     prepare_locations_job_name             = module.prepare_locations_job.job_name
     prepare_workers_job_name               = module.prepare_workers_job.job_name
+    worker_tracking_job_name               = module.worker_tracking_job.job_name
     estimate_jobs_job_name                 = module.estimate_job_counts_job.job_name
     locations_feature_engineering_job_name = module.locations_feature_engineering_job.job_name
     job_role_breakdown_job_name            = module.job_role_breakdown_job.job_name
