7ae339070f79f6e46e7de71ac8d0ffc7c2101919
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2022-12-02 17:00:39 +0000

    Write test for calculate_coverage()

--- tests/unit/test_cqc_coverage_based_on_login_purge.py
+++ tests/unit/test_cqc_coverage_based_on_login_purge.py
@@ -17,6 +17,7 @@ from tests.test_file_generator import (
     generate_ascwds_workplace_file,
     generate_cqc_locations_file,
     generate_cqc_providers_file,
+    generate_cqc_covarage_to_summarise_parquet,
 )
 
 
@@ -26,12 +27,14 @@ class PrepareLocationsTests(unittest.TestCase):
     TEST_CQC_LOCATION_FILE = "tests/test_data/domain=cqc/dataset=location"
     TEST_CQC_PROVIDERS_FILE = "tests/test_data/domain=cqc/dataset=providers"
     DESTINATION = "tests/test_data/domain=data_engineering/dataset=locations_prepared/version=1.0.0"
+    TEST_CQC_COVERAGE_FILE = "tests/test_data/domain=cqc/dataset=coverage"
 
     def setUp(self):
         self.spark = SparkSession.builder.appName("test_prepare_locations").getOrCreate()
         generate_ascwds_workplace_file(self.TEST_ASCWDS_WORKPLACE_FILE)
         self.cqc_loc_df = generate_cqc_locations_file(self.TEST_CQC_LOCATION_FILE)
         generate_cqc_providers_file(self.TEST_CQC_PROVIDERS_FILE)
+        coverage_df = generate_cqc_covarage_to_summarise_parquet(self.TEST_CQC_COVERAGE_FILE)
 
         warnings.simplefilter("ignore", ResourceWarning)
 
@@ -43,6 +46,18 @@ class PrepareLocationsTests(unittest.TestCase):
             shutil.rmtree(self.DESTINATION)
         except OSError:
             pass  # Ignore dir does not exist
+
+    def test_calculate_coverage(self):
+        coverage = cqc_coverage_based_on_login_purge.calculate_coverage(self.TEST_CQC_LOCATION_FILE, "region")
+        # check column names
+        self.assertEqual(coverage.columns[0], "region")
+        self.assertEqual(coverage.columns[1], "total_locations")
+        self.assertEqual(coverage.columns[2], "total_locations_in_ASC-WDS")
+        self.assertEqual(coverage.columns[3], "percentage_coverage_by_region")
+        # check counts
+        self.assertEqual(coverage[2, 1], 2)
+        # check coverage calculations
+        self.assertEqual(coverage[2, 3], 0.5)
 
 
 if __name__ == "__main__":
