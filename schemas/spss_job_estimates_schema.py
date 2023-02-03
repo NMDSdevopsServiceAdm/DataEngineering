2fd560635fb2b3d31de202afc6404bb262fa6702
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-02-03 11:09:03 +0000

    add variable names to schema

--- schemas/spss_job_estimates_schema.py
+++ schemas/spss_job_estimates_schema.py
@@ -7,4 +7,22 @@ from pyspark.sql.types import (
     IntegerType,
 )
 
-SPSS_JOBS_ESTIMATES = StructType(fields=[StructField()])
+SPSS_JOBS_ESTIMATES = StructType(
+    fields=[
+        StructField("LOCATIONID", StringType(), True),
+        StructField("Main_Service_Group", IntegerType(), # TODO),
+        StructField("Care_homes_beds"),
+        StructField("WEIGHTING_CSSR"),
+        StructField("WEIGHTING_REGION"),
+        StructField("totalstaff"),
+        StructField("wkrrecs"),
+        StructField("jr28work"),
+        StructField("Data_Used"),
+        StructField("All_jobs"),
+        StructField("Snapshot_date"),
+        StructField("Local_authority"),
+        StructField("Region"),
+        StructField("Main_service"),
+        StructField("Data_used_string"),
+    ]
+)
