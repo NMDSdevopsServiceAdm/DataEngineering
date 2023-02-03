89946d7dfb09fbc14a5111c73266fadabdcac794
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-02-03 10:22:07 +0000

    Add skeleton schema structure

---
+++ schemas/spss_job_estimates_schema.py
@@ -0,0 +1,10 @@
+from pyspark.sql.types import (
+    StructField,
+    StructType,
+    StringType,
+    ArrayType,
+    FloatType,
+    IntegerType,
+)
+
+SPSS_JOBS_ESTIMATES = StructType(fields=[StructField()])
