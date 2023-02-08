2accea0f6c7fd3f7b33b4aa8570ac3244317446c
Catherine Quilley <catherine.quilley@madetech.com>
2023-02-08 08:14:28 +0000

    adding tests where they didn't previously exist

--- utils/prepare_locations_utils/job_calculator/common_checks.py
+++ utils/prepare_locations_utils/job_calculator/common_checks.py
@@ -3,7 +3,8 @@ import pyspark.sql.functions as F
 
 
 def job_count_from_ascwds_is_not_populated(col_name: str) -> pyspark.sql.Column:
-    return F.col(col_name).isNull()
+    result = F.col(col_name).isNull()
+    return result
 
 
 def selected_column_is_not_null(col_name: str):
@@ -17,4 +18,5 @@ def selected_column_is_null(col_name: str):
 def column_value_is_less_than_min_abs_difference_between_total_staff_and_worker_record_count(
     col_name: str, min_abs_diff: float
 ) -> bool:
-    return F.col(col_name) < min_abs_diff
+    result = F.col(col_name) < min_abs_diff
+    return result
