258831f7cd800abc87b2c28ee8011b82e98d3a0b
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-02-09 10:05:07 +0000

    Extract snapshot date function for snapshot year

---
+++ utils/prepare_locations_utils/dataframe_utils.py
@@ -0,0 +1,9 @@
+import pyspark.sql.functions as F
+
+
+def add_column_with_snaphot_date_substring(df, column_name, start_of_substring, length_of_substring):
+    df_with_snapshot_date_substring_column = df.withColumn(
+        column_name,
+        F.col("snapshot_date").substr(start_of_substring, length_of_substring),
+    )
+    return df_with_snapshot_date_substring_column
