d71a097e2175b9a95fe8ee0d8be503e46191d5cc
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2023-02-10 16:59:08 +0000

    added column name into function

--- jobs/prepare_locations.py
+++ jobs/prepare_locations.py
@@ -117,7 +117,7 @@ def main(
 
         output_df = output_df.join(pir_df, "locationid", "left")
         output_df = add_geographical_data(output_df, latest_ons_data)
-        output_df = calculate_jobcount(output_df)
+        output_df = calculate_jobcount(output_df, "job_count")
 
         output_df = add_column_if_locationid_is_in_ascwds(output_df)
 
