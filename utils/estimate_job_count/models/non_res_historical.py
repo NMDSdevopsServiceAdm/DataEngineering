470746c11fa011597287ae087f1929526dade787
Catherine Quilley <catherine.quilley@madetech.com>
2023-02-23 14:24:02 +0000

    updating non_res_historical.py

--- utils/estimate_job_count/models/non_res_historical.py
+++ utils/estimate_job_count/models/non_res_historical.py
@@ -8,6 +8,8 @@ from utils.prepare_locations_utils.job_calculator.job_calculator import (
 )
 from pyspark.sql import functions as F
 
+PROJECTION_RATIO = 1.03
+
 
 def model_non_res_historical(df):
     """
@@ -22,9 +24,20 @@ def model_non_res_historical(df):
                 & (F.col(PRIMARY_SERVICE_TYPE) == "non-residential")
                 & F.col(LAST_KNOWN_JOB_COUNT).isNotNull()
             ),
-            F.col(LAST_KNOWN_JOB_COUNT) * 1.03,
+            F.col(LAST_KNOWN_JOB_COUNT) * PROJECTION_RATIO,
         ).otherwise(F.col(ESTIMATE_JOB_COUNT)),
     )
+
+    df = df.withColumn(
+        "model_non_res_historical",
+        F.when(
+            (
+                    (F.col(PRIMARY_SERVICE_TYPE) == "non-residential")
+                    & F.col(LAST_KNOWN_JOB_COUNT).isNotNull()
+            ),
+            F.col(LAST_KNOWN_JOB_COUNT) * PROJECTION_RATIO))
+
+
     df = update_dataframe_with_identifying_rule(
         df,
         "model_non_res_ascwds_projected_forward",
