520d9143275deb90d70a2389174cddf139cbe030
Catherine Quilley <catherine.quilley@madetech.com>
2023-03-14 13:09:44 +0000

    creating non res feature creation and tests

---
+++ jobs/locations_non_res_feature_engineering.py
@@ -0,0 +1,266 @@
+import argparse
+import re
+import sys
+from dataclasses import dataclass
+from typing import List, Dict, Tuple, Set
+
+import numpy as np
+import pyspark
+import pyspark.sql.functions as F
+from pyspark.ml.feature import VectorAssembler
+
+from utils import utils
+from utils.feature_engineering_dictionaries import (
+    SERVICES_LOOKUP,
+    RURAL_URBAN_INDICATOR_LOOKUP,
+)
+
+
+@dataclass
+class ColNamesFromPrepareLocations:
+    ons_region: str = "ons_region"
+    services_offered: str = "services_offered"
+    cqc_sector: str = "cqc_sector"
+    rural_urban_indicator: str = "rural_urban_indicator.year_2011"
+    people_directly_employed: str = "people_directly_employed"
+    carehome: str = "carehome"
+    snapshot_date: str = "snapshot_date"
+
+
+@dataclass
+class NewColNames:
+    service_count: str = "service_count"
+    date_diff: str = "date_diff"
+    rui_indicator: str = "rui_2011"
+
+
+@dataclass
+class FeatureNames:
+    care_home: str = "features"
+
+
+def main(prepared_locations_source, destination=None):
+    spark = utils.get_spark()
+
+    features_from_prepare_locations = ColNamesFromPrepareLocations()
+    new_cols_for_features = NewColNames()
+    services_dict = SERVICES_LOOKUP
+    rural_urban_indicator_dict = RURAL_URBAN_INDICATOR_LOOKUP
+
+    locations_df = spark.read.option("basePath", prepared_locations_source).parquet(
+        prepared_locations_source
+    )
+
+    filtered_loc_data = filter_locations_df_for_independent_non_res_care_home_data(
+        df=locations_df,
+        carehome_col_name=features_from_prepare_locations.carehome,
+        cqc_col_name=features_from_prepare_locations.cqc_sector,
+    )
+
+    filtered_data_with_employee_col = convert_col_to_integer_col(
+        df=filtered_loc_data,
+        col_name=features_from_prepare_locations.people_directly_employed,
+    )
+
+    data_with_service_count = add_service_count_to_data(
+        df=filtered_data_with_employee_col,
+        new_col_name=new_cols_for_features.service_count,
+        col_to_check=features_from_prepare_locations.services_offered,
+    )
+    service_keys = list(services_dict.keys())
+    data_with_expanded_services = column_expansion_with_dict(
+        df=data_with_service_count,
+        col_name=features_from_prepare_locations.services_offered,
+        lookup_dict=services_dict,
+    )
+    rui_indicators = list(rural_urban_indicator_dict.keys())
+    data_with_rui = add_rui_data_data_frame(
+        df=data_with_expanded_services,
+        new_rui_col_name=new_cols_for_features.rui_indicator,
+        col_to_check=features_from_prepare_locations.rural_urban_indicator,
+        lookup_dict=rural_urban_indicator_dict,
+    )
+
+    distinct_regions = get_list_of_distinct_ons_regions(
+        df=data_with_rui,
+        col_name=features_from_prepare_locations.ons_region,
+    )
+
+    data_with_region_cols, regions = explode_column_from_distinct_values(
+        df=data_with_rui,
+        column_name=features_from_prepare_locations.ons_region,
+        col_prefix="ons_",
+        col_list_set=set(distinct_regions),
+    )
+
+    data_with_date_diff = add_date_diff_into_df(
+        df=data_with_region_cols,
+        new_col_name=new_cols_for_features.date_diff,
+        snapshot_date_col=features_from_prepare_locations.snapshot_date,
+    )
+
+    list_for_vectorisation: list[str] = sorted(
+        [
+            new_cols_for_features.service_count,
+            features_from_prepare_locations.people_directly_employed,
+            new_cols_for_features.date_diff,
+        ]
+        + service_keys
+        + regions
+        + rui_indicators
+    )
+
+    vectorised_dataframe = vectorise_dataframe(
+        df=data_with_date_diff, list_for_vectorisation=list_for_vectorisation
+    )
+    features_df = vectorised_dataframe.select(
+        "locationid",
+        "snapshot_date",
+        "ons_region",
+        "number_of_beds",
+        "people_directly_employed",
+        "snapshot_year",
+        "snapshot_month",
+        "snapshot_day",
+        "carehome",
+        "features",
+        "job_count",
+    )
+
+    print("distinct_regions")
+    print(distinct_regions)
+    print("number_of_features:")
+    print(len(list_for_vectorisation))
+    print(f"length of feature df: {vectorised_dataframe.count()}")
+
+    if destination:
+        print(f"Exporting as parquet to {destination}")
+        utils.write_to_parquet(
+            features_df,
+            destination,
+            append=True,
+            partitionKeys=["snapshot_year", "snapshot_month", "snapshot_day"],
+        )
+    return features_df
+
+
+def convert_col_to_integer_col(df, col_name):
+    df = df.withColumn(col_name, F.col(col_name).cast("int"))
+    return df
+
+
+def vectorise_dataframe(
+    df: pyspark.sql.DataFrame, list_for_vectorisation: List[str]
+) -> pyspark.sql.DataFrame:
+    loc_df = VectorAssembler(
+        inputCols=list_for_vectorisation, outputCol="features", handleInvalid="skip"
+    ).transform(df)
+    return loc_df
+
+
+def get_list_of_distinct_ons_regions(
+    df: pyspark.sql.DataFrame, col_name: str
+) -> List[str]:
+    distinct_regions = df.select(col_name).distinct().dropna().collect()
+    dis_regions_list = [str(row.ons_region) for row in distinct_regions]
+    return dis_regions_list
+
+
+def column_expansion_with_dict(
+    df: pyspark.sql.DataFrame, col_name: str, lookup_dict: Dict[str, str]
+) -> pyspark.sql.DataFrame:
+    for key in lookup_dict.keys():
+        df = df.withColumn(
+            f"{key}",
+            F.array_contains(df[f"{col_name}"], lookup_dict[key]).cast("integer"),
+        )
+    return df
+
+
+def add_rui_data_data_frame(
+    df: pyspark.sql.DataFrame,
+    new_rui_col_name: str,
+    col_to_check: str,
+    lookup_dict: Dict[str, str],
+) -> pyspark.sql.DataFrame:
+    df = df.withColumn(new_rui_col_name, F.col(col_to_check))
+
+    for key in lookup_dict.keys():
+        df = df.withColumn(
+            key, (F.col(new_rui_col_name) == lookup_dict[key]).cast("integer")
+        )
+    return df
+
+
+def add_service_count_to_data(
+    df: pyspark.sql.DataFrame, new_col_name: str, col_to_check: str
+) -> pyspark.sql.DataFrame:
+    return df.withColumn(new_col_name, F.size(F.col(col_to_check)))
+
+
+def filter_locations_df_for_independent_non_res_care_home_data(
+    df: pyspark.sql.DataFrame, carehome_col_name: str, cqc_col_name: str
+) -> pyspark.sql.DataFrame:
+    care_home_data = df.filter(F.col(carehome_col_name) == "N")
+    independent_care_home_data = care_home_data.filter(
+        F.col(cqc_col_name) == "Independent"
+    )
+    return independent_care_home_data
+
+
+def format_strings(string: str) -> str:
+    no_space = string.replace(" ", "_").lower()
+    return re.sub("[^a-z_]", "", no_space)
+
+
+def explode_column_from_distinct_values(
+    df: pyspark.sql.DataFrame, column_name: str, col_prefix: str, col_list_set: Set[str]
+) -> Tuple[pyspark.sql.DataFrame, List[str]]:
+    col_names = []
+
+    for col in col_list_set:
+        clean = col_prefix + format_strings(col)
+        col_names.append(clean)
+
+        df = df.withColumn(f"{clean}", (F.col(f"{column_name}") == col).cast("integer"))
+    return df, col_names
+
+
+def add_date_diff_into_df(
+    df: pyspark.sql.DataFrame, new_col_name: str, snapshot_date_col: str
+) -> pyspark.sql.DataFrame:
+    max_d = df.agg(F.max(snapshot_date_col)).first()[0]
+
+    loc_df = df.withColumn(
+        new_col_name, F.datediff(F.lit(max_d), F.col(snapshot_date_col))
+    )
+    return loc_df
+
+
+def collect_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--prepared_locations_source",
+        help="Source S3 directory for data engineering prepared locations dataset",
+        required=True,
+    )
+    parser.add_argument(
+        "--destination",
+        help="A destination directory for outputting locations dataset after feature engineering and vectorizing",
+        required=True,
+    )
+    args, _ = parser.parse_known_args()
+
+    return args.prepared_locations_source, args.destination
+
+
+if __name__ == "__main__":
+    print("Spark job 'locations_feature_engineering' starting...")
+    print(f"Job parameters: {sys.argv}")
+
+    (prepared_locations_source, destination) = collect_arguments()
+
+    main(prepared_locations_source, destination)
+
+    print("Spark job 'locations_feature_engineering' complete")
