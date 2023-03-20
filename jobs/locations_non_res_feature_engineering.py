b8734b0118e4ac36bc39fe4c9befce7e76e0ecd3
Catherine Quilley <catherine.quilley@madetech.com>
2023-03-20 17:11:52 +0000

    refactor to remove duplicate function

--- jobs/locations_non_res_feature_engineering.py
+++ jobs/locations_non_res_feature_engineering.py
@@ -18,6 +18,7 @@ from utils.features.helper import (
     explode_column_from_distinct_values,
     add_date_diff_into_df,
     vectorise_dataframe,
+    filter_records_since_snapshot_date,
 )
 
 
@@ -149,26 +150,6 @@ def main(prepared_locations_source, destination=None):
             partitionKeys=["snapshot_year", "snapshot_month", "snapshot_day"],
         )
     return features_df
-
-
-def filter_records_since_snapshot_date(locations_df, max_snapshot):
-    if not max_snapshot:
-        return locations_df
-    max_snapshot_date = f"{max_snapshot[0]}{max_snapshot[1]:0>2}{max_snapshot[2]:0>2}"
-    print(f"max snapshot previously processed: {max_snapshot_date}")
-    locations_df = locations_df.withColumn(
-        "snapshot_full_date",
-        F.concat(
-            locations_df.snapshot_year.cast("string"),
-            F.lpad(locations_df.snapshot_month.cast("string"), 2, "0"),
-            F.lpad(locations_df.snapshot_day.cast("string"), 2, "0"),
-        ),
-    )
-    locations_df = locations_df.where(
-        locations_df["snapshot_full_date"] > max_snapshot_date
-    )
-    locations_df.drop("snapshot_full_date")
-    return locations_df
 
 
 def convert_col_to_integer_col(df, col_name):
