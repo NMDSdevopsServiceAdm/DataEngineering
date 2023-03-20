112a44cf190369b500464fc183c71ef941d0321d
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2023-03-20 11:27:26 +0000

    renamed column

--- tests/unit/test_calculate_job_estimate_from_bed_count.py
+++ tests/unit/test_calculate_job_estimate_from_bed_count.py
@@ -46,6 +46,6 @@ class TestCalculateJobCountBedEstimate(unittest.TestCase):
         self.assertEqual(df.count(), 3)
 
         df = df.collect()
-        self.assertEqual(df[0]["job_count"], None)
-        self.assertEqual(df[1]["job_count"], 30)
-        self.assertEqual(df[2]["job_count"], 35)
+        self.assertEqual(df[0]["job_count_unfiltered"], None)
+        self.assertEqual(df[1]["job_count_unfiltered"], 30)
+        self.assertEqual(df[2]["job_count_unfiltered"], 35)
