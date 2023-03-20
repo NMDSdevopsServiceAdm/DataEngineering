a73d9bed2017698e6ea753c6e0abb0ea1619821e
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2023-03-20 09:52:41 +0000

    move snapshot date before filter

--- jobs/prepare_locations.py
+++ jobs/prepare_locations.py
@@ -126,6 +126,10 @@ def main(
             "snapshot_date", F.lit(snapshot_date_row["snapshot_date"])
         )
 
+        output_df = add_three_columns_with_snapshot_date_substrings(output_df)
+
+        output_df = utils.format_import_date(output_df, fieldname="snapshot_date")
+
         output_df = calculate_jobcount(
             output_df, "total_staff", "worker_record_count", "job_count_unfiltered"
         )
@@ -133,10 +137,6 @@ def main(
         output_df = filter_job_count(output_df, "job_count_unfiltered", "job_count")
 
         output_df = add_column_if_locationid_is_in_ascwds(output_df)
-
-        output_df = add_three_columns_with_snapshot_date_substrings(output_df)
-
-        output_df = utils.format_import_date(output_df, fieldname="snapshot_date")
 
         output_df = output_df.select(
             "snapshot_date",
