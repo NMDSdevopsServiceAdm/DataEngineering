6ae8ec11eaee056bffa0d653164ded6f78e3eea8
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2023-03-21 08:43:09 +0000

    removed column names as arguments

--- utils/prepare_locations_utils/filter_job_count/filter_job_count.py
+++ utils/prepare_locations_utils/filter_job_count/filter_job_count.py
@@ -5,14 +5,10 @@ from utils.prepare_locations_utils.filter_job_count.care_home_jobs_per_bed_ratio
 )
 
 
-def filter_job_count(
-    input_df: pyspark.sql.DataFrame, col_to_filter: str, filtered_col_name: str
-) -> pyspark.sql.DataFrame:
+def filter_job_count(input_df: pyspark.sql.DataFrame) -> pyspark.sql.DataFrame:
 
     print("Filtering job_count...")
 
-    input_df = care_home_jobs_per_bed_ratio_outliers(
-        input_df, col_to_filter, filtered_col_name
-    )
+    input_df = care_home_jobs_per_bed_ratio_outliers(input_df)
 
     return input_df
