5a2b429c36db9c65e27d20b79b7e151684536820
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-03-24 12:55:56 +0000

    Fix join so that columns match

--- utils/estimate_job_count/models/non_res_rolling_average.py
+++ utils/estimate_job_count/models/non_res_rolling_average.py
@@ -21,11 +21,9 @@ ROLLING_AVERAGE_TIME_PERIOD_IN_DAYS = 90
 def model_non_res_rolling_average(
     df: DataFrame,
 ) -> DataFrame:
-
+    df.show()
     non_residential_df = df.where(df.primary_service_type == "non-residential")
-
-    care_home_df = df.where(df.primary_service_type != "non-residential")
-    care_home_df = care_home_df.withColumn("model_non_res_rolling_average", F.lit(None).cast(DoubleType()))
+    non_residential_df.show()
 
     df_all_dates = non_residential_df.withColumn("snapshot_timestamp", F.to_timestamp(non_residential_df.snapshot_date))
 
@@ -43,15 +41,23 @@ def model_non_res_rolling_average(
         .agg(F.avg(F.col("job_count")).alias("model_non_res_rolling_average"))
         .withColumn("snapshot_date", F.date_sub(F.col("window.end").cast("date"), 1))
     ).drop("window")
+    # it needs to join with the original data frame here and then we want to duplicate into estimate jobs where necessary, then add the column with the source
+    # will need to remove care home df throughout i think
+    df_with_rolling_average = df.join(rolling_avg, "snapshot_date", how="left")
 
-    non_residential_df_with_rolling_average = non_residential_df.join(rolling_avg, "snapshot_date", how="left")
-    non_residential_df_with_rolling_average.show()
-    df_with_rolling_average = non_residential_df_with_rolling_average.union(care_home_df)
     df_with_rolling_average.show()
+
+    df_with_rolling_average = df_with_rolling_average.withColumn(
+        ESTIMATE_JOB_COUNT,
+        F.when(
+            (F.col(ESTIMATE_JOB_COUNT).isNull() & (F.col(PRIMARY_SERVICE_TYPE) == "non-residential")),
+            F.col("model_non_res_rolling_average"),
+        ).otherwise(F.col(ESTIMATE_JOB_COUNT)),
+    )
+
     df_with_rolling_average = update_dataframe_with_identifying_rule(
         df_with_rolling_average, "model_non_res_rolling_average", ESTIMATE_JOB_COUNT
     )
     df_with_rolling_average.show()
-    df_with_rolling_average = df_with_rolling_average.drop("snapshot_timestamp")
-    df_with_rolling_average.show()
+
     return df_with_rolling_average
