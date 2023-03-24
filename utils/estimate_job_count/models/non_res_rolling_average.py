ba5b8bc11e0767d012d10071f05af77c7c80a9f7
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-03-24 13:43:38 +0000

    Revert "Update constants"
    This reverts commit 685286ee954d763110136400379e3ba1d542c38b.

--- utils/estimate_job_count/models/non_res_rolling_average.py
+++ utils/estimate_job_count/models/non_res_rolling_average.py
@@ -1,7 +1,10 @@
+import pyspark.sql
 from pyspark.sql import DataFrame
 import pyspark.sql.functions as F
+from pyspark.sql.types import DoubleType
 
 from utils.estimate_job_count.column_names import (
+    LOCATION_ID,
     SNAPSHOT_DATE,
     JOB_COUNT,
     ESTIMATE_JOB_COUNT,
@@ -12,58 +15,48 @@ from utils.prepare_locations_utils.job_calculator.job_calculator import (
     update_dataframe_with_identifying_rule,
 )
 
-ROLLING_AVERAGE_TIME_PERIOD = "91 days"
-ROLLING_AVERAGE_WINDOW_SLIDE = "1 days"
-
-NON_RESIDENTIAL = "non-residential"
-SNAPSHOT_TIMESTAMP = "snapshot_timestamp"
-WINDOW = "window"
-WINDOW_END = "window.end"
-MODEL_NAME = "model_non_res_rolling_average"
-
-LEFT_JOIN = "left"
-DATE_TYPE = "date"
-ONE_DAY = 1
+ROLLING_AVERAGE_TIME_PERIOD_IN_DAYS = 90
 
 
 def model_non_res_rolling_average(
     df: DataFrame,
 ) -> DataFrame:
     df.show()
-    non_residential_df = df.where(df.primary_service_type == NON_RESIDENTIAL)
+    non_residential_df = df.where(df.primary_service_type == "non-residential")
     non_residential_df.show()
 
-    df_all_dates = non_residential_df.withColumn(SNAPSHOT_TIMESTAMP, F.to_timestamp(non_residential_df.snapshot_date))
+    df_all_dates = non_residential_df.withColumn("snapshot_timestamp", F.to_timestamp(non_residential_df.snapshot_date))
 
     df_all_dates = df_all_dates.orderBy(df_all_dates.snapshot_timestamp)
     df_all_dates.persist()
 
     rolling_avg_window = F.window(
-        F.col(SNAPSHOT_TIMESTAMP),
-        windowDuration=ROLLING_AVERAGE_TIME_PERIOD,
-        slideDuration=ROLLING_AVERAGE_WINDOW_SLIDE,
-    ).alias(WINDOW)
+        F.col("snapshot_timestamp"),
+        windowDuration="91 days",
+        slideDuration="1 days",
+    ).alias("window")
 
     rolling_avg = (
         df_all_dates.groupBy(rolling_avg_window)
-        .agg(F.avg(F.col(JOB_COUNT)).alias(MODEL_NAME))
-        .withColumn(SNAPSHOT_DATE, F.date_sub(F.col(WINDOW_END).cast(DATE_TYPE), ONE_DAY))
-    ).drop(WINDOW)
-
-    df_with_rolling_average = df.join(rolling_avg, SNAPSHOT_DATE, how=LEFT_JOIN)
+        .agg(F.avg(F.col("job_count")).alias("model_non_res_rolling_average"))
+        .withColumn("snapshot_date", F.date_sub(F.col("window.end").cast("date"), 1))
+    ).drop("window")
+    # it needs to join with the original data frame here and then we want to duplicate into estimate jobs where necessary, then add the column with the source
+    # will need to remove care home df throughout i think
+    df_with_rolling_average = df.join(rolling_avg, "snapshot_date", how="left")
 
     df_with_rolling_average.show()
 
     df_with_rolling_average = df_with_rolling_average.withColumn(
         ESTIMATE_JOB_COUNT,
         F.when(
-            (F.col(ESTIMATE_JOB_COUNT).isNull() & (F.col(PRIMARY_SERVICE_TYPE) == NON_RESIDENTIAL)),
-            F.col(MODEL_NAME),
+            (F.col(ESTIMATE_JOB_COUNT).isNull() & (F.col(PRIMARY_SERVICE_TYPE) == "non-residential")),
+            F.col("model_non_res_rolling_average"),
         ).otherwise(F.col(ESTIMATE_JOB_COUNT)),
     )
 
     df_with_rolling_average = update_dataframe_with_identifying_rule(
-        df_with_rolling_average, MODEL_NAME, ESTIMATE_JOB_COUNT
+        df_with_rolling_average, "model_non_res_rolling_average", ESTIMATE_JOB_COUNT
     )
     df_with_rolling_average.show()
 
