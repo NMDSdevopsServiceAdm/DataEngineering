3c06a6a2093feaa816d41b70bd212a83f6cfb09c
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2023-05-05 13:50:40 +0100

    formatting

--- terraform/modules/s3-bucket/s3.tf
+++ terraform/modules/s3-bucket/s3.tf
@@ -4,8 +4,8 @@ resource "aws_s3_bucket" "s3_bucket" {
 }
 
 resource "aws_s3_bucket_acl" "s3_bucket_acl" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  acl    = "private"
+  bucket     = aws_s3_bucket.s3_bucket.id
+  acl        = "private"
   depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
 }
 
