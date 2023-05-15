f3f7443d20fec2c8dd95466c436901bc91be494c
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2023-05-15 15:00:36 +0100

    first attempt at interpolation

---
+++ utils/estimate_job_count/models/interpolation.py
@@ -0,0 +1,181 @@
+import sys
+import pyspark.sql.functions as F
+from pyspark.sql import DataFrame, Window
+import pyspark.sql
+
+from utils.utils import convert_days_to_unix_time
+from utils.estimate_job_count.column_names import (
+    LOCATION_ID,
+    UNIX_TIME,
+    JOB_COUNT,
+    ESTIMATE_JOB_COUNT,
+)
+from pyspark.sql.types import ArrayType, LongType, FloatType
+from utils.prepare_locations_utils.job_calculator.job_calculator import (
+    update_dataframe_with_identifying_rule,
+)
+
+FIRST_SUBMISSION_TIME = "first_submission_time"
+LAST_SUBMISSION_TIME = "last_submission_time"
+PREVIOUS_JOB_COUNT = "previous_job_count"
+NEXT_JOB_COUNT = "next_job_count"
+JOB_COUNT_UNIX_TIME = "job_count_unix_time"
+PREVIOUS_JOB_COUNT_UNIX_TIME = "previous_job_count_unix_time"
+NEXT_JOB_COUNT_UNIX_TIME = "next_job_count_unix_time"
+INTERPOLATION_MODEL = "interpolation_model"
+
+
+def model_interpolation(df: DataFrame) -> DataFrame:
+
+    known_job_count_df = filter_to_locations_with_a_known_job_count(df)
+
+    first_and_last_submission_date_df = (
+        calculate_first_and_last_submission_date_per_location(known_job_count_df)
+    )
+
+    all_dates_df = convert_first_and_last_known_time_into_timeseries_df(
+        first_and_last_submission_date_df
+    )
+
+    all_dates_df = add_known_job_count_information(all_dates_df, known_job_count_df)
+
+    all_dates_df = interpolate_values_for_all_dates(all_dates_df)
+
+    df = leftouter_join_on_locationid_and_unix_time(df, all_dates_df)
+
+    df = df.withColumn(
+        ESTIMATE_JOB_COUNT,
+        F.when(
+            F.col(ESTIMATE_JOB_COUNT).isNotNull(), F.col(ESTIMATE_JOB_COUNT)
+        ).otherwise(F.col(INTERPOLATION_MODEL)),
+    )
+    df = update_dataframe_with_identifying_rule(
+        df, INTERPOLATION_MODEL, ESTIMATE_JOB_COUNT
+    )
+
+    return df
+
+
+def filter_to_locations_with_a_known_job_count(
+    df: pyspark.sql.DataFrame,
+) -> pyspark.sql.DataFrame:
+
+    df = df.select(LOCATION_ID, UNIX_TIME, JOB_COUNT)
+
+    return df.where(F.col(JOB_COUNT).isNotNull)
+
+
+def calculate_first_and_last_submission_date_per_location(
+    df: pyspark.sql.DataFrame,
+) -> pyspark.sql.DataFrame:
+
+    return df.groupBy(LOCATION_ID).agg(
+        F.min(UNIX_TIME).cast("integer").alias(FIRST_SUBMISSION_TIME),
+        F.max(UNIX_TIME).cast("integer").alias(LAST_SUBMISSION_TIME),
+    )
+
+
+def convert_first_and_last_known_time_into_timeseries_df(df):
+    date_range_udf = F.udf(date_range, ArrayType(LongType()))
+
+    return df.withColumn(
+        "unix_time",
+        F.explode(date_range_udf(FIRST_SUBMISSION_TIME, LAST_SUBMISSION_TIME)),
+    ).drop(FIRST_SUBMISSION_TIME, LAST_SUBMISSION_TIME)
+
+
+def date_range(unix_start_time: int, unix_finish_time: int, step_size_in_days: int = 1):
+    """Return a list of equally spaced points between unix_start_time and unix_finish_time with set stepsizes"""
+    unix_time_step = convert_days_to_unix_time(step_size_in_days)
+
+    return [
+        unix_start_time + unix_time_step * x
+        for x in range(int((unix_finish_time - unix_start_time) / unix_time_step) + 1)
+    ]
+
+
+def add_known_job_count_information(
+    df: DataFrame, known_job_count_df: DataFrame
+) -> DataFrame:
+
+    df = leftouter_join_on_locationid_and_unix_time(df, known_job_count_df)
+
+    return add_unix_time_for_known_job_count(df)
+
+
+def leftouter_join_on_locationid_and_unix_time(
+    df: DataFrame, other_df: DataFrame
+) -> DataFrame:
+    return df.join(other_df, [LOCATION_ID, UNIX_TIME], "leftouter")
+
+
+def add_unix_time_for_known_job_count(df: DataFrame) -> DataFrame:
+    return df.withColumn(
+        JOB_COUNT_UNIX_TIME,
+        F.when((F.col(JOB_COUNT) > 0.0), F.col(UNIX_TIME)).otherwise(F.lit(None)),
+    )
+
+
+def window_for_previous_value() -> Window:
+    return (
+        Window.partitionBy(LOCATION_ID).orderBy(UNIX_TIME).rowsBetween(-sys.maxsize, 0)
+    )
+
+
+def get_previous_value_in_column(
+    df: DataFrame, column_name: str, new_column_name: str
+) -> DataFrame:
+    return df.withColumn(
+        new_column_name, F.last(F.col(column_name), ignorenulls=True)
+    ).over(window_for_previous_value)
+
+
+def window_for_next_value() -> Window:
+    return (
+        Window.partitionBy(LOCATION_ID).orderBy(UNIX_TIME).rowsBetween(0, sys.maxsize)
+    )
+
+
+def get_next_value_in_new_column(
+    df: DataFrame, column_name: str, new_column_name: str
+) -> DataFrame:
+    return df.withColumn(
+        new_column_name, F.first(F.col(column_name), ignorenulls=True)
+    ).over(window_for_next_value)
+
+
+def input_previous_and_next_values_into_df(df: DataFrame) -> DataFrame:
+    df = get_previous_value_in_column(df, JOB_COUNT, PREVIOUS_JOB_COUNT)
+    df = get_previous_value_in_column(
+        df, JOB_COUNT_UNIX_TIME, PREVIOUS_JOB_COUNT_UNIX_TIME
+    )
+    df = get_next_value_in_new_column(df, JOB_COUNT, NEXT_JOB_COUNT)
+    return get_next_value_in_new_column(
+        df, JOB_COUNT_UNIX_TIME, NEXT_JOB_COUNT_UNIX_TIME
+    )
+
+
+def interpolate_values_for_all_dates(df: DataFrame) -> DataFrame:
+    interpol_udf = F.udf(interpolation_calculation, FloatType())
+
+    df = df.withColumn(
+        INTERPOLATION_MODEL,
+        interpol_udf(
+            UNIX_TIME,
+            PREVIOUS_JOB_COUNT_UNIX_TIME,
+            NEXT_JOB_COUNT_UNIX_TIME,
+            JOB_COUNT,
+            PREVIOUS_JOB_COUNT,
+            NEXT_JOB_COUNT,
+        ),
+    )
+
+    return df.select(LOCATION_ID, UNIX_TIME, INTERPOLATION_MODEL)
+
+
+def interpolation_calculation(x, x_prev, x_next, y, y_prev, y_next):
+    if x_prev == x_next:
+        return y
+    else:
+        m = (y_next - y_prev) / (x_next - x_prev)
+        return y_prev + m * (x - x_prev)
