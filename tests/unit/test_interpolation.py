8b77f13954838efe60fd659bc84548de634f5213
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2023-05-26 14:19:39 +0100

    test functions to populate

--- tests/unit/test_interpolation.py
+++ tests/unit/test_interpolation.py
@@ -54,3 +54,46 @@ class TestModelInterpolation(unittest.TestCase):
     def test_model_interpolation_row_count_unchanged(self):
 
         self.assertEqual(self.interpolation_df.count(), len(self.rows))
+
+    def test_filter_to_locations_with_a_known_job_count(self):
+        df = self.spark.createDataFrame(self.rows, schema=self.column_schema)
+        filtered_df = job.filter_to_locations_with_a_known_job_count(df)
+
+        self.assertEqual(filtered_df.count(), 5)
+        self.assertEqual(filtered_df.columns, ["locationid", "unix_time", "job_count"])
+
+    def test_calculate_first_and_last_submission_date_per_location(self):
+        pass
+
+    def test_convert_first_and_last_known_time_into_timeseries_df(self):
+        pass
+
+    def test_date_range(self):
+        pass
+
+    def test_add_known_job_count_information(self):
+        pass
+
+    def test_leftouter_join_on_locationid_and_unix_time(self):
+        pass
+
+    def test_add_unix_time_for_known_job_count(self):
+        pass
+
+    def test_get_previous_value_in_column(self):
+        pass
+
+    def test_get_next_value_in_new_column(self):
+        pass
+
+    def test_interpolate_values_for_all_dates(self):
+        pass
+
+    def test_input_previous_and_next_values_into_df(self):
+        pass
+
+    def test_calculated_interpolated_values_in_new_column(self):
+        pass
+
+    def test_interpolation_calculation(self):
+        pass
