2b25133f4d96cc5531343c3500db383bb7fa1a39
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-05-26 15:12:11 +0100

    Update glue to 3.0 for worker_tracking.py

--- terraform/pipeline/glue.tf
+++ terraform/pipeline/glue.tf
@@ -113,7 +113,7 @@ module "worker_tracking_job" {
   glue_role       = aws_iam_role.sfc_glue_service_iam_role
   resource_bucket = module.pipeline_resources
   datasets_bucket = module.datasets_bucket
-  glue_version    = "2.0"
+  glue_version    = "3.0"
 
 
   job_parameters = {
