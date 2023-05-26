422eb817c4db491152eb9a1690a5eb0306016fc8
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-05-26 16:02:22 +0100

    Change schema name

---
+++ schemas/direct_payment_data_schema.py
@@ -0,0 +1,28 @@
+from pyspark.sql.types import (
+    StructField,
+    StructType,
+    StringType,
+    FloatType,
+    IntegerType,
+)
+
+
+DIRECT_PAYMENTS_DATA = StructType(
+    fields=[
+        StructField("LOCATIONID", StringType(), True),
+        StructField("Main_Service_Group", IntegerType(), False),
+        StructField("Care_homes_beds", FloatType(), False),
+        StructField("WEIGHTING_CSSR", IntegerType(), False),
+        StructField("WEIGHTING_REGION", IntegerType(), False),
+        StructField("totalstaff", FloatType(), False),
+        StructField("wkrrecs", FloatType(), False),
+        StructField("jr28work", FloatType(), False),
+        StructField("Data_Used", IntegerType(), True),
+        StructField("All_jobs", FloatType(), True),
+        StructField("Snapshot_date", StringType(), True),
+        StructField("Local_authority", StringType(), False),
+        StructField("Region", StringType(), False),
+        StructField("Main_service", StringType(), False),
+        StructField("Data_used_string", StringType(), False),
+    ]
+)
