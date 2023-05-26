1c93df60577407167209cd5e5c8abeaadd5e7a48
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2023-05-26 09:33:38 +0100

    added interpolation into estimate job count job

--- jobs/estimate_job_counts.py
+++ jobs/estimate_job_counts.py
@@ -31,6 +31,7 @@ from utils.estimate_job_count.models.primary_service_rolling_average import (
 from utils.estimate_job_count.models.non_res_historical import (
     model_non_res_historical,
 )
+from utils.estimate_job_count.models.interpolation import model_interpolation
 from utils.estimate_job_count.models.non_res_with_pir import (
     model_non_residential_with_pir,
 )
@@ -114,6 +115,8 @@ def main(
         carehome_features_df,
         care_home_model_directory,
     )
+
+    locations_df = model_interpolation(locations_df)
 
     care_home_model_info = care_home_model_directory.split("/")
     write_metrics_df(
