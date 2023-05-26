2e259fb863d530459f9597d5a029f2801b125aa7
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-05-26 16:05:27 +0100

    Update column names in schema

--- schemas/direct_payment_data_schema.py
+++ schemas/direct_payment_data_schema.py
@@ -9,20 +9,18 @@ from pyspark.sql.types import (
 
 DIRECT_PAYMENTS_DATA = StructType(
     fields=[
-        StructField("LOCATIONID", StringType(), True),
-        StructField("Main_Service_Group", IntegerType(), False),
-        StructField("Care_homes_beds", FloatType(), False),
-        StructField("WEIGHTING_CSSR", IntegerType(), False),
-        StructField("WEIGHTING_REGION", IntegerType(), False),
-        StructField("totalstaff", FloatType(), False),
-        StructField("wkrrecs", FloatType(), False),
-        StructField("jr28work", FloatType(), False),
-        StructField("Data_Used", IntegerType(), True),
-        StructField("All_jobs", FloatType(), True),
-        StructField("Snapshot_date", StringType(), True),
-        StructField("Local_authority", StringType(), False),
-        StructField("Region", StringType(), False),
-        StructField("Main_service", StringType(), False),
-        StructField("Data_used_string", StringType(), False),
+        StructField("la_area_adass", StringType(), True),
+        StructField("la_area_ascof", IntegerType(), False),
+        StructField("la_area_salt", FloatType(), False),
+        StructField("la_area_excel", IntegerType(), False),
+        StructField("la_area_imd", IntegerType(), False),
+        StructField("year", FloatType(), False),
+        StructField("proportion_su_employing_staff_adass", FloatType(), False),
+        StructField("number_su_dpr_salt", FloatType(), False),
+        StructField("number_carer_dpr_salt", IntegerType(), True),
+        StructField("number_su_dpr_year_end_ascof", FloatType(), True),
+        StructField("number_carer_dpr_year_end_ascof", StringType(), True),
+        StructField("imd_2010", StringType(), False),
+        StructField("la_area_aws", StringType(), False),
     ]
 )
