724866257b3c95dff975ad983d0575ad99d6370c
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-05-26 16:08:58 +0100

    Update datatypes and nullable parameters

--- schemas/direct_payment_data_schema.py
+++ schemas/direct_payment_data_schema.py
@@ -10,17 +10,17 @@ from pyspark.sql.types import (
 DIRECT_PAYMENTS_DATA = StructType(
     fields=[
         StructField("la_area_adass", StringType(), True),
-        StructField("la_area_ascof", IntegerType(), False),
-        StructField("la_area_salt", FloatType(), False),
-        StructField("la_area_excel", IntegerType(), False),
-        StructField("la_area_imd", IntegerType(), False),
-        StructField("year", FloatType(), False),
-        StructField("proportion_su_employing_staff_adass", FloatType(), False),
-        StructField("number_su_dpr_salt", FloatType(), False),
-        StructField("number_carer_dpr_salt", IntegerType(), True),
+        StructField("la_area_ascof", StringType(), True),
+        StructField("la_area_salt", StringType(), True),
+        StructField("la_area_excel", StringType(), True),
+        StructField("la_area_imd", StringType(), True),
+        StructField("year", IntegerType(), False),
+        StructField("proportion_su_employing_staff_adass", FloatType(), True),
+        StructField("number_su_dpr_salt", FloatType(), True),
+        StructField("number_carer_dpr_salt", FloatType(), True),
         StructField("number_su_dpr_year_end_ascof", FloatType(), True),
-        StructField("number_carer_dpr_year_end_ascof", StringType(), True),
-        StructField("imd_2010", StringType(), False),
+        StructField("number_carer_dpr_year_end_ascof", FloatType(), True),
+        StructField("imd_2010", FloatType(), True),
         StructField("la_area_aws", StringType(), False),
     ]
 )
