2e8cabbe3e35e67c11d59786dd2cff5e3cfb0d1b
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-05-30 16:37:09 +0100

    Create structure for determine areas including carers on adass script

---
+++ utils/prepare_direct_payments_utils/determine_areas_including_carers_on_adass.py
@@ -0,0 +1,10 @@
+from pyspark.sql import DataFrame
+
+from utils.prepare_direct_payments_utils.direct_payments_column_names import (
+    DirectPaymentColumnNames as DP,
+)
+
+
+def determine_areas_including_carers_on_adass(df: DataFrame) -> DataFrame:
+    # TODO
+    return df
