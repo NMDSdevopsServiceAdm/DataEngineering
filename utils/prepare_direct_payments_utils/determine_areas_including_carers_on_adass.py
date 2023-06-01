dcc3026e6c858100403f906e65726c7478f13f66
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-01 09:13:05 +0100

    Make notes on structure

--- utils/prepare_direct_payments_utils/determine_areas_including_carers_on_adass.py
+++ utils/prepare_direct_payments_utils/determine_areas_including_carers_on_adass.py
@@ -13,4 +13,9 @@ from utils.prepare_direct_payments_utils.direct_payments_column_names import (
 
 def determine_areas_including_carers_on_adass(df: DataFrame) -> DataFrame:
     # TODO
+    # calculate_total_dprs_during_year()
+    # calculate_dprs_employing_staff()
+    # calculate_carers_employing_staff()
+    # calculate_total_employing_staff_including_carers()
+    # calculate_difference_between_total_employing_staff_and_
     return df
