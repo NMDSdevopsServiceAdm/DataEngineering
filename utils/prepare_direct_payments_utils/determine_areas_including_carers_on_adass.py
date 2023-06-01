d00b2f0d80d86dd368869b56b19a8f99ebe5e8e8
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-01 12:49:11 +0100

    Create filter for most recent years

--- utils/prepare_direct_payments_utils/determine_areas_including_carers_on_adass.py
+++ utils/prepare_direct_payments_utils/determine_areas_including_carers_on_adass.py
@@ -11,11 +11,22 @@ from utils.prepare_direct_payments_utils.direct_payments_column_names import (
 )
 
 
-def determine_areas_including_carers_on_adass(df: DataFrame) -> DataFrame:
+def determine_areas_including_carers_on_adass(direct_payments_df: DataFrame) -> DataFrame:
     # TODO
+    # filter to most recent year
+    most_recent_direct_payments_df = filter_to_most_recent_year(direct_payments_df)
     # calculate_total_dprs_during_year()
     # calculate_dprs_employing_staff()
     # calculate_carers_employing_staff()
     # calculate_total_employing_staff_including_carers()
-    # calculate_difference_between_total_employing_staff_and_
+    # determine_if_survey_base_is_close_to_ascof_base
+    # alocate_method()
+    # calculate_proportion_of_su_employing_staff
+    # rejoin to table
+    return direct_payments_df
+
+
+def filter_to_most_recent_year(df: DataFrame) -> DataFrame:
+    most_recent_year = F.max(DP.YEAR)
+    df = df.where(DP.YEAR == most_recent_year)
     return df
