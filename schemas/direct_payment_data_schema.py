4410b47a505a846e2d31e83409999d16adca03ac
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-01 09:15:14 +0100

    Add new column names to schema

--- schemas/direct_payment_data_schema.py
+++ schemas/direct_payment_data_schema.py
@@ -22,5 +22,7 @@ DIRECT_PAYMENTS_DATA = StructType(
         StructField("number_carer_dpr_year_end_ascof", FloatType(), True),
         StructField("imd_2010", FloatType(), True),
         StructField("la_area_aws", StringType(), False),
+        StructField("number_of_dprs_adass", FloatType(), False),
+        StructField("number_of_dprs_who_employ_staff_adass", FloatType(), False),
     ]
 )
