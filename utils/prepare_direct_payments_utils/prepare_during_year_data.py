07a24e9b5732b11b70c311d0554b6938cdbfbb3d
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-09 10:06:46 +0100

    Calculate total dpr during year

--- utils/prepare_direct_payments_utils/prepare_during_year_data.py
+++ utils/prepare_direct_payments_utils/prepare_during_year_data.py
@@ -6,5 +6,14 @@ from utils.prepare_direct_payments_utils.direct_payments_column_names import (
 )
 
 
-def prepare_during_year_data(df: DataFrame) -> DataFrame:
+def prepare_during_year_data(direct_payments_df: DataFrame) -> DataFrame:
+    direct_payments_df = calculate_total_dprs_during_year(direct_payments_df)
+    return direct_payments_df
+
+
+def calculate_total_dprs_during_year(df: DataFrame) -> DataFrame:
+    df = df.withColumn(
+        DP.TOTAL_DPRS_DURING_YEAR,
+        F.col(DP.SERVICE_USER_DPRS_DURING_YEAR) + F.col(DP.CARER_DPRS_DURING_YEAR),
+    )
     return df
