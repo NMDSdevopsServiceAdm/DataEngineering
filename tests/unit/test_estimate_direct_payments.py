4fc6900c3dbecb4a5be2f03630c3c0d1c47db463
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-09 12:32:23 +0100

    Create python script for test estimate direct payments

---
+++ tests/unit/test_estimate_direct_payments.py
@@ -0,0 +1,13 @@
+import unittest
+import warnings
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    IntegerType,
+    FloatType,
+)
+
+import jobs.prepare_direct_payments as job
