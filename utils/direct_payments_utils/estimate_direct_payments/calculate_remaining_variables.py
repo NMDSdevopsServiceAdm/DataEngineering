2f2e017adaeb235f6fb6bbd6a9f4075cdbcf2a44
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-12 09:38:18 +0100

    Formatting

--- utils/direct_payments_utils/estimate_direct_payments/calculate_remaining_variables.py
+++ utils/direct_payments_utils/estimate_direct_payments/calculate_remaining_variables.py
@@ -4,10 +4,14 @@ from pyspark.sql import DataFrame
 def calculate_remaining_variables(
     direct_payments_df: DataFrame,
 ) -> DataFrame:
-    direct_payments_df = calculate_service_users_with_self_employed_staff(direct_payments_df)
+    direct_payments_df = calculate_service_users_with_self_employed_staff(
+        direct_payments_df
+    )
     direct_payments_df = calculate_carers_employing_staff(direct_payments_df)
     direct_payments_df = calculate_total_dpr_employing_staff(direct_payments_df)
-    direct_payments_df = calculate_total_personal_assistant_filled_posts(direct_payments_df)
+    direct_payments_df = calculate_total_personal_assistant_filled_posts(
+        direct_payments_df
+    )
     direct_payments_df = calculate_proportion_of_dpr_employing_staff(direct_payments_df)
     return direct_payments_df
 
