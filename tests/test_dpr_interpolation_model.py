cb74fae3748942792727f23b9d1894544eb3c34e
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-16 10:52:26 +0100

    Write test for filtering

--- tests/test_dpr_interpolation_model.py
+++ tests/test_dpr_interpolation_model.py
@@ -48,3 +48,34 @@ class TestDPRInterpolation(unittest.TestCase):
         df = self.spark.createDataFrame(rows, schema=test_schema)
         output_df = job.model_interpolation(df)
         self.assertEqual(df.count(), output_df.count())
+
+    @unittest.skip("to do")
+    def test_filter_to_locations_with_a_known_service_users_employing_staff_filters_correctly(self):
+        rows = [
+            ("area_1", 2021, 300.0, 0.3),
+            ("area_2", 2021, 300.0, 0.4),
+            ("area_1", 2020, None, 0.45),
+            ("area_2", 2020, 300.0, 0.35),
+            ("area_1", 2019, None, 0.375),
+            ("area_2", 2019, 300.0, 0.2),
+        ]
+        test_schema = StructType(
+            [
+                StructField(DP.LA_AREA, StringType(), False),
+                StructField(DP.YEAR, IntegerType(), True),
+                StructField(
+                    DP.ESTIMATED_SERVICE_USER_DPRS_DURING_YEAR_EMPLOYING_STAFF,
+                    FloatType(),
+                    True,
+                ),
+                StructField(DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF, FloatType(), True),
+            ]
+        )
+        df = self.spark.createDataFrame(rows, schema=test_schema)
+        output_df = job.filter_to_locations_with_a_known_service_users_employing_staff(df)
+
+        self.assertEqual(output_df.count(), 4)
+        self.assertEqual(
+            output_df.columns,
+            [DP.LA_AREA, DP.YEAR_AS_INTEGER, DP.ESTIMATED_SERVICE_USER_DPRS_DURING_YEAR_EMPLOYING_STAFF],
+        )
