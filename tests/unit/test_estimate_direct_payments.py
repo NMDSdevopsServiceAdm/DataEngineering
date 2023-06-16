b13c297f29e6856759b71b172e0bc215e2e3bd1f
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-16 16:39:38 +0100

    Create function to select models for estimate of proportions

--- tests/unit/test_estimate_direct_payments.py
+++ tests/unit/test_estimate_direct_payments.py
@@ -10,4 +10,76 @@ from pyspark.sql.types import (
     FloatType,
 )
 
-import jobs.prepare_direct_payments as job
+import utils.direct_payments_utils.estimate_direct_payments.estimate_service_users_employing_staff as job
+from utils.direct_payments_utils.direct_payments_column_names import (
+    DirectPaymentColumnNames as DP,
+)
+
+
+class TestEstimateServiceUsersEmployingStaff(unittest.TestCase):
+    def setUp(self):
+        self.spark = SparkSession.builder.appName("test_extrapolation_ratio").getOrCreate()
+
+        warnings.simplefilter("ignore", ResourceWarning)
+
+    def test_apply_models_returns_correct_values(self):
+        rows = [
+            ("area_1", 2021, 0.49, None, 0.4),
+            ("area_2", 2021, None, None, 0.4),
+            ("area_1", 2020, 0.45, 0.45, 0.5),
+            ("area_2", 2020, None, None, 0.5),
+            ("area_1", 2019, None, 0.41, 0.6),
+            ("area_2", 2019, None, None, 0.6),
+            ("area_1", 2018, 0.37, 0.37, 0.6),
+            ("area_2", 2018, None, None, 0.6),
+            ("area_1", 2017, 0.33, None, 0.6),
+            ("area_2", 2017, None, None, 0.6),
+        ]
+        test_schema = StructType(
+            [
+                StructField(DP.LA_AREA, StringType(), False),
+                StructField(DP.YEAR_AS_INTEGER, IntegerType(), True),
+                StructField(
+                    DP.ESTIMATE_USING_EXTRAPOLATION_RATIO,
+                    FloatType(),
+                    True,
+                ),
+                StructField(DP.ESTIMATE_USING_INTERPOLATION, FloatType(), True),
+                StructField(DP.ESTIMATE_USING_MEAN, FloatType(), True),
+            ]
+        )
+        df = self.spark.createDataFrame(rows, schema=test_schema)
+        output_df = job.apply_models(df)
+        output_df_list = output_df.sort(DP.LA_AREA, DP.YEAR_AS_INTEGER).collect()
+
+        self.assertAlmostEqual(
+            output_df_list[0][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.33, places=5
+        )
+        self.assertAlmostEqual(
+            output_df_list[1][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.37, places=5
+        )
+        self.assertAlmostEqual(
+            output_df_list[2][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.41, places=5
+        )
+        self.assertAlmostEqual(
+            output_df_list[3][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.45, places=5
+        )
+        self.assertAlmostEqual(
+            output_df_list[4][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.49, places=5
+        )
+        self.assertAlmostEqual(
+            output_df_list[5][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.6, places=5
+        )
+        self.assertAlmostEqual(
+            output_df_list[6][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.6, places=5
+        )
+        self.assertAlmostEqual(
+            output_df_list[7][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.6, places=5
+        )
+        self.assertAlmostEqual(
+            output_df_list[8][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.5, places=5
+        )
+        self.assertAlmostEqual(
+            output_df_list[9][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.4, places=5
+        )
+        self.assertEqual(output_df.count(), df.count())
--- utils/direct_payments_utils/estimate_direct_payments/estimate_service_users_employing_staff.py
+++ utils/direct_payments_utils/estimate_direct_payments/estimate_service_users_employing_staff.py
@@ -16,7 +16,7 @@ from utils.direct_payments_utils.estimate_direct_payments.models.interpolation i
     model_interpolation,
 )
 from utils.direct_payments_utils.estimate_direct_payments.models.mean_imputation import (
-    model_when_no_historical_data,
+    model_using_mean,
 )
 
 
@@ -24,13 +24,9 @@ def estimate_service_users_employing_staff(
     direct_payments_df: DataFrame,
 ) -> DataFrame:
 
-    direct_payments_df = calculate_known_values_for_service_users_employing_staff(
-        direct_payments_df
-    )
+    direct_payments_df = calculate_known_values_for_service_users_employing_staff(direct_payments_df)
 
-    direct_payments_df = estimate_missing_data_for_service_users_employing_staff(
-        direct_payments_df
-    )
+    direct_payments_df = estimate_missing_data_for_service_users_employing_staff(direct_payments_df)
     return direct_payments_df
 
 
@@ -39,8 +35,7 @@ def calculate_known_values_for_service_users_employing_staff(
 ) -> DataFrame:
     direct_payments_df = direct_payments_df.withColumn(
         DP.ESTIMATED_SERVICE_USER_DPRS_DURING_YEAR_EMPLOYING_STAFF,
-        F.col(DP.SERVICE_USER_DPRS_DURING_YEAR)
-        * F.col(DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF),
+        F.col(DP.SERVICE_USER_DPRS_DURING_YEAR) * F.col(DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF),
     )
     return direct_payments_df
 
@@ -54,7 +49,7 @@ def estimate_missing_data_for_service_users_employing_staff(
     # Calculate mean %
     # Calculate mean % * SALT
     # Save in column
-    direct_payments_df = model_when_no_historical_data(direct_payments_df)
+    direct_payments_df = model_using_mean(direct_payments_df)
 
     # If we have gaps at the beginning, ratio back from last known year
     # ratio model
@@ -67,5 +62,26 @@ def estimate_missing_data_for_service_users_employing_staff(
     direct_payments_df = model_interpolation(direct_payments_df)
 
     # select which column to use
+    direct_payments_df = apply_models(direct_payments_df)
 
+    return direct_payments_df
+
+
+def apply_models(direct_payments_df: DataFrame) -> DataFrame:
+    direct_payments_df = direct_payments_df.withColumn(
+        DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF, F.col(DP.ESTIMATE_USING_INTERPOLATION)
+    )
+    direct_payments_df = direct_payments_df.withColumn(
+        DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
+        F.when(
+            F.col(DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF).isNull(),
+            F.col(DP.ESTIMATE_USING_EXTRAPOLATION_RATIO),
+        ).otherwise(F.col(DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF)),
+    )
+    direct_payments_df = direct_payments_df.withColumn(
+        DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
+        F.when(
+            F.col(DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF).isNull(), F.col(DP.ESTIMATE_USING_MEAN)
+        ).otherwise(F.col(DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF)),
+    )
     return direct_payments_df
