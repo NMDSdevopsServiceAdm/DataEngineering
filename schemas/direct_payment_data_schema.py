bdca6f103c406f437ff50ca53a724e6e2fdae84e
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-19 11:27:09 +0100

    Update dpr schema

--- schemas/direct_payment_data_schema.py
+++ schemas/direct_payment_data_schema.py
@@ -9,13 +9,6 @@ from pyspark.sql.types import (
 
 DIRECT_PAYMENTS_DATA = StructType(
     fields=[
-        StructField("la_area_adass", StringType(), True),
-        StructField("la_area_ascof", StringType(), True),
-        StructField("la_area_salt", StringType(), True),
-        StructField("la_area_excel", StringType(), True),
-        StructField("la_area_imd", StringType(), True),
-        StructField("year", IntegerType(), False),
-        StructField("proportion_su_only_employing_staff", FloatType(), True),
         StructField("number_su_dpr_salt", FloatType(), True),
         StructField("number_carer_dpr_salt", FloatType(), True),
         StructField("number_su_dpr_year_end_ascof", FloatType(), True),
@@ -24,5 +17,8 @@ DIRECT_PAYMENTS_DATA = StructType(
         StructField("la_area_aws", StringType(), False),
         StructField("number_of_dprs_adass", FloatType(), False),
         StructField("number_of_dprs_who_employ_staff_adass", FloatType(), False),
+        StructField("year", IntegerType(), False),
+        StructField("proportion_su_only_employing_staff", FloatType(), True),
+        StructField("prev_service_user_employing_staff_proportion", FloatType(), True),
     ]
 )
