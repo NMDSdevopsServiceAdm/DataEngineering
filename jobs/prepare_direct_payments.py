457ff99b708ba16323258b148e4a159f81f997f9
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-19 11:30:05 +0100

    Update selected columns in prepare_direct_payments

--- jobs/prepare_direct_payments.py
+++ jobs/prepare_direct_payments.py
@@ -23,9 +23,7 @@ def main(
     direct_payments_source,
     destination,
 ):
-    spark = SparkSession.builder.appName(
-        "sfc_data_engineering_prepare_direct_payments"
-    ).getOrCreate()
+    spark = SparkSession.builder.appName("sfc_data_engineering_prepare_direct_payments").getOrCreate()
 
     direct_payments_df: DataFrame = spark.read.parquet(direct_payments_source).select(
         DP.LA_AREA,
@@ -36,8 +34,8 @@ def main(
         DP.CARER_DPRS_AT_YEAR_END,
         DP.SERVICE_USER_DPRS_DURING_YEAR,
         DP.CARER_DPRS_DURING_YEAR,
-        DP.IMD_SCORE,
         DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
+        DP.HISTORIC_SERVICE_USERS_EMPLOYING_STAFF_ESTIMATE,
     )
 
     direct_payments_df = determine_areas_including_carers_on_adass(direct_payments_df)
