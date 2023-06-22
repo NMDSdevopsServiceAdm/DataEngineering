808cfcf625dac1173acd46b8cf36ee53bfc22751
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-22 16:08:24 +0100

    Create outline of remaining tests

--- tests/unit/test_dpr_calculate_remaining_variables.py
+++ tests/unit/test_dpr_calculate_remaining_variables.py
@@ -53,3 +53,163 @@ class TestCalculateRemainingVariables(unittest.TestCase):
         output_df = job.calculate_remaining_variables(df)
 
         self.assertEqual(output_df.count(), df.count())
+
+    def test_calculate_service_users_with_self_employed_staff_returns_correct_values(self):
+        rows = [
+            ("area_1", 2021, 0.49, 400.0, 30.0, 800.0),
+            ("area_2", 2021, 0.34, 390.0, 32.0, 850.0),
+            ("area_1", 2020, 0.45, 380.0, 34.0, 1100.0),
+            ("area_2", 2020, 0.79, 370.0, 36.0, 670.0),
+            ("area_1", 2019, 0.27, 360.0, 38.0, 530.0),
+            ("area_2", 2019, 0.45, 350.0, 40.0, 640.0),
+            ("area_1", 2018, 0.37, 340.0, 42.0, 1000.0),
+            ("area_2", 2018, 0.63, 330.0, 44.0, 800.0),
+            ("area_1", 2017, 0.33, 320.0, 46.0, 900.0),
+            ("area_2", 2017, 0.34, 310.0, 50.0, 1000.0),
+        ]
+        test_schema = StructType(
+            [
+                StructField(DP.LA_AREA, StringType(), False),
+                StructField(DP.YEAR_AS_INTEGER, IntegerType(), True),
+                StructField(
+                    DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
+                    FloatType(),
+                    True,
+                ),
+                StructField(DP.ESTIMATED_SERVICE_USER_DPRS_DURING_YEAR_EMPLOYING_STAFF, FloatType(), True),
+                StructField(DP.CARER_DPRS_DURING_YEAR, FloatType(), True),
+                StructField(DP.TOTAL_DPRS_DURING_YEAR, FloatType(), True),
+            ]
+        )
+        df = self.spark.createDataFrame(rows, schema=test_schema)
+        output_df = job.calculate_service_users_with_self_employed_staff(df)
+
+        self.assertEqual(output_df.count(), df.count())
+
+    def test_calculate_carers_employing_staff_returns_correct_values(self):
+        rows = [
+            ("area_1", 2021, 0.49, 400.0, 30.0, 800.0),
+            ("area_2", 2021, 0.34, 390.0, 32.0, 850.0),
+            ("area_1", 2020, 0.45, 380.0, 34.0, 1100.0),
+            ("area_2", 2020, 0.79, 370.0, 36.0, 670.0),
+            ("area_1", 2019, 0.27, 360.0, 38.0, 530.0),
+            ("area_2", 2019, 0.45, 350.0, 40.0, 640.0),
+            ("area_1", 2018, 0.37, 340.0, 42.0, 1000.0),
+            ("area_2", 2018, 0.63, 330.0, 44.0, 800.0),
+            ("area_1", 2017, 0.33, 320.0, 46.0, 900.0),
+            ("area_2", 2017, 0.34, 310.0, 50.0, 1000.0),
+        ]
+        test_schema = StructType(
+            [
+                StructField(DP.LA_AREA, StringType(), False),
+                StructField(DP.YEAR_AS_INTEGER, IntegerType(), True),
+                StructField(
+                    DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
+                    FloatType(),
+                    True,
+                ),
+                StructField(DP.ESTIMATED_SERVICE_USER_DPRS_DURING_YEAR_EMPLOYING_STAFF, FloatType(), True),
+                StructField(DP.CARER_DPRS_DURING_YEAR, FloatType(), True),
+                StructField(DP.TOTAL_DPRS_DURING_YEAR, FloatType(), True),
+            ]
+        )
+        df = self.spark.createDataFrame(rows, schema=test_schema)
+        output_df = job.calculate_carers_employing_staff(df)
+
+        self.assertEqual(output_df.count(), df.count())
+
+    def test_calculate_total_dpr_employing_staff_returns_correct_values(self):
+        rows = [
+            ("area_1", 2021, 0.49, 400.0, 30.0, 800.0),
+            ("area_2", 2021, 0.34, 390.0, 32.0, 850.0),
+            ("area_1", 2020, 0.45, 380.0, 34.0, 1100.0),
+            ("area_2", 2020, 0.79, 370.0, 36.0, 670.0),
+            ("area_1", 2019, 0.27, 360.0, 38.0, 530.0),
+            ("area_2", 2019, 0.45, 350.0, 40.0, 640.0),
+            ("area_1", 2018, 0.37, 340.0, 42.0, 1000.0),
+            ("area_2", 2018, 0.63, 330.0, 44.0, 800.0),
+            ("area_1", 2017, 0.33, 320.0, 46.0, 900.0),
+            ("area_2", 2017, 0.34, 310.0, 50.0, 1000.0),
+        ]
+        test_schema = StructType(
+            [
+                StructField(DP.LA_AREA, StringType(), False),
+                StructField(DP.YEAR_AS_INTEGER, IntegerType(), True),
+                StructField(
+                    DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
+                    FloatType(),
+                    True,
+                ),
+                StructField(DP.ESTIMATED_SERVICE_USER_DPRS_DURING_YEAR_EMPLOYING_STAFF, FloatType(), True),
+                StructField(DP.CARER_DPRS_DURING_YEAR, FloatType(), True),
+                StructField(DP.TOTAL_DPRS_DURING_YEAR, FloatType(), True),
+            ]
+        )
+        df = self.spark.createDataFrame(rows, schema=test_schema)
+        output_df = job.calculate_total_dpr_employing_staff(df)
+
+        self.assertEqual(output_df.count(), df.count())
+
+    def test_calculate_total_personal_assistant_filled_posts_returns_correct_values(self):
+        rows = [
+            ("area_1", 2021, 0.49, 400.0, 30.0, 800.0),
+            ("area_2", 2021, 0.34, 390.0, 32.0, 850.0),
+            ("area_1", 2020, 0.45, 380.0, 34.0, 1100.0),
+            ("area_2", 2020, 0.79, 370.0, 36.0, 670.0),
+            ("area_1", 2019, 0.27, 360.0, 38.0, 530.0),
+            ("area_2", 2019, 0.45, 350.0, 40.0, 640.0),
+            ("area_1", 2018, 0.37, 340.0, 42.0, 1000.0),
+            ("area_2", 2018, 0.63, 330.0, 44.0, 800.0),
+            ("area_1", 2017, 0.33, 320.0, 46.0, 900.0),
+            ("area_2", 2017, 0.34, 310.0, 50.0, 1000.0),
+        ]
+        test_schema = StructType(
+            [
+                StructField(DP.LA_AREA, StringType(), False),
+                StructField(DP.YEAR_AS_INTEGER, IntegerType(), True),
+                StructField(
+                    DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
+                    FloatType(),
+                    True,
+                ),
+                StructField(DP.ESTIMATED_SERVICE_USER_DPRS_DURING_YEAR_EMPLOYING_STAFF, FloatType(), True),
+                StructField(DP.CARER_DPRS_DURING_YEAR, FloatType(), True),
+                StructField(DP.TOTAL_DPRS_DURING_YEAR, FloatType(), True),
+            ]
+        )
+        df = self.spark.createDataFrame(rows, schema=test_schema)
+        output_df = job.calculate_total_personal_assistant_filled_posts(df)
+
+        self.assertEqual(output_df.count(), df.count())
+
+    def test_calculate_proportion_of_dpr_employing_staff_returns_correct_values(self):
+        rows = [
+            ("area_1", 2021, 0.49, 400.0, 30.0, 800.0),
+            ("area_2", 2021, 0.34, 390.0, 32.0, 850.0),
+            ("area_1", 2020, 0.45, 380.0, 34.0, 1100.0),
+            ("area_2", 2020, 0.79, 370.0, 36.0, 670.0),
+            ("area_1", 2019, 0.27, 360.0, 38.0, 530.0),
+            ("area_2", 2019, 0.45, 350.0, 40.0, 640.0),
+            ("area_1", 2018, 0.37, 340.0, 42.0, 1000.0),
+            ("area_2", 2018, 0.63, 330.0, 44.0, 800.0),
+            ("area_1", 2017, 0.33, 320.0, 46.0, 900.0),
+            ("area_2", 2017, 0.34, 310.0, 50.0, 1000.0),
+        ]
+        test_schema = StructType(
+            [
+                StructField(DP.LA_AREA, StringType(), False),
+                StructField(DP.YEAR_AS_INTEGER, IntegerType(), True),
+                StructField(
+                    DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
+                    FloatType(),
+                    True,
+                ),
+                StructField(DP.ESTIMATED_SERVICE_USER_DPRS_DURING_YEAR_EMPLOYING_STAFF, FloatType(), True),
+                StructField(DP.CARER_DPRS_DURING_YEAR, FloatType(), True),
+                StructField(DP.TOTAL_DPRS_DURING_YEAR, FloatType(), True),
+            ]
+        )
+        df = self.spark.createDataFrame(rows, schema=test_schema)
+        output_df = job.calculate_proportion_of_dpr_employing_staff(df)
+
+        self.assertEqual(output_df.count(), df.count())
