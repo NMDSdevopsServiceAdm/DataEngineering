c99cfe01875eb6e47af131f459c548f927680af7
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-22 16:33:25 +0100

    add notes to each function

--- utils/direct_payments_utils/estimate_direct_payments/calculate_remaining_variables.py
+++ utils/direct_payments_utils/estimate_direct_payments/calculate_remaining_variables.py
@@ -4,14 +4,10 @@ from pyspark.sql import DataFrame
 def calculate_remaining_variables(
     direct_payments_df: DataFrame,
 ) -> DataFrame:
-    direct_payments_df = calculate_service_users_with_self_employed_staff(
-        direct_payments_df
-    )
+    direct_payments_df = calculate_service_users_with_self_employed_staff(direct_payments_df)
     direct_payments_df = calculate_carers_employing_staff(direct_payments_df)
     direct_payments_df = calculate_total_dpr_employing_staff(direct_payments_df)
-    direct_payments_df = calculate_total_personal_assistant_filled_posts(
-        direct_payments_df
-    )
+    direct_payments_df = calculate_total_personal_assistant_filled_posts(direct_payments_df)
     direct_payments_df = calculate_proportion_of_dpr_employing_staff(direct_payments_df)
     return direct_payments_df
 
@@ -20,7 +16,7 @@ def calculate_service_users_with_self_employed_staff(
     direct_payments_df: DataFrame,
 ) -> DataFrame:
     # TODO
-
+    # su dprs * self employed %
     return direct_payments_df
 
 
@@ -28,7 +24,7 @@ def calculate_carers_employing_staff(
     direct_payments_df: DataFrame,
 ) -> DataFrame:
     # TODO
-
+    # carer dprs * carer %
     return direct_payments_df
 
 
@@ -36,7 +32,7 @@ def calculate_total_dpr_employing_staff(
     direct_payments_df: DataFrame,
 ) -> DataFrame:
     # TODO
-
+    # su employing staff + su with self employed staff + carers employing staff
     return direct_payments_df
 
 
@@ -44,7 +40,7 @@ def calculate_total_personal_assistant_filled_posts(
     direct_payments_df: DataFrame,
 ) -> DataFrame:
     # TODO
-
+    # total dpr employing staff * pa ratio
     return direct_payments_df
 
 
@@ -52,5 +48,5 @@ def calculate_proportion_of_dpr_employing_staff(
     direct_payments_df: DataFrame,
 ) -> DataFrame:
     # TODO
-
+    # total dpr employing staff/ total dpr
     return direct_payments_df
