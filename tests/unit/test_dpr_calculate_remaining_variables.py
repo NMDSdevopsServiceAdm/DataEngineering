4e7a969fc282394989eb1e14ec548ee5e4756d71
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-22 15:54:49 +0100

    Write test to check calculate remaining variables runs

---
+++ tests/unit/test_dpr_calculate_remaining_variables.py
@@ -0,0 +1,55 @@
+import unittest
+import warnings
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    IntegerType,
+    FloatType,
+)
+
+import utils.direct_payments_utils.estimate_direct_payments.calculate_remaining_variables as job
+from utils.direct_payments_utils.direct_payments_column_names import (
+    DirectPaymentColumnNames as DP,
+)
+
+
+class TestCalculateRemainingVariables(unittest.TestCase):
+    def setUp(self):
+        self.spark = SparkSession.builder.appName("test_calculate_remaining_variables").getOrCreate()
+
+        warnings.simplefilter("ignore", ResourceWarning)
+
+    def test_calculate_remaining_variables_completes(self):
+        rows = [
+            ("area_1", 2021, 0.49, 400.0, 30.0, 800.0),
+            ("area_2", 2021, 0.34, 390.0, 32.0, 850.0),
+            ("area_1", 2020, 0.45, 380.0, 34.0, 1100.0),
+            ("area_2", 2020, 0.79, 370.0, 36.0, 670.0),
+            ("area_1", 2019, 0.27, 360.0, 38.0, 530.0),
+            ("area_2", 2019, 0.45, 350.0, 40.0, 640.0),
+            ("area_1", 2018, 0.37, 340.0, 42.0, 1000.0),
+            ("area_2", 2018, 0.63, 330.0, 44.0, 800.0),
+            ("area_1", 2017, 0.33, 320.0, 46.0, 900.0),
+            ("area_2", 2017, 0.34, 310.0, 50.0, 1000.0),
+        ]
+        test_schema = StructType(
+            [
+                StructField(DP.LA_AREA, StringType(), False),
+                StructField(DP.YEAR_AS_INTEGER, IntegerType(), True),
+                StructField(
+                    DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
+                    FloatType(),
+                    True,
+                ),
+                StructField(DP.ESTIMATED_SERVICE_USER_DPRS_DURING_YEAR_EMPLOYING_STAFF, FloatType(), True),
+                StructField(DP.CARER_DPRS_DURING_YEAR, FloatType(), True),
+                StructField(DP.TOTAL_DPRS_DURING_YEAR, FloatType(), True),
+            ]
+        )
+        df = self.spark.createDataFrame(rows, schema=test_schema)
+        output_df = job.calculate_remaining_variables(df)
+
+        self.assertEqual(output_df.count(), df.count())
