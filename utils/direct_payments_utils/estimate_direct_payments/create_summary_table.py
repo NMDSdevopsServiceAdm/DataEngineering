ccded94de3a7e1b8719886a90c1a8619fdb3b2c6
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-23 13:37:56 +0100

    Group by partition key

--- utils/direct_payments_utils/estimate_direct_payments/create_summary_table.py
+++ utils/direct_payments_utils/estimate_direct_payments/create_summary_table.py
@@ -10,7 +10,7 @@ def create_summary_table(
     direct_payments_df: DataFrame,
 ) -> DataFrame:
     # TODO
-    summary_direct_payments_df = direct_payments_df.groupBy(DP.YEAR_AS_INTEGER).agg(
+    summary_direct_payments_df = direct_payments_df.groupBy(DP.YEAR).agg(
         # total direct payment recipients during year
         F.sum(DP.TOTAL_DPRS_DURING_YEAR).cast("float").alias("total_dprs"),
         # estimated % of dprs that are service users
@@ -18,9 +18,7 @@ def create_summary_table(
         .cast("float")
         .alias("proportion_of_service_user_dprs"),
         # service user dprs during year
-        F.sum(DP.SERVICE_USER_DPRS_DURING_YEAR)
-        .cast("float")
-        .alias("service_user_dprs"),
+        F.sum(DP.SERVICE_USER_DPRS_DURING_YEAR).cast("float").alias("service_user_dprs"),
         # change in service users [to calculate]
         # % of change [to calculate]
         # estimated % of services users employing staff
@@ -36,13 +34,9 @@ def create_summary_table(
         .cast("float")
         .alias("estimated_service_users_with_self_employed_staff"),
         # estimated number of carers employing staff
-        F.sum(DP.ESTIMATED_CARERS_EMPLOYING_STAFF)
-        .cast("float")
-        .alias("estimated_carers_employing_staff"),
+        F.sum(DP.ESTIMATED_CARERS_EMPLOYING_STAFF).cast("float").alias("estimated_carers_employing_staff"),
         # estimated total dprs employing staff
-        F.sum(DP.ESTIMATED_TOTAL_DPR_EMPLOYING_STAFF)
-        .cast("float")
-        .alias("estimated_total_dprs_employing_staff"),
+        F.sum(DP.ESTIMATED_TOTAL_DPR_EMPLOYING_STAFF).cast("float").alias("estimated_total_dprs_employing_staff"),
         # estimated PA filled posts
         F.sum(DP.ESTIMATED_TOTAL_PERSONAL_ASSISTANT_FILLED_POSTS)
         .cast("float")
