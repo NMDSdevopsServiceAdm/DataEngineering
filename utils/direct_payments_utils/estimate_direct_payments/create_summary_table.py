80950bdb47d938a8ef3025fa292cb012a1e12415
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-06-23 11:41:13 +0100

    Add notes for summary table

--- utils/direct_payments_utils/estimate_direct_payments/create_summary_table.py
+++ utils/direct_payments_utils/estimate_direct_payments/create_summary_table.py
@@ -5,5 +5,17 @@ def create_summary_table(
     direct_payments_df: DataFrame,
 ) -> DataFrame:
     # TODO
-
+    # Data required:
+    # total direct payment recipients during year
+    # estimated % of dprs that are service users [to calculate]
+    # service user dprs during year
+    # change in service users [to calculate]
+    # % of change [to calculate]
+    # estimated % of services users employing staff
+    # estimated number of service users employing staff
+    # estimated number of service users with self employed pas
+    # estimated number of carers employing staff
+    # estimated total dprs employing staff
+    # estimated PA filled posts
+    # estimated % of total dprs employing staff
     return direct_payments_df
