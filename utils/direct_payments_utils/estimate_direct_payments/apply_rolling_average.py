0405e8872b61be0991bc13ac26746ab51210336b
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-07-03 13:38:29 +0100

    Update function to create rolling average column

--- utils/direct_payments_utils/estimate_direct_payments/apply_rolling_average.py
+++ utils/direct_payments_utils/estimate_direct_payments/apply_rolling_average.py
@@ -7,66 +7,55 @@ from utils.direct_payments_utils.direct_payments_column_names import (
 
 
 def apply_rolling_average(direct_payments_df: DataFrame) -> DataFrame:
-    # TODO
-    return direct_payments_df
-
-
-"""
-def calculate_rolling_average(
-    direct_payments_df: DataFrame,
-) -> DataFrame:
-    populated_df = filter_to_locations_with_known_service_users_employing_staff(direct_payments_df)
-    service_users_employing_staff_sum_and_count_df = calculate_aggregates_per_year(populated_df)
-    rolling_average_df = create_rolling_average_column(service_users_employing_staff_sum_and_count_df)
+    aggregated_df = calculate_aggregates_per_year(direct_payments_df)
+    rolling_average_df = create_rolling_average_column(aggregated_df)
     direct_payments_df = join_rolling_average_into_df(direct_payments_df, rolling_average_df)
     return direct_payments_df
-"""
 
 
-"""
 def calculate_aggregates_per_year(
     direct_payments_df: DataFrame,
 ) -> DataFrame:
     direct_payments_df = direct_payments_df.groupBy(DP.YEAR_AS_INTEGER).agg(
-        F.count(DP.ESTIMATED_SERVICE_USER_DPRS_DURING_YEAR_EMPLOYING_STAFF)
+        F.count(DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF)
         .cast("integer")
-        .alias(DP.COUNT_OF_SERVICE_USER_DPRS_DURING_YEAR),
-        F.sum(DP.ESTIMATED_SERVICE_USER_DPRS_DURING_YEAR_EMPLOYING_STAFF).alias(
-            DP.SUM_OF_SERVICE_USER_DPRS_DURING_YEAR
+        .alias(DP.COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF),
+        F.sum(DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF).alias(
+            DP.SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF
         ),
     )
     return direct_payments_df
-"""
-"""
+
+
 def create_rolling_average_column(
     direct_payments_df: DataFrame,
 ) -> DataFrame:
     direct_payments_df = calculate_rolling_sum(
         direct_payments_df,
-        DP.COUNT_OF_SERVICE_USER_DPRS_DURING_YEAR,
-        DP.ROLLING_TOTAL_COUNT_OF_SERVICE_USER_DPRS_DURING_YEAR,
+        DP.COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
+        DP.ROLLING_TOTAL_OF_COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
     )
     direct_payments_df = calculate_rolling_sum(
         direct_payments_df,
-        DP.SUM_OF_SERVICE_USER_DPRS_DURING_YEAR,
-        DP.ROLLING_TOTAL_SUM_OF_SERVICE_USER_DPRS_DURING_YEAR,
+        DP.SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
+        DP.ROLLING_TOTAL_OF_SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
     )
     direct_payments_df = direct_payments_df.withColumn(
-        DP.ROLLING_AVERAGE,
-        F.col(DP.ROLLING_TOTAL_SUM_OF_SERVICE_USER_DPRS_DURING_YEAR)
-        / F.col(DP.ROLLING_TOTAL_COUNT_OF_SERVICE_USER_DPRS_DURING_YEAR),
+        DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
+        F.col(DP.ROLLING_TOTAL_OF_SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF)
+        / F.col(DP.ROLLING_TOTAL_OF_COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF),
     )
     return direct_payments_df
-"""
-"""
+
+
 def calculate_rolling_sum(df: DataFrame, col_to_sum: str, new_col_name: str) -> DataFrame:
     df = df.withColumn(
         new_col_name,
         F.sum(col_to_sum).over(define_window_specifications(DP.YEAR_AS_INTEGER)),
     )
     return df
-"""
-"""
+
+
 def define_window_specifications(year_column: str) -> Window:
     rolling_window = (
         Window.partitionBy(F.lit(0))
@@ -74,8 +63,8 @@ def define_window_specifications(year_column: str) -> Window:
         .rangeBetween(-(Config.NUMBER_OF_YEARS_ROLLING_AVERAGE), 0)
     )
     return rolling_window
-"""
-"""
+
+
 def join_rolling_average_into_df(
     direct_payments_df: DataFrame,
     rolling_average_df: DataFrame,
@@ -83,4 +72,3 @@ def join_rolling_average_into_df(
     rolling_average_df = rolling_average_df.select(DP.YEAR_AS_INTEGER, DP.ROLLING_AVERAGE)
     direct_payments_df = direct_payments_df.join(rolling_average_df, [DP.YEAR_AS_INTEGER], "left")
     return direct_payments_df
-"""
