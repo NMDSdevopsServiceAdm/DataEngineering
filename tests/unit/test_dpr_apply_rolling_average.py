966c0e681bf7f8f6772a4c7144780475404b4519
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-07-03 14:56:06 +0100

    Create test assertions

--- tests/unit/test_dpr_apply_rolling_average.py
+++ tests/unit/test_dpr_apply_rolling_average.py
@@ -76,6 +76,56 @@ class TestApplyRollingAverage(unittest.TestCase):
         )
         df = self.spark.createDataFrame(rows, schema=test_schema)
         output_df = job.calculate_aggregates_per_year(df)
+        output_df_list = output_df.sort(DP.LA_AREA, DP.YEAR_AS_INTEGER).collect()
+
+        self.assertEqual(output_df_list[0][DP.COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 1.0)
+        self.assertEqual(output_df_list[1][DP.COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 1.0)
+        self.assertEqual(output_df_list[2][DP.COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 1.0)
+        self.assertEqual(output_df_list[3][DP.COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 1.0)
+        self.assertEqual(output_df_list[4][DP.COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 1.0)
+        self.assertEqual(output_df_list[5][DP.COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 1.0)
+        self.assertEqual(output_df_list[6][DP.COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 1.0)
+        self.assertEqual(output_df_list[7][DP.COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 1.0)
+        self.assertEqual(output_df_list[8][DP.COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 1.0)
+        self.assertEqual(output_df_list[9][DP.COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 1.0)
+        self.assertEqual(output_df_list[10][DP.COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 1.0)
+        self.assertEqual(output_df_list[11][DP.COUNT_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 1.0)
+        self.assertAlmostEqual(
+            output_df_list[0][DP.SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.42, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[1][DP.SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.37, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[2][DP.SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.51, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[3][DP.SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.46, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[4][DP.SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.45, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[5][DP.SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.25, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[6][DP.SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.62, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[7][DP.SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.65, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[8][DP.SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.74, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[9][DP.SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.54, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[10][DP.SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.80, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[11][DP.SUM_OF_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.62, places=2
+        )
         self.assertEqual(df.count(), output_df.count())
 
     def test_create_rolling_average_column_returns_correct_values(self):
@@ -104,6 +154,44 @@ class TestApplyRollingAverage(unittest.TestCase):
         )
         df = self.spark.createDataFrame(rows, schema=test_schema)
         output_df = job.create_rolling_average_column(df)
+        output_df_list = output_df.sort(DP.LA_AREA, DP.YEAR_AS_INTEGER).collect()
+
+        self.assertAlmostEqual(
+            output_df_list[0][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.42, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[1][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.39, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[2][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.43, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[3][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.45, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[4][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.47, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[5][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.39, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[6][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.62, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[7][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.63, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[8][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.67, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[9][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.64, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[10][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.69, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[11][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.65, places=2
+        )
         self.assertEqual(df.count(), output_df.count())
 
     def test_join_rolling_average_into_df_returns_correct_columns(self):
@@ -157,6 +245,7 @@ class TestApplyRollingAverage(unittest.TestCase):
         rolling_avg_df = self.spark.createDataFrame(rolling_avg_rows, schema=rolling_avg_test_schema)
         df = self.spark.createDataFrame(df_rows, schema=df_test_schema)
         output_df = job.join_rolling_average_into_df(df, rolling_avg_df)
+        self.assertEqual(len(output_df.columns), 6)
         self.assertEqual(df.count(), output_df.count())
 
     def test_apply_rolling_average_returns_correct_values(self):
@@ -183,4 +272,42 @@ class TestApplyRollingAverage(unittest.TestCase):
         )
         df = self.spark.createDataFrame(rows, schema=test_schema)
         output_df = job.apply_rolling_average(df)
+        output_df_list = output_df.sort(DP.LA_AREA, DP.YEAR_AS_INTEGER).collect()
+
+        self.assertAlmostEqual(
+            output_df_list[0][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.42, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[1][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.39, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[2][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.43, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[3][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.45, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[4][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.47, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[5][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.39, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[6][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.62, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[7][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.63, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[8][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.67, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[9][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.64, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[10][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.69, places=2
+        )
+        self.assertAlmostEqual(
+            output_df_list[11][DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF], 0.65, places=2
+        )
         self.assertEqual(df.count(), output_df.count())
