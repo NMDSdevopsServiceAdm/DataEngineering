13e82b4e2a2975360e0177574e2c87d28c9fabe1
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-07-20 11:50:11 +0100

    Update file path function name to remove reference to "main"

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -341,7 +341,7 @@ class UtilsTests(unittest.TestCase):
     def test_generate_s3_datasets_dir_date_path(self):
 
         dec_first_21 = datetime(2021, 12, 1)
-        dir_path = utils.generate_s3_main_datasets_dir_date_path(
+        dir_path = utils.generate_s3_datasets_dir_date_path(
             "s3://sfc-main-datasets", "test_domain", "test_dateset", dec_first_21
         )
         self.assertEqual(
