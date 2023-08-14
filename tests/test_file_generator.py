13be741b4eab800eea333df53cb3d2ef347a3ab6
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2023-08-14 15:01:41 +0100

    moved test file into generator

--- tests/test_file_generator.py
+++ tests/test_file_generator.py
@@ -1401,3 +1401,37 @@ def generate_data_for_calculating_rolling_average_column():
     df = spark.createDataFrame(rows, schema=schema)
 
     return df
+
+
+def generate_data_for_extrapolation_model():
+    spark = utils.get_spark()
+
+    schema = StructType(
+        [
+            StructField("locationid", StringType(), False),
+            StructField("snapshot_date", StringType(), False),
+            StructField("unix_time", LongType(), False),
+            StructField("job_count", DoubleType(), True),
+            StructField("primary_service_type", StringType(), False),
+            StructField("estimate_job_count", DoubleType(), True),
+            StructField("estimate_job_count_source", StringType(), True),
+            StructField("rolling_average_model", DoubleType(), True),
+        ]
+    )
+    # fmt: off
+    rows = [
+        ("1-000000001", "2023-01-01", 1672531200, 15.0, "Care home with nursing", None, None, 15.0),
+        ("1-000000001", "2023-02-01", 1675209600, None, "Care home with nursing", None, None, 15.1),
+        ("1-000000001", "2023-03-01", 1677628800, 30.0, "Care home with nursing", 30.0, "already_populated", 15.2),
+        ("1-000000002", "2023-01-01", 1672531200, 4.0, "non-residential", None, None, 50.3),
+        ("1-000000002", "2023-02-01", 1675209600, None, "non-residential", None, None, 50.5),
+        ("1-000000002", "2023-03-01", 1677628800, None, "non-residential", 5.0, "already_populated", 50.7),
+        ("1-000000002", "2023-04-01", 1680303600, None, "non-residential", None, None, 50.1),
+        ("1-000000003", "2023-01-01", 1672531200, None, "non-residential", None, None, 50.3),
+        ("1-000000003", "2023-02-01", 1675209600, 20.0, "non-residential", None, None, 50.5),
+        ("1-000000003", "2023-03-01", 1677628800, None, "non-residential", 30.0, "already_populated", 50.7),
+    ]
+    # fmt: on
+    df = spark.createDataFrame(rows, schema=schema)
+
+    return df
--- tests/unit/test_extrapolation.py
+++ tests/unit/test_extrapolation.py
@@ -2,53 +2,21 @@ import unittest
 import warnings
 
 from pyspark.sql import SparkSession
-from pyspark.sql.types import (
-    StructType,
-    StructField,
-    StringType,
-    DoubleType,
-    LongType,
-)
 
 import utils.estimate_job_count.models.extrapolation as job
+from tests.test_file_generator import (
+    generate_data_for_extrapolation_model,
+)
 
 
-class TestModelNonResDefault(unittest.TestCase):
-    column_schema = StructType(
-        [
-            StructField("locationid", StringType(), False),
-            StructField("snapshot_date", StringType(), False),
-            StructField("unix_time", LongType(), False),
-            StructField("job_count", DoubleType(), True),
-            StructField("primary_service_type", StringType(), False),
-            StructField("estimate_job_count", DoubleType(), True),
-            StructField("estimate_job_count_source", StringType(), True),
-            StructField("rolling_average_model", DoubleType(), True),
-        ]
-    )
-    # fmt: off
-    rows = [
-        ("1-000000001", "2023-01-01", 1672531200, 15.0, "Care home with nursing", None, None, 15.0),
-        ("1-000000001", "2023-02-01", 1675209600, None, "Care home with nursing", None, None, 15.1),
-        ("1-000000001", "2023-03-01", 1677628800, 30.0, "Care home with nursing", 30.0, "already_populated", 15.2),
-        ("1-000000002", "2023-01-01", 1672531200, 4.0, "non-residential", None, None, 50.3),
-        ("1-000000002", "2023-02-01", 1675209600, None, "non-residential", None, None, 50.5),
-        ("1-000000002", "2023-03-01", 1677628800, None, "non-residential", 5.0, "already_populated", 50.7),
-        ("1-000000002", "2023-04-01", 1680303600, None, "non-residential", None, None, 50.1),
-        ("1-000000003", "2023-01-01", 1672531200, None, "non-residential", None, None, 50.3),
-        ("1-000000003", "2023-02-01", 1675209600, 20.0, "non-residential", None, None, 50.5),
-        ("1-000000003", "2023-03-01", 1677628800, None, "non-residential", 30.0, "already_populated", 50.7),
-    ]
-    # fmt: on
-
+class TestModelExtrapolation(unittest.TestCase):
     def setUp(self):
         self.spark = SparkSession.builder.appName("test_extrapolation").getOrCreate()
-        self.non_res_model_df = job.model_extrapolation(
-            self.spark.createDataFrame(self.rows, schema=self.column_schema)
-        )
+        self.extrapolation_df = generate_data_for_extrapolation_model()
 
         warnings.filterwarnings("ignore", category=ResourceWarning)
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-    def test_model_non_res_row_count_unchanged(self):
-        self.assertEqual(self.non_res_model_df.count(), len(self.rows))
+    def test_model_extrapolation_row_count_unchanged(self):
+        df = job.model_extrapolation(self.extrapolation_df)
+        self.assertEqual(df.count(), self.extrapolation_df.count())
