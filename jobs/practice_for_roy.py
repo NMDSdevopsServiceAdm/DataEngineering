eb12c5865d1748ac4a6ec4f390a642001713ce32
Roy Price <roy.price@skillsforcare.org.uk>
2023-11-03 13:50:20 +0000

    attempt_1

--- jobs/practice_for_roy.py
+++ jobs/practice_for_roy.py
@@ -17,9 +17,7 @@ def main(
     direct_payments_source,
     destination,
 ):
-    spark = SparkSession.builder.appName(
-        "sfc_data_engineering_prepare_direct_payments"
-    ).getOrCreate()
+    spark = SparkSession.builder.appName("sfc_data_engineering_prepare_direct_payments").getOrCreate()
 
     direct_payments_df: DataFrame = spark.read.parquet(direct_payments_source).select(
         DP.LA_AREA,
@@ -34,16 +32,27 @@ def main(
     )
     # TODO define a function that takes a dataframe and returns a dataframe with only the columns la_area, year, and imd_score
     direct_payments_df_three_columns = select_la_area_year_imd_score(direct_payments_df)
-    
+
     # TODO define a function that takes a dataframe and reutns a dataframe filtered to leeds data only
     direct_payments_df_leeds_only = filter_to_leeds_local_authority(direct_payments_df)
 
-      
     # TODO define a function that takes a dataframe and returns that dataframe with a new column that is the proportion on direct payment recipients employing staff according to ADASS data
     direct_payments_df_with_proportion_employing_staff = calculate_proportion_employing_staff(direct_payments_df)
 
-    
-    
+
+def select_la_area_year_imd_score(direct_payments_df):
+    df = df.select(F.col(DP.LA_AREA), F.col(DP.YEAR), F.col(DP.IMD_SCORE))
+    return df
+
+
+def filter_to_leeds_local_authority(direct_payments_df):
+    df = df.where(F.col(DP.LA_AREA) == "Leeds")
+    return df
+
+
+def calculate_proportion_employing_staff(direct_payments_df):
+    df = df.withColumn("DP_EMPLOYING_STAFF", F.col(DP.DPRS_EMPLOYING_STAFF_ADASS) / F.col(DP.DPRS_ADASS))
+    return df
 
 
 if __name__ == "__main__":
