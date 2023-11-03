0167072463301d39ad53a1fdf6b81af21a6c012a
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-11-03 17:06:00 +0000

    Remove aggrigate averages from summary table

--- utils/direct_payments_utils/estimate_direct_payments/create_summary_table.py
+++ utils/direct_payments_utils/estimate_direct_payments/create_summary_table.py
@@ -11,15 +11,9 @@ def create_summary_table(
 ) -> DataFrame:
     summary_direct_payments_df = direct_payments_df.groupBy(DP.YEAR).agg(
         F.sum(DP.TOTAL_DPRS_DURING_YEAR).cast("float").alias(DP.TOTAL_DPRS),
-        F.avg(DP.ESTIMATED_PROPORTION_OF_DPR_WHO_ARE_SERVICE_USERS)
-        .cast("float")
-        .alias(DP.PROPORTION_OF_SERVICE_USER_DPRS),
         F.sum(DP.SERVICE_USER_DPRS_DURING_YEAR)
         .cast("float")
         .alias(DP.SERVICE_USER_DPRS),
-        F.avg(DP.ROLLING_AVERAGE_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF)
-        .cast("float")
-        .alias(DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_FINAL),
         F.sum(DP.ESTIMATED_SERVICE_USER_DPRS_DURING_YEAR_EMPLOYING_STAFF)
         .cast("float")
         .alias(DP.SERVICE_USERS_EMPLOYING_STAFF),
@@ -35,8 +29,5 @@ def create_summary_table(
         F.sum(DP.ESTIMATED_TOTAL_PERSONAL_ASSISTANT_FILLED_POSTS)
         .cast("float")
         .alias(DP.TOTAL_PERSONAL_ASSISTANT_FILLED_POSTS),
-        F.avg(DP.ESTIMATED_PROPORTION_OF_TOTAL_DPR_EMPLOYING_STAFF)
-        .cast("float")
-        .alias(DP.PROPORTION_OF_TOTAL_DPRS_EMPLOYING_STAFF),
     )
     return summary_direct_payments_df
