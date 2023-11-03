06373d6fd65a20d3e9a49a73fd36ab0de8d10f59
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-11-03 17:26:41 +0000

    Remove averaged aggregates from summary table

--- tests/unit/test_dpr_create_summary_table.py
+++ tests/unit/test_dpr_create_summary_table.py
@@ -151,15 +151,7 @@ class TestCreateSummaryTable(unittest.TestCase):
         output_df = job.create_summary_table(df)
         output_df_list = output_df.collect()
         self.assertAlmostEqual(output_df_list[0][DP.TOTAL_DPRS], 2500.0, places=5)
-        self.assertAlmostEqual(
-            output_df_list[0][DP.PROPORTION_OF_SERVICE_USER_DPRS], 0.6, places=5
-        )
         self.assertAlmostEqual(output_df_list[0][DP.SERVICE_USER_DPRS], 2406, places=5)
-        self.assertAlmostEqual(
-            output_df_list[0][DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_FINAL],
-            0.39,
-            places=5,
-        )
         self.assertAlmostEqual(
             output_df_list[0][DP.SERVICE_USERS_EMPLOYING_STAFF],
             1180.0,
@@ -179,10 +171,5 @@ class TestCreateSummaryTable(unittest.TestCase):
         self.assertAlmostEqual(
             output_df_list[0][DP.TOTAL_PERSONAL_ASSISTANT_FILLED_POSTS],
             2600,
-            places=5,
-        )
-        self.assertAlmostEqual(
-            output_df_list[0][DP.PROPORTION_OF_TOTAL_DPRS_EMPLOYING_STAFF],
-            0.5,
             places=5,
         )
