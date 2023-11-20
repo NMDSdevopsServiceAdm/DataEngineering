fcf54286da0240ed19fc1b6ae116efb27e3863c8
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-11-20 16:08:14 +0000

    Update code for capacity tracker data

--- jobs/ingest_capacity_tracker_data.py
+++ jobs/ingest_capacity_tracker_data.py
@@ -4,38 +4,58 @@ import pyspark.sql.functions as F
 
 from schemas.capacity_tracker_schema import (
     CAPACITY_TRACKER_CARE_HOMES, 
-    CAPACITY_TRACKER_NON_RESIDENTIAL,
+    CAPACITY_TRACKER_NON_RESIDENTIAL
     )
 from utils import utils
 
 
 def main(care_home_source, care_home_destination, non_res_source, non_res_destination):
-    df = utils.read_csv_with_defined_schema(source, SPSS_JOBS_ESTIMATES)
-    df_with_formatted_date = df.withColumn(
-        "snapshot_date_formatted", F.col("Snapshot_date")
+
+    care_home_df = utils.read_csv_with_defined_schema(care_home_source, CAPACITY_TRACKER_CARE_HOMES)
+    care_home_df = add_column_with_formatted_dates(care_home_df, "Last_Updated_UTC", "Last_Updated_UTC_formatted")
+    care_home_df = add_column_with_formatted_dates(care_home_df, "Last_Updated_BST", "Last_Updated_BST_formatted")
+    utils.write_to_parquet(care_home_df, care_home_destination, False)
+
+    non_res_df = utils.read_csv_with_defined_schema(non_res_source, CAPACITY_TRACKER_NON_RESIDENTIAL)
+    non_res_df = add_column_with_formatted_dates(non_res_df, "CQC_Survey_Last_Updated_UTC", "CQC_Survey_Last_Updated_UTC_formatted")
+    non_res_df = add_column_with_formatted_dates(non_res_df, "CQC_Survey_Last_Updated_BST", "CQC_Survey_Last_Updated_BST_formatted")
+    utils.write_to_parquet(non_res_df, non_res_destination, False)
+
+
+def add_column_with_formatted_dates(df, old_column, new_column):
+    df = df.withColumn(
+        new_column, F.col(old_column)
     )
     df_with_formatted_date = utils.format_date_fields(
-        df_with_formatted_date,
+        df,
         raw_date_format="yyyy/MM/dd",
-        date_column_identifier="snapshot_date_formatted",
+        date_column_identifier=new_column,
     )
-    utils.write_to_parquet(df_with_formatted_date, destination, False)
+    return df_with_formatted_date
 
 
 def collect_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--source", help="A CSV file used as source input", required=True
+        "--care_home_source", help="A CSV file with capacity tracker data for care homes", required=True
     )
     parser.add_argument(
-        "--destination",
+        "--care_home_destination",
+        help="A destination directory for outputting parquet files",
+        required=True,
+    )
+    parser.add_argument(
+        "--non_res_source", help="A CSV file with capacity tracker data for non residential", required=True
+    )
+    parser.add_argument(
+        "--non_res_destination",
         help="A destination directory for outputting parquet files",
         required=True,
     )
 
     args, _ = parser.parse_known_args()
 
-    return args.source, args.destination
+    return args.care_home_source, args.care_home_destination, args.non_res_source, args.non_res_destination
 
 
 if __name__ == "__main__":
