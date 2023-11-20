22e0747a0f0378360b4bcc2749dd5dfedf512377
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-11-20 16:10:57 +0000

    Formatting

--- jobs/ingest_capacity_tracker_data.py
+++ jobs/ingest_capacity_tracker_data.py
@@ -3,29 +3,42 @@ import argparse
 import pyspark.sql.functions as F
 
 from schemas.capacity_tracker_schema import (
-    CAPACITY_TRACKER_CARE_HOMES, 
-    CAPACITY_TRACKER_NON_RESIDENTIAL
-    )
+    CAPACITY_TRACKER_CARE_HOMES,
+    CAPACITY_TRACKER_NON_RESIDENTIAL,
+)
 from utils import utils
 
 
 def main(care_home_source, care_home_destination, non_res_source, non_res_destination):
-
-    care_home_df = utils.read_csv_with_defined_schema(care_home_source, CAPACITY_TRACKER_CARE_HOMES)
-    care_home_df = add_column_with_formatted_dates(care_home_df, "Last_Updated_UTC", "Last_Updated_UTC_formatted")
-    care_home_df = add_column_with_formatted_dates(care_home_df, "Last_Updated_BST", "Last_Updated_BST_formatted")
+    care_home_df = utils.read_csv_with_defined_schema(
+        care_home_source, CAPACITY_TRACKER_CARE_HOMES
+    )
+    care_home_df = add_column_with_formatted_dates(
+        care_home_df, "Last_Updated_UTC", "Last_Updated_UTC_formatted"
+    )
+    care_home_df = add_column_with_formatted_dates(
+        care_home_df, "Last_Updated_BST", "Last_Updated_BST_formatted"
+    )
     utils.write_to_parquet(care_home_df, care_home_destination, False)
 
-    non_res_df = utils.read_csv_with_defined_schema(non_res_source, CAPACITY_TRACKER_NON_RESIDENTIAL)
-    non_res_df = add_column_with_formatted_dates(non_res_df, "CQC_Survey_Last_Updated_UTC", "CQC_Survey_Last_Updated_UTC_formatted")
-    non_res_df = add_column_with_formatted_dates(non_res_df, "CQC_Survey_Last_Updated_BST", "CQC_Survey_Last_Updated_BST_formatted")
+    non_res_df = utils.read_csv_with_defined_schema(
+        non_res_source, CAPACITY_TRACKER_NON_RESIDENTIAL
+    )
+    non_res_df = add_column_with_formatted_dates(
+        non_res_df,
+        "CQC_Survey_Last_Updated_UTC",
+        "CQC_Survey_Last_Updated_UTC_formatted",
+    )
+    non_res_df = add_column_with_formatted_dates(
+        non_res_df,
+        "CQC_Survey_Last_Updated_BST",
+        "CQC_Survey_Last_Updated_BST_formatted",
+    )
     utils.write_to_parquet(non_res_df, non_res_destination, False)
 
 
 def add_column_with_formatted_dates(df, old_column, new_column):
-    df = df.withColumn(
-        new_column, F.col(old_column)
-    )
+    df = df.withColumn(new_column, F.col(old_column))
     df_with_formatted_date = utils.format_date_fields(
         df,
         raw_date_format="yyyy/MM/dd",
@@ -37,7 +50,9 @@ def add_column_with_formatted_dates(df, old_column, new_column):
 def collect_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--care_home_source", help="A CSV file with capacity tracker data for care homes", required=True
+        "--care_home_source",
+        help="A CSV file with capacity tracker data for care homes",
+        required=True,
     )
     parser.add_argument(
         "--care_home_destination",
@@ -45,7 +60,9 @@ def collect_arguments():
         required=True,
     )
     parser.add_argument(
-        "--non_res_source", help="A CSV file with capacity tracker data for non residential", required=True
+        "--non_res_source",
+        help="A CSV file with capacity tracker data for non residential",
+        required=True,
     )
     parser.add_argument(
         "--non_res_destination",
@@ -55,14 +72,24 @@ def collect_arguments():
 
     args, _ = parser.parse_known_args()
 
-    return args.care_home_source, args.care_home_destination, args.non_res_source, args.non_res_destination
+    return (
+        args.care_home_source,
+        args.care_home_destination,
+        args.non_res_source,
+        args.non_res_destination,
+    )
 
 
 if __name__ == "__main__":
     print("Spark job 'ingest_capacity_tracker_data' starting...")
     print(f"Job parameters: {sys.argv}")
 
-    care_home_source, care_home_destination, non_res_source, non_res_destination = collect_arguments()
+    (
+        care_home_source,
+        care_home_destination,
+        non_res_source,
+        non_res_destination,
+    ) = collect_arguments()
     main(care_home_source, care_home_destination, non_res_source, non_res_destination)
 
     print("Spark job 'ingest_capacity_tracker_data' done")
