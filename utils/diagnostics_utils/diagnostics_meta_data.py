b1a6ff92a8d2ab57751c33d180254012996c9011
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2023-12-18 09:30:43 +0000

    Refactor metadata

--- utils/diagnostics_utils/diagnostics_meta_data.py
+++ utils/diagnostics_utils/diagnostics_meta_data.py
@@ -13,7 +13,6 @@ from utils.estimate_job_count.column_names import (
 )
 
 
-
 care_home_with_nursing: str = "Care home with nursing"
 care_home_without_nursing: str = "Care home without nursing"
 non_residential: str = "non-residential"
@@ -29,6 +28,22 @@ average_prefix: str = "avg_"
 residuals_prefix: str = "residuals_"
 
 care_worker_to_all_jobs_ratio: float = 1.3
+
+
+CQC_ID: str = "CQC_ID"
+NURSES_EMPLOYED: str = "Nurses_Employed"
+CARE_WORKERS_EMPLOYED: str = "Care_Workers_Employed"
+NON_CARE_WORKERS_EMPLOYED: str = "Non_Care_Workers_Employed"
+AGENCY_NURSES_EMPLOYED: str = "Agency_Nurses_Employed"
+AGENCY_CARE_WORKERS_EMPLOYED: str = "Agency_Care_Workers_Employed"
+AGENCY_NON_CARE_WORKERS_EMPLOYED: str = "Agency_Non_Care_Workers_Employed"
+CQC_CARE_WORKERS_EMPLOYED: str = "CQC_Care_Workers_Employed"
+CARE_HOME_EMPLOYED: str = "care_home_employed"
+NON_RESIDENTIAL_EMPLOYED: str = "non_residential_employed"
+DESCRIPTION_OF_CHANGES: str = "description_of_changes"
+VALUE: str = "value"
+ID: str = "id"
+RUN_TIMESTAMP: str = "run_timestamp"
 
 
 @dataclass
@@ -54,19 +69,3 @@ class ResidualsRequired:
         NON_RESIDENTIAL_EMPLOYED,
         PEOPLE_DIRECTLY_EMPLOYED,
     ]
-
-
-CQC_ID: str = "CQC_ID"
-NURSES_EMPLOYED: str = "Nurses_Employed"
-CARE_WORKERS_EMPLOYED: str = "Care_Workers_Employed"
-NON_CARE_WORKERS_EMPLOYED: str = "Non_Care_Workers_Employed"
-AGENCY_NURSES_EMPLOYED: str = "Agency_Nurses_Employed"
-AGENCY_CARE_WORKERS_EMPLOYED: str = "Agency_Care_Workers_Employed"
-AGENCY_NON_CARE_WORKERS_EMPLOYED: str = "Agency_Non_Care_Workers_Employed"
-CQC_CARE_WORKERS_EMPLOYED: str = "CQC_Care_Workers_Employed"
-CARE_HOME_EMPLOYED: str = "care_home_employed"
-NON_RESIDENTIAL_EMPLOYED: str = "non_residential_employed"
-DESCRIPTION_OF_CHANGES: str = "description_of_changes"
-VALUE: str = "value"
-ID: str = "id"
-RUN_TIMESTAMP: str = "run_timestamp"
