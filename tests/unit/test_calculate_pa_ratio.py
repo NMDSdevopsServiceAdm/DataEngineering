a208b7bed7509b3ca3245dc0ac3c455a8dd5fdde
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-04 16:38:18 +0000

    Write test to estimate missing ratios

--- tests/unit/test_calculate_pa_ratio.py
+++ tests/unit/test_calculate_pa_ratio.py
@@ -115,3 +115,66 @@ class TestCalculatePARatio(unittest.TestCase):
             len(output_rows), len(expected_rows)
         )
     
+    def test_estimate_ratios(self):
+        rows = [
+            (2021, 1.8),
+            (2022, 1.8),
+            (2020, 1.8),
+            (2012, 1.5),
+            (2019, 1.8),
+            (2013, 1.5),
+        ]
+        test_schema = StructType(
+            [
+                StructField(DP.YEAR_AS_INTEGER, IntegerType(), True),
+                StructField(
+                    DP.TOTAL_STAFF_RECODED,
+                    FloatType(),
+                    True,
+                ),
+            ]
+        )
+        df = self.spark.createDataFrame(rows, schema=test_schema)
+        output_df = job.calculate_average_ratios(df)
+        output_rows = output_df.sort(DP.YEAR_AS_INTEGER).collect()
+        expected_rows = [1.5, 1.5, 1.5, 1.55, 1.6, 1.65, 1.7, 1.75, 1.8, 1.8, 1.8, 1.8]
+
+        self.assertEqual(
+            output_rows[0][DP.AVERAGE_STAFF], expected_rows[0]
+        )
+        self.assertEqual(
+            output_rows[1][DP.AVERAGE_STAFF], expected_rows[1]
+        )
+        self.assertEqual(
+            output_rows[2][DP.AVERAGE_STAFF], expected_rows[2]
+        )
+        self.assertEqual(
+            output_rows[3][DP.AVERAGE_STAFF], expected_rows[3]
+        )
+        self.assertEqual(
+            output_rows[4][DP.AVERAGE_STAFF], expected_rows[4]
+        )
+        self.assertEqual(
+            output_rows[5][DP.AVERAGE_STAFF], expected_rows[5]
+        )
+        self.assertEqual(
+            output_rows[6][DP.AVERAGE_STAFF], expected_rows[6]
+        )
+        self.assertEqual(
+            output_rows[7][DP.AVERAGE_STAFF], expected_rows[7]
+        )
+        self.assertEqual(
+            output_rows[8][DP.AVERAGE_STAFF], expected_rows[8]
+        )
+        self.assertEqual(
+            output_rows[9][DP.AVERAGE_STAFF], expected_rows[9]
+        )
+        self.assertEqual(
+            output_rows[10][DP.AVERAGE_STAFF], expected_rows[10]
+        )
+        self.assertEqual(
+            output_rows[11][DP.AVERAGE_STAFF], expected_rows[11]
+        )
+        self.assertEqual(
+            len(output_rows), len(expected_rows)
+        )
