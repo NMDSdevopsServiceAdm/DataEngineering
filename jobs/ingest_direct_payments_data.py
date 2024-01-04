2734a59a74f21f6d3f9c7a9ca25d31dd123d61cc
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-04 12:03:24 +0000

    Add args for survey data

--- jobs/ingest_direct_payments_data.py
+++ jobs/ingest_direct_payments_data.py
@@ -5,33 +5,41 @@ from schemas.direct_payment_data_schema import DIRECT_PAYMENTS_DATA
 from utils import utils
 
 
-def main(source, destination):
-    df = utils.read_csv_with_defined_schema(source, DIRECT_PAYMENTS_DATA)
+def main(external_data_source, survey_data_source, external_data_destination, survey_data_destination):
+    df = utils.read_csv_with_defined_schema(external_data_source, DIRECT_PAYMENTS_DATA)
 
-    utils.write_to_parquet(df, destination, False)
+    utils.write_to_parquet(df, external_data_destination, False)
 
 
 def collect_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--source", help="A CSV file used as source input", required=True
+        "--external_data_source", help="A CSV file used as source input for external data", required=True
     )
     parser.add_argument(
-        "--destination",
-        help="A destination directory for outputting parquet files",
+        "--survey_data_source", help="A CSV file used as source input for the IE/PA survey data", required=True
+    )
+    parser.add_argument(
+        "--external_data_destination",
+        help="A destination directory for outputting external data parquet files",
+        required=True,
+    )
+    parser.add_argument(
+        "--survey_data_destination",
+        help="A destination directory for outputting survey data parquet files",
         required=True,
     )
 
     args, _ = parser.parse_known_args()
 
-    return args.source, args.destination
+    return args.external_data_source, args.survey_data_source, args.external_data_destination, args.survey_data_destination
 
 
 if __name__ == "__main__":
     print("Spark job 'ingest_direct_payments_data' starting...")
     print(f"Job parameters: {sys.argv}")
 
-    source, destination = collect_arguments()
-    main(source, destination)
+    external_data_source, survey_data_source, external_data_destination, survey_data_destination = collect_arguments()
+    main(external_data_source, survey_data_source, external_data_destination, survey_data_destination)
 
     print("Spark job 'ingest_direct_payments_data' done")
