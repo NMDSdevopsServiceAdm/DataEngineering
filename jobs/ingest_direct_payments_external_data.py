ee5eda4eacc86229ac3aa2c3060ba14cddc8abc4
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-11 17:46:30 +0000

    Remove unneeded lines from external data ingest script

--- jobs/ingest_direct_payments_external_data.py
+++ jobs/ingest_direct_payments_external_data.py
@@ -1,23 +1,19 @@
 import sys
 import argparse
 
-from schemas.direct_payment_data_schema import EXTERNAL_DATA, SURVEY_DATA
+from schemas.direct_payment_data_schema import EXTERNAL_DATA
 from utils import utils
 
 
 def main(
     external_data_source,
-    survey_data_source,
     external_data_destination,
-    survey_data_destination,
 ):
     external_df = utils.read_csv_with_defined_schema(
         external_data_source, EXTERNAL_DATA
     )
-    survey_df = utils.read_csv_with_defined_schema(survey_data_source, SURVEY_DATA)
 
     utils.write_to_parquet(external_df, external_data_destination, False)
-    utils.write_to_parquet(survey_df, survey_data_destination, False)
 
 
 def collect_arguments():
@@ -28,18 +24,8 @@ def collect_arguments():
         required=True,
     )
     parser.add_argument(
-        "--survey_data_source",
-        help="A CSV file used as source input for the IE/PA survey data",
-        required=True,
-    )
-    parser.add_argument(
         "--external_data_destination",
         help="A destination directory for outputting external data parquet files",
-        required=True,
-    )
-    parser.add_argument(
-        "--survey_data_destination",
-        help="A destination directory for outputting survey data parquet files",
         required=True,
     )
 
@@ -47,9 +33,7 @@ def collect_arguments():
 
     return (
         args.external_data_source,
-        args.survey_data_source,
         args.external_data_destination,
-        args.survey_data_destination,
     )
 
 
@@ -59,15 +43,11 @@ if __name__ == "__main__":
 
     (
         external_data_source,
-        survey_data_source,
         external_data_destination,
-        survey_data_destination,
     ) = collect_arguments()
     main(
         external_data_source,
-        survey_data_source,
         external_data_destination,
-        survey_data_destination,
     )
 
     print("Spark job 'ingest_direct_payments_data' done")
