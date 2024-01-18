714ab5ffa60fc12fb8bf08915eb156070c4463cf
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-18 15:35:50 +0000

    Refactor names

--- schemas/cqc_provider_schema.py
+++ schemas/cqc_provider_schema.py
@@ -1,6 +1,42 @@
 from pyspark.sql.types import StructField, StructType, StringType, ArrayType, FloatType
 
+from utils.ind_cqc_column_names.cqc_provider_api_columns import (
+    CqcProviderApiColumns as ColNames,
+)
+
 PROVIDER_SCHEMA = StructType(
+    fields=[
+        StructField(ColNames.provider_id, StringType(), True),
+        StructField(
+            ColNames.location_ids,
+            ArrayType(
+                StringType(),
+            ),
+        ),
+        StructField(ColNames.organisation_type, StringType(), True),
+        StructField(ColNames.ownership_type, StringType(), True),
+        StructField(ColNames.type, StringType(), True),
+        StructField(ColNames.uprn, StringType(), True),
+        StructField(ColNames.name, StringType(), True),
+        StructField(ColNames.registration_status, StringType(), True),
+        StructField(ColNames.registration_date, StringType(), True),
+        StructField(ColNames.deregistration_date, StringType(), True),
+        StructField(ColNames.address_line_one, StringType(), True),
+        StructField(ColNames.town_or_city, StringType(), True),
+        StructField(ColNames.county, StringType(), True),
+        StructField(ColNames.region, StringType(), True),
+        StructField(ColNames.postcode, StringType(), True),
+        StructField(ColNames.latitude, FloatType(), True),
+        StructField(ColNames.longitude, FloatType(), True),
+        StructField(ColNames.phone_number, StringType(), True),
+        StructField(ColNames.companies_house_number, StringType(), True),
+        StructField(ColNames.inspection_directorate, StringType(), True),
+        StructField(ColNames.constituency, StringType(), True),
+        StructField(ColNames.local_authority, StringType(), True),
+    ]
+)
+
+PROVIDER_SCHEMA_OLD = StructType(
     fields=[
         StructField("providerId", StringType(), True),
         StructField(
---
+++ utils/ind_cqc_column_names/cqc_provider_api_columns.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+@dataclass
+class CqcProviderApiColumns:
+    provider_id:str = "providerId"
+    location_ids:str = "locationIds"
+    organisation_type: str = "organisationType"
+    ownership_type:str = "ownershipType"
+    type:str = "type"
+    uprn:str = "uprn"
+    name: str = "name"
+    registration_status:str = "registrationStatus"
+    registration_date:str = "registrationDate"
+    deregistration_date:str = "deregistrationDate"
+    address_line_one:str = "postalAddressLine1"
+    town_or_city:str = "postalAddressTownCity"
+    county:str = "postalAddressCounty"
+    region:str = "region"
+    postcode:str = "postalCode"
+    latitude:str = "onspdLatitude"
+    longitude:str = "onspdLongitude"
+    phone_number:str = "mainPhoneNumber"
+    companies_house_number:str = "companiesHouseNumber"
+    inspection_directorate:str = "inspectionDirectorate"
+    constituency:str = "constituency"
+    local_authority:str = "localAuthority"
