00d92a10dd31b889b7f4f7cd1833ec694a18cfdb
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-22 12:48:04 +0000

    Alphabetise column names

--- utils/ind_cqc_column_names/cqc_pir_columns.py
+++ utils/ind_cqc_column_names/cqc_pir_columns.py
@@ -3,20 +3,21 @@ from dataclasses import dataclass
 
 @dataclass
 class CqcPirColumns:
+    domiciliary_care: str = "Service_type_Domiciliary_care_service"
+    local_authority: str = "Location_local_authority"
     location_id: str = "Location_ID"
     location_name: str = "Location_name"
-    pir_type: str = "PIR_type"
-    pir_submission_date: str = "PIR_submission_date"
+    location_status: str = "Location_status"
+    number_of_beds: str = "Location_beds"
     people_directly_employed: str = "How_many_people_are_directly_employed_and_deliver_regulated_activities_at_your_service_as_part_of_their_daily_duties"
-    staff_leavers: str = "How_many_staff_have_left_your_service_in_the_past_12_months"
-    staff_vacancies: str = "How_many_staff_vacancies_do_you_have"
+    pir_submission_date: str = "PIR_submission_date"
+    pir_type: str = "PIR_type"
+    primary_inspection_category: str = "Location_primary_inspection_category"
+    region: str = "Location_region"
     shared_lives_leavers: str = (
         "How_many_Shared_Lives_workers_have_left_your_service_in_the_past_12_months"
     )
     shared_lives_vacancies: str = "How_many_Shared_Lives_worker_vacancies_do_you_have"
-    primary_inspection_category: str = "Location_primary_inspection_category"
-    region: str = "Location_region"
-    local_authority: str = "Location_local_authority"
-    number_of_beds: str = "Location_beds"
-    domiciliary_care: str = "Service_type_Domiciliary_care_service"
-    location_status: str = "Location_status"
+    staff_leavers: str = "How_many_staff_have_left_your_service_in_the_past_12_months"
+    staff_vacancies: str = "How_many_staff_vacancies_do_you_have"
+
--- utils/ind_cqc_column_names/feature_engineering_columns.py
+++ utils/ind_cqc_column_names/feature_engineering_columns.py
@@ -3,31 +3,32 @@ from dataclasses import dataclass
 
 @dataclass
 class CareHomeFeatureEngineeringColumns:
-    locationid: str = "locationid"
-    snapshot_date: str = "snapshot_date"
-    ons_region: str = "ons_region"
-    number_of_beds: str = "number_of_beds"
-    people_directly_employed: str = "people_directly_employed"
     carehome: str = "carehome"
     features: str = "features"
     job_count: str = "job_count"
+    locationid: str = "locationid"
+    number_of_beds: str = "number_of_beds"
+    ons_region: str = "ons_region"
     partition_0: str = "partition_0"
-    snapshot_year: str = "snapshot_year"
-    snapshot_month: str = "snapshot_month"
+    people_directly_employed: str = "people_directly_employed"
+    snapshot_date: str = "snapshot_date"
     snapshot_day: str = "snapshot_day"
+    snapshot_month: str = "snapshot_month"
+    snapshot_year: str = "snapshot_year"
+
 
 
 @dataclass
 class NonResidentialFeatureEngineeringColumns:
-    locationid: str = "locationid"
-    snapshot_date: str = "snapshot_date"
-    ons_region: str = "ons_region"
-    number_of_beds: str = "number_of_beds"
-    people_directly_employed: str = "people_directly_employed"
     carehome: str = "carehome"
     features: str = "features"
     job_count: str = "job_count"
+    locationid: str = "locationid"
+    number_of_beds: str = "number_of_beds"
+    ons_region: str = "ons_region"
     partition_0: str = "partition_0"
-    snapshot_year: str = "snapshot_year"
-    snapshot_month: str = "snapshot_month"
+    people_directly_employed: str = "people_directly_employed"
+    snapshot_date: str = "snapshot_date"
     snapshot_day: str = "snapshot_day"
+    snapshot_month: str = "snapshot_month"
+    snapshot_year: str = "snapshot_year"
