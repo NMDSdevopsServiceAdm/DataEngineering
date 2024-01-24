d8490c49b1fd213920048f6349ea1463333018cb
Luke Williams <lawilliams1993@gmail.com>
2024-01-24 18:21:30 +0000

    configure appropriate mocks and move first test to pass

--- tests/unit/test_clean_cqc_provider_data.py
+++ tests/unit/test_clean_cqc_provider_data.py
@@ -1,40 +1,54 @@
 import unittest
+from unittest.mock import Mock, patch
 import shutil
 import warnings
 
+import jobs.clean_cqc_provider_data as job
+
 from pyspark.sql import SparkSession
 
-from tests.test_file_generator import generate_cqc_providers_file
+from tests.test_file_generator import generate_cqc_providers_full_file
 
 
 class CleanCQCProviderDatasetTests(unittest.TestCase):
-    TEST_CQC_PROVIDERS_FILE = "tests/test_data/domain=cqc/dataset=providers"
+    TEST_FULL_CQC_PROVIDERS_FILE = (
+        "tests/test_data/domain=cqc/dataset=providers/format=parquet"
+    )
 
-    def setUp(self):
-        self.spark = SparkSession.builder.appName(
-            "sfc_data_engineering_test_clean_cqc_provider_data"
-        ).getOrCreate()
-        generate_cqc_providers_file(self.TEST_CQC_PROVIDERS_FILE)
-
-        warnings.simplefilter("ignore", ResourceWarning)
-
-    def tearDown(self):
-        try:
-            shutil.rmtree(self.TEST_CQC_PROVIDERS_FILE)
-        except OSError:
-            pass  # Ignore dir does not exist
+    def setUp(self) -> None:
+        self.test_cqc_providers_parquet = generate_cqc_providers_full_file(
+            self.TEST_FULL_CQC_PROVIDERS_FILE
+        )
+        self.spark_mock = Mock()
+        type(self.spark_mock).read = self.spark_mock
+        self.spark_mock.option.return_value = self.spark_mock
+        self.spark_mock.parquet.return_value = self.test_cqc_providers_parquet
 
     def test_get_cqc_provider_df(self):
-        print("todo")
+        df = job.get_cqc_provider_df(self.TEST_FULL_CQC_PROVIDERS_FILE, self.spark_mock)
+
+        self.spark_mock.read.option.assert_called_once_with(
+            "basePath", self.TEST_FULL_CQC_PROVIDERS_FILE
+        )
+        self.spark_mock.parquet.assert_called_once_with(
+            self.TEST_FULL_CQC_PROVIDERS_FILE
+        )
+
+        self.assertEqual(df.count(), self.test_cqc_providers_parquet.count())
+
+        # is it retrieving the file
+        # is the file empty or not?
+        # Are particular columns present?
+        # Does it return a dataframe?
 
     def test_clean_cqc_provider_df(self):
-        print("todo")
+        self.assertTrue(False)
 
     def test_write_cleaned_provider_df_to_parquet(self):
-        print("todo")
+        self.assertTrue(False)
 
     def test_main(self):
-        print("todo")
+        self.assertTrue(False)
 
 
 if __name__ == "__main__":
