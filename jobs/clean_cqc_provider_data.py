48dc3b5ac511d6f3dab13449603b883453fc10b1
Luke Williams <lawilliams1993@gmail.com>
2024-01-24 10:22:11 +0000

    extract spark context, black formatting

--- jobs/clean_cqc_provider_data.py
+++ jobs/clean_cqc_provider_data.py
@@ -6,20 +6,20 @@ import pyspark.sql.dataframe
 
 from utils import utils
 
-def get_cqc_provider_df(cqc_provider_source) -> pyspark.sql.DataFrame:
-    spark = utils.get_spark()
 
+def get_cqc_provider_df(cqc_provider_source, spark_session) -> pyspark.sql.DataFrame:
     print(f"Reading CQC providers parquet from {cqc_provider_source}")
-    cqc_provider_df = (
-        spark.read.option("basePath", cqc_provider_source) #used for partitioning
-        .parquet(cqc_provider_source)
+    cqc_provider_df = spark_session.read.option(
+        "basePath", cqc_provider_source
+    ).parquet(  # used for partitioning
+        cqc_provider_source
     )
 
     return cqc_provider_df
 
 
 def clean_cqc_provider_df(df_to_clean) -> pyspark.sql.DataFrame:
-    #Cleaning logic
+    # Cleaning logic
     return df_to_clean
 
 
@@ -28,21 +28,30 @@ def write_cleaned_provider_df_to_parquet(dataFrame, destination) -> None:
 
 
 def main(cqc_source: str, cleaned_cqc_destination: str):
-    cqc_provider_df = get_cqc_provider_df(cqc_source)
+    spark = utils.get_spark()
+    cqc_provider_df = get_cqc_provider_df(cqc_source, spark)
     cleaned_cqc_provider_df = clean_cqc_provider_df(cqc_provider_df)
-    write_cleaned_provider_df_to_parquet(cleaned_cqc_provider_df, cleaned_cqc_destination)
+    write_cleaned_provider_df_to_parquet(
+        cleaned_cqc_provider_df, cleaned_cqc_destination
+    )
 
 
 if __name__ == "__main__":
-#Where we tell Glue how to run the file, and what to print out
+    # Where we tell Glue how to run the file, and what to print out
     print("Spark job 'clean_cqc_provider_data' starting...")
     print(f"Job parameters: {sys.argv}")
 
     source, destination = utils.collect_arguments(
-        ("--cqc_provider_source", "Source s3 directory for parquet CQC providers dataset"),
-        ("--cqc_provider_cleaned", "Destination s3 directory for cleaned parquet CQC providers dataset"),
+        (
+            "--cqc_provider_source",
+            "Source s3 directory for parquet CQC providers dataset",
+        ),
+        (
+            "--cqc_provider_cleaned",
+            "Destination s3 directory for cleaned parquet CQC providers dataset",
+        ),
     )
-#Python logic ---> all in main
+    # Python logic ---> all in main
     main(source, destination)
 
     print("Spark job 'clean_cqc_provider_data' complete")
--- tests/unit/test_clean_cqc_provider_data.py
+++ tests/unit/test_clean_cqc_provider_data.py
@@ -6,6 +6,7 @@ from pyspark.sql import SparkSession
 
 from tests.test_file_generator import generate_cqc_providers_file
 
+
 class CleanCQCProviderDatasetTests(unittest.TestCase):
     TEST_CQC_PROVIDERS_FILE = "tests/test_data/domain=cqc/dataset=providers"
 
@@ -31,9 +32,10 @@ class CleanCQCProviderDatasetTests(unittest.TestCase):
 
     def test_write_cleaned_provider_df_to_parquet(self):
         print("todo")
-    
+
     def test_main(self):
         print("todo")
+
 
 if __name__ == "__main__":
     unittest.main(warnings="ignore")
