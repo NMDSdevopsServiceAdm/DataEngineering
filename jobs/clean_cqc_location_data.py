766182bf265baef3e4e24f910f5bc8bcf056e969
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-25 14:43:01 +0000

    Add clean cqc location data script

---
+++ jobs/clean_cqc_location_data.py
@@ -0,0 +1,40 @@
+import sys
+
+import pyspark.sql.dataframe
+
+from utils import utils
+
+def clean_cqc_location_df(df_to_clean) -> pyspark.sql.DataFrame:
+    # Cleaning logic
+    return df_to_clean
+
+
+def main(cqc_location_source: str, cleaned_cqc_location_destintion: str):
+    cqc_location_df = utils.read_from_parquet(cqc_location_source)
+    print(cqc_location_df)
+    cleaned_cqc_location_df = clean_cqc_location_df(cqc_location_df)
+    utils.write_to_parquet(
+        cleaned_cqc_location_df,
+        cleaned_cqc_location_destintion,
+        append=True,
+        partitionKeys=["year", "month", "day", "import_date"],
+    )
+
+
+if __name__ == "__main__":
+    print("Spark job 'clean_cqc_location_data' starting...")
+    print(f"Job parameters: {sys.argv}")
+
+    cqc_location_source, cleaned_cqc_location_destination = utils.collect_arguments(
+        (
+            "--cqc_location_source",
+            "Source s3 directory for parquet CQC locations dataset",
+        ),
+        (
+            "--cleaned_cqc_location_destination",
+            "Destination s3 directory for cleaned parquet CQC locations dataset",
+        ),
+    )
+    main(cqc_location_source, cleaned_cqc_location_destination)
+
+    print("Spark job 'clean_cqc_location_data' complete")
