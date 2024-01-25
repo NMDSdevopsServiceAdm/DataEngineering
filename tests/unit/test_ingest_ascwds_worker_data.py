b89fb1302fd18bcde65f6e5ee42d1b6262b0da0f
james-francis-mt <james.francis@madetech.com>
2024-01-25 10:16:06 +0000

    refactor mock setup order to reduce setup in test case

--- tests/unit/test_ingest_ascwds_worker_data.py
+++ tests/unit/test_ingest_ascwds_worker_data.py
@@ -13,6 +13,7 @@ class IngestASCWDSWorkerDatasetTests(unittest.TestCase):
         self.spark_mock = Mock()
         self.spark_mock.read = self.spark_mock
         self.spark_mock.option.return_value = self.spark_mock
+        self.spark_mock.csv.return_value = self.test_ascwds_worker_df
 
     @patch("jobs.ingest_ascwds_worker_data.get_delimiter_for_csv")
     @patch("utils.utils.write_to_parquet")
@@ -21,7 +22,6 @@ class IngestASCWDSWorkerDatasetTests(unittest.TestCase):
         self, get_spark_mock, write_to_parquet_mock, get_delimiter_for_csv_mock
     ):
         get_spark_mock.return_value = self.spark_mock
-        self.spark_mock.csv.return_value = self.test_ascwds_worker_df
         get_delimiter_for_csv_mock.return_value = ","
 
         job.main("some source", "some destination")
