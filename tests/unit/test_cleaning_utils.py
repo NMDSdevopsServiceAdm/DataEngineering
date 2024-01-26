f0cb2262c8430a650ccdba04b9aa925995ef6508
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-26 14:24:32 +0000

    Add test that new column is added

--- tests/unit/test_cleaning_utils.py
+++ tests/unit/test_cleaning_utils.py
@@ -22,3 +22,10 @@ class TestCleaningUtils(unittest.TestCase):
         returned_df = job.apply_categorical_labels(self.test_worker_df, self.label_dicts, new_column=True)
 
         self.assertIsNotNone(returned_df)
+
+    def test_apply_categorical_labels_adds_a_new_column_when_given_one_list_and_new_column_is_set_to_true(self):
+        returned_df = job.apply_categorical_labels(self.test_worker_df, self.label_dicts["gender_labels"], new_column=True)
+
+        expected_columns = len(self.test_worker_df.columns) + 1
+
+        self.assertEqual(len(returned_df.columns), expected_columns)
