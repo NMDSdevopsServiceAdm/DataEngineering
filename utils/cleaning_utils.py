89d2c161cc8792948fec29add8f01dcfb03e54ee
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-26 17:27:32 +0000

    Remove unused list

--- utils/cleaning_utils.py
+++ utils/cleaning_utils.py
@@ -6,14 +6,12 @@ def apply_categorical_labels(
     df: DataFrame, labels: dict, columns: list, new_column: bool = True
 ) -> DataFrame:
     if (new_column == True):
-        new_column_names = []
-        for i, column_name in enumerate(columns):
-            
-            new_column_names.append(column_name + "_labels")
-            df = df.withColumn(new_column_names[i], F.col(column_name))
-            df = replace_labels(df, labels, column_name, new_column_names[i])
+        for column_name in columns:
+            new_column_name = column_name + "_labels"
+            df = df.withColumn(new_column_name, F.col(column_name))
+            df = replace_labels(df, labels, column_name, new_column_name)
     elif (new_column == False):
-        for i, column_name in enumerate(columns):
+        for column_name in columns:
             df = replace_labels(df, labels, column_name)
     return df
 
