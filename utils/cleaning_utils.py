1016e5558f480f42fc882ccbdfaefa26caab4f94
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-26 14:57:37 +0000

    Add new column

--- utils/cleaning_utils.py
+++ utils/cleaning_utils.py
@@ -1,5 +1,7 @@
 from pyspark.sql import DataFrame
+import pyspark.sql.functions as F
 
-def apply_categorical_labels(df:DataFrame, labels:dict, new_column:bool) -> DataFrame:
-    # TODO 
+def apply_categorical_labels(df:DataFrame, labels:dict, columns: list, new_column:bool) -> DataFrame:
+    new_column_name = columns[0] + "labels"
+    df = df.withColumn(new_column_name, F.col(columns[0]))
     return df
