f7720a8cfdea2ffd1314416f757f2c19397a63a7
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-26 15:17:53 +0000

    Add loop for columns in list

--- utils/cleaning_utils.py
+++ utils/cleaning_utils.py
@@ -2,6 +2,10 @@ from pyspark.sql import DataFrame
 import pyspark.sql.functions as F
 
 def apply_categorical_labels(df:DataFrame, labels:dict, columns: list, new_column:bool) -> DataFrame:
-    new_column_name = columns[0] + "labels"
-    df = df.withColumn(new_column_name, F.col(columns[0]))
+    new_column_names = []
+    for i, column_name in enumerate(columns):
+        print(i)
+        new_column_names.append(column_name + "labels")
+        # new_column_name = columns[0] + "labels"
+        df = df.withColumn(new_column_names[i], F.col(column_name))
     return df
