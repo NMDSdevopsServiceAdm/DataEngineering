3e8a52fcfc2381f5aefa544c6ed9aba53fef44b3
Luke Williams <lawilliams1993@gmail.com>
2024-01-26 09:47:23 +0000

    remove duplicate schema definition

--- tests/test_file_schemas.py
+++ tests/test_file_schemas.py
@@ -165,33 +165,3 @@ class CalculatePaRatioSchemas:
             ),
         ]
     )
-
-
-@dataclass
-class CQCProviderSchemas:
-    full_parquet_schema = StructType(
-        [
-            StructField("providerId", StringType(), True),
-            StructField("locationIds", ArrayType(StringType(), True), True),
-            StructField("organisationType", StringType(), True),
-            StructField("ownershipType", StringType(), True),
-            StructField("type", StringType(), True),
-            StructField("uprn", StringType(), True),
-            StructField("name", StringType(), True),
-            StructField("registrationStatus", StringType(), True),
-            StructField("registrationDate", StringType(), True),
-            StructField("deregistrationDate", StringType(), True),
-            StructField("postalAddressLine1", StringType(), True),
-            StructField("postalAddressTownCity", StringType(), True),
-            StructField("postalAddressCounty", StringType(), True),
-            StructField("region", StringType(), True),
-            StructField("postalCode", StringType(), True),
-            StructField("onspdLatitude", FloatType(), True),
-            StructField("onspdLongitude", FloatType(), True),
-            StructField("mainPhoneNumber", StringType(), True),
-            StructField("companiesHouseNumber", StringType(), True),
-            StructField("inspectionDirectorate", StringType(), True),
-            StructField("constituency", StringType(), True),
-            StructField("localAuthority", StringType(), True),
-        ]
-    )
