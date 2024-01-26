57a8a975ae647c0f0f5a28405faafbd79837b277
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-26 16:11:37 +0000

    Format code

--- tests/unit/test_cleaning_utils.py
+++ tests/unit/test_cleaning_utils.py
@@ -7,7 +7,9 @@ import utils.cleaning_utils as job
 from tests.test_file_schemas import CleaningUtilsSchemas as Schemas
 from tests.test_file_data import CleaningUtilsData as Data
 
-from utils.column_names.raw_data_files.ascwds_worker_columns import AscwdsWorkerColumns as AWK
+from utils.column_names.raw_data_files.ascwds_worker_columns import (
+    AscwdsWorkerColumns as AWK,
+)
 
 
 class TestCleaningUtils(unittest.TestCase):
@@ -16,44 +18,105 @@ class TestCleaningUtils(unittest.TestCase):
         self.test_worker_df = self.spark.createDataFrame(
             Data.worker_rows, schema=Schemas.worker_schema
         )
-        self.label_dicts = {"gender": Data.gender, 
-                            "nationality": Data.nationality}
-
+        self.label_dicts = {"gender": Data.gender, "nationality": Data.nationality}
 
     def test_apply_categorical_labels_completes(self):
-        returned_df = job.apply_categorical_labels(self.test_worker_df, self.label_dicts, [AWK.gender, AWK.nationality], new_column=True)
+        returned_df = job.apply_categorical_labels(
+            self.test_worker_df,
+            self.label_dicts,
+            [AWK.gender, AWK.nationality],
+            new_column=True,
+        )
 
         self.assertIsNotNone(returned_df)
 
-    def test_apply_categorical_labels_adds_a_new_column_when_given_one_column_and_new_column_is_set_to_true(self):
-        returned_df = job.apply_categorical_labels(self.test_worker_df, self.label_dicts, [AWK.gender], new_column=True)
+    def test_apply_categorical_labels_adds_a_new_column_when_given_one_column_and_new_column_is_set_to_true(
+        self,
+    ):
+        returned_df = job.apply_categorical_labels(
+            self.test_worker_df, self.label_dicts, [AWK.gender], new_column=True
+        )
 
         expected_columns = len(self.test_worker_df.columns) + 1
 
         self.assertEqual(len(returned_df.columns), expected_columns)
 
-    def test_apply_categorical_labels_adds_two_new_columns_when_given_two_columns_and_new_column_is_set_to_true(self):
-        returned_df = job.apply_categorical_labels(self.test_worker_df, self.label_dicts, [AWK.gender, AWK.nationality], new_column=True)
+    def test_apply_categorical_labels_adds_two_new_columns_when_given_two_columns_and_new_column_is_set_to_true(
+        self,
+    ):
+        returned_df = job.apply_categorical_labels(
+            self.test_worker_df,
+            self.label_dicts,
+            [AWK.gender, AWK.nationality],
+            new_column=True,
+        )
 
         expected_columns = len(self.test_worker_df.columns) + 2
 
         self.assertEqual(len(returned_df.columns), expected_columns)
-    
-    def test_apply_categorical_labels_adds_new_columns_with_replaced_values_when_new_column_is_set_to_true(self):
-        returned_df = job.apply_categorical_labels(self.test_worker_df, self.label_dicts, [AWK.gender, AWK.nationality], new_column=True)
+
+    def test_apply_categorical_labels_adds_new_columns_with_replaced_values_when_new_column_is_set_to_true(
+        self,
+    ):
+        returned_df = job.apply_categorical_labels(
+            self.test_worker_df,
+            self.label_dicts,
+            [AWK.gender, AWK.nationality],
+            new_column=True,
+        )
         returned_data = returned_df.collect()
-        expected_data = {"gender_labels" : ["male", "male", "female", "female", None, "female"], "nationality_labels" : ["British", "French", "Spanish", "Portuguese", "Portuguese", None],}
+        expected_data = {
+            "gender_labels": ["male", "male", "female", "female", None, "female"],
+            "nationality_labels": [
+                "British",
+                "French",
+                "Spanish",
+                "Portuguese",
+                "Portuguese",
+                None,
+            ],
+        }
 
-        self.assertEqual(returned_data[0]["gender_labels"], expected_data["gender_labels"][0])
-        self.assertEqual(returned_data[1]["gender_labels"], expected_data["gender_labels"][1])
-        self.assertEqual(returned_data[2]["gender_labels"], expected_data["gender_labels"][2])
-        self.assertEqual(returned_data[3]["gender_labels"], expected_data["gender_labels"][3])
-        self.assertEqual(returned_data[4]["gender_labels"], expected_data["gender_labels"][4])
-        self.assertEqual(returned_data[5]["gender_labels"], expected_data["gender_labels"][5])
+        self.assertEqual(
+            returned_data[0]["gender_labels"], expected_data["gender_labels"][0]
+        )
+        self.assertEqual(
+            returned_data[1]["gender_labels"], expected_data["gender_labels"][1]
+        )
+        self.assertEqual(
+            returned_data[2]["gender_labels"], expected_data["gender_labels"][2]
+        )
+        self.assertEqual(
+            returned_data[3]["gender_labels"], expected_data["gender_labels"][3]
+        )
+        self.assertEqual(
+            returned_data[4]["gender_labels"], expected_data["gender_labels"][4]
+        )
+        self.assertEqual(
+            returned_data[5]["gender_labels"], expected_data["gender_labels"][5]
+        )
 
-        self.assertEqual(returned_data[0]["nationality_labels"], expected_data["nationality_labels"][0])
-        self.assertEqual(returned_data[1]["nationality_labels"], expected_data["nationality_labels"][1])
-        self.assertEqual(returned_data[2]["nationality_labels"], expected_data["nationality_labels"][2])
-        self.assertEqual(returned_data[3]["nationality_labels"], expected_data["nationality_labels"][3])
-        self.assertEqual(returned_data[4]["nationality_labels"], expected_data["nationality_labels"][4])
-        self.assertEqual(returned_data[5]["nationality_labels"], expected_data["nationality_labels"][5])
+        self.assertEqual(
+            returned_data[0]["nationality_labels"],
+            expected_data["nationality_labels"][0],
+        )
+        self.assertEqual(
+            returned_data[1]["nationality_labels"],
+            expected_data["nationality_labels"][1],
+        )
+        self.assertEqual(
+            returned_data[2]["nationality_labels"],
+            expected_data["nationality_labels"][2],
+        )
+        self.assertEqual(
+            returned_data[3]["nationality_labels"],
+            expected_data["nationality_labels"][3],
+        )
+        self.assertEqual(
+            returned_data[4]["nationality_labels"],
+            expected_data["nationality_labels"][4],
+        )
+        self.assertEqual(
+            returned_data[5]["nationality_labels"],
+            expected_data["nationality_labels"][5],
+        )
--- utils/cleaning_utils.py
+++ utils/cleaning_utils.py
@@ -1,13 +1,16 @@
 from pyspark.sql import DataFrame
 import pyspark.sql.functions as F
 
-def apply_categorical_labels(df:DataFrame, labels:dict, columns: list, new_column:bool) -> DataFrame:
+
+def apply_categorical_labels(
+    df: DataFrame, labels: dict, columns: list, new_column: bool
+) -> DataFrame:
     new_column_names = []
     for i, column_name in enumerate(columns):
         print(i)
         keys = list(labels[column_name].keys())
         values = list(labels[column_name].values())
-        print (keys, values)
+        print(keys, values)
         new_column_names.append(column_name + "_labels")
 
         df = df.withColumn(new_column_names[i], F.col(column_name))
