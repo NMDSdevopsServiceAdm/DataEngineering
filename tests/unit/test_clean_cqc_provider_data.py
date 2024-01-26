168d2e66e542d6bd0143b57b39dfe57ece40e5f4
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-26 11:36:23 +0000

    Undo previous revert

--- tests/unit/test_clean_cqc_provider_data.py
+++ tests/unit/test_clean_cqc_provider_data.py
@@ -2,7 +2,6 @@ import unittest
 from unittest.mock import Mock, patch
 
 from pyspark.sql import SparkSession
-from utils import utils
 
 import jobs.clean_cqc_provider_data as job
 
@@ -17,28 +16,47 @@ class CleanCQCProviderDatasetTests(unittest.TestCase):
     partition_keys = ["year", "month", "day", "import_date"]
 
     def setUp(self) -> None:
-        self.spark = utils.get_spark()
+        self.spark = SparkSession.builder.appName(
+            "test_clean_cqc_provider_data"
+        ).getOrCreate()
         self.test_cqc_providers_parquet = self.spark.createDataFrame(
             Data.sample_rows_full, schema=PROVIDER_SCHEMA
         )
+        self.spark_mock = helpers.create_spark_mock()
+        self.spark_mock.parquet.return_value = self.test_cqc_providers_parquet
+
+    def test_get_cqc_provider_df(self):
+        df = job.get_cqc_provider_df(self.TEST_SOURCE, self.spark_mock)
+
+        # Ensure specific functionality is called once
+        self.spark_mock.read.option.assert_called_once_with(
+            "basePath", self.TEST_SOURCE
+        )
+        self.spark_mock.parquet.assert_called_once_with(self.TEST_SOURCE)
+
+        # Test data read doesn't lose any data
+        self.assertEqual(df.count(), self.test_cqc_providers_parquet.count())
+
+        # Schema structure preserved
 
     def test_clean_cqc_provider_df(self):
         # Test returned df is the same as the one passed in
-        returned_df = job.clean_cqc_provider_df(self.test_clean_cqc_provider_df)
-
-        self.assertEqual(self.test_clean_cqc_provider_df, returned_df)
+        self.assertTrue(False)
 
     @patch("utils.utils.write_to_parquet")
-    @patch("utils.utils.read_from_parquet")
-    def test_main(self, read_from_parquet_patch, write_to_parquet_patch):
-        read_from_parquet_patch.return_value = self.test_cqc_providers_parquet
-        job.main(self.TEST_SOURCE, self.TEST_DESTINATION)
-        write_to_parquet_patch.assert_called_once_with(
+    def test_write_cleaned_provider_df_to_parquet(self, write_to_parquet_mock):
+        job.write_cleaned_provider_df_to_parquet(
+            self.test_cqc_providers_parquet, self.TEST_DESTINATION
+        )
+        write_to_parquet_mock.assert_called_once_with(
             self.test_cqc_providers_parquet,
             self.TEST_DESTINATION,
             append=True,
             partitionKeys=self.partition_keys,
         )
+
+    def test_main(self):
+        self.assertTrue(False)
 
 
 if __name__ == "__main__":
