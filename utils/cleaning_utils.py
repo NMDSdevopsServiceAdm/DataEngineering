0a09afe99502babb57cd31aa88155dda731f6ec4
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-26 16:25:20 +0000

    Add branching logic

--- utils/cleaning_utils.py
+++ utils/cleaning_utils.py
@@ -3,16 +3,24 @@ import pyspark.sql.functions as F
 
 
 def apply_categorical_labels(
-    df: DataFrame, labels: dict, columns: list, new_column: bool
+    df: DataFrame, labels: dict, columns: list, new_column: bool = True
 ) -> DataFrame:
-    new_column_names = []
-    for i, column_name in enumerate(columns):
-        print(i)
-        keys = list(labels[column_name].keys())
-        values = list(labels[column_name].values())
-        print(keys, values)
-        new_column_names.append(column_name + "_labels")
+    if (new_column == True):
+        new_column_names = []
+        for i, column_name in enumerate(columns):
+            print(i)
+            keys = list(labels[column_name].keys())
+            values = list(labels[column_name].values())
+            print(keys, values)
+            new_column_names.append(column_name + "_labels")
 
-        df = df.withColumn(new_column_names[i], F.col(column_name))
-        df = df.na.replace(keys, values, new_column_names[i])
+            df = df.withColumn(new_column_names[i], F.col(column_name))
+            df = df.na.replace(keys, values, new_column_names[i])
+    elif (new_column == False):
+        for i, column_name in enumerate(columns):
+            print(i)
+            keys = list(labels[column_name].keys())
+            values = list(labels[column_name].values())
+            print(keys, values)
+            df = df.na.replace(keys, values, column_name)
     return df
