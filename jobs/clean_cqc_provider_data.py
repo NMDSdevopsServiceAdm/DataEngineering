e38c2ad083beb0624c359e507d7c04f85f62c907
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-26 11:31:20 +0000

    Revert "latest changes with marion and james updates"
    This reverts commit 61733b034f88fcbc5788e95062ff3e91efa36e46.

--- jobs/clean_cqc_provider_data.py
+++ jobs/clean_cqc_provider_data.py
@@ -5,20 +5,37 @@ import pyspark.sql.dataframe
 from utils import utils
 
 
+def get_cqc_provider_df(cqc_provider_source, spark_session) -> pyspark.sql.DataFrame:
+    print(f"Reading CQC providers parquet from {cqc_provider_source}")
+    cqc_provider_df = spark_session.read.option(
+        "basePath", cqc_provider_source
+    ).parquet(  # used for partitioning
+        cqc_provider_source
+    )
+
+    return cqc_provider_df
+
+
 def clean_cqc_provider_df(df_to_clean) -> pyspark.sql.DataFrame:
     # Cleaning logic
     return df_to_clean
 
 
-def main(cqc_source: str, cleaned_cqc_destination: str):
-    cqc_provider_df = utils.read_from_parquet(cqc_source)
-    print(cqc_provider_df)
-    cleaned_cqc_provider_df = clean_cqc_provider_df(cqc_provider_df)
+def write_cleaned_provider_df_to_parquet(dataFrame, destination) -> None:
     utils.write_to_parquet(
-        cleaned_cqc_provider_df,
-        cleaned_cqc_destination,
+        dataFrame,
+        destination,
         append=True,
         partitionKeys=["year", "month", "day", "import_date"],
+    )
+
+
+def main(cqc_source: str, cleaned_cqc_destination: str):
+    spark = utils.get_spark()
+    cqc_provider_df = get_cqc_provider_df(cqc_source, spark)
+    cleaned_cqc_provider_df = clean_cqc_provider_df(cqc_provider_df)
+    write_cleaned_provider_df_to_parquet(
+        cleaned_cqc_provider_df, cleaned_cqc_destination
     )
 
 
