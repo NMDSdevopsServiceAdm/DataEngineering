b9fd357d051c46c69800f55f1450355f34530e10
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-26 17:14:03 +0000

    Remove print statements

--- utils/cleaning_utils.py
+++ utils/cleaning_utils.py
@@ -8,19 +8,14 @@ def apply_categorical_labels(
     if (new_column == True):
         new_column_names = []
         for i, column_name in enumerate(columns):
-            print(i)
             keys = list(labels[column_name].keys())
             values = list(labels[column_name].values())
-            print(keys, values)
             new_column_names.append(column_name + "_labels")
-
             df = df.withColumn(new_column_names[i], F.col(column_name))
             df = df.na.replace(keys, values, new_column_names[i])
     elif (new_column == False):
         for i, column_name in enumerate(columns):
-            print(i)
             keys = list(labels[column_name].keys())
             values = list(labels[column_name].values())
-            print(keys, values)
             df = df.na.replace(keys, values, column_name)
     return df
