c714ff9c2c6def567efca69b14755329ff86f228
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-26 14:57:58 +0000

    Add test that two columns are added

--- tests/unit/test_cleaning_utils.py
+++ tests/unit/test_cleaning_utils.py
@@ -7,6 +7,8 @@ import utils.cleaning_utils as job
 from tests.test_file_schemas import CleaningUtilsSchemas as Schemas
 from tests.test_file_data import CleaningUtilsData as Data
 
+from utils.column_names.raw_data_files.ascwds_worker_columns import AscwdsWorkerColumns as AWK
+
 
 class TestCleaningUtils(unittest.TestCase):
     def setUp(self):
@@ -14,18 +16,25 @@ class TestCleaningUtils(unittest.TestCase):
         self.test_worker_df = self.spark.createDataFrame(
             Data.worker_rows, schema=Schemas.worker_schema
         )
-        self.label_dicts = {"gender_labels": Data.gender_labels, 
-                            "nationality_labels": Data.nationality_labels}
+        self.label_dicts = {"gender": Data.gender_labels, 
+                            "nationality": Data.nationality_labels}
 
 
     def test_apply_categorical_labels_completes(self):
-        returned_df = job.apply_categorical_labels(self.test_worker_df, self.label_dicts, new_column=True)
+        returned_df = job.apply_categorical_labels(self.test_worker_df, self.label_dicts, [AWK.gender, AWK.nationality], new_column=True)
 
         self.assertIsNotNone(returned_df)
 
-    def test_apply_categorical_labels_adds_a_new_column_when_given_one_list_and_new_column_is_set_to_true(self):
-        returned_df = job.apply_categorical_labels(self.test_worker_df, self.label_dicts["gender_labels"], new_column=True)
+    def test_apply_categorical_labels_adds_a_new_column_when_given_one_column_and_new_column_is_set_to_true(self):
+        returned_df = job.apply_categorical_labels(self.test_worker_df, self.label_dicts, [AWK.gender], new_column=True)
 
         expected_columns = len(self.test_worker_df.columns) + 1
+
+        self.assertEqual(len(returned_df.columns), expected_columns)
+
+    def test_apply_categorical_labels_adds_two_new_columns_when_given_two_columns_and_new_column_is_set_to_true(self):
+        returned_df = job.apply_categorical_labels(self.test_worker_df, self.label_dicts, [AWK.gender, AWK.nationality], new_column=True)
+
+        expected_columns = len(self.test_worker_df.columns) + 2
 
         self.assertEqual(len(returned_df.columns), expected_columns)
