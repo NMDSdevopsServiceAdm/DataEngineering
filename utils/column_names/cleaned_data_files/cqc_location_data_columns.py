11feb489a70d24e2a7fce4380dda1304bd13db94
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-01-30 16:07:41 +0000

    Set up sector column name with inheritence

--- utils/column_names/cleaned_data_files/cqc_location_data_columns.py
+++ utils/column_names/cleaned_data_files/cqc_location_data_columns.py
@@ -3,9 +3,12 @@ from dataclasses import dataclass
 from utils.column_names.raw_data_files.cqc_location_api_columns import (
     CqcLocationApiColumns,
 )
+from utils.column_names.cleaned_data_files.cqc_provider_data_columns import (
+    CqcProviderCleanedColumns as CQCPClean,
+)
 
 
 @dataclass
 class CqcLocationCleanedColumns(CqcLocationApiColumns):
     primary_service_type: str = "primary_service_type"
-    sector: str = "sector"
+    sector: str = CQCPClean.sector
---
+++ utils/column_names/cleaned_data_files/cqc_provider_data_columns.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+from utils.column_names.raw_data_files.cqc_provider_api_columns import (
+    CqcProviderApiColumns
+)
+
+
+@dataclass
+class CqcProviderCleanedColumns(CqcProviderApiColumns):
+    sector: str = "sector"
