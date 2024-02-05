214ca09d9b54efa215cd9a22a3e4c01c3288dfaf
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-02-05 10:49:29 +0000

    Write test for handling directories

--- tests/unit/test_ingest_ons_data.py
+++ tests/unit/test_ingest_ons_data.py
@@ -1,17 +1,10 @@
-import shutil
 import unittest
-from pathlib import Path
-import warnings
 
-from pyspark.sql import SparkSession
 from unittest.mock import patch, Mock
 
 import jobs.ingest_ons_data as job
-from utils.column_names.raw_data_files.ons_columns import (
-    OnsPostcodeDirectoryColumns as ColNames,
-)
+
 from utils import utils
-from tests.unit.test_utils import StubberClass, StubberType
 from tests.test_file_data import IngestONSData as Data
 from tests.test_file_schemas import IngestONSData as Schemas
 
@@ -19,7 +12,7 @@ class IngestONSDataTests(unittest.TestCase):
     TEST_CSV_SOURCE = "some/directory/path/file.csv"
     TEST_DIRECTORY_SOURCE = "some/directory/path"
     TEST_DESTINATION = "s3://some/"
-    TEST_NEW_DESTINATION = "s3://some/directory/path" #Work out why this is the new destination
+    TEST_NEW_DESTINATION = "s3://some/directory/path" 
     
     def setUp(self):
         self.spark = utils.get_spark()
@@ -27,10 +20,10 @@ class IngestONSDataTests(unittest.TestCase):
             Data.sample_rows, Schemas.sample_schema
         )
         self.object_list = [
-            "version=1.0.0/import_date=20210101/some-data-file.csv",
-            "version=1.0.0/import_date=20210101/some-other-other-data-file.csv",
+            "directory/path/some-data-file.csv",
+            "directory/path/some-other-other-data-file.csv",
         ]
-        self.partial_csv_content = "Id,SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm,Species"
+        self.partial_csv_content = "Some, csv, content"
         
     @patch("utils.utils.read_partial_csv_content")
     @patch("utils.utils.get_s3_objects_list")
@@ -51,6 +44,30 @@ class IngestONSDataTests(unittest.TestCase):
         self.assertEqual(read_csv_patch.call_count, 1)
         self.assertEqual(read_partial_csv_content_patch.call_count, 1)
         write_to_parquet_patch.assert_called_once_with(
+            self.test_ons_df,
+            self.TEST_NEW_DESTINATION,
+        )
+    
+    @patch("utils.utils.read_partial_csv_content")
+    @patch("utils.utils.get_s3_objects_list")
+    @patch("utils.utils.write_to_parquet")
+    @patch("utils.utils.read_csv")
+    def test_main_runs_when_source_is_directory(
+        self, read_csv_patch: Mock, write_to_parquet_patch: Mock, get_s3_objects_list_patch: Mock, read_partial_csv_content_patch: Mock
+    ):  
+        
+
+        get_s3_objects_list_patch.return_value = self.object_list
+        read_csv_patch.return_value = self.test_ons_df
+        read_partial_csv_content_patch.return_value = self.partial_csv_content
+
+        job.main(self.TEST_DIRECTORY_SOURCE, self.TEST_DESTINATION)
+
+        self.assertEqual(get_s3_objects_list_patch.call_count, 1)
+        self.assertEqual(read_csv_patch.call_count, 2)
+        self.assertEqual(read_partial_csv_content_patch.call_count, 2)
+        self.assertEqual(write_to_parquet_patch.call_count, 2)
+        write_to_parquet_patch.assert_any_call(
             self.test_ons_df,
             self.TEST_NEW_DESTINATION,
         )
