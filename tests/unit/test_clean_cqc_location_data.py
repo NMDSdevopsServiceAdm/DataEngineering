535cdc9ed11a80b16992a3931f4ac9b514e35e36
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-02-08 15:27:14 +0000

    Write test

--- tests/unit/test_clean_cqc_location_data.py
+++ tests/unit/test_clean_cqc_location_data.py
@@ -35,6 +35,7 @@ class CleanCQCLocationDatasetTests(unittest.TestCase):
             Data.join_provider_rows, Schemas.join_provider_schema
         )
 
+
     @patch("utils.utils.write_to_parquet")
     @patch("utils.utils.read_from_parquet")
     def test_main_runs(
@@ -106,6 +107,17 @@ class CleanCQCLocationDatasetTests(unittest.TestCase):
         )
 
         self.assertCountEqual(returned_data, expected_data)
+    
+    def test_split_dataframe_into_registered_and_deregistered_rows_splits_data_correctly(self):
+        test_df = self.spark.createDataFrame(Data.registration_status_rows, Schemas.registration_status_schema)
+        
+        returned_registered_df, returned_deregistered_df = job.split_dataframe_into_registered_and_deregistered_rows(test_df)
+
+        expected_registered_df = self.spark.createDataFrame(Data.expected_registered_rows, Schemas.registration_status_schema)
+        expected_deregistered_df = self.spark.createDataFrame(Data.expected_deregistered_rows, Schemas.registration_status_schema)
+        
+        self.assertEqual(returned_registered_df, expected_registered_df)
+        self.assertEqual(returned_deregistered_df, expected_deregistered_df)
 
 
 if __name__ == "__main__":
