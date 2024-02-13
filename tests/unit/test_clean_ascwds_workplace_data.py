40491ba797a8ab8687af33a10a8b901d0e59da48
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-02-13 11:28:46 +0000

    Refactor tests

--- tests/unit/test_clean_ascwds_workplace_data.py
+++ tests/unit/test_clean_ascwds_workplace_data.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, ANY
 
 import jobs.clean_ascwds_workplace_data as job
 
@@ -23,22 +23,11 @@ class IngestASCWDSWorkerDatasetTests(unittest.TestCase):
     ]
 
     def setUp(self) -> None:
-        spark = get_spark()
-        self.test_ascwds_worker_df = spark.createDataFrame(
+        self.spark = get_spark()
+        self.test_ascwds_worker_df = self.spark.createDataFrame(
             Data.workplace_rows, Schemas.workplace_schema
         )
-        self.cast_to_int_df = spark.createDataFrame(
-            Data.cast_to_int_rows, Schemas.cast_to_int_schema
-        )
-        self.cast_to_int_with_errors_df = spark.createDataFrame(
-            Data.cast_to_int_errors_rows, Schemas.cast_to_int_schema
-        )
-        self.cast_to_int_expected_df = spark.createDataFrame(
-            Data.cast_to_int_expected_rows, Schemas.cast_to_int_expected_schema
-        )
-        self.cast_to_int_with_errors_expected_df = spark.createDataFrame(
-            Data.cast_to_int_errors_expected_rows, Schemas.cast_to_int_expected_schema
-        )
+        
         self.filled_posts_columns = [AWP.total_staff, AWP.worker_records]
 
     @patch("utils.utils.write_to_parquet")
@@ -50,32 +39,45 @@ class IngestASCWDSWorkerDatasetTests(unittest.TestCase):
 
         read_from_parquet_mock.assert_called_once_with(self.TEST_SOURCE)
         write_to_parquet_mock.assert_called_once_with(
-            self.test_ascwds_worker_df,
+            ANY,
             self.TEST_DESTINATION,
             True,
             self.partition_keys,
         )
 
     def test_cast_to_int_returns_strings_formatted_as_ints_to_ints(self):
-        returned_df = job.cast_to_int(self.cast_to_int_df, self.filled_posts_columns)
+        cast_to_int_df = self.spark.createDataFrame(
+            Data.cast_to_int_rows, Schemas.cast_to_int_schema
+        )
+        cast_to_int_expected_df = self.spark.createDataFrame(
+            Data.cast_to_int_expected_rows, Schemas.cast_to_int_expected_schema
+        )
+
+        returned_df = job.cast_to_int(cast_to_int_df, self.filled_posts_columns)
+
         returned_data = returned_df.sort(AWP.location_id).collect()
-        expected_data = self.cast_to_int_expected_df.sort(AWP.location_id).collect()
-        returned_df.show()
-        returned_df.printSchema()
-        self.cast_to_int_expected_df.show()
-        self.cast_to_int_expected_df.printSchema()
+        expected_data = cast_to_int_expected_df.sort(AWP.location_id).collect()
+
         self.assertEqual(expected_data, returned_data)
 
     def test_cast_to_int_returns_strings_not_formatted_as_ints_as_none(self):
-        returned_df = job.cast_to_int(
-            self.cast_to_int_with_errors_df, self.filled_posts_columns
+        
+        cast_to_int_with_errors_df = self.spark.createDataFrame(
+            Data.cast_to_int_errors_rows, Schemas.cast_to_int_schema
         )
+        cast_to_int_with_errors_expected_df = self.spark.createDataFrame(
+            Data.cast_to_int_errors_expected_rows, Schemas.cast_to_int_expected_schema
+        )
+
+        returned_df = job.cast_to_int(
+            cast_to_int_with_errors_df, self.filled_posts_columns
+        )
+        
         returned_data = returned_df.sort(AWP.location_id).collect()
-        expected_data = self.cast_to_int_with_errors_expected_df.sort(
+        expected_data = cast_to_int_with_errors_expected_df.sort(
             AWP.location_id
         ).collect()
-        returned_df.show()
-        self.cast_to_int_with_errors_expected_df.show()
+
         self.assertEqual(expected_data, returned_data)
 
 
