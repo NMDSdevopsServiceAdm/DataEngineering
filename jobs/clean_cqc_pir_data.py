f2aebf2ade73e0d0891bfb7ab3a6ffc57a244595
james-francis-mt <james.francis@madetech.com>
2024-02-14 10:29:39 +0000

    extract cqc pir import date column name to constant

--- jobs/clean_cqc_pir_data.py
+++ jobs/clean_cqc_pir_data.py
@@ -4,6 +4,10 @@ from utils import utils
 import utils.cleaning_utils as cUtils
 from utils.column_names.ind_cqc_pipeline_columns import PartitionKeys as Keys
 
+from utils.column_names.cleaned_data_files.cqc_pir_cleaned_values import (
+    CqcLPIRCleanedColumns as PIRClean,
+)
+
 pirPartitionKeys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
 
 
@@ -11,7 +15,7 @@ def main(cqc_pir_source: str, cleaned_cqc_pir_destination: str):
     cqc_pir_df = utils.read_from_parquet(cqc_pir_source)
 
     cqc_pir_df = cUtils.column_to_date(
-        cqc_pir_df, Keys.import_date, "cqc_pir_import_date"
+        cqc_pir_df, Keys.import_date, PIRClean.cqc_pir_import_date
     )
 
     utils.write_to_parquet(
---
+++ utils/column_names/cleaned_data_files/cqc_pir_cleaned_values.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+from utils.column_names.raw_data_files.cqc_pir_columns import CqcPirColumns
+
+
+@dataclass
+class CqcLPIRCleanedColumns(CqcPirColumns):
+    cqc_pir_import_date: str = "cqc_pir_import_date"
