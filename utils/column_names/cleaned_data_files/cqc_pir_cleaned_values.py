adbd5e171ae98459ad426b1a7a344d80785a21e5
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-02-19 15:12:06 +0000

    Add column name to cleaned PIR data

--- utils/column_names/cleaned_data_files/cqc_pir_cleaned_values.py
+++ utils/column_names/cleaned_data_files/cqc_pir_cleaned_values.py
@@ -6,3 +6,4 @@ from utils.column_names.raw_data_files.cqc_pir_columns import CqcPirColumns
 @dataclass
 class CqcLPIRCleanedColumns(CqcPirColumns):
     cqc_pir_import_date: str = "cqc_pir_import_date"
+    carehome: str = "careHome"
