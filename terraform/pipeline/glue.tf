8419a6a8d18f75bfcdef8544f12045baf11be533
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-02-22 09:30:10 +0000

    added module to glue terraform

--- terraform/pipeline/glue.tf
+++ terraform/pipeline/glue.tf
@@ -447,6 +447,13 @@ module "data_engineering_crawler" {
   workspace_glue_database_name = "${local.workspace_prefix}-${var.glue_database_name}"
 }
 
+module "ind_cqc_filled_posts_crawler" {
+  source                       = "../modules/glue-crawler"
+  dataset_for_crawler          = "ind_cqc_filled_posts"
+  glue_role                    = aws_iam_role.sfc_glue_service_iam_role
+  workspace_glue_database_name = "${local.workspace_prefix}-${var.glue_database_name}"
+}
+
 module "cqc_crawler" {
   source                       = "../modules/glue-crawler"
   dataset_for_crawler          = "CQC"
