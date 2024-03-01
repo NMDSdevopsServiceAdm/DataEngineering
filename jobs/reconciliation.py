5e97f396f6a83565d4c424e739a8f4ddcda45831
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-03-01 18:49:37 +0000

    sorting out ascwds data

--- jobs/reconciliation.py
+++ jobs/reconciliation.py
@@ -9,18 +9,35 @@ from utils.column_names.ind_cqc_pipeline_columns import (
 from utils.column_names.cleaned_data_files.cqc_location_cleaned_values import (
     CqcLocationCleanedColumns as CQCLClean,
 )
+from utils.column_names.cleaned_data_files.ascwds_workplace_cleaned_values import (
+    AscwdsWorkplaceCleanedColumns as AWPClean,
+)
 
 cqcPartitionKeys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
 
 cleaned_cqc_locations_columns_to_import = [
     CQCLClean.cqc_location_import_date,
     CQCLClean.location_id,
-    CQCLClean.registration_status,
     CQCLClean.deregistration_date,
     Keys.year,
     Keys.month,
     Keys.day,
     Keys.import_date,
+]
+cleaned_ascwds_workplace_columns_to_import = [
+    AWPClean.ascwds_workplace_import_date,
+    AWPClean.establishment_id,
+    AWPClean.nmds_id,
+    AWPClean.is_parent,
+    AWPClean.parent_id,
+    AWPClean.parent_permission,
+    AWPClean.establishment_type,
+    AWPClean.registration_type,
+    AWPClean.location_id,
+    AWPClean.main_service_id,
+    AWPClean.establishment_name,
+    AWPClean.master_update_date,
+    AWPClean.region_id,
 ]
 
 
@@ -42,6 +59,9 @@ def main(
     # Filter CQC reg file to latest date?
 
     # get latest? ascwds data
+    ascwds_workforce_df = prepare_latest_cleaned_ascwds_workforce_data(
+        cleaned_ascwds_workplace_source
+    )
 
     # add in all ascwds formatting
 
@@ -54,6 +74,75 @@ def main(
 
     # save single and subs file
     # save parent file
+
+
+def prepare_latest_cleaned_ascwds_workforce_data(
+    cleaned_ascwds_workforce_source: str,
+) -> DataFrame:
+    ascwds_workforce_df = utils.read_from_parquet(cleaned_ascwds_workforce_source)
+
+    latest_ascwds_workforce_df = filter_df_to_maximum_value_in_column(
+        ascwds_workforce_df, AWPClean.ascwds_workplace_import_date
+    )
+
+    # TODO - make better! genuine dict for all ascwds data
+    esttype_dict = {
+        "1": "Local authority (adult services)",
+        "3": "Local authority (generic/other)",
+        "6": "Private sector",
+        "7": "Voluntary/Charity",
+        "8": "Other",
+    }
+    latest_ascwds_workforce_df = latest_ascwds_workforce_df.replace(
+        esttype_dict, subset=[AWPClean.establishment_type]
+    )
+    # TODO - make better! dict specific to this job
+    region_dict = {
+        "4": "B - North East",
+        "2": "C - East Midlands",
+        "7": "D - South West",
+        "8": "E - West Midlands",
+        "5": "F - North West",
+        "3": "G - London",
+        "6": "H - South East",
+        "1": "I - Eastern",
+        "9": "J - Yorkshire Humber",
+    }
+    latest_ascwds_workforce_df = latest_ascwds_workforce_df.replace(
+        region_dict, subset=[AWPClean.region_id]
+    )
+
+    latest_ascwds_workforce_df = latest_ascwds_workforce_df.withColumn(
+        "ParentSubSingle",
+        F.when(
+            (F.col(AWPClean.is_parent) == 1),
+            F.lit("Parent"),
+        )
+        .when(
+            ((F.col(AWPClean.is_parent) == 0) & (F.col(AWPClean.parent_id) > 0)),
+            F.lit("Subsidiary"),
+        )
+        .otherwise(F.lit("Single")),
+    )
+
+    latest_ascwds_workforce_df = latest_ascwds_workforce_df.withColumn(
+        "ownership",
+        F.when(
+            (F.col(AWPClean.parent_permission) == 1),
+            F.lit("parent"),
+        ).otherwise(F.lit("workplace")),
+    ).drop(AWPClean.parent_permission)
+
+    return latest_ascwds_workforce_df
+
+
+# TODO - make utils function?
+def filter_df_to_maximum_value_in_column(
+    df: DataFrame, column_to_filter_on: str
+) -> DataFrame:
+    max_date = df.agg(F.max(column_to_filter_on)).collect()[0][0]
+
+    return df.filter(F.col(column_to_filter_on) == max_date)
 
 
 def get_all_location_ids(cqc_location_source: str) -> DataFrame:
