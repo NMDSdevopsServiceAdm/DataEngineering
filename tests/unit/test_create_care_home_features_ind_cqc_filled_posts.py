adf23503f0f416955eeaf130a730c7c2f64c9096
Roy Price <roy.price@skillsforcare.org.uk>
2024-03-01 16:27:39 +0000

    copied tests from previous test script

--- tests/unit/test_create_care_home_features_ind_cqc_filled_posts.py
+++ tests/unit/test_create_care_home_features_ind_cqc_filled_posts.py
@@ -22,6 +22,14 @@ class CareHomeFeaturesIndCqcFilledPosts(unittest.TestCase):
         )
         warnings.filterwarnings("ignore", category=ResourceWarning)
 
+    def tearDown(self) -> None:
+        try:
+            shutil.rmtree(self.PREPARED_LOCATIONS_TEST_DATA)
+            shutil.rmtree(self.OUTPUT_DESTINATION)
+        except OSError:
+            pass
+        return super().tearDown()
+
     @patch("utils.utils.write_to_parquet")
     @patch("utils.utils.read_from_parquet")
     def test_main(
@@ -41,6 +49,44 @@ class CareHomeFeaturesIndCqcFilledPosts(unittest.TestCase):
             self.CARE_HOME_FEATURES_DIR,
             mode=ANY,
             partitionKeys=["year", "month", "day", "import_date"],
+        )
+
+    def test_main_produces_dataframe_with_features(self):
+        result = job.main(self.PREPARED_LOCATIONS_TEST_DATA, self.OUTPUT_DESTINATION)
+
+        expected_features = SparseVector(
+            43, [8, 11, 12, 13, 42], [1.0, 10.0, 1.0, 1.0, 1.0]
+        )
+        actual_features = result.select(F.col("features")).collect()[0].features
+        self.assertEqual(actual_features, expected_features)
+
+    def test_main_is_filtering_out_rows_missing_data_for_features(self):
+        input_df_length = self.test_df.count()
+        self.assertTrue(input_df_length, 14)
+
+        result = job.main(self.PREPARED_LOCATIONS_TEST_DATA, self.OUTPUT_DESTINATION)
+
+        self.assertTrue(result.count() == 1)
+
+    def test_filter_locations_df_for_independent_care_home_data(self):
+        cols = ["carehome", "cqc_sector"]
+        rows = [
+            ("Y", "Independent"),
+            ("N", "Independent"),
+            ("Y", "local authority"),
+            ("Y", ""),
+            ("Y", None),
+        ]
+
+        df = self.spark.createDataFrame(rows, cols)
+
+        result = job.filter_locations_df_for_independent_care_home_data(
+            df=df, carehome_col_name="carehome", cqc_col_name="cqc_sector"
+        )
+        result_row = result.collect()
+        self.assertTrue(result.count() == 1)
+        self.assertEqual(
+            result_row[0].asDict(), {"carehome": "Y", "cqc_sector": "Independent"}
         )
 
 
