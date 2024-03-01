75fd9eb448e58e7ff888f58c75f88c5a78482f88
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-03-01 17:22:48 +0000

    import cqc data

--- jobs/reconciliation.py
+++ jobs/reconciliation.py
@@ -4,20 +4,42 @@ from utils import utils
 from utils.column_names.ind_cqc_pipeline_columns import (
     PartitionKeys as Keys,
 )
+from utils.column_names.cleaned_data_files.cqc_location_cleaned_values import (
+    CqcLocationCleanedColumns as CQCLClean,
+)
 
 cqcPartitionKeys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
 
+cleaned_cqc_locations_columns_to_import = [
+    CQCLClean.cqc_location_import_date,
+    CQCLClean.location_id,
+    CQCLClean.registration_status,
+    CQCLClean.deregistration_date,
+    Keys.year,
+    Keys.month,
+    Keys.day,
+    Keys.import_date,
+]
+
 
 def main(
+    cqc_location_api_source: str,
     deregistered_cqc_location_source: str,
     cleaned_ascwds_workplace_source: str,
     reconciliation_single_and_subs_destination: str,
     reconciliation_parents_destination: str,
 ):
 
-    # read required CQC cols
+    deregistered_locations_df = utils.read_from_parquet(
+        deregistered_cqc_location_source,
+        selected_columns=cleaned_cqc_locations_columns_to_import,
+    )
 
-    # Filter CQC reg file to latest date
+    # dedup deregistered file
+
+    # Filter CQC reg file to latest date?
+
+    # get latest? ascwds data
 
     # add in all ascwds formatting
 
@@ -34,11 +56,16 @@ if __name__ == "__main__":
     print(f"Job parameters: {sys.argv}")
 
     (
+        cqc_location_api_source,
         deregistered_cqc_location_source,
         cleaned_ascwds_workplace_source,
         reconciliation_single_and_subs_destination,
         reconciliation_parents_destination,
     ) = utils.collect_arguments(
+        (
+            "--cqc_location_api_source",
+            "Source s3 directory for initial CQC location api dataset",
+        ),
         (
             "--deregistered_cqc_location_source",
             "Source s3 directory for deregistered CQC locations dataset",
@@ -57,6 +84,7 @@ if __name__ == "__main__":
         ),
     )
     main(
+        cqc_location_api_source,
         deregistered_cqc_location_source,
         cleaned_ascwds_workplace_source,
         reconciliation_single_and_subs_destination,
