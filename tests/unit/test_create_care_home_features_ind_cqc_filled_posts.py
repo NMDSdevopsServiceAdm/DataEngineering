d845ae9e2332a5cf009156a21c9fb84c9810122b
Roy Price <roy.price@skillsforcare.org.uk>
2024-03-01 14:54:29 +0000

    added test data to test script

--- tests/unit/test_create_care_home_features_ind_cqc_filled_posts.py
+++ tests/unit/test_create_care_home_features_ind_cqc_filled_posts.py
@@ -4,14 +4,22 @@ from unittest.mock import ANY, Mock, patch
 
 import jobs.create_care_home_features_ind_cqc_filled_posts as job
 from utils import utils
+from tests.test_file_generator import generate_prepared_locations_file_parquet
 
 
 class CareHomeFeaturesIndCqcFilledPosts(unittest.TestCase):
     IND_FILLED_POSTS_CLEANED_DIR = "source_dir"
     CARE_HOME_FEATURES_DIR = "destination_dir"
 
+    PREPARED_LOCATIONS_TEST_DATA = (
+        "tests/test_data/domain=data_engineering/dataset=prepared_locations"
+    )
+
     def setUp(self):
         self.spark = utils.get_spark()
+        self.test_df = generate_prepared_locations_file_parquet(
+            self.PREPARED_LOCATIONS_TEST_DATA
+        )
         warnings.filterwarnings("ignore", category=ResourceWarning)
 
     @patch("utils.utils.write_to_parquet")
