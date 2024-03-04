311980fdde321d6dbe74073df917f2061caad53e
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-03-04 16:42:23 +0000

    added current ons date

--- utils/column_names/cleaned_data_files/ons_cleaned_values.py
+++ utils/column_names/cleaned_data_files/ons_cleaned_values.py
@@ -8,5 +8,6 @@ from utils.column_names.raw_data_files.ons_columns import (
 @dataclass
 class OnsCleanedColumns(ONScol):
     ons_import_date: str = "ons_import_date"
+    current_ons_import_date: str = "current_" + ons_import_date
     current: str = "current"
     contemporary: str = "contemporary"
