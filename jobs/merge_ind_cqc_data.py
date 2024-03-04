02f4ad1a439bb19d1acc3963b621be371cd94a8a
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-03-04 17:10:35 +0000

    Add extra columns for testing in branch

--- jobs/merge_ind_cqc_data.py
+++ jobs/merge_ind_cqc_data.py
@@ -1,6 +1,7 @@
 import sys
 import pyspark.sql.functions as F
 from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.types import FloatType, StringType
 
 from utils import utils
 import utils.cleaning_utils as cUtils
@@ -94,6 +95,11 @@ def main(
         CQCLClean.cqc_location_import_date,
         AWPClean.ascwds_workplace_import_date,
     )
+
+    # temporary code to be replaced by job calculator (needed now so later jobs can be tested)
+    ind_cqc_location_df = ind_cqc_location_df.withColumn("job_count", F.lit(None).cast(FloatType()))
+    ind_cqc_location_df = ind_cqc_location_df.withColumn("job_count_source", F.lit(None).cast(StringType()))
+    # end of temporary code
 
     utils.write_to_parquet(
         ind_cqc_location_df,
