96652e77b05a4c7ca44f7f907825591100f25e84
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-03-05 15:42:23 +0000

    Fix tests

--- tests/unit/test_create_care_home_features_ind_cqc_filled_posts.py
+++ tests/unit/test_create_care_home_features_ind_cqc_filled_posts.py
@@ -93,5 +93,41 @@ class CareHomeFeaturesIndCqcFilledPosts(unittest.TestCase):
         self.assertEqual(returned_data, expected_data)
 
 
+    @patch("utils.utils.write_to_parquet")
+    @patch("utils.utils.read_from_parquet")
+    def test_main_produces_dataframe_with_features(
+        self, read_from_parquet_mock: Mock, write_to_parquet_mock: Mock
+    ):
+        read_from_parquet_mock.return_value = self.test_df
+
+        job.main(self.IND_FILLED_POSTS_CLEANED_DIR, self.CARE_HOME_FEATURES_DIR)
+
+        result = write_to_parquet_mock.call_args[0][0].orderBy(
+            F.col(IndCQC.location_id)
+        )
+
+        self.assertTrue(result.filter(F.col("features").isNull()).count() == 0)
+        expected_features = SparseVector(
+            43, [8, 11, 12, 13, 42], [1.0, 10.0, 1.0, 1.0, 1.0]
+        )
+        actual_features = result.select(F.col("features")).collect()[0].features
+        self.assertEqual(actual_features, expected_features)
+
+    @patch("utils.utils.write_to_parquet")
+    @patch("utils.utils.read_from_parquet")
+    def test_main_is_filtering_out_rows_missing_data_for_features(
+        self, read_from_parquet_mock: Mock, write_to_parquet_mock: Mock
+    ):
+        read_from_parquet_mock.return_value = self.test_df
+
+        input_df_length = self.test_df.count()
+        self.assertEqual(input_df_length, 14)
+
+        job.main(self.IND_FILLED_POSTS_CLEANED_DIR, self.CARE_HOME_FEATURES_DIR)
+
+        result = write_to_parquet_mock.call_args[0][0]
+
+        self.assertEqual(result.count(), 1)
+
 if __name__ == "__main__":
     unittest.main(warnings="ignore")
