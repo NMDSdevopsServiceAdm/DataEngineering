b985b4ded2d3ee34dc895b166b405883a08cec8c
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-03-07 16:03:08 +0000

    Format code

--- jobs/clean_ons_data.py
+++ jobs/clean_ons_data.py
@@ -24,9 +24,7 @@ def main(ons_source: str, cleaned_ons_destination: str):
         contemporary_ons_df, Keys.import_date, ONSClean.contemporary_ons_import_date
     )
 
-    refactored_contemporary_ons_df = prepare_contemporary_ons_data(
-        contemporary_ons_df
-    )
+    refactored_contemporary_ons_df = prepare_contemporary_ons_data(contemporary_ons_df)
 
     refactored_current_ons_df = prepare_current_ons_data(contemporary_ons_df)
 
@@ -46,35 +44,33 @@ def prepare_current_ons_data(df: DataFrame) -> DataFrame:
     max_import_date = df.agg(F.max(ONSClean.contemporary_ons_import_date)).collect()[0][
         0
     ]
-    df = df.filter(
-        F.col(ONSClean.contemporary_ons_import_date) == max_import_date
-    )
+    df = df.filter(F.col(ONSClean.contemporary_ons_import_date) == max_import_date)
 
     current_ons_df = df.select(
-            df[ONSClean.postcode],
-            df[ONSClean.contemporary_ons_import_date].alias(
-                ONSClean.current_ons_import_date
-            ),
-            df[ONSClean.cssr].alias(ONSClean.current_cssr),
-            df[ONSClean.region].alias(ONSClean.current_region),
-            df[ONSClean.sub_icb].alias(ONSClean.current_sub_icb),
-            df[ONSClean.icb].alias(ONSClean.current_icb),
-            df[ONSClean.icb_region].alias(ONSClean.current_icb_region),
-            df[ONSClean.ccg].alias(ONSClean.current_ccg),
-            df[ONSClean.latitude].alias(ONSClean.current_latitude),
-            df[ONSClean.longitude].alias(ONSClean.current_longitude),
-            df[ONSClean.imd_score].alias(ONSClean.current_imd_score),
-            df[ONSClean.lower_super_output_area_2011].alias(ONSClean.current_lsoa11),
-            df[ONSClean.middle_super_output_area_2011].alias(ONSClean.current_msoa11),
-            df[ONSClean.rural_urban_indicator_2011].alias(
-                ONSClean.current_rural_urban_ind_11
-            ),
-            df[ONSClean.lower_super_output_area_2021].alias(ONSClean.current_lsoa21),
-            df[ONSClean.middle_super_output_area_2021].alias(ONSClean.current_msoa21),
-            df[ONSClean.westminster_parliamentary_consitituency].alias(
-                ONSClean.current_constituancy
-            ),
-        )
+        df[ONSClean.postcode],
+        df[ONSClean.contemporary_ons_import_date].alias(
+            ONSClean.current_ons_import_date
+        ),
+        df[ONSClean.cssr].alias(ONSClean.current_cssr),
+        df[ONSClean.region].alias(ONSClean.current_region),
+        df[ONSClean.sub_icb].alias(ONSClean.current_sub_icb),
+        df[ONSClean.icb].alias(ONSClean.current_icb),
+        df[ONSClean.icb_region].alias(ONSClean.current_icb_region),
+        df[ONSClean.ccg].alias(ONSClean.current_ccg),
+        df[ONSClean.latitude].alias(ONSClean.current_latitude),
+        df[ONSClean.longitude].alias(ONSClean.current_longitude),
+        df[ONSClean.imd_score].alias(ONSClean.current_imd_score),
+        df[ONSClean.lower_super_output_area_2011].alias(ONSClean.current_lsoa11),
+        df[ONSClean.middle_super_output_area_2011].alias(ONSClean.current_msoa11),
+        df[ONSClean.rural_urban_indicator_2011].alias(
+            ONSClean.current_rural_urban_ind_11
+        ),
+        df[ONSClean.lower_super_output_area_2021].alias(ONSClean.current_lsoa21),
+        df[ONSClean.middle_super_output_area_2021].alias(ONSClean.current_msoa21),
+        df[ONSClean.westminster_parliamentary_consitituency].alias(
+            ONSClean.current_constituancy
+        ),
+    )
 
     return current_ons_df
 
@@ -92,21 +88,13 @@ def prepare_contemporary_ons_data(df: DataFrame) -> DataFrame:
         df[ONSClean.latitude].alias(ONSClean.contemporary_latitude),
         df[ONSClean.longitude].alias(ONSClean.contemporary_longitude),
         df[ONSClean.imd_score].alias(ONSClean.contemporary_imd_score),
-        df[ONSClean.lower_super_output_area_2011].alias(
-            ONSClean.contemporary_lsoa11
-        ),
-        df[ONSClean.middle_super_output_area_2011].alias(
-            ONSClean.contemporary_msoa11
-        ),
+        df[ONSClean.lower_super_output_area_2011].alias(ONSClean.contemporary_lsoa11),
+        df[ONSClean.middle_super_output_area_2011].alias(ONSClean.contemporary_msoa11),
         df[ONSClean.rural_urban_indicator_2011].alias(
             ONSClean.contemporary_rural_urban_ind_11
         ),
-        df[ONSClean.lower_super_output_area_2021].alias(
-            ONSClean.contemporary_lsoa21
-        ),
-        df[ONSClean.middle_super_output_area_2021].alias(
-            ONSClean.contemporary_msoa21
-        ),
+        df[ONSClean.lower_super_output_area_2021].alias(ONSClean.contemporary_lsoa21),
+        df[ONSClean.middle_super_output_area_2021].alias(ONSClean.contemporary_msoa21),
         df[ONSClean.westminster_parliamentary_consitituency].alias(
             ONSClean.contemporary_constituancy
         ),
