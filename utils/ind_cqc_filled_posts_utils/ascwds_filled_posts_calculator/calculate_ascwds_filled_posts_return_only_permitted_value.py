9a4efde9ae9fd3a6144f228e571a8a034907b1c6
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-03-08 16:04:00 +0000

    rewrote only permitted value

--- utils/ind_cqc_filled_posts_utils/ascwds_filled_posts_calculator/calculate_ascwds_filled_posts_return_only_permitted_value.py
+++ utils/ind_cqc_filled_posts_utils/ascwds_filled_posts_calculator/calculate_ascwds_filled_posts_return_only_permitted_value.py
@@ -3,12 +3,12 @@ import pyspark.sql.functions as F
 from utils.ind_cqc_filled_posts_utils.ascwds_filled_posts_calculator.common_checks import (
     ascwds_filled_posts_is_null,
     selected_column_is_null,
-    selected_ascwds_job_count_is_at_least_the_min_permitted,
-    selected_ascwds_job_count_is_below_the_min_permitted,
+    selected_column_is_at_least_the_min_permitted_value,
+    selected_column_is_below_the_min_permitted_value,
 )
 
 
-def calculate_jobcount_select_only_value_which_is_at_least_minimum_job_count_permitted(
+def calculate_ascwds_filled_posts_select_only_value_which_is_at_least_minimum_job_count_permitted(
     input_df, permitted_column: str, non_permitted_column: str, output_column_name
 ):
     return input_df.withColumn(
@@ -16,12 +16,10 @@ def calculate_jobcount_select_only_value_which_is_at_least_minimum_job_count_per
         F.when(
             (
                 ascwds_filled_posts_is_null()
-                & selected_ascwds_job_count_is_at_least_the_min_permitted(
-                    permitted_column
-                )
+                & selected_column_is_at_least_the_min_permitted_value(permitted_column)
                 & (
                     selected_column_is_null(non_permitted_column)
-                    | selected_ascwds_job_count_is_below_the_min_permitted(
+                    | selected_column_is_below_the_min_permitted_value(
                         non_permitted_column
                     )
                 )
