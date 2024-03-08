178b3a7809ec33769c8d446f7557492c86a4d044
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-03-08 15:40:42 +0000

    Copy over old code

--- jobs/estimate_ind_cqc_filled_posts.py
+++ jobs/estimate_ind_cqc_filled_posts.py
@@ -25,7 +25,115 @@ def main(
 
     cleaned_ind_cqc_df = utils.read_from_parquet(cleaned_ind_cqc_source)
 
+    locations_df = (
+        spark.read.parquet(prepared_locations_cleaned_source)
+        .select(
+            LOCATION_ID,
+            SERVICES_OFFERED,
+            PRIMARY_SERVICE_TYPE,
+            PEOPLE_DIRECTLY_EMPLOYED,
+            NUMBER_OF_BEDS,
+            SNAPSHOT_DATE,
+            JOB_COUNT_UNFILTERED,
+            JOB_COUNT_UNFILTERED_SOURCE,
+            JOB_COUNT,
+            LOCAL_AUTHORITY,
+            CQC_SECTOR,
+        )
+        .filter(f"{REGISTRATION_STATUS} = 'Registered'")
+    )
 
+    # loads model features
+    carehome_features_df = spark.read.parquet(carehome_features_source)
+    non_res_features_df = spark.read.parquet(nonres_features_source)
+
+    locations_df = filter_to_only_cqc_independent_sector_data(locations_df)
+
+    locations_df = locations_df.withColumn(
+        ESTIMATE_JOB_COUNT, F.lit(None).cast(IntegerType())
+    )
+    locations_df = locations_df.withColumn(
+        ESTIMATE_JOB_COUNT_SOURCE, F.lit(None).cast(StringType())
+    )
+    latest_snapshot = utils.get_max_snapshot_date(locations_df)
+
+    locations_df = utils.create_unix_timestamp_variable_from_date_column(
+        locations_df,
+        date_col=SNAPSHOT_DATE,
+        date_format="yyyy-MM-dd",
+        new_col_name="unix_time",
+    )
+
+    locations_df = populate_estimate_jobs_when_job_count_known(locations_df)
+
+    locations_df = model_primary_service_rolling_average(
+        locations_df, NUMBER_OF_DAYS_IN_ROLLING_AVERAGE
+    )
+
+    locations_df = model_extrapolation(locations_df)
+
+    # Care homes model
+    locations_df, care_home_metrics_info = model_care_homes(
+        locations_df,
+        carehome_features_df,
+        care_home_model_directory,
+    )
+
+    locations_df = model_interpolation(locations_df)
+
+    care_home_model_info = care_home_model_directory.split("/")
+    write_metrics_df(
+        metrics_destination,
+        r2=care_home_metrics_info["r2"],
+        data_percentage=care_home_metrics_info["data_percentage"],
+        model_version=care_home_model_info[-2],
+        model_name="care_home_with_nursing_historical_jobs_prediction",
+        latest_snapshot=latest_snapshot,
+        job_run_id=job_run_id,
+        job_name=job_name,
+    )
+
+    # Non-res with PIR data model
+    (
+        locations_df,
+        non_residential_with_pir_metrics_info,
+    ) = model_non_residential_with_pir(
+        locations_df,
+        non_res_features_df,
+        non_res_model_directory,
+    )
+
+    non_res_model_info = non_res_model_directory.split("/")
+    write_metrics_df(
+        metrics_destination,
+        r2=non_residential_with_pir_metrics_info["r2"],
+        data_percentage=non_residential_with_pir_metrics_info["data_percentage"],
+        model_version=non_res_model_info[-2],
+        model_name="non_residential_with_pir",
+        latest_snapshot=latest_snapshot,
+        job_run_id=job_run_id,
+        job_name=job_name,
+    )
+
+    locations_df = locations_df.withColumnRenamed(
+        "rolling_average", "rolling_average_model"
+    )
+    locations_df = locations_df.withColumn(
+        ESTIMATE_JOB_COUNT,
+        F.when(
+            F.col(ESTIMATE_JOB_COUNT).isNotNull(), F.col(ESTIMATE_JOB_COUNT)
+        ).otherwise(F.col("rolling_average_model")),
+    )
+    locations_df = update_dataframe_with_identifying_rule(
+        locations_df, "rolling_average_model", ESTIMATE_JOB_COUNT
+    )
+
+    today = date.today()
+    locations_df = locations_df.withColumn("run_year", F.lit(today.year))
+    locations_df = locations_df.withColumn("run_month", F.lit(f"{today.month:0>2}"))
+    locations_df = locations_df.withColumn("run_day", F.lit(f"{today.day:0>2}"))
+
+    print("Completed estimated job counts")
 
     print(f"Exporting as parquet to {estimated_ind_cqc_destination}")
 
@@ -38,7 +146,65 @@ def main(
 
 
 
+def populate_estimate_jobs_when_job_count_known(
+    df: pyspark.sql.DataFrame,
+) -> pyspark.sql.DataFrame:
+    df = df.withColumn(
+        ESTIMATE_JOB_COUNT,
+        F.when(
+            (F.col(ESTIMATE_JOB_COUNT).isNull() & (F.col(JOB_COUNT).isNotNull())),
+            F.col(JOB_COUNT),
+        ).otherwise(F.col(ESTIMATE_JOB_COUNT)),
+    )
 
+    df = update_dataframe_with_identifying_rule(
+        df, "ascwds_job_count", ESTIMATE_JOB_COUNT
+    )
+
+    return df
+
+
+def write_metrics_df(
+    metrics_destination,
+    r2,
+    data_percentage,
+    model_name,
+    model_version,
+    latest_snapshot,
+    job_run_id,
+    job_name,
+):
+    spark = utils.get_spark()
+    columns = [
+        "r2",
+        "percentage_data",
+        "latest_snapshot",
+        "job_run_id",
+        "job_name",
+        "model_name",
+        "model_version",
+    ]
+    row = [
+        (
+            r2,
+            data_percentage,
+            latest_snapshot,
+            job_run_id,
+            job_name,
+            model_name,
+            model_version,
+        )
+    ]
+    df = spark.createDataFrame(row, columns)
+    df = df.withColumn("generated_metric_date", F.current_timestamp())
+
+    print(f"Writing model metrics as parquet to {metrics_destination}")
+    utils.write_to_parquet(
+        df,
+        metrics_destination,
+        mode="append",
+        partitionKeys=["model_name", "model_version"],
+    )
 
 if __name__ == "__main__":
     print("Spark job 'estimate_ind_cqc_filled_posts' starting...")
