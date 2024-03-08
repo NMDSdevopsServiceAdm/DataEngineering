3e2da5b73261e41c312f4b4a701454c3c7cbb514
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-03-08 16:08:57 +0000

    rewrite when equal

--- utils/ind_cqc_filled_posts_utils/ascwds_filled_posts_calculator/calculate_ascwds_filled_posts_return_worker_record_count_if_equal_to_total_staff.py
+++ utils/ind_cqc_filled_posts_utils/ascwds_filled_posts_calculator/calculate_ascwds_filled_posts_return_worker_record_count_if_equal_to_total_staff.py
@@ -2,19 +2,11 @@ import pyspark.sql.functions as F
 
 from utils.ind_cqc_filled_posts_utils.ascwds_filled_posts_calculator.common_checks import (
     ascwds_filled_posts_is_null,
-    selected_ascwds_job_count_is_at_least_the_min_permitted,
+    selected_column_is_at_least_the_min_permitted_value,
 )
 
 
-def two_cols_are_equal_and_at_least_minimum_job_count_permitted(
-    first_col: str, second_col: str
-):
-    return (
-        F.col(first_col) == F.col(second_col)
-    ) & selected_ascwds_job_count_is_at_least_the_min_permitted(first_col)
-
-
-def calculate_jobcount_totalstaff_equal_wkrrecs(
+def calculate_ascwds_filled_posts_totalstaff_equal_wkrrecs(
     input_df, total_staff_column: str, worker_records_column: str, output_column_name
 ):
     return input_df.withColumn(
@@ -22,10 +14,18 @@ def calculate_jobcount_totalstaff_equal_wkrrecs(
         F.when(
             (
                 ascwds_filled_posts_is_null()
-                & two_cols_are_equal_and_at_least_minimum_job_count_permitted(
+                & two_cols_are_equal_and_at_least_minimum_permitted_value(
                     total_staff_column, worker_records_column
                 )
             ),
             F.col(worker_records_column),
         ).otherwise(F.col(output_column_name)),
     )
+
+
+def two_cols_are_equal_and_at_least_minimum_permitted_value(
+    first_col: str, second_col: str
+):
+    return (
+        F.col(first_col) == F.col(second_col)
+    ) & selected_column_is_at_least_the_min_permitted_value(first_col)
