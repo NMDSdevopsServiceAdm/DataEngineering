30459288e0ae23f258f1aaa31b5157cdfe772770
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-03-08 16:18:05 +0000

    rewrite diff within range

--- utils/ind_cqc_filled_posts_utils/ascwds_filled_posts_calculator/calculate_ascwds_filled_posts_absolute_difference_within_range.py
+++ utils/ind_cqc_filled_posts_utils/ascwds_filled_posts_calculator/calculate_ascwds_filled_posts_absolute_difference_within_range.py
@@ -2,20 +2,22 @@ from pyspark.sql import functions as F
 
 from utils.ind_cqc_filled_posts_utils.ascwds_filled_posts_calculator.common_checks import (
     ascwds_filled_posts_is_null,
-    column_value_is_less_than_min_abs_difference_between_total_staff_and_worker_record_count,
-    selected_ascwds_job_count_is_at_least_the_min_permitted,
+    column_value_is_less_than_min_absolute_difference_between_total_staff_and_worker_record_count,
+    selected_column_is_at_least_the_min_permitted_value,
 )
 from utils.ind_cqc_filled_posts_utils.ascwds_filled_posts_calculator.calculation_constants import (
     ASCWDSFilledPostCalculationConstants as calculation_constant,
 )
 
+absolute_difference = "absolute_difference"
 
-def calculate_jobcount_abs_difference_within_range(
+
+def calculate_ascwds_filled_posts_absolute_difference_within_range(
     input_df, total_staff_column: str, worker_records_column: str, output_column_name
 ):
-    # Abs difference between total_staff & worker_record_count < 5 or < 10% take average:
+
     input_df = input_df.withColumn(
-        "abs_difference",
+        absolute_difference,
         F.abs(F.col(total_staff_column) - F.col(worker_records_column)),
     )
 
@@ -24,21 +26,21 @@ def calculate_jobcount_abs_difference_within_range(
         F.when(
             (
                 ascwds_filled_posts_is_null()
-                & selected_ascwds_job_count_is_at_least_the_min_permitted(
+                & selected_column_is_at_least_the_min_permitted_value(
                     total_staff_column
                 )
-                & selected_ascwds_job_count_is_at_least_the_min_permitted(
+                & selected_column_is_at_least_the_min_permitted_value(
                     worker_records_column
                 )
                 & (
-                    column_value_is_less_than_min_abs_difference_between_total_staff_and_worker_record_count(
-                        col_name="abs_difference",
-                        min_abs_diff=calculation_constant().MIN_ABS_DIFFERENCE_BETWEEN_TOTAL_STAFF_AND_WORKER_RECORD_COUNT,
+                    column_value_is_less_than_min_absolute_difference_between_total_staff_and_worker_record_count(
+                        col_name=absolute_difference,
+                        min_abs_diff=calculation_constant.MIN_ABSOLUTE_DIFFERENCE_BETWEEN_TOTAL_STAFF_AND_WORKER_RECORD_COUNT,
                     )
-                    | mean_abs_difference_less_than_min_pct_difference(
-                        abs_dff_col="abs_difference",
+                    | mean_absolute_difference_less_than_min_pct_difference(
+                        abs_dff_col=absolute_difference,
                         comparison_col=total_staff_column,
-                        min_diff_val=calculation_constant().MIN_PERCENTAGE_DIFFERENCE_BETWEEN_TOTAL_STAFF_AND_WORKER_RECORD_COUNT,
+                        min_diff_val=calculation_constant.MIN_PERCENTAGE_DIFFERENCE_BETWEEN_TOTAL_STAFF_AND_WORKER_RECORD_COUNT,
                     )
                 )
             ),
@@ -46,12 +48,12 @@ def calculate_jobcount_abs_difference_within_range(
         ).otherwise(F.col(output_column_name)),
     )
 
-    input_df = input_df.drop("abs_difference")
+    input_df = input_df.drop(absolute_difference)
 
     return input_df
 
 
-def mean_abs_difference_less_than_min_pct_difference(
+def mean_absolute_difference_less_than_min_pct_difference(
     abs_dff_col: str, comparison_col: str, min_diff_val: float
 ):
     return F.col(abs_dff_col) / F.col(comparison_col) < min_diff_val
