7303bd7680ad7c257bc5c2109f37ba1dc21f44f2
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-03-08 11:13:41 +0000

    Call copy of calculate job count

--- jobs/clean_ind_cqc_filled_posts.py
+++ jobs/clean_ind_cqc_filled_posts.py
@@ -4,6 +4,9 @@ import pyspark.sql
 import pyspark.sql.functions as F
 
 from utils import utils
+from utils.job_calculator.job_calculator import (
+    calculate_jobcount,
+)
 from utils.ind_cqc_filled_posts_utils.filter_job_count.filter_job_count import (
     null_job_count_outliers,
 )
@@ -23,6 +26,8 @@ def main(
     print("Cleaning merged_ind_cqc dataset...")
 
     locations_df = utils.read_from_parquet(merged_ind_cqc_source)
+
+    locations_df = calculate_jobcount(locations_df, IndCqcColumns.total_staff, IndCqcColumns.worker_records, IndCqcColumns.job_count)
 
     locations_df = replace_zero_beds_with_null(locations_df)
     locations_df = populate_missing_care_home_number_of_beds(locations_df)
