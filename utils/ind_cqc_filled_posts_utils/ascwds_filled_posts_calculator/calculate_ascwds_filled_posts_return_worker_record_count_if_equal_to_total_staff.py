fa3663d0b48d5bbc1ee56be07afbc2d31ba1eba4
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-03-11 15:25:49 +0000

    function and test for total staff equal worker records

--- utils/ind_cqc_filled_posts_utils/ascwds_filled_posts_calculator/calculate_ascwds_filled_posts_return_worker_record_count_if_equal_to_total_staff.py
+++ utils/ind_cqc_filled_posts_utils/ascwds_filled_posts_calculator/calculate_ascwds_filled_posts_return_worker_record_count_if_equal_to_total_staff.py
@@ -2,10 +2,14 @@ import pyspark.sql.functions as F
 
 from utils.ind_cqc_filled_posts_utils.ascwds_filled_posts_calculator.common_checks import (
     ascwds_filled_posts_is_null,
-    selected_column_is_at_least_the_min_permitted_value,
+    two_cols_are_equal_and_at_least_minimum_permitted_value,
 )
 from utils.ind_cqc_filled_posts_utils.utils import (
     update_dataframe_with_identifying_rule,
+)
+
+ascwds_filled_posts_totalstaff_equal_wkrrecs_source_description: str = (
+    "worker records and total staff were the same"
 )
 
 
@@ -26,13 +30,7 @@ def calculate_ascwds_filled_posts_totalstaff_equal_wkrrecs(
     )
 
     return update_dataframe_with_identifying_rule(
-        input_df, "worker records and total staff were the same", output_column_name
+        input_df,
+        ascwds_filled_posts_totalstaff_equal_wkrrecs_source_description,
+        output_column_name,
     )
-
-
-def two_cols_are_equal_and_at_least_minimum_permitted_value(
-    first_col: str, second_col: str
-):
-    return (
-        F.col(first_col) == F.col(second_col)
-    ) & selected_column_is_at_least_the_min_permitted_value(first_col)
