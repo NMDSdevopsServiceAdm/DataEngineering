b24c1ccab6b2453290397462e5c751e51943398a
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-03-11 15:10:49 +0000

    absolute diff file and tests

---
+++ tests/unit/test_calculate_ascwds_filled_posts_absolute_difference_in_range.py
@@ -0,0 +1,77 @@
+import unittest
+import warnings
+
+from tests.test_file_data import CleanIndCQCData as Data
+from tests.test_file_schemas import CleanIndCQCData as Schemas
+
+from utils import utils
+from utils.column_names.ind_cqc_pipeline_columns import (
+    PartitionKeys as Keys,
+    IndCqcColumns as IndCQC,
+)
+
+import utils.ind_cqc_filled_posts_utils.ascwds_filled_posts_calculator.calculate_ascwds_filled_posts_absolute_difference_within_range as job
+
+
+class TestAscwdsFilledPostsAbsoluteDiffInRange(unittest.TestCase):
+    partition_keys = [
+        Keys.year,
+        Keys.month,
+        Keys.day,
+        Keys.import_date,
+    ]
+
+    def setUp(self):
+        self.spark = utils.get_spark()
+        self.ascwds_total_staff_and_worker_record_df = self.spark.createDataFrame(
+            Data.calculate_ascwds_filled_posts_rows,
+            Schemas.calculate_ascwds_filled_posts_schema,
+        )
+        self.df = job.calculate_ascwds_filled_posts_absolute_difference_within_range(
+            self.ascwds_total_staff_and_worker_record_df,
+            IndCQC.total_staff_bounded,
+            IndCQC.worker_records_bounded,
+            IndCQC.ascwds_filled_posts,
+        )
+        warnings.filterwarnings("ignore", category=ResourceWarning)
+
+    def test_calculate_ascwds_filled_posts_absolute_difference_within_range_values(
+        self,
+    ):
+        self.assertEqual(self.df.count(), 9)
+
+        df = self.df.sort(IndCQC.location_id).collect()
+        self.assertEqual(df[0][IndCQC.ascwds_filled_posts], None)
+        self.assertEqual(df[1][IndCQC.ascwds_filled_posts], 500.0)
+        self.assertEqual(df[2][IndCQC.ascwds_filled_posts], None)
+        self.assertEqual(df[3][IndCQC.ascwds_filled_posts], None)
+        self.assertEqual(df[4][IndCQC.ascwds_filled_posts], None)
+        self.assertEqual(df[5][IndCQC.ascwds_filled_posts], None)
+        self.assertEqual(df[6][IndCQC.ascwds_filled_posts], 11.5)
+        self.assertEqual(df[7][IndCQC.ascwds_filled_posts], 487.5)
+        self.assertEqual(df[8][IndCQC.ascwds_filled_posts], 8.0)
+
+    def test_calculate_ascwds_filled_posts_absolute_difference_within_range_sources(
+        self,
+    ):
+        self.assertEqual(self.df.count(), 9)
+
+        df = self.df.sort(IndCQC.location_id).collect()
+        self.assertEqual(df[0][IndCQC.ascwds_filled_posts_source], None)
+        self.assertEqual(
+            df[1][IndCQC.ascwds_filled_posts_source],
+            job.ascwds_filled_posts_absolute_difference_within_range_source_description,
+        )
+        self.assertEqual(df[2][IndCQC.ascwds_filled_posts_source], None)
+        self.assertEqual(df[3][IndCQC.ascwds_filled_posts_source], None)
+        self.assertEqual(df[4][IndCQC.ascwds_filled_posts_source], None)
+        self.assertEqual(df[5][IndCQC.ascwds_filled_posts_source], None)
+        self.assertEqual(
+            df[6][IndCQC.ascwds_filled_posts_source],
+            job.ascwds_filled_posts_absolute_difference_within_range_source_description,
+        )
+        self.assertEqual(
+            df[7][IndCQC.ascwds_filled_posts_source],
+            job.ascwds_filled_posts_absolute_difference_within_range_source_description,
+        )
+        self.assertEqual(df[8][IndCQC.ascwds_filled_posts_source], "already populated")
--- utils/ind_cqc_filled_posts_utils/ascwds_filled_posts_calculator/calculate_ascwds_filled_posts_absolute_difference_within_range.py
+++ utils/ind_cqc_filled_posts_utils/ascwds_filled_posts_calculator/calculate_ascwds_filled_posts_absolute_difference_within_range.py
@@ -3,6 +3,8 @@ from pyspark.sql import functions as F
 from utils.ind_cqc_filled_posts_utils.ascwds_filled_posts_calculator.common_checks import (
     ascwds_filled_posts_is_null,
     selected_column_is_at_least_the_min_permitted_value,
+    column_value_is_less_than_max_absolute_difference,
+    mean_absolute_difference_less_than_max_pct_difference,
 )
 from utils.ind_cqc_filled_posts_utils.ascwds_filled_posts_calculator.calculation_constants import (
     ASCWDSFilledPostCalculationConstants as calculation_constant,
@@ -11,7 +13,10 @@ from utils.ind_cqc_filled_posts_utils.utils import (
     update_dataframe_with_identifying_rule,
 )
 
-absolute_difference = "absolute_difference"
+absolute_difference: str = "absolute_difference"
+ascwds_filled_posts_absolute_difference_within_range_source_description: str = (
+    "average of total staff and worker records as both were similar"
+)
 
 
 def calculate_ascwds_filled_posts_absolute_difference_within_range(
@@ -36,12 +41,12 @@ def calculate_ascwds_filled_posts_absolute_difference_within_range(
                 & (
                     column_value_is_less_than_max_absolute_difference(
                         col_name=absolute_difference,
-                        min_abs_diff=calculation_constant.MAX_ABSOLUTE_DIFFERENCE_BETWEEN_TOTAL_STAFF_AND_WORKER_RECORD_COUNT,
+                        max_abs_diff=calculation_constant.MAX_ABSOLUTE_DIFFERENCE_BETWEEN_TOTAL_STAFF_AND_WORKER_RECORD_COUNT,
                     )
                     | mean_absolute_difference_less_than_max_pct_difference(
                         abs_dff_col=absolute_difference,
                         comparison_col=total_staff_column,
-                        min_diff_val=calculation_constant.MAX_PERCENTAGE_DIFFERENCE_BETWEEN_TOTAL_STAFF_AND_WORKER_RECORD_COUNT,
+                        max_diff_val=calculation_constant.MAX_PERCENTAGE_DIFFERENCE_BETWEEN_TOTAL_STAFF_AND_WORKER_RECORD_COUNT,
                     )
                 )
             ),
@@ -53,20 +58,8 @@ def calculate_ascwds_filled_posts_absolute_difference_within_range(
 
     input_df = update_dataframe_with_identifying_rule(
         input_df,
-        "average of total staff and worker records as both were similar",
+        ascwds_filled_posts_absolute_difference_within_range_source_description,
         output_column_name,
     )
 
     return input_df
-
-
-def column_value_is_less_than_max_absolute_difference(
-    col_name: str, max_abs_diff: float
-) -> bool:
-    return F.col(col_name) < max_abs_diff
-
-
-def mean_absolute_difference_less_than_max_pct_difference(
-    abs_dff_col: str, comparison_col: str, max_diff_val: float
-):
-    return F.col(abs_dff_col) / F.col(comparison_col) < max_diff_val
