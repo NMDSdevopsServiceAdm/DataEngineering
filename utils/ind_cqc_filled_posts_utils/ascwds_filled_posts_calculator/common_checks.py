5f622abdc7c22dad07a45a5f67afd2084ee645c2
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-03-12 11:39:33 +0000

    cleaned up common checks

--- utils/ind_cqc_filled_posts_utils/ascwds_filled_posts_calculator/common_checks.py
+++ utils/ind_cqc_filled_posts_utils/ascwds_filled_posts_calculator/common_checks.py
@@ -5,48 +5,67 @@ from utils.ind_cqc_filled_posts_utils.ascwds_filled_posts_calculator.calculation
 )
 from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns
 
+absolute_difference: str = "absolute_difference"
 
-def ascwds_filled_posts_is_null():
+
+def ascwds_filled_posts_is_null() -> bool:
     return F.col(IndCqcColumns.ascwds_filled_posts).isNull()
 
 
-def selected_column_is_not_null(col_name: str):
+def selected_column_is_not_null(col_name: str) -> bool:
     return F.col(col_name).isNotNull()
 
 
-def selected_column_is_null(col_name: str):
+def selected_column_is_null(col_name: str) -> bool:
     return F.col(col_name).isNull()
 
 
-def selected_column_is_at_least_the_min_permitted_value(col_name: str):
-    return F.col(col_name).isNotNull() & (
+def selected_column_is_at_least_the_min_permitted_value(col_name: str) -> bool:
+    return selected_column_is_not_null(col_name) & (
         F.col(col_name) >= calculation_constant.MIN_ASCWDS_FILLED_POSTS_PERMITTED
     )
 
 
-def selected_column_is_below_the_min_permitted_value(col_name: str):
+def selected_column_is_below_the_min_permitted_value(col_name: str) -> bool:
     return F.col(col_name) < calculation_constant.MIN_ASCWDS_FILLED_POSTS_PERMITTED
 
 
-def column_value_is_less_than_max_absolute_difference(col_name: str) -> bool:
+def absolute_difference_between_total_staff_and_worker_records_below_cut_off() -> bool:
     return (
-        F.col(col_name)
+        absolute_difference_between_two_columns(
+            IndCqcColumns.total_staff_bounded, IndCqcColumns.worker_records_bounded
+        )
         < calculation_constant.MAX_ABSOLUTE_DIFFERENCE_BETWEEN_TOTAL_STAFF_AND_WORKER_RECORD_COUNT
     )
 
 
-def mean_absolute_difference_less_than_max_pct_difference(
-    abs_dff_col: str, comparison_col: str
+def percentage_difference_between_total_staff_and_worker_records_below_cut_off() -> (
+    bool
 ):
     return (
-        F.col(abs_dff_col) / F.col(comparison_col)
+        (
+            absolute_difference_between_two_columns(
+                IndCqcColumns.total_staff_bounded, IndCqcColumns.worker_records_bounded
+            )
+            / average_of_two_columns(
+                IndCqcColumns.total_staff_bounded, IndCqcColumns.worker_records_bounded
+            )
+        )
         < calculation_constant.MAX_PERCENTAGE_DIFFERENCE_BETWEEN_TOTAL_STAFF_AND_WORKER_RECORD_COUNT
     )
 
 
 def two_cols_are_equal_and_at_least_minimum_permitted_value(
     first_col: str, second_col: str
-):
+) -> bool:
     return (
         F.col(first_col) == F.col(second_col)
     ) & selected_column_is_at_least_the_min_permitted_value(first_col)
+
+
+def absolute_difference_between_two_columns(first_col: str, second_col: str):
+    return F.abs(F.col(first_col) - F.col(second_col))
+
+
+def average_of_two_columns(first_col: str, second_col: str):
+    return (F.col(first_col) + F.col(second_col)) / 2
