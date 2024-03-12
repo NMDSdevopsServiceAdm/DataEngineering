822a1bfea1301c3cc799fc54ce35b89f9d08368d
james-francis-mt <james.francis@madetech.com>
2024-03-12 14:47:01 +0000

    running black linter

--- jobs/estimate_by_job_role.py
+++ jobs/estimate_by_job_role.py
@@ -12,14 +12,16 @@ def main(source: str):
 
 
 if __name__ == "__main__":
-    print('spark job: estimate_by_job_role starting')
-    print(f'job args: {sys.argv}')
+    print("spark job: estimate_by_job_role starting")
+    print(f"job args: {sys.argv}")
 
-    source = next(utils.collect_arguments(
-        (
-            "--estimated_ind_cqc_filled_posts_data",
-            "Source s3 directory for estimated ind cqc filled posts data",
-        ),
-    ))
+    source = next(
+        utils.collect_arguments(
+            (
+                "--estimated_ind_cqc_filled_posts_data",
+                "Source s3 directory for estimated ind cqc filled posts data",
+            ),
+        )
+    )
 
     main(source)
--- tests/unit/test_estimate_by_job_role.py
+++ tests/unit/test_estimate_by_job_role.py
@@ -7,12 +7,12 @@ class BaseSetup(unittest.TestCase):
     def setUp(self) -> None:
         pass
 
+
 class MainTests(BaseSetup):
     @patch("utils.utils.read_from_parquet")
     def test_main_function(self, read_from_parquet_mock: Mock):
-        SOURCE = 'some/file/source'
+        SOURCE = "some/file/source"
 
         job.main(SOURCE)
-        
-        read_from_parquet_mock.assert_called_once_with(SOURCE)
 
+        read_from_parquet_mock.assert_called_once_with(SOURCE)
