06947b2544fef120ab11d6bed1abd256004248c8
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-03-12 15:22:02 +0000

    Revert "Remove scripts that are no longer needed"
    This reverts commit a9cae82aaf9e52e1c0e0304ad0166de33ae7e2df.

---
+++ jobs/estimate_job_counts.py
@@ -0,0 +1,300 @@
+from datetime import date
+import sys
+
+import pyspark.sql
+import pyspark.sql.functions as F
+from pyspark.sql.types import IntegerType, StringType
+from pyspark.sql import SparkSession
+
+from utils import utils
+from utils.estimate_job_count.column_names import (
+    LOCATION_ID,
+    SERVICES_OFFERED,
+    PEOPLE_DIRECTLY_EMPLOYED,
+    NUMBER_OF_BEDS,
+    SNAPSHOT_DATE,
+    JOB_COUNT_UNFILTERED,
+    JOB_COUNT_UNFILTERED_SOURCE,
+    JOB_COUNT,
+    LOCAL_AUTHORITY,
+    REGISTRATION_STATUS,
+    ESTIMATE_JOB_COUNT,
+    ESTIMATE_JOB_COUNT_SOURCE,
+    PRIMARY_SERVICE_TYPE,
+    CQC_SECTOR,
+)
+from utils.estimate_job_count.models.care_homes import model_care_homes
+from utils.estimate_job_count.models.primary_service_rolling_average import (
+    model_primary_service_rolling_average,
+)
+from utils.estimate_job_count.models.extrapolation import model_extrapolation
+from utils.estimate_job_count.models.interpolation import model_interpolation
+from utils.estimate_job_count.models.non_res_with_pir import (
+    model_non_residential_with_pir,
+)
+from utils.prepare_locations_utils.job_calculator.job_calculator import (
+    update_dataframe_with_identifying_rule,
+)
+from utils.estimate_job_count.common_filtering_functions import (
+    filter_to_only_cqc_independent_sector_data,
+)
+
+# Note: using 88 as a proxy for 3 months
+NUMBER_OF_DAYS_IN_ROLLING_AVERAGE = 88
+
+
+def main(
+    prepared_locations_cleaned_source,
+    carehome_features_source,
+    nonres_features_source,
+    destination,
+    care_home_model_directory,
+    non_res_model_directory,
+    metrics_destination,
+    job_run_id,
+    job_name,
+):
+    spark = (
+        SparkSession.builder.appName("sfc_data_engineering_estimate_jobs")
+        .config("spark.sql.broadcastTimeout", 600)
+        .getOrCreate()
+    )
+    print("Estimating job counts")
+
+    locations_df = (
+        spark.read.parquet(prepared_locations_cleaned_source)
+        .select(
+            LOCATION_ID,
+            SERVICES_OFFERED,
+            PRIMARY_SERVICE_TYPE,
+            PEOPLE_DIRECTLY_EMPLOYED,
+            NUMBER_OF_BEDS,
+            SNAPSHOT_DATE,
+            JOB_COUNT_UNFILTERED,
+            JOB_COUNT_UNFILTERED_SOURCE,
+            JOB_COUNT,
+            LOCAL_AUTHORITY,
+            CQC_SECTOR,
+        )
+        .filter(f"{REGISTRATION_STATUS} = 'Registered'")
+    )
+
+    # loads model features
+    carehome_features_df = spark.read.parquet(carehome_features_source)
+    non_res_features_df = spark.read.parquet(nonres_features_source)
+
+    locations_df = filter_to_only_cqc_independent_sector_data(locations_df)
+
+    locations_df = locations_df.withColumn(
+        ESTIMATE_JOB_COUNT, F.lit(None).cast(IntegerType())
+    )
+    locations_df = locations_df.withColumn(
+        ESTIMATE_JOB_COUNT_SOURCE, F.lit(None).cast(StringType())
+    )
+    latest_snapshot = utils.get_max_snapshot_date(locations_df)
+
+    locations_df = utils.create_unix_timestamp_variable_from_date_column(
+        locations_df,
+        date_col=SNAPSHOT_DATE,
+        date_format="yyyy-MM-dd",
+        new_col_name="unix_time",
+    )
+
+    locations_df = populate_estimate_jobs_when_job_count_known(locations_df)
+
+    locations_df = model_primary_service_rolling_average(
+        locations_df, NUMBER_OF_DAYS_IN_ROLLING_AVERAGE
+    )
+
+    locations_df = model_extrapolation(locations_df)
+
+    # Care homes model
+    locations_df, care_home_metrics_info = model_care_homes(
+        locations_df,
+        carehome_features_df,
+        care_home_model_directory,
+    )
+
+    locations_df = model_interpolation(locations_df)
+
+    care_home_model_info = care_home_model_directory.split("/")
+    write_metrics_df(
+        metrics_destination,
+        r2=care_home_metrics_info["r2"],
+        data_percentage=care_home_metrics_info["data_percentage"],
+        model_version=care_home_model_info[-2],
+        model_name="care_home_with_nursing_historical_jobs_prediction",
+        latest_snapshot=latest_snapshot,
+        job_run_id=job_run_id,
+        job_name=job_name,
+    )
+
+    # Non-res with PIR data model
+    (
+        locations_df,
+        non_residential_with_pir_metrics_info,
+    ) = model_non_residential_with_pir(
+        locations_df,
+        non_res_features_df,
+        non_res_model_directory,
+    )
+
+    non_res_model_info = non_res_model_directory.split("/")
+    write_metrics_df(
+        metrics_destination,
+        r2=non_residential_with_pir_metrics_info["r2"],
+        data_percentage=non_residential_with_pir_metrics_info["data_percentage"],
+        model_version=non_res_model_info[-2],
+        model_name="non_residential_with_pir",
+        latest_snapshot=latest_snapshot,
+        job_run_id=job_run_id,
+        job_name=job_name,
+    )
+
+    locations_df = locations_df.withColumnRenamed(
+        "rolling_average", "rolling_average_model"
+    )
+    locations_df = locations_df.withColumn(
+        ESTIMATE_JOB_COUNT,
+        F.when(
+            F.col(ESTIMATE_JOB_COUNT).isNotNull(), F.col(ESTIMATE_JOB_COUNT)
+        ).otherwise(F.col("rolling_average_model")),
+    )
+    locations_df = update_dataframe_with_identifying_rule(
+        locations_df, "rolling_average_model", ESTIMATE_JOB_COUNT
+    )
+
+    today = date.today()
+    locations_df = locations_df.withColumn("run_year", F.lit(today.year))
+    locations_df = locations_df.withColumn("run_month", F.lit(f"{today.month:0>2}"))
+    locations_df = locations_df.withColumn("run_day", F.lit(f"{today.day:0>2}"))
+
+    print("Completed estimated job counts")
+    print(f"Exporting as parquet to {destination}")
+
+    utils.write_to_parquet(
+        locations_df,
+        destination,
+        mode="append",
+        partitionKeys=["run_year", "run_month", "run_day"],
+    )
+
+
+def populate_estimate_jobs_when_job_count_known(
+    df: pyspark.sql.DataFrame,
+) -> pyspark.sql.DataFrame:
+    df = df.withColumn(
+        ESTIMATE_JOB_COUNT,
+        F.when(
+            (F.col(ESTIMATE_JOB_COUNT).isNull() & (F.col(JOB_COUNT).isNotNull())),
+            F.col(JOB_COUNT),
+        ).otherwise(F.col(ESTIMATE_JOB_COUNT)),
+    )
+
+    df = update_dataframe_with_identifying_rule(
+        df, "ascwds_job_count", ESTIMATE_JOB_COUNT
+    )
+
+    return df
+
+
+def write_metrics_df(
+    metrics_destination,
+    r2,
+    data_percentage,
+    model_name,
+    model_version,
+    latest_snapshot,
+    job_run_id,
+    job_name,
+):
+    spark = utils.get_spark()
+    columns = [
+        "r2",
+        "percentage_data",
+        "latest_snapshot",
+        "job_run_id",
+        "job_name",
+        "model_name",
+        "model_version",
+    ]
+    row = [
+        (
+            r2,
+            data_percentage,
+            latest_snapshot,
+            job_run_id,
+            job_name,
+            model_name,
+            model_version,
+        )
+    ]
+    df = spark.createDataFrame(row, columns)
+    df = df.withColumn("generated_metric_date", F.current_timestamp())
+
+    print(f"Writing model metrics as parquet to {metrics_destination}")
+    utils.write_to_parquet(
+        df,
+        metrics_destination,
+        mode="append",
+        partitionKeys=["model_name", "model_version"],
+    )
+
+
+if __name__ == "__main__":
+    print("Spark job 'estimate_job_counts' starting...")
+    print(f"Job parameters: {sys.argv}")
+
+    (
+        prepared_locations_cleaned_source,
+        carehome_features_source,
+        nonres_features_source,
+        destination,
+        care_home_model_directory,
+        non_res_with_pir_model_directory,
+        metrics_destination,
+        JOB_RUN_ID,
+        JOB_NAME,
+    ) = utils.collect_arguments(
+        (
+            "--prepared_locations_cleaned_source",
+            "Source s3 directory for prepared_locations_cleaned",
+        ),
+        (
+            "--carehome_features_source",
+            "Source s3 directory for prepared_locations ML features for care homes",
+        ),
+        (
+            "--nonres_features_source",
+            "Source s3 directory for prepared_locations ML features for non res care homes",
+        ),
+        (
+            "--destination",
+            "A destination directory for outputting cqc locations, if not provided shall default to S3 todays date.",
+        ),
+        (
+            "--care_home_model_directory",
+            "The directory where the care home models are saved",
+        ),
+        (
+            "--non_res_with_pir_model_directory",
+            "The directory where the non residential with PIR data models are saved",
+        ),
+        ("--metrics_destination", "The destination for the R2 metric data"),
+        ("--JOB_RUN_ID", "The Glue job run id"),
+        ("--JOB_NAME", "The Glue job name"),
+    )
+
+    main(
+        prepared_locations_cleaned_source,
+        carehome_features_source,
+        nonres_features_source,
+        destination,
+        care_home_model_directory,
+        non_res_with_pir_model_directory,
+        metrics_destination,
+        JOB_RUN_ID,
+        JOB_NAME,
+    )
+
+    print("Spark job 'estimate_job_counts' complete")
---
+++ tests/unit/test_estimate_job_counts.py
@@ -0,0 +1,210 @@
+import unittest
+import warnings
+from datetime import datetime, date
+import re
+import os
+from unittest.mock import patch
+
+from pyspark.sql import SparkSession
+from pyspark.ml.linalg import Vectors
+
+from tests.test_file_generator import generate_prepared_locations_file_parquet
+from tests.test_helpers import remove_file_path
+import jobs.estimate_job_counts as job
+
+
+class EstimateJobCountTests(unittest.TestCase):
+    CAREHOME_MODEL = (
+        "tests/test_models/care_home_with_nursing_historical_jobs_prediction/1.0.0/"
+    )
+    NON_RES_WITH_PIR_MODEL = (
+        "tests/test_models/non_residential_with_pir_jobs_prediction/1.0.0/"
+    )
+    METRICS_DESTINATION = "tests/test_data/tmp/data_engineering/model_metrics/"
+    PREPARED_LOCATIONS_CLEANED_DIR = "tests/test_data/tmp/prepared_locations_cleaned/"
+    LOCATIONS_FEATURES_DIR = "tests/test_data/tmp/location_features/"
+    DESTINATION = "tests/test_data/tmp/estimated_job_counts/"
+
+    def setUp(self):
+        self.spark = SparkSession.builder.appName(
+            "test_estimate_2021_jobs"
+        ).getOrCreate()
+        warnings.filterwarnings("ignore", category=ResourceWarning)
+        warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+    def tearDown(self):
+        remove_file_path(self.PREPARED_LOCATIONS_CLEANED_DIR)
+        remove_file_path(self.LOCATIONS_FEATURES_DIR)
+        remove_file_path(self.DESTINATION)
+        remove_file_path(self.METRICS_DESTINATION)
+
+    @patch("utils.utils.get_s3_sub_folders_for_path")
+    @patch("jobs.estimate_job_counts.date")
+    def test_main_partitions_data_based_on_todays_date(
+        self, mock_date, mock_get_s3_folders
+    ):
+        mock_get_s3_folders.return_value = ["1.0.0"]
+        mock_date.today.return_value = date(2022, 6, 29)
+        mock_date.side_effect = lambda *args, **kw: date(*args, **kw)
+        generate_prepared_locations_file_parquet(self.PREPARED_LOCATIONS_CLEANED_DIR)
+        features = self.generate_features_df()
+        features.write.mode("overwrite").partitionBy(
+            "snapshot_year", "snapshot_month", "snapshot_day"
+        ).parquet(self.LOCATIONS_FEATURES_DIR)
+
+        job.main(
+            self.PREPARED_LOCATIONS_CLEANED_DIR,
+            self.LOCATIONS_FEATURES_DIR,
+            self.LOCATIONS_FEATURES_DIR,
+            self.DESTINATION,
+            self.CAREHOME_MODEL,
+            self.NON_RES_WITH_PIR_MODEL,
+            self.METRICS_DESTINATION,
+            job_run_id="abc1234",
+            job_name="estimate_job_counts",
+        )
+
+        first_partitions = os.listdir(self.DESTINATION)
+        year_partition = next(
+            re.match("^run_year=([0-9]{4})$", path)
+            for path in first_partitions
+            if re.match("^run_year=([0-9]{4})$", path)
+        )
+
+        self.assertIsNotNone(year_partition)
+        self.assertEqual(year_partition.groups()[0], "2022")
+
+        second_partitions = os.listdir(f"{self.DESTINATION}/{year_partition.string}/")
+        month_partition = next(
+            re.match("^run_month=([0-9]{2})$", path) for path in second_partitions
+        )
+        self.assertIsNotNone(month_partition)
+        self.assertEqual(month_partition.groups()[0], "06")
+
+        third_partitions = os.listdir(
+            f"{self.DESTINATION}/{year_partition.string}/{month_partition.string}/"
+        )
+        day_partition = next(
+            re.match("^run_day=([0-9]{2})$", path) for path in third_partitions
+        )
+        self.assertIsNotNone(day_partition)
+        self.assertEqual(day_partition.groups()[0], "29")
+
+    def test_populate_known_jobs_use_job_count_from_current_snapshot(self):
+        columns = [
+            "locationid",
+            "job_count",
+            "snapshot_date",
+            "estimate_job_count",
+            "estimate_job_count_source",
+        ]
+        rows = [
+            ("1-000000001", 1, "2022-03-04", None, None),
+            ("1-000000002", None, "2022-03-04", None, None),
+            ("1-000000003", 5, "2022-03-04", 4, "already_populated"),
+            ("1-000000004", 10, "2022-03-04", None, None),
+            ("1-000000002", 7, "2022-02-04", None, None),
+        ]
+        df = self.spark.createDataFrame(rows, columns)
+
+        df = job.populate_estimate_jobs_when_job_count_known(df)
+        self.assertEqual(df.count(), 5)
+
+        df = df.collect()
+        self.assertEqual(df[0]["estimate_job_count"], 1)
+        self.assertEqual(df[0]["estimate_job_count_source"], "ascwds_job_count")
+        self.assertEqual(df[1]["estimate_job_count"], None)
+        self.assertEqual(df[1]["estimate_job_count_source"], None)
+        self.assertEqual(df[2]["estimate_job_count"], 4)
+        self.assertEqual(df[2]["estimate_job_count_source"], "already_populated")
+        self.assertEqual(df[3]["estimate_job_count"], 10)
+
+    def generate_features_df(self):
+        # fmt: off
+        feature_columns = ["locationid", "primary_service_type", "job_count", "carehome", "ons_region", "number_of_beds", "snapshot_date", "care_home_features", "non_residential_inc_pir_features", "people_directly_employed", "snapshot_year", "snapshot_month", "snapshot_day"]
+
+        feature_rows = [
+            ("1-000000001", "Care home with nursing", 10, "Y", "South West", 67, "2022-03-29", Vectors.sparse(46, {0: 1.0, 1: 60.0, 3: 1.0, 32: 97.0, 33: 1.0}), None, 34, "2021", "05", "05"),
+            ("1-000000002", "non-residential", 10, "N", "Merseyside", 12, "2022-03-29", None, Vectors.sparse(211, {0: 1.0, 1: 60.0, 3: 1.0, 32: 97.0, 33: 1.0}), 45, "2021", "05", "05"),
+            ("1-000000003", "Care home with nursing", 20, "N", "Merseyside", 34, "2022-03-29", None, None, 0, "2021", "05", "05"),
+            ("1-000000004", "non-residential", 10, "N", None, 0, "2022-03-29", None, None, None, "2021", "05", "05"),
+        ]
+        # fmt: on
+        return self.spark.createDataFrame(
+            feature_rows,
+            schema=feature_columns,
+        )
+
+    def generate_predictions_df(self):
+        # fmt: off
+        columns = ["locationid", "primary_service_type", "job_count", "carehome", "ons_region", "number_of_beds", "snapshot_date", "prediction"]
+
+        rows = [
+            ("1-000000001", "Care home with nursing", 50, "Y", "South West", 67, "2022-03-29", 56.89),
+            ("1-000000004", "non-residential", 10, "N", None, 0, "2022-03-29", 12.34),
+        ]
+        # fmt: on
+        return self.spark.createDataFrame(
+            rows,
+            schema=columns,
+        )
+
+    def generate_locations_df(self):
+        # fmt: off
+        columns = [
+            "locationid",
+            "primary_service_type",
+            "estimate_job_count",
+            "estimate_job_count_source",
+            "carehome",
+            "ons_region",
+            "number_of_beds",
+            "snapshot_date"
+        ]
+        rows = [
+            ("1-000000001", "Care home with nursing", None, None, "Y", "South West", 67, "2022-03-29"),
+            ("1-000000002", "Care home without nursing", None, None, "N", "Merseyside", 12, "2022-03-29"),
+            ("1-000000003", "Care home with nursing", None, None, None, "Merseyside", 34, "2022-03-29"),
+            ("1-000000004", "non-residential", 10, "already_populated", "N", None, 0, "2022-03-29"),
+            ("1-000000001", "non-residential", None, None, "N", None, 0, "2022-02-20"),
+        ]
+        # fmt: on
+        return self.spark.createDataFrame(rows, columns)
+
+    def test_write_metrics_df_creates_metrics_df(self):
+        job.write_metrics_df(
+            metrics_destination=self.METRICS_DESTINATION,
+            r2=0.99,
+            data_percentage=50.0,
+            model_version="1.0.0",
+            model_name="care_home_jobs_prediction",
+            latest_snapshot="20220601",
+            job_run_id="abc1234",
+            job_name="estimate_job_counts",
+        )
+        df = self.spark.read.parquet(self.METRICS_DESTINATION)
+        expected_columns = [
+            "r2",
+            "percentage_data",
+            "latest_snapshot",
+            "job_run_id",
+            "job_name",
+            "generated_metric_date",
+            "model_name",
+            "model_version",
+        ]
+
+        self.assertEqual(expected_columns, df.columns)
+        self.assertAlmostEqual(df.first()["r2"], 0.99, places=2)
+        self.assertEqual(df.first()["model_version"], "1.0.0")
+        self.assertEqual(df.first()["model_name"], "care_home_jobs_prediction")
+        self.assertEqual(df.first()["latest_snapshot"], "20220601")
+        self.assertEqual(df.first()["job_name"], "estimate_job_counts")
+        self.assertIsInstance(df.first()["generated_metric_date"], datetime)
+
+    def test_number_of_days_constant_is_eighty_eight(self):
+        self.assertEqual(job.NUMBER_OF_DAYS_IN_ROLLING_AVERAGE, 88)
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
