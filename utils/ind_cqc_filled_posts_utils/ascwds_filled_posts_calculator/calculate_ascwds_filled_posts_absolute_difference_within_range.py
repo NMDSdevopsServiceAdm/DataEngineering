b08056d6024129a4eeced6ea390bd2635cc94a0c
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-03-12 12:22:16 +0000

    refactor based on common check changes

--- utils/ind_cqc_filled_posts_utils/ascwds_filled_posts_calculator/calculate_ascwds_filled_posts_absolute_difference_within_range.py
+++ utils/ind_cqc_filled_posts_utils/ascwds_filled_posts_calculator/calculate_ascwds_filled_posts_absolute_difference_within_range.py
@@ -3,14 +3,14 @@ from pyspark.sql import functions as F
 from utils.ind_cqc_filled_posts_utils.ascwds_filled_posts_calculator.common_checks import (
     ascwds_filled_posts_is_null,
     selected_column_is_at_least_the_min_permitted_value,
-    column_value_is_less_than_max_absolute_difference,
-    mean_absolute_difference_less_than_max_pct_difference,
+    absolute_difference_between_total_staff_and_worker_records_below_cut_off,
+    percentage_difference_between_total_staff_and_worker_records_below_cut_off,
+    average_of_two_columns,
 )
 from utils.ind_cqc_filled_posts_utils.utils import (
     update_dataframe_with_identifying_rule,
 )
 
-absolute_difference: str = "absolute_difference"
 ascwds_filled_posts_absolute_difference_within_range_source_description: str = (
     "average of total staff and worker records as both were similar"
 )
@@ -19,11 +19,6 @@ ascwds_filled_posts_absolute_difference_within_range_source_description: str = (
 def calculate_ascwds_filled_posts_absolute_difference_within_range(
     input_df, total_staff_column: str, worker_records_column: str, output_column_name
 ):
-    input_df = input_df.withColumn(
-        absolute_difference,
-        F.abs(F.col(total_staff_column) - F.col(worker_records_column)),
-    )
-
     input_df = input_df.withColumn(
         output_column_name,
         F.when(
@@ -36,25 +31,16 @@ def calculate_ascwds_filled_posts_absolute_difference_within_range(
                     worker_records_column
                 )
                 & (
-                    column_value_is_less_than_max_absolute_difference(
-                        col_name=absolute_difference,
-                    )
-                    | mean_absolute_difference_less_than_max_pct_difference(
-                        abs_dff_col=absolute_difference,
-                        comparison_col=total_staff_column,
-                    )
+                    absolute_difference_between_total_staff_and_worker_records_below_cut_off()
+                    | percentage_difference_between_total_staff_and_worker_records_below_cut_off()
                 )
             ),
-            (F.col(total_staff_column) + F.col(worker_records_column)) / 2,
+            average_of_two_columns(total_staff_column, worker_records_column),
         ).otherwise(F.col(output_column_name)),
     )
 
-    input_df = input_df.drop(absolute_difference)
-
-    input_df = update_dataframe_with_identifying_rule(
+    return update_dataframe_with_identifying_rule(
         input_df,
         ascwds_filled_posts_absolute_difference_within_range_source_description,
         output_column_name,
     )
-
-    return input_df
