c1b68b3a7aab3a9701b0aa34df07c354ab3a6ce3
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-03-18 11:51:38 +0000

    remove timing functions

--- utils/cleaning_utils.py
+++ utils/cleaning_utils.py
@@ -5,19 +5,6 @@ from pyspark.sql import (
 
 import pyspark.sql.functions as F
 
-import time
-
-
-def measure_execution_time(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        print(f"Execution time of {func.__name__}: {end_time - start_time:.2f} seconds")
-        return result
-
-    return wrapper
-
 
 key: str = "key"
 value: str = "value"
@@ -104,7 +91,6 @@ def set_bounds_for_columns(
     return df
 
 
-@measure_execution_time
 def column_to_date(
     df: DataFrame,
     column_to_format: str,
