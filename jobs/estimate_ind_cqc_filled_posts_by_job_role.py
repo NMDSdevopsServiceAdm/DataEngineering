c9eaf6d945cb733fad7437f03ff64ecbe53664e2
james-francis-mt <james.francis@madetech.com>
2024-03-19 11:13:27 +0000

    also use updated args in main function

--- jobs/estimate_ind_cqc_filled_posts_by_job_role.py
+++ jobs/estimate_ind_cqc_filled_posts_by_job_role.py
@@ -6,17 +6,25 @@ from utils.column_names.ind_cqc_pipeline_columns import PartitionKeys as Keys
 PartitionKeys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
 
 
-def main(source: str, output_dir: str):
+def main(
+    estimated_ind_cqc_filled_posts_source: str,
+    estimated_ind_cqc_filled_posts_by_job_role_destination: str,
+):
     """creates job role estimates
 
     Args:
         source: path to the estimates ind cqc filled posts data
         output_dir: path to the output directory
     """
-    df_estimated_ind_cqc_filled_posts_data = utils.read_from_parquet(source)
+    df_estimated_ind_cqc_filled_posts_data = utils.read_from_parquet(
+        estimated_ind_cqc_filled_posts_source
+    )
 
     utils.write_to_parquet(
-        df_estimated_ind_cqc_filled_posts_data, output_dir, "overwrite", PartitionKeys
+        df_estimated_ind_cqc_filled_posts_data,
+        estimated_ind_cqc_filled_posts_by_job_role_destination,
+        "overwrite",
+        PartitionKeys,
     )
 
 
