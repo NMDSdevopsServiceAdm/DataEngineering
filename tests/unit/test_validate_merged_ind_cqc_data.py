bb970011290522bf6f71afeaf7a038a2da7de767
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-03-21 15:53:52 +0000

    Setup test script

---
+++ tests/unit/test_validate_merged_ind_cqc_data.py
@@ -0,0 +1,67 @@
+import unittest
+
+from unittest.mock import ANY, Mock, patch
+
+import jobs.validate_merged_ind_cqc_data as job
+
+from tests.test_file_data import ValidateMergedIndCqcData as Data
+from tests.test_file_schemas import ValidateMergedIndCqcData as Schemas
+
+from utils import utils
+from utils.column_names.ind_cqc_pipeline_columns import (
+    PartitionKeys as Keys,
+)
+from utils.column_names.cleaned_data_files.cqc_location_cleaned_values import (
+    CqcLocationCleanedColumns as CQCLClean,
+)
+from utils.column_names.ind_cqc_pipeline_columns import (
+    IndCqcColumns as IndCqc,
+)
+
+
+class ValidateMergedIndCQCDatasetTests(unittest.TestCase):
+    TEST_CQC_LOCATION_SOURCE = "some/directory"
+    TEST_MERGED_IND_CQC_SOURCE = "some/other/directory"
+    TEST_DESTINATION = "some/other/other/directory"
+    partition_keys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
+
+    def setUp(self) -> None:
+        self.spark = utils.get_spark()
+        self.test_clean_cqc_location_df = self.spark.createDataFrame(
+            Data.cqc_locations_rows,
+            Schemas.cqc_locations_schema,
+        )
+        self.test_merged_ind_cqc_df = self.spark.createDataFrame(
+            Data.merged_ind_cqc_rows, Schemas.merged_ind_cqc_schema
+        )
+
+    @patch("utils.utils.write_to_parquet")
+    @patch("utils.utils.read_from_parquet")
+    def test_main_runs(
+        self,
+        read_from_parquet_patch: Mock,
+        write_to_parquet_patch: Mock,
+    ):
+        read_from_parquet_patch.side_effect = [
+            self.test_clean_cqc_location_df,
+            self.test_merged_ind_cqc_df,
+        ]
+
+        job.main(
+            self.TEST_CQC_LOCATION_SOURCE,
+            self.TEST_MERGED_IND_CQC_SOURCE,
+            self.TEST_DESTINATION,
+        )
+
+        self.assertEqual(read_from_parquet_patch.call_count, 2)
+
+        write_to_parquet_patch.assert_called_once_with(
+            ANY,
+            self.TEST_DESTINATION,
+            mode="overwrite",
+            partitionKeys=self.partition_keys,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
