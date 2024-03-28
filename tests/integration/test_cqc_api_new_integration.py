c326190d078f9aee1053664ec4fe3359d19cbc5e
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-03-28 10:35:41 +0000

    Create Provider API integration tests

--- tests/integration/test_cqc_api_new_integration.py
+++ tests/integration/test_cqc_api_new_integration.py
@@ -65,5 +65,47 @@ class LocationApiTests(CqcApiIntegrationTests):
             self.assertIsNotNone(location[CQCL.provider_id])
 
 
+class ProviderApiTests(CqcApiIntegrationTests):
+    def setUp(self) -> None:
+        super().setUp()
+        self.object_type = "providers"
+        self.example_object = [
+            {CQCP.location_ids: ["1-10000792582"]},
+            {CQCP.provider_id: "1-9098203603"},
+            {CQCP.organisation_type: "Provider"},
+        ]
+
+    def test_get_object_returns_provider(self):
+        result = cqc.get_object(EXAMPLE_LOCATION_ID, self.object_type)
+
+        self.assertEqual(
+            result[CQCP.location_ids], self.example_object[CQCP.location_ids]
+        )
+        self.assertEqual(
+            result[CQCP.provider_id], self.example_object[CQCP.provider_id]
+        )
+        self.assertEqual(
+            result[CQCP.organisation_type], self.example_object[CQCP.organisation_type]
+        )
+
+    def test_get_page_providers_returns_all_providers_for_page(self):
+        url = f"{cqc.CQC_API_BASE_URL}/public/{cqc.CQC_API_VERSION}/{self.object_type}"
+
+        providers = cqc.get_page_objects(
+            url,
+            self.page,
+            self.object_type,
+            CQCP.provider_id,
+            self.PARTNER_CODE_STUB,
+            per_page=5,
+        )
+        self.assertEqual(len(providers), 5)
+
+        regex = re.compile(LOCATION_ID_REGEX)
+        for provider in providers:
+            self.assertTrue(regex.match(provider[CQCP.provider_id]))
+            self.assertIsNotNone(provider[CQCP.location_ids])
+
+
 if __name__ == "__main__":
     unittest.main()
