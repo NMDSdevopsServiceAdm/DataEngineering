ea6095e9e529f812fd92170a3d7de2178e0a046d
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-04-05 09:11:35 +0100

    setup

---
+++ jobs/reconciliation.py
@@ -0,0 +1,83 @@
+import sys
+
+from utils import utils
+from utils.column_names.cleaned_data_files.cqc_location_cleaned_values import (
+    CqcLocationCleanedColumns as CQCLClean,
+)
+from utils.column_names.cleaned_data_files.ascwds_workplace_cleaned_values import (
+    AscwdsWorkplaceCleanedColumns as AWPClean,
+)
+
+cqc_locations_columns_to_import = [
+    CQCLClean.import_date,
+    CQCLClean.location_id,
+    CQCLClean.registration_status,
+    CQCLClean.deregistration_date,
+]
+cleaned_ascwds_workplace_columns_to_import = [
+    AWPClean.ascwds_workplace_import_date,
+    AWPClean.establishment_id,
+    AWPClean.nmds_id,
+    AWPClean.is_parent,
+    AWPClean.parent_id,
+    AWPClean.organisation_id,
+    AWPClean.parent_permission,
+    AWPClean.establishment_type,
+    AWPClean.registration_type,
+    AWPClean.location_id,
+    AWPClean.main_service_id,
+    AWPClean.establishment_name,
+    AWPClean.region_id,
+]
+
+
+def main(
+    cqc_location_api_source: str,
+    cleaned_ascwds_workplace_source: str,
+    reconciliation_single_and_subs_destination: str,
+    reconciliation_parents_destination: str,
+):
+    cqc_location_df = utils.read_from_parquet(
+        cqc_location_api_source, cqc_locations_columns_to_import
+    )
+    ascwds_workplace_df = utils.read_from_parquet(
+        cleaned_ascwds_workplace_source,
+        selected_columns=cleaned_ascwds_workplace_columns_to_import,
+    )
+
+
+if __name__ == "__main__":
+    print("Spark job 'reconciliation' starting...")
+    print(f"Job parameters: {sys.argv}")
+
+    (
+        cqc_location_api_source,
+        cleaned_ascwds_workplace_source,
+        reconciliation_single_and_subs_destination,
+        reconciliation_parents_destination,
+    ) = utils.collect_arguments(
+        (
+            "--cqc_location_api_source",
+            "Source s3 directory for initial CQC location api dataset",
+        ),
+        (
+            "--cleaned_ascwds_workplace_source",
+            "Source s3 directory for cleaned parquet ASCWDS workplace dataset",
+        ),
+        (
+            "--reconciliation_single_and_subs_destination",
+            "Destination s3 directory for reconciliation parquet singles and subs dataset",
+        ),
+        (
+            "--reconciliation_parents_destination",
+            "Destination s3 directory for reconciliation parquet parents dataset",
+        ),
+    )
+    main(
+        cqc_location_api_source,
+        cleaned_ascwds_workplace_source,
+        reconciliation_single_and_subs_destination,
+        reconciliation_parents_destination,
+    )
+
+    print("Spark job 'reconciliation' complete")
