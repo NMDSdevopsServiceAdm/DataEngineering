1f8b8243d32ecc79ac89818f48e0457a61ab9a92
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-04-08 14:51:58 +0100

    actually added col names

--- utils/column_names/cleaned_data_files/ascwds_workplace_cleaned_values.py
+++ utils/column_names/cleaned_data_files/ascwds_workplace_cleaned_values.py
@@ -9,5 +9,9 @@ from utils.column_names.raw_data_files.ascwds_workplace_columns import (
 class AscwdsWorkplaceCleanedColumns(AWP):
     ascwds_workplace_import_date: str = "ascwds_workplace_import_date"
     last_logged_in_date: str = "last_logged_in_date"
+    data_purge_date: str = "data_purge_date"
+    coverage_purge_date: str = "coverage_purge_date"
+    master_update_date_org: str = "master_update_date_org"
+    remove_if_before_this_date: str = "remove_if_before_this_date"
     total_staff_bounded: str = AWP.total_staff + "_bounded"
     worker_records_bounded: str = AWP.worker_records + "_bounded"
