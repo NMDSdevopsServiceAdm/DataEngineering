880cebced85928b8355ee3db7f7966bbf101038b
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-04-08 10:58:14 +0100

    remove unused col reference

--- utils/column_names/cleaned_data_files/ascwds_workplace_cleaned_values.py
+++ utils/column_names/cleaned_data_files/ascwds_workplace_cleaned_values.py
@@ -8,7 +8,6 @@ from utils.column_names.raw_data_files.ascwds_workplace_columns import (
 @dataclass
 class AscwdsWorkplaceCleanedColumns(AWP):
     ascwds_workplace_import_date: str = "ascwds_workplace_import_date"
-    purge_data: str = "purge_data"
     last_logged_in_date: str = "last_logged_in_date"
     total_staff_bounded: str = AWP.total_staff + "_bounded"
     worker_records_bounded: str = AWP.worker_records + "_bounded"
