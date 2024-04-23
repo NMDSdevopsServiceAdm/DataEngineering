917e8caf0f52de21a8a314c036380016e76e7b9a
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-04-23 12:25:43 +0100

    Setup cron test for main

--- terraform/pipeline/eventbridge.tf
+++ terraform/pipeline/eventbridge.tf
@@ -107,7 +107,7 @@ resource "aws_scheduler_schedule" "bulk_download_cqc_api_schedule" {
     mode = "OFF"
   }
 
-  schedule_expression = "cron(30 01 01,08,15,23 * ? *)"
+  schedule_expression = "cron(30 01 01,08,15,23, 24 * ? *)" # 24th added for a test run in main tonight - remove this and cron in glue jobs once successful
 
   target {
     arn      = aws_sfn_state_machine.bulk-download-cqc-api-state-machine.arn
