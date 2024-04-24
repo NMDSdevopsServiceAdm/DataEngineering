02dc196da81f2ee0ad17c03914241a7f57c5cc67
Roy Price <roy.price@skillsforcare.org.uk>
2024-04-24 15:48:47 +0100

    added arguments to main function

--- utils/direct_payments_utils/estimate_direct_payments/split_pa_filled_posts_into_icb_areas.py
+++ utils/direct_payments_utils/estimate_direct_payments/split_pa_filled_posts_into_icb_areas.py
@@ -1,3 +1,32 @@
 from pyspark.sql import DataFrame
 
 from utils import utils
+
+
+def main(sourcepostcode_directory_source, pa_filled_posts_source, destination): ...
+
+
+if __name__ == "__main__":
+    (
+        postcode_directory_source,
+        pa_filled_posts_source,
+        destination,
+    ) = utils.collect_arguments(
+        (
+            "--postcode_directory_source",
+            "Source s3 directory for cleaned ons postcode directory",
+        )(
+            "--pa_filled_posts_source",
+            "Source s3 directory for estimated pa filled posts split by la area dataset",
+        ),
+        (
+            "--destination",
+            "A destination directory for outputting pa filled posts split by icb area data.",
+        ),
+    )
+
+    main(
+        postcode_directory_source,
+        pa_filled_posts_source,
+        destination,
+    )
