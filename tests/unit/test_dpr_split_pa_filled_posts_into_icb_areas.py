137282357ab32a57cf2250b68813564ca0bdd321
Roy Price <roy.price@skillsforcare.org.uk>
2024-04-24 16:49:23 +0100

    started to write test for creating a dataframe from the cleaned ons postcode directory function. Dont know how to do it yet, just wrote a note

--- tests/unit/test_dpr_split_pa_filled_posts_into_icb_areas.py
+++ tests/unit/test_dpr_split_pa_filled_posts_into_icb_areas.py
@@ -1,22 +1,15 @@
 import unittest
 import warnings
 
-from pyspark.sql.types import (
-    StructType,
-    StructField,
-    StringType,
-    IntegerType,
-    FloatType,
-)
 
 from utils import utils
 from tests.test_file_data import CreateListFromRowsOfICBs as TestData
 from tests.test_file_schemas import CreateListFromRowsOfICBs as TestSchema
 
+import utils.direct_payments_utils.estimate_direct_payments.split_pa_filled_posts_into_icb_areas as job
+
 
 class SplitPAFilledPostsIntoICBAreas(unittest.TestCase):
-    TEST_SOURCE = "some/other/directory"
-    TEST_DESTINATION = "some/other/directory"
 
     def setUp(self) -> None:
         self.spark = utils.get_spark()
@@ -26,3 +19,12 @@ class SplitPAFilledPostsIntoICBAreas(unittest.TestCase):
         self.test_location_df = self.spark.createDataFrame(
             TestData.expected_rows, TestSchema.expected_schema
         )
+
+
+class CreatePostCodeDirectoryDfTests(SplitPAFilledPostsIntoICBAreas):
+    def setUp(self) -> None:
+        super().setUp()
+
+    def test_create_postcode_directory_df(self):
+        # need to test that the dataframe only has 4 columns and those four columns are import date, postcode, cssr and icb.
+        
