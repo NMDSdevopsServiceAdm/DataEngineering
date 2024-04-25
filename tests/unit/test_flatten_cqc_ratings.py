d1a65c3130720885920fa7054d57e45c6bbf48e0
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-04-25 13:11:47 +0100

    Refactor test data

--- tests/unit/test_flatten_cqc_ratings.py
+++ tests/unit/test_flatten_cqc_ratings.py
@@ -6,18 +6,27 @@ from utils import utils
 
 import jobs.flatten_cqc_ratings as job
 
-from tests.test_file_data import CQCLocationsData as Data
-from tests.test_file_schemas import CQCLocationsSchema as Schema
+from tests.test_file_data import FlattenCQCRatings as Data
+from tests.test_file_schemas import FlattenCQCRatings as Schema
 
 
 class CleanCQCProviderDatasetTests(unittest.TestCase):
-    TEST_SOURCE = "some/directory"
-    TEST_DESTINATION = "some/other/directory"
+    TEST_LOCATIONS_SOURCE = "some/directory"
+    TEST_PROVIDERS_SOURCE = "some/directory"
+    TEST_WORKPLACE_SOURCE = "some/directory"
+    TEST_CQC_RATINGS_DESTINATION = "some/other/directory"
+    TEST_BENCHMARK_RATINGS_DESTINATION = "some/other/directory"
 
     def setUp(self) -> None:
         self.spark = utils.get_spark()
         self.test_cqc_locations_df = self.spark.createDataFrame(
-            Data.sample_rows_full, schema=Schema.full_schema
+            Data.test_cqc_locations_rows, schema=Schema.test_cqc_locations_schema
+        )
+        self.test_cqc_providers_df = self.spark.createDataFrame(
+            Data.test_cqc_providers_rows, schema=Schema.test_cqc_providers_schema
+        )
+        self.test_ascwds_df = self.spark.createDataFrame(
+            Data.test_ascwds_workplace_rows, schema=Schema.test_ascwds_workplace_schema
         )
 
 
@@ -28,11 +37,15 @@ class MainTests(CleanCQCProviderDatasetTests):
     @patch("utils.utils.write_to_parquet")
     @patch("utils.utils.read_from_parquet")
     def test_main(self, read_from_parquet_patch: Mock, write_to_parquet_patch: Mock):
-        read_from_parquet_patch.return_value = self.test_cqc_locations_df
-        job.main(self.TEST_SOURCE, self.TEST_DESTINATION)
+        read_from_parquet_patch.side_effect = [
+            self.test_cqc_locations_df,
+            self.test_cqc_providers_df,
+            self.test_ascwds_df,
+        ]
+        job.main(self.TEST_LOCATIONS_SOURCE, self.TEST_PROVIDERS_SOURCE, self.TEST_WORKPLACE_SOURCE, self.TEST_CQC_RATINGS_DESTINATION, self.TEST_BENCHMARK_RATINGS_DESTINATION)
         write_to_parquet_patch.assert_called_once_with(
             ANY,
-            self.TEST_DESTINATION,
+            self.TEST_CQC_RATINGS_DESTINATION,
             mode="overwrite",
         )
 
