62ce38962792e0d817ac8539fd067d8e1bdd3643
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-04-25 12:35:42 +0100

    Set up script and test filed

---
+++ jobs/flatten_cqc_ratings.py
@@ -0,0 +1,34 @@
+import sys
+
+from utils import utils
+
+def main(cqc_location_source: str, cqc_ratings_destination: str):
+    cqc_location_df = utils.read_from_parquet(cqc_location_source)
+
+
+
+    utils.write_to_parquet(
+        cqc_location_df,
+        cqc_ratings_destination,
+        mode="overwrite",
+    )
+
+
+
+if __name__ == "__main__":
+    print("Spark job 'flatten_cqc_ratings' starting...")
+    print(f"Job parameters: {sys.argv}")
+
+    source, destination = utils.collect_arguments(
+        (
+            "--cqc_location_source",
+            "Source s3 directory for parquet CQC locations dataset",
+        ),
+        (
+            "--cqc_ratings_destination",
+            "Destination s3 directory for cleaned parquet CQC ratings dataset",
+        ),
+    )
+    main(source, destination)
+
+    print("Spark job 'flatten_cqc_ratings' complete")
---
+++ tests/unit/test_flatten_cqc_ratings.py
@@ -0,0 +1,43 @@
+import unittest
+from unittest.mock import ANY, Mock, patch
+import pyspark.sql.functions as F
+
+from utils import utils
+
+import jobs.flatten_cqc_ratings as job
+
+from tests.test_file_data import CQCLocationsData as Data
+from tests.test_file_schemas import CQCLocationsSchema as Schema
+
+
+
+class CleanCQCProviderDatasetTests(unittest.TestCase):
+    TEST_SOURCE = "some/directory"
+    TEST_DESTINATION = "some/other/directory"
+    
+    def setUp(self) -> None:
+        self.spark = utils.get_spark()
+        self.test_cqc_locations_df = self.spark.createDataFrame(
+            Data.sample_rows_full, schema=Schema.full_schema
+        )
+
+
+class MainTests(CleanCQCProviderDatasetTests):
+    def setUp(self) -> None:
+        super().setUp()
+
+    @patch("utils.utils.write_to_parquet")
+    @patch("utils.utils.read_from_parquet")
+    def test_main(self, read_from_parquet_patch: Mock, write_to_parquet_patch: Mock):
+        read_from_parquet_patch.return_value = self.test_cqc_locations_df
+        job.main(self.TEST_SOURCE, self.TEST_DESTINATION)
+        write_to_parquet_patch.assert_called_once_with(
+            ANY,
+            self.TEST_DESTINATION,
+            mode="overwrite",
+        )
+
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
