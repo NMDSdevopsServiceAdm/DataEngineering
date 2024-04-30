3b2522955ac656104527a38b81ad6a91573a8103
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-04-30 15:43:02 +0100

    Update location schema import to test file schemas

--- tests/test_file_schemas.py
+++ tests/test_file_schemas.py
@@ -78,7 +78,7 @@ from utils.cqc_ratings_utils.cqc_ratings_values import (
     CQCRatingsColumns as CQCRatings,
 )
 
-from schemas.cqc_location_schema import OLD_LOCATION_SCHEMA
+from schemas.cqc_location_schema import LOCATION_SCHEMA
 
 
 from utils.column_names.ind_cqc_pipeline_columns import (
@@ -520,7 +520,7 @@ class CapacityTrackerDomCareSchema:
 class CQCLocationsSchema:
     full_schema = StructType(
         [
-            *OLD_LOCATION_SCHEMA,
+            *LOCATION_SCHEMA,
             StructField(Keys.import_date, StringType(), True),
         ]
     )
