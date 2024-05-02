6f85f44fb9292f12b08553259f671bdf30c03f6a
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-02 14:46:21 +0100

    Add timezone

--- terraform/pipeline/eventbridge.tf
+++ terraform/pipeline/eventbridge.tf
@@ -125,6 +125,7 @@ resource "aws_scheduler_schedule" "bulk_download_cqc_api_schedule" {
   }
 
   schedule_expression = "cron(30 01 01,08,15,23 * ? *)"
+  schedule_expression_timezone = "GMT"
 
   target {
     arn      = aws_sfn_state_machine.bulk-download-cqc-api-state-machine.arn
