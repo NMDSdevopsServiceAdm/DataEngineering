232fc03c76fc96275946bc5fb1d08b69b96b5d89
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-17 17:23:08 +0100

    Remove all changes from validation utils

--- utils/validation/validation_utils.py
+++ utils/validation/validation_utils.py
@@ -26,34 +26,30 @@ def validate_dataset(dataset: DataFrame, rules: dict) -> DataFrame:
 def add_checks_to_run(
     run: VerificationRunBuilder, rules_to_check: dict
 ) -> VerificationRunBuilder:
-    for rule_name in rules_to_check.keys():
-        rule = rules_to_check[rule_name]
-        if rule_name == RuleToCheck.size_of_dataset:
-            check = create_check_of_size_of_dataset(rule)
-            run = run.addCheck(check)
-        elif rule_name == RuleToCheck.complete_columns:
-            check = create_check_for_column_completeness(rule)
-            run = run.addCheck(check)
-        elif rule_name == RuleToCheck.index_columns:
-            check = create_check_of_uniqueness_of_two_index_columns(rule)
-            run = run.addCheck(check)
-        elif rule_name == RuleToCheck.min_values:
-            for column_name in rule.keys():
-                check = create_check_of_min_values(column_name, rule[column_name])
-                run = run.addCheck(check)
-        elif rule_name == RuleToCheck.max_values:
-            for column_name in rule.keys():
-                check = create_check_of_max_values(column_name, rule[column_name])
-                run = run.addCheck(check)
-        elif rule_name == RuleToCheck.categorical_values_in_columns:
-            check = create_check_of_categorical_values_in_columns(rule)
-            run = run.addCheck(check)
-        elif rule_name == RuleToCheck.distinct_values:
-            check = create_check_of_number_of_distinct_values(rule)
-            run = run.addCheck(check)
-        else:
-            raise ValueError("Unknown rule to check")
+    for rule in rules_to_check.keys():
+        check = create_check(rule, rules_to_check[rule])
+        run = run.addCheck(check)
     return run
+
+
+def create_check(rule_name: str, rule) -> Check:
+    if rule_name == RuleToCheck.size_of_dataset:
+        check = create_check_of_size_of_dataset(rule)
+    elif rule_name == RuleToCheck.complete_columns:
+        check = create_check_for_column_completeness(rule)
+    elif rule_name == RuleToCheck.index_columns:
+        check = create_check_of_uniqueness_of_two_index_columns(rule)
+    elif rule_name == RuleToCheck.min_values:
+        check = create_check_of_min_values(rule)
+    elif rule_name == RuleToCheck.max_values:
+        check = create_check_of_max_values(rule)
+    elif rule_name == RuleToCheck.categorical_values_in_columns:
+        check = create_check_of_categorical_values_in_columns(rule)
+    elif rule_name == RuleToCheck.distinct_values:
+        check = create_check_of_number_of_distinct_values(rule)
+    else:
+        raise ValueError("Unknown rule to check")
+    return check
 
 
 def create_check_for_column_completeness(complete_columns: list) -> Check:
@@ -83,25 +79,27 @@ def create_check_of_size_of_dataset(expected_size: int) -> Check:
     return check
 
 
-def create_check_of_min_values(column_name: str, min_value: int) -> Check:
+def create_check_of_min_values(column_minimums: dict) -> Check:
     spark = utils.get_spark()
-    check = Check(spark, CheckLevel.Warning, f"Min value in column")
-    check = check.hasMin(
-        column_name,
-        lambda x: x >= min_value,
-        f"The minimum value for {column_name} should be {min_value}.",
-    )
+    check = Check(spark, CheckLevel.Warning, "Min value in column")
+    for column in column_minimums.keys():
+        check = check.hasMin(
+            column,
+            lambda x: x >= column_minimums[column],
+            f"The minimum value for {column} should be {column_minimums[column]}.",
+        )
     return check
 
 
-def create_check_of_max_values(column_name: str, max_value: int) -> Check:
+def create_check_of_max_values(column_maximums: dict) -> Check:
     spark = utils.get_spark()
     check = Check(spark, CheckLevel.Warning, "Max value in column")
-    check = check.hasMax(
-        column_name,
-        lambda x: x <= max_value,
-        f"The maximum value for {column_name} should be {max_value}.",
-    )
+    for column in column_maximums.keys():
+        check = check.hasMax(
+            column,
+            lambda x: x <= column_maximums[column],
+            f"The maximum value for {column} should be {column_maximums[column]}.",
+        )
     return check
 
 
