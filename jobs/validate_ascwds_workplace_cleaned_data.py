da1768da4bc86cfb412181f846fbe2aa9f0fabb9
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-20 14:03:25 +0100

    Update job from pir to ascwds workplace

--- jobs/validate_ascwds_workplace_cleaned_data.py
+++ jobs/validate_ascwds_workplace_cleaned_data.py
@@ -3,52 +3,43 @@ import sys
 
 os.environ["SPARK_VERSION"] = "3.3"
 
-
 from utils import utils
-from utils.column_names.raw_data_files.cqc_pir_columns import (
-    CqcPirColumns as CQCPIR,
-)
 from utils.column_names.ind_cqc_pipeline_columns import (
     PartitionKeys as Keys,
 )
-from utils.validation.validation_rules.pir_cleaned_validation_rules import (
-    PIRCleanedValidationRules as Rules,
+from utils.validation.validation_rules.ascwds_workplace_cleaned_validation_rules import (
+    ASCWDSWorkplaceCleanedValidationRules as Rules,
 )
 from utils.validation.validation_utils import validate_dataset
 
 PartitionKeys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
 
-raw_cqc_pir_columns_to_import = [
-    Keys.import_date,
-    CQCPIR.location_id,
-]
-
 
 def main(
-    cleaned_cqc_pir_source: str,
+    cleaned_ascwds_workplace_source: str,
     report_destination: str,
 ):
-    cleaned_cqc_pir_df = utils.read_from_parquet(
-        cleaned_cqc_pir_source,
+    cleaned_ascwds_workplace_df = utils.read_from_parquet(
+        cleaned_ascwds_workplace_source,
     )
     rules = Rules.rules_to_check
 
-    check_result_df = validate_dataset(cleaned_cqc_pir_df, rules)
+    check_result_df = validate_dataset(cleaned_ascwds_workplace_df, rules)
 
     utils.write_to_parquet(check_result_df, report_destination, mode="overwrite")
 
 
 if __name__ == "__main__":
-    print("Spark job 'validate_pir_api_cleaned_data' starting...")
+    print("Spark job 'validate_ascwds_workplace_cleaned_data' starting...")
     print(f"Job parameters: {sys.argv}")
 
     (
-        cleaned_cqc_pir_source,
+        cleaned_ascwds_workplace_source,
         report_destination,
     ) = utils.collect_arguments(
         (
-            "--cleaned_cqc_pir_source",
-            "Source s3 directory for parquet pir api cleaned dataset",
+            "--cleaned_ascwds_workplace_source",
+            "Source s3 directory for parquet ascwds workplace cleaned dataset",
         ),
         (
             "--report_destination",
@@ -57,7 +48,7 @@ if __name__ == "__main__":
     )
     try:
         main(
-            cleaned_cqc_pir_source,
+            cleaned_ascwds_workplace_source,
             report_destination,
         )
     finally:
@@ -66,4 +57,4 @@ if __name__ == "__main__":
             spark.sparkContext._gateway.shutdown_callback_server()
         spark.stop()
 
-    print("Spark job 'validate_pir_api_cleaned_data' complete")
+    print("Spark job 'validate_ascwds_workplace_cleaned_data' complete")
