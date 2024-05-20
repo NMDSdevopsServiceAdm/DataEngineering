e5f49fc1ee41970d838cfc23ada511501b617653
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-20 11:49:16 +0100

    Update validation test script for provider

--- tests/unit/test_validate_providers_api_cleaned_data.py
+++ tests/unit/test_validate_providers_api_cleaned_data.py
@@ -4,25 +4,25 @@ from unittest.mock import Mock, patch
 
 import jobs.validate_providers_api_cleaned_data as job
 
-from tests.test_file_data import ValidateLocationsAPICleanedData as Data
-from tests.test_file_schemas import ValidateLocationsAPICleanedData as Schemas
+from tests.test_file_data import ValidateProvidersAPICleanedData as Data
+from tests.test_file_schemas import ValidateProvidersAPICleanedData as Schemas
 
 from utils import utils
 
 
-class ValidateLocationsAPICleanedDatasetTests(unittest.TestCase):
-    TEST_RAW_CQC_LOCATION_SOURCE = "some/directory"
-    TEST_CQC_LOCATIONS_API_CLEANED_SOURCE = "some/other/directory"
+class ValidateProvidersAPICleanedDatasetTests(unittest.TestCase):
+    TEST_RAW_CQC_PROVIDER_SOURCE = "some/directory"
+    TEST_CQC_PROVIDERS_API_CLEANED_SOURCE = "some/other/directory"
     TEST_DESTINATION = "some/other/other/directory"
 
     def setUp(self) -> None:
         self.spark = utils.get_spark()
-        self.test_raw_cqc_location_df = self.spark.createDataFrame(
-            Data.raw_cqc_locations_rows,
-            Schemas.raw_cqc_locations_schema,
+        self.test_raw_cqc_provider_df = self.spark.createDataFrame(
+            Data.raw_cqc_providers_rows,
+            Schemas.raw_cqc_providers_schema,
         )
-        self.test_cleaned_cqc_locations_df = self.spark.createDataFrame(
-            Data.cleaned_cqc_locations_rows, Schemas.cleaned_cqc_locations_schema
+        self.test_cleaned_cqc_providers_df = self.spark.createDataFrame(
+            Data.cleaned_cqc_providers_rows, Schemas.cleaned_cqc_providers_schema
         )
 
     def tearDown(self) -> None:
@@ -30,7 +30,7 @@ class ValidateLocationsAPICleanedDatasetTests(unittest.TestCase):
             self.spark.sparkContext._gateway.shutdown_callback_server()
 
 
-class MainTests(ValidateLocationsAPICleanedDatasetTests):
+class MainTests(ValidateProvidersAPICleanedDatasetTests):
     def setUp(self) -> None:
         return super().setUp()
 
@@ -42,13 +42,13 @@ class MainTests(ValidateLocationsAPICleanedDatasetTests):
         write_to_parquet_patch: Mock,
     ):
         read_from_parquet_patch.side_effect = [
-            self.test_raw_cqc_location_df,
-            self.test_cleaned_cqc_locations_df,
+            self.test_raw_cqc_provider_df,
+            self.test_cleaned_cqc_providers_df,
         ]
 
         job.main(
-            self.TEST_RAW_CQC_LOCATION_SOURCE,
-            self.TEST_CQC_LOCATIONS_API_CLEANED_SOURCE,
+            self.TEST_RAW_CQC_PROVIDER_SOURCE,
+            self.TEST_CQC_PROVIDERS_API_CLEANED_SOURCE,
             self.TEST_DESTINATION,
         )
 
@@ -56,11 +56,11 @@ class MainTests(ValidateLocationsAPICleanedDatasetTests):
         self.assertEqual(write_to_parquet_patch.call_count, 1)
 
 
-class CalculateExpectedSizeofDataset(ValidateLocationsAPICleanedDatasetTests):
+class CalculateExpectedSizeofDataset(ValidateProvidersAPICleanedDatasetTests):
     def setUp(self) -> None:
         return super().setUp()
 
-    def test_calculate_expected_size_of_cleaned_cqc_locations_dataset_returns_correct_row_count(
+    def test_calculate_expected_size_of_cleaned_cqc_providers_dataset_returns_correct_row_count(
         self,
     ):
         test_df = self.spark.createDataFrame(
@@ -68,7 +68,7 @@ class CalculateExpectedSizeofDataset(ValidateLocationsAPICleanedDatasetTests):
         )
         expected_row_count = 1
         returned_row_count = (
-            job.calculate_expected_size_of_cleaned_cqc_locations_dataset(test_df)
+            job.calculate_expected_size_of_cleaned_cqc_providers_dataset(test_df)
         )
         self.assertEqual(returned_row_count, expected_row_count)
 
