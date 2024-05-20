a5d80f2df1e8bb1668b10a4071ed80c0af3da071
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-20 11:47:40 +0100

    Update validation job script for provider

--- jobs/validate_providers_api_cleaned_data.py
+++ jobs/validate_providers_api_cleaned_data.py
@@ -6,80 +6,72 @@ os.environ["SPARK_VERSION"] = "3.3"
 from pyspark.sql.dataframe import DataFrame
 
 from utils import utils
-from utils.column_names.cleaned_data_files.cqc_location_cleaned_values import (
-    CqcLocationCleanedValues as CQCLValues,
-)
-from utils.column_names.raw_data_files.cqc_location_api_columns import (
-    NewCqcLocationApiColumns as CQCL,
+from utils.column_names.raw_data_files.cqc_provider_api_columns import (
+    CqcProviderApiColumns as CQCP,
 )
 from utils.column_names.ind_cqc_pipeline_columns import (
     PartitionKeys as Keys,
 )
 from utils.validation.validation_rules.providers_api_cleaned_validation_rules import (
-    LocationsAPICleanedValidationRules as Rules,
+    ProvidersAPICleanedValidationRules as Rules,
 )
 from utils.validation.validation_utils import validate_dataset
 from utils.validation.validation_rule_names import RuleNames as RuleName
 
 PartitionKeys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
 
-raw_cqc_locations_columns_to_import = [
+raw_cqc_providers_columns_to_import = [
     Keys.import_date,
-    CQCL.location_id,
-    CQCL.type,
-    CQCL.registration_status,
+    CQCP.provider_id,
 ]
 
 
 def main(
-    raw_cqc_location_source: str,
-    cleaned_cqc_locations_source: str,
+    raw_cqc_provider_source: str,
+    cleaned_cqc_providers_source: str,
     report_destination: str,
 ):
-    raw_location_df = utils.read_from_parquet(
-        raw_cqc_location_source,
-        selected_columns=raw_cqc_locations_columns_to_import,
+    raw_provider_df = utils.read_from_parquet(
+        raw_cqc_provider_source,
+        selected_columns=raw_cqc_providers_columns_to_import,
     )
-    cleaned_cqc_locations_df = utils.read_from_parquet(
-        cleaned_cqc_locations_source,
+    cleaned_cqc_providers_df = utils.read_from_parquet(
+        cleaned_cqc_providers_source,
     )
     rules = Rules.rules_to_check
 
     rules[RuleName.size_of_dataset] = (
-        calculate_expected_size_of_cleaned_cqc_locations_dataset(raw_location_df)
+        calculate_expected_size_of_cleaned_cqc_providers_dataset(raw_provider_df)
     )
 
-    check_result_df = validate_dataset(cleaned_cqc_locations_df, rules)
+    check_result_df = validate_dataset(cleaned_cqc_providers_df, rules)
 
     utils.write_to_parquet(check_result_df, report_destination, mode="overwrite")
 
 
-def calculate_expected_size_of_cleaned_cqc_locations_dataset(
-    raw_location_df: DataFrame,
+def calculate_expected_size_of_cleaned_cqc_providers_dataset(
+    raw_provider_df: DataFrame,
 ) -> int:
-    expected_size = raw_location_df.where(
-        (raw_location_df[CQCL.type] == CQCLValues.social_care_identifier)
-        & (raw_location_df[CQCL.registration_status] == CQCLValues.registered)
-    ).count()
+    expected_size = raw_provider_df.count()
     return expected_size
 
 
 if __name__ == "__main__":
-    print("Spark job 'validate_locations_api_cleaned_data' starting...")
+    print("Spark job 'validate_providers_api_cleaned_data' starting...")
     print(f"Job parameters: {sys.argv}")
 
     (
-        raw_cqc_location_source,
-        cleaned_cqc_locations_source,
+        raw_cqc_provider_source,
+        cleaned_cqc_providers_source,
         report_destination,
     ) = utils.collect_arguments(
         (
-            "--raw_cqc_location_source",
-            "Source s3 directory for parquet locations api dataset",
+            "--raw_cqc_provider_source",
+            "Source s3 directory for parquet providers api dataset",
         ),
         (
-            "--cleaned_cqc_locations_source",
-            "Source s3 directory for parquet locations api cleaned dataset",
+            "--cleaned_cqc_providers_source",
+            "Source s3 directory for parquet providers api cleaned dataset",
         ),
         (
             "--report_destination",
@@ -88,8 +80,8 @@ if __name__ == "__main__":
     )
     try:
         main(
-            raw_cqc_location_source,
-            cleaned_cqc_locations_source,
+            raw_cqc_provider_source,
+            cleaned_cqc_providers_source,
             report_destination,
         )
     finally:
@@ -98,4 +90,4 @@ if __name__ == "__main__":
             spark.sparkContext._gateway.shutdown_callback_server()
         spark.stop()
 
-    print("Spark job 'validate_locations_api_cleaned_data' complete")
+    print("Spark job 'validate_providers_api_cleaned_data' complete")
