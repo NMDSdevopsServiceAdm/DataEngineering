27c5d2e3674772b30a3b68be4870760507d4fe9d
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-20 15:49:47 +0100

    Create glue module

--- terraform/pipeline/glue.tf
+++ terraform/pipeline/glue.tf
@@ -435,6 +435,20 @@ module "validate_ascwds_worker_cleaned_data_job" {
   }
 }
 
+module "validate_postcode_directory_cleaned_data_job" {
+  source          = "../modules/glue-job"
+  script_name     = "validate_postcode_directory_cleaned_data.py"
+  glue_role       = aws_iam_role.sfc_glue_service_iam_role
+  resource_bucket = module.pipeline_resources
+  datasets_bucket = module.datasets_bucket
+  glue_version    = "4.0"
+
+  job_parameters = {
+    "--cleaned_postcode_directory_source" = "${module.datasets_bucket.bucket_uri}/domain=ONS/dataset=postcode_directory_cleaned/"
+    "--report_destination"                = "${module.datasets_bucket.bucket_uri}/domain=data_validation_reports/dataset=postcode_directory_cleaned_data_report/"
+  }
+}
+
 module "validate_merged_ind_cqc_data_job" {
   source          = "../modules/glue-job"
   script_name     = "validate_merged_ind_cqc_data.py"
