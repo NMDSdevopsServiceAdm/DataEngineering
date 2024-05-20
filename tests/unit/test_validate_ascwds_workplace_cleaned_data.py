4244d54f571d8772f6d06792153bb9a8a9e11a48
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-20 14:05:09 +0100

    Update tests from pir to ascwds workplace

--- tests/unit/test_validate_ascwds_workplace_cleaned_data.py
+++ tests/unit/test_validate_ascwds_workplace_cleaned_data.py
@@ -2,22 +2,22 @@ import unittest
 
 from unittest.mock import Mock, patch
 
-import jobs.validate_pir_cleaned_data as job
+import jobs.validate_ascwds_workplace_cleaned_data as job
 
-from tests.test_file_data import ValidatePIRCleanedData as Data
-from tests.test_file_schemas import ValidatePIRCleanedData as Schemas
+from tests.test_file_data import ValidateASCWDSWorkplaceCleanedData as Data
+from tests.test_file_schemas import ValidateASCWDSWorkplaceCleanedData as Schemas
 
 from utils import utils
 
 
-class ValidatePIRCleanedDatasetTests(unittest.TestCase):
-    TEST_CQC_PIR_CLEANED_SOURCE = "some/other/directory"
+class ValidateASCWDSWorkplaceCleanedDatasetTests(unittest.TestCase):
+    TEST_ASCWDS_WORKPLACE_CLEANED_SOURCE = "some/other/directory"
     TEST_DESTINATION = "some/other/other/directory"
 
     def setUp(self) -> None:
         self.spark = utils.get_spark()
-        self.test_cleaned_cqc_pir_df = self.spark.createDataFrame(
-            Data.cleaned_cqc_pir_rows, Schemas.cleaned_cqc_pir_schema
+        self.test_cleaned_ascwds_workplace_df = self.spark.createDataFrame(
+            Data.cleaned_ascwds_workplace_rows, Schemas.cleaned_ascwds_workplace_schema
         )
 
     def tearDown(self) -> None:
@@ -25,7 +25,7 @@ class ValidatePIRCleanedDatasetTests(unittest.TestCase):
             self.spark.sparkContext._gateway.shutdown_callback_server()
 
 
-class MainTests(ValidatePIRCleanedDatasetTests):
+class MainTests(ValidateASCWDSWorkplaceCleanedDatasetTests):
     def setUp(self) -> None:
         return super().setUp()
 
@@ -37,11 +37,11 @@ class MainTests(ValidatePIRCleanedDatasetTests):
         write_to_parquet_patch: Mock,
     ):
         read_from_parquet_patch.side_effect = [
-            self.test_cleaned_cqc_pir_df,
+            self.test_cleaned_ascwds_workplace_df,
         ]
 
         job.main(
-            self.TEST_CQC_PIR_CLEANED_SOURCE,
+            self.TEST_ASCWDS_WORKPLACE_CLEANED_SOURCE,
             self.TEST_DESTINATION,
         )
 
