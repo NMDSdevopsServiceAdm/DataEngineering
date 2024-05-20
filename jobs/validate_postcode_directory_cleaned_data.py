4839cc910d471056f61767d1bda1fa522c20d918
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-20 15:12:30 +0100

    Update job from provider to postcode directory

--- jobs/validate_postcode_directory_cleaned_data.py
+++ jobs/validate_postcode_directory_cleaned_data.py
@@ -6,72 +6,74 @@ os.environ["SPARK_VERSION"] = "3.3"
 from pyspark.sql.dataframe import DataFrame
 
 from utils import utils
-from utils.column_names.raw_data_files.cqc_provider_api_columns import (
-    CqcProviderApiColumns as CQCP,
+from utils.column_names.raw_data_files.ons_columns import (
+    OnsPostcodeDirectoryColumns as ONS,
 )
 from utils.column_names.ind_cqc_pipeline_columns import (
     PartitionKeys as Keys,
 )
-from utils.validation.validation_rules.providers_api_cleaned_validation_rules import (
-    ProvidersAPICleanedValidationRules as Rules,
+from utils.validation.validation_rules.postcode_directory_cleaned_validation_rules import (
+    PostcodeDirectoryCleanedValidationRules as Rules,
 )
 from utils.validation.validation_utils import validate_dataset
 from utils.validation.validation_rule_names import RuleNames as RuleName
 
 PartitionKeys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
 
-raw_cqc_providers_columns_to_import = [
+raw_postcode_directory_columns_to_import = [
     Keys.import_date,
-    CQCP.provider_id,
+    ONS.postcode,
 ]
 
 
 def main(
-    raw_cqc_provider_source: str,
-    cleaned_cqc_providers_source: str,
+    raw_postcode_directory_source: str,
+    cleaned_postcode_directory_source: str,
     report_destination: str,
 ):
-    raw_provider_df = utils.read_from_parquet(
-        raw_cqc_provider_source,
-        selected_columns=raw_cqc_providers_columns_to_import,
+    raw_postcode_directory_df = utils.read_from_parquet(
+        raw_postcode_directory_source,
+        selected_columns=raw_postcode_directory_columns_to_import,
     )
-    cleaned_cqc_providers_df = utils.read_from_parquet(
-        cleaned_cqc_providers_source,
+    cleaned_postcode_directory_df = utils.read_from_parquet(
+        cleaned_postcode_directory_source,
     )
     rules = Rules.rules_to_check
 
     rules[RuleName.size_of_dataset] = (
-        calculate_expected_size_of_cleaned_cqc_providers_dataset(raw_provider_df)
+        calculate_expected_size_of_cleaned_postcode_directory_dataset(
+            raw_postcode_directory_df
+        )
     )
 
-    check_result_df = validate_dataset(cleaned_cqc_providers_df, rules)
+    check_result_df = validate_dataset(cleaned_postcode_directory_df, rules)
 
     utils.write_to_parquet(check_result_df, report_destination, mode="overwrite")
 
 
-def calculate_expected_size_of_cleaned_cqc_providers_dataset(
-    raw_provider_df: DataFrame,
+def calculate_expected_size_of_cleaned_postcode_directory_dataset(
+    raw_postcode_directory_df: DataFrame,
 ) -> int:
-    expected_size = raw_provider_df.count()
+    expected_size = raw_postcode_directory_df.count()
     return expected_size
 
 
 if __name__ == "__main__":
-    print("Spark job 'validate_providers_api_cleaned_data' starting...")
+    print("Spark job 'validate_postcode_directory_cleaned_data' starting...")
     print(f"Job parameters: {sys.argv}")
 
     (
-        raw_cqc_provider_source,
-        cleaned_cqc_providers_source,
+        raw_postcode_directory_source,
+        cleaned_postcode_directory_source,
         report_destination,
     ) = utils.collect_arguments(
         (
-            "--raw_cqc_provider_source",
-            "Source s3 directory for parquet providers api dataset",
+            "--raw_postcode_directory_source",
+            "Source s3 directory for parquet postcode directory dataset",
         ),
         (
-            "--cleaned_cqc_providers_source",
-            "Source s3 directory for parquet providers api cleaned dataset",
+            "--cleaned_postcode_directory_source",
+            "Source s3 directory for parquet postcode directory cleaned dataset",
         ),
         (
             "--report_destination",
@@ -80,8 +82,8 @@ if __name__ == "__main__":
     )
     try:
         main(
-            raw_cqc_provider_source,
-            cleaned_cqc_providers_source,
+            raw_postcode_directory_source,
+            cleaned_postcode_directory_source,
             report_destination,
         )
     finally:
@@ -90,4 +92,4 @@ if __name__ == "__main__":
             spark.sparkContext._gateway.shutdown_callback_server()
         spark.stop()
 
-    print("Spark job 'validate_providers_api_cleaned_data' complete")
+    print("Spark job 'validate_postcode_directory_cleaned_data' complete")
