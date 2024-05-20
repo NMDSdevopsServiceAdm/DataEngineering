55e9b47783aa95106e99a6a3686760b2a944c031
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-20 12:46:56 +0100

    Update pir validaiton rules

--- utils/validation/validation_rules/pir_cleaned_validation_rules.py
+++ utils/validation/validation_rules/pir_cleaned_validation_rules.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
-from utils.column_names.cleaned_data_files.cqc_provider_cleaned_values import (
-    CqcProviderCleanedColumns as CQCPClean,
+from utils.column_names.cleaned_data_files.cqc_pir_cleaned_values import (
+    CqcPIRCleanedColumns as CQCPIRClean,
 )
 
 from utils.validation.validation_rule_names import RuleNames as RuleName
@@ -14,21 +14,26 @@ from utils.validation.categorical_column_values import (
 @dataclass
 class ProvidersAPICleanedValidationRules:
     rules_to_check = {
-        RuleName.size_of_dataset: None,
         RuleName.complete_columns: [
-            CQCPClean.cqc_provider_import_date,
-            CQCPClean.provider_id,
-            CQCPClean.cqc_sector,
-            CQCPClean.name,
+            CQCPIRClean.cqc_pir_import_date,
+            CQCPIRClean.location_id,
+            CQCPIRClean.people_directly_employed,
+            CQCPIRClean.care_home,
         ],
         RuleName.index_columns: [
-            CQCPClean.provider_id,
-            CQCPClean.cqc_provider_import_date,
+            CQCPIRClean.location_id,
+            CQCPIRClean.cqc_pir_import_date,
         ],
+        RuleName.max_values: {
+            CQCPIRClean.people_directly_employed: 10000,
+        },
+        RuleName.min_values: {
+            CQCPIRClean.people_directly_employed: 0,
+        },
         RuleName.categorical_values_in_columns: {
-            CQCPClean.cqc_sector: CQCCategoricalValues.cqc_sector,
+            CQCPIRClean.care_home: CQCCategoricalValues.care_home_values,
         },
         RuleName.distinct_values: {
-            CQCPClean.cqc_sector: CQCDistinctValues.cqc_sector_values,
+            CQCPIRClean.care_home: CQCDistinctValues.care_home_values,
         },
     }
