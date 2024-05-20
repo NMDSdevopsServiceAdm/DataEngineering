51989d96c952c04275c2c670f52be57f9f570d89
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-20 14:08:33 +0100

    Update rules from pir to ascwds workplace

--- utils/validation/validation_rules/ascwds_workplace_cleaned_validation_rules.py
+++ utils/validation/validation_rules/ascwds_workplace_cleaned_validation_rules.py
@@ -1,39 +1,31 @@
 from dataclasses import dataclass
 
-from utils.column_names.cleaned_data_files.cqc_pir_cleaned_values import (
-    CqcPIRCleanedColumns as CQCPIRClean,
+from utils.column_names.cleaned_data_files.ascwds_workplace_cleaned_values import (
+    AscwdsWorkplaceCleanedColumns as AWPClean,
 )
 
 from utils.validation.validation_rule_names import RuleNames as RuleName
-from utils.validation.categorical_column_values import (
-    CQCCategoricalValues,
-    CQCDistinctValues,
-)
 
 
 @dataclass
-class PIRCleanedValidationRules:
+class ASCWDSWorkplaceCleanedValidationRules:
     rules_to_check = {
         RuleName.complete_columns: [
-            CQCPIRClean.cqc_pir_import_date,
-            CQCPIRClean.location_id,
-            CQCPIRClean.people_directly_employed,
-            CQCPIRClean.care_home,
+            AWPClean.organisation_id,
+            AWPClean.location_id,
+            AWPClean.ascwds_workplace_import_date,
+            AWPClean.establishment_id,
         ],
         RuleName.index_columns: [
-            CQCPIRClean.location_id,
-            CQCPIRClean.cqc_pir_import_date,
+            AWPClean.establishment_id,
+            AWPClean.ascwds_workplace_import_date,
         ],
         RuleName.max_values: {
-            CQCPIRClean.people_directly_employed: 10000,
+            AWPClean.total_staff_bounded: 3000,
+            AWPClean.worker_records_bounded: 3000,
         },
         RuleName.min_values: {
-            CQCPIRClean.people_directly_employed: 0,
-        },
-        RuleName.categorical_values_in_columns: {
-            CQCPIRClean.care_home: CQCCategoricalValues.care_home_values,
-        },
-        RuleName.distinct_values: {
-            CQCPIRClean.care_home: CQCDistinctValues.care_home_values,
+            AWPClean.total_staff_bounded: 1,
+            AWPClean.worker_records_bounded: 1,
         },
     }
