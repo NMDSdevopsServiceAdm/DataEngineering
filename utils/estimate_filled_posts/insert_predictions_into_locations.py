0cce7bc9b38db52da198c703fe8e5fb59aa45332
Gary Polzin <Gary.Polzin@skillsforcare.org.uk>
2024-05-20 10:35:40 +0100

    import tidying

--- utils/estimate_filled_posts/insert_predictions_into_locations.py
+++ utils/estimate_filled_posts/insert_predictions_into_locations.py
@@ -1,17 +1,15 @@
-import pyspark.sql
+from pyspark.sql import DataFrame
 
 from utils.column_names.ind_cqc_pipeline_columns import (
     IndCqcColumns as IndCqc,
 )
 
-from pyspark.sql import functions as F
-
 
 def insert_predictions_into_locations(
-    locations_df: pyspark.sql.DataFrame,
-    predictions_df: pyspark.sql.DataFrame,
+    locations_df: DataFrame,
+    predictions_df: DataFrame,
     model_column_name: str,
-) -> pyspark.sql.DataFrame:
+) -> DataFrame:
     locations_with_predictions = locations_df.join(
         predictions_df,
         (locations_df[IndCqc.location_id] == predictions_df[IndCqc.location_id])
