73c0324a39c743bfad08af3579ddcd3d957f1df9
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-21 12:19:17 +0100

    Update rules from merged ind cqc to care home ind cqc features

--- utils/validation/validation_rules/care_home_ind_cqc_features_validation_rules.py
+++ utils/validation/validation_rules/care_home_ind_cqc_features_validation_rules.py
@@ -16,26 +16,16 @@ from utils.validation.categorical_column_values import (
 
 
 @dataclass
-class MergedIndCqcValidationRules:
+class CareHomeIndCqcFeaturesValidationRules:
     rules_to_check = {
         RuleName.size_of_dataset: None,
         RuleName.complete_columns: [
             IndCqcColumns.location_id,
-            IndCqcColumns.ascwds_workplace_import_date,
             IndCqcColumns.cqc_location_import_date,
             IndCqcColumns.care_home,
-            IndCqcColumns.provider_id,
-            IndCqcColumns.cqc_sector,
-            IndCqcColumns.registration_status,
-            IndCqcColumns.registration_date,
-            IndCqcColumns.primary_service_type,
-            IndCqcColumns.contemporary_ons_import_date,
-            IndCqcColumns.contemporary_cssr,
-            IndCqcColumns.contemporary_region,
-            IndCqcColumns.current_ons_import_date,
-            IndCqcColumns.current_cssr,
+            IndCqcColumns.number_of_beds,
+            IndCqcColumns.features,
             IndCqcColumns.current_region,
-            IndCqcColumns.current_rural_urban_indicator_2011,
         ],
         RuleName.index_columns: [
             IndCqcColumns.location_id,
@@ -44,37 +34,19 @@ class MergedIndCqcValidationRules:
         RuleName.min_values: {
             IndCqcColumns.number_of_beds: 1,
             IndCqcColumns.people_directly_employed: 0,
-            IndCqcColumns.total_staff_bounded: 1,
-            IndCqcColumns.worker_records_bounded: 1,
+            IndCqcColumns.ascwds_filled_posts_dedup_clean: 3000.0,
         },
         RuleName.max_values: {
             IndCqcColumns.number_of_beds: 500,
             IndCqcColumns.people_directly_employed: 10000,
-            IndCqcColumns.total_staff_bounded: 3000,
-            IndCqcColumns.worker_records_bounded: 3000,
+            IndCqcColumns.ascwds_filled_posts_dedup_clean: 1.0,
         },
         RuleName.categorical_values_in_columns: {
             IndCqcColumns.care_home: CQCCategoricalValues.care_home_values,
-            IndCqcColumns.cqc_sector: IndCQCCategoricalValues.cqc_sector,
-            IndCqcColumns.registration_status: CQCCategoricalValues.registration_status,
-            IndCqcColumns.dormancy: CQCCategoricalValues.dormancy_values,
-            IndCqcColumns.primary_service_type: CQCCategoricalValues.primary_service_types,
-            IndCqcColumns.contemporary_cssr: ONSCategoricalValues.cssrs,
-            IndCqcColumns.contemporary_region: ONSCategoricalValues.regions,
-            IndCqcColumns.current_cssr: ONSCategoricalValues.cssrs,
             IndCqcColumns.current_region: ONSCategoricalValues.regions,
-            IndCqcColumns.current_rural_urban_indicator_2011: ONSCategoricalValues.rural_urban_indicators,
         },
         RuleName.distinct_values: {
             IndCqcColumns.care_home: CQCDistinctValues.care_home_values,
-            IndCqcColumns.cqc_sector: IndCQCDistinctValues.cqc_sector,
-            IndCqcColumns.registration_status: CQCDistinctValues.registration_status_values,
-            IndCqcColumns.dormancy: CQCDistinctValues.dormancy_values,
-            IndCqcColumns.primary_service_type: CQCDistinctValues.primary_service_types,
-            IndCqcColumns.contemporary_cssr: ONSDistinctValues.cssrs,
-            IndCqcColumns.contemporary_region: ONSDistinctValues.regions,
-            IndCqcColumns.current_cssr: ONSDistinctValues.cssrs,
             IndCqcColumns.current_region: ONSDistinctValues.regions,
-            IndCqcColumns.current_rural_urban_indicator_2011: ONSDistinctValues.rural_urban_indicators,
         },
     }
