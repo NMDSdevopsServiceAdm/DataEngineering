7662af6d03dda9618639dadc0e9f36c329c7a8de
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-28 09:00:13 +0100

    Update job from estimate ind cqc filled posts to estimate ind cqc filled posts by job role

--- jobs/validate_estimated_ind_cqc_filled_posts_by_job_role_data.py
+++ jobs/validate_estimated_ind_cqc_filled_posts_by_job_role_data.py
@@ -9,8 +9,8 @@ from utils import utils
 from utils.column_names.ind_cqc_pipeline_columns import (
     PartitionKeys as Keys,
 )
-from utils.validation.validation_rules.estimated_ind_cqc_filled_posts_validation_rules import (
-    EstimatedIndCqcFilledPostsValidationRules as Rules,
+from utils.validation.validation_rules.estimated_ind_cqc_filled_posts_by_job_role_validation_rules import (
+    EstimatedIndCqcFilledPostsByJobRoleValidationRules as Rules,
 )
 from utils.validation.validation_utils import validate_dataset
 from utils.validation.validation_rule_names import RuleNames as RuleName
@@ -20,29 +20,31 @@ PartitionKeys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
 
 def main(
     cleaned_ind_cqc_source: str,
-    estimated_ind_cqc_filled_posts_source: str,
+    estimated_ind_cqc_filled_posts_by_job_role_source: str,
     report_destination: str,
 ):
     cleaned_ind_cqc_df = utils.read_from_parquet(
         cleaned_ind_cqc_source,
     )
-    estimated_ind_cqc_filled_posts_df = utils.read_from_parquet(
-        estimated_ind_cqc_filled_posts_source,
+    estimated_ind_cqc_filled_posts_by_job_role_df = utils.read_from_parquet(
+        estimated_ind_cqc_filled_posts_by_job_role_source,
     )
     rules = Rules.rules_to_check
 
-    rules[
-        RuleName.size_of_dataset
-    ] = calculate_expected_size_of_estimated_ind_cqc_filled_posts_dataset(
-        cleaned_ind_cqc_df
+    rules[RuleName.size_of_dataset] = (
+        calculate_expected_size_of_estimated_ind_cqc_filled_posts_by_job_role_dataset(
+            cleaned_ind_cqc_df
+        )
     )
 
-    check_result_df = validate_dataset(estimated_ind_cqc_filled_posts_df, rules)
+    check_result_df = validate_dataset(
+        estimated_ind_cqc_filled_posts_by_job_role_df, rules
+    )
 
     utils.write_to_parquet(check_result_df, report_destination, mode="overwrite")
 
 
-def calculate_expected_size_of_estimated_ind_cqc_filled_posts_dataset(
+def calculate_expected_size_of_estimated_ind_cqc_filled_posts_by_job_role_dataset(
     cleaned_ind_cqc_df: DataFrame,
 ) -> int:
     expected_size = cleaned_ind_cqc_df.count()
@@ -50,12 +52,14 @@ def calculate_expected_size_of_estimated_ind_cqc_filled_posts_dataset(
 
 
 if __name__ == "__main__":
-    print("Spark job 'validate_estimated_ind_cqc_filled_posts_source_data' starting...")
+    print(
+        "Spark job 'validate_estimated_ind_cqc_filled_posts_by_job_role_data' starting..."
+    )
     print(f"Job parameters: {sys.argv}")
 
     (
         cleaned_ind_cqc_source,
-        estimated_ind_cqc_filled_posts_source,
+        estimated_ind_cqc_filled_posts_by_job_role_source,
         report_destination,
     ) = utils.collect_arguments(
         (
@@ -63,8 +67,8 @@ if __name__ == "__main__":
             "Source s3 directory for parquet cleaned independent CQC dataset",
         ),
         (
-            "--estimated_ind_cqc_filled_posts_source",
-            "Source s3 directory for parquet estimated independent CQC filled posts dataset",
+            "--estimated_ind_cqc_filled_posts_by_job_role_source",
+            "Source s3 directory for parquet estimated independent CQC filled posts by job role dataset",
         ),
         (
             "--report_destination",
@@ -74,7 +78,7 @@ if __name__ == "__main__":
     try:
         main(
             cleaned_ind_cqc_source,
-            estimated_ind_cqc_filled_posts_source,
+            estimated_ind_cqc_filled_posts_by_job_role_source,
             report_destination,
         )
     finally:
@@ -83,4 +87,6 @@ if __name__ == "__main__":
             spark.sparkContext._gateway.shutdown_callback_server()
         spark.stop()
 
-    print("Spark job 'validate_estimated_ind_cqc_filled_posts_source_data' complete")
+    print(
+        "Spark job 'validate_estimated_ind_cqc_filled_posts_by_job_role_data' complete"
+    )
