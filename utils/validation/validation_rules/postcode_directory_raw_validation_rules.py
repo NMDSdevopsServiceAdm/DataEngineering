d825f76bf1e503d2fcc31e0ebce5fb6612df9f34
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-28 14:09:41 +0100

    Update rules from postcode directory cleaned to postcode directory raw

--- utils/validation/validation_rules/postcode_directory_raw_validation_rules.py
+++ utils/validation/validation_rules/postcode_directory_raw_validation_rules.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
 
-from utils.column_names.cleaned_data_files.ons_cleaned_values import (
-    OnsCleanedColumns as ONSClean,
+from utils.column_names.raw_data_files.ons_columns import (
+    OnsPostcodeDirectoryColumns as ONS,
+)
+from utils.column_names.ind_cqc_pipeline_columns import (
+    PartitionKeys as Keys,
 )
 
 from utils.validation.validation_rule_names import RuleNames as RuleName
@@ -12,35 +15,27 @@ from utils.validation.categorical_column_values import (
 
 
 @dataclass
-class PostcodeDirectoryCleanedValidationRules:
+class PostcodeDirectoryRawValidationRules:
     rules_to_check = {
-        RuleName.size_of_dataset: None,
         RuleName.complete_columns: [
-            ONSClean.postcode,
-            ONSClean.contemporary_ons_import_date,
-            ONSClean.contemporary_cssr,
-            ONSClean.contemporary_region,
-            ONSClean.current_ons_import_date,
-            ONSClean.current_cssr,
-            ONSClean.current_region,
-            ONSClean.current_rural_urban_ind_11,
+            ONS.postcode,
+            Keys.import_date,
+            ONS.cssr,
+            ONS.region,
+            ONS.rural_urban_indicator_2011,
         ],
         RuleName.index_columns: [
-            ONSClean.postcode,
-            ONSClean.contemporary_ons_import_date,
+            ONS.postcode,
+            Keys.import_date,
         ],
         RuleName.categorical_values_in_columns: {
-            ONSClean.contemporary_cssr: ONSCategoricalValues.cssrs,
-            ONSClean.contemporary_region: ONSCategoricalValues.regions,
-            ONSClean.current_cssr: ONSCategoricalValues.cssrs,
-            ONSClean.current_region: ONSCategoricalValues.regions,
-            ONSClean.current_rural_urban_ind_11: ONSCategoricalValues.rural_urban_indicators,
+            ONS.cssr: ONSCategoricalValues.cssrs,
+            ONS.region: ONSCategoricalValues.regions,
+            ONS.rural_urban_indicator_2011: ONSCategoricalValues.rural_urban_indicators,
         },
         RuleName.distinct_values: {
-            ONSClean.contemporary_cssr: ONSDistinctValues.cssrs,
-            ONSClean.contemporary_region: ONSDistinctValues.regions,
-            ONSClean.current_cssr: ONSDistinctValues.cssrs,
-            ONSClean.current_region: ONSDistinctValues.regions,
-            ONSClean.current_rural_urban_ind_11: ONSDistinctValues.rural_urban_indicators,
+            ONS.cssr: ONSDistinctValues.cssrs,
+            ONS.region: ONSDistinctValues.regions,
+            ONS.rural_urban_indicator_2011: ONSDistinctValues.rural_urban_indicators,
         },
     }
