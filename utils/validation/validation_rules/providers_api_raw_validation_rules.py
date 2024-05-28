30ec024e355f490c959de84d29398ee684eca53d
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-28 12:21:20 +0100

    Update rules from providers api cleaned to providers api raw

--- utils/validation/validation_rules/providers_api_raw_validation_rules.py
+++ utils/validation/validation_rules/providers_api_raw_validation_rules.py
@@ -1,34 +1,24 @@
 from dataclasses import dataclass
 
-from utils.column_names.cleaned_data_files.cqc_provider_cleaned_values import (
-    CqcProviderCleanedColumns as CQCPClean,
+from utils.column_names.raw_data_files.cqc_provider_api_columns import (
+    CqcProviderApiColumns as CQCP,
 )
-
+from utils.column_names.ind_cqc_pipeline_columns import (
+    PartitionKeys as Keys,
+)
 from utils.validation.validation_rule_names import RuleNames as RuleName
-from utils.validation.categorical_column_values import (
-    CQCCategoricalValues,
-    CQCDistinctValues,
-)
 
 
 @dataclass
-class ProvidersAPICleanedValidationRules:
+class ProvidersAPIRawValidationRules:
     rules_to_check = {
-        RuleName.size_of_dataset: None,
         RuleName.complete_columns: [
-            CQCPClean.cqc_provider_import_date,
-            CQCPClean.provider_id,
-            CQCPClean.cqc_sector,
-            CQCPClean.name,
+            Keys.import_date,
+            CQCP.provider_id,
+            CQCP.name,
         ],
         RuleName.index_columns: [
-            CQCPClean.provider_id,
-            CQCPClean.cqc_provider_import_date,
+            CQCP.provider_id,
+            Keys.import_date,
         ],
-        RuleName.categorical_values_in_columns: {
-            CQCPClean.cqc_sector: CQCCategoricalValues.cqc_sector,
-        },
-        RuleName.distinct_values: {
-            CQCPClean.cqc_sector: CQCDistinctValues.cqc_sector_values,
-        },
     }
