0994ff79b972a737130a654bf0c47f235458b0f2
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-28 11:21:24 +0100

    Update job from ascwds worker cleaned to ascwds worker raw

--- jobs/validate_ascwds_worker_raw_data.py
+++ jobs/validate_ascwds_worker_raw_data.py
@@ -8,8 +8,8 @@ from utils import utils
 from utils.column_names.ind_cqc_pipeline_columns import (
     PartitionKeys as Keys,
 )
-from utils.validation.validation_rules.ascwds_worker_cleaned_validation_rules import (
-    ASCWDSWorkerCleanedValidationRules as Rules,
+from utils.validation.validation_rules.ascwds_worker_raw_validation_rules import (
+    ASCWDSWorkerRawValidationRules as Rules,
 )
 from utils.validation.validation_utils import validate_dataset
 
@@ -17,30 +17,30 @@ PartitionKeys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
 
 
 def main(
-    cleaned_ascwds_worker_source: str,
+    raw_ascwds_worker_source: str,
     report_destination: str,
 ):
-    cleaned_ascwds_worker_df = utils.read_from_parquet(
-        cleaned_ascwds_worker_source,
+    raw_ascwds_worker_df = utils.read_from_parquet(
+        raw_ascwds_worker_source,
     )
     rules = Rules.rules_to_check
 
-    check_result_df = validate_dataset(cleaned_ascwds_worker_df, rules)
+    check_result_df = validate_dataset(raw_ascwds_worker_df, rules)
 
     utils.write_to_parquet(check_result_df, report_destination, mode="overwrite")
 
 
 if __name__ == "__main__":
-    print("Spark job 'validate_ascwds_worker_cleaned_data' starting...")
+    print("Spark job 'validate_ascwds_worker_raw_data' starting...")
     print(f"Job parameters: {sys.argv}")
 
     (
-        cleaned_ascwds_worker_source,
+        raw_ascwds_worker_source,
         report_destination,
     ) = utils.collect_arguments(
         (
-            "--cleaned_ascwds_worker_source",
-            "Source s3 directory for parquet ascwds worker api cleaned dataset",
+            "--raw_ascwds_worker_source",
+            "Source s3 directory for parquet ascwds worker raw dataset",
         ),
         (
             "--report_destination",
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     )
     try:
         main(
-            cleaned_ascwds_worker_source,
+            raw_ascwds_worker_source,
             report_destination,
         )
     finally:
@@ -58,4 +58,4 @@ if __name__ == "__main__":
             spark.sparkContext._gateway.shutdown_callback_server()
         spark.stop()
 
-    print("Spark job 'validate_ascwds_worker_cleaned_data' complete")
+    print("Spark job 'validate_ascwds_worker_raw_data' complete")
