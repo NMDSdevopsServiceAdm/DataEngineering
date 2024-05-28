953e40b95b1886dea2a412f19a178f59f034eff3
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-28 12:00:08 +0100

    Update rules from locations api cleaned to locations api raw

--- utils/validation/validation_rules/locations_api_raw_validation_rules.py
+++ utils/validation/validation_rules/locations_api_raw_validation_rules.py
@@ -1,74 +1,49 @@
 from dataclasses import dataclass
 
-from utils.column_names.cleaned_data_files.cqc_location_cleaned_values import (
-    CqcLocationCleanedColumns as CQCLClean,
+from utils.column_names.raw_data_files.cqc_location_api_columns import (
+    NewCqcLocationApiColumns as CQCL,
+)
+from utils.column_names.ind_cqc_pipeline_columns import (
+    PartitionKeys as Keys,
 )
 
 from utils.validation.validation_rule_names import RuleNames as RuleName
 from utils.validation.categorical_column_values import (
     CQCCategoricalValues,
     CQCDistinctValues,
-    ONSCategoricalValues,
-    ONSDistinctValues,
 )
 
 
 @dataclass
-class LocationsAPICleanedValidationRules:
+class LocationsAPIRawValidationRules:
     rules_to_check = {
-        RuleName.size_of_dataset: None,
         RuleName.complete_columns: [
-            CQCLClean.location_id,
-            CQCLClean.cqc_provider_import_date,
-            CQCLClean.cqc_location_import_date,
-            CQCLClean.care_home,
-            CQCLClean.provider_id,
-            CQCLClean.cqc_sector,
-            CQCLClean.registration_status,
-            CQCLClean.registration_date,
-            CQCLClean.primary_service_type,
-            CQCLClean.name,
-            CQCLClean.provider_name,
-            CQCLClean.contemporary_ons_import_date,
-            CQCLClean.contemporary_cssr,
-            CQCLClean.contemporary_region,
-            CQCLClean.current_ons_import_date,
-            CQCLClean.current_cssr,
-            CQCLClean.current_region,
-            CQCLClean.current_rural_urban_ind_11,
+            CQCL.location_id,
+            Keys.import_date,
+            CQCL.care_home,
+            CQCL.provider_id,
+            CQCL.registration_status,
+            CQCL.registration_date,
+            CQCL.name,
         ],
         RuleName.index_columns: [
-            CQCLClean.location_id,
-            CQCLClean.cqc_location_import_date,
+            CQCL.location_id,
+            Keys.import_date,
         ],
         RuleName.min_values: {
-            CQCLClean.number_of_beds: 1,
+            CQCL.number_of_beds: 0,
         },
         RuleName.max_values: {
-            CQCLClean.number_of_beds: 500,
+            CQCL.number_of_beds: 500,
         },
         RuleName.categorical_values_in_columns: {
-            CQCLClean.care_home: CQCCategoricalValues.care_home_values,
-            CQCLClean.cqc_sector: CQCCategoricalValues.cqc_sector,
-            CQCLClean.registration_status: CQCCategoricalValues.registration_status,
-            CQCLClean.dormancy: CQCCategoricalValues.dormancy_values,
-            CQCLClean.primary_service_type: CQCCategoricalValues.primary_service_types,
-            CQCLClean.contemporary_cssr: ONSCategoricalValues.cssrs,
-            CQCLClean.contemporary_region: ONSCategoricalValues.regions,
-            CQCLClean.current_cssr: ONSCategoricalValues.cssrs,
-            CQCLClean.current_region: ONSCategoricalValues.regions,
-            CQCLClean.current_rural_urban_ind_11: ONSCategoricalValues.rural_urban_indicators,
+            CQCL.care_home: CQCCategoricalValues.care_home_values,
+            CQCL.registration_status: CQCCategoricalValues.registration_status_raw,
+            CQCL.dormancy: CQCCategoricalValues.dormancy_values,
         },
         RuleName.distinct_values: {
-            CQCLClean.care_home: CQCDistinctValues.care_home_values,
-            CQCLClean.cqc_sector: CQCDistinctValues.cqc_sector_values,
-            CQCLClean.registration_status: CQCDistinctValues.registration_status_values,
-            CQCLClean.dormancy: CQCDistinctValues.dormancy_values,
-            CQCLClean.primary_service_type: CQCDistinctValues.primary_service_types,
-            CQCLClean.contemporary_cssr: ONSDistinctValues.cssrs,
-            CQCLClean.contemporary_region: ONSDistinctValues.regions,
-            CQCLClean.current_cssr: ONSDistinctValues.cssrs,
-            CQCLClean.current_region: ONSDistinctValues.regions,
-            CQCLClean.current_rural_urban_ind_11: ONSDistinctValues.rural_urban_indicators,
+            CQCL.care_home: CQCDistinctValues.care_home_values,
+            CQCL.registration_status: CQCDistinctValues.registration_status_raw_values,
+            CQCL.dormancy: CQCDistinctValues.dormancy_values,
         },
     }
