42118dff214113d5b1c20d48e5b88f6230886524
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-28 12:41:34 +0100

    Update job from pir cleaned to pir raw

--- jobs/validate_pir_raw_data.py
+++ jobs/validate_pir_raw_data.py
@@ -5,42 +5,37 @@ os.environ["SPARK_VERSION"] = "3.3"
 
 
 from utils import utils
-from utils.column_names.ind_cqc_pipeline_columns import (
-    PartitionKeys as Keys,
-)
-from utils.validation.validation_rules.pir_cleaned_validation_rules import (
-    PIRCleanedValidationRules as Rules,
+from utils.validation.validation_rules.pir_raw_validation_rules import (
+    PIRRawValidationRules as Rules,
 )
 from utils.validation.validation_utils import validate_dataset
 
-PartitionKeys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
-
 
 def main(
-    cleaned_cqc_pir_source: str,
+    raw_cqc_pir_source: str,
     report_destination: str,
 ):
-    cleaned_cqc_pir_df = utils.read_from_parquet(
-        cleaned_cqc_pir_source,
+    raw_cqc_pir_df = utils.read_from_parquet(
+        raw_cqc_pir_source,
     )
     rules = Rules.rules_to_check
 
-    check_result_df = validate_dataset(cleaned_cqc_pir_df, rules)
+    check_result_df = validate_dataset(raw_cqc_pir_df, rules)
 
     utils.write_to_parquet(check_result_df, report_destination, mode="overwrite")
 
 
 if __name__ == "__main__":
-    print("Spark job 'validate_pir_api_cleaned_data' starting...")
+    print("Spark job 'validate_pir_api_raw_data' starting...")
     print(f"Job parameters: {sys.argv}")
 
     (
-        cleaned_cqc_pir_source,
+        raw_cqc_pir_source,
         report_destination,
     ) = utils.collect_arguments(
         (
-            "--cleaned_cqc_pir_source",
-            "Source s3 directory for parquet pir api cleaned dataset",
+            "--raw_cqc_pir_source",
+            "Source s3 directory for parquet pir api raw dataset",
         ),
         (
             "--report_destination",
@@ -49,7 +44,7 @@ if __name__ == "__main__":
     )
     try:
         main(
-            cleaned_cqc_pir_source,
+            raw_cqc_pir_source,
             report_destination,
         )
     finally:
@@ -58,4 +53,4 @@ if __name__ == "__main__":
             spark.sparkContext._gateway.shutdown_callback_server()
         spark.stop()
 
-    print("Spark job 'validate_pir_api_cleaned_data' complete")
+    print("Spark job 'validate_pir_api_raw_data' complete")
