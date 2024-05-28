00cc4fed288b9f31c462890754e359d59747ae43
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-28 12:19:27 +0100

    Update tests from providers api cleaned to providers api raw

--- tests/unit/test_validate_providers_api_raw_data.py
+++ tests/unit/test_validate_providers_api_raw_data.py
@@ -2,17 +2,16 @@ import unittest
 
 from unittest.mock import Mock, patch
 
-import jobs.validate_providers_api_cleaned_data as job
+import jobs.validate_providers_api_raw_data as job
 
-from tests.test_file_data import ValidateProvidersAPICleanedData as Data
-from tests.test_file_schemas import ValidateProvidersAPICleanedData as Schemas
+from tests.test_file_data import ValidateProvidersAPIRawData as Data
+from tests.test_file_schemas import ValidateProvidersAPIRawData as Schemas
 
 from utils import utils
 
 
-class ValidateProvidersAPICleanedDatasetTests(unittest.TestCase):
+class ValidateProvidersAPIRawDatasetTests(unittest.TestCase):
     TEST_RAW_CQC_PROVIDER_SOURCE = "some/directory"
-    TEST_CQC_PROVIDERS_API_CLEANED_SOURCE = "some/other/directory"
     TEST_DESTINATION = "some/other/other/directory"
 
     def setUp(self) -> None:
@@ -21,16 +20,13 @@ class ValidateProvidersAPICleanedDatasetTests(unittest.TestCase):
             Data.raw_cqc_providers_rows,
             Schemas.raw_cqc_providers_schema,
         )
-        self.test_cleaned_cqc_providers_df = self.spark.createDataFrame(
-            Data.cleaned_cqc_providers_rows, Schemas.cleaned_cqc_providers_schema
-        )
 
     def tearDown(self) -> None:
         if self.spark.sparkContext._gateway:
             self.spark.sparkContext._gateway.shutdown_callback_server()
 
 
-class MainTests(ValidateProvidersAPICleanedDatasetTests):
+class MainTests(ValidateProvidersAPIRawDatasetTests):
     def setUp(self) -> None:
         return super().setUp()
 
@@ -41,36 +37,15 @@ class MainTests(ValidateProvidersAPICleanedDatasetTests):
         read_from_parquet_patch: Mock,
         write_to_parquet_patch: Mock,
     ):
-        read_from_parquet_patch.side_effect = [
-            self.test_raw_cqc_provider_df,
-            self.test_cleaned_cqc_providers_df,
-        ]
+        read_from_parquet_patch.return_value = self.test_raw_cqc_provider_df
 
         job.main(
             self.TEST_RAW_CQC_PROVIDER_SOURCE,
-            self.TEST_CQC_PROVIDERS_API_CLEANED_SOURCE,
             self.TEST_DESTINATION,
         )
 
-        self.assertEqual(read_from_parquet_patch.call_count, 2)
+        self.assertEqual(read_from_parquet_patch.call_count, 1)
         self.assertEqual(write_to_parquet_patch.call_count, 1)
-
-
-class CalculateExpectedSizeofDataset(ValidateProvidersAPICleanedDatasetTests):
-    def setUp(self) -> None:
-        return super().setUp()
-
-    def test_calculate_expected_size_of_cleaned_cqc_providers_dataset_returns_correct_row_count(
-        self,
-    ):
-        test_df = self.spark.createDataFrame(
-            Data.calculate_expected_size_rows, Schemas.calculate_expected_size_schema
-        )
-        expected_row_count = 4
-        returned_row_count = (
-            job.calculate_expected_size_of_cleaned_cqc_providers_dataset(test_df)
-        )
-        self.assertEqual(returned_row_count, expected_row_count)
 
 
 if __name__ == "__main__":
