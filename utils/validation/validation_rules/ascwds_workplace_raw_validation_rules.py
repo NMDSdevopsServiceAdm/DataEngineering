b6071c9bfa9e05ed9a2cfbc17a5226d307e8f6c4
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-28 10:58:33 +0100

    Copy files from ascwds workplace cleaned as starting point

---
+++ utils/validation/validation_rules/ascwds_workplace_raw_validation_rules.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+from utils.column_names.cleaned_data_files.ascwds_workplace_cleaned_values import (
+    AscwdsWorkplaceCleanedColumns as AWPClean,
+)
+
+from utils.validation.validation_rule_names import RuleNames as RuleName
+
+
+@dataclass
+class ASCWDSWorkplaceCleanedValidationRules:
+    rules_to_check = {
+        RuleName.complete_columns: [
+            AWPClean.organisation_id,
+            AWPClean.ascwds_workplace_import_date,
+            AWPClean.establishment_id,
+        ],
+        RuleName.index_columns: [
+            AWPClean.establishment_id,
+            AWPClean.ascwds_workplace_import_date,
+        ],
+        RuleName.max_values: {
+            AWPClean.total_staff_bounded: 3000,
+            AWPClean.worker_records_bounded: 3000,
+        },
+        RuleName.min_values: {
+            AWPClean.total_staff_bounded: 1,
+            AWPClean.worker_records_bounded: 1,
+        },
+    }
