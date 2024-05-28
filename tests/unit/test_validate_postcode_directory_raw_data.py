f849c5d8c15b99596a49549c2550c06347e20819
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-28 14:06:38 +0100

    Update tests from postcode directory cleaned to postcode directory raw

--- tests/unit/test_validate_postcode_directory_raw_data.py
+++ tests/unit/test_validate_postcode_directory_raw_data.py
@@ -2,17 +2,16 @@ import unittest
 
 from unittest.mock import Mock, patch
 
-import jobs.validate_postcode_directory_cleaned_data as job
+import jobs.validate_postcode_directory_raw_data as job
 
-from tests.test_file_data import ValidatePostcodeDirectoryCleanedData as Data
-from tests.test_file_schemas import ValidatePostcodeDirectoryCleanedData as Schemas
+from tests.test_file_data import ValidatePostcodeDirectoryRawData as Data
+from tests.test_file_schemas import ValidatePostcodeDirectoryRawData as Schemas
 
 from utils import utils
 
 
-class ValidatePostcodeDirectoryCleanedDatasetTests(unittest.TestCase):
+class ValidatePostcodeDirectoryRawDatasetTests(unittest.TestCase):
     TEST_RAW_POSTCODE_DIRECTORY_SOURCE = "some/directory"
-    TEST_POSTCODE_DIRECTORY_CLEANED_SOURCE = "some/other/directory"
     TEST_DESTINATION = "some/other/other/directory"
 
     def setUp(self) -> None:
@@ -21,17 +20,13 @@ class ValidatePostcodeDirectoryCleanedDatasetTests(unittest.TestCase):
             Data.raw_postcode_directory_rows,
             Schemas.raw_postcode_directory_schema,
         )
-        self.test_cleaned_postcode_directory_df = self.spark.createDataFrame(
-            Data.cleaned_postcode_directory_rows,
-            Schemas.cleaned_postcode_directory_schema,
-        )
 
     def tearDown(self) -> None:
         if self.spark.sparkContext._gateway:
             self.spark.sparkContext._gateway.shutdown_callback_server()
 
 
-class MainTests(ValidatePostcodeDirectoryCleanedDatasetTests):
+class MainTests(ValidatePostcodeDirectoryRawDatasetTests):
     def setUp(self) -> None:
         return super().setUp()
 
@@ -42,36 +37,15 @@ class MainTests(ValidatePostcodeDirectoryCleanedDatasetTests):
         read_from_parquet_patch: Mock,
         write_to_parquet_patch: Mock,
     ):
-        read_from_parquet_patch.side_effect = [
-            self.test_raw_postcode_directory_df,
-            self.test_cleaned_postcode_directory_df,
-        ]
+        read_from_parquet_patch.return_value = (self.test_raw_postcode_directory_df,)
 
         job.main(
             self.TEST_RAW_POSTCODE_DIRECTORY_SOURCE,
-            self.TEST_POSTCODE_DIRECTORY_CLEANED_SOURCE,
             self.TEST_DESTINATION,
         )
 
-        self.assertEqual(read_from_parquet_patch.call_count, 2)
+        self.assertEqual(read_from_parquet_patch.call_count, 1)
         self.assertEqual(write_to_parquet_patch.call_count, 1)
-
-
-class CalculateExpectedSizeofDataset(ValidatePostcodeDirectoryCleanedDatasetTests):
-    def setUp(self) -> None:
-        return super().setUp()
-
-    def test_calculate_expected_size_of_cleaned_postcode_directory_dataset_returns_correct_row_count(
-        self,
-    ):
-        test_df = self.spark.createDataFrame(
-            Data.calculate_expected_size_rows, Schemas.calculate_expected_size_schema
-        )
-        expected_row_count = 4
-        returned_row_count = (
-            job.calculate_expected_size_of_cleaned_postcode_directory_dataset(test_df)
-        )
-        self.assertEqual(returned_row_count, expected_row_count)
 
 
 if __name__ == "__main__":
