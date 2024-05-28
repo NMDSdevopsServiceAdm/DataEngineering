449336e4df00412da2625663f746f1b094cd6bd6
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-28 12:55:07 +0100

    Update rules from pir cleaned to pir raw

--- utils/validation/validation_rules/pir_raw_validation_rules.py
+++ utils/validation/validation_rules/pir_raw_validation_rules.py
@@ -1,39 +1,30 @@
 from dataclasses import dataclass
 
-from utils.column_names.cleaned_data_files.cqc_pir_cleaned_values import (
-    CqcPIRCleanedColumns as CQCPIRClean,
+from utils.column_names.raw_data_files.cqc_pir_columns import (
+    CqcPirColumns as CQCPIR,
 )
-
+from utils.column_names.ind_cqc_pipeline_columns import (
+    PartitionKeys as Keys,
+)
 from utils.validation.validation_rule_names import RuleNames as RuleName
-from utils.validation.categorical_column_values import (
-    CQCCategoricalValues,
-    CQCDistinctValues,
-)
 
 
 @dataclass
 class PIRCleanedValidationRules:
     rules_to_check = {
         RuleName.complete_columns: [
-            CQCPIRClean.cqc_pir_import_date,
-            CQCPIRClean.location_id,
-            CQCPIRClean.people_directly_employed,
-            CQCPIRClean.care_home,
+            Keys.import_date,
+            CQCPIR.location_id,
+            CQCPIR.people_directly_employed,
         ],
         RuleName.index_columns: [
-            CQCPIRClean.location_id,
-            CQCPIRClean.cqc_pir_import_date,
+            CQCPIR.location_id,
+            Keys.import_date,
         ],
         RuleName.max_values: {
-            CQCPIRClean.people_directly_employed: 10000,
+            CQCPIR.people_directly_employed: 10000,
         },
         RuleName.min_values: {
-            CQCPIRClean.people_directly_employed: 0,
-        },
-        RuleName.categorical_values_in_columns: {
-            CQCPIRClean.care_home: CQCCategoricalValues.care_home_values,
-        },
-        RuleName.distinct_values: {
-            CQCPIRClean.care_home: CQCDistinctValues.care_home_values,
+            CQCPIR.people_directly_employed: 0,
         },
     }
