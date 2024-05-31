6ab5273c7d19939cd651d587a9fe25abdd8a7013
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-05-31 15:07:35 +0100

    Format code

--- utils/column_values/cqc_locations_values.py
+++ utils/column_values/cqc_locations_values.py
@@ -85,9 +85,7 @@ class Services:
     hospice_services: str = "Hospice services"
     domiciliary_care_service: str = "Domiciliary care service"
     remote_clinical_advice_service: str = "Remote clinical advice service"
-    acute_services_without_overnight_beds: str = (
-        "Acute services without overnight beds / listed acute services with or without overnight beds"
-    )
+    acute_services_without_overnight_beds: str = "Acute services without overnight beds / listed acute services with or without overnight beds"
     specialist_college_service: str = "Specialist college service"
     ambulance_service: str = "Ambulance service"
     extra_care_housing_services: str = "Extra Care housing services"
@@ -117,9 +115,7 @@ class Services:
     rehabilitation_services: str = "Rehabilitation services"
     doctors_treatment_service: str = "Doctors treatment service"
     hospice_services_at_home: str = "Hospice services at home"
-    hospital_services_for_people_with_mental_health_needs: str = (
-        "Hospital services for people with mental health needs, learning disabilities and problems with substance misuse"
-    )
+    hospital_services_for_people_with_mental_health_needs: str = "Hospital services for people with mental health needs, learning disabilities and problems with substance misuse"
 
     values_list = [
         care_home_service_with_nursing,
