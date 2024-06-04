74de46e2c765916fcec62c6462ac8fae41bd8b2e
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-06-04 15:17:10 +0100

    Update job for coverage

---
+++ jobs/merge_coverage_data.py
@@ -0,0 +1,156 @@
+import sys
+
+from pyspark.sql.dataframe import DataFrame
+
+from utils import utils
+import utils.cleaning_utils as cUtils
+from utils.column_names.cleaned_data_files.cqc_location_cleaned import (
+    CqcLocationCleanedColumns as CQCLClean,
+)
+from utils.column_names.cleaned_data_files.ons_cleaned import (
+    OnsCleanedColumns as ONSClean,
+)
+from utils.column_names.cleaned_data_files.ascwds_workplace_cleaned import (
+    AscwdsWorkplaceCleanedColumns as AWPClean,
+)
+from utils.column_names.ind_cqc_pipeline_columns import (
+    PartitionKeys as Keys,
+)
+
+PartitionKeys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
+
+cleaned_cqc_locations_columns_to_import = [
+    CQCLClean.cqc_location_import_date,
+    CQCLClean.location_id,
+    CQCLClean.name,
+    CQCLClean.provider_id,
+    CQCLClean.provider_name,
+    CQCLClean.cqc_sector,
+    CQCLClean.registration_status,
+    CQCLClean.registration_date,
+    CQCLClean.dormancy,
+    CQCLClean.care_home,
+    CQCLClean.number_of_beds,
+    CQCLClean.regulated_activities,
+    CQCLClean.gac_service_types,
+    CQCLClean.services_offered,
+    CQCLClean.specialisms,
+    CQCLClean.primary_service_type,
+    ONSClean.contemporary_ons_import_date,
+    ONSClean.contemporary_cssr,
+    ONSClean.contemporary_icb,
+    ONSClean.contemporary_region,
+    ONSClean.current_ons_import_date,
+    ONSClean.current_cssr,
+    ONSClean.current_icb,
+    ONSClean.current_region,
+    ONSClean.current_rural_urban_ind_11,
+    Keys.year,
+    Keys.month,
+    Keys.day,
+    Keys.import_date,
+]
+cleaned_ascwds_workplace_columns_to_import = [
+    AWPClean.ascwds_workplace_import_date,
+    AWPClean.location_id,
+    AWPClean.establishment_id,
+    AWPClean.organisation_id,
+    AWPClean.total_staff_bounded,
+    AWPClean.worker_records_bounded,
+]
+
+
+def main(
+    cleaned_cqc_location_source: str,
+    workplace_for_reconciliation_source: str,
+    merged_coverage_destination: str,
+):
+    spark = utils.get_spark()
+    spark.sql(
+        "set spark.sql.broadcastTimeout = 2000"
+    )  # TODO: Test if this is needed still
+
+    cqc_location_df = utils.read_from_parquet(
+        cleaned_cqc_location_source,
+        selected_columns=cleaned_cqc_locations_columns_to_import,
+    )
+
+    ascwds_workplace_df = utils.read_from_parquet(
+        workplace_for_reconciliation_source,
+        selected_columns=cleaned_ascwds_workplace_columns_to_import,
+    )
+
+    merged_coverage_df = join_ascwds_data_into_cqc_location_df(
+        cqc_location_df,
+        ascwds_workplace_df,
+        CQCLClean.cqc_location_import_date,
+        AWPClean.ascwds_workplace_import_date,
+    )
+
+    utils.write_to_parquet(
+        merged_coverage_df,
+        merged_coverage_destination,
+        mode="overwrite",
+        partitionKeys=PartitionKeys,
+    )
+
+
+def join_ascwds_data_into_cqc_location_df(
+    cqc_location_df: DataFrame,
+    ascwds_workplace_df: DataFrame,
+    cqc_location_import_date_column: str,
+    ascwds_workplace_import_date_column: str,
+) -> DataFrame:
+    merged_coverage_ascwds_df_with_ascwds_workplace_import_date = (
+        cUtils.add_aligned_date_column(
+            cqc_location_df,
+            ascwds_workplace_df,
+            cqc_location_import_date_column,
+            ascwds_workplace_import_date_column,
+        )
+    )
+
+    formatted_ascwds_workplace_df = ascwds_workplace_df.withColumnRenamed(
+        AWPClean.location_id, CQCLClean.location_id
+    )
+
+    merged_coverage_df = (
+        merged_coverage_ascwds_df_with_ascwds_workplace_import_date.join(
+            formatted_ascwds_workplace_df,
+            [CQCLClean.location_id, AWPClean.ascwds_workplace_import_date],
+            how="left",
+        )
+    )
+
+    return merged_coverage_df
+
+
+if __name__ == "__main__":
+    print("Spark job 'merge_coverage_data' starting...")
+    print(f"Job parameters: {sys.argv}")
+
+    (
+        cleaned_cqc_location_source,
+        workplace_for_reconciliation_source,
+        merged_coverage_destination,
+    ) = utils.collect_arguments(
+        (
+            "--cleaned_cqc_location_source",
+            "Source s3 directory for parquet CQC locations cleaned dataset",
+        ),
+        (
+            "--workplace_for_reconciliation_source",
+            "Source s3 directory for parquet ASCWDS workplace for reconciliation dataset",
+        ),
+        (
+            "--merged_coverage_destination",
+            "Destination s3 directory for parquet",
+        ),
+    )
+    main(
+        cleaned_cqc_location_source,
+        workplace_for_reconciliation_source,
+        merged_coverage_destination,
+    )
+
+    print("Spark job 'merge_coverage_data' complete")
---
+++ tests/unit/test_merge_coverage_data.py
@@ -0,0 +1,146 @@
+import unittest
+
+from unittest.mock import ANY, Mock, patch
+
+import jobs.merge_ind_cqc_data as job
+
+from tests.test_file_data import MergeIndCQCData as Data
+from tests.test_file_schemas import MergeIndCQCData as Schemas
+
+from utils import utils
+from utils.column_names.ind_cqc_pipeline_columns import (
+    PartitionKeys as Keys,
+)
+from utils.column_names.cleaned_data_files.cqc_location_cleaned import (
+    CqcLocationCleanedColumns as CQCLClean,
+)
+from utils.column_names.cleaned_data_files.ascwds_workplace_cleaned import (
+    AscwdsWorkplaceCleanedColumns as AWPClean,
+)
+
+
+class MergeIndCQCDatasetTests(unittest.TestCase):
+    TEST_CQC_LOCATION_SOURCE = "some/directory"
+    TEST_CQC_PIR_SOURCE = "some/other/directory"
+    TEST_ASCWDS_WORKPLACE_SOURCE = "some/other/directory"
+    TEST_DESTINATION = "some/other/directory"
+    partition_keys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
+
+    def setUp(self) -> None:
+        self.spark = utils.get_spark()
+        self.test_clean_cqc_location_df = self.spark.createDataFrame(
+            Data.clean_cqc_location_for_merge_rows,
+            Schemas.clean_cqc_location_for_merge_schema,
+        )
+        self.test_clean_cqc_pir_df = self.spark.createDataFrame(
+            Data.clean_cqc_pir_rows, Schemas.clean_cqc_pir_schema
+        )
+        self.test_clean_ascwds_workplace_df = self.spark.createDataFrame(
+            Data.clean_ascwds_workplace_for_merge_rows,
+            Schemas.clean_ascwds_workplace_for_merge_schema,
+        )
+
+    @patch("jobs.merge_ind_cqc_data.join_ascwds_data_into_merged_df")
+    @patch(
+        "jobs.merge_ind_cqc_data.filter_df_to_independent_sector_only",
+        wraps=job.filter_df_to_independent_sector_only,
+    )
+    @patch("utils.utils.write_to_parquet")
+    @patch("utils.utils.read_from_parquet")
+    def test_main_runs(
+        self,
+        read_from_parquet_patch: Mock,
+        write_to_parquet_patch: Mock,
+        filter_df_to_independent_sector_only: Mock,
+        join_ascwds_data_into_merged_df: Mock,
+    ):
+        read_from_parquet_patch.side_effect = [
+            self.test_clean_cqc_location_df,
+            self.test_clean_ascwds_workplace_df,
+            self.test_clean_cqc_pir_df,
+        ]
+
+        job.main(
+            self.TEST_CQC_LOCATION_SOURCE,
+            self.TEST_CQC_PIR_SOURCE,
+            self.TEST_ASCWDS_WORKPLACE_SOURCE,
+            self.TEST_DESTINATION,
+        )
+
+        self.assertEqual(read_from_parquet_patch.call_count, 3)
+
+        filter_df_to_independent_sector_only.assert_called_once_with(
+            self.test_clean_cqc_location_df
+        )
+        join_ascwds_data_into_merged_df.assert_called_once()
+
+        write_to_parquet_patch.assert_called_once_with(
+            ANY,
+            self.TEST_DESTINATION,
+            mode="overwrite",
+            partitionKeys=self.partition_keys,
+        )
+
+    def test_filter_df_to_independent_sector_only_keeps_independent_locations(
+        self,
+    ):
+        test_df = self.spark.createDataFrame(
+            Data.cqc_sector_rows, Schemas.cqc_sector_schema
+        )
+
+        returned_ind_cqc_df = job.filter_df_to_independent_sector_only(test_df)
+        returned_ind_cqc_data = returned_ind_cqc_df.collect()
+
+        expected_ind_cqc_data = self.spark.createDataFrame(
+            Data.expected_cqc_sector_rows, Schemas.cqc_sector_schema
+        ).collect()
+
+        self.assertEqual(returned_ind_cqc_data, expected_ind_cqc_data)
+
+    def test_join_ascwds_data_into_merged_df(self):
+        returned_df = job.join_ascwds_data_into_merged_df(
+            self.test_clean_cqc_location_df,
+            self.test_clean_ascwds_workplace_df,
+            CQCLClean.cqc_location_import_date,
+            AWPClean.ascwds_workplace_import_date,
+        )
+
+        expected_merged_df = self.spark.createDataFrame(
+            Data.expected_cqc_and_ascwds_merged_rows,
+            Schemas.expected_cqc_and_ascwds_merged_schema,
+        )
+
+        returned_data = returned_df.sort(
+            CQCLClean.cqc_location_import_date, CQCLClean.location_id
+        ).collect()
+        expected_data = expected_merged_df.sort(
+            CQCLClean.cqc_location_import_date, CQCLClean.location_id
+        ).collect()
+
+        self.assertEqual(returned_data, expected_data)
+
+    def test_join_pir_data_into_merged_df(self):
+        returned_df = job.join_pir_data_into_merged_df(
+            self.test_clean_cqc_location_df,
+            self.test_clean_cqc_pir_df,
+        )
+
+        expected_merged_df = self.spark.createDataFrame(
+            Data.expected_merged_cqc_and_pir,
+            Schemas.expected_cqc_and_pir_merged_schema,
+        )
+
+        returned_data = returned_df.sort(
+            CQCLClean.cqc_location_import_date, CQCLClean.location_id
+        ).collect()
+        expected_data = (
+            expected_merged_df.select(returned_df.columns)
+            .sort(CQCLClean.cqc_location_import_date, CQCLClean.location_id)
+            .collect()
+        )
+
+        self.assertEqual(returned_data, expected_data)
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
