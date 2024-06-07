6058e79ef7c7ba95e7418f4684a55d2c67e1d29e
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-06-07 11:52:42 +0100

    Add docstring with rationale

--- utils/raw_data_adjustments.py
+++ utils/raw_data_adjustments.py
@@ -6,6 +6,13 @@ from utils.column_names.cleaned_data_files.ascwds_worker_cleaned import (
 
 
 def remove_duplicate_worker_in_raw_worker_data(raw_worker_df: DataFrame) -> DataFrame:
+    """
+    This function removes a record known to be a duplicate in the raw data.
+
+    The worker ID exists in other files but not under this establishment
+    ID. No worker IDs should be duplicated accross establishments, so this
+    record was selected for removal.
+    """
     raw_worker_df = raw_worker_df.where(
         (raw_worker_df[AWKClean.worker_id] != "1737540")
         | (raw_worker_df[AWKClean.import_date] != "20230802")
