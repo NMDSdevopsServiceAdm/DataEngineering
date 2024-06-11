83c20d7672a1f9567667f6db22cead1962456111
Roy Price <roy.price@skillsforcare.org.uk>
2024-06-11 13:14:47 +0100

    Changed la name replace function to use dict instead of two lists.

--- utils/direct_payments_utils/direct_payments_configuration.py
+++ utils/direct_payments_utils/direct_payments_configuration.py
@@ -61,20 +61,11 @@ class EstimatePeriodAsDate:
 
 @dataclass
 class DirectPaymentsMisspelledLaNames:
-    LIST_OF_INCORRECT_LA_NAMES = [
-        "Bath & N E Somerset",
-        "Blackburn",
-        "Bournemouth, Christchurch and Poole",
-        "East Riding",
-        "Medway Towns",
-        "Southend",
-    ]
-
-    LIST_OF_CORRECT_LA_NAMES = [
-        "Bath and North East Somerset",
-        "Blackburn with Darwen",
-        "Bournemouth Christchurch and Poole",
-        "East Riding of Yorkshire",
-        "Medway",
-        "Southend on Sea",
-    ]
+    DICT_TO_CORRECT_LA_NAMES = {
+        "Bath & N E Somerset": "Bath and North East Somerset",
+        "Blackburn": "Blackburn with Darwen",
+        "Bournemouth, Christchurch and Poole": "Bournemouth Christchurch and Poole",
+        "East Riding": "East Riding of Yorkshire",
+        "Medway Towns": "Medway",
+        "Southend": "Southend on Sea",
+    }
