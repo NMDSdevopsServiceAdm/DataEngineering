ffb32609f53f79c68a0a1b74dcb6f4570d050cfb
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-07-05 11:36:48 +0100

    Refactor primary service type values

--- tests/test_file_data.py
+++ tests/test_file_data.py
@@ -52,7 +52,7 @@ from utils.validation.validation_rule_names import RuleNames as RuleName
 class CreateJobEstimatesDiagnosticsData:
     # fmt: off
     estimate_jobs_rows = [
-        ("location_1", date(2023, 4, 1), 40.0, 40.0, Values.care_home_with_nursing, 60.9, 23.4, 45.1, None, None, 40.0, 45,),
+        ("location_1", date(2023, 4, 1), 40.0, 40.0, PrimaryServiceType.care_home_with_nursing, 60.9, 23.4, 45.1, None, None, 40.0, 45,),
     ]
     capacity_tracker_care_home_rows = [
         ("location_1", 8.0, 12.0, 15.0, 1.0, 3.0, 2.0),
@@ -61,30 +61,30 @@ class CreateJobEstimatesDiagnosticsData:
         ("location_2", 67.0),
     ]
     prepare_capacity_tracker_care_home_rows = [
-        ("location_1", Values.care_home_with_nursing, 8.0, 12.0, 15.0, 1.0, 3.0, 2.0, None,),
-        ("location_2", Values.non_residential, None, None, None, None, None, None, 30.0,),
+        ("location_1", PrimaryServiceType.care_home_with_nursing, 8.0, 12.0, 15.0, 1.0, 3.0, 2.0, None,),
+        ("location_2", PrimaryServiceType.non_residential, None, None, None, None, None, None, 30.0,),
     ]
     prepare_capacity_tracker_non_residential_rows = [
-        ("location_1", Values.care_home_with_nursing, 8.0, 12.0, 15.0, 1.0, 3.0, 2.0, None,),
-        ("location_2", Values.non_residential, None, None, None, None, None, None, 75.0,),
+        ("location_1", PrimaryServiceType.care_home_with_nursing, 8.0, 12.0, 15.0, 1.0, 3.0, 2.0, None,),
+        ("location_2", PrimaryServiceType.non_residential, None, None, None, None, None, None, 75.0,),
     ]
     calculate_residuals_rows = [
-        ("location_2", 40.0, 40.0, Values.non_residential, 60.9, 23.4, 45.1, None, None, 40.0, 40, None, 40.0,),
-        ("location_3", 40.0, 40.0, Values.non_residential, 60.9, 23.4, 45.1, None, None, 40.0, 45, 41.0, None,),
-        ("location_4", None, None, Values.non_residential, 60.9, 23.4, None, None, None, 60.0, 45, None, None,),
-        ("location_5", None, None, Values.care_home_with_nursing, 60.9, 23.4, None, None, None, 60.9, None, 50.0, None,),
+        ("location_2", 40.0, 40.0, PrimaryServiceType.non_residential, 60.9, 23.4, 45.1, None, None, 40.0, 40, None, 40.0,),
+        ("location_3", 40.0, 40.0, PrimaryServiceType.non_residential, 60.9, 23.4, 45.1, None, None, 40.0, 45, 41.0, None,),
+        ("location_4", None, None, PrimaryServiceType.non_residential, 60.9, 23.4, None, None, None, 60.0, 45, None, None,),
+        ("location_5", None, None, PrimaryServiceType.care_home_with_nursing, 60.9, 23.4, None, None, None, 60.9, None, 50.0, None,),
     ]
     run_residuals_rows = [
-        ("location_1", None, None, Values.care_home_with_nursing, 60.9, 23.4, None, None, None, 60.9, None, None, None,),
-        ("location_2", 40.0, 40.0, Values.non_residential, 60.9, 23.4, 45.1, None, None, 40.0, None, None, 40.0,),
-        ("location_3", 40.0, 40.0, Values.care_home_without_nursing, 60.9, 23.4, 45.1, None, None, 40.0, 45, 41.0, None,),
-        ("location_4", None, None, Values.care_home_with_nursing, 60.9, 23.4, None, None, None, 60.9, 45, None, None,),
-        ("location_5", None, None, Values.care_home_with_nursing, 60.9, 23.4, None, None, None, 60.9, None, 50.0, None,),
-        ("location_6", None, None, Values.care_home_with_nursing, 60.9, 23.4, None, None, None, 60.9, 45, 50.0, None,),
-        ("location_7", None, None, Values.non_residential, 60.9, None, None, None, 40.0, 60.9, 45, None, None,),
-        ("location_8", None, None, Values.non_residential, 60.9, None, None, None, 40.0, 60.9, None, None, 40.0,),
-        ("location_9", None, None, Values.non_residential, 60.9, None, None, None, 40.0, 60.9, 45, None, 40.0,),
-        ("location_10", None, None, Values.care_home_with_nursing, 60.9, 23.4, None, None, None, 60.9, None, None, None,),
+        ("location_1", None, None, PrimaryServiceType.care_home_with_nursing, 60.9, 23.4, None, None, None, 60.9, None, None, None,),
+        ("location_2", 40.0, 40.0, PrimaryServiceType.non_residential, 60.9, 23.4, 45.1, None, None, 40.0, None, None, 40.0,),
+        ("location_3", 40.0, 40.0, PrimaryServiceType.care_home_only, 60.9, 23.4, 45.1, None, None, 40.0, 45, 41.0, None,),
+        ("location_4", None, None, PrimaryServiceType.care_home_with_nursing, 60.9, 23.4, None, None, None, 60.9, 45, None, None,),
+        ("location_5", None, None, PrimaryServiceType.care_home_with_nursing, 60.9, 23.4, None, None, None, 60.9, None, 50.0, None,),
+        ("location_6", None, None, PrimaryServiceType.care_home_with_nursing, 60.9, 23.4, None, None, None, 60.9, 45, 50.0, None,),
+        ("location_7", None, None, PrimaryServiceType.non_residential, 60.9, None, None, None, 40.0, 60.9, 45, None, None,),
+        ("location_8", None, None, PrimaryServiceType.non_residential, 60.9, None, None, None, 40.0, 60.9, None, None, 40.0,),
+        ("location_9", None, None, PrimaryServiceType.non_residential, 60.9, None, None, None, 40.0, 60.9, 45, None, 40.0,),
+        ("location_10", None, None, PrimaryServiceType.care_home_with_nursing, 60.9, 23.4, None, None, None, 60.9, None, None, None,),
     ]
     residuals_rows = [
         ("location_1", 0.0, 0.0,),
--- utils/diagnostics_utils/diagnostics_meta_data.py
+++ utils/diagnostics_utils/diagnostics_meta_data.py
@@ -9,10 +9,6 @@ from utils.column_names.capacity_tracker_columns import CapacityTrackerColumns a
 
 @dataclass
 class Variables:
-    care_home_with_nursing: str = "Care home with nursing"
-    care_home_without_nursing: str = "Care home without nursing"
-    non_residential: str = "non-residential"
-
     asc_wds: str = "ascwds"
     capacity_tracker: str = "capacity_tracker"
     pir: str = "pir"
