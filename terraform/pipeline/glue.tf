5913cfdadbb529cc84bb2d1c0e156b4823a7759f
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-07-08 15:54:34 +0100

    Update glue module

--- terraform/pipeline/glue.tf
+++ terraform/pipeline/glue.tf
@@ -152,16 +152,17 @@ module "clean_ons_data_job" {
 }
 
 
-module "prepare_non_res_ascwds_inc_dormancy_ind_cqc_features_job" {
+module "prepare_non_res_ascwds_ind_cqc_features_job" {
   source          = "../modules/glue-job"
-  script_name     = "prepare_non_res_ascwds_inc_dormancy_ind_cqc_features.py"
+  script_name     = "prepare_non_res_ascwds_ind_cqc_features.py"
   glue_role       = aws_iam_role.sfc_glue_service_iam_role
   resource_bucket = module.pipeline_resources
   datasets_bucket = module.datasets_bucket
 
   job_parameters = {
-    "--ind_cqc_filled_posts_cleaned_source"                      = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=cleaned_ind_cqc_data/"
-    "--non_res_ascwds_inc_dormancy_ind_cqc_features_destination" = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=non_res_ascwds_inc_dormancy_ind_cqc_features/"
+    "--ind_cqc_filled_posts_cleaned_source"                          = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=cleaned_ind_cqc_data/"
+    "--non_res_ascwds_inc_dormancy_ind_cqc_features_destination"     = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=non_res_ascwds_inc_dormancy_ind_cqc_features/"
+    "--non_res_ascwds_without_dormancy_ind_cqc_features_destination" = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=non_res_ascwds_without_dormancy_ind_cqc_features/"
   }
 }
 
