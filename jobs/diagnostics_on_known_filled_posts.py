231d4219f50a963cfe110c7c55aa44bb8c5ecd31
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-07-16 12:12:04 +0100

    Make notes on job structure

--- jobs/diagnostics_on_known_filled_posts.py
+++ jobs/diagnostics_on_known_filled_posts.py
@@ -1,33 +1,29 @@
-from datetime import datetime, date
 import sys
 
-from pyspark.sql import SparkSession, DataFrame, functions as F
-from pyspark.sql.types import StringType
+from pyspark.sql import DataFrame, functions as F
 
 from utils import utils
-from utils.column_values.categorical_column_values import CareHome, DataSource
+from utils.column_values.categorical_column_values import (
+    CareHome,
+    DataSource,
+    PrimaryServiceType,
+)
 from utils.column_names.ind_cqc_pipeline_columns import (
     IndCqcColumns as IndCQC,
 )
-from utils.diagnostics_utils.diagnostics_meta_data import (
-    Variables as Values,
-    Prefixes,
-    CareWorkerToJobsRatio as Ratio,
-    ResidualsRequired,
-)
+
 
 estimate_filled_posts_columns: list = [
     IndCQC.location_id,
     IndCQC.cqc_location_import_date,
+    IndCQC.ascwds_filled_posts_clean,
     IndCQC.ascwds_filled_posts_dedup_clean,
     IndCQC.primary_service_type,
     IndCQC.rolling_average_model,
     IndCQC.care_home_model,
     IndCQC.extrapolation_care_home_model,
     IndCQC.interpolation_model,
-    IndCQC.non_res_model,
     IndCQC.estimate_filled_posts,
-    IndCQC.people_directly_employed,
 ]
 
 
@@ -41,6 +37,22 @@ def main(
     filled_posts_df: DataFrame = utils.read_from_parquet(
         estimate_filled_posts_source, estimate_filled_posts_columns
     )
+
+    # filter to where ascwds clean is not null
+
+    # reshape df so that cols are: location id, cqc_location_import date, service, ascwds_filled-posts_clean, estimate_source, estimate_value
+
+    # drop rows where estimate value is missing
+
+    # create windows for model/ service splits
+
+    # calculate metrics for distribution (mean, sd, kurtosis, skewness)
+
+    # calculate residuals (abs, %)
+
+    # calculate aggregate residuals (avg abs, avg %, max, % within abs of actual, % within % of actual)
+
+    # calculate date_diff for interpolation and extrapolation
 
 
 if __name__ == "__main__":
