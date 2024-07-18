2ec59dbed4e6ae12b15b2fef1b96fa3ed079e085
Roy Price <roy.price@skillsforcare.org.uk>
2024-07-18 17:04:54 +0100

    Added test data to show the filter function only filters the given column.

--- tests/test_file_data.py
+++ tests/test_file_data.py
@@ -5174,11 +5174,19 @@ class DiagnosticsOnKnownFilledPostsData:
             "loc 2",
             None,
         ),
+        (
+            None,
+            1.0,
+        ),
     ]
 
     expected_filter_to_known_values_rows = [
         (
             "loc 1",
+            1.0,
+        ),
+        (
+            None,
             1.0,
         ),
     ]
