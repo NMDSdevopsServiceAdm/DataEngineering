175862ea7496ff1503006e397f3646728dc69105
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-07-26 09:23:05 +0100

    Add test data where estimate is higher than known value

--- tests/test_file_data.py
+++ tests/test_file_data.py
@@ -5622,13 +5622,17 @@ class DiagnosticsOnKnownFilledPostsData:
 
     calculate_residuals_rows = [
         ("loc 1", 10.0, 5.0),
+        ("loc 2", 5.0, 10.0),
     ]
     expected_calculate_absolute_residual_rows = [
         ("loc 1", 10.0, 5.0, 5.0),
+        ("loc 2", 5.0, 10.0, 5.0),
     ]
     expected_calculate_percentage_residual_rows = [
         ("loc 1", 10.0, 5.0, -1.0),
+        ("loc 2", 5.0, 10.0, 0.5),
     ]
     expected_calculate_residuals_rows = [
         ("loc 1", 10.0, 5.0, 5.0, -1.0),
+        ("loc 2", 5.0, 10.0, 5.0, 0.5),
     ]
