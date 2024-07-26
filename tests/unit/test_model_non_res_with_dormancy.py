f5e6c9d83da68fe1363b78e290ada5ff9e78be7a
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-07-26 10:05:53 +0100

    Update test to non res with dormancy

--- tests/unit/test_model_non_res_with_dormancy.py
+++ tests/unit/test_model_non_res_with_dormancy.py
@@ -4,53 +4,60 @@ import warnings
 from datetime import date
 
 from utils import utils
-import utils.estimate_filled_posts.models.care_homes as job
+import utils.estimate_filled_posts.models.non_res_with_dormancy as job
 from utils.column_names.ind_cqc_pipeline_columns import (
     IndCqcColumns as IndCqc,
 )
-from tests.test_file_data import ModelCareHomes as Data
-from tests.test_file_schemas import ModelCareHomes as Schemas
+from tests.test_file_data import ModelNonResWithDormancy as Data
+from tests.test_file_schemas import ModelNonResWithDormancy as Schemas
 
 
-class TestModelCareHome(unittest.TestCase):
-    CARE_HOME_MODEL = "tests/test_models/care_home_filled_posts_prediction/1.0.0/"
+class TestModelNonResWithDormancy(unittest.TestCase):
+    NON_RES_WITH_DORMANCY_MODEL = (
+        "tests/test_models/non_residential_with_dormancy_prediction/1.0.0/"
+    )
     METRICS_DESTINATION = "metrics destination"
 
     def setUp(self):
         self.spark = utils.get_spark()
-        self.care_homes_cleaned_ind_cqc_df = self.spark.createDataFrame(
-            Data.care_homes_cleaned_ind_cqc_rows,
-            Schemas.care_homes_cleaned_ind_cqc_schema,
+        self.non_res_with_dormancy_cleaned_ind_cqc_df = self.spark.createDataFrame(
+            Data.non_res_with_dormancy_cleaned_ind_cqc_rows,
+            Schemas.non_res_with_dormancy_cleaned_ind_cqc_schema,
         )
-        self.care_homes_features_df = self.spark.createDataFrame(
-            Data.care_homes_features_rows, Schemas.care_homes_features_schema
+        self.non_res_with_dormancy_features_df = self.spark.createDataFrame(
+            Data.non_res_with_dormancy_features_rows,
+            Schemas.non_res_with_dormancy_features_schema,
         )
         warnings.filterwarnings("ignore", category=ResourceWarning)
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-    @patch("utils.estimate_filled_posts.models.care_homes.save_model_metrics")
-    def test_model_care_homes_runs(
+    @patch(
+        "utils.estimate_filled_posts.models.non_res_with_dormancy.save_model_metrics"
+    )
+    def test_model_non_res_with_dormancy_runs(
         self,
         save_model_metrics: Mock,
     ):
-        job.model_care_homes(
-            self.care_homes_cleaned_ind_cqc_df,
-            self.care_homes_features_df,
-            self.CARE_HOME_MODEL,
+        job.model_non_res_with_dormancy(
+            self.non_res_with_dormancy_cleaned_ind_cqc_df,
+            self.non_res_with_dormancy_features_df,
+            self.NON_RES_WITH_DORMANCY_MODEL,
             self.METRICS_DESTINATION,
         )
 
         self.assertEqual(save_model_metrics.call_count, 1)
 
-    @patch("utils.estimate_filled_posts.models.care_homes.save_model_metrics")
-    def test_model_care_homes_returns_expected_data(
+    @patch(
+        "utils.estimate_filled_posts.models.non_res_with_dormancy.save_model_metrics"
+    )
+    def test_model_non_res_with_dormancy_returns_expected_data(
         self,
         save_model_metrics: Mock,
     ):
-        df = job.model_care_homes(
-            self.care_homes_cleaned_ind_cqc_df,
-            self.care_homes_features_df,
-            self.CARE_HOME_MODEL,
+        df = job.model_non_res_with_dormancy(
+            self.non_res_with_dormancy_cleaned_ind_cqc_df,
+            self.non_res_with_dormancy_features_df,
+            self.NON_RES_WITH_DORMANCY_MODEL,
             self.METRICS_DESTINATION,
         )
 
@@ -64,5 +71,9 @@ class TestModelCareHome(unittest.TestCase):
             df[IndCqc.location_id] == "1-000000002"
         ).collect()[0]
 
-        self.assertIsNotNone(expected_location_with_prediction.care_home_model)
-        self.assertIsNone(expected_location_without_prediction.care_home_model)
+        self.assertIsNotNone(
+            expected_location_with_prediction.non_res_with_dormancy_model
+        )
+        self.assertIsNone(
+            expected_location_without_prediction.non_res_with_dormancy_model
+        )
