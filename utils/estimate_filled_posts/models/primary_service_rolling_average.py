2e8de3a7d9bcee6c0b0a09f59b75594c91dc3438
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-08-07 12:41:01 +0100

    add docstrings

--- utils/estimate_filled_posts/models/primary_service_rolling_average.py
+++ utils/estimate_filled_posts/models/primary_service_rolling_average.py
@@ -10,6 +10,24 @@ from utils.column_names.ind_cqc_pipeline_columns import (
 def model_primary_service_rolling_average(
     df: DataFrame, column_to_average: str, number_of_days: int, model_column_name: str
 ) -> DataFrame:
+    """
+    Calculates the rolling average of a specified column over a given window of days.
+
+    Calculates the rolling average of a specified column over a given window of days. In order to calculate
+    the average this function first creates a rolling sum and a rolling count of the column values to
+    include in the calculation. The average is calculated by dividing the sum by the count. Temporary
+    columns created during the calculation process are dropped before returning the final dataframe. The only
+    additional column added will be the rolling average with the column name 'model_column_name'.
+
+    Args:
+        df (DataFrame): The input DataFrame.
+        column_to_average (str): The name of the column to average.
+        number_of_days (int): The number of days to include in the rolling average time period.
+        model_column_name (str): The name of the new column to store the rolling average.
+
+    Returns:
+        DataFrame: The input DataFrame with the new column containing the rolling average.
+    """
     df = add_flag_if_included_in_count(df, column_to_average)
     rolling_average_df = create_rolling_average_column(
         df, column_to_average, number_of_days, model_column_name
@@ -28,6 +46,24 @@ def add_flag_if_included_in_count(df: DataFrame, column_to_average: str):
 def create_rolling_average_column(
     df: DataFrame, column_to_average: str, number_of_days: int, model_column_name: str
 ) -> DataFrame:
+    """
+    Calculates the rolling average of a specified column over a given window of days.
+
+    Calculates the rolling average of a specified column over a given window of days. In order to calculate
+    the average this function first creates a rolling sum and a rolling count of the column values to
+    include in the calculation. The average is calculated by dividing the sum by the count. Temporary
+    columns created during the calculation process are dropped before returning the final dataframe. The only
+    additional column added will be the rolling average with the column name 'model_column_name'.
+
+    Args:
+        df (DataFrame): The input DataFrame.
+        column_to_average (str): The name of the column to average.
+        number_of_days (int): The number of days to include in the rolling average time period.
+        model_column_name (str): The name of the new column to store the rolling average.
+
+    Returns:
+        DataFrame: The input DataFrame with the new column containing the rolling average.
+    """
     df = calculate_rolling_sum(
         df,
         column_to_average,
@@ -56,6 +92,20 @@ def create_rolling_average_column(
 def calculate_rolling_sum(
     df: DataFrame, col_to_sum: str, number_of_days: int, new_col_name: str
 ) -> DataFrame:
+    """
+    Calculates the rolling sum of a specified column over a given window of days.
+
+    Calculates the rolling sum of a specified column over a given window of days.
+
+    Args:
+        df (DataFrame): The input DataFrame.
+        col_to_sum (str): The name of the column to sum.
+        number_of_days (int): The number of days to include in the rolling sum time period.
+        new_col_name (str): The name of the new column to store the rolling sum.
+
+    Returns:
+        DataFrame: The input DataFrame with the new column containing the rolling sum.
+    """
     df = df.withColumn(
         new_col_name,
         F.sum(col_to_sum).over(
@@ -66,6 +116,18 @@ def calculate_rolling_sum(
 
 
 def define_window_specifications(unix_date_col: str, number_of_days: int) -> Window:
+    """
+    Define the Window specification partitioned by primary service column.
+
+    Define the Window specification partitioned by primary service column.
+
+    Args:
+        unix_date_col (str): The name of the column containing unix timestamps.
+        number_of_days (int): The number of days to use for the rolling average calculations.
+
+    Returns:
+        Window: The required Window specification partitioned by primary service column.
+    """
     return (
         Window.partitionBy(IndCqc.primary_service_type)
         .orderBy(F.col(unix_date_col).cast("long"))
