cba81ae7d0c5f83ea4b2a0eb27d063cb60f79b42
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-08-08 13:58:28 +0100

    Update file path on diagnostics job

--- terraform/pipeline/glue.tf
+++ terraform/pipeline/glue.tf
@@ -748,7 +748,7 @@ module "diagnostics_on_known_filled_posts_job" {
   datasets_bucket = module.datasets_bucket
 
   job_parameters = {
-    "--estimate_filled_posts_source"    = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=estimated_ind_cqc_filled_posts/"
+    "--estimate_filled_posts_source"    = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=estimated_ml_ind_cqc_filled_posts/"
     "--diagnostics_destination"         = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=estimated_ind_cqc_filled_posts_diagnostics/"
     "--summary_diagnostics_destination" = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=estimated_ind_cqc_filled_posts_diagnostics_summary/"
     "--charts_destination"              = "${module.datasets_bucket.bucket_name}"
