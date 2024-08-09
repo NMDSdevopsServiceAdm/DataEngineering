9d7f8c6e352caa14908ebac0e0756f906799ba9b
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-08-09 11:16:06 +0100

    Update argument names

--- jobs/estimate_missing_ascwds_ind_cqc_filled_posts.py
+++ jobs/estimate_missing_ascwds_ind_cqc_filled_posts.py
@@ -13,19 +13,17 @@ PartitionKeys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
 
 def main(
     cleaned_ind_cqc_source: str,
-    estimated_missing_ascwds_filled_posts_destination: str,
+    estimated_missing_ascwds_ind_cqc_destination: str,
 ) -> DataFrame:
     print("Estimating independent CQC filled posts...")
 
     cleaned_ind_cqc_df = utils.read_from_parquet(cleaned_ind_cqc_source)
 
-    print(
-        f"Exporting as parquet to {estimated_missing_ascwds_filled_posts_destination}"
-    )
+    print(f"Exporting as parquet to {estimated_missing_ascwds_ind_cqc_destination}")
 
     utils.write_to_parquet(
         cleaned_ind_cqc_df,
-        estimated_missing_ascwds_filled_posts_destination,
+        estimated_missing_ascwds_ind_cqc_destination,
         mode="overwrite",
         partitionKeys=PartitionKeys,
     )
@@ -39,19 +37,19 @@ if __name__ == "__main__":
 
     (
         cleaned_ind_cqc_source,
-        estimated_missing_ascwds_filled_posts_destination,
+        estimated_missing_ascwds_ind_cqc_destination,
     ) = utils.collect_arguments(
         (
             "--cleaned_ind_cqc_source",
             "Source s3 directory for cleaned_ind_cqc_filled_posts",
         ),
         (
-            "--estimated_missing_ascwds_filled_posts_destination",
+            "--estimated_missing_ascwds_ind_cqc_destination",
             "Destination s3 directory for outputting estimates for filled posts",
         ),
     )
 
     main(
         cleaned_ind_cqc_source,
-        estimated_missing_ascwds_filled_posts_destination,
+        estimated_missing_ascwds_ind_cqc_destination,
     )
