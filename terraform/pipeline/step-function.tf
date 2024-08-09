28ad0178575ed798112f4060dbdf4c1929dd3d11
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-08-09 12:58:57 +0100

    Update terraform

--- terraform/pipeline/step-function.tf
+++ terraform/pipeline/step-function.tf
@@ -207,6 +207,7 @@ resource "aws_sfn_state_machine" "gold_validation_state_machine" {
     dataset_bucket_uri                                                      = module.datasets_bucket.bucket_uri
     validate_merged_ind_cqc_data_job_name                                   = module.validate_merged_ind_cqc_data_job.job_name
     validate_cleaned_ind_cqc_data_job_name                                  = module.validate_cleaned_ind_cqc_data_job.job_name
+    validate_estimated_missing_ascwds_filled_posts_data_job_name            = module.validate_estimated_missing_ascwds_filled_posts_data_job.job_name
     validate_care_home_ind_cqc_features_data_job_name                       = module.validate_care_home_ind_cqc_features_data_job.job_name
     validate_non_res_ascwds_inc_dormancy_ind_cqc_features_data_job_name     = module.validate_non_res_ascwds_inc_dormancy_ind_cqc_features_data_job.job_name
     validate_non_res_ascwds_without_dormancy_ind_cqc_features_data_job_name = module.validate_non_res_ascwds_without_dormancy_ind_cqc_features_data_job.job_name
