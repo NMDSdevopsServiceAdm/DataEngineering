a27f971cd2bb3ea8a661396040a88ae4011c11a4
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-08-27 10:18:07 +0100

    Add dedup_clean column to test data

--- tests/test_file_schemas.py
+++ tests/test_file_schemas.py
@@ -2095,6 +2095,11 @@ class EstimateMissingAscwdsFilledPostsSchemas:
                 FloatType(),
                 True,
             ),
+            StructField(
+                IndCQC.ascwds_filled_posts_dedup_clean,
+                FloatType(),
+                True,
+            ),
         ]
     )
 
