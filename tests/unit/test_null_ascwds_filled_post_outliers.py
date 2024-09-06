73f02328a02972ab5730b6a7e38bf1b5b776769d
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-09-06 13:35:17 +0100

    Add new functions to test

--- tests/unit/test_null_ascwds_filled_post_outliers.py
+++ tests/unit/test_null_ascwds_filled_post_outliers.py
@@ -20,10 +20,27 @@ class NullAscwdsFilledPostOutliersTests(unittest.TestCase):
         warnings.filterwarnings("ignore", category=ResourceWarning)
 
     @patch(
+        "utils.ind_cqc_filled_posts_utils.null_ascwds_filled_post_outliers.null_ascwds_filled_post_outliers.add_filtering_rule_column"
+    )
+    @patch(
+        "utils.ind_cqc_filled_posts_utils.null_ascwds_filled_post_outliers.null_ascwds_filled_post_outliers.null_filled_posts_where_locations_use_invalid_missing_data_code"
+    )
+    @patch(
+        "utils.ind_cqc_filled_posts_utils.null_ascwds_filled_post_outliers.null_ascwds_filled_post_outliers.null_grouped_providers"
+    )
+    @patch(
         "utils.ind_cqc_filled_posts_utils.null_ascwds_filled_post_outliers.null_ascwds_filled_post_outliers.null_care_home_filled_posts_per_bed_ratio_outliers"
     )
     def test_functions_are_called(
-        self, null_care_home_filled_posts_per_bed_ratio_outliers_mock: Mock
+        self,
+        null_care_home_filled_posts_per_bed_ratio_outliers_mock: Mock,
+        null_grouped_provders_mock: Mock,
+        null_filled_posts_where_locations_use_invalid_missing_data_code_mock: Mock,
+        add_filtering_rule_column_mock: Mock,
     ):
         job.null_ascwds_filled_post_outliers(self.unfiltered_ind_cqc_df)
+
         null_care_home_filled_posts_per_bed_ratio_outliers_mock.assert_called_once()
+        null_grouped_provders_mock.assert_called_once()
+        null_filled_posts_where_locations_use_invalid_missing_data_code_mock.assert_called_once()
+        add_filtering_rule_column_mock.assert_called_once()
