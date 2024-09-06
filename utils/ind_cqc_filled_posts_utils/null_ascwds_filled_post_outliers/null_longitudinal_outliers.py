66e25a575bfd34539e21bd127dce9dcfcff3150b
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-09-06 15:16:45 +0100

    Refactor functions

--- utils/ind_cqc_filled_posts_utils/null_ascwds_filled_post_outliers/null_longitudinal_outliers.py
+++ utils/ind_cqc_filled_posts_utils/null_ascwds_filled_post_outliers/null_longitudinal_outliers.py
@@ -23,6 +23,63 @@ def null_longitudinal_outliers(
     Returns:
         DataFrame: A dataframe with longitudinal outliers removed.
     """
+    df = calculate_max_and_min_permitted_values(df)
+    df = null_outlying_values(df)
+
+    df = df.drop(
+        *[
+            IndCQC.location_mean,
+            IndCQC.standard_deviation,
+            IndCQC.max_permitted_value,
+            IndCQC.min_permitted_value,
+        ]
+    )
+    df = update_filtering_rule(df, AscwdsFilteringRule.longitudinal_outlier)
+    return df
+
+
+def null_outlying_values(df: DataFrame) -> DataFrame:
+    """
+    Null outliers outside of the maximum and minimum permitted values for filled_posts_per_bed_ratio for each location.
+
+    Args:
+        df(DataFrame): A dataframe with the columns filled_posts_per_bed_ratio, ascwds_filled_posts_dedup_clean, min_permitted_value and max_permitted_value.
+
+    Returns:
+        DataFrame: A dataframe with longitudinal outliers nulled.
+    """
+    df = df.withColumn(
+        IndCQC.ascwds_filled_posts_dedup_clean,
+        F.when(
+            (df[IndCQC.filled_posts_per_bed_ratio] > df[IndCQC.max_permitted_value])
+            | (df[IndCQC.filled_posts_per_bed_ratio] < df[IndCQC.min_permitted_value]),
+            None,
+        ).otherwise(df[IndCQC.ascwds_filled_posts_dedup_clean]),
+    )
+    df = df.withColumn(
+        IndCQC.filled_posts_per_bed_ratio,
+        F.when(
+            (df[IndCQC.filled_posts_per_bed_ratio] > df[IndCQC.max_permitted_value])
+            | (df[IndCQC.filled_posts_per_bed_ratio] < df[IndCQC.min_permitted_value]),
+            None,
+        ).otherwise(df[IndCQC.filled_posts_per_bed_ratio]),
+    )
+
+    return df
+
+
+def calculate_max_and_min_permitted_values(df: DataFrame) -> DataFrame:
+    """
+    Calculate the maximum and minimum permitted values for filled_posts_per_bed_ratio for each location.
+
+     A value is defined as an outlier for that location when it is more than 3 standard deviations above or below the mean for that location.
+
+    Args:
+        df(DataFrame): A dataframe with the columns filled_posts_per_bed_ratio and location_id.
+
+    Returns:
+        DataFrame: A dataframe with maximum and minimum permitted values for filled_posts_per_bed_ratio for each location defined.
+    """
     permitted_number_of_standard_deviations_from_mean = 3
     w = Window.partitionBy(IndCQC.location_id).rowsBetween(
         Window.unboundedPreceding, Window.unboundedFollowing
@@ -50,30 +107,5 @@ def null_longitudinal_outliers(
             * permitted_number_of_standard_deviations_from_mean
         ),
     )
-    df = df.withColumn(
-        IndCQC.ascwds_filled_posts_dedup_clean,
-        F.when(
-            (df[IndCQC.filled_posts_per_bed_ratio] > df[IndCQC.max_permitted_value])
-            | (df[IndCQC.filled_posts_per_bed_ratio] < df[IndCQC.min_permitted_value]),
-            None,
-        ).otherwise(df[IndCQC.ascwds_filled_posts_dedup_clean]),
-    )
-    df = df.withColumn(
-        IndCQC.filled_posts_per_bed_ratio,
-        F.when(
-            (df[IndCQC.filled_posts_per_bed_ratio] > df[IndCQC.max_permitted_value])
-            | (df[IndCQC.filled_posts_per_bed_ratio] < df[IndCQC.min_permitted_value]),
-            None,
-        ).otherwise(df[IndCQC.filled_posts_per_bed_ratio]),
-    )
 
-    df = df.drop(
-        *[
-            IndCQC.location_mean,
-            IndCQC.standard_deviation,
-            IndCQC.max_permitted_value,
-            IndCQC.min_permitted_value,
-        ]
-    )
-    df = update_filtering_rule(df, AscwdsFilteringRule.longitudinal_outlier)
     return df
