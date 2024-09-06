f0eca14fb64c5ccee183eec1632b936c95ebe87d
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-09-06 14:10:18 +0100

    Update test for calling cleaning functions

--- tests/unit/test_null_ascwds_filled_post_outliers.py
+++ tests/unit/test_null_ascwds_filled_post_outliers.py
@@ -22,8 +22,14 @@ class NullAscwdsFilledPostOutliersTests(unittest.TestCase):
     @patch(
         "utils.ind_cqc_filled_posts_utils.null_ascwds_filled_post_outliers.null_ascwds_filled_post_outliers.null_care_home_filled_posts_per_bed_ratio_outliers"
     )
+    @patch(
+        "utils.ind_cqc_filled_posts_utils.null_ascwds_filled_post_outliers.null_ascwds_filled_post_outliers.null_longitudinal_outliers"
+    )
     def test_functions_are_called(
-        self, null_care_home_filled_posts_per_bed_ratio_outliers_mock: Mock
+        self,
+        null_care_home_filled_posts_per_bed_ratio_outliers_mock: Mock,
+        null_longitudinal_outliers_mock: Mock,
     ):
         job.null_ascwds_filled_post_outliers(self.unfiltered_ind_cqc_df)
         null_care_home_filled_posts_per_bed_ratio_outliers_mock.assert_called_once()
+        null_longitudinal_outliers_mock.assert_called_once()
