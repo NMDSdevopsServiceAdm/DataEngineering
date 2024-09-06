632d411c445cba91c64ca17e20c2f7312ae0793b
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-09-06 15:44:38 +0100

    Refactor function

--- utils/ind_cqc_filled_posts_utils/null_ascwds_filled_post_outliers/null_grouped_providers.py
+++ utils/ind_cqc_filled_posts_utils/null_ascwds_filled_post_outliers/null_grouped_providers.py
@@ -45,6 +45,34 @@ def null_care_home_grouped_providers(df: DataFrame) -> DataFrame:
     Returns:
         DataFrame: A dataframe with grouped providers' care home data nulled.
     """
+    df = calculate_data_for_grouped_provider_identification(df)
+    df = null_values_which_meet_the_grouped_provider_criteria(df)
+    df = df.drop(
+        *[
+            IndCQC.locations_at_provider,
+            IndCQC.locations_in_ascwds_at_provider,
+            IndCQC.locations_in_ascwds_with_data_at_provider,
+            IndCQC.number_of_beds_at_provider,
+        ]
+    )
+    df = update_filtering_rule(
+        df, rule_name=AscwdsFilteringRule.care_home_location_was_grouped_provider
+    )
+    return df
+
+
+def calculate_data_for_grouped_provider_identification(df: DataFrame) -> DataFrame:
+    """
+    Calculates the variables needed to determine whether a location is likely to be a grouped provider.
+
+    Calculates the variables locations_at_provider, locations_in_ascwds_at_provider, locations_in_ascwds_with_data_at_provider and number_of_beds_at_provider.
+
+    Args:
+        df (DataFrame): A dataframe with independent cqc data.
+
+    Returns:
+        DataFrame: A dataframe with the new variables locations_at_provider, locations_in_ascwds_at_provider, locations_in_ascwds_with_data_at_provider and number_of_beds_at_provider..
+    """
     w = Window.partitionBy(
         [IndCQC.provider_id, IndCQC.cqc_location_import_date]
     ).rowsBetween(Window.unboundedPreceding, Window.unboundedFollowing)
@@ -62,6 +90,20 @@ def null_care_home_grouped_providers(df: DataFrame) -> DataFrame:
     df = df.withColumn(
         IndCQC.number_of_beds_at_provider, F.sum(df[IndCQC.number_of_beds]).over(w)
     )
+
+    return df
+
+
+def null_values_which_meet_the_grouped_provider_criteria(df: DataFrame) -> DataFrame:
+    """
+    Null ascwds_filled_posts_dedup_clean where a provider has multiple locations, all their ascwds is under one care home location.
+
+    Args:
+        df (DataFrame): A dataframe with grouped provider identification columns added.
+
+    Returns:
+        DataFrame: A dataframe with grouped providers' care home data nulled.
+    """
     df = df.withColumn(
         IndCQC.ascwds_filled_posts_dedup_clean,
         F.when(
@@ -85,17 +127,7 @@ def null_care_home_grouped_providers(df: DataFrame) -> DataFrame:
             None,
         ).otherwise(F.col(IndCQC.ascwds_filled_posts_dedup_clean)),
     )
-    df = df.drop(
-        *[
-            IndCQC.locations_at_provider,
-            IndCQC.locations_in_ascwds_at_provider,
-            IndCQC.locations_in_ascwds_with_data_at_provider,
-            IndCQC.number_of_beds_at_provider,
-        ]
-    )
-    df = update_filtering_rule(
-        df, rule_name=AscwdsFilteringRule.care_home_location_was_grouped_provider
-    )
+
     return df
 
 
