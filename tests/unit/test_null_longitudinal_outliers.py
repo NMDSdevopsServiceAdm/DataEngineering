dd8245c9a0455630475e67d6120604459ec708ca
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-09-09 11:29:13 +0100

    Fix import after merge

--- tests/unit/test_null_longitudinal_outliers.py
+++ tests/unit/test_null_longitudinal_outliers.py
@@ -11,7 +11,7 @@ from utils import utils
 from utils.column_names.ind_cqc_pipeline_columns import (
     IndCqcColumns as IndCQC,
 )
-from utils.ind_cqc_filled_posts_utils.null_ascwds_filled_post_outliers import (
+from utils.ind_cqc_filled_posts_utils.clean_ascwds_filled_post_outliers import (
     null_longitudinal_outliers as job,
 )
 
