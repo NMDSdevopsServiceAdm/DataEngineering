9347fead90994c1d17b2c56ca1f4323f1c1ab680
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-09-10 10:13:47 +0100

    hash out tests

--- tests/unit/test_null_grouped_providers.py
+++ tests/unit/test_null_grouped_providers.py
@@ -51,132 +51,132 @@ class MainTests(NullGroupedProvidersTests):
         self.assertEqual(self.returned_df.schema, self.test_df.schema)
 
 
-class NullCareHomeGroupedProvidersTests(NullGroupedProvidersTests):
-    def setUp(self) -> None:
-        super().setUp()
+# class NullCareHomeGroupedProvidersTests(NullGroupedProvidersTests):
+#     def setUp(self) -> None:
+#         super().setUp()
 
-    def test_null_care_home_grouped_providers_returns_correct_values_when_grouped_provider_exists(
-        self,
-    ):
-        test_df = self.spark.createDataFrame(
-            Data.null_care_home_grouped_providers_where_grouped_provider_exists_rows,
-            Schemas.null_grouped_providers_schema,
-        )
+#     def test_null_care_home_grouped_providers_returns_correct_values_when_grouped_provider_exists(
+#         self,
+#     ):
+#         test_df = self.spark.createDataFrame(
+#             Data.null_care_home_grouped_providers_where_grouped_provider_exists_rows,
+#             Schemas.null_grouped_providers_schema,
+#         )
 
-        returned_df = job.null_care_home_grouped_providers(test_df)
-        expected_df = self.spark.createDataFrame(
-            Data.expected_null_care_home_grouped_providers_where_grouped_provider_exists_rows,
-            Schemas.null_grouped_providers_schema,
-        )
-        self.assertEqual(
-            expected_df.collect(),
-            returned_df.sort(IndCQC.location_id).collect(),
-        )
+#         returned_df = job.null_care_home_grouped_providers(test_df)
+#         expected_df = self.spark.createDataFrame(
+#             Data.expected_null_care_home_grouped_providers_where_grouped_provider_exists_rows,
+#             Schemas.null_grouped_providers_schema,
+#         )
+#         self.assertEqual(
+#             expected_df.collect(),
+#             returned_df.sort(IndCQC.location_id).collect(),
+#         )
 
-    def test_null_care_home_grouped_providers_returns_correct_values_when_grouped_provider_does_not_exist(
-        self,
-    ):
-        test_df = self.spark.createDataFrame(
-            Data.null_care_home_grouped_providers_where_grouped_provider_does_not_exist_rows,
-            Schemas.null_grouped_providers_schema,
-        )
+#     def test_null_care_home_grouped_providers_returns_correct_values_when_grouped_provider_does_not_exist(
+#         self,
+#     ):
+#         test_df = self.spark.createDataFrame(
+#             Data.null_care_home_grouped_providers_where_grouped_provider_does_not_exist_rows,
+#             Schemas.null_grouped_providers_schema,
+#         )
 
-        returned_df = job.null_care_home_grouped_providers(test_df)
-        expected_df = self.spark.createDataFrame(
-            Data.expected_null_care_home_grouped_providers_where_grouped_provider_does_not_exist_rows,
-            Schemas.null_grouped_providers_schema,
-        )
-        self.assertEqual(
-            expected_df.collect(),
-            returned_df.sort(IndCQC.location_id).collect(),
-        )
+#         returned_df = job.null_care_home_grouped_providers(test_df)
+#         expected_df = self.spark.createDataFrame(
+#             Data.expected_null_care_home_grouped_providers_where_grouped_provider_does_not_exist_rows,
+#             Schemas.null_grouped_providers_schema,
+#         )
+#         self.assertEqual(
+#             expected_df.collect(),
+#             returned_df.sort(IndCQC.location_id).collect(),
+#         )
 
-    def test_null_care_home_grouped_providers_returns_correct_values_when_all_locations_are_non_residential(
-        self,
-    ):
-        test_df = self.spark.createDataFrame(
-            Data.null_care_home_grouped_providers_where_all_locations_are_non_residential_rows,
-            Schemas.null_grouped_providers_schema,
-        )
+#     def test_null_care_home_grouped_providers_returns_correct_values_when_all_locations_are_non_residential(
+#         self,
+#     ):
+#         test_df = self.spark.createDataFrame(
+#             Data.null_care_home_grouped_providers_where_all_locations_are_non_residential_rows,
+#             Schemas.null_grouped_providers_schema,
+#         )
 
-        returned_df = job.null_care_home_grouped_providers(test_df)
-        expected_df = self.spark.createDataFrame(
-            Data.expected_null_care_home_grouped_providers_where_all_locations_are_non_residential_rows,
-            Schemas.null_grouped_providers_schema,
-        )
-        self.assertEqual(
-            expected_df.collect(),
-            returned_df.sort(IndCQC.location_id).collect(),
-        )
+#         returned_df = job.null_care_home_grouped_providers(test_df)
+#         expected_df = self.spark.createDataFrame(
+#             Data.expected_null_care_home_grouped_providers_where_all_locations_are_non_residential_rows,
+#             Schemas.null_grouped_providers_schema,
+#         )
+#         self.assertEqual(
+#             expected_df.collect(),
+#             returned_df.sort(IndCQC.location_id).collect(),
+#         )
 
-    def test_null_care_home_grouped_providers_returns_correct_values_when_some_locations_are_non_residential_but_grouped_provider_is_care_home(
-        self,
-    ):
-        test_df = self.spark.createDataFrame(
-            Data.null_care_home_grouped_providers_where_some_locations_are_non_residential_but_grouped_provider_is_care_home_rows,
-            Schemas.null_grouped_providers_schema,
-        )
+#     def test_null_care_home_grouped_providers_returns_correct_values_when_some_locations_are_non_residential_but_grouped_provider_is_care_home(
+#         self,
+#     ):
+#         test_df = self.spark.createDataFrame(
+#             Data.null_care_home_grouped_providers_where_some_locations_are_non_residential_but_grouped_provider_is_care_home_rows,
+#             Schemas.null_grouped_providers_schema,
+#         )
 
-        returned_df = job.null_care_home_grouped_providers(test_df)
-        expected_df = self.spark.createDataFrame(
-            Data.expected_null_care_home_grouped_providers_where_some_locations_are_non_residential_but_grouped_provider_is_care_home_rows,
-            Schemas.null_grouped_providers_schema,
-        )
-        self.assertEqual(
-            expected_df.collect(),
-            returned_df.sort(IndCQC.location_id).collect(),
-        )
+#         returned_df = job.null_care_home_grouped_providers(test_df)
+#         expected_df = self.spark.createDataFrame(
+#             Data.expected_null_care_home_grouped_providers_where_some_locations_are_non_residential_but_grouped_provider_is_care_home_rows,
+#             Schemas.null_grouped_providers_schema,
+#         )
+#         self.assertEqual(
+#             expected_df.collect(),
+#             returned_df.sort(IndCQC.location_id).collect(),
+#         )
 
-    def test_null_care_home_grouped_providers_returns_correct_values_when_not_enough_locations(
-        self,
-    ):
-        test_df = self.spark.createDataFrame(
-            Data.null_care_home_grouped_providers_where_not_enough_locations_rows,
-            Schemas.null_grouped_providers_schema,
-        )
+#     def test_null_care_home_grouped_providers_returns_correct_values_when_not_enough_locations(
+#         self,
+#     ):
+#         test_df = self.spark.createDataFrame(
+#             Data.null_care_home_grouped_providers_where_not_enough_locations_rows,
+#             Schemas.null_grouped_providers_schema,
+#         )
 
-        returned_df = job.null_care_home_grouped_providers(test_df)
-        expected_df = self.spark.createDataFrame(
-            Data.expected_null_care_home_grouped_providers_where_not_enough_locations_rows,
-            Schemas.null_grouped_providers_schema,
-        )
-        self.assertEqual(
-            expected_df.collect(),
-            returned_df.sort(IndCQC.location_id).collect(),
-        )
+#         returned_df = job.null_care_home_grouped_providers(test_df)
+#         expected_df = self.spark.createDataFrame(
+#             Data.expected_null_care_home_grouped_providers_where_not_enough_locations_rows,
+#             Schemas.null_grouped_providers_schema,
+#         )
+#         self.assertEqual(
+#             expected_df.collect(),
+#             returned_df.sort(IndCQC.location_id).collect(),
+#         )
 
-    def test_null_care_home_grouped_providers_returns_correct_values_when_multiple_locations_in_ascwds(
-        self,
-    ):
-        test_df = self.spark.createDataFrame(
-            Data.null_care_home_grouped_providers_where_multiple_locations_in_ascwds_rows,
-            Schemas.null_grouped_providers_schema,
-        )
+#     def test_null_care_home_grouped_providers_returns_correct_values_when_multiple_locations_in_ascwds(
+#         self,
+#     ):
+#         test_df = self.spark.createDataFrame(
+#             Data.null_care_home_grouped_providers_where_multiple_locations_in_ascwds_rows,
+#             Schemas.null_grouped_providers_schema,
+#         )
 
-        returned_df = job.null_care_home_grouped_providers(test_df)
-        expected_df = self.spark.createDataFrame(
-            Data.expected_null_care_home_grouped_providers_where_multiple_locations_in_ascwds_rows,
-            Schemas.null_grouped_providers_schema,
-        )
-        self.assertEqual(
-            expected_df.collect(),
-            returned_df.sort(IndCQC.location_id).collect(),
-        )
+#         returned_df = job.null_care_home_grouped_providers(test_df)
+#         expected_df = self.spark.createDataFrame(
+#             Data.expected_null_care_home_grouped_providers_where_multiple_locations_in_ascwds_rows,
+#             Schemas.null_grouped_providers_schema,
+#         )
+#         self.assertEqual(
+#             expected_df.collect(),
+#             returned_df.sort(IndCQC.location_id).collect(),
+#         )
 
-    def test_null_care_home_grouped_providers_returns_correct_values_when_no_ascwds_data(
-        self,
-    ):
-        test_df = self.spark.createDataFrame(
-            Data.null_care_home_grouped_providers_where_no_ascwds_data_rows,
-            Schemas.null_grouped_providers_schema,
-        )
+#     def test_null_care_home_grouped_providers_returns_correct_values_when_no_ascwds_data(
+#         self,
+#     ):
+#         test_df = self.spark.createDataFrame(
+#             Data.null_care_home_grouped_providers_where_no_ascwds_data_rows,
+#             Schemas.null_grouped_providers_schema,
+#         )
 
-        returned_df = job.null_care_home_grouped_providers(test_df)
-        expected_df = self.spark.createDataFrame(
-            Data.expected_null_care_home_grouped_providers_where_no_ascwds_data_rows,
-            Schemas.null_grouped_providers_schema,
-        )
-        self.assertEqual(
-            expected_df.collect(),
-            returned_df.sort(IndCQC.location_id).collect(),
-        )
+#         returned_df = job.null_care_home_grouped_providers(test_df)
+#         expected_df = self.spark.createDataFrame(
+#             Data.expected_null_care_home_grouped_providers_where_no_ascwds_data_rows,
+#             Schemas.null_grouped_providers_schema,
+#         )
+#         self.assertEqual(
+#             expected_df.collect(),
+#             returned_df.sort(IndCQC.location_id).collect(),
+#         )
