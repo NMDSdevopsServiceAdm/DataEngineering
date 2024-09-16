e6e769d945635be2f2b05c3c9d3012863dd63e02
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-09-16 12:32:54 +0100

    Update job

--- jobs/clean_capacity_tracker_non_res_data.py
+++ jobs/clean_capacity_tracker_non_res_data.py
@@ -1,7 +1,5 @@
 import sys
 
-from pyspark.sql.dataframe import DataFrame
-
 from utils import utils
 import utils.cleaning_utils as cUtils
 from utils.column_names.capacity_tracker_columns import (
@@ -12,12 +10,7 @@ from utils.column_names.ind_cqc_pipeline_columns import PartitionKeys as Keys
 
 CAPACITY_TRACKER_NON_RES_COLUMNS = [
     CTNR.cqc_id,
-    CTNR.nurses_employed,
-    CTNR.care_workers_employed,
-    CTNR.non_care_workers_employed,
-    CTNR.agency_nurses_employed,
-    CTNR.agency_care_workers_employed,
-    CTNR.agency_non_care_workers_employed,
+    CTNR.cqc_care_workers_employed,
     Keys.year,
     Keys.month,
     Keys.day,
@@ -26,33 +19,28 @@ CAPACITY_TRACKER_NON_RES_COLUMNS = [
 
 
 def main(
-    capacity_tracker_care_home_source: str,
-    cleaned_capacity_tracker_care_home_destination: str,
+    capacity_tracker_non_res_source: str,
+    cleaned_capacity_tracker_non_res_destination: str,
 ):
-    capacity_tracker_care_home_df = utils.read_from_parquet(
-        capacity_tracker_care_home_source, CAPACITY_TRACKER_NON_RES_COLUMNS
+    capacity_tracker_non_res_df = utils.read_from_parquet(
+        capacity_tracker_non_res_source, CAPACITY_TRACKER_NON_RES_COLUMNS
     )
     columns_to_cast_to_integers = [
-        CTNR.nurses_employed,
-        CTNR.care_workers_employed,
-        CTNR.non_care_workers_employed,
-        CTNR.agency_nurses_employed,
-        CTNR.agency_care_workers_employed,
-        CTNR.agency_non_care_workers_employed,
+        CTNR.cqc_care_workers_employed,
     ]
-    capacity_tracker_care_home_df = cUtils.cast_to_int(
-        capacity_tracker_care_home_df, columns_to_cast_to_integers
+    capacity_tracker_non_res_df = cUtils.cast_to_int(
+        capacity_tracker_non_res_df, columns_to_cast_to_integers
     )
-    capacity_tracker_care_home_df = cUtils.column_to_date(
-        capacity_tracker_care_home_df,
+    capacity_tracker_non_res_df = cUtils.column_to_date(
+        capacity_tracker_non_res_df,
         Keys.import_date,
         CTNRClean.capacity_tracker_import_date,
     )
 
-    print(f"Exporting as parquet to {cleaned_capacity_tracker_care_home_destination}")
+    print(f"Exporting as parquet to {cleaned_capacity_tracker_non_res_destination}")
     utils.write_to_parquet(
-        capacity_tracker_care_home_df,
-        cleaned_capacity_tracker_care_home_destination,
+        capacity_tracker_non_res_df,
+        cleaned_capacity_tracker_non_res_destination,
         mode="overwrite",
         partitionKeys=[
             Keys.year,
@@ -64,25 +52,25 @@ def main(
 
 
 if __name__ == "__main__":
-    print("Spark job 'clean_capacity_tracker_care_home_dataset' starting...")
+    print("Spark job 'clean_capacity_tracker_non_res_dataset' starting...")
     print(f"Job parameters: {sys.argv}")
 
     (
-        capacity_tracker_care_home_source,
-        cleaned_capacity_tracker_care_home_destination,
+        capacity_tracker_non_res_source,
+        cleaned_capacity_tracker_non_res_destination,
     ) = utils.collect_arguments(
         (
-            "--capacity_tracker_care_home_source",
-            "Source s3 directory for parquet capacity tracker care home dataset",
+            "--capacity_tracker_non_res_source",
+            "Source s3 directory for parquet capacity tracker non residential dataset",
         ),
         (
-            "--cleaned_capacity_tracker_care_home_destination",
-            "Destination s3 directory for cleaned parquet capacity tracker care home dataset",
+            "--cleaned_capacity_tracker_non_res_destination",
+            "Destination s3 directory for cleaned parquet capacity tracker non residential dataset",
         ),
     )
     main(
-        capacity_tracker_care_home_source,
-        cleaned_capacity_tracker_care_home_destination,
+        capacity_tracker_non_res_source,
+        cleaned_capacity_tracker_non_res_destination,
     )
 
-    print("Spark job 'clean_capacity_tracker_care_home_dataset' complete")
+    print("Spark job 'clean_capacity_tracker_non_res_dataset' complete")
--- utils/column_names/capacity_tracker_columns.py
+++ utils/column_names/capacity_tracker_columns.py
@@ -51,3 +51,47 @@ class CapacityTrackerCareHomeCleanColumns:
     agency_total_employed: str = "agency_total_employed"
     non_agency_total_employed: str = "non_agency_total_employed"
     agency_and_non_agency_total_employed: str = "agency_and_non_agency_total_employed"
+
+
+@dataclass
+class CapacityTrackerNonResColumns:
+    can_provider_more_hours: str = "can_provider_more_hours"
+    confirmed_save: str = "confirmed_save"
+    covid_confirmed_and_suspected: str = "covid_confirmed_and_suspected"
+    covid_notes: str = "covid_notes"
+    covid_vaccination_full_course: str = "covid_vaccination_full_course"
+    cqc_care_workers_absent: str = "cqc_care_workers_absent"
+    cqc_care_workers_employed: str = "cqc_care_workers_employed"
+    cqc_id: str = CapacityTrackerCareHomeColumns.cqc_id
+    cqc_survey_last_updated_bst: str = "cqc_survey_last_updated_bst"
+    cqc_survey_last_updated_utc: str = "cqc_survey_last_updated_utc"
+    cqc_survey_last_updated_utc_formatted: str = "cqc_survey_last_updated_utc_formatted"
+    days_absence_dom_care: str = "days_absence_dom_care"
+    extra_hours_count: str = "extra_hours_count"
+    has_cqc_ppe_issues: str = "has_cqc_ppe_issues"
+    hours_absence_dom_care: str = "hours_absence_dom_care"
+    hours_agency_dom_care: str = "hours_agency_dom_care"
+    hours_overtime_dom_care: str = "hours_overtime_dom_care"
+    hours_paid_dom_care: str = "hours_paid_dom_care"
+    icb: str = CapacityTrackerCareHomeColumns.icb
+    la_region_name: str = "la_region_name"
+    legacy_covid_confirmed: str = "legacy_covid_confirmed"
+    legacy_covid_suspected: str = "legacy_covid_suspected"
+    local_authority: str = CapacityTrackerCareHomeColumns.local_authority
+    localauthority: str = CapacityTrackerCareHomeColumns.localauthority
+    location: str = CapacityTrackerCareHomeColumns.location
+    lrf: str = CapacityTrackerCareHomeColumns.lrf
+    ods_code: str = CapacityTrackerCareHomeColumns.ods_code
+    region: str = CapacityTrackerCareHomeColumns.region
+    return_poc_percent: str = "return_poc_percent"
+    service_user_count: str = "service_user_count"
+    sub_icb: str = CapacityTrackerCareHomeColumns.sub_icb
+    users_nhs_la: str = "users_nhs_la"
+    users_self_funded: str = "users_self_funded"
+
+
+@dataclass
+class CapacityTrackerNonResCleanColumns:
+    capacity_tracker_import_date: str = (
+        CapacityTrackerCareHomeCleanColumns.capacity_tracker_import_date
+    )
