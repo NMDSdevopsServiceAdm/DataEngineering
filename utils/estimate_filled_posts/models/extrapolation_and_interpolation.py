0f5119ad1a1187e792f7050bd796a7e8f1102599
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-09-19 13:57:19 +0100

    Create overarching function for both

--- utils/estimate_filled_posts/models/extrapolation_and_interpolation.py
+++ utils/estimate_filled_posts/models/extrapolation_and_interpolation.py
@@ -1,20 +1,31 @@
-from pyspark.sql import DataFrame, functions as F
+from pyspark.sql import DataFrame
 
-from utils.column_names.ind_cqc_pipeline_columns import (
-    IndCqcColumns as IndCqc,
-)
-
-# from utils.estimate_filled_posts.models.extrapolation import model_extrapolation
-# from utils.estimate_filled_posts.models.interpolation import model_interpolation
+from utils.estimate_filled_posts.models.extrapolation import model_extrapolation
+from utils.estimate_filled_posts.models.interpolation import model_interpolation
 
 
 def model_extrapolation_and_interpolation(
-    df: DataFrame, model_column_name: str
+    df: DataFrame,
+    model_column_name: str,
+    column_to_interpolate: str,
+    new_column_name: str,
 ) -> DataFrame:
-    # create cols used for both
+    """
+    Adds two new columns, one with extrapolated values and one with interpolated values.
 
-    # df = model_extrapolation(df, model_column_name)
+    The function prepares the dataset with features present in both models before calling each model separately.
 
-    # df = model_interpolation(df, model_column_name)
+    Args:
+        df (DataFrame): A dataframe with a column to interpolate.
+        model_column_name (str): The model which contains the trned for extrapolation for follow
+        column_to_interpolate (str): The name of the column that needs interpolating.
+        new_column_name (str): The name of the new column with interpolated values.
+
+    Returns:
+        DataFrame: A dataframe with a new column containing interpolated values.
+    """
+    df = model_extrapolation(df, model_column_name)
+
+    df = model_interpolation(df, column_to_interpolate, new_column_name)
 
     return df
