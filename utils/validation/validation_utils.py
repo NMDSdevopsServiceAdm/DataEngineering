a4677a3135dd7bf3880864ce9e7e96e4cce06e23
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-09-23 13:26:00 +0100

    Add doc strings

--- utils/validation/validation_utils.py
+++ utils/validation/validation_utils.py
@@ -22,6 +22,19 @@ from utils.validation.validation_rule_names import (
 
 
 def validate_dataset(dataset: DataFrame, rules: dict) -> DataFrame:
+    """
+    Handles the overarching process of validating a dataset.
+
+    This function creates a verification suite on the given datasets, populates the verification
+    suite with checks, runs the checks and then formats the results as a dataframe.
+
+    Args:
+        dataset(DataFrame): The dataset which requires validation.
+        rules(dict): A dictionary of validation rules to apply to the dataset.
+
+    Returns:
+        DataFrame: A dataframe containing the results of each check.
+    """
     spark = utils.get_spark()
     verification_run = VerificationSuite(spark).onData(dataset)
     verification_run = add_checks_to_run(verification_run, rules)
@@ -33,6 +46,19 @@ def validate_dataset(dataset: DataFrame, rules: dict) -> DataFrame:
 def add_checks_to_run(
     run: VerificationRunBuilder, rules_to_check: dict
 ) -> VerificationRunBuilder:
+    """
+    Adds the checks listed in the ruleset to the verification suite.
+
+    This function adds the checks definied in the given ruleset to the varification run.
+    If a rule is not recognised, a value error is raised.
+
+    Args:
+        run(VerificationRunBuilder): A verification run for a particular dataset.
+        rules_to_check(dict): A dictionary of validation rules to apply to the dataset.
+
+    Returns:
+        VerificationRunBuilder: A verification run containing all the listed checks.
+    """
     for rule_name in rules_to_check.keys():
         rule = rules_to_check[rule_name]
         if rule_name == RuleToCheck.size_of_dataset:
@@ -74,6 +100,17 @@ def add_checks_to_run(
 
 
 def create_check_for_column_completeness(complete_columns: list) -> Check:
+    """
+    Creates a check of column completeness to add to a verification run.
+
+    This function creates a check of column completeness for the given columns to add to a verification run.
+
+    Args:
+        complete_columns(list): A list of columns in the dataset which should have no null values.
+
+    Returns:
+        Check: A check of column completeness to add to a verification run.
+    """
     spark = utils.get_spark()
     check = Check(spark, CheckLevel.Warning, "Column is complete")
     for column in complete_columns:
@@ -82,6 +119,17 @@ def create_check_for_column_completeness(complete_columns: list) -> Check:
 
 
 def create_check_of_uniqueness_of_two_index_columns(column_names: list) -> Check:
+    """
+    Creates a check of the uniqueness of two index columns to add to a verification run.
+
+    This function creates a check of the uniqueness of two index columns for the given columns to add to a verification run.
+
+    Args:
+        column_names(list): A list of two columns in the dataset which should be unique indexes.
+
+    Returns:
+        Check: A check of column uniqueness to add to a verification run.
+    """
     spark = utils.get_spark()
     check = Check(spark, CheckLevel.Warning, "Index columns are unique")
     check = check.hasUniqueness(
@@ -91,6 +139,17 @@ def create_check_of_uniqueness_of_two_index_columns(column_names: list) -> Check
 
 
 def create_check_of_size_of_dataset(expected_size: int) -> Check:
+    """
+    Creates a check of the number of rows in the dataset to add to a verification run.
+
+    This function creates a check of the number of rows in the dataset to add to a verification run.
+
+    Args:
+        expected_size(int): The number of rows expected in the dataset.
+
+    Returns:
+        Check: A check of the number of rows in the dataset to add to a verification run.
+    """
     spark = utils.get_spark()
     check = Check(spark, CheckLevel.Warning, "Size of dataset")
     check = check.hasSize(
@@ -101,6 +160,18 @@ def create_check_of_size_of_dataset(expected_size: int) -> Check:
 
 
 def create_check_of_min_values(column_name: str, min_value: int) -> Check:
+    """
+    Creates a check of the minimum value of a column to add to a verification run.
+
+    This function creates a check of the minimum value of the given column to add to a verification run.
+
+    Args:
+        column_name(str): A numerical column in the dataset.
+        min_value(int): The minimum permitted value of the given column.
+
+    Returns:
+        Check: A check of the minimum value of a column to add to a verification run.
+    """
     spark = utils.get_spark()
     check = Check(spark, CheckLevel.Warning, f"Min value in column")
     check = check.hasMin(
@@ -112,6 +183,18 @@ def create_check_of_min_values(column_name: str, min_value: int) -> Check:
 
 
 def create_check_of_max_values(column_name: str, max_value: int) -> Check:
+    """
+    Creates a check of the maximum value of a column to add to a verification run.
+
+    This function creates a check of the maximum value of the given column to add to a verification run.
+
+    Args:
+        column_name(str): A numerical column in the dataset.
+        max_value(int): The maximum permitted value of the given column.
+
+    Returns:
+        Check: A check of the maximum value of a column to add to a verification run.
+    """
     spark = utils.get_spark()
     check = Check(spark, CheckLevel.Warning, "Max value in column")
     check = check.hasMax(
@@ -123,6 +206,17 @@ def create_check_of_max_values(column_name: str, max_value: int) -> Check:
 
 
 def create_check_of_categorical_values_in_columns(categorical_values: dict) -> Check:
+    """
+    Creates a check of the categorical values in columns to add to a verification run.
+
+    This function creates a check of the categorical values of the given columns to add to a verification run.
+
+    Args:
+        categorical_values(dict): A dictionary of column names and lists of categorical values permittedin those columns.
+
+    Returns:
+        Check: A check of the categorical values in columns to add to a verification run.
+    """
     spark = utils.get_spark()
     check = Check(
         spark, CheckLevel.Warning, "Categorical values are in list of expected values"
@@ -139,6 +233,18 @@ def create_check_of_categorical_values_in_columns(categorical_values: dict) -> C
 def create_check_of_number_of_distinct_values(
     column_name: str, distinct_values: int
 ) -> Check:
+    """
+    Creates a check of the number of distinct values in a column to add to a verification run.
+
+    This function creates a check of the number of distinct values in the given column to add to a verification run.
+
+    Args:
+        column_name(str): A categorical column in the dataset.
+        distinct_values(int): The number of distinct values that should be present in the given column.
+
+    Returns:
+        Check: A check of the categorical values in columns to add to a verification run.
+    """
     spark = utils.get_spark()
     check = Check(
         spark, CheckLevel.Warning, "Column contains correct number of distinct values"
@@ -154,6 +260,19 @@ def create_check_of_number_of_distinct_values(
 
 
 def create_check_of_custom_type(rule: str, constraint_name: str, hint: str) -> Check:
+    """
+    Creates a custom check to add to a verification run.
+
+    This function creates a custom check using a SQL WHERE clause to add to a verification run.
+
+    Args:
+        rule(str): A custom rule in the format of a SQL WHERE clause.
+        constraint_name(str): A name for the custom constraint.
+        hint(str): A hint for when the custom conatraint fails.
+
+    Returns:
+        Check: A custom check to add to a verification run.
+    """
     spark = utils.get_spark()
     check = Check(spark, CheckLevel.Warning, "custom type")
     check = check.satisfies(
@@ -168,6 +287,20 @@ def create_check_of_custom_type(rule: str, constraint_name: str, hint: str) -> C
 def add_column_with_length_of_string(
     df: DataFrame, column_names: list[str]
 ) -> DataFrame:
+    """
+    Adds new columns to the dataframe containing the length of the given columns.
+
+    This function generates a new column in the dataframe for every column given and
+    populates it with the length of the given column. THe length columns are named with
+    the original column name and the suffix _length.
+
+    Args:
+        df(DataFrame): A dataframe with the given columns.
+        column_names(list[str]): A list of strings with the column names to evaluate.
+
+    Returns:
+        DataFrame: A dataframe with additional columns containing the length of each given column.
+    """
     for column_name in column_names:
         new_column_name = column_name + "_length"
         df = df.withColumn(new_column_name, F.length(column_name))
@@ -175,6 +308,16 @@ def add_column_with_length_of_string(
 
 
 def raise_exception_if_any_checks_failed(df: DataFrame):
+    """
+    Checks the results of the validation checks and raises an error if any checks have failed.
+
+    This function identifies any validation failures and raises an error if any are detected.
+    Args:
+        df(DataFrame): A dataframe of validation results.
+
+    Raises:
+        ValueError: Data quaility failures detected.
+    """
     df = df.where(df[Validation.constraint_status] == "Failure")
 
     failures_count = df.count()
