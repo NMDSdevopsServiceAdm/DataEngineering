26110371782008d2aebf91bffbb7ca51d4ddaad2
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-09-23 10:54:10 +0100

    Revert "Add test comment"
    This reverts commit 794a7b9e086c0eca1ddc771c368be1368366314b.

--- utils/validation/validation_utils.py
+++ utils/validation/validation_utils.py
@@ -143,7 +143,6 @@ def create_check_of_number_of_distinct_values(
     return check
 
 
-# test commit
 def add_column_with_length_of_string(
     df: DataFrame, column_names: list[str]
 ) -> DataFrame:
