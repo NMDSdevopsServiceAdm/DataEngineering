3efdea1b51e59e804f197b51a056737af48a4bd8
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-09-23 12:14:02 +0100

    Convert condition to f string

--- utils/validation/validation_rule_custom_type.py
+++ utils/validation/validation_rule_custom_type.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass
 
+from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCQC
+from utils.column_values.categorical_column_values import CareHome, PrimaryServiceType
 from utils.validation.validation_rule_names import CustomTypeArguments
 
 
 @dataclass
 class CustomValidationRules:
     care_home_and_primary_service_type = {
-        CustomTypeArguments.column_condition: "(carehome = 'N' AND primary_service_type = 'non-residential') OR (carehome = 'Y' AND primary_service_type = 'Care home with nursing') OR (carehome = 'Y' AND primary_service_type = 'Care home without nursing')",
+        CustomTypeArguments.column_condition: f"({IndCQC.care_home} = '{CareHome.not_care_home}' AND {IndCQC.primary_service_type} = '{PrimaryServiceType.non_residential}') OR ({IndCQC.care_home} = '{CareHome.care_home}' AND {IndCQC.primary_service_type} = '{PrimaryServiceType.care_home_with_nursing}') OR ({IndCQC.care_home} = '{CareHome.care_home}' AND {IndCQC.primary_service_type} = '{PrimaryServiceType.care_home_only}')",
         CustomTypeArguments.constraint_name: "care_home_and_primary_service_type",
         CustomTypeArguments.hint: "The data in carehome and primary_service_type should be related.",
     }
