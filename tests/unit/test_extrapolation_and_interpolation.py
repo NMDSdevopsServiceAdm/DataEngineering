426ef4660b3534af8b91a3d5da2169a424edb85a
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-09-24 14:49:52 +0100

    remove interpolation assertion

--- tests/unit/test_extrapolation_and_interpolation.py
+++ tests/unit/test_extrapolation_and_interpolation.py
@@ -27,25 +27,25 @@ class MainTests(ModelExtrapolationAndInterpolationTests):
             Data.extrapolation_and_interpolation_rows,
             Schemas.extrapolation_and_interpolation_schema,
         )
+        self.column_with_null_values = IndCqc.ascwds_filled_posts_dedup_clean
         self.model_column_name = IndCqc.rolling_average_model
 
-    @patch(
-        "utils.estimate_filled_posts.models.extrapolation_and_interpolation.model_interpolation"
-    )
+    # @patch(
+    #     "utils.estimate_filled_posts.models.extrapolation_and_interpolation.model_interpolation"
+    # )
     @patch(
         "utils.estimate_filled_posts.models.extrapolation_and_interpolation.model_extrapolation"
     )
     def test_main_runs(
         self,
         model_extrapolation_mock: Mock,
-        model_interpolation_mock: Mock,
+        # model_interpolation_mock: Mock,
     ):
         job.model_extrapolation_and_interpolation(
             self.extrapolation_and_interpolation_df,
+            self.column_with_null_values,
             self.model_column_name,
-            IndCqc.ascwds_filled_posts_dedup_clean,
-            IndCqc.interpolation_model_ascwds_filled_posts_dedup_clean,
         )
 
         model_extrapolation_mock.assert_called_once()
-        model_interpolation_mock.assert_called_once()
+        # model_interpolation_mock.assert_called_once()
