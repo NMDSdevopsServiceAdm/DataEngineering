106a8b517b4fdad8d35d0712473dae0f6dc11dee
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-09-25 14:11:01 +0100

    first and last submission tests

--- tests/unit/test_extrapolation_new.py
+++ tests/unit/test_extrapolation_new.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import Mock, patch
 import warnings
 
 from pyspark.sql import Window, WindowSpec
@@ -16,6 +17,12 @@ class TestModelExtrapolation(unittest.TestCase):
     def setUp(self):
         self.spark = utils.get_spark()
 
+        self.extrapolation_df = self.spark.createDataFrame(
+            Data.extrapolation_new_rows, Schemas.extrapolation_schema
+        )
+        self.extrapolation_model_column_name = "extrapolation_rolling_average_model"
+        self.model_column_name = IndCqc.rolling_average_model
+
         warnings.filterwarnings("ignore", category=ResourceWarning)
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
@@ -24,14 +31,6 @@ class MainTests(TestModelExtrapolation):
     def setUp(self) -> None:
         super().setUp()
 
-        self.extrapolation_df = self.spark.createDataFrame(
-            Data.extrapolation_new_rows, Schemas.extrapolation_schema
-        )
-        self.extrapolation_model_column_name = "extrapolation_rolling_average_model"
-        self.model_column_name = IndCqc.rolling_average_model
-        self.window_spec = Window.partitionBy(IndCqc.location_id).orderBy(
-            IndCqc.unix_time
-        )
         self.returned_df = job.model_extrapolation(
             self.extrapolation_df,
             IndCqc.ascwds_filled_posts_dedup_clean,
@@ -56,3 +55,63 @@ class DefineWindowSpecsTests(TestModelExtrapolation):
         self.assertEqual(len(returned_window_specs), 2)
         self.assertIsInstance(returned_window_specs[0], WindowSpec)
         self.assertIsInstance(returned_window_specs[1], WindowSpec)
+
+
+class CalculateFirstAndLastSubmissionDatesTests(TestModelExtrapolation):
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.column_with_null_values = IndCqc.ascwds_filled_posts_dedup_clean
+        self.window_spec_all_rows = (
+            Window.partitionBy(IndCqc.location_id)
+            .orderBy(IndCqc.unix_time)
+            .rowsBetween(Window.unboundedPreceding, Window.unboundedFollowing)
+        )
+
+        self.returned_df = job.calculate_first_and_last_submission_dates(
+            self.extrapolation_df,
+            self.column_with_null_values,
+            self.window_spec_all_rows,
+        )
+
+    @patch("utils.estimate_filled_posts.models.extrapolation_new.get_selected_value")
+    def test_calculate_first_and_last_submission_dates_calls_correct_functions(
+        self,
+        get_selected_value_mock: Mock,
+    ):
+        get_selected_value_mock.return_value = self.extrapolation_df
+
+        job.calculate_first_and_last_submission_dates(
+            self.extrapolation_df,
+            self.column_with_null_values,
+            self.window_spec_all_rows,
+        )
+
+        self.assertEqual(get_selected_value_mock.call_count, 2)
+
+        get_selected_value_mock.assert_any_call(
+            self.extrapolation_df,
+            self.window_spec_all_rows,
+            self.column_with_null_values,
+            IndCqc.unix_time,
+            IndCqc.first_submission_time,
+            "min",
+        )
+
+        get_selected_value_mock.assert_any_call(
+            self.extrapolation_df,
+            self.window_spec_all_rows,
+            self.column_with_null_values,
+            IndCqc.unix_time,
+            IndCqc.last_submission_time,
+            "max",
+        )
+
+    def test_calculate_first_and_last_submission_dates_returns_same_number_of_rows(
+        self,
+    ):
+        self.assertEqual(self.extrapolation_df.count(), self.returned_df.count())
+
+    def test_model_extrapolation_and_interpolation_returns_new_columns(self):
+        self.assertIn(IndCqc.first_submission_time, self.returned_df.columns)
+        self.assertIn(IndCqc.last_submission_time, self.returned_df.columns)
