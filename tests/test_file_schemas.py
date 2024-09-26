8e8cda832dbc3eaf1f8366fffe8680a855e6e060
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-09-26 14:02:01 +0100

    amended test data for CT diagnostics

--- tests/test_file_schemas.py
+++ tests/test_file_schemas.py
@@ -4139,6 +4139,7 @@ class DiagnosticsOnCapacityTrackerSchemas:
             StructField(
                 IndCQC.extrapolation_non_res_with_dormancy_model, FloatType(), True
             ),
+            StructField(IndCQC.extrapolation_rolling_average_model, FloatType(), True),
             StructField(IndCQC.estimate_filled_posts, FloatType(), True),
             StructField(Keys.year, StringType(), True),
             StructField(Keys.month, StringType(), True),
