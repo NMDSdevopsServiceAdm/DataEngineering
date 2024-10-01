edf911d330d5ab05fed399220c0978dd22017199
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-10-01 12:13:46 +0100

    interpolation data

--- tests/test_file_schemas.py
+++ tests/test_file_schemas.py
@@ -2640,7 +2640,7 @@ class ModelInterpolationNew:
             StructField(IndCQC.cqc_location_import_date, DateType(), False),
             StructField(IndCQC.unix_time, LongType(), False),
             StructField(IndCQC.ascwds_filled_posts_dedup_clean, DoubleType(), True),
-            StructField(IndCQC.rolling_average_model, DoubleType(), True),
+            StructField(IndCQC.extrapolation_forwards, DoubleType(), True),
         ]
     )
 
