85d1e17922ad8b4e0a980de167e89f0e19e3fdc4
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-10-15 15:12:04 +0100

    Remove interpolation from job

--- jobs/estimate_missing_ascwds_ind_cqc_filled_posts.py
+++ jobs/estimate_missing_ascwds_ind_cqc_filled_posts.py
@@ -15,7 +15,6 @@ from utils.estimate_filled_posts.models.primary_service_rolling_average import (
 from utils.estimate_filled_posts.models.imputation_with_extrapolation_and_interpolation import (
     model_imputation_with_extrapolation_and_interpolation,
 )
-from utils.estimate_filled_posts.models.interpolation import model_interpolation
 from utils.estimate_filled_posts.models.extrapolation import model_extrapolation
 
 
@@ -65,21 +64,9 @@ def main(
         True,
         IndCQC.imputed_ratio_rolling_avg_model,
     )
-
+    """
     estimate_missing_ascwds_df = model_extrapolation(
         estimate_missing_ascwds_df, IndCQC.rolling_average_model
-    )  # TODO remove
-
-    estimate_missing_ascwds_df = model_interpolation(
-        estimate_missing_ascwds_df,
-        IndCQC.ascwds_filled_posts_dedup_clean,
-        IndCQC.interpolation_model_ascwds_filled_posts_dedup_clean,
-    )  # TODO remove
-
-    estimate_missing_ascwds_df = model_interpolation(
-        estimate_missing_ascwds_df,
-        IndCQC.filled_posts_per_bed_ratio,
-        IndCQC.interpolation_model_filled_posts_per_bed_ratio,
     )  # TODO remove
 
     estimate_missing_ascwds_df = (
@@ -91,11 +78,11 @@ def main(
     estimate_missing_ascwds_df = merge_imputed_columns(
         estimate_missing_ascwds_df
     )  # TODO function no longer required
-
+    
     estimate_missing_ascwds_df = null_changing_carehome_status_from_imputed_columns(
         estimate_missing_ascwds_df
     )  # TODO check if the function is still required
-
+    """
     print(f"Exporting as parquet to {estimated_missing_ascwds_ind_cqc_destination}")
 
     utils.write_to_parquet(
