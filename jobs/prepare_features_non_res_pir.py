b7e68907cb382df037029347b0bc1790e750e09e
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-10-28 10:23:51 +0000

    create non res pir features job

---
+++ jobs/prepare_features_non_res_pir.py
@@ -0,0 +1,73 @@
+import sys
+
+from pyspark.sql import DataFrame
+
+from utils.column_names.ind_cqc_pipeline_columns import (
+    IndCqcColumns as IndCqc,
+)
+from utils.column_values.categorical_column_values import CareHome
+from utils.features.helper import vectorise_dataframe
+from utils import utils
+
+non_res_pir_columns = [
+    IndCqc.location_id,
+    IndCqc.cqc_location_import_date,
+    IndCqc.care_home,
+    IndCqc.people_directly_employed_dedup,
+    IndCqc.imputed_non_res_people_directly_employed,
+]
+
+
+def main(
+    ind_cqc_filled_posts_cleaned_source: str,
+    non_res_pir_features_destination: str,
+) -> DataFrame:
+    print("Creating non res PIR features dataset...")
+
+    locations_df = utils.read_from_parquet(
+        ind_cqc_filled_posts_cleaned_source, non_res_pir_columns
+    )
+    non_res_df = utils.select_rows_with_value(
+        locations_df, IndCqc.care_home, CareHome.not_care_home
+    )
+    features_df = utils.select_rows_with_non_null_value(
+        non_res_df, IndCqc.imputed_non_res_people_directly_employed
+    )
+    vectorised_features_df = vectorise_dataframe(
+        df=features_df,
+        list_for_vectorisation=[IndCqc.imputed_non_res_people_directly_employed],
+    )
+
+    print(f"Exporting as parquet to {non_res_pir_features_destination}")
+
+    utils.write_to_parquet(
+        vectorised_features_df,
+        non_res_pir_features_destination,
+        mode="overwrite",
+    )
+
+
+if __name__ == "__main__":
+    print("Spark job 'prepare_features_non_res_pir' starting...")
+    print(f"Job parameters: {sys.argv}")
+
+    (
+        ind_cqc_filled_posts_cleaned_source,
+        non_res_pir_features_destination,
+    ) = utils.collect_arguments(
+        (
+            "--ind_cqc_filled_posts_cleaned_source",
+            "Source s3 directory for ind_cqc_filled_posts_cleaned dataset",
+        ),
+        (
+            "--non_res_pir_features_destination",
+            "A destination directory for outputting non-res PIR features",
+        ),
+    )
+
+    main(
+        ind_cqc_filled_posts_cleaned_source,
+        non_res_pir_features_destination,
+    )
+
+    print("Spark job 'prepare_features_non_res_pir' complete")
