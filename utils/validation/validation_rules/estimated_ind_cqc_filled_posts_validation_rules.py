5e0dddfea094efc559aae2549d4cec732aaa0f7c
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-11-01 11:26:51 +0000

    Comment out pir max value check

--- utils/validation/validation_rules/estimated_ind_cqc_filled_posts_validation_rules.py
+++ utils/validation/validation_rules/estimated_ind_cqc_filled_posts_validation_rules.py
@@ -52,7 +52,7 @@ class EstimatedIndCqcFilledPostsValidationRules:
             IndCqcColumns.non_res_with_dormancy_model: 3000.0,
             IndCqcColumns.non_res_without_dormancy_model: 3000.0,
             IndCqcColumns.number_of_beds: 500,
-            IndCqcColumns.people_directly_employed_dedup: 3000,
+            # IndCqcColumns.people_directly_employed_dedup: 3000, # temporarily removed until PIR data is cleaned
             IndCqcColumns.non_res_pir_linear_regression_model: 3000.0,
             IndCqcColumns.unix_time: int(time.time()),  # current unix time
         },
