5fffd5a61104a7241c073b94ee076892698f7940
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2024-11-05 15:55:25 +0000

    trigger coverage pipeline

--- terraform/pipeline/step-function.tf
+++ terraform/pipeline/step-function.tf
@@ -27,7 +27,7 @@ resource "aws_sfn_state_machine" "ind_cqc_filled_post_estimates_pipeline_state_m
     cqc_crawler_name                                      = module.cqc_crawler.crawler_name
     ind_cqc_filled_posts_crawler_name                     = module.ind_cqc_filled_posts_crawler.crawler_name
     ons_crawler_name                                      = module.ons_crawler.crawler_name
-    run_coverage_state_machine_arn                        = aws_sfn_state_machine.coverage_state_machine.arn
+    trigger_coverage_state_machine_arn                    = aws_sfn_state_machine.coverage_state_machine.arn
     run_silver_validation_state_machine_arn               = aws_sfn_state_machine.silver_validation_state_machine.arn
     run_gold_validation_state_machine_arn                 = aws_sfn_state_machine.gold_validation_state_machine.arn
     pipeline_failure_lambda_function_arn                  = aws_lambda_function.error_notification_lambda.arn
