605eb8b66bdfeb9fc89af59002b34ab9d463c5b2
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-11-29 14:15:47 +0000

    Define validaiton rules

---
+++ utils/validation/validation_rules/cleaned_capacity_tracker_care_home_validation_rules.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+
+from utils.column_names.capacity_tracker_columns import (
+    CapacityTrackerCareHomeCleanColumns as CTCHClean,
+)
+from utils.validation.validation_rule_names import RuleNames as RuleName
+
+
+@dataclass
+class CleanedCapacityTrackerCareHomeValidationRules:
+    rules_to_check = {
+        RuleName.size_of_dataset: None,
+        RuleName.complete_columns: [
+            CTCHClean.cqc_id,
+            CTCHClean.capacity_tracker_import_date,
+        ],
+        RuleName.index_columns: [
+            CTCHClean.cqc_id,
+            CTCHClean.capacity_tracker_import_date,
+        ],
+        RuleName.min_values: {
+            CTCHClean.nurses_employed: 0,
+            CTCHClean.care_workers_employed: 0,
+            CTCHClean.non_care_workers_employed: 0,
+            CTCHClean.agency_nurses_employed: 0,
+            CTCHClean.agency_care_workers_employed: 0,
+            CTCHClean.agency_non_care_workers_employed: 0,
+            CTCHClean.non_agency_total_employed: 1,
+            CTCHClean.agency_total_employed: 0,
+            CTCHClean.agency_and_non_agency_total_employed: 1,
+        },
+        RuleName.max_values: {
+            CTCHClean.nurses_employed: 300,
+            CTCHClean.care_workers_employed: 1000,
+            CTCHClean.non_care_workers_employed: 300,
+            CTCHClean.agency_nurses_employed: 1000,
+            CTCHClean.agency_care_workers_employed: 2500,
+            CTCHClean.agency_non_care_workers_employed: 1000,
+            CTCHClean.non_agency_total_employed: 1000,
+            CTCHClean.agency_total_employed: 4000,
+            CTCHClean.agency_and_non_agency_total_employed: 4000,
+        },
+    }
