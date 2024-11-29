26507dd69967751448e45d51c86256631822bb1a
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2024-11-29 14:29:03 +0000

    Set up step function

--- terraform/pipeline/step-function.tf
+++ terraform/pipeline/step-function.tf
@@ -281,6 +281,29 @@ resource "aws_sfn_state_machine" "gold_validation_state_machine" {
   ]
 }
 
+resource "aws_sfn_state_machine" "capacity_tracker_validation_state_machine" {
+  name     = "${local.workspace_prefix}-Capacity-Tracker-Validation-Pipeline"
+  role_arn = aws_iam_role.step_function_iam_role.arn
+  type     = "STANDARD"
+  definition = templatefile("step-functions/CapacityTrackerValidationPipeline-StepFunction.json", {
+    dataset_bucket_uri                                = module.datasets_bucket.bucket_uri
+    validate_capacity_tracker_care_home_data_job_name = module.validate_capacity_tracker_care_home_data_job.job_name
+    data_validation_reports_crawler_name              = module.data_validation_reports_crawler.crawler_name
+    pipeline_failure_lambda_function_arn              = aws_lambda_function.error_notification_lambda.arn
+  })
+
+  logging_configuration {
+    log_destination        = "${aws_cloudwatch_log_group.state_machines.arn}:*"
+    include_execution_data = true
+    level                  = "ERROR"
+  }
+
+  depends_on = [
+    aws_iam_policy.step_function_iam_policy,
+    module.datasets_bucket
+  ]
+}
+
 resource "aws_sfn_state_machine" "coverage_state_machine" {
   name     = "${local.workspace_prefix}-Coverage-Pipeline"
   role_arn = aws_iam_role.step_function_iam_role.arn
