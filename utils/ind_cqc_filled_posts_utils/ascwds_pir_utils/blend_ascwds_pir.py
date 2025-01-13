b26bd048388a88220d8182713a14bbab2a4603a1
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2025-01-13 18:07:26 +0000

    Add notes to script for tomorrow

--- utils/ind_cqc_filled_posts_utils/ascwds_pir_utils/blend_ascwds_pir.py
+++ utils/ind_cqc_filled_posts_utils/ascwds_pir_utils/blend_ascwds_pir.py
@@ -1,48 +1,44 @@
-import sys
-from typing import Optional
-
 from pyspark.sql import DataFrame, Window, functions as F
 
-from utils import utils
-import utils.cleaning_utils as cUtils
 from utils.column_names.ind_cqc_pipeline_columns import (
-    PartitionKeys as Keys,
     IndCqcColumns as IndCQC,
-)
-from utils.column_values.categorical_column_values import CareHome
-from utils.ind_cqc_filled_posts_utils.ascwds_filled_posts_calculator.ascwds_filled_posts_calculator import (
-    calculate_ascwds_filled_posts,
-)
-from utils.ind_cqc_filled_posts_utils.clean_ascwds_filled_post_outliers.clean_ascwds_filled_post_outliers import (
-    clean_ascwds_filled_post_outliers,
 )
 from utils.ind_cqc_filled_posts_utils.utils import (
     get_selected_value,
 )
 
-    w = w = (
+
+def blend_pir_and_ascwds_when_ascwds_out_of_date(df: DataFrame) -> DataFrame:
+    w = w = (  # for selected values
         Window.partitionBy(IndCQC.location_id)
         .orderBy(IndCQC.cqc_location_import_date)
         .rowsBetween(Window.unboundedPreceding, Window.currentRow)
     )
-    locations_df = create_repeated_ascwds_clean_column(locations_df)
+    df = create_repeated_ascwds_clean_column(df)
+    # TODO: create pir dedup modelled column for comparison
+    # TODO: Abstract get selected value functions into function to create last submission dates
 
-    locations_df = get_selected_value(
-        locations_df,
+    df = get_selected_value(
+        df,
         w,
         IndCQC.ascwds_filled_posts_dedup_clean,
         IndCQC.cqc_location_import_date,
         "last_ascwds_submission",
         "last",
     )
-    locations_df = get_selected_value(
-        locations_df,
+    df = get_selected_value(
+        df,
         w,
-        IndCQC.people_directly_employed_dedup,
+        IndCQC.people_directly_employed_dedup,  # use modelled column instead
         IndCQC.cqc_location_import_date,
         "last_pir_submission",
         "last",
     )
+
+    # TODO: for rows where pir is more than 2 years later than asc and gap in value is greater than +/- 100 and +/- 50% and pir filled posts is not null, add pir filled posts into ascwds clean column
+    # TODO: drop unwanted columns
+    return df
+
 
 def create_repeated_ascwds_clean_column(df: DataFrame):
     w = (
