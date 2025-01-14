0d33b1b2d2dfee11e06a67e4fce659fd9db46af3
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2025-01-14 09:08:17 +0000

    Create blank functions in job

--- utils/ind_cqc_filled_posts_utils/ascwds_pir_utils/blend_ascwds_pir.py
+++ utils/ind_cqc_filled_posts_utils/ascwds_pir_utils/blend_ascwds_pir.py
@@ -9,14 +9,41 @@ from utils.ind_cqc_filled_posts_utils.utils import (
 
 
 def blend_pir_and_ascwds_when_ascwds_out_of_date(df: DataFrame) -> DataFrame:
-    w = w = (  # for selected values
+    df = create_repeated_ascwds_clean_column(df)
+    # TODO: create pir dedup modelled column for comparison
+    df = create_people_directly_employed_dedup_modelled_column(df)
+    # TODO: Abstract get selected value functions into function to create last submission dates
+    df = create_last_submission_columns()
+    # TODO: for rows where pir is more than 2 years later than asc and gap in value is greater than +/- 100 and +/- 50% and pir filled posts is not null, add pir filled posts into ascwds clean column
+    df = merge_people_directly_employed_modelled_into_ascwds_clean_column(df)
+    # TODO: drop unwanted columns
+    df = drop_unwanted_columns(df)
+    return df
+
+
+def create_repeated_ascwds_clean_column(df: DataFrame) -> DataFrame:
+    w = (
+        Window.partitionBy(IndCQC.location_id)
+        .orderBy(IndCQC.cqc_location_import_date)
+        .rowsBetween(Window.unboundedPreceding, Window.currentRow)
+    )
+    df = df.withColumn(
+        IndCQC.ascwds_filled_posts_dedup_clean_repeated,
+        F.last(IndCQC.ascwds_filled_posts_dedup_clean, ignorenulls=True).over(w),
+    )
+    return df
+
+
+def create_people_directly_employed_dedup_modelled_column(df: DataFrame) -> DataFrame:
+    return df
+
+
+def create_last_submission_columns(df: DataFrame) -> DataFrame:
+    w = w = (
         Window.partitionBy(IndCQC.location_id)
         .orderBy(IndCQC.cqc_location_import_date)
         .rowsBetween(Window.unboundedPreceding, Window.unboundedFollowing)
     )
-    df = create_repeated_ascwds_clean_column(df)
-    # TODO: create pir dedup modelled column for comparison
-    # TODO: Abstract get selected value functions into function to create last submission dates
 
     df = get_selected_value(
         df,
@@ -35,19 +62,14 @@ def blend_pir_and_ascwds_when_ascwds_out_of_date(df: DataFrame) -> DataFrame:
         "last",
     )
 
-    # TODO: for rows where pir is more than 2 years later than asc and gap in value is greater than +/- 100 and +/- 50% and pir filled posts is not null, add pir filled posts into ascwds clean column
-    # TODO: drop unwanted columns
     return df
 
 
-def create_repeated_ascwds_clean_column(df: DataFrame):
-    w = (
-        Window.partitionBy(IndCQC.location_id)
-        .orderBy(IndCQC.cqc_location_import_date)
-        .rowsBetween(Window.unboundedPreceding, Window.currentRow)
-    )
-    df = df.withColumn(
-        IndCQC.ascwds_filled_posts_dedup_clean_repeated,
-        F.last(IndCQC.ascwds_filled_posts_dedup_clean, ignorenulls=True).over(w),
-    )
+def merge_people_directly_employed_modelled_into_ascwds_clean_column(
+    df: DataFrame,
+) -> DataFrame:
+    return df
+
+
+def drop_unwanted_columns(df: DataFrame) -> DataFrame:
     return df
