ccc2918636cc7cca7e261fe99bdc9bc2401e69a9
Turhan Mehmed <turhan.mehmed@madetech.com>
2025-02-05 13:33:12 +0000

    removed the previous to functions from the ascwds_job_role_count scipt in the utils folder as we are no longer creating a mapped column

--- utils/estimate_filled_posts_by_job_role_utils/ascwds_job_role_count/ascwds_job_role_count.py
+++ utils/estimate_filled_posts_by_job_role_utils/ascwds_job_role_count/ascwds_job_role_count.py
@@ -48,66 +48,6 @@ list_of_job_roles = [
     MainJobRoleLabels.support_worker
     ]
 
-
-def count_job_role_per_establishment(df: DataFrame) -> DataFrame:
-    """
-    Counts the number of rows per establishmentid, import date and main job role.
-
-    This function groups the ASC-WDS worker dataset by establishmentid, import date and main job role
-    and adds a column with the count of rows per group.
-    Duplicate rows by establishmentid, import date and main job role are removed.
-
-    Args:
-        df (DataFrame): A dataframe containing cleaned ASC-WDS worker data.
-
-    Returns:
-        DataFrame: A dataframe with unique establishmentid, import date and main job role and row count.
-    """
-
-    df = df.groupBy(
-        F.col(AWKClean.establishment_id),
-        F.col(AWKClean.ascwds_worker_import_date),
-        F.col(AWKClean.main_job_role_clean_labelled),
-    ).agg(
-        F.count(F.col(AWKClean.main_job_role_clean_labelled)).alias(
-            IndCQC.ascwds_main_job_role_counts
-        )
-    )
-    return df
-
-
-def convert_job_role_count_to_job_role_map(df: DataFrame) -> DataFrame:
-    """
-    Adds a column with a dictionary created from main job role and main job role count.
-
-    Adds column which contains a dictionary. The keys are main job role and values are main job role count.
-    Each dictionary is per establishmentid and import date.
-    Main job role and main job role count columns are removed and duplicate rows by establishmentid and
-    import date are removed.
-
-    Args:
-        df (DataFrame): A dataframe containing cleaned ASC-WDS worker data with a count per main job role.
-
-    Returns:
-        DataFrame: A dataframe with unique establishmentid, import date and dictionary where key = main job role value = main job role count.
-    """
-    struct_column: str = "struct_column"
-    df_struct = df.withColumn(
-        struct_column,
-        F.struct(
-            F.col(AWKClean.main_job_role_clean_labelled),
-            F.col(IndCQC.ascwds_main_job_role_counts),
-        ),
-    )
-    df_mapped = df_struct.groupBy(
-        F.col(AWKClean.establishment_id), F.col(AWKClean.ascwds_worker_import_date)
-    ).agg(
-        F.map_from_entries(F.collect_list(struct_column)).alias(
-            IndCQC.ascwds_main_job_role_counts
-        )
-    )
-    return df_mapped
-
 def count_job_role_per_establishment_as_columns(df: DataFrame) -> DataFrame:
     """
     Group the worker dataset by establishment id and import date.
