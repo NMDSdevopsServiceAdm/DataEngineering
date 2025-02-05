7b17be5d0ae9ada056f93e6977e572bbf48c1af7
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-02-05 16:06:54 +0000

    add docstrings

--- jobs/ingest_ascwds_dataset.py
+++ jobs/ingest_ascwds_dataset.py
@@ -10,6 +10,14 @@ from utils.column_names.raw_data_files.ascwds_worker_columns import (
 
 
 def main(source: str, destination: str, dataset: str = "ascwds"):
+    """
+    Main function to handle the ingestion of single or multiple ASCWDS/NMDSSC files.
+
+    Args:
+        source (str): The source CSV file or S3 directory.
+        destination (str): The destination directory for outputting parquet files.
+        dataset (str): The dataset type, either 'ascwds' or 'nmdssc'. Defaults to 'ascwds'.
+    """
     bucket, prefix = utils.split_s3_uri(source)
 
     if utils.is_csv(source):
@@ -21,12 +29,31 @@ def main(source: str, destination: str, dataset: str = "ascwds"):
 def ingest_single_file(
     source: str, bucket: str, prefix: str, destination: str, dataset: str
 ):
+    """
+    Handle the ingestion of a single CSV file.
+
+    Args:
+        source (str): The source file.
+        bucket (str): The S3 bucket name.
+        prefix (str): The S3 prefix (key) of the file.
+        destination (str): The destination directory for outputting parquet files.
+        dataset (str): The dataset type, either 'ascwds' or 'nmdssc'.
+    """
     print("Single file provided to job. Handling single file.")
     new_destination = utils.construct_destination_path(destination, prefix)
     handle_job(source, bucket, prefix, new_destination, dataset)
 
 
 def ingest_multiple_files(bucket: str, prefix: str, destination: str, dataset: str):
+    """
+    Handle the ingestion of multiple files.
+
+    Args:
+        bucket (str): The S3 bucket name.
+        prefix (str): The S3 prefix (key) of the files.
+        destination (str): The destination directory for outputting parquet files.
+        dataset (str): The dataset type, either 'ascwds' or 'nmdssc'.
+    """
     print("Multiple files provided to job. Handling each file...")
     objects_list = utils.get_s3_objects_list(bucket, prefix)
 
@@ -42,6 +69,20 @@ def ingest_multiple_files(bucket: str, prefix: str, destination: str, dataset: s
 def handle_job(
     source: str, source_bucket: str, source_key: str, destination: str, dataset: str
 ):
+    """
+    Handle the job of reading CSV file(s), processing then writing the data to parquet format.
+
+    ASCWDS and NMDSSC files are very similar in structure but have some differences in the data they contain.
+    - An error is raised if any new ASCWDS files contain unknown main job role IDs as this should no longer occur.
+    - NMDSSC files contain date columns in the format MM/dd/yyyy which are converted to dd/MM/yyyy to match ASCWDS date formats.
+
+    Args:
+        source (str): The source file.
+        source_bucket (str): The S3 bucket name.
+        source_key (str): The S3 prefix (key) of the file.
+        destination (str): The destination directory for outputting parquet files.
+        dataset (str): The dataset type, either 'ascwds' or 'nmdssc'.
+    """
     file_sample = utils.read_partial_csv_content(source_bucket, source_key)
     delimiter = utils.identify_csv_delimiter(file_sample)
 
