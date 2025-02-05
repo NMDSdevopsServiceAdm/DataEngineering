4d4eb8b02f9b7111c9b5e3a19f2dc9c82bd04727
Roy Price <Roy.price1988@gmail.com>
2025-02-05 16:09:52 +0000

    Added remaining test cases to test script

--- tests/unit/test_count_registered_manager_names.py
+++ tests/unit/test_count_registered_manager_names.py
@@ -32,3 +32,79 @@ class CountRegisteredManagerNamesTests(unittest.TestCase):
         self.assertEqual(
             returned_df.sort(IndCQC.location_id).collect(), expected_df.collect()
         )
+
+    def test_count_registered_manager_names_when_location_has_two_registered_managers(
+        self,
+    ):
+        test_df = self.spark.createDataFrame(
+            Data.location_with_two_registered_managers,
+            Schemas.location_with_list_of_names,
+        )
+
+        expected_df = self.spark.createDataFrame(
+            Data.expected_location_with_two_registered_managers,
+            Schemas.location_with_list_of_names,
+        )
+
+        returned_df = count_registered_manager_names(test_df)
+
+        self.assertEqual(
+            returned_df.sort(IndCQC.location_id).collect(), expected_df.collect()
+        )
+
+    def test_count_registered_manager_names_when_location_has_null_registered_manager(
+        self,
+    ):
+        test_df = self.spark.createDataFrame(
+            Data.location_with_no_registered_manager,
+            Schemas.location_with_list_of_names,
+        )
+
+        expected_df = self.spark.createDataFrame(
+            Data.expected_location_with_no_registered_manager,
+            Schemas.location_with_list_of_names,
+        )
+
+        returned_df = count_registered_manager_names(test_df)
+
+        self.assertEqual(
+            returned_df.sort(IndCQC.location_id).collect(), expected_df.collect()
+        )
+
+    def test_count_registered_manager_names_when_two_locations_have_different_number_of_registered_managers(
+        self,
+    ):
+        test_df = self.spark.createDataFrame(
+            Data.two_locations_with_different_number_of_registered_managers,
+            Schemas.location_with_list_of_names,
+        )
+
+        expected_df = self.spark.createDataFrame(
+            Data.expected_two_locations_with_different_number_of_registered_managers,
+            Schemas.location_with_list_of_names,
+        )
+
+        returned_df = count_registered_manager_names(test_df)
+
+        self.assertEqual(
+            returned_df.sort(IndCQC.location_id).collect(), expected_df.collect()
+        )
+
+    def test_count_registered_manager_names_when_location_has_different_registered_manager_count_at_different_import_dates(
+        self,
+    ):
+        test_df = self.spark.createDataFrame(
+            Data.location_with_different_number_of_registered_managers_at_different_import_dates,
+            Schemas.location_with_list_of_names,
+        )
+
+        expected_df = self.spark.createDataFrame(
+            Data.expected_location_with_different_number_of_registered_managers_at_different_import_dates,
+            Schemas.location_with_list_of_names,
+        )
+
+        returned_df = count_registered_manager_names(test_df)
+
+        self.assertEqual(
+            returned_df.sort(IndCQC.location_id).collect(), expected_df.collect()
+        )
