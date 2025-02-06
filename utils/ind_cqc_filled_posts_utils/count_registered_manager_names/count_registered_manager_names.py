23a2d4d91ff4b58284d694622246deadad49a1c7
Roy Price <Roy.price1988@gmail.com>
2025-02-06 13:04:38 +0000

    Updated doc string and removed comment line.

--- utils/ind_cqc_filled_posts_utils/count_registered_manager_names/count_registered_manager_names.py
+++ utils/ind_cqc_filled_posts_utils/count_registered_manager_names/count_registered_manager_names.py
@@ -7,6 +7,11 @@ def count_registered_manager_names(df: DataFrame) -> DataFrame:
     """
     Adds a column with a count of elements within list of registered manager names.
 
+    This function uses the size method to count elements in list of strings. This method
+    returns the count, including null elements within a list, when list partially populated.
+    It returns 0 when list is empty. It returns -1 when column is null.
+    Therefore, after the counting, this function recodes values of -1 to 0.
+
     Args:
         df (DataFrame): A dataframe containing list of registered manager names.
 
@@ -18,7 +23,6 @@ def count_registered_manager_names(df: DataFrame) -> DataFrame:
         IndCQC.registered_manager_count, F.size(F.col(IndCQC.registered_manager_names))
     )
 
-    # The size method returns -1 when sizing null.
     df = df.withColumn(
         IndCQC.registered_manager_count,
         F.when(F.col(IndCQC.registered_manager_count) == -1, F.lit(0)).otherwise(
