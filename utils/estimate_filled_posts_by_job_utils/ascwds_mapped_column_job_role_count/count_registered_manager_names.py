cd9d060dc8908864b8f3b2ab84fc0cd9ade97917
Roy Price <Roy.price1988@gmail.com>
2025-02-06 11:16:39 +0000

    Added doc string

--- utils/estimate_filled_posts_by_job_utils/ascwds_mapped_column_job_role_count/count_registered_manager_names.py
+++ utils/estimate_filled_posts_by_job_utils/ascwds_mapped_column_job_role_count/count_registered_manager_names.py
@@ -4,6 +4,16 @@ from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCQC
 
 
 def count_registered_manager_names(df: DataFrame) -> DataFrame:
+    """
+    Adds a column with a count of elements within list of registered manager names.
+
+    Args:
+        df (DataFrame): A dataframe containing list of registered manager names.
+
+    Returns:
+        DataFrame: A dataframe with count of elements in list of registered manager names.
+    """
+
     df = df.withColumn(
         IndCQC.registered_manager_count, F.size(F.col(IndCQC.registered_manager_names))
     )
