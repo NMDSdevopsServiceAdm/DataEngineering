faa2c00dd8f54801394d28a3c58639928c489aa1
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-02-19 12:30:56 +0000

    temporarily remove code and tidy up before adding back in

--- utils/estimate_filled_posts_by_job_role_utils/utils.py
+++ utils/estimate_filled_posts_by_job_role_utils/utils.py
@@ -1,8 +1,5 @@
 from pyspark.sql import DataFrame, functions as F
 
-from utils.column_names.cleaned_data_files.ascwds_worker_cleaned import (
-    AscwdsWorkerCleanedColumns as AWKClean,
-)
 from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCQC
 from utils.value_labels.ascwds_worker.ascwds_worker_mainjrid import (
     AscwdsWorkerValueLabelsMainjrid,
@@ -39,64 +36,3 @@ def count_registered_manager_names(df: DataFrame) -> DataFrame:
     )
 
     return df
-
-
-def count_job_role_per_establishment_as_columns(
-    df: DataFrame, list_of_columns_for_job_role: list
-) -> DataFrame:
-    """
-    Group the worker dataset by establishment id and import date.
-    Subsequently performs a pivot on the clean job role labels which will be the additional columns in the grouped data.
-    Any labels in the list of job roles not present in the establishment will have their counts aggregated to zero.
-
-    Args:
-        df (DataFrame): A dataframe containing cleaned ASC-WDS worker data.
-        list_of_columns_for_job_role (list): A list containing the ASC-WDS job role.
-
-    Returns:
-        DataFrame: A dataframe with unique establishmentid and import date.
-    """
-    df = (
-        df.groupBy(
-            F.col(AWKClean.establishment_id),
-            F.col(AWKClean.ascwds_worker_import_date),
-        )
-        .pivot(AWKClean.main_job_role_clean_labelled, list_of_columns_for_job_role)
-        .count()
-    )
-
-    df = df.na.fill(0, subset=list_of_columns_for_job_role)
-
-    for column in list_of_columns_for_job_role:
-        df = df.withColumnRenamed(column, f"job_role_count_{column}")
-
-    return df
-
-
-def merge_dataframes(posts_df: DataFrame, workers_df: DataFrame) -> DataFrame:
-    """
-    Join the individual job role count columns from the ASCWDS worker file into the estimated filled post DataFrame, matched on ASCWDS establishment_id and import_date.
-
-    Args:
-        posts_df (DataFrame): A dataframe containing cleaned IndCQC workplace Data.
-        workers_df (DataFrame): ASC-WDS worker dataframe grouped to include columns with job role counts per job role.
-
-    Returns:
-        DataFrame: The IndCQC DataFrame merged to include job role count columns.
-    """
-
-    result_df = (
-        posts_df.join(
-            workers_df,
-            (posts_df[IndCQC.establishment_id] == workers_df[AWKClean.establishment_id])
-            & (
-                posts_df[IndCQC.ascwds_workplace_import_date]
-                == workers_df[AWKClean.ascwds_worker_import_date]
-            ),
-            "left",
-        )
-        .drop(workers_df[AWKClean.establishment_id])
-        .drop(workers_df[AWKClean.ascwds_worker_import_date])
-    )
-
-    return result_df
