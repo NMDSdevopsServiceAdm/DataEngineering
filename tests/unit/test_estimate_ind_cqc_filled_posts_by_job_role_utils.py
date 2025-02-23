2eb87b0de29fade8161263c23801d3d023e48ab0
Turhan Mehmed <turhan.mehmed@madetech.com>
2025-02-23 18:32:10 +0000

    changed variable names in the test scrit

--- tests/unit/test_estimate_ind_cqc_filled_posts_by_job_role_utils.py
+++ tests/unit/test_estimate_ind_cqc_filled_posts_by_job_role_utils.py
@@ -600,43 +600,43 @@ class CountJobRoleSplitByServiceTypeTests(EstimateIndCQCFilledPostsByJobRoleUtil
     def test_sum_job_role_count_split_by_service_when_duplicate_entries_in_partition_column(
         self,
     ):
-        input_df = self.spark.createDataFrame(
+        test_df = self.spark.createDataFrame(
             Data.count_job_role_split_by_service_with_duplicate_service_types_data,
             Schemas.count_job_role_split_by_service_schema,
         )
 
-        expected_output_df = self.spark.createDataFrame(
+        expected_df = self.spark.createDataFrame(
             Data.expected_count_job_role_split_by_service_with_duplicate_service_types_data,
             Schemas.expected_count_role_split_by_service_schema,
         )
 
-        output_df = job.sum_job_role_count_split_by_service(
-            input_df, Data.list_of_job_roles_for_tests
+        return_df = job.sum_job_role_count_split_by_service(
+            test_df, Data.list_of_job_roles_for_tests
         )
 
         self.assertEqual(
-            expected_output_df.sort(IndCQC.primary_service_type).collect(),
-            output_df.sort(IndCQC.primary_service_type).collect(),
+            expected_df.sort(IndCQC.primary_service_type).collect(),
+            return_df.sort(IndCQC.primary_service_type).collect(),
         )
 
     def test_sum_job_role_count_split_by_service_when_one_entry_in_partition_column(
         self,
     ):
-        input_df = self.spark.createDataFrame(
+        test_df = self.spark.createDataFrame(
             Data.count_job_role_split_by_service_with_one_service_types_data,
             Schemas.count_job_role_split_by_service_schema,
         )
 
-        expected_output_df = self.spark.createDataFrame(
+        expected_df = self.spark.createDataFrame(
             Data.expected_count_job_role_split_by_service_with_one_service_types_data,
             Schemas.expected_count_role_split_by_service_schema,
         )
 
-        output_df = job.sum_job_role_count_split_by_service(
-            input_df, Data.list_of_job_roles_for_tests
+        return_df = job.sum_job_role_count_split_by_service(
+            test_df, Data.list_of_job_roles_for_tests
         )
 
         self.assertEqual(
-            expected_output_df.sort(IndCQC.primary_service_type).collect(),
-            output_df.sort(IndCQC.primary_service_type).collect(),
+            expected_df.sort(IndCQC.primary_service_type).collect(),
+            return_df.sort(IndCQC.primary_service_type).collect(),
         )
