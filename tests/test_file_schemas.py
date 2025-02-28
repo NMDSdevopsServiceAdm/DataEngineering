6a1b90627e7ae7cb786031d8779a3acfbc3144a0
Turhan Mehmed <turhan.mehmed@madetech.com>
2025-02-28 14:38:12 +0000

    minor change, getting rid of a line

--- tests/test_file_schemas.py
+++ tests/test_file_schemas.py
@@ -6055,11 +6055,12 @@ class EstimateIndCQCFilledPostsByJobRoleUtilsSchemas:
     unpacked_mapped_column_schema = StructType(
         [
             StructField(IndCQC.establishment_id, StringType(), True),
-            StructField(IndCQC.ascwds_worker_import_date, DateType(), True),
+            StructField(IndCQC.ascwds_worker_import_date, DateType(), True),            
             StructField(
                 IndCQC.ascwds_job_role_ratios, MapType(StringType(), FloatType()), True
-            ),
+            )
         ]
+
     )
 
     expected_unpacked_mapped_column_schema = StructType(
@@ -6070,4 +6071,4 @@ class EstimateIndCQCFilledPostsByJobRoleUtilsSchemas:
             StructField(MainJobRoleLabels.senior_care_worker, IntegerType(), True),
             StructField(MainJobRoleLabels.senior_management, IntegerType(), True),
         ]
-    )
+    )    
