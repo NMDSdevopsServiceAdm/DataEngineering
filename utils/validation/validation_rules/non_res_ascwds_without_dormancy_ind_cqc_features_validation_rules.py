a94cf246f128776940f9c35155fc5fc9cfe47595
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-03-05 18:49:09 +0000

    update validations

--- utils/validation/validation_rules/non_res_ascwds_without_dormancy_ind_cqc_features_validation_rules.py
+++ utils/validation/validation_rules/non_res_ascwds_without_dormancy_ind_cqc_features_validation_rules.py
@@ -23,7 +23,6 @@ class NonResASCWDSWithoutDormancyIndCqcFeaturesValidationRules:
             IndCqcColumns.time_registered,
             IndCqcColumns.service_count,
             IndCqcColumns.activity_count,
-            IndCqcColumns.specialism_count,
         ],
         RuleName.index_columns: [
             IndCqcColumns.location_id,
@@ -32,9 +31,8 @@ class NonResASCWDSWithoutDormancyIndCqcFeaturesValidationRules:
         RuleName.min_values: {
             IndCqcColumns.ascwds_pir_merged: 1.0,
             IndCqcColumns.time_registered: 0.0,
-            # IndCqcColumns.service_count: 1, # Temporarily removed whilst we fix DQ
+            IndCqcColumns.service_count: 1,
             IndCqcColumns.activity_count: 0,
-            IndCqcColumns.specialism_count: 0,
         },
         RuleName.max_values: {
             IndCqcColumns.ascwds_pir_merged: 3000.0,
