81f9244d60b964c157ba77b80eeb43c3cd48e65e
Turhan Mehmed <turhan.mehmed@madetech.com>
2025-03-09 17:48:23 +0000

    Change to the interpolation job itself

--- jobs/estimate_ind_cqc_filled_posts_by_job_role.py
+++ jobs/estimate_ind_cqc_filled_posts_by_job_role.py
@@ -127,7 +127,7 @@ def main(
         )
     )
 
-    estimated_ind_cqc_interpolated_filled_posts_by_job_role_df = (
+    estimated_ind_cqc_filled_posts_by_job_role_df = (
         interpolation.model_job_role_ratio_interpolation(
             estimated_ind_cqc_filled_posts_by_job_role_df, "straight"
         )
