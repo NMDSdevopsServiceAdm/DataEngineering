0bfda87866e690c385e34de3e607b2c96b16e2cd
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-03-10 17:07:15 +0000

    rename df

--- utils/estimate_filled_posts/models/care_homes.py
+++ utils/estimate_filled_posts/models/care_homes.py
@@ -20,21 +20,21 @@ def model_care_homes(
     gbt_trained_model = GBTRegressionModel.load(model_source)
     features_df = features_df.where(features_df[IndCqc.number_of_beds].isNotNull())
 
-    care_home_predictions = gbt_trained_model.transform(features_df)
+    care_home_predictions_df = gbt_trained_model.transform(features_df)
 
     save_model_metrics(
-        care_home_predictions,
+        care_home_predictions_df,
         IndCqc.filled_posts_per_bed_ratio,
         model_source,
         metrics_destination,
     )
 
-    locations_df = calculate_filled_posts_from_beds_and_ratio(
-        locations_df, IndCqc.prediction, IndCqc.prediction
+    care_home_predictions_df = calculate_filled_posts_from_beds_and_ratio(
+        care_home_predictions_df, IndCqc.prediction, IndCqc.prediction
     )
 
     locations_df = insert_predictions_into_locations(
-        locations_df, care_home_predictions, IndCqc.care_home_model
+        locations_df, care_home_predictions_df, IndCqc.care_home_model
     )
 
     return locations_df
