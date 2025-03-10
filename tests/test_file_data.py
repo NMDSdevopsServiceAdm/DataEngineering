03bad110391c5da2e061794f5596f5487e8caceb
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-03-10 16:31:47 +0000

    create function and tests

--- tests/test_file_data.py
+++ tests/test_file_data.py
@@ -6262,6 +6262,21 @@ class EstimateFilledPostsModelsUtils:
         ),
     ]
 
+    set_min_prediction_value_when_below_minimum_rows = [
+        ("1-001", 0.5, -7.6),
+    ]
+    expected_set_min_prediction_value_when_below_minimum_rows = [
+        ("1-001", 0.5, 1.0),
+    ]
+
+    set_min_prediction_value_when_above_minimum_rows = [
+        ("1-001", 1.5, 1.5),
+    ]
+
+    set_min_prediction_value_when_null_rows = [
+        ("1-001", None, None),
+    ]
+
 
 @dataclass
 class MLModelMetrics:
