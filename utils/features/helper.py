fe198974bbdbc7eac783bb75bcbd39e7567a5700
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-03-24 13:35:08 +0000

    remove functions no longer required

--- utils/features/helper.py
+++ utils/features/helper.py
@@ -8,35 +8,25 @@ from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCQC
 
 
 def vectorise_dataframe(df: DataFrame, list_for_vectorisation: List[str]) -> DataFrame:
+    """
+    Combines specified columns into a single feature vector for the modelling process.
+
+    This function uses `VectorAssembler` to merge multiple input columns into a single vector column.
+    Invalid values are skipped to prevent transformation errors.
+
+    Args:
+        df (DataFrame): Input DataFrame containing columns to be vectorised.
+        list_for_vectorisation (List[str]): List of column names to be combined into the feature vector.
+
+    Returns:
+        DataFrame: A DataFrame with an additional 'features' column.
+    """
     loc_df = VectorAssembler(
         inputCols=list_for_vectorisation,
         outputCol=IndCQC.features,
         handleInvalid="skip",
     ).transform(df)
     return loc_df
-
-
-def column_expansion_with_dict(
-    df: DataFrame, col_name: str, lookup_dict: Dict[str, str]
-) -> DataFrame:
-    for key in lookup_dict.keys():
-        df = df.withColumn(
-            f"{key}",
-            F.array_contains(df[f"{col_name}"], lookup_dict[key]).cast("integer"),
-        )
-    return df
-
-
-def convert_categorical_variable_to_binary_variables_based_on_a_dictionary(
-    df: DataFrame,
-    categorical_col_name: str,
-    lookup_dict: Dict[str, str],
-) -> DataFrame:
-    for key in lookup_dict.keys():
-        df = df.withColumn(
-            key, (F.col(categorical_col_name) == lookup_dict[key]).cast("integer")
-        )
-    return df
 
 
 def expand_encode_and_extract_features(
