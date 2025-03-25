335ed07d0855fdaae6c02b2dd8849d54d0e788ce
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-03-25 07:58:33 +0000

    tidy nonres features

--- jobs/prepare_features_non_res_ascwds_ind_cqc.py
+++ jobs/prepare_features_non_res_ascwds_ind_cqc.py
@@ -32,8 +32,7 @@ from utils.features.helper import (
     add_date_index_column,
     calculate_time_registered_for,
     cap_integer_at_max_value,
-    column_expansion_with_dict,
-    convert_categorical_variable_to_binary_variables_based_on_a_dictionary,
+    expand_encode_and_extract_features,
     group_rural_urban_sparse_categories,
     lag_column_value,
     vectorise_dataframe,
@@ -42,21 +41,81 @@ from utils.features.helper import (
 
 def main(
     ind_cqc_filled_posts_cleaned_source: str,
-    non_res_ascwds_inc_dormancy_ind_cqc_features_destination: str,
-    non_res_ascwds_without_dormancy_ind_cqc_features_destination: str,
+    with_dormancy_features_destination: str,
+    without_dormancy_features_destination: str,
 ) -> DataFrame:
     print("Creating non res ascwds inc dormancy features dataset...")
 
     df = utils.read_from_parquet(ind_cqc_filled_posts_cleaned_source)
 
-    features_df = create_general_non_res_feature_columns(df)
+    df = utils.select_rows_with_value(df, IndCQC.care_home, CareHome.not_care_home)
+
+    df = expand_encode_and_extract_features(
+        df,
+        col_name=IndCQC.services_offered,
+        lookup_dict=ServicesFeatures.non_res_model_labels_dict,
+        is_array_col=True,
+    )
+    df = add_array_column_count(df, IndCQC.service_count, IndCQC.services_offered)
+    df = cap_integer_at_max_value(
+        df,
+        IndCQC.service_count,
+        max_value=4,
+        new_col_name=IndCQC.service_count_capped,
+    )
+
+    df = add_array_column_count(
+        df, IndCQC.activity_count, IndCQC.imputed_regulated_activities
+    )
+    df = cap_integer_at_max_value(
+        df,
+        IndCQC.activity_count,
+        max_value=3,
+        new_col_name=IndCQC.activity_count_capped,
+    )
+
+    df = expand_encode_and_extract_features(
+        df,
+        IndCQC.specialisms_offered,
+        SpecialismsFeatures.labels_dict,
+        is_array_col=True,
+    )
+
+    df = group_rural_urban_sparse_categories(df)
+    df = expand_encode_and_extract_features(
+        df,
+        IndCQC.current_rural_urban_indicator_2011_for_non_res_model,
+        RuralUrbanFeatures.non_res_model_labels_dict,
+        is_array_col=False,
+    )
+
+    df = expand_encode_and_extract_features(
+        df,
+        IndCQC.current_region,
+        RegionFeatures.labels_dict,
+        is_array_col=False,
+    )
+
+    df = expand_encode_and_extract_features(
+        df,
+        IndCQC.related_location,
+        RelatedLocationFeatures.labels_dict,
+        is_array_col=False,
+    )
+
+    df = calculate_time_registered_for(df)
+
+    # TODO fix lag function (null values)
+    df = lag_column_value(
+        df, IndCQC.posts_rolling_average_model, IndCQC.posts_rolling_average_model_lag
+    )
 
     model_without_dormancy_features_df = (
-        create_features_specific_to_without_dormancy_model(features_df)
+        create_features_specific_to_without_dormancy_model(df)
     )
 
     model_with_dormancy_features_df = create_features_specific_to_with_dormancy_model(
-        features_df
+        df
     )
 
     dormancy_key = list(DormancyFeatures.labels_dict.keys())
@@ -110,99 +169,24 @@ def main(
     )
 
     print(
-        f"Exporting non_res_ascwds_without_dormancy_ind_cqc_features as parquet to {non_res_ascwds_without_dormancy_ind_cqc_features_destination}"
+        f"Exporting non_res_ascwds_without_dormancy_ind_cqc_features as parquet to {without_dormancy_features_destination}"
     )
     utils.write_to_parquet(
         vectorised_features_without_dormancy_df,
-        non_res_ascwds_without_dormancy_ind_cqc_features_destination,
+        without_dormancy_features_destination,
         mode="overwrite",
         partitionKeys=[Keys.year, Keys.month, Keys.day, Keys.import_date],
     )
 
     print(
-        f"Exporting non_res_ascwds_inc_dormancy_ind_cqc_features as parquet to {non_res_ascwds_inc_dormancy_ind_cqc_features_destination}"
+        f"Exporting non_res_ascwds_inc_dormancy_ind_cqc_features as parquet to {with_dormancy_features_destination}"
     )
     utils.write_to_parquet(
         vectorised_features_with_dormancy_df,
-        non_res_ascwds_inc_dormancy_ind_cqc_features_destination,
+        with_dormancy_features_destination,
         mode="overwrite",
         partitionKeys=[Keys.year, Keys.month, Keys.day, Keys.import_date],
     )
-
-
-# TODO add tests
-def create_general_non_res_feature_columns(df: DataFrame) -> DataFrame:
-    """
-    Create features which apply to both non-res models.
-
-    Args:
-        df (DataFrame): The input DataFrame.
-
-    Returns:
-        DataFrame: The output DataFrame with the added features.
-    """
-    df = utils.select_rows_with_value(df, IndCQC.care_home, CareHome.not_care_home)
-
-    df = add_array_column_count(
-        df, new_col_name=IndCQC.service_count, col_to_check=IndCQC.services_offered
-    )
-    df = cap_integer_at_max_value(
-        df,
-        col_name=IndCQC.service_count,
-        max_value=4,
-        new_col_name=IndCQC.service_count_capped,
-    )
-
-    df = add_array_column_count(
-        df,
-        new_col_name=IndCQC.activity_count,
-        col_to_check=IndCQC.imputed_regulated_activities,
-    )
-    df = cap_integer_at_max_value(
-        df,
-        col_name=IndCQC.activity_count,
-        max_value=3,
-        new_col_name=IndCQC.activity_count_capped,
-    )
-
-    df = column_expansion_with_dict(
-        df,
-        col_name=IndCQC.services_offered,
-        lookup_dict=ServicesFeatures.non_res_model_labels_dict,
-    )
-
-    df = column_expansion_with_dict(
-        df,
-        col_name=IndCQC.specialisms_offered,
-        lookup_dict=SpecialismsFeatures.labels_dict,
-    )
-
-    df = group_rural_urban_sparse_categories(df)
-    df = convert_categorical_variable_to_binary_variables_based_on_a_dictionary(
-        df,
-        categorical_col_name=IndCQC.current_rural_urban_indicator_2011_for_non_res_model,
-        lookup_dict=RuralUrbanFeatures.non_res_model_labels_dict,
-    )
-
-    df = convert_categorical_variable_to_binary_variables_based_on_a_dictionary(
-        df,
-        categorical_col_name=IndCQC.current_region,
-        lookup_dict=RegionFeatures.labels_dict,
-    )
-
-    df = convert_categorical_variable_to_binary_variables_based_on_a_dictionary(
-        df,
-        categorical_col_name=IndCQC.related_location,
-        lookup_dict=RelatedLocationFeatures.labels_dict,
-    )
-
-    df = calculate_time_registered_for(df)
-
-    df = lag_column_value(
-        df, IndCQC.posts_rolling_average_model, IndCQC.posts_rolling_average_model_lag
-    )
-
-    return df
 
 
 # TODO add tests
@@ -240,21 +224,21 @@ def create_features_specific_to_with_dormancy_model(df: DataFrame) -> DataFrame:
     """
     df = utils.select_rows_with_non_null_value(df, IndCQC.dormancy)
 
-    df = convert_categorical_variable_to_binary_variables_based_on_a_dictionary(
+    df = expand_encode_and_extract_features(
         df,
-        categorical_col_name=IndCQC.dormancy,
-        lookup_dict=DormancyFeatures.labels_dict,
+        IndCQC.dormancy,
+        DormancyFeatures.labels_dict,
+        is_array_col=False,
     )
 
     df = add_date_index_column(df)
 
     df = cap_integer_at_max_value(
         df,
-        col_name=IndCQC.time_registered,
+        IndCQC.time_registered,
         max_value=120,
         new_col_name=IndCQC.time_registered_capped_at_ten_years,
     )
-
     return df
 
 
@@ -264,27 +248,27 @@ if __name__ == "__main__":
 
     (
         ind_cqc_filled_posts_cleaned_source,
-        non_res_ascwds_inc_dormancy_ind_cqc_features_destination,
-        non_res_ascwds_without_dormancy_ind_cqc_features_destination,
+        with_dormancy_features_destination,
+        without_dormancy_features_destination,
     ) = utils.collect_arguments(
         (
             "--ind_cqc_filled_posts_cleaned_source",
             "Source s3 directory for ind_cqc_filled_posts_cleaned dataset",
         ),
         (
-            "--non_res_ascwds_inc_dormancy_ind_cqc_features_destination",
+            "--with_dormancy_features_destination",
             "A destination directory for outputting non-res ASCWDS inc dormancy model features dataset",
         ),
         (
-            "--non_res_ascwds_without_dormancy_ind_cqc_features_destination",
+            "--without_dormancy_features_destination",
             "A destination directory for outputting non-res ASCWDS without dormancy model features dataset",
         ),
     )
 
     main(
         ind_cqc_filled_posts_cleaned_source,
-        non_res_ascwds_inc_dormancy_ind_cqc_features_destination,
-        non_res_ascwds_without_dormancy_ind_cqc_features_destination,
+        with_dormancy_features_destination,
+        without_dormancy_features_destination,
     )
 
     print("Spark job 'prepare_features_non_res_ascwds_ind_cqc' complete")
