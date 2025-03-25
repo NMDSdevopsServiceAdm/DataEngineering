24de40490ce38a93a1b3c8dc10677097f8d5ce91
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-03-25 08:52:53 +0000

    non res test functions called

--- tests/unit/test_prepare_features_non_res_ascwds_ind_cqc.py
+++ tests/unit/test_prepare_features_non_res_ascwds_ind_cqc.py
@@ -2,8 +2,6 @@ import unittest
 import warnings
 from unittest.mock import ANY, Mock, patch, call
 
-from pyspark.sql import DataFrame
-
 import jobs.prepare_features_non_res_ascwds_ind_cqc as job
 from tests.test_file_data import NonResAscwdsFeaturesData as Data
 from tests.test_file_schemas import NonResAscwdsFeaturesSchema as Schemas
@@ -22,79 +20,36 @@ class NonResLocationsFeatureEngineeringTests(unittest.TestCase):
 
         warnings.simplefilter("ignore", ResourceWarning)
 
-    # TODO amend tests
-    # @patch("utils.utils.write_to_parquet")
-    # @patch("jobs.prepare_features_non_res_ascwds_ind_cqc.vectorise_dataframe")
-    # @patch("utils.utils.select_rows_with_non_null_value")
-    # @patch("jobs.prepare_features_non_res_ascwds_ind_cqc.calculate_time_registered_for")
-    # @patch("jobs.prepare_features_non_res_ascwds_ind_cqc.add_date_index_column")
-    # @patch(
-    #     "jobs.prepare_features_non_res_ascwds_ind_cqc.convert_categorical_variable_to_binary_variables_based_on_a_dictionary"
-    # )
-    # @patch("jobs.prepare_features_non_res_ascwds_ind_cqc.column_expansion_with_dict")
-    # @patch("jobs.prepare_features_non_res_ascwds_ind_cqc.cap_integer_at_max_value")
-    # @patch("jobs.prepare_features_non_res_ascwds_ind_cqc.add_array_column_count")
-    # @patch("utils.utils.select_rows_with_value")
-    # @patch("utils.utils.read_from_parquet")
-    # def test_main(
-    #     self,
-    #     read_from_parquet_mock: Mock,
-    #     select_rows_with_value_mock: Mock,
-    #     add_array_column_count_mock: Mock,
-    #     cap_integer_at_max_value_mock: Mock,
-    #     column_expansion_with_dict_mock: Mock,
-    #     convert_categorical_variable_to_binary_variables_based_on_a_dictionary_mock: Mock,
-    #     add_date_index_column_mock: Mock,
-    #     calculate_time_registered_for_mock: Mock,
-    #     select_rows_with_non_null_value_mock: Mock,
-    #     vectorise_dataframe_mock: Mock,
-    #     write_to_parquet_mock: Mock,
-    # ):
-    #     read_from_parquet_mock.return_value = self.test_df
-
-    #     job.main(
-    #         self.CLEANED_IND_CQC_TEST_DATA,
-    #         self.WITH_DORMANCY_DESTINATION,
-    #         self.WITHOUT_DORMANCY_DESTINATION,
-    #     )
-
-    #     self.assertEqual(select_rows_with_value_mock.call_count, 1)
-    #     self.assertEqual(add_array_column_count_mock.call_count, 2)
-    #     self.assertEqual(cap_integer_at_max_value_mock.call_count, 4)
-    #     self.assertEqual(column_expansion_with_dict_mock.call_count, 2)
-    #     self.assertEqual(
-    #         convert_categorical_variable_to_binary_variables_based_on_a_dictionary_mock.call_count,
-    #         4,
-    #     )
-    #     self.assertEqual(add_date_index_column_mock.call_count, 2)
-    #     self.assertEqual(calculate_time_registered_for_mock.call_count, 1)
-    #     self.assertEqual(select_rows_with_non_null_value_mock.call_count, 1)
-    #     self.assertEqual(vectorise_dataframe_mock.call_count, 2)
-
-    #     write_to_parquet_calls = [
-    #         call(
-    #             ANY,
-    #             self.WITHOUT_DORMANCY_DESTINATION,
-    #             mode="overwrite",
-    #             partitionKeys=[Keys.year, Keys.month, Keys.day, Keys.import_date],
-    #         ),
-    #         call(
-    #             ANY,
-    #             self.WITH_DORMANCY_DESTINATION,
-    #             mode="overwrite",
-    #             partitionKeys=[Keys.year, Keys.month, Keys.day, Keys.import_date],
-    #         ),
-    #     ]
-
-    #     write_to_parquet_mock.assert_has_calls(write_to_parquet_calls)
-
-    # TODO amend tests
+    @patch("jobs.prepare_features_non_res_ascwds_ind_cqc.vectorise_dataframe")
+    @patch("jobs.prepare_features_non_res_ascwds_ind_cqc.lag_column_value")
+    @patch(
+        "jobs.prepare_features_non_res_ascwds_ind_cqc.group_rural_urban_sparse_categories"
+    )
+    @patch(
+        "jobs.prepare_features_non_res_ascwds_ind_cqc.expand_encode_and_extract_features"
+    )
+    @patch("jobs.prepare_features_non_res_ascwds_ind_cqc.cap_integer_at_max_value")
+    @patch("jobs.prepare_features_non_res_ascwds_ind_cqc.calculate_time_registered_for")
+    @patch("jobs.prepare_features_non_res_ascwds_ind_cqc.add_date_index_column")
+    @patch("jobs.prepare_features_non_res_ascwds_ind_cqc.add_array_column_count")
     @patch("utils.utils.write_to_parquet")
+    @patch("utils.utils.select_rows_with_value")
+    @patch("utils.utils.select_rows_with_non_null_value")
     @patch("utils.utils.read_from_parquet")
-    def test_main(
+    def test_main_calls_all_functions(
         self,
         read_from_parquet_mock: Mock,
+        select_rows_with_non_null_value_mock: Mock,
+        select_rows_with_value_mock: Mock,
         write_to_parquet_mock: Mock,
+        add_array_column_count_mock: Mock,
+        add_date_index_column_mock: Mock,
+        calculate_time_registered_for_mock: Mock,
+        cap_integer_at_max_value_mock: Mock,
+        expand_encode_and_extract_features_mock: Mock,
+        group_rural_urban_sparse_categories_mock: Mock,
+        lag_column_value_mock: Mock,
+        vectorise_dataframe_mock: Mock,
     ):
         read_from_parquet_mock.return_value = self.test_df
 
@@ -119,32 +74,15 @@ class NonResLocationsFeatureEngineeringTests(unittest.TestCase):
             ),
         ]
 
+        read_from_parquet_mock.assert_called_once_with(self.CLEANED_IND_CQC_TEST_DATA)
+        self.assertEqual(select_rows_with_non_null_value_mock.call_count, 1)
+        self.assertEqual(select_rows_with_value_mock.call_count, 1)
         write_to_parquet_mock.assert_has_calls(write_to_parquet_calls)
-
-
-# TODO amend tests
-# @patch("utils.utils.write_to_parquet")
-# @patch("utils.utils.read_from_parquet")
-# def test_main_is_filtering_out_rows_missing_data_for_features(
-#     self, read_from_parquet_mock: Mock, write_to_parquet_mock: Mock
-# ):
-#     read_from_parquet_mock.return_value = self.test_df
-
-#     job.main(
-#         self.CLEANED_IND_CQC_TEST_DATA,
-#         self.WITH_DORMANCY_DESTINATION,
-#         self.WITHOUT_DORMANCY_DESTINATION,
-#     )
-
-#     result_with_dormancy: DataFrame = write_to_parquet_mock.call_args_list[0][0][0]
-#     result_without_dormancy: DataFrame = write_to_parquet_mock.call_args_list[1][0][
-#         0
-#     ]
-
-#     self.assertEqual(self.test_df.count(), 10)
-#     self.assertEqual(result_with_dormancy.count(), 6)
-#     self.assertEqual(result_without_dormancy.count(), 7)
-
-
-if __name__ == "__main__":
-    unittest.main(warnings="ignore")
+        self.assertEqual(add_array_column_count_mock.call_count, 2)
+        self.assertEqual(add_date_index_column_mock.call_count, 2)
+        self.assertEqual(calculate_time_registered_for_mock.call_count, 1)
+        self.assertEqual(cap_integer_at_max_value_mock.call_count, 4)
+        self.assertEqual(expand_encode_and_extract_features_mock.call_count, 6)
+        self.assertEqual(group_rural_urban_sparse_categories_mock.call_count, 1)
+        self.assertEqual(lag_column_value_mock.call_count, 1)
+        self.assertEqual(vectorise_dataframe_mock.call_count, 2)
