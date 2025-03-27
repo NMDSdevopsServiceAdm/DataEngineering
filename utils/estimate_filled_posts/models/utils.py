592b5c638ebfb25daceea70aa3fd1021f9ac0b94
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-03-27 16:39:14 +0000

    generalise function

--- utils/estimate_filled_posts/models/utils.py
+++ utils/estimate_filled_posts/models/utils.py
@@ -34,21 +34,22 @@ def insert_predictions_into_pipeline(
     return locations_with_predictions
 
 
-def set_min_prediction_value(
-    df: DataFrame, min_prediction_value: float = 1.0
-) -> DataFrame:
+def set_min_value(df: DataFrame, col_name: str, min_value: float = 1.0) -> DataFrame:
     """
-    Sets the minimum value of the 'prediction' column to a specified value (default is 1.0).
+    Sets the minimum value of a specified column to a specified value (default is 1.0).
+
     Args:
-        df (DataFrame): A dataframe containing the 'prediction' column.
-        min_prediction_value (float): The minimum value allowed in the 'prediction' column.
+        df (DataFrame): A dataframe containing the specified column.
+        col_name (str): The name of the column to set the minimum value for.
+        min_value (float): The minimum value allowed in the specified column.
+
     Returns:
-        DataFrame: A dataframe with the 'prediction' column set to the minimum value.
+        DataFrame: A dataframe with the specified column set to the minimum value.
     """
     return df.withColumn(
-        IndCqc.prediction,
+        col_name,
         F.when(
-            F.col(IndCqc.prediction).isNotNull(),
-            F.greatest(F.col(IndCqc.prediction), F.lit(min_prediction_value)),
+            F.col(col_name).isNotNull(),
+            F.greatest(F.col(col_name), F.lit(min_value)),
         ).otherwise(F.lit(None)),
     )
