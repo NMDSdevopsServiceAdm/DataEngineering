96d027924f8e87da5ca950c24fd9689eb14ff1a0
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-03-28 09:21:54 +0000

    refactor and replace current function calls

--- utils/estimate_filled_posts_by_job_role_utils/models/interpolation.py
+++ utils/estimate_filled_posts_by_job_role_utils/models/interpolation.py
@@ -1,20 +1,17 @@
 from pyspark.sql import DataFrame, functions as F
+from typing import List
 
-from utils.column_names.ind_cqc_pipeline_columns import (
-    IndCqcColumns as IndCQC,
-)
+from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCQC
 from utils.estimate_filled_posts_by_job_role_utils.utils import (
-    unpack_mapped_column,
     create_map_column,
     pivot_interpolated_job_role_ratios,
     convert_map_with_all_null_values_to_null,
 )
-
 from utils.estimate_filled_posts.models.interpolation import model_interpolation
 
 
 def model_job_role_ratio_interpolation(
-    df: DataFrame,
+    df: DataFrame, column_list: List[str]
 ) -> DataFrame:
     """
     Performs interpolation on ascwds_job_role_ratio column
@@ -26,25 +23,10 @@ def model_job_role_ratio_interpolation(
         DataFrame: The DataFrame with the ascwds_job_role_ratio_interpolated column
 
     """
-
-    df_to_interpolate = unpack_mapped_column(df, IndCQC.ascwds_job_role_ratios)
-
-    df_keys = df_to_interpolate.select(
-        F.explode(F.map_keys(F.col(IndCQC.ascwds_job_role_ratios)))
-    ).distinct()
-    columns_to_interpolate = sorted([row[0] for row in df_keys.collect()])
-
-    df_to_interpolate = df_to_interpolate.withColumn(
-        IndCQC.ascwds_job_role_ratios_temporary,
-        create_map_column(columns_to_interpolate),
-    )
-
-    df_to_interpolate = df_to_interpolate.drop(*columns_to_interpolate)
-
-    df_to_interpolate = df_to_interpolate.select(
+    df_to_interpolate = df.select(
         IndCQC.location_id,
         IndCQC.unix_time,
-        F.explode(IndCQC.ascwds_job_role_ratios_temporary).alias(
+        F.explode(IndCQC.ascwds_job_role_ratios).alias(
             IndCQC.main_job_role_clean_labelled, IndCQC.ascwds_job_role_ratios_exploded
         ),
     )
@@ -67,12 +49,9 @@ def model_job_role_ratio_interpolation(
 
     df_to_interpolate = pivot_interpolated_job_role_ratios(df_to_interpolate)
 
-    df_to_interpolate = df_to_interpolate.withColumn(
-        IndCQC.ascwds_job_role_ratios_interpolated,
-        create_map_column(columns_to_interpolate),
+    df_to_interpolate = create_map_column(
+        df_to_interpolate, column_list, IndCQC.ascwds_job_role_ratios_interpolated
     )
-
-    df_to_interpolate = df_to_interpolate.drop(*columns_to_interpolate)
 
     df_joined = df.join(
         df_to_interpolate, on=[IndCQC.location_id, IndCQC.unix_time], how="inner"
