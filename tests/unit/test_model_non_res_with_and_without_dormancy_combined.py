31a4d43d5060e5528afedd59026ba393b7bb7223
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-04-08 12:50:25 +0100

    merge tests

--- tests/unit/test_model_non_res_with_and_without_dormancy_combined.py
+++ tests/unit/test_model_non_res_with_and_without_dormancy_combined.py
@@ -311,12 +311,12 @@ class ApplyResidualsTests(ModelNonResWithAndWithoutDormancyCombinedTests):
         super().setUp()
 
         test_df = self.spark.createDataFrame(
-            Data.apply_residuals_when_no_null_values_rows,
+            Data.apply_residuals_rows,
             Schemas.apply_residuals_schema,
         )
         self.returned_df = job.apply_residuals(test_df)
         self.expected_df = self.spark.createDataFrame(
-            Data.expected_apply_residuals_when_no_null_values_rows,
+            Data.expected_apply_residuals_rows,
             Schemas.expected_apply_residuals_schema,
         )
 
@@ -326,41 +326,13 @@ class ApplyResidualsTests(ModelNonResWithAndWithoutDormancyCombinedTests):
     def test_apply_residuals_returns_expected_columns(self):
         self.assertEqual(self.returned_df.columns, self.expected_df.columns)
 
-    def test_apply_residuals_returns_expected_values_when_no_nulls_present(self):
+    def test_apply_residuals_returns_expected_values(self):
         for i in range(len(self.returned_data)):
             self.assertAlmostEqual(
                 self.returned_data[i][
                     NRModel_TempCol.non_res_without_dormancy_model_adjusted_and_residual_applied
                 ],
                 self.expected_data[i][
-                    NRModel_TempCol.non_res_without_dormancy_model_adjusted_and_residual_applied
-                ],
-                places=3,
-                msg=f"Returned value for row {i} does not match expected",
-            )
-
-    def test_apply_residuals_returns_expected_values_when_data_contains_null_values(
-        self,
-    ):
-        test_df = self.spark.createDataFrame(
-            Data.apply_residuals_when_null_values_present_rows,
-            Schemas.apply_residuals_schema,
-        )
-        returned_df = job.apply_residuals(test_df)
-        expected_df = self.spark.createDataFrame(
-            Data.expected_apply_residuals_when_null_values_present_rows,
-            Schemas.expected_apply_residuals_schema,
-        )
-
-        returned_data = returned_df.sort(IndCqc.location_id).collect()
-        expected_data = expected_df.collect()
-
-        for i in range(len(self.returned_data)):
-            self.assertAlmostEqual(
-                returned_data[i][
-                    NRModel_TempCol.non_res_without_dormancy_model_adjusted_and_residual_applied
-                ],
-                expected_data[i][
                     NRModel_TempCol.non_res_without_dormancy_model_adjusted_and_residual_applied
                 ],
                 places=3,
