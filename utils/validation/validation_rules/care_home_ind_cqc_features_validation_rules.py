4a5d890a72baaa8fc12e1ecbcf079a4c734c2a06
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-04-09 10:38:32 +0100

    removed PIR stuff from validations, not used here

--- utils/validation/validation_rules/care_home_ind_cqc_features_validation_rules.py
+++ utils/validation/validation_rules/care_home_ind_cqc_features_validation_rules.py
@@ -27,13 +27,9 @@ class CareHomeIndCqcFeaturesValidationRules:
         ],
         RuleName.min_values: {
             IndCqcColumns.number_of_beds: 1,
-            IndCqcColumns.pir_people_directly_employed: 0,
-            IndCqcColumns.ascwds_pir_merged: 1.0,
         },
         RuleName.max_values: {
             IndCqcColumns.number_of_beds: 500,
-            IndCqcColumns.pir_people_directly_employed: 10000,
-            IndCqcColumns.ascwds_pir_merged: 3000.0,
         },
         RuleName.categorical_values_in_columns: {
             IndCqcColumns.care_home: CatValues.care_home_column_care_home_values.categorical_values,
