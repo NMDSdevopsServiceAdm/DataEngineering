f402b32a16a581b205d1f8d392418ac841024bfd
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-04-14 14:32:34 +0100

    change patches to new style

--- tests/unit/test_model_care_home.py
+++ tests/unit/test_model_care_home.py
@@ -9,6 +9,8 @@ from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCqc
 from tests.test_file_data import ModelCareHomes as Data
 from tests.test_file_schemas import ModelCareHomes as Schemas
 
+PATCH_PATH: str = "utils.estimate_filled_posts.models.care_homes"
+
 
 class TestModelCareHome(unittest.TestCase):
     CARE_HOME_MODEL = "tests/test_models/care_home_filled_posts_prediction/1.0.0/"
@@ -26,14 +28,10 @@ class TestModelCareHome(unittest.TestCase):
         warnings.filterwarnings("ignore", category=ResourceWarning)
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-    @patch(
-        "utils.estimate_filled_posts.models.care_homes.insert_predictions_into_pipeline"
-    )
-    @patch("utils.estimate_filled_posts.models.care_homes.save_model_metrics")
-    @patch("utils.estimate_filled_posts.models.care_homes.set_min_value")
-    @patch(
-        "utils.estimate_filled_posts.models.care_homes.calculate_filled_posts_from_beds_and_ratio"
-    )
+    @patch(f"{PATCH_PATH}.insert_predictions_into_pipeline")
+    @patch(f"{PATCH_PATH}.save_model_metrics")
+    @patch(f"{PATCH_PATH}.set_min_value")
+    @patch(f"{PATCH_PATH}.calculate_filled_posts_from_beds_and_ratio")
     def test_model_care_homes_runs(
         self,
         calculate_filled_posts_from_beds_and_ratio_mock: Mock,
@@ -53,7 +51,7 @@ class TestModelCareHome(unittest.TestCase):
         save_model_metrics_mock.assert_called_once()
         insert_predictions_into_pipeline_mock.assert_called_once()
 
-    @patch("utils.estimate_filled_posts.models.care_homes.save_model_metrics")
+    @patch(f"{PATCH_PATH}.save_model_metrics")
     def test_model_care_homes_returns_expected_data(
         self,
         save_model_metrics_mock: Mock,
@@ -75,5 +73,5 @@ class TestModelCareHome(unittest.TestCase):
             df[IndCqc.location_id] == "1-000000002"
         ).collect()[0]
 
-        self.assertIsNotNone(expected_location_with_prediction.care_home_model)
-        self.assertIsNone(expected_location_without_prediction.care_home_model)
+        self.assertIsNotNone(expected_location_with_prediction[IndCqc.care_home_model])
+        self.assertIsNone(expected_location_without_prediction[IndCqc.care_home_model])
