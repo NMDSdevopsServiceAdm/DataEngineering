e4428d32710b333566603e6b1fb9923b844a8c64
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-04-28 13:22:16 +0100

    train model

--- projects/_03_independent_cqc/_05_model/tests/jobs/test_train_linear_regression_model.py
+++ projects/_03_independent_cqc/_05_model/tests/jobs/test_train_linear_regression_model.py
@@ -2,19 +2,23 @@ import unittest
 import warnings
 from unittest.mock import ANY, Mock, patch
 
-import jobs.train_linear_regression_model as job
+import projects._03_independent_cqc._05_model.jobs.train_linear_regression_model as job
 from tests.test_file_data import TrainLinearRegressionModelData as Data
 from tests.test_file_schemas import TrainLinearRegressionModelSchema as Schemas
 from utils import utils
 from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCQC
 
-PATCH_PATH: str = "jobs.train_linear_regression_model"
+PATCH_PATH: str = (
+    "projects._03_independent_cqc._05_model.jobs.train_linear_regression_model"
+)
 
 
 class Main(unittest.TestCase):
-    features_source = "some/source"
-    model_source = "s3://pipeline-resources/models/prediction/1.0.0/"
-    metrics_destination = "some/destination"
+    branch_name = "test_branch"
+    care_home_model_name = "test_care_home_model"
+    non_res_model_name = "test_non_res_model"
+    model_version = "1.0.0"
+    model_run_number = 3
     dependent_variable_when_care_home = IndCQC.imputed_filled_posts_per_bed_ratio_model
     dependent_variable_when_not_care_home = IndCQC.imputed_filled_post_model
 
@@ -29,64 +33,89 @@ class Main(unittest.TestCase):
     @patch(f"{PATCH_PATH}.save_model_metrics")
     @patch(f"{PATCH_PATH}.mUtils.save_model_to_s3")
     @patch(f"{PATCH_PATH}.mUtils.train_lasso_regression_model")
+    @patch(f"{PATCH_PATH}.mUtils.create_test_and_train_datasets")
     @patch(f"{PATCH_PATH}.utils.select_rows_with_non_null_value")
     @patch(f"{PATCH_PATH}.utils.read_from_parquet")
+    @patch(f"{PATCH_PATH}.mUtils.generate_model_s3_path")
+    @patch(f"{PATCH_PATH}.mUtils.generate_features_s3_path")
     def test_main_when_care_home(
         self,
+        generate_features_s3_path_mock: Mock,
+        generate_model_s3_path_mock: Mock,
         read_from_parquet_mock: Mock,
         select_rows_with_non_null_value_mock: Mock,
+        create_test_and_train_datasets_mock: Mock,
         train_lasso_regression_model_mock: Mock,
         save_model_to_s3_mock: Mock,
         save_model_metrics_mock: Mock,
     ):
         read_from_parquet_mock.return_value = self.test_df
+        create_test_and_train_datasets_mock.return_value = (self.test_df, self.test_df)
+        save_model_to_s3_mock.return_value = self.model_run_number
 
         job.main(
-            self.features_source,
-            self.model_source,
-            self.metrics_destination,
-            is_care_home_model=True,
+            self.branch_name,
+            self.care_home_model_name,
+            self.model_version,
         )
 
+        generate_features_s3_path_mock.assert_called_once()
+        generate_model_s3_path_mock.assert_called_once()
         read_from_parquet_mock.assert_called_once()
         select_rows_with_non_null_value_mock.assert_called_once()
         train_lasso_regression_model_mock.assert_called_once()
         save_model_to_s3_mock.assert_called_once()
         save_model_metrics_mock.assert_called_once_with(
             ANY,
-            self.dependent_variable_when_care_home,
             ANY,
-            self.metrics_destination,
+            self.dependent_variable_when_care_home,
+            self.branch_name,
+            self.care_home_model_name,
+            self.model_version,
+            save_model_to_s3_mock.return_value,
         )
 
     @patch(f"{PATCH_PATH}.save_model_metrics")
     @patch(f"{PATCH_PATH}.mUtils.save_model_to_s3")
     @patch(f"{PATCH_PATH}.mUtils.train_lasso_regression_model")
+    @patch(f"{PATCH_PATH}.mUtils.create_test_and_train_datasets")
     @patch(f"{PATCH_PATH}.utils.select_rows_with_non_null_value")
     @patch(f"{PATCH_PATH}.utils.read_from_parquet")
+    @patch(f"{PATCH_PATH}.mUtils.generate_model_s3_path")
+    @patch(f"{PATCH_PATH}.mUtils.generate_features_s3_path")
     def test_main_when_not_care_home(
         self,
+        generate_features_s3_path_mock: Mock,
+        generate_model_s3_path_mock: Mock,
         read_from_parquet_mock: Mock,
         select_rows_with_non_null_value_mock: Mock,
+        create_test_and_train_datasets_mock: Mock,
         train_lasso_regression_model_mock: Mock,
         save_model_to_s3_mock: Mock,
         save_model_metrics_mock: Mock,
     ):
         read_from_parquet_mock.return_value = self.test_df
+        create_test_and_train_datasets_mock.return_value = (self.test_df, self.test_df)
+        save_model_to_s3_mock.return_value = self.model_run_number
 
         job.main(
-            self.features_source,
-            self.model_source,
-            self.metrics_destination,
+            self.branch_name,
+            self.non_res_model_name,
+            self.model_version,
         )
 
+        generate_features_s3_path_mock.assert_called_once()
+        generate_model_s3_path_mock.assert_called_once()
         read_from_parquet_mock.assert_called_once()
         select_rows_with_non_null_value_mock.assert_called_once()
         train_lasso_regression_model_mock.assert_called_once()
         save_model_to_s3_mock.assert_called_once()
         save_model_metrics_mock.assert_called_once_with(
             ANY,
-            self.dependent_variable_when_not_care_home,
             ANY,
-            self.metrics_destination,
+            self.dependent_variable_when_not_care_home,
+            self.branch_name,
+            self.non_res_model_name,
+            self.model_version,
+            save_model_to_s3_mock.return_value,
         )
