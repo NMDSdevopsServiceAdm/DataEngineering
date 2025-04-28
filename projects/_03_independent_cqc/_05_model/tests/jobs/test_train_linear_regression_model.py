98a5f5c132565219d406cc80867ca6b5528d87d0
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-04-28 15:38:19 +0100

    fix imports in train

--- projects/_03_independent_cqc/_05_model/tests/jobs/test_train_linear_regression_model.py
+++ projects/_03_independent_cqc/_05_model/tests/jobs/test_train_linear_regression_model.py
@@ -3,8 +3,12 @@ import warnings
 from unittest.mock import ANY, Mock, patch
 
 import projects._03_independent_cqc._05_model.jobs.train_linear_regression_model as job
-from tests.test_file_data import TrainLinearRegressionModelData as Data
-from tests.test_file_schemas import TrainLinearRegressionModelSchema as Schemas
+from projects._03_independent_cqc.unittest_data.ind_cqc_test_file_data import (
+    TrainLinearRegressionModelData as Data,
+)
+from projects._03_independent_cqc.unittest_data.ind_cqc_test_file_schemas import (
+    TrainLinearRegressionModelSchema as Schemas,
+)
 from utils import utils
 from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCQC
 
