918e634b13681cf04f0747bc05c5797860ded62b
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-04-29 15:28:40 +0100

    add empty row back in

--- projects/_03_independent_cqc/_05_model/tests/jobs/test_train_linear_regression_model.py
+++ projects/_03_independent_cqc/_05_model/tests/jobs/test_train_linear_regression_model.py
@@ -1,6 +1,7 @@
 import unittest
 import warnings
 from unittest.mock import ANY, Mock, patch
+
 import projects._03_independent_cqc._05_model.jobs.train_linear_regression_model as job
 from projects._03_independent_cqc.unittest_data.ind_cqc_test_file_data import (
     TrainLinearRegressionModelData as Data,
