127211cdf4057a2869c1e24dee99e2a1ac01876d
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-04-30 09:04:02 +0100

    rename new pir col

--- utils/column_names/ind_cqc_pipeline_columns.py
+++ utils/column_names/ind_cqc_pipeline_columns.py
@@ -232,12 +232,10 @@ class IndCqcColumns:
     person_given_name: str = CQCLClean.person_given_name
     person_roles: str = CQCLClean.person_roles
     person_title: str = CQCLClean.person_title
+    pir_filled_posts_model: str = "pir_filled_posts_model"
     pir_people_directly_employed: str = CQCPIRClean.pir_people_directly_employed
     pir_people_directly_employed_dedup: str = (
         CQCPIRClean.pir_people_directly_employed + "_deduplicated"
-    )
-    pir_people_directly_employed_filled_posts: str = (
-        pir_people_directly_employed + "_filled_posts"
     )
     postcode: str = CQCLClean.postal_code
     posts_rolling_average_model: str = "posts_rolling_average_model"
