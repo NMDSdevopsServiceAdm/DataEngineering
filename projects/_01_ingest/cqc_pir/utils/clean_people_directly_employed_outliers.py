8264970fc4629d4c9d2f3cdf09daecc209361d38
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-05-08 17:17:35 +0100

    add TODOs

--- projects/_01_ingest/cqc_pir/utils/clean_people_directly_employed_outliers.py
+++ projects/_01_ingest/cqc_pir/utils/clean_people_directly_employed_outliers.py
@@ -1,4 +1,4 @@
-from pyspark.sql import DataFrame, functions as F
+from pyspark.sql import DataFrame
 
 from utils.column_names.raw_data_files.cqc_pir_columns import CqcPirColumns as PIRCols
 from utils.column_names.cleaned_data_files.cqc_pir_cleaned import (
@@ -25,4 +25,9 @@ def clean_people_directly_employed_outliers(df: DataFrame) -> DataFrame:
         PIRCols.pir_people_directly_employed,
         PIRCleanCols.pir_people_directly_employed_cleaned,
     )
+
+    # TODO only include locations with 100+ posts if they have 2 submission (so we can validate them against each other)
+
+    # TODO remove locations with large differences (top 1% of (max-min)/avg?)
+
     return df
