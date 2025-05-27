4ce619f59866cf49a3739135efe3aa7c5420bf38
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-05-27 17:10:27 +0100

    block shows again

--- scripts/prevent_show_usage.sh
+++ scripts/prevent_show_usage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if git diff --cached --name-only | grep -E '\.py$' | xargs grep -nE '\.shw\(' ; then
+if git diff --cached --name-only | grep -E '\.py$' | xargs grep -nE '\.show\(' ; then
   echo "❌ ERROR: .show() detected in staged Python files."
   echo "Please remove or comment out Spark .show() calls before committing."
   exit 1
