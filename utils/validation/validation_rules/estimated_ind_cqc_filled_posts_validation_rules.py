4f8c3908fb25a443f4bb4a9bacfabe7ffcf2f13f
Roy Price <Roy.price1988@gmail.com>
2025-06-09 15:19:43 +0100

    Fixed validation script issue

--- utils/validation/validation_rules/estimated_ind_cqc_filled_posts_validation_rules.py
+++ utils/validation/validation_rules/estimated_ind_cqc_filled_posts_validation_rules.py
@@ -65,7 +65,7 @@ class EstimatedIndCqcFilledPostsValidationRules:
         RuleName.categorical_values_in_columns: {
             IndCqcColumns.care_home: CatValues.care_home_column_values.categorical_values,
             IndCqcColumns.primary_service_type: CatValues.primary_service_type_column_values.categorical_values,
-            IndCqcColumns.primary_service_type_second_level: CatValues.primary_service_type_second_level_column_values,
+            IndCqcColumns.primary_service_type_second_level: CatValues.primary_service_type_second_level_column_values.categorical_values,
             IndCqcColumns.current_cssr: CatValues.current_cssr_column_values.categorical_values,
             IndCqcColumns.current_region: CatValues.current_region_column_values.categorical_values,
             IndCqcColumns.ascwds_filled_posts_source: CatValues.ascwds_filled_posts_source_column_values.categorical_values,
@@ -74,7 +74,7 @@ class EstimatedIndCqcFilledPostsValidationRules:
         RuleName.distinct_values: {
             IndCqcColumns.care_home: CatValues.care_home_column_values.count_of_categorical_values,
             IndCqcColumns.primary_service_type: CatValues.primary_service_type_column_values.count_of_categorical_values,
-            IndCqcColumns.primary_service_type_second_level: CatValues.primary_service_type_second_level_column_values,
+            IndCqcColumns.primary_service_type_second_level: CatValues.primary_service_type_second_level_column_values.count_of_categorical_values,
             IndCqcColumns.current_cssr: CatValues.current_cssr_column_values.count_of_categorical_values,
             IndCqcColumns.current_region: CatValues.current_region_column_values.count_of_categorical_values,
             IndCqcColumns.ascwds_filled_posts_source: CatValues.ascwds_filled_posts_source_column_values.count_of_categorical_values,
