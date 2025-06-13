647e5a6a612b53bfb38ff60a6ea24188ccead823
Roy Price <Roy.price1988@gmail.com>
2025-06-13 15:14:50 +0100

    Made schema like a smart person

--- tests/test_file_schemas.py
+++ tests/test_file_schemas.py
@@ -718,18 +718,7 @@ class IndCQCDataUtils:
     )
     expected_allocate_primary_service_type_second_level_schema = StructType(
         [
-            StructField(CQCL.location_id, StringType(), True),
-            StructField(
-                CQCLClean.imputed_gac_service_types,
-                ArrayType(
-                    StructType(
-                        [
-                            StructField(CQCL.name, StringType(), True),
-                            StructField(CQCL.description, StringType(), True),
-                        ]
-                    )
-                ),
-            ),
+            *allocate_primary_service_type_second_level_schema,
             StructField(IndCQC.primary_service_type_second_level, StringType(), True),
         ]
     )
