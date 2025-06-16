a19cd31172fb8eeac9073448756c056f1f0dc0bd
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-06-16 09:31:33 +0100

    give job more ooomph

--- terraform/pipeline/glue.tf
+++ terraform/pipeline/glue.tf
@@ -899,12 +899,14 @@ module "diagnostics_on_known_filled_posts_job" {
 }
 
 module "diagnostics_on_capacity_tracker_job" {
-  source          = "../modules/glue-job"
-  script_dir      = "jobs"
-  script_name     = "diagnostics_on_capacity_tracker.py"
-  glue_role       = aws_iam_role.sfc_glue_service_iam_role
-  resource_bucket = module.pipeline_resources
-  datasets_bucket = module.datasets_bucket
+  source            = "../modules/glue-job"
+  script_dir        = "jobs"
+  script_name       = "diagnostics_on_capacity_tracker.py"
+  glue_role         = aws_iam_role.sfc_glue_service_iam_role
+  worker_type       = "G.1X"
+  number_of_workers = 4
+  resource_bucket   = module.pipeline_resources
+  datasets_bucket   = module.datasets_bucket
 
   job_parameters = {
     "--estimate_filled_posts_source"              = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=ind_cqc_estimated_filled_posts/"
