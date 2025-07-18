c6e53da267dfb35d917777d89b35373b4002ab3f
tedmonds-mt <tigs.edmonds@madetech.com>
2025-07-18 15:21:27 +0100

    added docker push and lambda function back in to test full deployment

--- terraform/pipeline/ecr.tf
+++ terraform/pipeline/ecr.tf
@@ -1,6 +1,6 @@
 # ECR Repository to store the Docker image
 resource "aws_ecr_repository" "create_dataset_snapshot" {
-  name                 = "${local.workspace_prefix}-create-snapshot-lambda-repo"
+  name                 = "create-snapshot-lambda-repo"
   image_tag_mutability = "MUTABLE"
   force_delete         = true
   image_scanning_configuration {
@@ -8,8 +8,8 @@ resource "aws_ecr_repository" "create_dataset_snapshot" {
   }
 }
 
-# # Get the latest image digest
-# data "aws_ecr_image" "create_dataset_snapshot" {
-#   repository_name = aws_ecr_repository.create_dataset_snapshot.name
-#   image_tag       = "latest"
-# }
+# Get the latest image digest
+data "aws_ecr_image" "create_dataset_snapshot" {
+  repository_name = aws_ecr_repository.create_dataset_snapshot.name
+  image_tag       = "latest"
+}
