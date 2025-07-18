70c3753247e592328987af3b3b1c658f619593fe
Roy Price <Roy.price1988@gmail.com>
2025-07-18 15:02:25 +0100

    Changed function name

--- projects/_03_independent_cqc/_01_merge/jobs/merge_ind_cqc_data.py
+++ projects/_03_independent_cqc/_01_merge/jobs/merge_ind_cqc_data.py
@@ -114,7 +114,7 @@ def main(
         selected_columns=cleaned_cqc_locations_columns_to_import,
     )
 
-    cqc_location_df = remove_june_2025_cqc(cqc_location_df)
+    cqc_location_df = remove_july_2025_cqc(cqc_location_df)
 
     ascwds_workplace_df = utils.read_from_parquet(
         cleaned_ascwds_workplace_source,
