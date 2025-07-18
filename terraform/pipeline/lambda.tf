e24ae9648ab59e3ace069e9175db0e3e18b39fc0
tedmonds-mt <tigs.edmonds@madetech.com>
2025-07-18 11:48:20 +0100

    Moving terraform testing over to new branch so that this branch focuses on one ticket

--- terraform/pipeline/lambda.tf
+++ terraform/pipeline/lambda.tf
@@ -4,12 +4,6 @@ data "archive_file" "error_notification_lambda" {
   output_path = "../../lambdas/error_notifications.zip"
 }
 
-# data "archive_file" "create_snapshot_lambda" {
-#   type        = "zip"
-#   source_dir  = "../../lambdas/create_dataset_snapshot"
-#   output_path = "../../lambdas/create_dataset_snapshot.zip"
-# }
-
 resource "aws_s3_object" "error_notification_lambda" {
   bucket      = module.pipeline_resources.bucket_name
   key         = "lambda-scripts/error_notifications.py"
@@ -17,14 +11,6 @@ resource "aws_s3_object" "error_notification_lambda" {
   acl         = "private"
   source_hash = data.archive_file.error_notification_lambda.output_base64sha256
 }
-
-# resource "aws_s3_object" "create_snapshot_lambda" {
-#   bucket      = module.pipeline_resources.bucket_name
-#   key         = "lambda-scripts/create_snapshot.zip"
-#   source      = data.archive_file.create_snapshot_lambda.output_path
-#   acl         = "private"
-#   source_hash = data.archive_file.error_notification_lambda.output_base64sha256
-# }
 
 resource "aws_lambda_function" "error_notification_lambda" {
   role             = aws_iam_role.error_notification_lambda.arn
@@ -42,32 +28,7 @@ resource "aws_lambda_function" "error_notification_lambda" {
   }
 }
 
-resource "aws_lambda_function" "create_snapshot_lambda" {
-  role          = aws_iam_role.create_snapshot_lambda.arn
-  handler       = "create_snapshot.main"
-  runtime       = "python3.11"
-  function_name = "${local.workspace_prefix}-create-full-snapshot"
-  package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.create_dataset_snapshot.repository_url}@${data.aws_ecr_image.create_dataset_snapshot.image_digest}"
-  memory_size   = 3072
-  timeout       = 60
-}
-
 data "aws_iam_policy_document" "error_notification_lambda_assume_role" {
-  statement {
-    actions = [
-      "sts:AssumeRole"
-    ]
-    principals {
-      identifiers = [
-        "lambda.amazonaws.com"
-      ]
-      type = "Service"
-    }
-  }
-}
-
-data "aws_iam_policy_document" "create_snapshot_lambda_assume_role" {
   statement {
     actions = [
       "sts:AssumeRole"
@@ -84,54 +45,6 @@ data "aws_iam_policy_document" "create_snapshot_lambda_assume_role" {
 resource "aws_iam_role" "error_notification_lambda" {
   name               = "${local.workspace_prefix}-error-notification-lambda"
   assume_role_policy = data.aws_iam_policy_document.error_notification_lambda_assume_role.json
-}
-
-resource "aws_iam_role" "create_snapshot_lambda" {
-  name               = "${local.workspace_prefix}-create-snapshot-lambda"
-  assume_role_policy = data.aws_iam_policy_document.create_snapshot_lambda_assume_role.json
-}
-
-data "aws_iam_policy_document" "create_snapshot_lambda" {
-  statement {
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-    ]
-    effect = "Allow"
-    resources = [
-      "arn:aws:logs:*:*:*"
-    ]
-  }
-
-  statement {
-    actions = [
-      "states:SendTaskSuccess",
-      "states:SendTaskFailure"
-    ]
-    effect = "Allow"
-    resources = [
-      "*"
-    ]
-  }
-
-  statement {
-    actions = [
-      "s3:Get*",
-      "s3:List*",
-      "s3:Describe*"
-    ]
-    effect    = "Allow"
-    resources = ["*"]
-  }
-
-  statement {
-    actions = [
-      "s3:Put*"
-    ]
-    effect    = "Allow"
-    resources = ["arn:aws:s3:::${local.workspace_prefix}-datasets/domain=CQC/dataset=providers_api/version=3.0.0/"]
-  }
 }
 
 data "aws_iam_policy_document" "error_notification_lambda" {
@@ -185,17 +98,8 @@ resource "aws_iam_policy" "error_notification_lambda" {
   policy = data.aws_iam_policy_document.error_notification_lambda.json
 }
 
-resource "aws_iam_policy" "create_snapshot_lambda" {
-  name   = "${local.workspace_prefix}-create-snapshot-lambda"
-  policy = data.aws_iam_policy_document.create_snapshot_lambda.json
-}
-
 resource "aws_iam_role_policy_attachment" "error_notification_lambda" {
   role       = aws_iam_role.error_notification_lambda.name
   policy_arn = aws_iam_policy.error_notification_lambda.arn
 }
 
-resource "aws_iam_role_policy_attachment" "create_snapshot_lambda" {
-  role       = aws_iam_role.create_snapshot_lambda.name
-  policy_arn = aws_iam_policy.create_snapshot_lambda.arn
-}
--- terraform/pipeline/outputs.tf
+++ terraform/pipeline/outputs.tf
@@ -5,19 +5,3 @@ output "pipeline_resources_bucket_name" {
 output "datasets_bucket_name" {
   value = module.datasets_bucket.bucket_name
 }
-
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
-
-output "caller_arn" {
-  value = data.aws_caller_identity.current.arn
-}
-
-output "caller_user" {
-  value = data.aws_caller_identity.current.user_id
-}
-
-output "image_digest" {
-  value = data.aws_ecr_image.create_dataset_snapshot.image_digest
-}
