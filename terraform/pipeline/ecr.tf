3080eb44ad3724aea3af7c77b92ed3a21722f365
tedmonds-mt <tigs.edmonds@madetech.com>
2025-07-21 10:01:12 +0100

    Removed all triggers from null resource and commented out test for failing fast purposes
    │ Error: Error in function call
    │
    │   on ecr.tf line 4, in resource "null_resource" "docker_image":
    │    4:     docker_file = filesha256("../../lambdas/create_dataset_snapshot/Dockerfile")
    │
    │ Call to function "filesha256" failed: open ../../lambdas/create_dataset_snapshot/Dockerfile: no such file or directory.

--- terraform/pipeline/ecr.tf
+++ terraform/pipeline/ecr.tf
@@ -1,8 +1,8 @@
 # Build and push the Docker image
 resource "null_resource" "docker_image" {
   triggers = {
-    docker_file = filesha256("../../lambdas/create_dataset_snapshot/Dockerfile")
-    source_dir  = sha256(join("", [for f in fileset("../../lambdas/create_dataset_snapshot", "**") : filesha256("../../lambdas/create_dataset_snapshot/${f}")]))
+    # docker_file = filesha256("../../lambdas/create_dataset_snapshot/Dockerfile")
+    # source_dir  = sha256(join("", [for f in fileset("../../lambdas/create_dataset_snapshot", "**") : filesha256("../../lambdas/create_dataset_snapshot/${f}")]))
   }
   provisioner "local-exec" {
     command = <<EOF
