d39383e6143a127f7852e3f306bee8520969af40
tedmonds-mt <tigs.edmonds@madetech.com>
2025-07-21 09:45:55 +0100

    Added null resource so that terraform can locate it before we delete it
    │ Error: failed to read provider configuration schema for registry.terraform.io/hashicorp/null: failed to instantiate provider "registry.terraform.io/hashicorp/null" to obtain schema: unavailable provider "registry.terraform.io/hashicorp/null"
    │
    │
    ╵

---
+++ terraform/pipeline/ecr.tf
@@ -0,0 +1,12 @@
+# Build and push the Docker image
+resource "null_resource" "docker_image" {
+  triggers = {
+    docker_file = filesha256("../../lambdas/create_dataset_snapshot/Dockerfile")
+    source_dir  = sha256(join("", [for f in fileset("../../lambdas/create_dataset_snapshot", "**") : filesha256("../../lambdas/create_dataset_snapshot/${f}")]))
+  }
+  provisioner "local-exec" {
+    command = <<EOF
+      pwd
+    EOF
+  }
+}
