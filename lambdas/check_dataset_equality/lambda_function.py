6872edeb517d5fe920e16acf13a4931e6f5d6e5b
tedmonds-mt <tigs.edmonds@madetech.com>
2025-07-23 16:12:28 +0100

    Debug: added drop columns back in because historic/deregistered items will have faulty mainPhoneNumber

--- lambdas/check_dataset_equality/lambda_function.py
+++ lambdas/check_dataset_equality/lambda_function.py
@@ -14,16 +14,16 @@ def lambda_handler(event, context):
     drop_cols = event["drop_cols"].split(",")
 
     pl_test.assert_frame_equal(
-        left_df.filter(pl.col("deregistrationDate").is_null()),
-        right_df.filter(pl.col("deregistrationDate").is_null()),
+        left_df.filter(pl.col("deregistrationDate").is_null()).drop(drop_cols),
+        right_df.filter(pl.col("deregistrationDate").is_null()).drop(drop_cols),
         check_row_order=False,
     )
 
     logger.info("The provided datasets are equal, excluding deregistered items")
 
     pl_test.assert_frame_equal(
-        left_df,
-        right_df,
+        left_df.drop(drop_cols),
+        right_df.drop(drop_cols),
         check_row_order=False,
     )
 
