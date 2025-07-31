5c5dd409565fd8b63146fa9b50a293fd316cfb95
mulvj-mt <joe.mulvey@madetech.com>
2025-07-31 15:20:27 +0100

    [869] Fix Terraform prefix length

--- terraform/modules/fargate-task/locals.tf
+++ terraform/modules/fargate-task/locals.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 locals {
-  workspace_prefix = substr(lower(replace(terraform.workspace, "/[^a-zA-Z0-9]+/", "-")), 0, 30)
-  resource_prefix  = "${local.workspace_prefix}-${var.task_name}"
+  workspace_prefix = substr(lower(replace(terraform.workspace, "/[^a-zA-Z0-9]+/", "-")), 0, 20)
+  resource_prefix  = substr("${local.workspace_prefix}-${var.task_name}", 0, 30)
   account_id       = data.aws_caller_identity.current.account_id
 }
