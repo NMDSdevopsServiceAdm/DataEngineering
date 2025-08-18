e809101b20e73ce1e033bccacfd6f94e4f5b3c2f
tedmonds-mt <tigs.edmonds@madetech.com>
2025-08-18 14:20:52 +0100

    [933] Fix: Step functions need permission to access delta ingestion jobs now

--- terraform/pipeline/step-function.tf
+++ terraform/pipeline/step-function.tf
@@ -557,8 +557,8 @@ resource "aws_iam_policy" "step_function_iam_policy" {
           "glue:GetJobRuns"
         ],
         "Resource" : [
-          "arn:aws:glue:eu-west-2:344210435447:job/main-bulk_download_cqc_providers_job",
-          "arn:aws:glue:eu-west-2:344210435447:job/main-bulk_download_cqc_locations_job"
+          module.delta_cqc_locations_download_job.job_arn,
+          module.delta_cqc_providers_download_job.job_arn
         ]
       },
       {
