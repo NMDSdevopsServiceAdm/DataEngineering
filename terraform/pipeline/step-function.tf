f0f1587ad3c0d8a5a02c18d0af43cfe22521de6c
tedmonds-mt <tigs.edmonds@madetech.com>
2025-08-18 09:46:03 +0100

    [933] Refactor: repoint delta "last run" to the delta glue jobs

--- terraform/pipeline/step-function.tf
+++ terraform/pipeline/step-function.tf
@@ -165,9 +165,7 @@ resource "aws_sfn_state_machine" "cqc_api_pipeline_state_machine" {
   definition = templatefile("step-functions/CQC-API-Pipeline.json", {
     dataset_bucket_uri                             = module.datasets_bucket.bucket_uri
     dataset_bucket_name                            = module.datasets_bucket.bucket_name
-    bulk_cqc_providers_download_job_name           = "main-bulk_download_cqc_providers_job" # TODO: remove and point to delta
     delta_cqc_providers_download_job_name          = module.delta_cqc_providers_download_job.job_name
-    bulk_cqc_locations_download_job_name           = "main-bulk_download_cqc_locations_job" # TODO: remove and point to delta
     delta_cqc_locations_download_job_name          = module.delta_cqc_locations_download_job.job_name
     validate_providers_api_raw_delta_data_job_name = module.validate_providers_api_raw_delta_data_job.job_name
     validate_locations_api_raw_delta_data_job_name = module.validate_locations_api_raw_delta_data_job.job_name
