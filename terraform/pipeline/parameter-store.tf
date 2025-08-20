394b0804e20c43322bb41961971d3171ad9e4295
tedmonds-mt <tigs.edmonds@madetech.com>
2025-08-20 09:34:48 +0100

    [958] Fix: Terraform linting

--- terraform/pipeline/parameter-store.tf
+++ terraform/pipeline/parameter-store.tf
@@ -1,13 +1,13 @@
 resource "aws_ssm_parameter" "providers_last_run" {
-  name  = "${local.workspace_prefix}/providers-last-run"
-  type  = "String"
-  value = timeadd(timestamp(), "-24h")
+  name      = "${local.workspace_prefix}/providers-last-run"
+  type      = "String"
+  value     = timeadd(timestamp(), "-24h")
   overwrite = false
 }
 
 resource "aws_ssm_parameter" "locations_last_run" {
-  name  = "${local.workspace_prefix}/locations-last-run"
-  type  = "String"
-  value = timeadd(timestamp(), "-24h")
+  name      = "${local.workspace_prefix}/locations-last-run"
+  type      = "String"
+  value     = timeadd(timestamp(), "-24h")
   overwrite = false
 }
