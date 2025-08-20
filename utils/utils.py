2c66bc9345a7d24fc4054269d3b0a7796f4ad9c1
AchyutRatkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2025-08-20 10:09:54 +0100

    updated docstrings

--- utils/utils.py
+++ utils/utils.py
@@ -102,7 +102,7 @@ def read_from_parquet(
     Args:
         data_source (str): Path to the Parquet file.
         selected_columns (List[str]): Optional - List of column names to select. Defaults to None (all columns).
-        schema (StructType): Optional - schema to use when reading parquet. Defaults to None.
+        schema (Optional[StructType]): Optional - schema to use when reading parquet. Defaults to None.
 
     Returns:
         DataFrame: A dataframe of the data in the parquet file, with all or selected columns.
