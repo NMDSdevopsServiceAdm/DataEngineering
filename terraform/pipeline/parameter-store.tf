d2bbf1aba03ee361322975ed6126f2ccd5777d34
tedmonds-mt <tigs.edmonds@madetech.com>
2025-08-20 12:34:10 +0100

    [958] Refactor: Lifecycle policy should prevent overwriting the value after creation

--- terraform/pipeline/parameter-store.tf
+++ terraform/pipeline/parameter-store.tf
@@ -1,13 +1,17 @@
 resource "aws_ssm_parameter" "providers_last_run" {
-  name      = "/${local.workspace_prefix}/providers-last-run"
-  type      = "String"
-  value     = timeadd(timestamp(), "-24h")
-  overwrite = false
+  name  = "/${local.workspace_prefix}/providers-last-run"
+  type  = "String"
+  value = timeadd(timestamp(), "-24h")
+  lifecycle {
+    ignore_changes = [value, ]
+  }
 }
 
 resource "aws_ssm_parameter" "locations_last_run" {
-  name      = "/${local.workspace_prefix}/locations-last-run"
-  type      = "String"
-  value     = timeadd(timestamp(), "-24h")
-  overwrite = false
+  name  = "/${local.workspace_prefix}/locations-last-run"
+  type  = "String"
+  value = timeadd(timestamp(), "-24h")
+  lifecycle {
+    ignore_changes = [value, ]
+  }
 }
