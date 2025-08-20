d9693cb5fdbc36ef77c0382c013515255f08583d
tedmonds-mt <tigs.edmonds@madetech.com>
2025-08-20 17:09:05 +0100

    [958] Refactor: add cqc prefix to parameter names

--- terraform/pipeline/parameter-store.tf
+++ terraform/pipeline/parameter-store.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "providers_last_run" {
-  name  = "/${local.workspace_prefix}/providers-last-run"
+  name  = "/${local.workspace_prefix}/cqc-providers-last-run"
   type  = "String"
   value = timeadd(timestamp(), "-24h")
   lifecycle {
@@ -8,7 +8,7 @@ resource "aws_ssm_parameter" "providers_last_run" {
 }
 
 resource "aws_ssm_parameter" "locations_last_run" {
-  name  = "/${local.workspace_prefix}/locations-last-run"
+  name  = "/${local.workspace_prefix}/cqc-locations-last-run"
   type  = "String"
   value = timeadd(timestamp(), "-24h")
   lifecycle {
