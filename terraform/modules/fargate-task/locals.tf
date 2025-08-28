a08ac1a2167e9a6aae1f8050026962227e14eff2
AchyutRatkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2025-08-28 15:20:40 +0100

    changing fargate-task prefix length to 23 for testing

--- terraform/modules/fargate-task/locals.tf
+++ terraform/modules/fargate-task/locals.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 locals {
-  workspace_prefix = substr(lower(replace(terraform.workspace, "/[^a-zA-Z0-9]+/", "-")), 0, 20)
+  workspace_prefix = substr(lower(replace(terraform.workspace, "/[^a-zA-Z0-9]+/", "-")), 0, 23)
   account_id       = data.aws_caller_identity.current.account_id
 }
