6a2c33ffd999987c974481b57c2cf7be9cd6509f
tedmonds-mt <tigs.edmonds@madetech.com>
2025-08-29 11:15:49 +0100

    [953] Refactor: Modify new step functions in line with the changes on this branch

--- terraform/pipeline/step-function.tf
+++ terraform/pipeline/step-function.tf
@@ -224,6 +224,7 @@ resource "aws_sfn_state_machine" "transform_cqc_data_state_machine" {
   definition = templatefile("step-functions/TransformCQCData-StepFunction.json", {
     dataset_bucket_uri                           = module.datasets_bucket.bucket_uri
     dataset_bucket_name                          = module.datasets_bucket.bucket_name
+    create_snapshot_lambda_lambda_arn            = aws_lambda_function.create_snapshot_lambda.arn
     clean_cqc_provider_data_job_name             = module.clean_cqc_provider_data_job.job_name
     clean_cqc_location_data_job_name             = module.delta_clean_cqc_location_data_job.job_name
     validate_locations_api_cleaned_data_job_name = module.validate_delta_locations_api_cleaned_data_job.job_name
@@ -245,6 +246,34 @@ resource "aws_sfn_state_machine" "transform_cqc_data_state_machine" {
   ]
 }
 
+resource "aws_sfn_state_machine" "transform_cqc_data_state_machine" {
+  name     = "${local.workspace_prefix}-Legacy-Transform-CQC-Data"
+  role_arn = aws_iam_role.step_function_iam_role.arn
+  type     = "STANDARD"
+  definition = templatefile("step-functions/LegacyTransformCQCData-StepFunction.json", {
+    dataset_bucket_uri                           = module.datasets_bucket.bucket_uri
+    dataset_bucket_name                          = module.datasets_bucket.bucket_name
+    create_snapshot_lambda_lambda_arn            = aws_lambda_function.create_snapshot_lambda.arn
+    clean_cqc_provider_data_job_name             = module.clean_cqc_provider_data_job.job_name
+    clean_cqc_location_data_job_name             = module.delta_clean_cqc_location_data_job.job_name
+    validate_locations_api_cleaned_data_job_name = module.validate_delta_locations_api_cleaned_data_job.job_name
+    validate_providers_api_cleaned_data_job_name = module.validate_providers_api_cleaned_data_job.job_name
+    cqc_crawler_name                             = module.cqc_crawler_delta.crawler_name # TODO: point back to main crawler
+    data_validation_reports_crawler_name         = module.data_validation_reports_crawler.crawler_name
+    pipeline_failure_lambda_function_arn         = aws_lambda_function.error_notification_lambda.arn
+  })
+
+  logging_configuration {
+    log_destination        = "${aws_cloudwatch_log_group.state_machines.arn}:*"
+    include_execution_data = true
+    level                  = "ERROR"
+  }
+
+  depends_on = [
+    aws_iam_policy.step_function_iam_policy,
+    module.datasets_bucket
+  ]
+}
 
 resource "aws_sfn_state_machine" "direct_payments_state_machine" {
   name     = "${local.workspace_prefix}-Direct-Payment-Recipients"
