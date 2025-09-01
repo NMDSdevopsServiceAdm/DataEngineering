2bab6d3d1c6249db7c4ee4b5f7eb37469a4fde1f
Micah Dougall <micah.dougall@madetech.com>
2025-09-01 12:06:06 +0100

    [969] linting

--- projects/_03_independent_cqc/_06_estimate_filled_posts/tests/utils/models/test_model_primary_service_rate_of_change.py
+++ projects/_03_independent_cqc/_06_estimate_filled_posts/tests/utils/models/test_model_primary_service_rate_of_change.py
@@ -12,7 +12,9 @@ from projects._03_independent_cqc.unittest_data.ind_cqc_test_file_schemas import
 from utils import utils
 from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCqc
 
-PATCH_PATH: str = "projects._03_independent_cqc._06_estimate_filled_posts.utils.models.primary_service_rate_of_change"
+PATCH_PATH: str = (
+    "projects._03_independent_cqc._06_estimate_filled_posts.utils.models.primary_service_rate_of_change"
+)
 
 
 class ModelPrimaryServiceRateOfChangeTests(unittest.TestCase):
