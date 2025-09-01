786c16477ea108d931b2ba0909ccd7948cc989f5
Micah Dougall <micah.dougall@madetech.com>
2025-09-01 14:52:27 +0100

    [969] linting

--- projects/_03_independent_cqc/_04_feature_engineering/tests/jobs/test_validate_features_non_res_ascwds_with_dormancy_ind_cqc.py
+++ projects/_03_independent_cqc/_04_feature_engineering/tests/jobs/test_validate_features_non_res_ascwds_with_dormancy_ind_cqc.py
@@ -10,7 +10,9 @@ from projects._03_independent_cqc.unittest_data.ind_cqc_test_file_schemas import
 )
 from utils import utils
 
-PATCH_PATH: str = "projects._03_independent_cqc._04_feature_engineering.jobs.validate_features_non_res_ascwds_with_dormancy_ind_cqc_data"
+PATCH_PATH: str = (
+    "projects._03_independent_cqc._04_feature_engineering.jobs.validate_features_non_res_ascwds_with_dormancy_ind_cqc_data"
+)
 
 
 class ValidateFeaturesNonResASCWDSWithDormancyIndCqcDatasetTests(unittest.TestCase):
