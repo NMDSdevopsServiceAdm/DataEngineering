2cfa904341f0e4b370a8facc81e19ac9c053895e
mulvj-mt <joe.mulvey@madetech.com>
2025-09-05 14:37:17 +0100

    [967] Add terraformed SNS topic for model training notifications.

--- terraform/pipeline/sns.tf
+++ terraform/pipeline/sns.tf
@@ -1,3 +1,7 @@
 resource "aws_sns_topic" "pipeline_failures" {
   name = "${local.workspace_prefix}-pipeline-failures"
 }
+
+resource "aws_sns_topic" "model_retrain" {
+  name = "${local.workspace_prefix}-model-retrain"
+}
