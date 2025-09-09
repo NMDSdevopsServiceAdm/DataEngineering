f7771bf982390b2ec6f684f9131a06d684ad1845
tedmonds-mt <tigs.edmonds@madetech.com>
2025-09-09 16:11:26 +0100

    [953] Fix: dimension date parsing

--- projects/_01_ingest/cqc_api/jobs/delta_clean_cqc_location_data.py
+++ projects/_01_ingest/cqc_api/jobs/delta_clean_cqc_location_data.py
@@ -266,8 +266,8 @@ def create_postcode_matching_dimension(
         )
         .withColumn(DimensionKeys.last_updated, F.lit(dimension_update_date))
         .withColumn(DimensionKeys.year, F.lit(dimension_update_date[:4]))
-        .withColumn(DimensionKeys.month, F.lit(dimension_update_date[5:7]))
-        .withColumn(DimensionKeys.day, F.lit(dimension_update_date[8:10]))
+        .withColumn(DimensionKeys.month, F.lit(dimension_update_date[4:6]))
+        .withColumn(DimensionKeys.day, F.lit(dimension_update_date[6:]))
     )
 
     missing_dim_columns = list(set(previous_dimension.columns) - set(delta.columns))
@@ -322,8 +322,8 @@ def create_dimension_from_missing_struct_column(
         )
         .withColumn(DimensionKeys.last_updated, F.lit(dimension_update_date))
         .withColumn(DimensionKeys.year, F.lit(dimension_update_date[:4]))
-        .withColumn(DimensionKeys.month, F.lit(dimension_update_date[5:7]))
-        .withColumn(DimensionKeys.day, F.lit(dimension_update_date[8:10]))
+        .withColumn(DimensionKeys.month, F.lit(dimension_update_date[4:6]))
+        .withColumn(DimensionKeys.day, F.lit(dimension_update_date[6:]))
     )
 
     missing_dim_columns = list(set(previous_dimension.columns) - set(delta.columns))
