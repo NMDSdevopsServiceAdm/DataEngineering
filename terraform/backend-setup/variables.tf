5be0ebc7b00b110d8c46c49b1776aa13774f8823
Toby Petty <tobypetty1@gmail.com>
2025-09-16 13:01:38 +0100

    Add default values for aws auth parameters
    to allow env vars from aws-vault to be detected without breakign existing auth

--- terraform/backend-setup/variables.tf
+++ terraform/backend-setup/variables.tf
@@ -1,11 +1,13 @@
 variable "aws_access_key" {
   description = "Access key for AWS - find this in IAM"
   type        = string
+  default     = ""
   sensitive   = true
 }
 variable "aws_secret_key" {
   description = "Secret key for AWS - displayed when IAM user is created"
   type        = string
+  default     = ""
   sensitive   = true
 }
 
