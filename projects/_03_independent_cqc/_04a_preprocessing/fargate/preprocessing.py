5e2e6621ccd687fcce252cf3f0148828bed36850
mulvj-mt <joe.mulvey@madetech.com>
2025-09-16 09:05:06 +0100

    [976] Preprocessing initial commit

---
+++ projects/_03_independent_cqc/_04a_preprocessing/fargate/preprocessing.py
@@ -0,0 +1,15 @@
+import polars as pl
+
+
+class NonResPIRPreprocessor:
+    def __init__(self):
+        self.df = None
+
+    def read_data(self, data_source: str):
+        pass
+
+    def preprocess(self):
+        pass
+
+    def save(self, data_destination: str):
+        pass
---
+++ projects/_03_independent_cqc/_04a_preprocessing/tests/test_preprocessing.py
@@ -0,0 +1,30 @@
+from projects._03_independent_cqc._04a_preprocessing.fargate.preprocessing import (
+    NonResPIRPreprocessor,
+)
+import unittest
+
+
+class TestPreprocessNonResPir(unittest.TestCase):
+    def test_preprocess_non_res_pir_reads_data(self):
+        pass
+
+    def test_non_res_pir_saves_to_parquet(self):
+        pass
+
+    def test_preprocess_non_res_pir_returns_correct_columns(self):
+        pass
+
+    def test_preprocess_non_res_pir_eliminates_nulls(self):
+        pass
+
+    def test_preprocess_non_res_pir_eliminates_negatives_or_zeros(self):
+        pass
+
+    def test_preprocess_non_res_pir_eliminates_small_residuals(self):
+        pass
+
+    def test_preprocess_non_res_pir_logs_failure(self):
+        pass
+
+    def test_preprocess_non_res_pir_logs_success(self):
+        pass
