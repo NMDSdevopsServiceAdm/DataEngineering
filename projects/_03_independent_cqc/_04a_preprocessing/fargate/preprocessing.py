a709e297072f92241cbe92624cc954216f5800b7
mulvj-mt <joe.mulvey@madetech.com>
2025-09-17 08:31:48 +0100

    [976] Add LazyFrame sink to preprocessor

--- projects/_03_independent_cqc/_04a_preprocessing/fargate/preprocessing.py
+++ projects/_03_independent_cqc/_04a_preprocessing/fargate/preprocessing.py
@@ -1,7 +1,5 @@
 import polars as pl
 
-from polars_utils.utils import write_to_parquet
-
 
 def preprocess_non_res_pir(path_to_data: str, destination: str, lazy=False) -> None:
     data = pl.scan_parquet(path_to_data) if lazy else pl.read_parquet(path_to_data)
@@ -12,24 +10,28 @@ def preprocess_non_res_pir(path_to_data: str, destination: str, lazy=False) -> N
         "ascwds_filled_posts_deduplicated_clean",
         "pir_people_directly_employed_deduplicated",
     ]
-    data.select(*required_columns).filter(
-        (
-            (pl.col("ascwds_filled_posts_deduplicated_clean").is_not_null())
-            & (pl.col("pir_people_directly_employed_deduplicated").is_not_null())
-            & (pl.col("ascwds_filled_posts_deduplicated_clean") > 0)
-            & (pl.col("pir_people_directly_employed_deduplicated") > 0)
+    result = (
+        data.select(*required_columns)
+        .filter(
+            (
+                (pl.col("ascwds_filled_posts_deduplicated_clean").is_not_null())
+                & (pl.col("pir_people_directly_employed_deduplicated").is_not_null())
+                & (pl.col("ascwds_filled_posts_deduplicated_clean") > 0)
+                & (pl.col("pir_people_directly_employed_deduplicated") > 0)
+            )
         )
-    ).with_columns(
-        (
-            pl.col("ascwds_filled_posts_deduplicated_clean")
-            - pl.col("pir_people_directly_employed_deduplicated")
+        .with_columns(
+            (
+                pl.col("ascwds_filled_posts_deduplicated_clean")
+                - pl.col("pir_people_directly_employed_deduplicated")
+            )
+            .abs()
+            .alias("abs_resid"),
         )
-        .abs()
-        .alias("abs_resid"),
-    ).filter(
-        pl.col("abs_resid") <= 500
-    ).drop(
-        "abs_resid"
-    ).write_parquet(
-        destination
+        .filter(pl.col("abs_resid") <= 500)
+        .drop("abs_resid")
     )
+    if lazy:
+        result.sink_parquet(destination)
+    else:
+        result.write_parquet(destination)
--- projects/_03_independent_cqc/_04a_preprocessing/tests/test_preprocessing.py
+++ projects/_03_independent_cqc/_04a_preprocessing/tests/test_preprocessing.py
@@ -10,22 +10,25 @@ import polars as pl
 from unittest.mock import MagicMock, patch
 import shutil
 import tempfile
+from pathlib import Path
 
 
 PATCH_STEM = "projects._03_independent_cqc._04a_preprocessing.fargate.preprocessing"
 DUMMY_SOURCE_BUCKET = "dummy-source-bucket"
 DUMMY_DESTINATION_BUCKET = "dummy-destination-bucket"
+SAMPLE_DATA_PATH = Path(__file__).parent / "testfile.parquet"
 
 
 class TestPreprocessNonResPir(unittest.TestCase):
-    df_test = pl.read_parquet(
-        "projects/_03_independent_cqc/_04a_preprocessing/tests/testfile.parquet"
-    )
+    df_test = pl.read_parquet(SAMPLE_DATA_PATH)
     s3_uri = "s3://test_bucket/test_file.parquet"
 
     def setUp(self):
         self.temp_dir = tempfile.mkdtemp()
         self.destination = os.path.join(self.temp_dir, "test_dest.parquet")
+        with patch(f"{PATCH_STEM}.pl.read_parquet") as mock_read_parquet:
+            mock_read_parquet.return_value = self.df_test
+            preprocess_non_res_pir(self.s3_uri, self.destination, lazy=False)
 
     def tearDown(self):
         shutil.rmtree(self.temp_dir)
@@ -41,9 +44,6 @@ class TestPreprocessNonResPir(unittest.TestCase):
         mock_scan_parquet.assert_called_once_with(self.s3_uri)
 
     def test_preprocess_non_res_pir_returns_correct_columns_from_read(self):
-        with patch(f"{PATCH_STEM}.pl.read_parquet") as mock_read_parquet:
-            mock_read_parquet.return_value = self.df_test
-            preprocess_non_res_pir(self.s3_uri, self.destination, lazy=False)
         df = pl.read_parquet(self.destination)
         expected_columns = [
             "locationId",
@@ -55,9 +55,6 @@ class TestPreprocessNonResPir(unittest.TestCase):
         self.assertListEqual(df.columns, expected_columns)
 
     def test_preprocess_non_res_pir_eliminates_nulls(self):
-        with patch(f"{PATCH_STEM}.pl.read_parquet") as mock_read_parquet:
-            mock_read_parquet.return_value = self.df_test
-            preprocess_non_res_pir(self.s3_uri, self.destination, lazy=False)
         df = pl.read_parquet(self.destination)
         self.assertEqual(
             df.filter(pl.col("ascwds_filled_posts_deduplicated_clean").is_null()).shape[
@@ -73,9 +70,6 @@ class TestPreprocessNonResPir(unittest.TestCase):
         )
 
     def test_preprocess_non_res_pir_eliminates_negatives_or_zeros(self):
-        with patch(f"{PATCH_STEM}.pl.read_parquet") as mock_read_parquet:
-            mock_read_parquet.return_value = self.df_test
-            preprocess_non_res_pir(self.s3_uri, self.destination, lazy=False)
         df = pl.read_parquet(self.destination)
         self.assertEqual(
             df.filter(pl.col("ascwds_filled_posts_deduplicated_clean") <= 0).shape[0],
@@ -89,9 +83,6 @@ class TestPreprocessNonResPir(unittest.TestCase):
         )
 
     def test_preprocess_non_res_pir_eliminates_small_residuals(self):
-        with patch(f"{PATCH_STEM}.pl.read_parquet") as mock_read_parquet:
-            mock_read_parquet.return_value = self.df_test
-            preprocess_non_res_pir(self.s3_uri, self.destination, lazy=False)
         df = pl.read_parquet(self.destination).with_columns(
             (
                 pl.col("ascwds_filled_posts_deduplicated_clean")
@@ -102,6 +93,12 @@ class TestPreprocessNonResPir(unittest.TestCase):
         )
         self.assertEqual(df.filter(pl.col("abs_resid") > 500).shape[0], 0)
 
+    def test_preprocess_works_with_lazy_frames(self):
+        preprocess_non_res_pir(str(SAMPLE_DATA_PATH), self.destination, lazy=True)
+        df = pl.read_parquet(self.destination)
+        self.assertEqual(df.shape[0], 3)
+        ids = set(df["locationId"].to_list())
+        self.assertEqual({"1-119187505", "1-2206520209", "1-118618710"}, ids)
 
     # @patch(f"{PATCH_STEM}.pl.read_parquet")
     # def test_preprocess_non_res_pir_logs_failure(self, mock_read_parquet):
