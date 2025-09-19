3f25e42ebe394d7d2c34cc4dc1ca3364655d334b
mulvj-mt <joe.mulvey@madetech.com>
2025-09-19 19:06:03 +0100

    [976] Fix incorrect data source prefix

--- projects/_03_independent_cqc/_05_model/fargate/model_registry.py
+++ projects/_03_independent_cqc/_05_model/fargate/model_registry.py
@@ -9,7 +9,7 @@ model_definitions = {
         "model_identifier": "non_res_pir",
         "model_params": dict(),
         "version_parameter_location": f"/models/{ENVIRONMENT}/non_res_pir",
-        "data_source_prefix": "domain=ind_cqc_filled_posts/dataset=ind_cqc_estimated_missing_ascwds_filled_posts/",
+        "data_source_prefix": "domain=ind_cqc_filled_posts/dataset=ind_cqc_estimated_missing_ascwds_filled_posts_processed",
         "target_columns": ["ascwds_filled_posts_deduplicated_clean"],
         "feature_columns": ["pir_people_directly_employed_deduplicated"],
     }
