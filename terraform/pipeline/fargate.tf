3a628f6ffd0aa61d42a115cf85049572047b8c82
mulvj-mt <joe.mulvey@madetech.com>
2025-09-23 10:26:17 +0100

    [976] Minor refactor to include required variables in module

--- terraform/pipeline/fargate.tf
+++ terraform/pipeline/fargate.tf
@@ -66,4 +66,8 @@ module "_03_independent_cqc" {
   task_name     = "_03_independent_cqc"
   ecr_repo_name = "fargate/03_independent_cqc"
   cluster_arn   = aws_ecs_cluster.polars_cluster.arn
+  environment = [
+    { "name" : "AWS_REGION", "value" : "eu-west-2" }
+  ]
+  tag_name = terraform.workspace
 }
