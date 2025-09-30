dc6943b81a43c4ae326fd48c1200a23fcd8dc016
Toby Petty <tobypetty1@gmail.com>
2025-09-30 16:21:57 +0100

    Fix terraform lint errors

--- terraform/pipeline/main.tf
+++ terraform/pipeline/main.tf
@@ -15,8 +15,8 @@ terraform {
 }
 
 locals {
-  workspace_prefix               = substr(lower(replace(terraform.workspace, "/[^a-zA-Z0-9]+/", "-")), 0, 30)
-  is_development_environment     = local.workspace_prefix != "main"
+  workspace_prefix           = substr(lower(replace(terraform.workspace, "/[^a-zA-Z0-9]+/", "-")), 0, 30)
+  is_development_environment = local.workspace_prefix != "main"
 }
 
 data "aws_caller_identity" "current" {}
