da2f12c419acd22ecd51dd15a5c25a4ab6cd2700
Roy Price <Roy.price1988@gmail.com>
2025-10-08 12:49:12 +0100

    1032 - Added the numerator to file name, so its writes multiple files.

--- projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/estimate_ind_cqc_filled_posts_by_job_role.py
+++ projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/estimate_ind_cqc_filled_posts_by_job_role.py
@@ -112,7 +112,7 @@ def main(
     batch_size = pl.lit(100000, pl.Int64())
     for i in range(0, total_rows, batch_size):
         estimated_ind_cqc_filled_posts_by_job_role_lf.slice(i, batch_size).sink_parquet(
-            path=f"{estimated_ind_cqc_filled_posts_by_job_role_destination}estimated_ind_cqc_filled_posts_by_job_role_lf.parquet",
+            path=f"{estimated_ind_cqc_filled_posts_by_job_role_destination}estimated_ind_cqc_filled_posts_by_job_role_lf-{i}.parquet",
             compression="snappy",
         )
 
