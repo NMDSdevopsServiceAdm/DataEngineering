fec251dc56b560abe10f9da0714e8e2cdf466b3c
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2025-10-13 14:51:09 +0100

    Add incorrect postcodes to dict

--- projects/_01_ingest/cqc_api/utils/postcode_replacement_dictionary.py
+++ projects/_01_ingest/cqc_api/utils/postcode_replacement_dictionary.py
@@ -10,8 +10,14 @@ class ManualPostcodeCorrections:
     """
 
     postcode_corrections_dict = {
+        "CF105AS": None,  # Welsh postcode
         "CH52LY": "CH16HU",  # Welsh postcode, replaced with nearest English postcode
+        "CH53DJ": "CH48PH",  # Welsh postcode, replaced with nearest English postcode
+        "CH71AP": "CH48PH",  # Welsh postcode, replaced with nearest English postcode
         "DH66QZ": "DH96QZ",  # Incorrectly entered and no other postcodes exist starting 'DH6 6'
         "HU17ORH": "HU170RH",  # Replaced letter 'O' with zero '0'
+        "LL137YP": "CH36QD",  # Welsh postcode, replaced with nearest English postcode
+        "NP206QS": None,  # Welsh postcode
         "TF73QH": "TF74EH",  # Incorrectly entered and no other postcodes exist starting 'TF7 3'
+        "W19RR": "W1G9RR",  # Incorrectly entered - missing 'G'
     }
