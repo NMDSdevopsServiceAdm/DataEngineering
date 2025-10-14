d4c8cfebf1be4b72aacebc472d2e0e81146d08d7
Roy Price <Roy.price1988@gmail.com>
2025-10-14 10:10:44 +0100

    1032 - Commented out util calls in main function

--- projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/estimate_ind_cqc_filled_posts_by_job_role.py
+++ projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/estimate_ind_cqc_filled_posts_by_job_role.py
@@ -54,23 +54,23 @@ def main(
         cleaned_ascwds_worker_source,
     ).select(cleaned_ascwds_worker_columns_to_import)
 
-    aggregated_worker_lf = JRUtils.aggregate_ascwds_worker_job_roles_per_establishment(
-        cleaned_ascwds_worker_lf, JRUtils.LIST_OF_JOB_ROLES_SORTED
-    )
+    # aggregated_worker_lf = JRUtils.aggregate_ascwds_worker_job_roles_per_establishment(
+    #     cleaned_ascwds_worker_lf, JRUtils.LIST_OF_JOB_ROLES_SORTED
+    # )
 
-    estimated_ind_cqc_filled_posts_by_job_role_lf = (
-        JRUtils.join_worker_to_estimates_dataframe(
-            estimated_ind_cqc_filled_posts_lf, aggregated_worker_lf
-        )
-    )
+    # estimated_ind_cqc_filled_posts_by_job_role_lf = (
+    #     JRUtils.join_worker_to_estimates_dataframe(
+    #         estimated_ind_cqc_filled_posts_lf, aggregated_worker_lf
+    #     )
+    # )
 
-    plan = estimated_ind_cqc_filled_posts_by_job_role_lf.explain(
-        streaming=True,
+    plan = estimated_ind_cqc_filled_posts_lf.explain(
+        engine="streaming",
     )
     print(plan)
 
     utils.write_to_parquet(
-        estimated_ind_cqc_filled_posts_by_job_role_lf.collect(engine="streaming"),
+        estimated_ind_cqc_filled_posts_lf.collect(engine="streaming"),
         f"{estimated_ind_cqc_filled_posts_by_job_role_destination}estimated_ind_cqc_filled_posts_by_job_role_lf.parquet",
         logger=logger,
         append=False,
