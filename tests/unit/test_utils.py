4243d99af446ad9cce9ef98720b7e20312b825da
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2025-10-14 14:15:29 +0100

    Create test and test data for reading incorrect postcode csv to dict

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,8 +1,9 @@
+import csv
 import shutil
 import unittest
 from datetime import date, datetime
 from enum import Enum
-from io import BytesIO
+from io import BytesIO, StringIO
 from pathlib import Path
 
 import boto3
@@ -353,8 +354,24 @@ class GeneralUtilsTests(UtilsTests):
         self.assertEqual(df.count(), 3)
 
     def test_read_incorrect_postcode_csv_to_dict(self):
+        with open(self.test_postcode_csv_path, "rb") as file:
+            body = file.read()
+        byte_string_length = len(body)
+
+        body = StreamingBody(BytesIO(body), byte_string_length)
+
+        test_response = {
+            "Body": body,
+            "ContentLength": byte_string_length * self.smaller_string_boost,
+        }
+
+        expected_params = {"Bucket": "test-bucket", "Key": "my-test/key/"}
+
+        stubber = StubberClass(StubberType.client)
+        stubber.add_response("get_object", test_response, expected_params)
+
         returned_dict = utils.read_incorrect_postcode_csv_to_dict(
-            self.test_postcode_csv_path
+            "test-bucket", "my-test/key/", stubber.get_s3_client()
         )
         expected_dict = UtilsData.expected_dict
         self.assertEqual(returned_dict, expected_dict)
