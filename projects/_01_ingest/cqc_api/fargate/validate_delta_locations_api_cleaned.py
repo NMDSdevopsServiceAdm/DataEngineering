63e3d899bdd02dde4aa036481d619943d591b541
AchyutRatkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2025-10-16 10:54:37 +0100

    removed dataset row count validation from validate delta clean cqc location job

--- projects/_01_ingest/cqc_api/fargate/validate_delta_locations_api_cleaned.py
+++ projects/_01_ingest/cqc_api/fargate/validate_delta_locations_api_cleaned.py
@@ -66,10 +66,10 @@ def main(
             actions=GLOBAL_ACTIONS,
         )
         # dataset size
-        .row_count_match(
-            expected_row_count,
-            brief=f"Cleaned file has {source_df.height} rows but expecting {expected_row_count} rows",
-        )
+        # .row_count_match(
+        #     expected_row_count,
+        #     brief=f"Cleaned file has {source_df.height} rows but expecting {expected_row_count} rows",
+        # )
         # complete columns
         .col_vals_not_null(
             [
