9a1e3ce2d144e23ade075bfd4f8fd41a8ecbf5f0
AchyutRatkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2025-10-17 16:37:25 +0100

    full run or merge ind cqc job with logs

--- utils/utils.py
+++ utils/utils.py
@@ -336,7 +336,7 @@ def join_dimension(
 
     current_dimension = current_dimension.repartition(
         primary_key, DimensionKeys.import_date
-    )
+    ).withColumnRenamed(DimensionKeys.import_date, "d_import_date")
     fact_df = fact_df.repartition(primary_key, DimensionKeys.import_date)
 
     joined_df = fact_df.join(
@@ -348,7 +348,7 @@ def join_dimension(
             # Keys.import_date,
         ],
         how="left",
-    )
+    ).drop("d_import_date")
 
     window_spec = Window.partitionBy(primary_key).orderBy(
         F.col(DimensionKeys.import_date)
