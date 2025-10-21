34963d8ce7830bfb6a1c8363e726d2f47f12588e
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-10-21 09:49:26 +0100

    1105 - Added tests for function

---
+++ tests/test_polars_utils/test_cleaning_utils.py
@@ -0,0 +1,147 @@
+import unittest
+
+import polars as pl
+import polars.testing as pl_testing
+
+import polars_utils.cleaning_utils as job
+from tests.test_polars_utils_data import CleaningUtilsData as Data
+from tests.test_polars_utils_schemas import CleaningUtilsSchemas as Schemas
+from utils.column_names.cleaned_data_files.ascwds_workplace_cleaned import (
+    AscwdsWorkplaceCleanedColumns as AWPClean,
+)
+from utils.column_names.cleaned_data_files.cqc_location_cleaned import (
+    CqcLocationCleanedColumns as CQCLClean,
+)
+
+
+class PolarsCleaningUtilsTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+class AddAlignedDateColumnsTests(PolarsCleaningUtilsTests):
+    def setUp(self):
+        self.primary_column = CQCLClean.cqc_location_import_date
+        self.secondary_column = AWPClean.ascwds_workplace_import_date
+
+        self.primary_lf = pl.LazyFrame(
+            Data.align_dates_primary_rows, Schemas.align_dates_primary_schema
+        )
+        self.secondary_lf = pl.LazyFrame(
+            Data.align_dates_secondary_rows,
+            Schemas.align_dates_secondary_schema,
+        )
+        self.column_order_for_assertion = [
+            self.primary_column,
+            self.secondary_column,
+        ]
+        self.returned_lf = job.add_aligned_date_column(
+            self.primary_lf,
+            self.secondary_lf,
+            self.primary_column,
+            self.secondary_column,
+        )
+
+    def test_function_returns_the_original_number_of_rows(self):
+        returned_rows = self.returned_lf.collect().height
+        original_rows = self.primary_lf.collect().height
+
+        self.assertEqual(returned_rows, original_rows)
+
+    def test_expected_columns_are_returned(self):
+        returned_columns = sorted(self.returned_lf.collect_schema().names())
+        expected_columns = sorted(
+            self.primary_lf.collect_schema().names() + [self.secondary_column]
+        )
+
+        self.assertEqual(returned_columns, expected_columns)
+
+    def test_mix_of_misaligned_dates_joined_correctly(self):
+        expected_lf = pl.LazyFrame(
+            Data.expected_align_dates_primary_with_secondary_rows,
+            Schemas.expected_merged_dates_schema,
+        )
+
+        pl_testing.assert_frame_equal(
+            self.returned_lf.select(self.column_order_for_assertion).sort(
+                self.primary_column
+            ),
+            expected_lf.select(self.column_order_for_assertion).sort(
+                self.primary_column
+            ),
+        )
+
+    def test_exact_match_date_joined_correctly(self):
+        primary_lf = pl.LazyFrame(
+            Data.align_dates_primary_single_row, Schemas.align_dates_primary_schema
+        )
+        secondary_lf = pl.LazyFrame(
+            Data.align_dates_secondary_exact_match_rows,
+            Schemas.align_dates_secondary_schema,
+        )
+        returned_lf = job.add_aligned_date_column(
+            primary_lf,
+            secondary_lf,
+            self.primary_column,
+            self.secondary_column,
+        )
+
+        expected_lf = pl.LazyFrame(
+            Data.expected_align_dates_secondary_exact_match_rows,
+            Schemas.expected_merged_dates_schema,
+        )
+
+        pl_testing.assert_frame_equal(
+            returned_lf.select(self.column_order_for_assertion),
+            expected_lf.select(self.column_order_for_assertion),
+        )
+
+    def test_closest_historical_date_joined_correctly(self):
+        primary_lf = pl.LazyFrame(
+            Data.align_dates_primary_single_row, Schemas.align_dates_primary_schema
+        )
+        secondary_lf = pl.LazyFrame(
+            Data.align_dates_secondary_closest_historical_rows,
+            Schemas.align_dates_secondary_schema,
+        )
+        returned_lf = job.add_aligned_date_column(
+            primary_lf,
+            secondary_lf,
+            self.primary_column,
+            self.secondary_column,
+        )
+
+        expected_lf = pl.LazyFrame(
+            Data.expected_align_dates_secondary_closest_historical_rows,
+            Schemas.expected_merged_dates_schema,
+        )
+
+        pl_testing.assert_frame_equal(
+            returned_lf.select(self.column_order_for_assertion),
+            expected_lf.select(self.column_order_for_assertion),
+        )
+
+    def test_future_date_only_returns_null_secondary_column(self):
+        primary_lf = pl.LazyFrame(
+            Data.align_dates_primary_single_row, Schemas.align_dates_primary_schema
+        )
+        secondary_lf = pl.LazyFrame(
+            Data.align_dates_secondary_future_rows,
+            Schemas.align_dates_secondary_schema,
+        )
+        returned_lf = job.add_aligned_date_column(
+            primary_lf,
+            secondary_lf,
+            self.primary_column,
+            self.secondary_column,
+        )
+
+        expected_lf = pl.LazyFrame(
+            Data.expected_align_dates_secondary_future_rows,
+            Schemas.expected_merged_dates_schema,
+        )
+
+        pl_testing.assert_frame_equal(
+            returned_lf.select(self.column_order_for_assertion),
+            expected_lf.select(self.column_order_for_assertion),
+        )
---
+++ tests/test_polars_utils_data.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from datetime import date
+
+
+@dataclass
+class CleaningUtilsData:
+    align_dates_primary_rows = [
+        (date(2019, 1, 1), date(2021, 1, 15), date(2020, 1, 8), date(2021, 1, 15)),
+        ("1-001", "1-001", "1-001", "1-002"),
+    ]
+
+    align_dates_secondary_rows = [
+        (
+            date(2020, 1, 1),
+            date(2021, 1, 1),
+            date(2020, 1, 5),
+            date(2020, 1, 1),
+            date(2021, 1, 5),
+            date(2022, 1, 1),
+        ),
+        ("123", "123", "123", "456", "789", "789"),
+    ]
+    expected_align_dates_primary_with_secondary_rows = [
+        (date(2019, 1, 1), date(2020, 1, 8), date(2021, 1, 15), date(2021, 1, 15)),
+        ("1-001", "1-001", "1-001", "1-002"),
+        (None, date(2020, 1, 5), date(2021, 1, 5), date(2021, 1, 5)),
+    ]
+
+    align_dates_primary_single_row = [(date(2025, 5, 1),), ("1-001",)]
+
+    align_dates_secondary_exact_match_rows = [(date(2025, 5, 1),), ("123",)]
+    expected_align_dates_secondary_exact_match_rows = [
+        (date(2025, 5, 1),),
+        ("1-001",),
+        (date(2025, 5, 1),),
+    ]
+
+    align_dates_secondary_closest_historical_rows = [
+        (date(2024, 4, 4), date(2023, 3, 3), date(2025, 5, 5)),
+        ("123", "123", "123"),
+    ]
+    expected_align_dates_secondary_closest_historical_rows = [
+        (date(2025, 5, 1),),
+        ("1-001",),
+        (date(2024, 4, 4),),
+    ]
+
+    align_dates_secondary_future_rows = [(date(2025, 5, 5),), ("123",)]
+    expected_align_dates_secondary_future_rows = [
+        (date(2025, 5, 1),),
+        ("1-001",),
+        (None,),
+    ]
