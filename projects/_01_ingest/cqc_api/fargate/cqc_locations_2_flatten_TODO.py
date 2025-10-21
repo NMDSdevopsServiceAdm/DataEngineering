6aaf757c6abe0c5b2b478e073cf07a628fc01de8
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-10-21 10:46:18 +0100

    1092 - read in CQC data

--- projects/_01_ingest/cqc_api/fargate/cqc_locations_2_flatten_TODO.py
+++ projects/_01_ingest/cqc_api/fargate/cqc_locations_2_flatten_TODO.py
@@ -1,4 +1,50 @@
-# TODO - scan parquet to get delta data (only select cols required)
+from polars_utils import logger, utils
+from schemas.cqc_locations_schema_polars import POLARS_LOCATION_SCHEMA
+from utils.column_names.cleaned_data_files.cqc_location_cleaned import (
+    CqcLocationCleanedColumns as CQCLClean,
+)
+from utils.column_names.ind_cqc_pipeline_columns import PartitionKeys as Keys
+
+logger = logger.get_logger(__name__)
+
+cqc_partition_keys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
+
+cqc_location_cols_to_import = [
+    CQCLClean.location_id,
+    CQCLClean.provider_id,
+    CQCLClean.name,
+    CQCLClean.postal_address_line1,
+    CQCLClean.postal_code,
+    CQCLClean.registration_status,
+    CQCLClean.registration_date,
+    CQCLClean.deregistration_date,
+    CQCLClean.type,
+    CQCLClean.relationships,
+    CQCLClean.care_home,
+    CQCLClean.number_of_beds,
+    CQCLClean.dormancy,
+    CQCLClean.gac_service_types,
+    CQCLClean.regulated_activities,
+    CQCLClean.specialisms,
+    Keys.import_date,
+    Keys.year,
+    Keys.month,
+    Keys.day,
+]
+
+
+def main(
+    cqc_locations_source: str,
+    cleaned_ons_source: str,
+    flattened_cqc_locations_destination: str,
+) -> None:
+    # TODO - scan parquet to get delta data (only select cols required)
+    cqc_lf = utils.scan_parquet(
+        cqc_locations_source,
+        schema=POLARS_LOCATION_SCHEMA,
+        selected_columns=cqc_location_cols_to_import,
+    )
+
 
 # TODO - remove_records_from_locations_data
 
