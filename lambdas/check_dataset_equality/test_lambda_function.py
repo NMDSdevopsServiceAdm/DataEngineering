ce6b66ad91b064ecaac6437a518f1b22ff8c3b92
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2025-10-24 14:12:25 +0100

    Rename check dataset equality script

--- lambdas/check_dataset_equality/test_lambda_function.py
+++ lambdas/check_dataset_equality/test_lambda_function.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 
 import polars as pl
 
-from lambdas.check_dataset_equality.lambda_function import lambda_handler
+from lambdas.check_dataset_equality.check_dataset_equality import lambda_handler
 
 
 class TestLambdaFunction(unittest.TestCase):
