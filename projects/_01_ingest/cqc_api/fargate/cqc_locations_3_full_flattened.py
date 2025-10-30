6efa43612d7e8438017343d600e5219b60dfe03a
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-10-30 14:15:21 +0000

    1142 - Refactor to include the very first period, and consistent partition dtypes

--- projects/_01_ingest/cqc_api/fargate/cqc_locations_3_full_flattened.py
+++ projects/_01_ingest/cqc_api/fargate/cqc_locations_3_full_flattened.py
@@ -57,8 +57,20 @@ def main(
         f"Processing {len(import_dates_to_process)} new import_dates: {import_dates_to_process}"
     )
 
-    # Loop through new import_dates in date order
+    # If there is an existing full snapshot in destination, load the latest one.
+    latest_existing_date = None
     full_lf = None
+    if dest_import_dates:
+        latest_existing_date = max(dest_import_dates)
+        logger.info(
+            f"Loading latest existing full snapshot import_date={latest_existing_date}"
+        )
+        # Lazy scan and filter down to that snapshot (keeps it lazy)
+        full_lf = utils.scan_parquet(full_flattened_destination).filter(
+            pl.col(Keys.import_date) == latest_existing_date
+        )
+
+    # Loop through new import_dates in chronological order
     for index, delta_import_date in enumerate(sorted(import_dates_to_process)):
         logger.info(
             f"Processing import_date={delta_import_date} ({index+1} of {len(import_dates_to_process)})"
@@ -67,45 +79,37 @@ def main(
         # Filter delta for this import_date
         delta_lf = entire_delta_lf.filter(pl.col(Keys.import_date) == delta_import_date)
 
-        # Merge with previous full snapshot
-        if full_lf is None and not dest_import_dates:
-            # first ever snapshot
-            full_lf = delta_lf
+        # If there is no previous full_lf then the merged snapshot is the delta for the first ever snapshot.
+        # If there is, merge delta data with previous full snapshot, keeping the latest record for each location_id.
+        if full_lf is None:
+            merged_lf = delta_lf
         else:
-            # Load previous full snapshot from destination if exists and get the latest full snapshot
-            if full_lf is None:
-                latest_existing_date = max(dest_import_dates)
-
-                full_lf = utils.scan_parquet(full_flattened_destination).filter(
-                    pl.col(Keys.import_date) == latest_existing_date
-                )
-
-            # Merge delta with previous full snapshot, keeping latest record for each location_id
             merged_lf = pl.concat([full_lf, delta_lf]).unique(
                 subset=[CQCLClean.location_id], keep="last"
             )
-            # Assign partition columns based on current import_date
-            date_str = str(delta_import_date)
-            merged_lf = merged_lf.with_columns(
-                [
-                    pl.lit(date_str).cast(pl.Int64).alias(Keys.import_date),
-                    pl.lit(date_str[:4]).cast(pl.Int64).alias(Keys.year),
-                    pl.lit(date_str[4:6]).cast(pl.Int64).alias(Keys.month),
-                    pl.lit(date_str[6:8]).cast(pl.Int64).alias(Keys.day),
-                ]
-            )
 
-            # Store this snapshot in data in S3
-            utils.sink_to_parquet(
-                merged_lf,
-                full_flattened_destination,
-                logger=logger,
-                partition_cols=cqc_partition_keys,
-                append=False,
-            )
+        # Assign partition columns based on current import_date
+        date_str = str(delta_import_date)
+        merged_lf = merged_lf.with_columns(
+            [
+                pl.lit(date_str).alias(Keys.import_date),
+                pl.lit(date_str[:4]).alias(Keys.year),
+                pl.lit(date_str[4:6]).alias(Keys.month),
+                pl.lit(date_str[6:8]).alias(Keys.day),
+            ]
+        )
 
-            # Reassign this merged snapshot for next delta if required
-            full_lf = merged_lf
+        # Store this snapshot in data in S3
+        utils.sink_to_parquet(
+            merged_lf,
+            full_flattened_destination,
+            logger=logger,
+            partition_cols=cqc_partition_keys,
+            append=False,
+        )
+
+        # Reassign this merged snapshot for next delta if required
+        full_lf = merged_lf
 
     logger.info("All new import_dates processed successfully")
 
