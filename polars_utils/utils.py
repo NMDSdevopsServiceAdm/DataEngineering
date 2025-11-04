0d1c127d7b57db462fd23a0919e7bdee3a656258
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-11-04 18:31:36 +0000

    1116 - Added mkdir to sink parquet when not using partitions

--- polars_utils/utils.py
+++ polars_utils/utils.py
@@ -209,7 +209,7 @@ def sink_to_parquet(
                 f"LazyFrame sunk to Parquet at {output_path} partitioned by {partition_cols}"
             )
         else:
-            lazy_df.sink_parquet(output_path, engine="streaming")
+            lazy_df.sink_parquet(output_path, mkdir=True, engine="streaming")
             logger.info(
                 f"LazyFrame sunk to Parquet at {output_path} without partitioning"
             )
