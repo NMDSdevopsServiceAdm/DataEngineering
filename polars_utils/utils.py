4feb849ecc04c35471b1268fea30b0c49dc688df
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-11-06 08:52:27 +0000

    1116 - Include file name in path

--- polars_utils/utils.py
+++ polars_utils/utils.py
@@ -209,12 +209,9 @@ def sink_to_parquet(
                 f"LazyFrame sunk to Parquet at {output_path} partitioned by {partition_cols}"
             )
         else:
-            path = pl.PartitionByKey(
-                base_path=f"{output_path}",
-                include_key=False,
-                by=[],
+            lazy_df.sink_parquet(
+                f"{output_path}file.parquet", mkdir=True, engine="streaming"
             )
-            lazy_df.sink_parquet(path=path, mkdir=True, engine="streaming")
             logger.info(
                 f"LazyFrame sunk to Parquet at {output_path} without partitioning"
             )
