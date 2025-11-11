321bd59357996b82f72cc79788c9c66f7e3fdd7d
Roy Price <Roy.price1988@gmail.com>
2025-11-11 13:30:30 +0000

    Revert "Trying to fix terraform issue."
    This reverts commit b103aa06b0170b34a120806f99a24081d24bb928.

--- terraform/pipeline/step-function.tf
+++ terraform/pipeline/step-function.tf
@@ -14,7 +14,7 @@ resource "aws_sfn_state_machine" "run_crawler" {
   definition = templatefile("step-functions/Run-Crawler.json", {})
 
   logging_configuration {
-    log_destination        = aws_cloudwatch_log_group.state_machines.arn
+    log_destination        = "${aws_cloudwatch_log_group.state_machines.arn}:*"
     include_execution_data = true
     level                  = "ERROR"
   }
@@ -41,7 +41,7 @@ resource "aws_sfn_state_machine" "workforce_intelligence_state_machine" {
   })
 
   logging_configuration {
-    log_destination        = aws_cloudwatch_log_group.state_machines.arn
+    log_destination        = "${aws_cloudwatch_log_group.state_machines.arn}:*"
     include_execution_data = true
     level                  = "ERROR"
   }
@@ -66,7 +66,7 @@ resource "aws_sfn_state_machine" "cqc_and_ascwds_orchestrator_state_machine" {
   })
 
   logging_configuration {
-    log_destination        = aws_cloudwatch_log_group.state_machines.arn
+    log_destination        = "${aws_cloudwatch_log_group.state_machines.arn}:*"
     include_execution_data = true
     level                  = "ERROR"
   }
@@ -191,7 +191,7 @@ resource "aws_sfn_state_machine" "sf_pipelines" {
   })
 
   logging_configuration {
-    log_destination        = aws_cloudwatch_log_group.state_machines.arn
+    log_destination        = "${aws_cloudwatch_log_group.state_machines.arn}:*"
     include_execution_data = true
     level                  = "ERROR"
   }
@@ -204,7 +204,7 @@ resource "aws_sfn_state_machine" "sf_pipelines" {
 
 
 resource "aws_cloudwatch_log_group" "state_machines" {
-  name = "/aws/vendedlogs/states/${local.workspace_prefix}-state-machines"
+  name_prefix = "/aws/vendedlogs/states/${local.workspace_prefix}-state-machines"
 }
 
 resource "aws_iam_role" "step_function_iam_role" {
