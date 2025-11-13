e4c902902664e0b3fac585a0078706b4129b4e15
AchyutRatkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2025-11-13 09:51:36 +0000

    got registered and deregistered both validations

--- projects/_01_ingest/cqc_api/fargate/validate_cqc_locations_4_full_latest_snapshot.py
+++ projects/_01_ingest/cqc_api/fargate/validate_cqc_locations_4_full_latest_snapshot.py
@@ -57,7 +57,10 @@ def main(bucket_name: str, source_path: str, reports_path: str) -> None:
             ]
         )
         # categorical column values match expected set
-        .col_vals_in_set(CQCLClean.registration_status, [RegistrationStatus.registered,RegistrationStatus.deregistered])
+        .col_vals_in_set(
+            CQCLClean.registration_status,
+            [RegistrationStatus.registered, RegistrationStatus.deregistered],
+        )
         # numeric column values are between (inclusive)
         .col_vals_between(Validation.location_id_length, 3, 14).interrogate()
     )
