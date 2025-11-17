5a693bb91c31743be01acf009ee320177ee547c1
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-11-17 16:38:39 +0000

    1211 - Remove individual ticket TODOs

--- projects/_03_independent_cqc/_04_feature_engineering/fargate/utils/feature_utils.py
+++ projects/_03_independent_cqc/_04_feature_engineering/fargate/utils/feature_utils.py
@@ -5,26 +5,21 @@ import polars as pl
 from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCQC
 
 
-# TODO ticket #1213
 def expand_encode_and_extract_features():
     pass
 
 
-# TODO ticket #1215
 def add_array_column_count():
     pass
 
 
-# TODO ticket #1212
 def cap_integer_at_max_value():
     pass
 
 
-# TODO ticket #1216
 def add_date_index_column():
     pass
 
 
-# TODO ticket #1214
 def group_rural_urban_sparse_categories():
     pass
