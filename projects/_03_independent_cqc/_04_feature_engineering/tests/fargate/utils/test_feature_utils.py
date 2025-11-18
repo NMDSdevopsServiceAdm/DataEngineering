ce2368742ab4e6351e88019b4832635612f92df9
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2025-11-18 11:25:45 +0000

    1211 - Added tests

--- projects/_03_independent_cqc/_04_feature_engineering/tests/fargate/utils/test_feature_utils.py
+++ projects/_03_independent_cqc/_04_feature_engineering/tests/fargate/utils/test_feature_utils.py
@@ -1,26 +1,262 @@
 import unittest
 
+import polars as pl
+import polars.testing as pl_testing
+
 from projects._03_independent_cqc._04_feature_engineering.fargate.utils import (
     feature_utils as job,
+)
+from projects._03_independent_cqc.unittest_data.polars_ind_cqc_test_file_data import (
+    FeaturesEngineeringUtilsData as Data,
+)
+from projects._03_independent_cqc.unittest_data.polars_ind_cqc_test_file_schemas import (
+    FeaturesEngineeringUtilsSchemas as Schemas,
 )
 from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCQC
 
 
-class ExpandEncodeAndExtractFeaturesTests(unittest.TestCase):
-    pass
-
-
 class AddArrayColumnCountTests(unittest.TestCase):
-    pass
+    def test_returns_expected_lf_when_one_element_in_array(self):
+        test_lf = pl.DataFrame(
+            Data.add_array_column_count_with_one_element_rows,
+            Schemas.add_array_column_count_schema,
+            orient="row",
+        )
+        returned_lf = job.add_array_column_count(
+            test_lf,
+            new_col_name=IndCQC.service_count,
+            col_to_check=IndCQC.services_offered,
+        )
 
+        expected_lf = pl.DataFrame(
+            Data.expected_add_array_column_count_with_one_element_rows,
+            Schemas.expected_add_array_column_count_schema,
+            orient="row",
+        )
 
-class CapIntegerAtMaxValueTests(unittest.TestCase):
-    pass
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
+
+    def test_returns_expected_lf_when_multiple_elements_in_array(self):
+        test_lf = pl.DataFrame(
+            Data.add_array_column_count_with_multiple_elements_rows,
+            Schemas.add_array_column_count_schema,
+            orient="row",
+        )
+        returned_lf = job.add_array_column_count(
+            test_lf,
+            new_col_name=IndCQC.service_count,
+            col_to_check=IndCQC.services_offered,
+        )
+
+        expected_lf = pl.DataFrame(
+            Data.expected_add_array_column_count_with_multiple_elements_rows,
+            Schemas.expected_add_array_column_count_schema,
+            orient="row",
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
+
+    def test_returns_zero_when_array_is_empty(self):
+        test_lf = pl.DataFrame(
+            Data.add_array_column_count_with_empty_array_rows,
+            Schemas.add_array_column_count_schema,
+            orient="row",
+        )
+        returned_lf = job.add_array_column_count(
+            test_lf,
+            new_col_name=IndCQC.service_count,
+            col_to_check=IndCQC.services_offered,
+        )
+
+        expected_lf = pl.DataFrame(
+            Data.expected_add_array_column_count_with_empty_array_rows,
+            Schemas.expected_add_array_column_count_schema,
+            orient="row",
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
+
+    def test_returns_zero_when_array_is_null(self):
+        test_lf = pl.DataFrame(
+            Data.add_array_column_count_with_null_value_rows,
+            Schemas.add_array_column_count_schema,
+            orient="row",
+        )
+        returned_lf = job.add_array_column_count(
+            test_lf,
+            new_col_name=IndCQC.service_count,
+            col_to_check=IndCQC.services_offered,
+        )
+
+        expected_lf = pl.DataFrame(
+            Data.expected_add_array_column_count_with_null_value_rows,
+            Schemas.expected_add_array_column_count_schema,
+            orient="row",
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
 
 
 class AddDateIndexColumnTests(unittest.TestCase):
-    pass
+    def test_returns_same_index_for_same_import_dates(self):
+        test_lf = pl.DataFrame(
+            Data.add_date_index_column_same_index_for_same_date_rows,
+            Schemas.add_date_index_column_schema,
+            orient="row",
+        )
+        returned_lf = job.add_date_index_column(test_lf)
+
+        expected_lf = pl.DataFrame(
+            Data.expected_add_date_index_column_same_index_for_same_date_rows,
+            Schemas.expected_add_date_index_column_schema,
+            orient="row",
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
+
+    def test_returns_expected_dense_ranked_indexes(self):
+        test_lf = pl.DataFrame(
+            Data.add_date_index_column_applies_incremental_index_rows,
+            Schemas.add_date_index_column_schema,
+            orient="row",
+        )
+        returned_lf = job.add_date_index_column(test_lf)
+
+        expected_lf = pl.DataFrame(
+            Data.expected_add_date_index_column_applies_incremental_index_rows,
+            Schemas.expected_add_date_index_column_schema,
+            orient="row",
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
+
+    def test_returns_indexes_separated_on_care_home_column(self):
+        test_lf = pl.DataFrame(
+            Data.add_date_index_column_indexes_by_care_home_rows,
+            Schemas.add_date_index_column_schema,
+            orient="row",
+        )
+        returned_lf = job.add_date_index_column(test_lf)
+
+        expected_lf = pl.DataFrame(
+            Data.expected_add_date_index_column_indexes_by_care_home_rows,
+            Schemas.expected_add_date_index_column_schema,
+            orient="row",
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
+
+
+class CapIntegerAtMaxValueTests(unittest.TestCase):
+    def test_returns_expected_lf(self):
+        test_lf = pl.DataFrame(
+            Data.cap_integer_at_max_value_rows,
+            Schemas.cap_integer_at_max_value_schema,
+            orient="row",
+        )
+        returned_lf = job.cap_integer_at_max_value(
+            test_lf,
+            col_name=IndCQC.service_count,
+            max_value=2,
+            new_col_name=IndCQC.service_count_capped,
+        )
+
+        expected_lf = pl.DataFrame(
+            Data.expected_cap_integer_at_max_value_rows,
+            Schemas.expected_cap_integer_at_max_value_schema,
+            orient="row",
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
+
+
+class ExpandEncodeAndExtractFeaturesTests(unittest.TestCase):
+    def test_returns_expected_lf_when_not_array(self):
+        test_lf = pl.DataFrame(
+            Data.expand_encode_and_extract_features_when_not_array_rows,
+            Schemas.expand_encode_and_extract_features_when_not_array_schema,
+            orient="row",
+        )
+        returned_lf = job.expand_encode_and_extract_features(
+            test_lf,
+            Schemas.col_with_categories,
+            Data.expand_encode_and_extract_features_lookup_dict,
+            is_array_col=False,
+        )
+        expected_lf = pl.DataFrame(
+            Data.expected_expand_encode_and_extract_features_when_not_array_rows,
+            Schemas.expected_expand_encode_and_extract_features_when_not_array_schema,
+            orient="row",
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
+
+    def test_returns_expected_lf_when_is_array(self):
+        test_lf = pl.DataFrame(
+            Data.expand_encode_and_extract_features_when_is_array_rows,
+            Schemas.expand_encode_and_extract_features_when_is_array_schema,
+            orient="row",
+        )
+        returned_lf = job.expand_encode_and_extract_features(
+            test_lf,
+            Schemas.col_with_categories,
+            Data.expand_encode_and_extract_features_lookup_dict,
+            is_array_col=True,
+        )
+        expected_lf = pl.DataFrame(
+            Data.expected_expand_encode_and_extract_features_when_is_array_rows,
+            Schemas.expected_expand_encode_and_extract_features_when_is_array_schema,
+            orient="row",
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
 
 
 class GroupRuralUrbanSparseCategoriesTests(unittest.TestCase):
-    pass
+    def test_returns_original_values_when_does_not_contain_sparse(self):
+        test_lf = pl.DataFrame(
+            Data.group_rural_urban_sparse_categories_non_sparse_do_not_change_rows,
+            Schemas.group_rural_urban_sparse_categories_schema,
+            orient="row",
+        )
+        returned_lf = job.group_rural_urban_sparse_categories(test_lf)
+
+        expected_lf = pl.DataFrame(
+            Data.expected_group_rural_urban_sparse_categories_non_sparse_do_not_change_rows,
+            Schemas.expected_group_rural_urban_sparse_categories_schema,
+            orient="row",
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
+
+    def test_returns_expected_value_when_does_contain_sparse(self):
+        test_lf = pl.DataFrame(
+            Data.group_rural_urban_sparse_categories_identifies_sparse_rows,
+            Schemas.group_rural_urban_sparse_categories_schema,
+            orient="row",
+        )
+        returned_lf = job.group_rural_urban_sparse_categories(test_lf)
+
+        expected_lf = pl.DataFrame(
+            Data.expected_group_rural_urban_sparse_categories_identifies_sparse_rows,
+            Schemas.expected_group_rural_urban_sparse_categories_schema,
+            orient="row",
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
+
+    def test_returns_expected_lf_when_combination_of_indicators(self):
+        test_lf = pl.DataFrame(
+            Data.group_rural_urban_sparse_categories_combination_rows,
+            Schemas.group_rural_urban_sparse_categories_schema,
+            orient="row",
+        )
+        returned_lf = job.group_rural_urban_sparse_categories(test_lf)
+
+        expected_lf = pl.DataFrame(
+            Data.expected_group_rural_urban_sparse_categories_combination_rows,
+            Schemas.expected_group_rural_urban_sparse_categories_schema,
+            orient="row",
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
--- projects/_03_independent_cqc/unittest_data/polars_ind_cqc_test_file_data.py
+++ projects/_03_independent_cqc/unittest_data/polars_ind_cqc_test_file_data.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from datetime import date
 
-from utils.column_values.categorical_column_values import MainJobRoleLabels
+from utils.column_values.categorical_column_values import CareHome, MainJobRoleLabels
 
 
 @dataclass
@@ -74,6 +74,157 @@ class PrepareJobRoleCountsUtilsData:
             MainJobRoleLabels.senior_care_worker,
         ),
         (1, 0, 1, 0),
+    ]
+
+
+@dataclass
+class FeaturesEngineeringUtilsData:
+    add_array_column_count_with_one_element_rows = [
+        ("1-001", ["name"]),
+    ]
+    expected_add_array_column_count_with_one_element_rows = [
+        ("1-001", ["name"], 1),
+    ]
+
+    add_array_column_count_with_multiple_elements_rows = [
+        ("1-001", ["name_1", "name_2", "name_3"]),
+    ]
+    expected_add_array_column_count_with_multiple_elements_rows = [
+        ("1-001", ["name_1", "name_2", "name_3"], 3),
+    ]
+
+    add_array_column_count_with_empty_array_rows = [
+        ("1-001", []),
+    ]
+    expected_add_array_column_count_with_empty_array_rows = [
+        ("1-001", [], 0),
+    ]
+
+    add_array_column_count_with_null_value_rows = [
+        ("1-001", None),
+    ]
+    expected_add_array_column_count_with_null_value_rows = [
+        ("1-001", None, 0),
+    ]
+
+    add_date_index_column_same_index_for_same_date_rows = [
+        ("1-0001", CareHome.not_care_home, date(2024, 12, 1)),
+        ("1-0002", CareHome.not_care_home, date(2024, 12, 1)),
+    ]
+    expected_add_date_index_column_same_index_for_same_date_rows = [
+        ("1-0001", CareHome.not_care_home, date(2024, 12, 1), 1),
+        ("1-0002", CareHome.not_care_home, date(2024, 12, 1), 1),
+    ]
+
+    add_date_index_column_applies_incremental_index_rows = [
+        ("1-0001", CareHome.not_care_home, date(2024, 12, 1)),
+        ("1-0002", CareHome.not_care_home, date(2024, 12, 1)),
+        ("1-0003", CareHome.not_care_home, date(2025, 2, 1)),
+    ]
+    expected_add_date_index_column_applies_incremental_index_rows = [
+        ("1-0001", CareHome.not_care_home, date(2024, 12, 1), 1),
+        ("1-0002", CareHome.not_care_home, date(2024, 12, 1), 1),
+        ("1-0003", CareHome.not_care_home, date(2025, 2, 1), 2),
+    ]
+
+    add_date_index_column_indexes_by_care_home_rows = [
+        ("1-0001", CareHome.not_care_home, date(2024, 12, 1)),
+        ("1-0002", CareHome.not_care_home, date(2025, 2, 1)),
+        ("1-0003", CareHome.care_home, date(2025, 2, 1)),
+    ]
+    expected_add_date_index_column_indexes_by_care_home_rows = [
+        ("1-0001", CareHome.not_care_home, date(2024, 12, 1), 1),
+        ("1-0002", CareHome.not_care_home, date(2025, 2, 1), 2),
+        ("1-0003", CareHome.care_home, date(2025, 2, 1), 1),
+    ]
+
+    cap_integer_at_max_value_rows = [
+        ("1-0001", 1),
+        ("1-0002", 2),
+        ("1-0003", 3),
+        ("1-0004", None),
+    ]
+    expected_cap_integer_at_max_value_rows = [
+        ("1-0001", 1, 1),
+        ("1-0002", 2, 2),
+        ("1-0003", 3, 2),
+        ("1-0004", None, None),
+    ]
+
+    expand_encode_and_extract_features_lookup_dict = {
+        "has_A": "A",
+        "has_B": "B",
+        "has_C": "C",
+    }
+    expected_expand_encode_and_extract_features_feature_list = [
+        "has_A",
+        "has_B",
+        "has_C",
+    ]
+
+    expand_encode_and_extract_features_when_not_array_rows = [
+        ("1-0001", "A"),
+        ("1-0002", "C"),
+        ("1-0003", "B"),
+        ("1-0004", "D"),
+        ("1-0005", None),
+    ]
+    expected_expand_encode_and_extract_features_when_not_array_rows = [
+        ("1-0001", "A", 1, 0, 0),
+        ("1-0002", "C", 0, 0, 1),
+        ("1-0003", "B", 0, 1, 0),
+        ("1-0004", "D", 0, 0, 0),
+        ("1-0005", None, None, None, None),
+    ]
+
+    expand_encode_and_extract_features_when_is_array_rows = [
+        ("1-0001", ["A", "B"]),
+        ("1-0002", ["B"]),
+        ("1-0003", ["C", "A"]),
+        ("1-0004", ["B", "D"]),
+        ("1-0005", None),
+    ]
+    expected_expand_encode_and_extract_features_when_is_array_rows = [
+        ("1-0001", ["A", "B"], 1, 1, 0),
+        ("1-0002", ["B"], 0, 1, 0),
+        ("1-0003", ["C", "A"], 1, 0, 1),
+        ("1-0004", ["B", "D"], 0, 1, 0),
+        ("1-0005", None, None, None, None),
+    ]
+
+    group_rural_urban_sparse_categories_non_sparse_do_not_change_rows = [
+        ("1-001", "Rural"),
+        ("1-002", "Urban"),
+    ]
+    expected_group_rural_urban_sparse_categories_non_sparse_do_not_change_rows = [
+        ("1-001", "Rural", "Rural"),
+        ("1-002", "Urban", "Urban"),
+    ]
+
+    group_rural_urban_sparse_categories_identifies_sparse_rows = [
+        ("1-001", "Rural sparse"),
+        ("1-002", "Another with sparse in it"),
+        ("1-003", "Capitalised Sparse"),
+    ]
+    expected_group_rural_urban_sparse_categories_identifies_sparse_rows = [
+        ("1-001", "Rural sparse", "Sparse setting"),
+        ("1-002", "Another with sparse in it", "Sparse setting"),
+        ("1-003", "Capitalised Sparse", "Sparse setting"),
+    ]
+
+    group_rural_urban_sparse_categories_combination_rows = [
+        ("1-001", "Rural"),
+        ("1-002", "Rural sparse"),
+        ("1-003", "Another with sparse in it"),
+        ("1-004", "Urban"),
+        ("1-005", "Sparse with a capital S"),
+    ]
+    expected_group_rural_urban_sparse_categories_combination_rows = [
+        ("1-001", "Rural", "Rural"),
+        ("1-002", "Rural sparse", "Sparse setting"),
+        ("1-003", "Another with sparse in it", "Sparse setting"),
+        ("1-004", "Urban", "Urban"),
+        ("1-005", "Sparse with a capital S", "Sparse setting"),
     ]
 
 
