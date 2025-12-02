f16f87e10ac37d054a2dc0910e1e7ded9fe48e04
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2025-12-02 14:49:27 +0000

    Include complex types

--- projects/_01_ingest/cqc_api/fargate/validate_cqc_locations_4_full_clean.py
+++ projects/_01_ingest/cqc_api/fargate/validate_cqc_locations_4_full_clean.py
@@ -51,7 +51,7 @@ def main(
         compare_path (str): path to a dataset to compare against for expected size
     """
     source_df = utils.read_parquet(
-        f"s3://{bucket_name}/{source_path}", exclude_complex_types=True
+        f"s3://{bucket_name}/{source_path}", exclude_complex_types=False
     ).with_columns(
         str_length_cols([CQCLClean.location_id, CQCLClean.provider_id]),
     )
