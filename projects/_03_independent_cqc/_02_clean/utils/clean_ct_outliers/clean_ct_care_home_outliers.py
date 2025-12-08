293e2b0f26512f780fb3e540f71221501dc1bef7
AchyutRatkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2025-12-08 09:53:38 +0000

    function call updated with correct dataFrame

--- projects/_03_independent_cqc/_02_clean/utils/clean_ct_outliers/clean_ct_care_home_outliers.py
+++ projects/_03_independent_cqc/_02_clean/utils/clean_ct_outliers/clean_ct_care_home_outliers.py
@@ -43,8 +43,8 @@ def clean_capacity_tracker_care_home_outliers(df: DataFrame) -> DataFrame:
 
     df = null_posts_per_bed_outliers(df)
 
-    locations_df = clean_longitudinal_outliers(
-        df=locations_df,
+    df = clean_longitudinal_outliers(
+        df=df,
         group_by_col=IndCQC.location_id,
         col_to_clean=IndCQC.ct_care_home_total_employed_cleaned,
         cleaned_column_name=IndCQC.ct_care_home_total_employed_cleaned,
