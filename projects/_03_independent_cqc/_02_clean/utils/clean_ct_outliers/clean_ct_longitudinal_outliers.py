6bd89f20213805c5008d8fe9ea89b0ced686638e
AchyutRatkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2025-12-09 09:48:27 +0000

    updated docstrings

--- projects/_03_independent_cqc/_02_clean/utils/clean_ct_outliers/clean_ct_longitudinal_outliers.py
+++ projects/_03_independent_cqc/_02_clean/utils/clean_ct_outliers/clean_ct_longitudinal_outliers.py
@@ -163,6 +163,7 @@ def compute_outlier_cutoff(
         df (DataFrame): DataFrame containing an 'abs_diff' column.
         group_by_col (str): Column to group by when computing cutoff thresholds.
         proportion_to_filter (float): Proportion of extreme values to consider as outliers.
+        col_to_clean (str): Column to use to get outlier cutoff.
 
     Returns:
         DataFrame: Original DataFrame joined with a new column 'abs_diff_cutoff'
