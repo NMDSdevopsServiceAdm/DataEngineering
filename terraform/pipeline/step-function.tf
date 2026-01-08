92f94f36c6dd6301627f4620c7b4302da4916534
Roy Price <Roy.price1988@gmail.com>
2026-01-08 14:59:30 +0000

    1277 - Deleted job script, test script for job, test data and schema used in tests, glue job and step function module name.

--- terraform/pipeline/step-function.tf
+++ terraform/pipeline/step-function.tf
@@ -86,7 +86,6 @@ resource "aws_sfn_state_machine" "sf_pipelines" {
     clean_ascwds_worker_job_name                                            = module.clean_ascwds_worker_job.job_name
     validate_ascwds_workplace_cleaned_data_job_name                         = module.validate_ascwds_workplace_cleaned_data_job.job_name
     validate_ascwds_worker_cleaned_data_job_name                            = module.validate_ascwds_worker_cleaned_data_job.job_name
-    merge_ind_cqc_data_job_name                                             = module.merge_ind_cqc_data_job.job_name
     validate_merged_ind_cqc_data_job_name                                   = module.validate_merged_ind_cqc_data_job.job_name
     clean_ind_cqc_filled_posts_job_name                                     = module.clean_ind_cqc_filled_posts_job.job_name
     validate_cleaned_ind_cqc_data_job_name                                  = module.validate_cleaned_ind_cqc_data_job.job_name
