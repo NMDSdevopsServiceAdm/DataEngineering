80167110868fca2cd39b7917b3866028351ed07e
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-01-19 08:36:48 +0000

    Test duplicate code removal

--- terraform/backend-setup/main.tf
+++ terraform/backend-setup/main.tf
@@ -1,28 +1,28 @@
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.region
-  alias      = "prod"
-  profile    = "prod"
-}
+# provider "aws" {
+#   access_key = var.aws_access_key
+#   secret_key = var.aws_secret_key
+#   region     = var.region
+#   alias      = "prod"
+#   profile    = "prod"
+# }
 
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.region
-  alias      = "non-prod"
-  profile    = "non-prod"
-}
+# provider "aws" {
+#   access_key = var.aws_access_key
+#   secret_key = var.aws_secret_key
+#   region     = var.region
+#   alias      = "non-prod"
+#   profile    = "non-prod"
+# }
 
-terraform {
-  backend "s3" {
-    # Bucket defined in ../*.s3.tfbackend
-    key            = "statefiles/workspace=default/backend.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     # Bucket defined in ../*.s3.tfbackend
+#     key            = "statefiles/workspace=default/backend.tfstate"
+#     region         = "eu-west-2"
+#     dynamodb_table = "terraform-locks"
+#     encrypt        = true
+#   }
+# }
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket
