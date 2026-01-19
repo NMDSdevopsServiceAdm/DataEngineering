7832b2f9fa3cbc4c507e7542ddaab34cf0451918
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-01-19 16:26:17 +0000

    1286 - Update variables to correct dataset

--- projects/_03_independent_cqc/_04_model/fargate/validate_model_01_features_non_res_with_dormancy.py
+++ projects/_03_independent_cqc/_04_model/fargate/validate_model_01_features_non_res_with_dormancy.py
@@ -6,16 +6,11 @@ from polars_utils import utils
 from polars_utils.expressions import str_length_cols
 from polars_utils.validation import actions as vl
 from polars_utils.validation.constants import GLOBAL_ACTIONS, GLOBAL_THRESHOLDS
-from projects._01_ingest.cqc_api.utils.validate_cqc_locations import (
+from projects._03_independent_cqc._04_model.utils.validate_models import (
     add_list_column_validation_check_flags,
-    get_expected_row_count_for_validation_full_clean,
+    get_expected_row_count_for_validation_model_01_features_non_res_with_dormancy,
 )
-from utils.column_names.cleaned_data_files.cqc_location_cleaned import (
-    CqcLocationCleanedColumns as CQCLClean,
-)
-from utils.column_names.cleaned_data_files.cqc_location_cleaned import (
-    CqcLocationCleanedNewValidationColumns as CQCLVal,
-)
+from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCQC
 from utils.column_names.ind_cqc_pipeline_columns import PartitionKeys as Keys
 from utils.column_names.validation_table_columns import Validation
 from utils.column_values.categorical_column_values import (
@@ -23,18 +18,12 @@ from utils.column_values.categorical_column_values import (
     NumericTrueFalse,
     RegistrationStatus,
 )
-from utils.column_values.categorical_columns_by_dataset import (
-    LocationsApiCleanedCategoricalValues as CatValues,
-)
 
 compare_columns_to_import = [
     Keys.import_date,
-    CQCLClean.location_id,
-    CQCLClean.provider_id,
-    CQCLClean.type,
-    CQCLClean.registration_status,
-    CQCLClean.regulated_activities_offered,
-    CQCLClean.services_offered,
+    IndCQC.location_id,
+    IndCQC.care_home,
+    IndCQC.dormancy,
 ]
 
 
@@ -53,21 +42,25 @@ def main(
     source_df = utils.read_parquet(
         f"s3://{bucket_name}/{source_path}", exclude_complex_types=False
     ).with_columns(
-        str_length_cols([CQCLClean.location_id, CQCLClean.provider_id]),
+        str_length_cols([IndCQC.location_id, IndCQC.provider_id]),
     )
 
     compare_df = utils.read_parquet(
         f"s3://{bucket_name}/{compare_path}",
         selected_columns=compare_columns_to_import,
     )
-    expected_row_count = get_expected_row_count_for_validation_full_clean(compare_df)
+    expected_row_count = (
+        get_expected_row_count_for_validation_model_01_features_non_res_with_dormancy(
+            compare_df
+        )
+    )
     source_df = add_list_column_validation_check_flags(
         source_df,
         [
-            CQCLClean.specialisms_offered,
-            CQCLClean.regulated_activities_offered,
-            CQCLClean.services_offered,
-            CQCLClean.registered_manager_names,
+            IndCQC.specialisms_offered,
+            IndCQC.regulated_activities_offered,
+            IndCQC.services_offered,
+            IndCQC.registered_manager_names,
         ],
     )
 
@@ -87,36 +80,36 @@ def main(
         # complete columns
         .col_vals_not_null(
             [
-                CQCLClean.location_id,
+                IndCQC.location_id,
                 Keys.import_date,
-                CQCLClean.cqc_location_import_date,
-                CQCLClean.name,
-                CQCLClean.type,
-                CQCLClean.imputed_registration_date,
-                CQCLClean.registration_status,
-                CQCLClean.provider_id,
-                CQCLClean.primary_service_type,
-                CQCLClean.primary_service_type_second_level,
-                CQCLClean.care_home,
-                CQCLClean.cqc_sector,
-                CQCLClean.related_location,
-                CQCLClean.specialism_dementia,
-                CQCLClean.specialism_learning_disabilities,
-                CQCLClean.specialism_mental_health,
-                CQCLClean.contemporary_ons_import_date,
-                CQCLClean.contemporary_cssr,
-                CQCLClean.contemporary_region,
-                CQCLClean.current_ons_import_date,
-                CQCLClean.current_cssr,
-                CQCLClean.current_region,
-                CQCLClean.current_rural_urban_ind_11,
+                IndCQC.cqc_location_import_date,
+                IndCQC.name,
+                IndCQC.type,
+                IndCQC.imputed_registration_date,
+                IndCQC.registration_status,
+                IndCQC.provider_id,
+                IndCQC.primary_service_type,
+                IndCQC.primary_service_type_second_level,
+                IndCQC.care_home,
+                IndCQC.cqc_sector,
+                IndCQC.related_location,
+                IndCQC.specialism_dementia,
+                IndCQC.specialism_learning_disabilities,
+                IndCQC.specialism_mental_health,
+                IndCQC.contemporary_ons_import_date,
+                IndCQC.contemporary_cssr,
+                IndCQC.contemporary_region,
+                IndCQC.current_ons_import_date,
+                IndCQC.current_cssr,
+                IndCQC.current_region,
+                IndCQC.current_rural_urban_ind_11,
             ]
         )
         # index columns
         .rows_distinct(
             [
-                CQCLClean.location_id,
-                CQCLClean.cqc_location_import_date,
+                IndCQC.location_id,
+                IndCQC.cqc_location_import_date,
                 Keys.import_date,
             ]
         )
@@ -141,169 +134,169 @@ def main(
             CQCLVal.specialisms_offered_has_no_empty_or_null, [NumericTrueFalse.true]
         )
         # categorical column values match expected set
-        .col_vals_in_set(CQCLClean.type, [LocationType.social_care_identifier])
-        .col_vals_in_set(CQCLClean.registration_status, [RegistrationStatus.registered])
+        .col_vals_in_set(IndCQC.type, [LocationType.social_care_identifier])
+        .col_vals_in_set(IndCQC.registration_status, [RegistrationStatus.registered])
         .col_vals_in_set(
-            CQCLClean.primary_service_type,
+            IndCQC.primary_service_type,
             CatValues.primary_service_type_column_values.categorical_values,
         )
         .col_vals_in_set(
-            CQCLClean.primary_service_type_second_level,
+            IndCQC.primary_service_type_second_level,
             CatValues.primary_service_type_second_level_column_values.categorical_values,
         )
         .col_vals_in_set(
-            CQCLClean.care_home, CatValues.care_home_column_values.categorical_values
+            IndCQC.care_home, CatValues.care_home_column_values.categorical_values
         )
         .col_vals_in_set(
-            CQCLClean.cqc_sector, CatValues.sector_column_values.categorical_values
+            IndCQC.cqc_sector, CatValues.sector_column_values.categorical_values
         )
         .col_vals_in_set(
-            CQCLClean.related_location,
+            IndCQC.related_location,
             CatValues.related_location_column_values.categorical_values,
         )
         .col_vals_in_set(
-            CQCLClean.specialism_dementia,
+            IndCQC.specialism_dementia,
             CatValues.specialism_dementia_column_values.categorical_values,
         )
         .col_vals_in_set(
-            CQCLClean.specialism_learning_disabilities,
+            IndCQC.specialism_learning_disabilities,
             CatValues.specialism_learning_disabilities_column_values.categorical_values,
         )
         .col_vals_in_set(
-            CQCLClean.specialism_mental_health,
+            IndCQC.specialism_mental_health,
             CatValues.specialism_mental_health_column_values.categorical_values,
         )
         .col_vals_in_set(
-            CQCLClean.dormancy,
+            IndCQC.dormancy,
             # na_pass is not an optional parameter to .col_vals_in_set
             # extending list to allow for null values as not included
             # in categorical values
             [*CatValues.dormancy_column_values.categorical_values, None],
         )
         .col_vals_in_set(
-            CQCLClean.contemporary_cssr,
+            IndCQC.contemporary_cssr,
             CatValues.contemporary_cssr_column_values.categorical_values,
         )
         .col_vals_in_set(
-            CQCLClean.contemporary_region,
+            IndCQC.contemporary_region,
             CatValues.contemporary_region_column_values.categorical_values,
         )
         .col_vals_in_set(
-            CQCLClean.current_cssr,
+            IndCQC.current_cssr,
             CatValues.current_cssr_column_values.categorical_values,
         )
         .col_vals_in_set(
-            CQCLClean.current_region,
+            IndCQC.current_region,
             CatValues.current_region_column_values.categorical_values,
         )
         .col_vals_in_set(
-            CQCLClean.current_rural_urban_ind_11,
+            IndCQC.current_rural_urban_ind_11,
             CatValues.current_rui_column_values.categorical_values,
         )
         # catagorical column unique values count matches expected count
         .specially(
             vl.is_unique_count_equal(
-                CQCLClean.primary_service_type,
+                IndCQC.primary_service_type,
                 CatValues.primary_service_type_column_values.count_of_categorical_values,
             ),
-            brief=f"{CQCLClean.primary_service_type} needs to be one of {CatValues.primary_service_type_column_values.categorical_values}",
+            brief=f"{IndCQC.primary_service_type} needs to be one of {CatValues.primary_service_type_column_values.categorical_values}",
         )
         .specially(
             vl.is_unique_count_equal(
-                CQCLClean.primary_service_type_second_level,
+                IndCQC.primary_service_type_second_level,
                 CatValues.primary_service_type_second_level_column_values.count_of_categorical_values,
             ),
-            brief=f"{CQCLClean.primary_service_type_second_level} needs to be one of {CatValues.primary_service_type_second_level_column_values.categorical_values}",
+            brief=f"{IndCQC.primary_service_type_second_level} needs to be one of {CatValues.primary_service_type_second_level_column_values.categorical_values}",
         )
         .specially(
             vl.is_unique_count_equal(
-                CQCLClean.care_home,
+                IndCQC.care_home,
                 CatValues.care_home_column_values.count_of_categorical_values,
             ),
-            brief=f"{CQCLClean.care_home} needs to be one of {CatValues.care_home_column_values.categorical_values}",
+            brief=f"{IndCQC.care_home} needs to be one of {CatValues.care_home_column_values.categorical_values}",
         )
         .specially(
             vl.is_unique_count_equal(
-                CQCLClean.cqc_sector,
+                IndCQC.cqc_sector,
                 CatValues.sector_column_values.count_of_categorical_values,
             ),
-            brief=f"{CQCLClean.cqc_sector} needs to be one of {CatValues.sector_column_values.categorical_values}",
+            brief=f"{IndCQC.cqc_sector} needs to be one of {CatValues.sector_column_values.categorical_values}",
         )
         .specially(
             vl.is_unique_count_equal(
-                CQCLClean.related_location,
+                IndCQC.related_location,
                 CatValues.related_location_column_values.count_of_categorical_values,
             ),
-            brief=f"{CQCLClean.related_location} needs to be one of {CatValues.related_location_column_values.categorical_values}",
+            brief=f"{IndCQC.related_location} needs to be one of {CatValues.related_location_column_values.categorical_values}",
         )
         .specially(
             vl.is_unique_count_equal(
-                CQCLClean.specialism_dementia,
+                IndCQC.specialism_dementia,
                 CatValues.specialism_dementia_column_values.count_of_categorical_values,
             ),
-            brief=f"{CQCLClean.specialism_dementia} needs to be one of {CatValues.specialism_dementia_column_values.categorical_values}",
+            brief=f"{IndCQC.specialism_dementia} needs to be one of {CatValues.specialism_dementia_column_values.categorical_values}",
         )
         .specially(
             vl.is_unique_count_equal(
-                CQCLClean.specialism_learning_disabilities,
+                IndCQC.specialism_learning_disabilities,
                 CatValues.specialism_learning_disabilities_column_values.count_of_categorical_values,
             ),
-            brief=f"{CQCLClean.specialism_learning_disabilities} needs to be one of {CatValues.specialism_learning_disabilities_column_values.categorical_values}",
+            brief=f"{IndCQC.specialism_learning_disabilities} needs to be one of {CatValues.specialism_learning_disabilities_column_values.categorical_values}",
         )
         .specially(
             vl.is_unique_count_equal(
-                CQCLClean.specialism_mental_health,
+                IndCQC.specialism_mental_health,
                 CatValues.specialism_mental_health_column_values.count_of_categorical_values,
             ),
-            brief=f"{CQCLClean.specialism_mental_health} needs to be one of {CatValues.specialism_mental_health_column_values.categorical_values}",
+            brief=f"{IndCQC.specialism_mental_health} needs to be one of {CatValues.specialism_mental_health_column_values.categorical_values}",
         )
         .specially(
             vl.is_unique_count_equal(
-                CQCLClean.dormancy,
+                IndCQC.dormancy,
                 CatValues.dormancy_column_values.count_of_categorical_values,
             ),
-            brief=f"{CQCLClean.dormancy} needs to be null, or one of {CatValues.dormancy_column_values.categorical_values}",
+            brief=f"{IndCQC.dormancy} needs to be null, or one of {CatValues.dormancy_column_values.categorical_values}",
         )
         .specially(
             vl.is_unique_count_equal(
-                CQCLClean.contemporary_cssr,
+                IndCQC.contemporary_cssr,
                 CatValues.contemporary_cssr_column_values.count_of_categorical_values,
             ),
-            brief=f"{CQCLClean.contemporary_cssr} needs to be null, or one of {CatValues.contemporary_cssr_column_values.categorical_values}",
+            brief=f"{IndCQC.contemporary_cssr} needs to be null, or one of {CatValues.contemporary_cssr_column_values.categorical_values}",
         )
         .specially(
             vl.is_unique_count_equal(
-                CQCLClean.contemporary_region,
+                IndCQC.contemporary_region,
                 CatValues.contemporary_region_column_values.count_of_categorical_values,
             ),
-            brief=f"{CQCLClean.contemporary_region} needs to be null, or one of {CatValues.contemporary_region_column_values.categorical_values}",
+            brief=f"{IndCQC.contemporary_region} needs to be null, or one of {CatValues.contemporary_region_column_values.categorical_values}",
         )
         .specially(
             vl.is_unique_count_equal(
-                CQCLClean.current_cssr,
+                IndCQC.current_cssr,
                 CatValues.current_cssr_column_values.count_of_categorical_values,
             ),
-            brief=f"{CQCLClean.current_cssr} needs to be null, or one of {CatValues.current_cssr_column_values.categorical_values}",
+            brief=f"{IndCQC.current_cssr} needs to be null, or one of {CatValues.current_cssr_column_values.categorical_values}",
         )
         .specially(
             vl.is_unique_count_equal(
-                CQCLClean.current_region,
+                IndCQC.current_region,
                 CatValues.current_region_column_values.count_of_categorical_values,
             ),
-            brief=f"{CQCLClean.current_region} needs to be null, or one of {CatValues.current_region_column_values.categorical_values}",
+            brief=f"{IndCQC.current_region} needs to be null, or one of {CatValues.current_region_column_values.categorical_values}",
         )
         .specially(
             vl.is_unique_count_equal(
-                CQCLClean.current_rural_urban_ind_11,
+                IndCQC.current_rural_urban_ind_11,
                 CatValues.current_rui_column_values.count_of_categorical_values,
             ),
-            brief=f"{CQCLClean.current_rural_urban_ind_11} needs to be null, or one of {CatValues.current_rui_column_values.categorical_values}",
+            brief=f"{IndCQC.current_rural_urban_ind_11} needs to be null, or one of {CatValues.current_rui_column_values.categorical_values}",
         )
         # numeric column values are between (inclusive)
         .col_vals_between(Validation.location_id_length, 3, 14)
         .col_vals_between(Validation.provider_id_length, 3, 14)
         # numeric column values are greater than or equal to
-        .col_vals_ge(CQCLClean.number_of_beds, 0, na_pass=True)
+        .col_vals_ge(IndCQC.number_of_beds, 0, na_pass=True)
         .interrogate()
     )
     vl.write_reports(validation, bucket_name, reports_path)
--- projects/_03_independent_cqc/_04_model/utils/validate_models.py
+++ projects/_03_independent_cqc/_04_model/utils/validate_models.py
@@ -6,42 +6,32 @@ from projects._01_ingest.cqc_api.fargate.utils import cleaning_utils as cUtils
 from utils.column_names.cleaned_data_files.cqc_location_cleaned import (
     CqcLocationCleanedColumns as CQCLClean,
 )
+from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCQC
 from utils.column_names.ind_cqc_pipeline_columns import PartitionKeys as Keys
 from utils.column_values.categorical_column_values import (
+    CareHome,
     LocationType,
     RegistrationStatus,
 )
 
 
-def get_expected_row_count_for_validation_full_clean(df: pl.DataFrame) -> int:
+def get_expected_row_count_for_validation_model_01_features_non_res_with_dormancy(
+    df: pl.DataFrame,
+) -> int:
     """
-    Returns the expected row count for validation of a fully cleaned dataset.
-    This function tries to replicate the cleaning process to get the row count.
+    Returns the expected row count for validation of the model_01_features_non_res_with_dormancy dataset.
+    This function tries to replicate the feature creation process to get the row count.
 
     Args:
         df (pl.DataFrame): compare Dataframe to get expect row count from
 
     Returns:
-        int: The expected row count after performing minimum set of cleaning steps.
+        int: The expected row count after performing minimum set of feature creation steps.
     """
-
-    df = column_to_date(df, Keys.import_date, CQCLClean.cqc_location_import_date)
-    df = cUtils.clean_provider_id_column(df)
-    df = cUtils.impute_missing_values(
-        df,
-        [
-            CQCLClean.provider_id,
-            CQCLClean.regulated_activities_offered,
-        ],
-    )
     df = df.filter(
-        pl.col(CQCLClean.type) == LocationType.social_care_identifier,
-        raw_data_adjustments.is_valid_location(),
-        pl.col(CQCLClean.registration_status) == RegistrationStatus.registered,
-        pl.col(CQCLClean.provider_id).is_not_null(),
-        pl.col(CQCLClean.regulated_activities_offered).is_not_null(),
+        pl.col(IndCQC.care_home) == CareHome.care_home,
+        pl.col(IndCQC.dormancy).is_not_null(),
     )
-    df = cUtils.remove_specialist_colleges(df)
     row_count = df.height
 
     return row_count
