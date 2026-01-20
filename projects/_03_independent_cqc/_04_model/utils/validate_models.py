efeea43b6fda38501a8ba4cc08cd6cb826f5d910
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-01-20 09:55:00 +0000

    1286 - Update imports on utils script

--- projects/_03_independent_cqc/_04_model/utils/validate_models.py
+++ projects/_03_independent_cqc/_04_model/utils/validate_models.py
@@ -1,18 +1,7 @@
 import polars as pl
 
-from polars_utils import raw_data_adjustments
-from polars_utils.cleaning_utils import column_to_date
-from projects._01_ingest.cqc_api.fargate.utils import cleaning_utils as cUtils
-from utils.column_names.cleaned_data_files.cqc_location_cleaned import (
-    CqcLocationCleanedColumns as CQCLClean,
-)
 from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCQC
-from utils.column_names.ind_cqc_pipeline_columns import PartitionKeys as Keys
-from utils.column_values.categorical_column_values import (
-    CareHome,
-    LocationType,
-    RegistrationStatus,
-)
+from utils.column_values.categorical_column_values import CareHome
 
 
 def get_expected_row_count_for_validation_model_01_features_non_res_with_dormancy(
