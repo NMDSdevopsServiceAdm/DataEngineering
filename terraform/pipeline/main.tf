49309ee50a8b21285397b7dc2018f96a35a36d78
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-01-26 08:54:04 +0000

    1328 - Format terraform code

--- terraform/pipeline/main.tf
+++ terraform/pipeline/main.tf
@@ -13,8 +13,8 @@ terraform {
 provider "aws" {
   #access_key = var.aws_access_key
   #secret_key = var.aws_secret_key
-  region     = var.region
-  profile    = "non-prod"
+  region  = var.region
+  profile = "non-prod"
 
   default_tags {
     tags = {
@@ -33,10 +33,10 @@ provider "aws" {
 terraform {
   backend "s3" {
     # Bucket defined in ../*.s3.tfbackend
-    key            = "statefiles/workspace=default/terraform.tfstate"
-    region         = "eu-west-2"
-    use_lockfile   = true
-    encrypt        = true
+    key          = "statefiles/workspace=default/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+    encrypt      = true
   }
 }
 
