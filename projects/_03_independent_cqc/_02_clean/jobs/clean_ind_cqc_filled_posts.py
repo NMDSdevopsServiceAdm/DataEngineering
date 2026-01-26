08732a1ca1e658628d5ec8737cb76c87eff2ed56
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2026-01-26 14:18:59 +0000

    1303 - Reinstate forward_fill_latest_known_value

--- projects/_03_independent_cqc/_02_clean/jobs/clean_ind_cqc_filled_posts.py
+++ projects/_03_independent_cqc/_02_clean/jobs/clean_ind_cqc_filled_posts.py
@@ -91,17 +91,17 @@ def main(
 
     locations_df = clean_ascwds_filled_post_outliers(locations_df)
 
-    # locations_df = forward_fill_latest_known_value(
-    #     locations_df,
-    #     IndCQC.ascwds_filled_posts_dedup_clean,
-    #     NumericalValues.number_of_days_to_forward_fill,
-    # )
+    locations_df = forward_fill_latest_known_value(
+        locations_df,
+        IndCQC.ascwds_filled_posts_dedup_clean,
+        NumericalValues.number_of_days_to_forward_fill,
+    )
 
-    # locations_df = forward_fill_latest_known_value(
-    #     locations_df,
-    #     IndCQC.pir_people_directly_employed_dedup,
-    #     NumericalValues.number_of_days_to_forward_fill,
-    # )
+    locations_df = forward_fill_latest_known_value(
+        locations_df,
+        IndCQC.pir_people_directly_employed_dedup,
+        NumericalValues.number_of_days_to_forward_fill,
+    )
 
     locations_df = cUtils.calculate_filled_posts_per_bed_ratio(
         locations_df,
