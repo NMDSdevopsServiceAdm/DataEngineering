e878d8e8c5651db179728ce3d6cdfa5096e49e19
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-01-29 11:14:58 +0000

    1311 - Format code

--- terraform/pipeline/s3.tf
+++ terraform/pipeline/s3.tf
@@ -1,11 +1,11 @@
 module "pipeline_resources" {
-  source                  = "../modules/s3-bucket"
-  bucket_name             = "${local.workspace_prefix}-pipeline-resources"
+  source      = "../modules/s3-bucket"
+  bucket_name = "${local.workspace_prefix}-pipeline-resources"
 }
 
 module "datasets_bucket" {
-  source                  = "../modules/s3-bucket"
-  bucket_name             = "${local.workspace_prefix}-datasets"
+  source      = "../modules/s3-bucket"
+  bucket_name = "${local.workspace_prefix}-datasets"
 }
 
 resource "aws_s3_bucket_policy" "cross_account_access_read_only" {
