defe9472e2002aac279946020525e90d52e909d5
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2026-01-30 10:42:46 +0000

    Reverted NRwD model name

--- projects/_03_independent_cqc/_06_estimate_filled_posts/utils/models/non_res_with_dormancy.py
+++ projects/_03_independent_cqc/_06_estimate_filled_posts/utils/models/non_res_with_dormancy.py
@@ -40,7 +40,7 @@ def model_non_res_with_dormancy(
     locations_df = join_model_predictions(
         locations_df,
         predictions_df,
-        "non_res_with_dormancy_model_EMR_rerun",
+        IndCqc.non_res_with_dormancy_model,
         include_run_id=False,
     )
 
