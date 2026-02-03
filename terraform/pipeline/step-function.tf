823a8ee5b4f0546ea11d58e4336f8875b3edbf1f
AchyutRatkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2026-02-03 16:20:53 +0000

    updated docker file and dockerbake file for sfc-internal jobs

--- terraform/pipeline/step-function.tf
+++ terraform/pipeline/step-function.tf
@@ -147,11 +147,13 @@ resource "aws_sfn_state_machine" "sf_pipelines" {
 
     # ecs tasks
     cqc_api_task_arn               = module.cqc-api.task_arn
+    sfc_internal_task_arn          = module._02_sfc_internal.task_arn
     independent_cqc_task_arn       = module._03_independent_cqc.task_arn
     independent_cqc_model_task_arn = module._03_independent_cqc_model.task_arn
 
     # ecs task security groups
     cqc_api_security_group_id               = module.cqc-api.security_group_id
+    sfc_internal_security_group_id          = module._02_sfc_internal.security_group_id
     independent_cqc_security_group_id       = module._03_independent_cqc.security_group_id
     independent_cqc_model_security_group_id = module._03_independent_cqc_model.security_group_id
 
@@ -307,6 +309,7 @@ resource "aws_iam_policy" "step_function_iam_policy" {
         ],
         "Resource" : [
           module.cqc-api.task_arn,
+          module._02_sfc_internal.task_arn,
           module._03_independent_cqc.task_arn,
           module._03_independent_cqc_model.task_arn,
           aws_ecs_cluster.polars_cluster.arn
@@ -318,6 +321,8 @@ resource "aws_iam_policy" "step_function_iam_policy" {
         Resource = [
           module.cqc-api.task_exc_role_arn,
           module.cqc-api.task_role_arn,
+          module._02_sfc_internal.task_exc_role_arn,
+          module._02_sfc_internal.task_role_arn,
           module._03_independent_cqc.task_exc_role_arn,
           module._03_independent_cqc.task_role_arn,
           module._03_independent_cqc_model.task_exc_role_arn,
