5f31fa8e73205ccf103c50ede635ae6db9aabea0
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-02-05 16:00:05 +0000

    1329 - Remove duplicate definitions

--- terraform/pipeline/locals.tf
+++ terraform/pipeline/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  workspace_prefix           = substr(lower(replace(terraform.workspace, "/[^a-zA-Z0-9]+/", "-")), 0, 30)
-  is_development_environment = local.workspace_prefix != "main"
+  workspace_prefix    = substr(lower(replace(terraform.workspace, "/[^a-zA-Z0-9]+/", "-")), 0, 30)
+  is_main_environment = local.workspace_prefix == "main"
   # Maps StepFunction files in step-functions/dynamic using filenames as keys
   step_functions = tomap({
     for fn in fileset("step-functions/dynamic", "*.json") :
--- terraform/pipeline/s3.tf
+++ terraform/pipeline/s3.tf
@@ -1,18 +1,15 @@
 module "pipeline_resources" {
   source                  = "../modules/s3-bucket"
   bucket_name             = "${local.workspace_prefix}-pipeline-resources"
-  empty_bucket_on_destroy = local.is_development_environment
 }
 
 module "datasets_bucket" {
   source                  = "../modules/s3-bucket"
   bucket_name             = "${local.workspace_prefix}-datasets"
-  empty_bucket_on_destroy = local.is_development_environment
-  enable_versioning       = !local.is_development_environment # only version main
 }
 
 resource "aws_s3_bucket_policy" "cross_account_access_read_only" {
-  count = local.workspace_prefix == "main" ? 1 : 0
+  count = local.is_main_environment ? 0 : 1
 
   bucket = module.datasets_bucket.bucket_name
   policy = file("policy-documents/sfc-main-datasets.cross-account-access.json")
