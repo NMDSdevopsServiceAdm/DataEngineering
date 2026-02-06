9cec08e237fe3ca590edda5f1358e0cabf036508
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-02-06 10:13:11 +0000

    1329 - Refactor outputs

--- terraform/modules/glue-job/outputs.tf
+++ terraform/modules/glue-job/outputs.tf
@@ -1,9 +1,9 @@
 output "job_name" {
-  value       = aws_glue_job.glue_job.name
   description = "The name of a glue job"
+  value       = aws_glue_job.glue_job.name
 }
 
 output "job_arn" {
-  value       = aws_glue_job.glue_job.arn
   description = "The ARN of a glue job"
+  value       = aws_glue_job.glue_job.arn
 }
