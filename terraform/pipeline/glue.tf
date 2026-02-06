99dcbe74a4075f84abf0ecb49da474fcb5024256
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2026-02-06 10:32:13 +0000

    1303 - Removed terraform code for PySpark non res models

--- terraform/pipeline/glue.tf
+++ terraform/pipeline/glue.tf
@@ -208,21 +208,6 @@ module "archive_filled_posts_estimates_job" {
   }
 }
 
-
-module "prepare_features_non_res_ascwds_ind_cqc_job" {
-  source          = "../modules/glue-job"
-  script_dir      = "projects/_03_independent_cqc/_04_feature_engineering/jobs"
-  script_name     = "prepare_features_non_res_ascwds_ind_cqc.py"
-  glue_role       = aws_iam_role.sfc_glue_service_iam_role
-  resource_bucket = module.pipeline_resources
-  datasets_bucket = module.datasets_bucket
-
-  job_parameters = {
-    "--ind_cqc_filled_posts_cleaned_source" = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=ind_cqc_03_imputed_ascwds_and_pir/"
-    "--with_dormancy_features_destination"  = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=ind_cqc_features_non_res_ascwds_with_dormancy/"
-  }
-}
-
 module "clean_ind_cqc_filled_posts_job" {
   source            = "../modules/glue-job"
   script_dir        = "projects/_03_independent_cqc/_02_clean/jobs"
@@ -522,22 +507,6 @@ module "validate_imputed_ind_cqc_ascwds_and_pir_data_job" {
     "--cleaned_ind_cqc_source"                = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=ind_cqc_02_cleaned_data/"
     "--imputed_ind_cqc_ascwds_and_pir_source" = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=ind_cqc_03_imputed_ascwds_and_pir/"
     "--report_destination"                    = "${module.datasets_bucket.bucket_uri}/domain=data_validation_reports/dataset=data_quality_report_ind_cqc_03_imputed_ascwds_and_pir/"
-  }
-}
-
-module "validate_features_non_res_ascwds_with_dormancy_ind_cqc_data_job" {
-  source          = "../modules/glue-job"
-  script_dir      = "projects/_03_independent_cqc/_04_feature_engineering/jobs"
-  script_name     = "validate_features_non_res_ascwds_with_dormancy_ind_cqc_data.py"
-  glue_role       = aws_iam_role.sfc_glue_service_iam_role
-  resource_bucket = module.pipeline_resources
-  datasets_bucket = module.datasets_bucket
-  glue_version    = "5.0"
-
-  job_parameters = {
-    "--cleaned_ind_cqc_source"                               = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=ind_cqc_02_cleaned_data/"
-    "--non_res_ascwds_with_dormancy_ind_cqc_features_source" = "${module.datasets_bucket.bucket_uri}/domain=ind_cqc_filled_posts/dataset=ind_cqc_features_non_res_ascwds_with_dormancy/"
-    "--report_destination"                                   = "${module.datasets_bucket.bucket_uri}/domain=data_validation_reports/dataset=data_quality_report_ind_cqc_features_non_res_ascwds_with_dormancy/"
   }
 }
 
