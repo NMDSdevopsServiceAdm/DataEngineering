c79586a675b2c8052869fda6b48dcac31418e2e5
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-02-06 09:54:33 +0000

    1329 - Separate out core definition blocks

---
+++ terraform/pipeline/terraform.tf
@@ -0,0 +1,12 @@
+terraform {
+    required_version = "1.13.1"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      # This is needed to ensure that we are running the same version of the terraform provider as circleCI
+      # CircleCI will reinitialise with the latest provider version each time, whereas our local terraform would only
+      # update when we pass `-update` explicitly into `terraform init`
+      version = "~> 6.15.0"
+    }
+  }
+}
