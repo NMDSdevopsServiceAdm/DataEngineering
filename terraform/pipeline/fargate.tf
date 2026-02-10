e75c0c246f4a481f51158e4326af55f3e6b37f48
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-02-10 08:49:14 +0000

    1329 - Refactor cluster arn
    Removing cluster arn as a variable (it is not changing in each configuration) allows the cluster definition to move into the module.

--- terraform/pipeline/fargate.tf
+++ terraform/pipeline/fargate.tf
@@ -2,7 +2,6 @@ module "cqc-api" {
   source        = "../modules/fargate-task"
   task_name     = "cqc-api"
   ecr_repo_name = "fargate/cqc"
-  cluster_arn   = aws_ecs_cluster.polars_cluster.arn
   tag_name      = terraform.workspace
   environment = [
     { "name" : "AWS_REGION", "value" : "eu-west-2" },
@@ -14,7 +13,6 @@ module "_03_independent_cqc" {
   source        = "../modules/fargate-task"
   task_name     = "_03_independent_cqc"
   ecr_repo_name = "fargate/03_independent_cqc"
-  cluster_arn   = aws_ecs_cluster.polars_cluster.arn
   environment = [
     { "name" : "AWS_REGION", "value" : "eu-west-2" }
   ]
@@ -25,7 +23,6 @@ module "_03_independent_cqc_model" {
   source        = "../modules/fargate-task"
   task_name     = "_03_independent_cqc_model"
   ecr_repo_name = "fargate/03_independent_cqc_model"
-  cluster_arn   = aws_ecs_cluster.polars_cluster.arn
   tag_name      = terraform.workspace
   cpu_size      = 8192
   ram_size      = 32768
