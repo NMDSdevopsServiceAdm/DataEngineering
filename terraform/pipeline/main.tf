eec6f07140d09f30e4985f887b1da1402cfd7071
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-02-12 15:35:59 +0000

    1329 - Refactor root module according to aws recs

--- terraform/pipeline/main.tf
+++ terraform/pipeline/main.tf
@@ -1,1 +1,23 @@
 data "aws_caller_identity" "current" {}
+
+resource "aws_sns_topic" "pipeline_failures" {
+  name = "${local.workspace_prefix}-pipeline-failures"
+}
+
+resource "aws_ssm_parameter" "providers_last_run" {
+  name  = "/${local.workspace_prefix}/cqc-providers-last-run"
+  type  = "String"
+  value = timeadd(timestamp(), "-24h")
+  lifecycle {
+    ignore_changes = [value, ]
+  }
+}
+
+resource "aws_ssm_parameter" "locations_last_run" {
+  name  = "/${local.workspace_prefix}/cqc-locations-last-run"
+  type  = "String"
+  value = timeadd(timestamp(), "-24h")
+  lifecycle {
+    ignore_changes = [value, ]
+  }
+}
