449fe026f401620486922206df904771cdc58e0b
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-02-13 08:45:41 +0000

    1329 - Move lambda module to main

--- terraform/pipeline/lambda.tf
+++
@@ -1,6 +0,0 @@
-module "error_notification_lambda" {
-  source          = "../modules/lambda-notification"
-  lambda_name     = "error_notifications"
-  resource_bucket = module.pipeline_resources.bucket_name
-  sns_topic_arn   = aws_sns_topic.pipeline_failures.arn
-}
--- terraform/pipeline/main.tf
+++ terraform/pipeline/main.tf
@@ -21,3 +21,10 @@ resource "aws_ssm_parameter" "locations_last_run" {
     ignore_changes = [value, ]
   }
 }
+
+module "error_notification_lambda" {
+  source          = "../modules/lambda-notification"
+  lambda_name     = "error_notifications"
+  resource_bucket = module.pipeline_resources.bucket_name
+  sns_topic_arn   = aws_sns_topic.pipeline_failures.arn
+}
