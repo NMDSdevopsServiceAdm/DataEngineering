17d56a8987ac8f662ef31a61b05d99a8c51219b9
Achyut Ratkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2026-02-13 16:46:59 +0000

    updated docstrings

--- projects/_03_independent_cqc/_02_clean/fargate/utils/utils.py
+++ projects/_03_independent_cqc/_02_clean/fargate/utils/utils.py
@@ -21,7 +21,7 @@ def create_column_with_repeated_values_removed(
     Args:
         lf (pl.LazyFrame): The polars LazyFrame to use
         column_to_clean (str): The name of the column to convert
-        new_column_name (Optional [str] = Null): If not provided, "_deduplicated" will be appended onto the original column name
+        new_column_name (Optional [str]): If not provided, "_deduplicated" will be appended onto the original column name
         column_to_partition_by (str): A column to partition by when deduplicating. Defaults to 'locationid'.
 
     Returns:
