20517e75b709bc2940f2e8c0b77356c776b50c73
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2026-02-16 14:34:40 +0000

    1377 - Added hash to avoid duplication with long script names being identical once cropped

--- terraform/modules/glue-job/iam.tf
+++ terraform/modules/glue-job/iam.tf
@@ -1,7 +1,7 @@
 locals {
-  s3_iam_role_name        = substr("role_${local.job_name}", 0, 63)
-  s3_policy_name          = substr("pol_${local.job_name}", 0, 63)
-  read_raw_s3_policy_name = substr("s3pol_${local.job_name}", 0, 63)
+  glue_job_s3_iam_role_name         = substr("role_${local.job_name}", 0, 63)
+  glue_job_s3_policy_name           = "${substr("policy_${local.job_name}", 0, 58)}-${substr(md5(local.job_name), 0, 4)}"
+  glue_jobs_read_raw_s3_policy_name = "${substr("policy-raw_${local.job_name}", 0, 58)}-${substr(md5(local.job_name), 0, 4)}"
 }
 
 data "aws_iam_policy_document" "glue_service_assume_role_policy" {
@@ -16,7 +16,7 @@ data "aws_iam_policy_document" "glue_service_assume_role_policy" {
 }
 
 resource "aws_iam_role" "sfc_glue_service_iam_role" {
-  name               = local.s3_iam_role_name
+  name               = local.glue_job_s3_iam_role_name
   assume_role_policy = data.aws_iam_policy_document.glue_service_assume_role_policy.json
 }
 
@@ -26,7 +26,7 @@ resource "aws_iam_role_policy_attachment" "AWSGlueServiceRole_policy_attachment"
 }
 
 resource "aws_iam_policy" "glue_job_s3_data_engineering_policy" {
-  name        = local.s3_policy_name
+  name        = local.glue_job_s3_policy_name
   path        = "/"
   description = "Iam policy for branch buckets on workspace: ${local.workspace_prefix}"
 
@@ -57,7 +57,7 @@ resource "aws_iam_role_policy_attachment" "glue_job_s3_policy_attachment" {
 }
 
 resource "aws_iam_policy" "glue_jobs_read_raw_s3_data_policy" {
-  name        = local.read_raw_s3_policy_name
+  name        = local.glue_jobs_read_raw_s3_policy_name
   path        = "/"
   description = "Iam policy for workspace: ${local.workspace_prefix} to read the raw and main data"
 
