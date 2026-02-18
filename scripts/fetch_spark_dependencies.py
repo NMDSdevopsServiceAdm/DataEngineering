bd35d420ea323821840876309217184292ff7e9a
Mitch Edmunds (MadeTech) <mitchell.edmunds@madetech.com>
2026-02-18 15:19:27 +0000

    Update script name to fetch_spark_dependencies.py

--- scripts/fetch_spark_dependencies.py
+++ scripts/fetch_spark_dependencies.py
@@ -1,11 +1,14 @@
 # scripts/fetch_dependencies.py
+from typing import cast
+
 import pydeequ
 from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
     print("Fetching PyDeequ dependencies...")
+    builder = cast(SparkSession.Builder, SparkSession.builder)
     spark = (
-        SparkSession.builder.appName("dependency-fetcher")
+        builder.appName("dependency-fetcher")
         .config("spark.jars.packages", pydeequ.deequ_maven_coord)
         .config("spark.jars.excludes", pydeequ.f2j_maven_coord)
         # Use a tiny memory footprint for the fetcher
