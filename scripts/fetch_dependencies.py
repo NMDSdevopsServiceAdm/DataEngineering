c736331d66107e347e9ae6b6fc23db163c801aa7
Mitch Edmunds (MadeTech) <mitchell.edmunds@madetech.com>
2026-02-18 13:11:26 +0000

    Make sure pydeequ jars are cached before running tests in CI

---
+++ scripts/fetch_dependencies.py
@@ -0,0 +1,16 @@
+# scripts/fetch_dependencies.py
+import pydeequ
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    print("Fetching PyDeequ dependencies...")
+    spark = (
+        SparkSession.builder.appName("dependency-fetcher")
+        .config("spark.jars.packages", pydeequ.deequ_maven_coord)
+        .config("spark.jars.excludes", pydeequ.f2j_maven_coord)
+        # Use a tiny memory footprint for the fetcher
+        .config("spark.driver.memory", "512m")
+        .getOrCreate()
+    )
+    print("Dependencies downloaded successfully.")
+    spark.stop()
