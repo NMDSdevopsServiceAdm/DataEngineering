f5577954a0a46fe8225d30e1ae56091354c5e16f
Mitch Edmunds (MadeTech) <mitchell.edmunds@madetech.com>
2026-02-23 16:11:02 +0000

    Add step into the by job roles pipeline

--- projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/estimate_ind_cqc_filled_posts_by_job_role.py
+++ projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/estimate_ind_cqc_filled_posts_by_job_role.py
@@ -74,6 +74,10 @@ def main(
         estimated_posts_lf, ascwds_job_role_counts_lf
     )
 
+    estimated_job_role_posts_lf = JRUtils.nullify_job_role_count_when_source_not_ascwds(
+        estimated_job_role_posts_lf
+    )
+
     utils.sink_to_parquet(
         lazy_df=estimated_job_role_posts_lf,
         output_path=estimates_by_job_role_destination,
