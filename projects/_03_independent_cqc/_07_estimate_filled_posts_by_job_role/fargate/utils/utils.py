309273cbe6974aa954c5085340fb8391217ef04c
Mitch Edmunds (MadeTech) <mitchell.edmunds@madetech.com>
2026-02-23 16:08:44 +0000

    Move function down

--- projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/utils/utils.py
+++ projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/utils/utils.py
@@ -6,6 +6,36 @@ from utils.column_values.categorical_column_values import (
 )
 
 
+def join_worker_to_estimates_dataframe(
+    estimated_filled_posts_lf: pl.LazyFrame,
+    aggregated_job_roles_per_establishment_lf: pl.LazyFrame,
+) -> pl.LazyFrame:
+    """
+    Join the mainjrid_clean_labels and ascwds_job_role_counts columns from the aggregated worker LazyFrame into the estimated filled post LazyFrame.
+
+    Join as left join where:
+      left = estimated filled post LazyFrame
+      right = aggregated worker LazyFrame
+      where establishment_id matches and ascwds_workplace_import_date == ascwds_worker_import_date.
+
+    Args:
+        estimated_filled_posts_lf (pl.LazyFrame): A LazyFrame containing estimated filled posts per workplace.
+        aggregated_job_roles_per_establishment_lf (pl.LazyFrame): A LazyFrame with job role counts per workplace.
+
+    Returns:
+        pl.LazyFrame: The estimated filled post LazyFrame with columns main_job_role_clean_labelled and ascwds_job_role_counts added.
+    """
+
+    merged_lf = estimated_filled_posts_lf.join(
+        other=aggregated_job_roles_per_establishment_lf,
+        left_on=[IndCQC.establishment_id, IndCQC.ascwds_workplace_import_date],
+        right_on=[IndCQC.establishment_id, IndCQC.ascwds_worker_import_date],
+        how="left",
+    )
+
+    return merged_lf
+
+
 def nullify_job_role_count_when_source_not_ascwds(lf: pl.LazyFrame) -> pl.LazyFrame:
     """
     Set job role counts to NULL when source is not ASCDWS.
@@ -36,33 +66,3 @@ def nullify_job_role_count_when_source_not_ascwds(lf: pl.LazyFrame) -> pl.LazyFr
             ).then(IndCQC.ascwds_job_role_counts)
         )
     )
-
-
-def join_worker_to_estimates_dataframe(
-    estimated_filled_posts_lf: pl.LazyFrame,
-    aggregated_job_roles_per_establishment_lf: pl.LazyFrame,
-) -> pl.LazyFrame:
-    """
-    Join the mainjrid_clean_labels and ascwds_job_role_counts columns from the aggregated worker LazyFrame into the estimated filled post LazyFrame.
-
-    Join as left join where:
-      left = estimated filled post LazyFrame
-      right = aggregated worker LazyFrame
-      where establishment_id matches and ascwds_workplace_import_date == ascwds_worker_import_date.
-
-    Args:
-        estimated_filled_posts_lf (pl.LazyFrame): A LazyFrame containing estimated filled posts per workplace.
-        aggregated_job_roles_per_establishment_lf (pl.LazyFrame): A LazyFrame with job role counts per workplace.
-
-    Returns:
-        pl.LazyFrame: The estimated filled post LazyFrame with columns main_job_role_clean_labelled and ascwds_job_role_counts added.
-    """
-
-    merged_lf = estimated_filled_posts_lf.join(
-        other=aggregated_job_roles_per_establishment_lf,
-        left_on=[IndCQC.establishment_id, IndCQC.ascwds_workplace_import_date],
-        right_on=[IndCQC.establishment_id, IndCQC.ascwds_worker_import_date],
-        how="left",
-    )
-
-    return merged_lf
