c277b1bfe158c478efaf2f649fdaadda3217d18f
Mitch Edmunds (MadeTech) <mitchell.edmunds@madetech.com>
2026-02-25 10:57:53 +0000

    Convert some test data to start

--- projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/tests/fargate/utils/test_utils.py
+++ projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/tests/fargate/utils/test_utils.py
@@ -108,3 +108,87 @@ class TestGetPercentageShare(unittest.TestCase):
         expected_df = pl.DataFrame({"ratios": [None, 0.6, None, 0.4]})
         returned_df = input_df.select(job.get_percentage_share("vals").alias("ratios"))
         pl_testing.assert_frame_equal(returned_df, expected_df)
+
+
+class TestInterpolate:
+    def setUp():
+        input_data = [
+            ("1000", 1000, MainJobRoleLabels.care_worker, 1.0),
+            ("1000", 1000, MainJobRoleLabels.registered_nurse, 1.0),
+            ("1000", 1000, MainJobRoleLabels.senior_care_worker, 1.0),
+            ("1000", 1000, MainJobRoleLabels.senior_management, 1.0),
+            ("1000", 1001, None, None),
+            ("1000", 1002, MainJobRoleLabels.care_worker, 3.0),
+            ("1000", 1002, MainJobRoleLabels.registered_nurse, 3.0),
+            ("1000", 1002, MainJobRoleLabels.senior_care_worker, 3.0),
+            ("1000", 1002, MainJobRoleLabels.senior_management, 3.0),
+        ]
+
+        expected_data = [
+            ("1000", 1000, MainJobRoleLabels.care_worker, 1.0, 1.0),
+            ("1000", 1000, MainJobRoleLabels.registered_nurse, 1.0, 1.0),
+            ("1000", 1000, MainJobRoleLabels.senior_care_worker, 1.0, 1.0),
+            ("1000", 1000, MainJobRoleLabels.senior_management, 1.0, 1.0),
+            ("1000", 1001, MainJobRoleLabels.care_worker, None, 2.0),
+            ("1000", 1001, MainJobRoleLabels.registered_nurse, None, 2.0),
+            ("1000", 1001, MainJobRoleLabels.senior_care_worker, None, 2.0),
+            ("1000", 1001, MainJobRoleLabels.senior_management, None, 2.0),
+            ("1000", 1002, MainJobRoleLabels.care_worker, 3.0, 3.0),
+            ("1000", 1002, MainJobRoleLabels.registered_nurse, 3.0, 3.0),
+            ("1000", 1002, MainJobRoleLabels.senior_care_worker, 3.0, 3.0),
+            ("1000", 1002, MainJobRoleLabels.senior_management, 3.0, 3.0),
+        ]
+        input_schema = {
+            IndCQC.location_id: pl.String,
+            IndCQC.unix_time: pl.Int32,
+            IndCQC.main_job_role_clean_labelled: pl.String,
+            IndCQC.ascwds_job_role_counts: pl.Int64,
+        }
+        expected_schema = input_schema.copy()
+        expected_schema.update({IndCQC.ascwds_job_role_ratios_interpolated: pl.Int64})
+
+        input_data2 = [
+            ("1000", 1000, None, None),
+            ("1000", 1001, MainJobRoleLabels.care_worker, 2.0),
+            ("1000", 1001, MainJobRoleLabels.registered_nurse, 4.0),
+            ("1000", 1001, MainJobRoleLabels.senior_care_worker, 6.0),
+            ("1000", 1001, MainJobRoleLabels.senior_management, 8.0),
+            ("1000", 1002, None, None),
+            ("1000", 1003, MainJobRoleLabels.care_worker, 0.0),
+            ("1000", 1003, MainJobRoleLabels.registered_nurse, 2.0),
+            ("1000", 1003, MainJobRoleLabels.senior_care_worker, 4.0),
+            ("1000", 1003, MainJobRoleLabels.senior_management, 5.0),
+            ("1000", 1004, MainJobRoleLabels.care_worker, 1.0),
+            ("1000", 1004, MainJobRoleLabels.registered_nurse, 1.0),
+            ("1000", 1004, MainJobRoleLabels.senior_care_worker, 1.0),
+            ("1000", 1004, MainJobRoleLabels.senior_management, 1.0),
+            ("1000", 1005, MainJobRoleLabels.care_worker, 5.0),
+            ("1000", 1005, MainJobRoleLabels.registered_nurse, 4.0),
+            ("1000", 1005, MainJobRoleLabels.senior_care_worker, 3.0),
+            ("1000", 1005, MainJobRoleLabels.senior_management, 2.0),
+            ("1000", 1006, None, None),
+        ]
+        expected_data2 = [
+            ("1000", 1000, None, None, None),
+            ("1000", 1001, MainJobRoleLabels.care_worker, 2.0, 2.0),
+            ("1000", 1001, MainJobRoleLabels.registered_nurse, 4.0, 4.0),
+            ("1000", 1001, MainJobRoleLabels.senior_care_worker, 6.0, 6.0),
+            ("1000", 1001, MainJobRoleLabels.senior_management, 8.0, 8.0),
+            ("1000", 1002, MainJobRoleLabels.care_worker, None, 1.0),
+            ("1000", 1002, MainJobRoleLabels.registered_nurse, None, 3.0),
+            ("1000", 1002, MainJobRoleLabels.senior_care_worker, None, 5.0),
+            ("1000", 1002, MainJobRoleLabels.senior_management, None, 6.5),
+            ("1000", 1003, MainJobRoleLabels.care_worker, 0.0, 0.0),
+            ("1000", 1003, MainJobRoleLabels.registered_nurse, 2.0, 2.0),
+            ("1000", 1003, MainJobRoleLabels.senior_care_worker, 4.0, 4.0),
+            ("1000", 1003, MainJobRoleLabels.senior_management, 5.0, 5.0),
+            ("1000", 1004, MainJobRoleLabels.care_worker, 1.0, 1.0),
+            ("1000", 1004, MainJobRoleLabels.registered_nurse, 1.0, 1.0),
+            ("1000", 1004, MainJobRoleLabels.senior_care_worker, 1.0, 1.0),
+            ("1000", 1004, MainJobRoleLabels.senior_management, 1.0, 1.0),
+            ("1000", 1005, MainJobRoleLabels.care_worker, 5.0, 5.0),
+            ("1000", 1005, MainJobRoleLabels.registered_nurse, 4.0, 4.0),
+            ("1000", 1005, MainJobRoleLabels.senior_care_worker, 3.0, 3.0),
+            ("1000", 1005, MainJobRoleLabels.senior_management, 2.0, 2.0),
+            ("1000", 1006, None, None),
+        ]
