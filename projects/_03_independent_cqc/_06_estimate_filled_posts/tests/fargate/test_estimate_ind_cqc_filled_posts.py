00936cf39424a571957dc8e68ad2b472c8712fa5
Roy Price <Roy.price1988@gmail.com>
2026-03-17 16:47:49 +0000

    1455 - Added test script for polars estimates job

---
+++ projects/_03_independent_cqc/_06_estimate_filled_posts/tests/fargate/test_estimate_ind_cqc_filled_posts.py
@@ -0,0 +1,41 @@
+import unittest
+from unittest.mock import ANY, Mock, patch
+
+import projects._03_independent_cqc._06_estimate_filled_posts.fargate.estimate_ind_cqc_filled_posts as job
+from utils.column_names.ind_cqc_pipeline_columns import PartitionKeys as Keys
+
+PATCH_PATH = "projects._03_independent_cqc._06_estimate_filled_posts.fargate.estimate_ind_cqc_filled_posts"
+
+
+class EstimateIndCQCFilledPostsTests(unittest.TestCase):
+    TEST_BUCKET_NAME = "some/bucket/name"
+    TEST_IMPUTED_IND_CQC_DATA_SOURCE = "some/s3/uri"
+    TEST_DESTINATION = "some/other/s3/uri"
+    partition_keys = [Keys.year, Keys.month, Keys.day, Keys.import_date]
+
+    mock_data = Mock(name="data")
+
+    @patch(f"{PATCH_PATH}.utils.sink_to_parquet")
+    @patch(f"{PATCH_PATH}.utils.scan_parquet", return_value=mock_data)
+    def test_main_runs_successfully(
+        self,
+        scan_parquet_mock: Mock,
+        sink_to_parquet_mock: Mock,
+    ):
+
+        job.main(
+            self.TEST_BUCKET_NAME,
+            self.TEST_IMPUTED_IND_CQC_DATA_SOURCE,
+            self.TEST_DESTINATION,
+        )
+
+        scan_parquet_mock.assert_called_once_with(
+            source=ANY,
+            selected_columns=job.ind_cqc_columns,
+        )
+        sink_to_parquet_mock.assert_called_once_with(
+            ANY,
+            self.TEST_DESTINATION,
+            partition_cols=self.partition_keys,
+            append=False,
+        )
