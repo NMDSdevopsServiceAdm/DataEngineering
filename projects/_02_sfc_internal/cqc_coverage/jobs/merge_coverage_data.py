f78dbf3d34bda548c6d979be3f01551749443cce
Roy Price <Roy.price1988@gmail.com>
2026-03-24 17:51:02 +0000

    1191 - Removed duplicate column name from list of cols to import

--- projects/_02_sfc_internal/cqc_coverage/jobs/merge_coverage_data.py
+++ projects/_02_sfc_internal/cqc_coverage/jobs/merge_coverage_data.py
@@ -78,7 +78,6 @@ cleaned_ascwds_workplace_columns_to_import = [
     AWPClean.master_update_date_org,
     AWPClean.establishment_created_date,
     AWPClean.nmds_id,
-    AWPClean.last_logged_in_date,
     AWPClean.is_parent,
     AWPClean.parent_permission,
     AWPClean.last_logged_in_date,
