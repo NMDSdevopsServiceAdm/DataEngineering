a772365535cdd578d1b62adefd7eb6209e307926
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2026-04-02 13:19:43 +0100

    1477 - Separated schema so `import_date` isn't in cleaned test data

--- projects/_01_ingest/unittest_data/ingest_test_file_schemas.py
+++ projects/_01_ingest/unittest_data/ingest_test_file_schemas.py
@@ -403,7 +403,13 @@ class ValidateCleanedCapacityTrackerCareHomeData:
     )
     cleaned_ct_care_home_schema = StructType(
         [
-            *ct_care_home_schema,
+            StructField(CTCH.cqc_id, StringType(), True),
+            StructField(CTCH.nurses_employed, StringType(), True),
+            StructField(CTCH.care_workers_employed, StringType(), True),
+            StructField(CTCH.non_care_workers_employed, StringType(), True),
+            StructField(CTCH.agency_nurses_employed, StringType(), True),
+            StructField(CTCH.agency_care_workers_employed, StringType(), True),
+            StructField(CTCH.agency_non_care_workers_employed, StringType(), True),
             StructField(CTCHClean.ct_care_home_import_date, DateType(), True),
             StructField(CTCHClean.non_agency_total_employed, IntegerType(), True),
             StructField(CTCHClean.agency_total_employed, IntegerType(), True),
