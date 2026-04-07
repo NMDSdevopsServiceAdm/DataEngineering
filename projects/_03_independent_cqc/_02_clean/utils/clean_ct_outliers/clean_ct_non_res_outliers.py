ed27b85b074cc7e8ec7e271b77dbcca11c276226
Roy Price <Roy.price1988@gmail.com>
2026-04-07 15:28:39 +0100

    1497 - Changed ct non res longitudinal outlier proportion to filter from 0.001 to 0.05

--- projects/_03_independent_cqc/_02_clean/utils/clean_ct_outliers/clean_ct_non_res_outliers.py
+++ projects/_03_independent_cqc/_02_clean/utils/clean_ct_outliers/clean_ct_non_res_outliers.py
@@ -57,7 +57,7 @@ def clean_capacity_tracker_non_res_outliers(df: DataFrame) -> DataFrame:
         df=df,
         col_to_clean=IndCQC.ct_non_res_care_workers_employed,
         cleaned_column_name=IndCQC.ct_non_res_care_workers_employed_cleaned,
-        proportion_to_filter=0.001,
+        proportion_to_filter=0.05,
         care_home=False,
     )
 
