7462991dd8889733dc5c55fab919aaca23c2c19c
Achyut Ratkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2026-04-23 16:38:24 +0100

    Updated tests and test data

--- projects/_04_direct_payment_recipients/fargate/utils/models/interpolation.py
+++ projects/_04_direct_payment_recipients/fargate/utils/models/interpolation.py
@@ -1,5 +1,4 @@
 import polars as pl
-import sys
 
 from projects._04_direct_payment_recipients.direct_payments_column_names import (
     DirectPaymentColumnNames as DP,
@@ -22,18 +21,18 @@ def model_interpolation(
     )
 
     # calculate first and last submission year per la area
+    first_submission_expr = (
+        pl.col(DP.YEAR_AS_INTEGER).min().cast(pl.Int32).alias(DP.FIRST_SUBMISSION_YEAR)
+    )
+    last_submission_expr = (
+        pl.col(DP.YEAR_AS_INTEGER).max().cast(pl.Int32).alias(DP.LAST_SUBMISSION_YEAR)
+    )
     first_and_last_submission_year_lf = known_service_users_employing_staff_lf.group_by(
         DP.LA_AREA
     ).agg(
         [
-            pl.col(DP.YEAR_AS_INTEGER)
-            .min()
-            .cast(pl.Int32)
-            .alias(DP.FIRST_SUBMISSION_YEAR),
-            pl.col(DP.YEAR_AS_INTEGER)
-            .max()
-            .cast(pl.Int32)
-            .alias(DP.LAST_SUBMISSION_YEAR),
+            first_submission_expr,
+            last_submission_expr,
         ]
     )
 
@@ -43,7 +42,7 @@ def model_interpolation(
         first_and_last_submission_year_lf.with_columns(
             pl.int_ranges(
                 pl.col(DP.FIRST_SUBMISSION_YEAR),
-                pl.col(DP.LAST_SUBMISSION_YEAR) + 1,  # inclusive
+                pl.col(DP.LAST_SUBMISSION_YEAR) + 1,
             ).alias(DP.INTERPOLATION_YEAR)
         )
         .explode(DP.INTERPOLATION_YEAR)
@@ -80,99 +79,7 @@ def model_interpolation(
     return direct_payments_lf
 
 
-# def filter_to_locations_with_known_service_users_employing_staff(
-#     df: DataFrame,
-# ) -> DataFrame:
-#     df = df.select(
-#         DP.LA_AREA,
-#         DP.YEAR_AS_INTEGER,
-#         DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
-#     )
-#     df = df.where(
-#         F.col(DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF).isNotNull()
-#     )
-#     return df
-
-
-# def calculate_first_and_last_submission_year_per_la_area(
-#     df: DataFrame,
-# ) -> DataFrame:
-#     df = df.groupBy(DP.LA_AREA).agg(
-#         F.min(DP.YEAR_AS_INTEGER).cast("integer").alias(DP.FIRST_SUBMISSION_YEAR),
-#         F.max(DP.YEAR_AS_INTEGER).cast("integer").alias(DP.LAST_SUBMISSION_YEAR),
-#     )
-#     return df
-
-
-# def convert_first_and_last_known_years_into_exploded_df(
-#     df: DataFrame,
-# ) -> DataFrame:
-#     create_list_of_equally_spaced_points_between_start_and_finish_years_udf = F.udf(
-#         create_list_of_equally_spaced_points_between_start_and_finish_years,
-#         ArrayType(LongType()),
-#     )
-#     df = df.withColumn(
-#         DP.INTERPOLATION_YEAR,
-#         F.explode(
-#             create_list_of_equally_spaced_points_between_start_and_finish_years_udf(
-#                 DP.FIRST_SUBMISSION_YEAR, DP.LAST_SUBMISSION_YEAR
-#             )
-#         ),
-#     )
-
-#     df = df.drop(DP.FIRST_SUBMISSION_YEAR, DP.LAST_SUBMISSION_YEAR)
-#     return df
-
-
-# def create_list_of_equally_spaced_points_between_start_and_finish_years(
-#     start_year: int, finish_year: int
-# ) -> int:
-#     year_step = 1
-#     years = range(int((finish_year - start_year) / year_step) + 1)
-#     array_of_years = [start_year + year_step * year for year in years]
-#     return array_of_years
-
-
-# def merge_known_values_with_exploded_dates(
-#     all_dates_df: DataFrame, known_service_users_employing_staff_df: DataFrame
-# ) -> DataFrame:
-#     all_dates_df = all_dates_df.withColumnRenamed(
-#         DP.INTERPOLATION_YEAR, DP.YEAR_AS_INTEGER
-#     )
-#     merged_df = all_dates_df.join(
-#         known_service_users_employing_staff_df, [DP.LA_AREA, DP.YEAR_AS_INTEGER], "left"
-#     )
-
-#     merged_df = add_year_with_data_for_known_service_users_employing_staff(merged_df)
-#     return merged_df
-
-
-# def add_year_with_data_for_known_service_users_employing_staff(
-#     df: DataFrame,
-# ) -> DataFrame:
-#     df = df.withColumn(
-#         DP.estimated_proportion_of_service_users_employing_staff_year_provided,
-#         F.when(
-#             (
-#                 F.col(
-#                     DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF
-#                 ).isNotNull()
-#             ),
-#             F.col(DP.YEAR_AS_INTEGER),
-#         ).otherwise(F.lit(None)),
-#     )
-#     return df
-
-
 def interpolate_values_for_all_dates(lf: pl.LazyFrame) -> pl.LazyFrame:
-    lf = input_previous_and_next_values_into_df(lf)
-    lf = calculated_interpolated_values_in_new_column(
-        lf, DP.ESTIMATE_USING_INTERPOLATION
-    )
-    return lf
-
-
-def input_previous_and_next_values_into_df(lf: pl.LazyFrame) -> pl.LazyFrame:
     lf = get_previous_value_in_column(
         lf,
         DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
@@ -193,6 +100,9 @@ def input_previous_and_next_values_into_df(lf: pl.LazyFrame) -> pl.LazyFrame:
         DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED,
         DP.NEXT_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED,
     )
+    lf = calculated_interpolated_values_in_new_column(
+        lf, DP.ESTIMATE_USING_INTERPOLATION
+    )
     return lf
 
 
@@ -200,10 +110,7 @@ def get_previous_value_in_column(
     lf: pl.LazyFrame, column_name: str, new_column_name: str
 ) -> pl.LazyFrame:
     return lf.sort([DP.LA_AREA, DP.YEAR_AS_INTEGER]).with_columns(
-        pl.col(column_name)
-        .forward_fill()  # last non-null up to current row
-        .over(DP.LA_AREA)
-        .alias(new_column_name)
+        pl.col(column_name).forward_fill().over(DP.LA_AREA).alias(new_column_name)
     )
 
 
@@ -211,10 +118,7 @@ def get_next_value_in_new_column(
     lf: pl.LazyFrame, column_name: str, new_column_name: str
 ) -> pl.LazyFrame:
     return lf.sort([DP.LA_AREA, DP.YEAR_AS_INTEGER]).with_columns(
-        pl.col(column_name)
-        .backward_fill()  # first non-null from current row onward
-        .over(DP.LA_AREA)
-        .alias(new_column_name)
+        pl.col(column_name).backward_fill().over(DP.LA_AREA).alias(new_column_name)
     )
 
 
--- projects/_04_direct_payment_recipients/tests/fargate/utils/models/test_interpolation.py
+++ projects/_04_direct_payment_recipients/tests/fargate/utils/models/test_interpolation.py
@@ -1,8 +1,6 @@
 import unittest
-from unittest.mock import Mock, patch
 import polars as pl
 import polars.testing as pl_testing
-import pytest
 
 import projects._04_direct_payment_recipients.fargate.utils.models.interpolation as job
 from projects._04_direct_payment_recipients.direct_payments_column_names import (
@@ -13,19 +11,18 @@ PATCH_PATH = "projects._04_direct_payment_recipients.fargate.utils.models.interp
 
 
 class TestDPRModelInterpolation(unittest.TestCase):
-    def test_model_interpolation_completes(self):
+    def test_function_retuns_expected_values(self):
         rows = [
-            ("area_1", 2021, 300.0, 0.3, 0.3),
-            ("area_2", 2021, 300.0, 0.4, 0.4),
-            ("area_1", 2020, None, None, 0.3375),
-            ("area_2", 2020, 300.0, 0.35, 0.35),
-            ("area_1", 2019, 300.0, 0.375, 0.375),
-            ("area_2", 2019, 300.0, 0.2, 0.2),
+            ("area_1", 2021, 0.3, 0.3),
+            ("area_2", 2021, 0.4, 0.4),
+            ("area_1", 2020, None, 0.3375),
+            ("area_2", 2020, 0.35, 0.35),
+            ("area_1", 2019, 0.375, 0.375),
+            ("area_2", 2019, 0.2, 0.2),
         ]
         test_schema = {
             DP.LA_AREA: pl.String,
             DP.YEAR_AS_INTEGER: pl.Int64,
-            DP.ESTIMATED_SERVICE_USER_DPRS_DURING_YEAR_EMPLOYING_STAFF: pl.Float64,
             DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF: pl.Float64,
             DP.ESTIMATE_USING_INTERPOLATION: pl.Float64,
         }
@@ -34,231 +31,9 @@ class TestDPRModelInterpolation(unittest.TestCase):
         returned_lf = job.model_interpolation(test_lf)
         pl_testing.assert_frame_equal(returned_lf, expected_lf, check_row_order=False)
 
-    # def test_filter_to_locations_with_known_service_users_employing_staff_filters_correctly(
-    #     self,
-    # ):
-    #     rows = [
-    #         ("area_1", 2021, 300.0, 0.3),
-    #         ("area_2", 2021, 300.0, 0.4),
-    #         ("area_1", 2020, None, None),
-    #         ("area_2", 2020, 300.0, 0.35),
-    #         ("area_1", 2019, 300.0, 0.375),
-    #         ("area_2", 2019, 300.0, 0.2),
-    #     ]
-    #     test_schema = StructType(
-    #         [
-    #             DP.LA_AREA: pl.String,
-    #             DP.YEAR_AS_INTEGER, IntegerType(), True),
 
-    #                 DP.ESTIMATED_SERVICE_USER_DPRS_DURING_YEAR_EMPLOYING_STAFF,
-    #                 FloatType(),
-    #                 True,
-    #             ),
-
-    #                 DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
-    #                 FloatType(),
-    #                 True,
-    #             ),
-    #         ]
-    #     )
-    #     df = self.spark.createDataFrame(rows, schema=test_schema)
-    #     output_df = job.filter_to_locations_with_known_service_users_employing_staff(df)
-
-    #     self.assertEqual(output_df.count(), 5)
-    #     self.assertEqual(
-    #         output_df.columns,
-    #         [
-    #             DP.LA_AREA,
-    #             DP.YEAR_AS_INTEGER,
-    #             DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
-    #         ],
-    #     )
-
-    # def test_calculate_first_and_last_submission_date_per_location(self):
-    #     rows = [
-    #         ("area_1", 2021, 0.5),
-    #         ("area_1", 2019, 0.5),
-    #         ("area_2", 2021, 0.5),
-    #         ("area_2", 2020, 0.5),
-    #         ("area_2", 2019, 0.5),
-    #     ]
-    #     test_schema = StructType(
-    #         [
-    #             DP.LA_AREA, StringType(), False),
-    #             DP.YEAR_AS_INTEGER, IntegerType(), True),
-
-    #                 DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
-    #                 FloatType(),
-    #                 True,
-    #             ),
-    #         ]
-    #     )
-    #     df = self.spark.createDataFrame(rows, schema=test_schema)
-    #     output_df = job.calculate_first_and_last_submission_year_per_la_area(df)
-
-    #     self.assertEqual(output_df.count(), 2)
-    #     self.assertEqual(
-    #         output_df.columns,
-    #         [DP.LA_AREA, DP.FIRST_SUBMISSION_YEAR, DP.LAST_SUBMISSION_YEAR],
-    #     )
-
-    #     output_df_list = output_df.sort(DP.LA_AREA).collect()
-    #     self.assertEqual(output_df_list[0][DP.FIRST_SUBMISSION_YEAR], 2019)
-    #     self.assertEqual(output_df_list[0][DP.LAST_SUBMISSION_YEAR], 2021)
-    #     self.assertEqual(output_df_list[1][DP.FIRST_SUBMISSION_YEAR], 2019)
-    #     self.assertEqual(output_df_list[1][DP.LAST_SUBMISSION_YEAR], 2021)
-
-    # def test_convert_first_and_last_known_time_into_timeseries_df(self):
-    #     rows = [
-    #         ("area_1", 2019, 2021),
-    #         ("area_2", 2019, 2021),
-    #     ]
-    #     test_schema = StructType(
-    #         [
-    #             DP.LA_AREA, StringType(), False),
-    #             DP.FIRST_SUBMISSION_YEAR, IntegerType(), True),
-
-    #                 DP.LAST_SUBMISSION_YEAR,
-    #                 IntegerType(),
-    #                 True,
-    #             ),
-    #         ]
-    #     )
-    #     df = self.spark.createDataFrame(rows, schema=test_schema)
-    #     output_df = job.convert_first_and_last_known_years_into_exploded_df(df)
-    #     output_df_list = output_df.sort(DP.LA_AREA, DP.INTERPOLATION_YEAR).collect()
-
-    #     self.assertEqual(output_df.count(), 6)
-    #     self.assertEqual(
-    #         output_df.columns,
-    #         [DP.LA_AREA, DP.INTERPOLATION_YEAR],
-    #     )
-
-    #     self.assertEqual(output_df_list[0][DP.INTERPOLATION_YEAR], 2019)
-    #     self.assertEqual(output_df_list[1][DP.INTERPOLATION_YEAR], 2020)
-    #     self.assertEqual(output_df_list[2][DP.INTERPOLATION_YEAR], 2021)
-    #     self.assertEqual(output_df_list[3][DP.INTERPOLATION_YEAR], 2019)
-    #     self.assertEqual(output_df_list[4][DP.INTERPOLATION_YEAR], 2020)
-    #     self.assertEqual(output_df_list[5][DP.INTERPOLATION_YEAR], 2021)
-
-    # def test_merge_known_values_with_exploded_dates_returns_correct_values(self):
-    #     all_dates_rows = [
-    #         ("area_1", 2021),
-    #         ("area_2", 2021),
-    #         ("area_1", 2020),
-    #         ("area_2", 2020),
-    #         ("area_1", 2019),
-    #         ("area_2", 2019),
-    #     ]
-    #     all_dates_test_schema = StructType(
-    #         [
-    #             DP.LA_AREA, StringType(), False),
-    #             DP.INTERPOLATION_YEAR, IntegerType(), True),
-    #         ]
-    #     )
-    #     all_dates_df = self.spark.createDataFrame(
-    #         all_dates_rows, schema=all_dates_test_schema
-    #     )
-
-    #     known_values_rows = [
-    #         ("area_1", 2021, 0.5),
-    #         ("area_1", 2019, 0.5),
-    #         ("area_2", 2021, 0.5),
-    #         ("area_2", 2020, 0.5),
-    #         ("area_2", 2019, 0.5),
-    #     ]
-    #     known_values_test_schema = StructType(
-    #         [
-    #             DP.LA_AREA, StringType(), False),
-    #             DP.YEAR_AS_INTEGER, IntegerType(), True),
-
-    #                 DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
-    #                 FloatType(),
-    #                 True,
-    #             ),
-    #         ]
-    #     )
-    #     known_service_users_employing_staff_df = self.spark.createDataFrame(
-    #         known_values_rows, schema=known_values_test_schema
-    #     )
-
-    #     output_df = job.merge_known_values_with_exploded_dates(
-    #         all_dates_df, known_service_users_employing_staff_df
-    #     )
-    #     output_df_list = output_df.sort(DP.LA_AREA, DP.YEAR_AS_INTEGER).collect()
-    #     self.assertEqual(output_df.count(), 6)
-    #     self.assertEqual(
-    #         output_df.columns,
-    #         [
-    #             DP.LA_AREA,
-    #             DP.YEAR_AS_INTEGER,
-    #             DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
-    #             DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED,
-    #         ],
-    #     )
-
-    #     self.assertEqual(
-    #         output_df_list[0][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF],
-    #         0.5,
-    #     )
-    #     self.assertEqual(
-    #         output_df_list[1][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF],
-    #         None,
-    #     )
-    #     self.assertEqual(
-    #         output_df_list[2][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF],
-    #         0.5,
-    #     )
-    #     self.assertEqual(
-    #         output_df_list[3][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF],
-    #         0.5,
-    #     )
-    #     self.assertEqual(
-    #         output_df_list[4][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF],
-    #         0.5,
-    #     )
-    #     self.assertEqual(
-    #         output_df_list[5][DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF],
-    #         0.5,
-    #     )
-    #     self.assertEqual(
-    #         output_df_list[0][
-    #             DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED
-    #         ],
-    #         2019,
-    #     )
-    #     self.assertEqual(
-    #         output_df_list[1][
-    #             DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED
-    #         ],
-    #         None,
-    #     )
-    #     self.assertEqual(
-    #         output_df_list[2][
-    #             DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED
-    #         ],
-    #         2021,
-    #     )
-    #     self.assertEqual(
-    #         output_df_list[3][
-    #             DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED
-    #         ],
-    #         2019,
-    #     )
-    #     self.assertEqual(
-    #         output_df_list[4][
-    #             DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED
-    #         ],
-    #         2020,
-    #     )
-    #     self.assertEqual(
-    #         output_df_list[5][
-    #             DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED
-    #         ],
-    #         2021,
-    #     )
-
-    def test_interpolate_values_for_all_dates_returns_interpolated_values(self):
+class TestInterpolateValuesForAllDates(unittest.TestCase):
+    def test_function_retuns_expected_values(self):
         rows = [
             ("area_1", 2019, 0.5, 2019, 0.5),
             ("area_1", 2020, None, None, 0.5),
@@ -283,3 +58,97 @@ class TestDPRModelInterpolation(unittest.TestCase):
         )
         returned_lf = job.interpolate_values_for_all_dates(test_lf)
         pl_testing.assert_frame_equal(returned_lf, expected_lf, check_row_order=False)
+
+
+class TestGetPreviousValueInColumn(unittest.TestCase):
+    def test_function_retuns_expected_values(self):
+        rows = [
+            ("area_1", 2019, 2019, 2019),
+            ("area_1", 2020, None, 2019),
+            ("area_1", 2021, 2021, 2021),
+            ("area_2", 2019, 2019, 2019),
+            ("area_2", 2020, None, 2019),
+            ("area_2", 2021, 2021, 2021),
+        ]
+        test_schema = {
+            DP.LA_AREA: pl.String,
+            DP.YEAR_AS_INTEGER: pl.Int64,
+            DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED: pl.Int64,
+            DP.PREVIOUS_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED: pl.Int64,
+        }
+
+        expected_lf = pl.LazyFrame(rows, test_schema, orient="row")
+        test_lf = expected_lf.drop(
+            DP.PREVIOUS_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED
+        )
+        returned_lf = job.get_previous_value_in_column(
+            test_lf,
+            DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED,
+            DP.PREVIOUS_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED,
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
+
+
+class TestGetNextValueInColumn(unittest.TestCase):
+    def test_function_retuns_expected_values(self):
+        rows = [
+            ("area_1", 2019, 2019, 2019),
+            ("area_1", 2020, None, 2021),
+            ("area_1", 2021, 2021, 2021),
+            ("area_2", 2019, 2019, 2019),
+            ("area_2", 2020, None, 2021),
+            ("area_2", 2021, 2021, 2021),
+        ]
+        test_schema = {
+            DP.LA_AREA: pl.String,
+            DP.YEAR_AS_INTEGER: pl.Int64,
+            DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED: pl.Int64,
+            DP.NEXT_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED: pl.Int64,
+        }
+
+        expected_lf = pl.LazyFrame(rows, test_schema, orient="row")
+        test_lf = expected_lf.drop(
+            DP.NEXT_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED
+        )
+        returned_lf = job.get_next_value_in_new_column(
+            test_lf,
+            DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED,
+            DP.NEXT_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED,
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
+
+
+class TestCalculatedInterpolatedValuesInNewColumn(unittest.TestCase):
+    def test_function_retuns_expected_values(self):
+        rows = [
+            ("area_1", 2019, 10, 10, 10, 2019, 2019, 10.0),
+            ("area_1", 2020, 15, 10, 20, 2019, 2021, 15.0),
+            ("area_1", 2021, 20, 20, 20, 2021, 2021, 20.0),
+            ("area_2", 2019, 5, 5, 5, 2019, 2019, 5.0),
+            ("area_2", 2020, 10, 5, 15, 2019, 2021, 10.0),
+            ("area_2", 2021, 15, 15, 15, 2021, 2021, 15.0),
+        ]
+        test_schema = {
+            DP.LA_AREA: pl.String,
+            DP.YEAR_AS_INTEGER: pl.Int64,
+            DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF: pl.Int64,
+            DP.PREVIOUS_SERVICE_USERS_EMPLOYING_STAFF: pl.Int64,
+            DP.NEXT_SERVICE_USERS_EMPLOYING_STAFF: pl.Int64,
+            DP.PREVIOUS_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED: pl.Int64,
+            DP.NEXT_ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF_YEAR_PROVIDED: pl.Int64,
+            DP.ESTIMATE_USING_INTERPOLATION: pl.Float64,
+        }
+
+        base_lf = pl.LazyFrame(rows, test_schema, orient="row")
+        test_lf = base_lf.drop(DP.ESTIMATE_USING_INTERPOLATION)
+        expected_lf = base_lf.select(
+            DP.LA_AREA, DP.YEAR_AS_INTEGER, DP.ESTIMATE_USING_INTERPOLATION
+        )
+        returned_lf = job.calculated_interpolated_values_in_new_column(
+            test_lf,
+            DP.ESTIMATE_USING_INTERPOLATION,
+        )
+
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
