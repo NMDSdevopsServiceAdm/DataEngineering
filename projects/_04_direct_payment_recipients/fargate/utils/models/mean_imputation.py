aa2f049dbcdcabcc474d84186606329d117db0e5
Roy Price <Roy.price1988@gmail.com>
2026-04-23 15:10:48 +0100

    1553 - returned with_columns instead of assigning and returning. Also added doc string.

--- projects/_04_direct_payment_recipients/fargate/utils/models/mean_imputation.py
+++ projects/_04_direct_payment_recipients/fargate/utils/models/mean_imputation.py
@@ -6,12 +6,21 @@ from projects._04_direct_payment_recipients.direct_payments_column_names import 
 
 
 def model_using_mean(lf: pl.LazyFrame) -> pl.LazyFrame:
+    """
+    Adds a column 'ESTIMATE_USING_MEAN' which is the mean
+    'PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF' per 'YEAR_AS_INTEGER'.
+
+    Args:
+        lf (pl.LazyFrame): A LazyFrame with columns
+            'PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF' and 'YEAR_AS_INTEGER'.
+
+    Returns:
+        pl.LazyFrame: A LazyFrame with new column 'ESTIMATE_USING_MEAN'.
+    """
     mean_expression = pl.mean(DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF).over(
         DP.YEAR_AS_INTEGER
     )
 
-    lf = lf.with_columns(
+    return lf.with_columns(
         mean_expression.alias(DP.ESTIMATE_USING_MEAN),
     )
-
-    return lf
