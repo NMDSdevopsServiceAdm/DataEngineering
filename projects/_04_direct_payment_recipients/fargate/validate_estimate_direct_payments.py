978252a80be0c8a443e48a1e0df5c1fe48870d5d
Roy Price <Roy.price1988@gmail.com>
2026-04-24 14:11:41 +0100

    1551 - Added basic validation script for DPR estimates to check LA area column

---
+++ projects/_04_direct_payment_recipients/fargate/validate_estimate_direct_payments.py
@@ -0,0 +1,87 @@
+import sys
+
+import pointblank as pb
+
+from polars_utils import utils
+from polars_utils.validation import actions as vl
+from polars_utils.validation.constants import GLOBAL_ACTIONS, GLOBAL_THRESHOLDS
+from projects._04_direct_payment_recipients.direct_payments_column_names import (
+    DirectPaymentColumnNames as DP,
+)
+from utils.column_values.categorical_columns_by_dataset import (
+    PostcodeDirectoryCleanedCategoricalValues as CatValues,
+)
+
+
+def main(
+    bucket_name: str, source_path: str, reports_path: str, compare_path: str
+) -> None:
+    """Validates a dataset according to a set of provided rules and produces a summary report as well as failure outputs.
+
+    Args:
+        bucket_name (str): the bucket (name only) in which to source the dataset and output the report to
+            - shoud correspond to workspace / feature branch name
+        source_path (str): the source dataset path to be validated
+        reports_path (str): the output path to write reports to
+        compare_path (str): path to a dataset to compare against for expected size
+    """
+
+    source_df = utils.read_parquet(
+        f"s3://{bucket_name}/{source_path}", exclude_complex_types=True
+    )
+    compare_df = utils.read_parquet(f"s3://{bucket_name}/{compare_path}")
+    expected_row_count = compare_df.height
+
+    validation = (
+        pb.Validate(
+            data=source_df,
+            label=f"Validation of {source_path}",
+            thresholds=GLOBAL_THRESHOLDS,
+            brief=True,
+            actions=GLOBAL_ACTIONS,
+        )
+        # dataset size
+        .row_count_match(
+            expected_row_count,
+            brief=f"Merged DPR data file has {source_df.height} rows but expecting {expected_row_count} rows",
+        )
+        # complete columns
+        .col_vals_not_null(
+            [
+                DP.YEAR_AS_INTEGER,
+                DP.LA_AREA,
+            ]
+        )
+        # categorical
+        .col_vals_in_set(
+            DP.LA_AREA,
+            CatValues.contemporary_cssr_column_values.categorical_values,
+        )
+        # distinct values
+        .specially(
+            vl.is_unique_count_equal(
+                DP.LA_AREA,
+                CatValues.contemporary_cssr_column_values.count_of_categorical_values,
+            ),
+            brief=f"{DP.LA_AREA} needs to be one of {CatValues.contemporary_cssr_column_values.categorical_values}",
+        ).interrogate()
+    )
+    vl.write_reports(validation, bucket_name, reports_path)
+
+
+if __name__ == "__main__":
+    print(f"Validation script called with parameters: {sys.argv}")
+
+    args = utils.get_args(
+        ("--bucket_name", "S3 bucket for source dataset and validation report"),
+        ("--source_path", "The filepath of the dataset to validate"),
+        ("--reports_path", "The filepath to output reports"),
+        (
+            "--compare_path",
+            "The filepath to a dataset to compare against for expected size",
+        ),
+    )
+    print(f"Starting validation for {args.source_path}")
+
+    main(args.bucket_name, args.source_path, args.reports_path, args.compare_path)
+    print(f"Validation of {args.source_path} complete")
