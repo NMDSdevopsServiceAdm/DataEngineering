81e84403b3dc93eda03a6d658866df1931a178eb
Roy Price <Roy.price1988@gmail.com>
2026-04-24 09:01:26 +0100

    1551 - Added test for main succeeds test in test_estimate_direct_payments.

---
+++ projects/_04_direct_payment_recipients/tests/fargate/test_estimate_direct_payments.py
@@ -0,0 +1,44 @@
+import unittest
+from unittest.mock import ANY, Mock, patch
+
+import polars as pl
+import polars.testing as pl_testing
+
+import projects._04_direct_payment_recipients.fargate.estimate_direct_payments as job
+
+PATCH_PATH: str = (
+    "projects._04_direct_payment_recipients.fargate.estimate_direct_payments"
+)
+
+
+class EstimateDirectPaymentsTests(unittest.TestCase):
+    SOME_SOURCE = "some/source"
+    SOME_DESTINATION = "some/destination"
+    SOME_OTHER_DESTINATION = "some/other/destination"
+
+    @patch(f"{PATCH_PATH}.utils.sink_to_parquet")
+    @patch(f"{PATCH_PATH}.utils.scan_parquet")
+    def test_main_succeeds(
+        self,
+        scan_parquet_mock: Mock,
+        sink_to_parquet_mock: Mock,
+    ):
+        job.main(
+            self.SOME_SOURCE,
+            self.SOME_DESTINATION,
+            self.SOME_OTHER_DESTINATION,
+        )
+
+        scan_parquet_mock.assert_called_once_with(
+            source=self.SOME_SOURCE,
+            selected_columns=job.direct_payments_columns,
+        )
+
+        sink_to_parquet_mock.assert_called_once_with(
+            ANY,
+            self.SOME_DESTINATION,
+            append=False,
+        )
+
+    def test_main_returns_expected_values(self):
+        """"""
