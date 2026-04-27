c4602bc4478b2afc381c3b8c3a497418869582f2
Achyut Ratkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2026-04-27 14:13:08 +0100

    refactor the DPR model_extrapolation function to use group_by, agg and join rather than polars expr

--- projects/_04_direct_payment_recipients/fargate/utils/models/extrapolation_ratio.py
+++ projects/_04_direct_payment_recipients/fargate/utils/models/extrapolation_ratio.py
@@ -24,76 +24,64 @@ def model_extrapolation(direct_payments_lf: pl.LazyFrame) -> pl.LazyFrame:
             - estimate_using_extrapolation_ratio: extrapolated proportion for years
                 outside the known data range, null for years within the range.
     """
-    has_data = pl.col(DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF).is_not_null()
+    first_value = "first_value"
+    first_mean = "first_mean"
+    last_value = "last_value"
+    last_mean = "last_mean"
 
-    first_year = (
-        pl.when(has_data)
-        .then(pl.col(DP.YEAR_AS_INTEGER))
-        .min()
-        .over(DP.LA_AREA)
-        .cast(pl.Int32)
-        .alias(DP.FIRST_YEAR_WITH_DATA)
+    boundaries_lf = (
+        direct_payments_lf.filter(
+            pl.col(DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF).is_not_null()
+        )
+        .group_by(DP.LA_AREA)
+        .agg(
+            pl.col(DP.YEAR_AS_INTEGER)
+            .min()
+            .cast(pl.Int32)
+            .alias(DP.FIRST_YEAR_WITH_DATA),
+            pl.col(DP.YEAR_AS_INTEGER)
+            .max()
+            .cast(pl.Int32)
+            .alias(DP.LAST_YEAR_WITH_DATA),
+        )
     )
-    last_year = (
-        pl.when(has_data)
-        .then(pl.col(DP.YEAR_AS_INTEGER))
-        .max()
-        .over(DP.LA_AREA)
-        .cast(pl.Int32)
-        .alias(DP.LAST_YEAR_WITH_DATA)
+    direct_payments_lf = direct_payments_lf.join(
+        boundaries_lf, on=DP.LA_AREA, how="left"
     )
+
+    first_values_lf = direct_payments_lf.filter(
+        pl.col(DP.YEAR_AS_INTEGER) == pl.col(DP.FIRST_YEAR_WITH_DATA)
+    ).select(
+        DP.LA_AREA,
+        pl.col(DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF).alias(first_value),
+        pl.col(DP.ESTIMATE_USING_MEAN).alias(first_mean),
+    )
+
+    last_values_lf = direct_payments_lf.filter(
+        pl.col(DP.YEAR_AS_INTEGER) == pl.col(DP.LAST_YEAR_WITH_DATA)
+    ).select(
+        DP.LA_AREA,
+        pl.col(DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF).alias(last_value),
+        pl.col(DP.ESTIMATE_USING_MEAN).alias(last_mean),
+    )
+
+    direct_payments_lf = direct_payments_lf.join(
+        first_values_lf, on=DP.LA_AREA, how="left"
+    ).join(last_values_lf, on=DP.LA_AREA, how="left")
 
     before_first = pl.col(DP.YEAR_AS_INTEGER) < pl.col(DP.FIRST_YEAR_WITH_DATA)
     after_last = pl.col(DP.YEAR_AS_INTEGER) > pl.col(DP.LAST_YEAR_WITH_DATA)
 
-    extrapolation_ratio = (
+    mean_ratio_first = pl.col(DP.ESTIMATE_USING_MEAN) / pl.col(first_mean)
+    mean_ratio_last = pl.col(DP.ESTIMATE_USING_MEAN) / pl.col(last_mean)
+
+    direct_payments_lf = direct_payments_lf.with_columns(
         pl.when(before_first)
-        .then(
-            pl.col(DP.ESTIMATE_USING_MEAN)
-            / value_at_boundary_year(DP.ESTIMATE_USING_MEAN, DP.FIRST_YEAR_WITH_DATA)
-        )
+        .then(mean_ratio_first * pl.col(first_value))
         .when(after_last)
-        .then(
-            pl.col(DP.ESTIMATE_USING_MEAN)
-            / value_at_boundary_year(DP.ESTIMATE_USING_MEAN, DP.LAST_YEAR_WITH_DATA)
-        )
+        .then(mean_ratio_last * pl.col(last_value))
+        .otherwise(None)
+        .alias(DP.ESTIMATE_USING_EXTRAPOLATION_RATIO)
     )
 
-    data_point = (
-        pl.when(before_first)
-        .then(
-            value_at_boundary_year(
-                DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF, DP.FIRST_YEAR_WITH_DATA
-            )
-        )
-        .when(after_last)
-        .then(
-            value_at_boundary_year(
-                DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF, DP.LAST_YEAR_WITH_DATA
-            )
-        )
-    )
-
-    return direct_payments_lf.with_columns(first_year, last_year).with_columns(
-        (extrapolation_ratio * data_point).alias(DP.ESTIMATE_USING_EXTRAPOLATION_RATIO)
-    )
-
-
-def value_at_boundary_year(value_col: str, boundary_year_col: str) -> pl.Expr:
-    """
-    Returns the value of a column at the boundary year, broadcast across the LA area group.
-
-    Args:
-        value_col (str): Name of the column to retrieve the value from.
-        boundary_year_col (str): Name of the column holding the boundary year to match against.
-
-    Returns:
-        pl.Expr: A Polars expression resolving to the value of `value_col` at the boundary year
-        for each row in the LA area group.
-    """
-    return (
-        pl.when(pl.col(DP.YEAR_AS_INTEGER) == pl.col(boundary_year_col))
-        .then(pl.col(value_col))
-        .max()
-        .over(DP.LA_AREA)
-    )
+    return direct_payments_lf.drop(last_mean, first_value, first_mean, last_value)
