9c15be3421503e9ba89c8511e2ac690ab6679b71
Achyut Ratkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2026-04-27 13:54:11 +0100

    initial push for ocnverting calculate_remaining_variables to Polars

---
+++ projects/_04_direct_payment_recipients/fargate/utils/estimate_direct_payments_utils/calculate_remaining_variables.py
@@ -0,0 +1,15 @@
+import polars as pl
+
+
+def calculate_remaining_variables(
+    lf: pl.LazyFrame,
+) -> pl.LazyFrame:
+    """
+    """
+    service_users_with_self_employed_staff
+    carers_employing_staff
+    total_dpr_employing_staff
+    total_personal_assistant_filled_posts
+    proportion_of_dpr_employing_staff
+    proportion_of_dpr_who_are_service_users
+    return direct_payments_lf
