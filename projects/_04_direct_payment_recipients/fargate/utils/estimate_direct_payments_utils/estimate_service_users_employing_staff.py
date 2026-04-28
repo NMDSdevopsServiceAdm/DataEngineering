6413e3624dd5d73b37021e7910b1c5ef30f9cb8e
Roy Price <Roy.price1988@gmail.com>
2026-04-28 14:46:42 +0100

    1552 - Changed interpolation function so it takes a column to interpolate. Need it to interpolate raw data first, then estimates later on.

--- projects/_04_direct_payment_recipients/fargate/utils/estimate_direct_payments_utils/estimate_service_users_employing_staff.py
+++ projects/_04_direct_payment_recipients/fargate/utils/estimate_direct_payments_utils/estimate_service_users_employing_staff.py
@@ -32,7 +32,7 @@ def calculate_estimated_service_users_employing_staff(lf: pl.LazyFrame) -> pl.La
 
     lf = model_extrapolation(lf)
 
-    lf = model_interpolation(lf)
+    lf = model_interpolation(lf, DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF)
 
     lf = lf.with_columns(
         coalesce_with_source_labels(
@@ -46,23 +46,25 @@ def calculate_estimated_service_users_employing_staff(lf: pl.LazyFrame) -> pl.La
         )
     )
 
-    # lf = lf.drop(DP.ESTIMATE_USING_INTERPOLATION)
-    # lf = model_interpolation(lf)
+    lf = lf.drop(DP.ESTIMATE_USING_INTERPOLATION)
+    lf = model_interpolation(
+        lf, DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF
+    )
 
-    # lf = lf.with_columns(
-    #     pl.coalesce(
-    #         [
-    #             DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
-    #             DP.ESTIMATE_USING_INTERPOLATION,
-    #         ]
-    #     ).alias(DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF)
-    # ).with_columns(
-    #     pl.when(
-    #         pl.col(DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF).is_null()
-    #     )
-    #     .then(pl.lit(DP.ESTIMATE_USING_INTERPOLATION))
-    #     .alias("estimated_proportion_of_service_users_employing_staff_source")
-    # )
+    lf = lf.with_columns(
+        pl.coalesce(
+            [
+                DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF,
+                DP.ESTIMATE_USING_INTERPOLATION,
+            ]
+        ).alias(DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF)
+    ).with_columns(
+        pl.when(
+            pl.col(DP.ESTIMATED_PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF).is_null()
+        )
+        .then(pl.lit(DP.ESTIMATE_USING_INTERPOLATION))
+        .alias("estimated_proportion_of_service_users_employing_staff_source")
+    )
 
     lf = calculate_rolling_mean(lf)
 
--- projects/_04_direct_payment_recipients/tests/fargate/utils/models/test_interpolation.py
+++ projects/_04_direct_payment_recipients/tests/fargate/utils/models/test_interpolation.py
@@ -32,6 +32,8 @@ class TestDPRModelInterpolation(unittest.TestCase):
         }
         expected_lf = pl.LazyFrame(rows, schema=test_schema, orient="row")
         test_lf = expected_lf.drop(DP.ESTIMATE_USING_INTERPOLATION)
-        returned_lf = job.model_interpolation(test_lf)
+        returned_lf = job.model_interpolation(
+            test_lf, DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF
+        )
         pl_testing.assert_frame_equal(returned_lf, expected_lf, check_row_order=False)
         pl_testing.assert_frame_equal(returned_lf, expected_lf, check_row_order=False)
