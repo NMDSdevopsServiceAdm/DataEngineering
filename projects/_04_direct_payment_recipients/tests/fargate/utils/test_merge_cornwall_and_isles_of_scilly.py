89cb013b2fb3ab19bedc1acda710731a51558dd1
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-05-07 14:33:35 +0100

    1568 - Change updated test data to commented out test data

--- projects/_04_direct_payment_recipients/tests/fargate/utils/test_merge_cornwall_and_isles_of_scilly.py
+++ projects/_04_direct_payment_recipients/tests/fargate/utils/test_merge_cornwall_and_isles_of_scilly.py
@@ -22,6 +22,64 @@ class MergeCornwallAndIslesOfScillyTestCase:
         return pytest.param(self.input_data, self.expected_data, id=self.id)
 
 
+# merge_cornwall_and_isles_of_scilly_test_cases = [
+#     MergeCornwallAndIslesOfScillyTestCase(
+#         id="aggregates_single_year",
+#         input_data=[
+#             ("any_other_area",  2020, 1.0, 4.0, 0.1, 0.4, 6.0, 1.9),
+#             ("Cornwall",        2020, 2.0, 5.0, 0.2, 0.5, 7.0, 1.9),
+#             ("Isles of Scilly", 2020, 3.0, 6.0, 0.3, 0.6, 8.0, 1.9),
+#         ],  # fmt: skip
+#         expected_data=[
+#             ("any_other_area",               2020, 1.0, 4.0, 0.1, 0.4, 6.0, 1.9),
+#             ("Cornwall and Isles of Scilly", 2020, 5.0, 11.0, 0.2, 0.5, 15.0, 1.9),
+#         ],  # fmt: skip
+#     ),
+#     MergeCornwallAndIslesOfScillyTestCase(
+#         id="aggregates_multiple_years",
+#         input_data=[
+#             ("any_other_area",  2020, 1.0, 4.0, 0.1, 0.4, 6.0, 1.9),
+#             ("any_other_area",  2021, 1.0, 4.0, 0.1, 0.4, 6.0, 1.9),
+#             ("Cornwall",        2020, 2.0, 5.0, 0.2, 0.5, 7.0, 1.9),
+#             ("Isles of Scilly", 2020, 3.0, 6.0, 0.3, 0.6, 8.0, 1.9),
+#             ("Cornwall",        2021, 2.5, 5.5, 0.25, 0.55, 7.5, 1.9),
+#             ("Isles of Scilly", 2021, 3.5, 6.5, 0.35, 0.65, 8.5, 1.9),
+#         ],  # fmt: skip
+#         expected_data=[
+#             ("any_other_area",               2020, 1.0, 4.0, 0.1, 0.4, 6.0, 1.9),
+#             ("any_other_area",               2021, 1.0, 4.0, 0.1, 0.4, 6.0, 1.9),
+#             ("Cornwall and Isles of Scilly", 2020, 5.0, 11.0, 0.2, 0.5, 15.0, 1.9),
+#             ("Cornwall and Isles of Scilly", 2021, 6.0, 12.0, 0.25, 0.55, 16.0, 1.9),
+#         ],  # fmt: skip
+#     ),
+#     MergeCornwallAndIslesOfScillyTestCase(
+#         id="handles_areas_when_not_in_alphabetical_order",
+#         input_data=[
+#             ("Isles of Scilly", 2020, 3.0, 6.0, 0.3, 0.6, 8.0, 1.9),
+#             ("Isles of Scilly", 2021, 3.5, 6.5, 0.35, 0.65, 8.5, 1.9),
+#             ("Cornwall",        2020, 2.0, 5.0, 0.2, 0.5, 7.0, 1.9),
+#             ("Cornwall",        2021, 2.5, 5.5, 0.25, 0.55, 7.5, 1.9),
+#         ],  # fmt: skip
+#         expected_data=[
+#             ("Cornwall and Isles of Scilly", 2020, 5.0, 11.0, 0.2, 0.5, 15.0, 1.9),
+#             ("Cornwall and Isles of Scilly", 2021, 6.0, 12.0, 0.25, 0.55, 16.0, 1.9),
+#         ],  # fmt: skip
+#     ),
+#     MergeCornwallAndIslesOfScillyTestCase(
+#         id="handles_null_values",
+#         input_data=[
+#             ("Cornwall",        2020, None, None, None, None, None, None),
+#             ("Isles of Scilly", 2020, 3.0, 6.0, 0.3, 0.6, 8.0, 1.9),
+#             ("Cornwall",        2021, None, None, None, None, None, None),
+#             ("Isles of Scilly", 2021, None, None, None, None, None, None),
+#         ],  # fmt: skip
+#         expected_data=[
+#             ("Cornwall and Isles of Scilly", 2020, 3.0, 6.0, None, None, 8.0, None),
+#             ("Cornwall and Isles of Scilly", 2021, None, None, None, None, None, None),
+#         ],  # fmt: skip
+#     ),
+# ]
+
 merge_cornwall_and_isles_of_scilly_test_cases = [
     MergeCornwallAndIslesOfScillyTestCase(
         id="aggregates_single_year",
