2d4a6146f54661477aa4b9965dabe8208c61eaab
Achyut Ratkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2026-05-13 12:04:24 +0100

    Updated docstrings

--- projects/_03_independent_cqc/_02_clean/fargate/utils/clean_ascwds_filled_post_outliers/non_res_brand_id_filter.py
+++ projects/_03_independent_cqc/_02_clean/fargate/utils/clean_ascwds_filled_post_outliers/non_res_brand_id_filter.py
@@ -9,8 +9,14 @@ from utils.column_values.categorical_column_values import AscwdsFilteringRule, C
 
 def non_res_brand_id_filter(lf: pl.LazyFrame) -> pl.LazyFrame:
     """
-    Nulls 'ascwds_filled_posts_dedup_clean' for non-residential
-    locations associated with brand ID 'BD214' within a specified date range.
+    Nulls 'ascwds_filled_posts_dedup_clean' for non-residential locations with a
+    specific brand ID.
+
+    This function identifies non-residential locations with the target brand ID
+    (BD214) and nulls their 'ascwds_filled_posts_dedup_clean' values if they
+    fall within the specified date range (after 1st March 2024 and before 1st
+    May 2026). The filtering rule is updated to 'brand_id_filter' for affected
+    rows.
 
     This is done because the data submitted by the provider BD214 has weird
     jumps from March 2024 to May 2026, which we suspect are not valid. To avoid
@@ -18,16 +24,6 @@ def non_res_brand_id_filter(lf: pl.LazyFrame) -> pl.LazyFrame:
     filter to nullify the 'ascwds_filled_posts_dedup_clean' values for
     non-residential locations with the target brand ID within the specified date
     range.
-
-    Rows are affected when:
-    - 'care_home' equals 'not_care_home'
-    - 'brand_id' equals 'BD214'
-    - 'cqc_location_import_date' is strictly after 1st March 2024
-    - 'cqc_location_import_date' is strictly before 1st May 2026
-
-    For matching rows:
-    - 'ascwds_filled_posts_dedup_clean' is set to null
-    - the filtering rule is updated to 'brand_id_filter'
 
     Args:
         lf (pl.LazyFrame): A polars LazyFrame containing
