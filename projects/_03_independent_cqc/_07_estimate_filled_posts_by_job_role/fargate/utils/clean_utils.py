61c8c6006d89637f7d90aa6495438c327a89aa54
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-05-13 13:16:15 +0100

    1574 - Comment out functions for testing

--- projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/utils/clean_utils.py
+++ projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/utils/clean_utils.py
@@ -123,29 +123,30 @@ def filter_job_role_group_outliers(
     }
     job_role_group_lf = pl.LazyFrame(job_role_group_data, schema=job_role_group_schema)
 
-    filter_lf = filter_lf.join(
+    # filter_lf = filter_lf.join(
+    #     job_role_group_lf, on=IndCQC.main_job_role_clean_labelled, how="left"
+    # )
+    lf = filter_lf.join(
         job_role_group_lf, on=IndCQC.main_job_role_clean_labelled, how="left"
     )
     # 2. Calculate job group ASCWDS count for location, service type and date.
-    agg_lf = (
-        filter_lf.group_by(splits_for_location_sum)
-        .agg(
-            pl.col(IndCQC.id_per_locationid_import_date_job_role),
-            pl.col(IndCQC.ascwds_job_role_counts_cleaned)
-            .sum()
-            .alias(temp_ascwds_job_group_count_column),
-        )
-        .explode(
-            IndCQC.id_per_locationid_import_date_job_role,
-        )
-        .drop(splits_for_location_sum)
-    )  # Drop groups to prevent duplicate columns after join.
+    # agg_lf = (
+    #     filter_lf.group_by(splits_for_location_sum)
+    #     .agg(
+    #         pl.col(IndCQC.id_per_locationid_import_date_job_role),
+    #         pl.col(IndCQC.ascwds_job_role_counts_cleaned)
+    #         .sum()
+    #         .alias(temp_ascwds_job_group_count_column),
+    #     )
+    #     .explode(
+    #         IndCQC.id_per_locationid_import_date_job_role,
+    #     )
+    #     .drop(splits_for_location_sum)
+    # )  # Drop groups to prevent duplicate columns after join.
     # filter_lf = filter_lf.join(
     #     agg_lf, on=IndCQC.id_per_locationid_import_date_job_role, how="left"
     # )
-    lf = filter_lf.join(
-        agg_lf, on=IndCQC.id_per_locationid_import_date_job_role, how="left"
-    )
+
     # 3. Calculate job group percentage of total ASCWDS count for location, service type and date.
     # job_group_percentage_expr = (pl.col(temp_ascwds_job_group_count_column)) / (
     #     pl.col(IndCQC.ascwds_job_role_counts_cleaned).sum()
