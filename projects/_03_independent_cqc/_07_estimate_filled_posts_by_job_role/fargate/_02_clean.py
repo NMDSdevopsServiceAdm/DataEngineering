b8ff23cde67c05c3091e058ce7d3bffe7eb08838
Roy Price <Roy.price1988@gmail.com>
2026-05-21 16:30:22 +0100

    1613 - Fitler out function correctly

--- projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/_02_clean.py
+++ projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/_02_clean.py
@@ -54,13 +54,13 @@ def main(
         IndCQC.ascwds_filled_posts_dedup_clean,
     )
 
-    estimated_job_role_posts_lf = cUtils.filter_job_role_group_equal_zero(
-        estimated_job_role_posts_lf
-    )
-
-    # estimated_job_role_posts_lf = cUtils.filter_job_role_group_outliers(
+    # estimated_job_role_posts_lf = cUtils.filter_job_role_group_equal_zero(
     #     estimated_job_role_posts_lf
     # )
+
+    estimated_job_role_posts_lf = cUtils.filter_job_role_group_outliers(
+        estimated_job_role_posts_lf
+    )
 
     utils.sink_to_parquet(
         lazy_df=estimated_job_role_posts_lf,
