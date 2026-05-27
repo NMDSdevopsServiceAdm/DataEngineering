d08dde023a17f2d3a8893a54912a5741b9f2d6c5
Achyut Ratkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2026-05-27 11:40:38 +0100

    commented out big validations for now

--- projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/validate_01_merge.py
+++ projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/validate_01_merge.py
@@ -87,8 +87,8 @@ def main(
         reports_path (str): the output path to write reports to
     """
 
-    run_key_validation(source_path, compare_path, bucket_name, reports_path)
-    gc.collect()
+    # run_key_validation(source_path, compare_path, bucket_name, reports_path)
+    # gc.collect()
     run_categorical_validation(source_path, bucket_name, reports_path)
     gc.collect()
     run_numeric_validation(source_path, bucket_name, reports_path)
