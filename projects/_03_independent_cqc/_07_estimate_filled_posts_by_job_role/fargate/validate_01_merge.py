63ed1537020f39df8716d6d91a2b99eafe73ec3a
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-05-28 11:25:09 +0100

    Test segmentation on rows distinct

--- projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/validate_01_merge.py
+++ projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/validate_01_merge.py
@@ -112,9 +112,8 @@ def run_distinct_key_validation(source_path, bucket_name, reports_path):
             **VALIDATE_KWARGS,
         )
         .rows_distinct(
+            segments=[IndCqcColumns.id_per_locationid_import_date],
             columns_subset=[
-                IndCqcColumns.location_id,
-                IndCqcColumns.cqc_location_import_date,
                 IndCqcColumns.main_job_role_clean_labelled,
             ],
             brief="Primary key (location_id, cqc_location_import_date, main_job_role_clean_labelled) should be unique",
