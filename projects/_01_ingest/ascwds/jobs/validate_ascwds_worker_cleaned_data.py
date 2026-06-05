61e683e6f7af33d95a2afe40a815f66c42c05b31
Roy Price <Roy.price1988@gmail.com>
2026-06-05 10:38:31 +0100

    1615 - Testing the clean worker validation job

--- projects/_01_ingest/ascwds/jobs/validate_ascwds_worker_cleaned_data.py
+++ projects/_01_ingest/ascwds/jobs/validate_ascwds_worker_cleaned_data.py
@@ -4,6 +4,9 @@ import sys
 os.environ["SPARK_VERSION"] = "3.5"
 
 from utils import utils
+from utils.column_values.categorical_columns_by_dataset import (
+    ASCWDSWorkerCleanedCategoricalValues as CatValues,
+)
 from utils.validation.validation_rules.ascwds_worker_cleaned_validation_rules import (
     ASCWDSWorkerCleanedValidationRules as Rules,
 )
@@ -12,6 +15,23 @@ from utils.validation.validation_utils import validate_dataset
 
 def main(cleaned_ascwds_worker_source: str, report_destination: str):
     cleaned_ascwds_worker_df = utils.read_from_parquet(cleaned_ascwds_worker_source)
+
+    # count distinct values in job role column.
+    print(
+        cleaned_ascwds_worker_df.select(
+            "main_job_role_clean_labelled",
+        ).distinct()
+    )
+
+    print(
+        cleaned_ascwds_worker_df.select(
+            "main_job_role_clean_labelled",
+        )
+        .distinct()
+        .count()
+    )
+
+    print(CatValues.main_job_role_labels_column_values.count_of_categorical_values)
 
     rules = Rules.rules_to_check
 
@@ -24,7 +44,7 @@ if __name__ == "__main__":
     print("Spark job 'validate_ascwds_worker_cleaned_data' starting...")
     print(f"Job parameters: {sys.argv}")
 
-    (cleaned_ascwds_worker_source, report_destination) = utils.collect_arguments(
+    cleaned_ascwds_worker_source, report_destination = utils.collect_arguments(
         (
             "--cleaned_ascwds_worker_source",
             "Source s3 directory for parquet ascwds worker api cleaned dataset",
