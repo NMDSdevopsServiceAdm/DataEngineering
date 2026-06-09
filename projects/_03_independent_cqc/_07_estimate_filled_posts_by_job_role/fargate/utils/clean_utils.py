dbb7424e27002f7cd16647e8dd3ca38231e5ad26
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-06-09 14:30:58 +0100

    1629 - Save out piv_lf too

--- projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/utils/clean_utils.py
+++ projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/utils/clean_utils.py
@@ -107,6 +107,11 @@ def filter_job_role_group_outliers(
             Exprs.job_group_percentage_expr
         )
     )
+    utils.sink_to_parquet(
+        piv_lf,
+        "s3://sfc-1629-test-01-wod-datasets/domain=ind_cqc_filled_posts/dataset=ind_cqc_07_02_clean_job_group_pct/",
+        append=False,
+    )
 
     # 4. Calculate upper and lower percentile bounds of job group percentages for each job group and primary service type.
     bounds_lf = piv_lf.group_by(IndCQC.primary_service_type).agg(
