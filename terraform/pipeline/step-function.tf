ab192086c4ae2ec76aa56ddec9c6a0fe99034d8e
Roy Price <Roy.price1988@gmail.com>
2026-06-17 16:29:18 +0100

    1685 - Added dockerfile, terraform changes and added merge job to slv step function

--- terraform/pipeline/step-function.tf
+++ terraform/pipeline/step-function.tf
@@ -137,18 +137,20 @@ resource "aws_sfn_state_machine" "sf_pipelines" {
     public_subnet_ids = jsonencode(data.aws_subnets.public.ids)
 
     # ecs tasks
-    cqc_api_task_arn               = module.cqc-api.task_arn
-    sfc_internal_task_arn          = module._02_sfc_internal.task_arn
-    independent_cqc_task_arn       = module._03_independent_cqc.task_arn
-    independent_cqc_model_task_arn = module._03_independent_cqc_model.task_arn
-    direct_payments_task_arn       = module._04_direct_payments.task_arn
+    cqc_api_task_arn                   = module.cqc-api.task_arn
+    sfc_internal_task_arn              = module._02_sfc_internal.task_arn
+    independent_cqc_task_arn           = module._03_independent_cqc.task_arn
+    independent_cqc_model_task_arn     = module._03_independent_cqc_model.task_arn
+    direct_payments_task_arn           = module._04_direct_payments.task_arn
+    workforce_characteristics_task_arn = module._07_workforce_characteristics.task_arn
 
     # ecs task security groups
-    cqc_api_security_group_id               = module.cqc-api.security_group_id
-    sfc_internal_security_group_id          = module._02_sfc_internal.security_group_id
-    independent_cqc_security_group_id       = module._03_independent_cqc.security_group_id
-    independent_cqc_model_security_group_id = module._03_independent_cqc_model.security_group_id
-    direct_payments_security_group_id       = module._04_direct_payments.security_group_id
+    cqc_api_security_group_id                   = module.cqc-api.security_group_id
+    sfc_internal_security_group_id              = module._02_sfc_internal.security_group_id
+    independent_cqc_security_group_id           = module._03_independent_cqc.security_group_id
+    independent_cqc_model_security_group_id     = module._03_independent_cqc_model.security_group_id
+    direct_payments_security_group_id           = module._04_direct_payments.security_group_id
+    workforce_characteristics_security_group_id = module._07_workforce_characteristics.security_group_id
 
     # models
     preprocessor_name = "preprocess_non_res_pir"
@@ -306,6 +308,7 @@ resource "aws_iam_policy" "step_function_iam_policy" {
           module._03_independent_cqc.task_arn,
           module._03_independent_cqc_model.task_arn,
           module._04_direct_payments.task_arn,
+          module._07_workforce_characteristics.task_arn,
           aws_ecs_cluster.polars_cluster.arn
         ]
       },
@@ -322,7 +325,9 @@ resource "aws_iam_policy" "step_function_iam_policy" {
           module._03_independent_cqc_model.task_exc_role_arn,
           module._03_independent_cqc_model.task_role_arn,
           module._04_direct_payments.task_exc_role_arn,
-          module._04_direct_payments.task_role_arn
+          module._04_direct_payments.task_role_arn,
+          module._07_workforce_characteristics.task_exc_role_arn,
+          module._07_workforce_characteristics.task_role_arn
         ],
         Condition = {
           StringLike = {
