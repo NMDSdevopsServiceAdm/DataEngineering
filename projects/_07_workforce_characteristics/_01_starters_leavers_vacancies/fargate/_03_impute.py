63ef0227130ff58e4371bc97e3f0cd254cf14f0e
Roy Price <Roy.price1988@gmail.com>
2026-06-19 10:28:16 +0100

    1686 - Added impute, validation and their test scripts.

---
+++ projects/_07_workforce_characteristics/_01_starters_leavers_vacancies/fargate/_03_impute.py
@@ -0,0 +1,38 @@
+from polars_utils import utils
+
+
+def main(
+    cleaned_data_source: str,
+    imputed_data_destination: str,
+) -> None:
+    """
+    Impute values to fill gaps between and around known values.
+
+    Args:
+        cleaned_data_source (str): path to the cleaned data
+        imputed_data_destination (str): destination for output
+    """
+    lf = utils.scan_parquet(cleaned_data_source)
+
+    utils.sink_to_parquet(
+        lazy_df=lf,
+        output_path=imputed_data_destination,
+        append=False,
+    )
+
+
+if __name__ == "__main__":
+    args = utils.get_args(
+        (
+            "--cleaned_data_source",
+            "Source s3 directory for merged data",
+        ),
+        (
+            "--imputed_data_destination",
+            "Destination s3 directory for imputed data",
+        ),
+    )
+    main(
+        cleaned_data_source=args.cleaned_data_source,
+        imputed_data_destination=args.imputed_data_destination,
+    )
