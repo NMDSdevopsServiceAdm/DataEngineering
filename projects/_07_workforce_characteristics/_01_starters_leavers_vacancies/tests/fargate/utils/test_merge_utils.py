15eed995615edade0bc166f9c42e2a2b341f5bba
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-06-24 16:12:40 +0100

    1695 - Set up placeholder function

--- projects/_07_workforce_characteristics/_01_starters_leavers_vacancies/tests/fargate/utils/test_merge_utils.py
+++ projects/_07_workforce_characteristics/_01_starters_leavers_vacancies/tests/fargate/utils/test_merge_utils.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import ANY, Mock, patch
 
 import polars as pl
+import polars.testing as pl_testing
 
 import projects._07_workforce_characteristics._01_starters_leavers_vacancies.fargate.utils.merge_utils as job
 from projects._07_workforce_characteristics.unittest_data.polars_slv_test_data import (
@@ -17,4 +18,6 @@ PATCH_PATH = "projects._07_workforce_characteristics._01_starters_leavers_vacanc
 class TestPlaceholderFunction:
     def test_placeholder_function_does_something(self):
         test_lf = pl.LazyFrame(Data.test, Schema.test, orient="row")
-        pass
+        returned_lf = job.placeholder_function(test_lf)
+        expected_lf = test_lf
+        pl_testing.assert_frame_equal(returned_lf, expected_lf)
