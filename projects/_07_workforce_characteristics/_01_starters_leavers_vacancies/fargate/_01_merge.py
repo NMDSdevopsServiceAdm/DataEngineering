a957bdc7cdeafc38c2f8a75a1d347f9cb18ef0ef
Roy Price <Roy.price1988@gmail.com>
2026-06-25 09:56:58 +0100

    Read in required columns and cast job roles cols to int

--- projects/_07_workforce_characteristics/_01_starters_leavers_vacancies/fargate/_01_merge.py
+++ projects/_07_workforce_characteristics/_01_starters_leavers_vacancies/fargate/_01_merge.py
@@ -1,4 +1,48 @@
+import polars as pl
+
 from polars_utils import utils
+from utils.column_names.cleaned_data_files.ascwds_workplace_cleaned import (
+    AscwdsWorkplaceCleanedColumns as AWPClean,
+)
+
+ascwds_workplace_columns_to_import = [
+    AWPClean.organisation_id,
+    AWPClean.period,
+    AWPClean.establishment_id,
+    AWPClean.establishment_id_from_nmds,
+    AWPClean.parent_id,
+    AWPClean.nmds_id,
+    AWPClean.establishment_created_date,
+    AWPClean.establishment_updated_date,
+    AWPClean.master_update_date,
+    AWPClean.last_logged_in,
+    AWPClean.la_permission,
+    AWPClean.is_bulk_uploader,
+    AWPClean.is_parent,
+    AWPClean.parent_permission,
+    AWPClean.registration_type,
+    AWPClean.provider_id,
+    AWPClean.location_id,
+    AWPClean.establishment_type,
+    AWPClean.establishment_name,
+    AWPClean.address,
+    AWPClean.postcode,
+    AWPClean.region_id,
+    AWPClean.total_staff,
+    AWPClean.worker_records,
+    AWPClean.total_starters,
+    AWPClean.total_leavers,
+    AWPClean.total_vacancies,
+    AWPClean.main_service_id,
+    AWPClean.version,
+    AWPClean.import_date,
+]
+
+job_role_cols = [
+    value
+    for role, value in vars(AWPClean()).items()
+    if role.startswith("job_role_") and "flag" not in role
+]
 
 
 def main(
@@ -12,7 +56,13 @@ def main(
         estimates_source (str): path to the estimates ind cqc filled posts data
         merged_data_destination (str): destination for merged output
     """
-    lf = utils.scan_parquet(estimates_source)
+    lf = utils.scan_parquet(
+        estimates_source,
+        selected_columns=list(set(ascwds_workplace_columns_to_import + job_role_cols)),
+    )
+
+    # cast jr## columns to ints.
+    lf = lf.with_columns(pl.col(*job_role_cols).cast(pl.Int16))
 
     utils.sink_to_parquet(
         lazy_df=lf,
