01ebab14ec8f0135547a1ded621d8007daf749b7
Marion Holloway <marion.holloway@skillsforcare.org.uk>
2026-06-25 13:39:50 +0100

    1699 - Set up validatoin script

---
+++ projects/_01_ingest/ascwds/fargate/validate_clean_ascwds_workplace.py
@@ -0,0 +1,67 @@
+import sys
+
+import pointblank as pb
+
+from polars_utils import utils
+from polars_utils.validation import actions as vl
+from polars_utils.validation.constants import GLOBAL_ACTIONS, GLOBAL_THRESHOLDS
+from utils.column_names.cleaned_data_files.ascwds_workplace_cleaned import (
+    AscwdsWorkplaceCleanedColumns as ASCWPClean,
+)
+
+COMPARE_COLS_TO_IMPORT = [
+    ASCWPClean.establishment_id,
+]
+
+
+def main(
+    bucket_name: str, source_path: str, compare_path: str, reports_path: str
+) -> None:
+    """Validates a dataset according to a set of provided rules and produces a
+        summary report as well as failure outputs.
+
+    Args:
+        bucket_name (str): the bucket (name only) in which to source the dataset
+            and output the report to (should correspond to workspace / feature
+            branch name)
+        source_path (str): the source dataset path to be validated
+        compare_path (str): the path to the dataset to compare against
+        reports_path (str): the output path to write reports to
+    """
+    source_df = utils.read_parquet(source=f"s3://{bucket_name}/{source_path}")
+    compare_df = utils.read_parquet(
+        source=f"s3://{bucket_name}/{compare_path}",
+        selected_columns=COMPARE_COLS_TO_IMPORT,
+    )
+    expected_row_count = compare_df.height
+
+    validation = (
+        pb.Validate(
+            data=source_df,
+            label=f"Validation of {source_path}",
+            thresholds=GLOBAL_THRESHOLDS,
+            brief=True,
+            actions=GLOBAL_ACTIONS,
+        )
+        # dataset size
+        .row_count_match(
+            expected_row_count,
+            brief=f"Expects {expected_row_count} rows",
+        ).interrogate()
+    )
+    vl.write_reports(validation, bucket_name, reports_path)
+
+
+if __name__ == "__main__":
+    print(f"Validation script called with parameters: {sys.argv}")
+
+    args = utils.get_args(
+        ("--bucket_name", "S3 bucket for source dataset and validation report"),
+        ("--source_path", "The filepath of the dataset to validate"),
+        ("--compare_path", "The filepath of the dataset to compare against"),
+        ("--reports_path", "The filepath to output reports"),
+    )
+    print(f"Starting validation for {args.source_path}")
+
+    main(args.bucket_name, args.source_path, args.compare_path, args.reports_path)
+    print(f"Validation of {args.source_path} complete")
