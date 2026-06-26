52243b2790cc7ab58f7855ccc7f3f81f7d9c3ac1
Roy Price <Roy.price1988@gmail.com>
2026-06-26 12:10:05 +0100

    Commented out provider outlier filtering

--- projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/_02_clean.py
+++ projects/_03_independent_cqc/_07_estimate_filled_posts_by_job_role/fargate/_02_clean.py
@@ -102,13 +102,13 @@ def main(
         estimated_job_role_posts_lf
     )
 
-    # estimated_job_role_posts_lf = cUtils.filter_job_role_group_outliers(
-    #     estimated_job_role_posts_lf, id_column=IndCQC.brand_id
-    # )
-
     estimated_job_role_posts_lf = cUtils.filter_job_role_group_outliers(
-        estimated_job_role_posts_lf, id_column=IndCQC.provider_id
+        estimated_job_role_posts_lf, id_column=IndCQC.brand_id
     )
+
+    # estimated_job_role_posts_lf = cUtils.filter_job_role_group_outliers(
+    #     estimated_job_role_posts_lf, id_column=IndCQC.provider_id
+    # )
 
     estimated_job_role_posts_lf = cUtils.filter_job_role_group_outliers(
         estimated_job_role_posts_lf, id_column=IndCQC.location_id
