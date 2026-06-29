733f909800bf258845a4db59788bf4b781fc3434
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2026-06-29 16:43:12 +0100

    1724 - Select reduced list of columns to import

--- projects/_01_ingest/ascwds/fargate/clean_ascwds_workplace.py
+++ projects/_01_ingest/ascwds/fargate/clean_ascwds_workplace.py
@@ -7,6 +7,39 @@ from utils.column_names.cleaned_data_files.ascwds_workplace_cleaned import (
     AscwdsWorkplaceCleanedColumns as AWPClean,
 )
 
+columns_to_import = [
+    AWPClean.organisation_id,
+    AWPClean.period,
+    AWPClean.establishment_id,
+    AWPClean.establishment_id_from_nmds,
+    AWPClean.parent_id,
+    AWPClean.nmds_id,
+    AWPClean.establishment_created_date,
+    AWPClean.establishment_updated_date,
+    AWPClean.master_update_date,
+    AWPClean.last_logged_in,
+    AWPClean.la_permission,
+    AWPClean.is_bulk_uploader,
+    AWPClean.is_parent,
+    AWPClean.parent_permission,
+    AWPClean.registration_type,
+    AWPClean.provider_id,
+    AWPClean.location_id,
+    AWPClean.establishment_type,
+    AWPClean.establishment_name,
+    AWPClean.address,
+    AWPClean.postcode,
+    AWPClean.region_id,
+    AWPClean.total_staff,
+    AWPClean.worker_records,
+    AWPClean.total_starters,
+    AWPClean.total_leavers,
+    AWPClean.total_vacancies,
+    AWPClean.main_service_id,
+    AWPClean.version,
+    AWPClean.import_date,
+]
+
 
 def main(
     workplace_source: str,
@@ -21,7 +54,7 @@ def main(
         cleaned_workplace_destination (str): destination for cleaned ascwds workplace output
         workplace_for_reconciliation_destination (str): destination for reconciliation workplace output
     """
-    lf = utils.scan_parquet(workplace_source)
+    lf = utils.scan_parquet(workplace_source, selected_columns=columns_to_import)
 
     lf = lf.filter(wUtils.valid_workplace_filter())
 
