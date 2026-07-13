0912d308e809358cd5cf01a9a0832b0dc3b4df65
Achyut Ratkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2026-07-13 15:25:09 +0100

    Updated merge job columns to select as list rather than schema

--- projects/_07_workforce_characteristics/_01_starters_leavers_vacancies/fargate/_01_merge.py
+++ projects/_07_workforce_characteristics/_01_starters_leavers_vacancies/fargate/_01_merge.py
@@ -2,30 +2,27 @@ import polars as pl
 
 import projects._07_workforce_characteristics._01_starters_leavers_vacancies.fargate.utils.merge_utils as mUtils
 from polars_utils import utils
-from projects._03_independent_cqc._07_estimate_filled_posts_by_job_role.fargate.utils.utils import (
-    CategoricalColumnTypes as CatColType,
-)
 from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCQC
 
-metadata_columns_schema = {
-    IndCQC.id_per_locationid_import_date: pl.Int64,
-    IndCQC.name: str,
-    IndCQC.provider_id: CatColType.ProviderCatType,
-    IndCQC.brand_id: CatColType.BrandCatType,
-    IndCQC.services_offered: pl.List(str),
-    IndCQC.primary_service_type_second_level: pl.Categorical,
-    IndCQC.care_home: pl.Categorical,
-    IndCQC.dormancy: pl.Categorical,
-    IndCQC.number_of_beds: pl.Int16,
-    IndCQC.imputed_registration_date: pl.Date,
-    IndCQC.ascwds_workplace_import_date: pl.Date,
-    IndCQC.establishment_id: CatColType.EstablishmentCatType,
-    IndCQC.organisation_id: str,
-    IndCQC.worker_records_bounded: pl.Int16,
-    IndCQC.ascwds_filled_posts_dedup_clean: pl.Float32,
-    IndCQC.ascwds_pir_merged: pl.Float32,
-    IndCQC.ascwds_filtering_rule: pl.Categorical,
-    IndCQC.estimate_filled_posts_source: CatColType.EstimatesFilledPostSourceEnumType,
+metadata_columns = {
+    IndCQC.id_per_locationid_import_date,
+    IndCQC.name,
+    IndCQC.provider_id,
+    IndCQC.brand_id,
+    IndCQC.services_offered,
+    IndCQC.primary_service_type_second_level,
+    IndCQC.care_home,
+    IndCQC.dormancy,
+    IndCQC.number_of_beds,
+    IndCQC.imputed_registration_date,
+    IndCQC.ascwds_workplace_import_date,
+    IndCQC.establishment_id,
+    IndCQC.organisation_id,
+    IndCQC.worker_records_bounded,
+    IndCQC.ascwds_filled_posts_dedup_clean,
+    IndCQC.ascwds_pir_merged,
+    IndCQC.ascwds_filtering_rule,
+    IndCQC.estimate_filled_posts_source,
 }
 
 
@@ -48,7 +45,7 @@ def main(
     # mUtils.create_list_of_cols_for_ascwds()
 
     metadata_lf = utils.scan_parquet(
-        source=metadata_source, schema=metadata_columns_schema
+        source=metadata_source, selected_columns=metadata_columns
     )
     job_role_estimates_lf = utils.scan_parquet(job_role_estimates_source)
     cleaned_ascwds_workplace_lf = utils.scan_parquet(cleaned_ascwds_workplace_source)
