7febd1040fef859f5fd0ffcbe87dca2603a67248
Achyut Ratkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2026-07-13 12:22:53 +0100

    Added polars_streaming comments

--- projects/_04_direct_payment_recipients/fargate/utils/models/extrapolation_ratio.py
+++ projects/_04_direct_payment_recipients/fargate/utils/models/extrapolation_ratio.py
@@ -34,6 +34,7 @@ def model_extrapolation(direct_payments_lf: pl.LazyFrame) -> pl.LazyFrame:
         direct_payments_lf.filter(
             pl.col(DP.PROPORTION_OF_SERVICE_USERS_EMPLOYING_STAFF).is_not_null()
         )
+        # polars_streaming: groupby-agg-join workaround; could be .min().over() and .max().over() when window functions support streaming
         .group_by(DP.LA_AREA)
         .agg(
             pl.col(DP.YEAR_AS_INTEGER)
