7108d6e1a7406d7f7450761e080a6c8e31187741
Achyut Ratkanthiwar <achyut.ratkanthiwar@skillsforcare.org.uk>
2026-07-31 13:06:12 +0100

    black formatting

--- projects/_01_ingest/cqc_pir/jobs/validate_pir_raw_data.py
+++ projects/_01_ingest/cqc_pir/jobs/validate_pir_raw_data.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     print("Spark job 'validate_pir_api_raw_data' starting...")
     print(f"Job parameters: {sys.argv}")
 
-    (raw_cqc_pir_source, report_destination) = utils.collect_arguments(
+    raw_cqc_pir_source, report_destination = utils.collect_arguments(
         (
             "--raw_cqc_pir_source",
             "Source s3 directory for parquet pir api raw dataset",
