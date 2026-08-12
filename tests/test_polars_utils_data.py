3a70ab80a8bf35ab8c5ae78ea4433551bf05b305
Gary Polzin <gary.polzin@skillsforcare.org.uk>
2026-08-12 12:49:38 +0100

    1882 - Renamed in line with #1883 changes

--- tests/test_polars_utils_data.py
+++ tests/test_polars_utils_data.py
@@ -427,54 +427,54 @@ class CategoricalColumnTypeCase:
 class ColumnTypesData:
     categorical_column_type_cases = [
         CategoricalColumnTypeCase(
-            id="location_cat_type_uses_cqc_location_namespace",
+            id="location_cat_type",
             actual=CatColType.LocationCatType,
             expected=pl.Categorical(
                 pl.Categories("location", namespace="cqc_location")
             ),
         ),
         CategoricalColumnTypeCase(
-            id="establishment_cat_type_uses_ascwds_workplace_namespace",
+            id="establishment_cat_type",
             actual=CatColType.EstablishmentCatType,
             expected=pl.Categorical(
                 pl.Categories("establishment", namespace="ascwds_workplace")
             ),
         ),
         CategoricalColumnTypeCase(
-            id="provider_cat_type_uses_cqc_location_namespace",
+            id="provider_cat_type",
             actual=CatColType.ProviderCatType,
             expected=pl.Categorical(
                 pl.Categories("provider", namespace="cqc_location")
             ),
         ),
         CategoricalColumnTypeCase(
-            id="brand_cat_type_uses_cqc_location_namespace",
+            id="brand_cat_type",
             actual=CatColType.BrandCatType,
             expected=pl.Categorical(pl.Categories("brand", namespace="cqc_location")),
         ),
         CategoricalColumnTypeCase(
-            id="job_role_enum_type_matches_main_job_role_labels_values",
+            id="job_role_enum_type",
             actual=CatColType.JobRoleEnumType,
             expected=pl.Enum(
                 CatVals.main_job_role_labels_column_values.categorical_values
             ),
         ),
         CategoricalColumnTypeCase(
-            id="job_group_enum_type_matches_main_job_group_labels_values",
+            id="job_group_enum_type",
             actual=CatColType.JobGroupEnumType,
             expected=pl.Enum(
                 CatVals.main_job_group_labels_column_values.categorical_values
             ),
         ),
         CategoricalColumnTypeCase(
-            id="published_job_role_label_enum_type_matches_published_job_role_labels_values",
+            id="published_job_role_label_enum_type",
             actual=CatColType.PublishedJobRoleLabelEnumType,
             expected=pl.Enum(
                 SLVPrepareCategoricalValues.published_job_role_labels_column_values.categorical_values
             ),
         ),
         CategoricalColumnTypeCase(
-            id="estimates_filled_post_source_enum_type_covers_all_sources",
+            id="estimates_filled_post_source_enum_type",
             actual=CatColType.EstimatesFilledPostSourceEnumType,
             expected=pl.Enum(
                 [
@@ -489,14 +489,14 @@ class ColumnTypesData:
             ),
         ),
         CategoricalColumnTypeCase(
-            id="primary_service_enum_type_covers_all_service_types",
+            id="primary_service_enum_type",
             actual=CatColType.PrimaryServiceEnumType,
             expected=pl.Enum(
                 CQCLocationCatVals.primary_service_type_column_values.categorical_values
             ),
         ),
         CategoricalColumnTypeCase(
-            id="job_role_filtering_rule_cat_type_uses_uint8_physical_type",
+            id="job_role_filtering_rule_cat_type",
             actual=CatColType.JobRoleFilteringRuleCatType,
             expected=pl.Categorical(
                 pl.Categories(
