df1adc6f2fa5296e32ad3b740f1c477573b439e6
Roy Price <Roy.price1988@gmail.com>
2026-08-18 11:30:27 +0100

    1889 - Clarified TestFlagRowsEligibleForImputation

--- projects/_03_independent_cqc/unittest_data/polars_ind_cqc_test_file_schemas.py
+++ projects/_03_independent_cqc/unittest_data/polars_ind_cqc_test_file_schemas.py
@@ -1656,6 +1656,8 @@ class ModelExtrapolation:
 
 @dataclass
 class ModelImputation:
+    row_id = "row_id"
+
     expected_model_imputation_schema = {
         IndCQC.location_id: pl.String,
         IndCQC.cqc_location_import_date: pl.Date,
@@ -1666,14 +1668,14 @@ class ModelImputation:
     }
 
     input_flag_rows_eligible_for_imputation_schema = {
-        "row_id": pl.Int8,
+        row_id: pl.Int8,
         IndCQC.location_id: pl.String,
         IndCQC.care_home: pl.String,
         "null_values": pl.Float32,
     }
 
     expected_flag_rows_eligible_for_imputation_schema = {
-        "row_id": pl.Int8,
+        row_id: pl.Int8,
         IndCQC.location_id: pl.String,
         IndCQC.care_home: pl.String,
         "null_values": pl.Float32,
--- projects/_03_independent_cqc/utils/tests/imputation/test_imputation.py
+++ projects/_03_independent_cqc/utils/tests/imputation/test_imputation.py
@@ -13,6 +13,7 @@ from projects._03_independent_cqc.unittest_data.polars_ind_cqc_test_file_data im
 from projects._03_independent_cqc.unittest_data.polars_ind_cqc_test_file_schemas import (
     ModelImputation as Schemas,
 )
+from utils.column_names.ind_cqc_pipeline_columns import Imputation
 from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCqc
 from utils.column_values.categorical_column_values import CareHome
 
@@ -90,76 +91,56 @@ class TestModelImputationResults:
         )
 
 
-@dataclass
-class FlagRowsEligibleForImputationTestCase:
-    id: str
-    care_home: bool
-    expected_eligible_row_ids: list[int]
-
-    def as_pytest_param(self):
-        return pytest.param(
-            (self.care_home, self.expected_eligible_row_ids), id=self.id
-        )
-
-
-flag_rows_eligible_for_imputation_test_cases = [
-    FlagRowsEligibleForImputationTestCase(
-        id="flags_care_home_locations_with_at_least_one_value",
-        care_home=True,
-        expected_eligible_row_ids=[1, 2, 3],
-    ),
-    FlagRowsEligibleForImputationTestCase(
-        id="flags_non_care_home_locations_with_at_least_one_value",
-        care_home=False,
-        expected_eligible_row_ids=[6, 7, 8],
-    ),
-]
-
-
 class TestFlagRowsEligibleForImputation:
-    @pytest.mark.parametrize(
-        "test_case",
-        [
-            case.as_pytest_param()
-            for case in flag_rows_eligible_for_imputation_test_cases
-        ],
-    )
-    def test_function_flags_expected_rows(self, test_case: tuple[bool, list[int]]):
-        care_home, expected_eligible_row_ids = test_case
-        input_data: list[Any] = [
-            (1, "1-001", CareHome.care_home, 10.0),
-            (2, "1-001", CareHome.care_home, None),
-            (3, "1-002", CareHome.care_home, 10.0),
-            (4, "1-003", CareHome.care_home, None),
-            (5, "1-003", CareHome.care_home, None),
-            (6, "1-004", CareHome.not_care_home, 10.0),
-            (7, "1-004", CareHome.not_care_home, None),
-            (8, "1-005", CareHome.not_care_home, 10.0),
-            (9, "1-006", CareHome.not_care_home, None),
-            (10, "1-006", CareHome.not_care_home, None),
-        ]
+    def test_function_flags_expected_rows(self):
         input_lf = pl.LazyFrame(
-            data=input_data,
+            data=[
+                (1, "1-001", CareHome.care_home, 10.0),
+                (2, "1-001", CareHome.care_home, None),
+                (3, "1-002", CareHome.care_home, 10.0),
+                (4, "1-003", CareHome.care_home, None),
+                (5, "1-003", CareHome.care_home, None),
+                (6, "1-004", CareHome.not_care_home, 10.0),
+                (7, "1-004", CareHome.not_care_home, None),
+                (8, "1-005", CareHome.not_care_home, 10.0),
+                (9, "1-006", CareHome.not_care_home, None),
+                (10, "1-006", CareHome.not_care_home, None),
+            ],
             schema=Schemas.input_flag_rows_eligible_for_imputation_schema,
             orient="row",
         )
-        expected_data = [
-            row + (row[0] in expected_eligible_row_ids,) for row in input_data
-        ]
-        expected_lf = pl.LazyFrame(
-            data=expected_data,
-            schema=Schemas.expected_flag_rows_eligible_for_imputation_schema,
-            orient="row",
-        )
 
-        returned_lf = job.flag_rows_eligible_for_imputation(
+        returned_when_care_home_lf = job.flag_rows_eligible_for_imputation(
             input_lf,
             Data.column_with_null_values_name,
-            care_home=care_home,
+            care_home=True,
+        )
+        returned_when_not_care_home_lf = job.flag_rows_eligible_for_imputation(
+            input_lf,
+            Data.column_with_null_values_name,
+            care_home=False,
         )
 
-        pl_testing.assert_frame_equal(
-            returned_lf,
-            expected_lf,
-            check_row_order=False,
+        expected_rowid_when_care_home_returned_lf = [1, 2, 3]
+        expected_rowid_when_not_care_home_returned_lf = [6, 7, 8]
+
+        assert (
+            returned_when_care_home_lf.filter(
+                pl.col(Imputation.eligible_for_imputation)
+            )
+            .select(Schemas.row_id)
+            .collect()
+            .to_series()
+            .to_list()
+            == expected_rowid_when_care_home_returned_lf
+        )
+        assert (
+            returned_when_not_care_home_lf.filter(
+                pl.col(Imputation.eligible_for_imputation)
+            )
+            .select(Schemas.row_id)
+            .collect()
+            .to_series()
+            .to_list()
+            == expected_rowid_when_not_care_home_returned_lf
         )
