aee6dcb4f7034772d1a413072f5cf53222a86e5e
Roy Price <Roy.price1988@gmail.com>
2026-08-18 15:32:00 +0100

    1889 - casted imputed_column_name back to float32

--- projects/_03_independent_cqc/utils/imputation/imputation.py
+++ projects/_03_independent_cqc/utils/imputation/imputation.py
@@ -75,7 +75,7 @@ def model_imputation(
                 IndCqc.interpolation_model,
             )
         )
-        .cast(pl.Float64)
+        .cast(pl.Float32)
         .alias(imputed_column_name)
     ).drop(
         IndCqc.extrapolation_forwards,
