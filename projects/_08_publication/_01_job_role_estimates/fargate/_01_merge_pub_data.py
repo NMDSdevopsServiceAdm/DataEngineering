fa7092ab4ca69e501eef9bf4350f434cc9d396b0
Roy Price <Roy.price1988@gmail.com>
2026-09-07 14:18:03 +0100

    1979 - printing row count

--- projects/_08_publication/_01_job_role_estimates/fargate/_01_merge_pub_data.py
+++ projects/_08_publication/_01_job_role_estimates/fargate/_01_merge_pub_data.py
@@ -48,13 +48,19 @@ def main(
         pl.col(IndCQC.location_id).cast(CatColType.LocationCatType)
     )
 
+    print(f"After scanning: {jr_estimates_lf.collect().height}")
+
     jr_estimates_lf = jr_estimates_lf.join(
         metadata_lf, on=IndCQC.id_per_locationid_import_date, how="left"
     )
 
+    print(f"After joining metadata: {jr_estimates_lf.collect().height}")
+
     jr_estimates_lf = jr_estimates_lf.join(
         geography_lf, on=IndCQC.location_id, how="left"
     )
+
+    print(f"After joining geography: {jr_estimates_lf.collect().height}")
 
     utils.sink_to_parquet(
         lazy_df=jr_estimates_lf,
