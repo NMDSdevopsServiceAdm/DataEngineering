40b09865b1b8bb6d3daf69a4eecfe76677905ee8
Roy Price <Roy.price1988@gmail.com>
2026-09-10 15:11:25 +0100

    1978 - Tidied up test data

--- projects/_99_publication/monthly_tracker_filled_posts/tests/fargate/utils/test_clean_utils.py
+++ projects/_99_publication/monthly_tracker_filled_posts/tests/fargate/utils/test_clean_utils.py
@@ -3,42 +3,38 @@ import polars.testing as pl_testing
 import pytest
 
 import projects._99_publication.monthly_tracker_filled_posts.fargate.utils.clean_utils as job
-from projects._99_publication.unittest_data.polars_pub_test_data import (
-    HAS_COLUMN_DATA_SINCE_DATE_TEST_CASES,
-    HasColumnDataSinceDateTestCase,
-)
+import projects._99_publication.unittest_data.polars_pub_test_data as Data
 from utils.column_names.ind_cqc_pipeline_columns import IndCqcColumns as IndCQC
-
-CT_HAS_DATA_SCHEMA = pl.Schema(
-    [
-        (IndCQC.location_id, pl.String()),
-        (IndCQC.cqc_location_import_date, pl.Date()),
-        (IndCQC.ct_care_home_total_employed_imputed, pl.Float32()),
-        (IndCQC.ct_non_res_care_workers_employed_imputed, pl.Float32()),
-    ]
-)
 
 
 class TestHasColumnDataSinceDate:
+    has_column_data_since_data_schema = pl.Schema(
+        [
+            (IndCQC.location_id, pl.String()),
+            (IndCQC.cqc_location_import_date, pl.Date()),
+            (IndCQC.ct_care_home_total_employed_imputed, pl.Float32()),
+            (IndCQC.ct_non_res_care_workers_employed_imputed, pl.Float32()),
+        ]
+    )
+
     @pytest.mark.parametrize(
         "case",
-        [case.as_pytest_param() for case in HAS_COLUMN_DATA_SINCE_DATE_TEST_CASES],
+        [case.as_pytest_param() for case in Data.has_column_data_since_date_test_cases],
     )
-    def test_flags_locations_with_column_data_since_a_given_date(
-        self, case: HasColumnDataSinceDateTestCase
-    ):
-        test_lf = pl.LazyFrame(case.test_data, CT_HAS_DATA_SCHEMA, orient="row")
+    def test_flags_locations_with_column_data_since_a_given_date(self, case):
+        expected_schema = pl.Schema(
+            list(self.has_column_data_since_data_schema.items())
+            + [(case.column_alias, pl.Boolean())]
+        )
+        expected_lf = pl.LazyFrame(case.expected_data, expected_schema, orient="row")
+
+        test_lf = expected_lf.drop(case.column_alias)
 
         returned_lf = test_lf.with_columns(
             job.has_column_data_since_date(
                 case.column_name, case.from_date, case.column_alias
             )
         )
-
-        expected_schema = pl.Schema(
-            list(CT_HAS_DATA_SCHEMA.items()) + [(case.column_alias, pl.Boolean())]
-        )
-        expected_lf = pl.LazyFrame(case.expected_data, expected_schema, orient="row")
 
         pl_testing.assert_frame_equal(returned_lf, expected_lf)
 
--- projects/_99_publication/unittest_data/polars_pub_test_data.py
+++ projects/_99_publication/unittest_data/polars_pub_test_data.py
@@ -11,7 +11,6 @@ from utils.column_names.publication_columns import PublicationColumns as Pub
 @dataclass
 class HasColumnDataSinceDateTestCase:
     id: str
-    test_data: list[Any]
     column_name: str
     from_date: date
     column_alias: str
@@ -21,13 +20,9 @@ class HasColumnDataSinceDateTestCase:
         return pytest.param(self, id=self.id)
 
 
-HAS_COLUMN_DATA_SINCE_DATE_TEST_CASES = [
+has_column_data_since_date_test_cases = [
     HasColumnDataSinceDateTestCase(
         id="true_when_no_nulls_in_the_checked_column_since_cutoff",
-        test_data=[
-            ("1-001", date(2021, 7, 1), 5.0, None),
-            ("1-001", date(2022, 7, 1), 6.0, None),
-        ],
         column_name=IndCQC.ct_care_home_total_employed_imputed,
         from_date=date(2021, 7, 1),
         column_alias=Pub.ct_care_home_has_data_short_term,
@@ -38,10 +33,6 @@ HAS_COLUMN_DATA_SINCE_DATE_TEST_CASES = [
     ),
     HasColumnDataSinceDateTestCase(
         id="false_when_the_checked_column_has_a_null_since_cutoff",
-        test_data=[
-            ("1-002", date(2021, 7, 1), None, 3.0),
-            ("1-002", date(2022, 7, 1), 5.0, 4.0),
-        ],
         column_name=IndCQC.ct_care_home_total_employed_imputed,
         from_date=date(2021, 7, 1),
         column_alias=Pub.ct_care_home_has_data_short_term,
@@ -52,9 +43,6 @@ HAS_COLUMN_DATA_SINCE_DATE_TEST_CASES = [
     ),
     HasColumnDataSinceDateTestCase(
         id="false_for_location_with_no_rows_on_or_after_cutoff",
-        test_data=[
-            ("1-003", date(2020, 7, 1), 5.0, 5.0),
-        ],
         column_name=IndCQC.ct_care_home_total_employed_imputed,
         from_date=date(2021, 7, 1),
         column_alias=Pub.ct_care_home_has_data_short_term,
@@ -64,10 +52,6 @@ HAS_COLUMN_DATA_SINCE_DATE_TEST_CASES = [
     ),
     HasColumnDataSinceDateTestCase(
         id="honours_the_column_name_argument",
-        test_data=[
-            ("1-004", date(2021, 7, 1), None, 3.0),
-            ("1-004", date(2022, 7, 1), None, 4.0),
-        ],
         column_name=IndCQC.ct_non_res_care_workers_employed_imputed,
         from_date=date(2021, 7, 1),
         column_alias=Pub.ct_non_res_has_data_short_term,
@@ -78,9 +62,6 @@ HAS_COLUMN_DATA_SINCE_DATE_TEST_CASES = [
     ),
     HasColumnDataSinceDateTestCase(
         id="honours_a_non_default_column_alias",
-        test_data=[
-            ("1-005", date(2025, 4, 1), 5.0, None),
-        ],
         column_name=IndCQC.ct_care_home_total_employed_imputed,
         from_date=date(2025, 4, 1),
         column_alias=Pub.ct_care_home_has_data_medium_term,
@@ -90,13 +71,6 @@ HAS_COLUMN_DATA_SINCE_DATE_TEST_CASES = [
     ),
     HasColumnDataSinceDateTestCase(
         id="false_for_location_missing_a_period_another_location_has",
-        test_data=[
-            ("1-006", date(2021, 7, 1), 5.0, None),
-            ("1-006", date(2022, 7, 1), 5.0, None),
-            ("1-007", date(2021, 7, 1), 5.0, None),
-            ("1-007", date(2022, 7, 1), 5.0, None),
-            ("1-007", date(2023, 7, 1), 5.0, None),
-        ],
         column_name=IndCQC.ct_care_home_total_employed_imputed,
         from_date=date(2021, 7, 1),
         column_alias=Pub.ct_care_home_has_data_short_term,
@@ -110,11 +84,6 @@ HAS_COLUMN_DATA_SINCE_DATE_TEST_CASES = [
     ),
     HasColumnDataSinceDateTestCase(
         id="duplicate_rows_at_the_same_date_do_not_inflate_the_count",
-        test_data=[
-            ("1-008", date(2021, 7, 1), 5.0, None),
-            ("1-008", date(2021, 7, 1), 5.0, None),
-            ("1-008", date(2022, 7, 1), 6.0, None),
-        ],
         column_name=IndCQC.ct_care_home_total_employed_imputed,
         from_date=date(2021, 7, 1),
         column_alias=Pub.ct_care_home_has_data_short_term,
